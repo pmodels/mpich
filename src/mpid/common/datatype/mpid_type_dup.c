2394c1a88cfd7627a68974a5d17e82f6ec003877
David Goodell <goodell@mcs.anl.gov>
2007-11-26 21:58:33 +0000

    [svn-r209] Fix deref of NULL by some datatype debugging code when duplicating a builtin datatype.

--- src/mpid/common/datatype/mpid_type_dup.c
+++ src/mpid/common/datatype/mpid_type_dup.c
@@ -98,7 +98,7 @@ int MPID_Type_dup(MPI_Datatype oldtype,
 	}
     }
 
-    MPIU_DBG_MSG_P(DATATYPE,VERBOSE,"dup type %x created.", new_dtp->handle);
+    MPIU_DBG_MSG_D(DATATYPE,VERBOSE, "dup type %x created.", *newtype);
 
  fn_fail:
     return mpi_errno;
