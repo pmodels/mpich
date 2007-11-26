43026324c1f60b00d0be3ac36cc81b69b0bbf401
David Goodell <goodell@mcs.anl.gov>
2007-11-26 21:54:56 +0000

    [svn-r208] Fix a warning about an uninitialized variable that you only get when you compile with --enable-strict.

--- src/mpi/spawn/comm_spawn_multiple.c
+++ src/mpi/spawn/comm_spawn_multiple.c
@@ -68,7 +68,7 @@ int MPI_Comm_spawn_multiple(int count, char *array_of_commands[],
     int mpi_errno = MPI_SUCCESS, i;
     MPID_Comm *comm_ptr = NULL;
     MPID_Comm *intercomm_ptr = NULL;
-    MPID_Info **array_of_info_ptrs;
+    MPID_Info **array_of_info_ptrs = NULL;
     MPIU_CHKLMEM_DECL(1);
     MPID_MPI_STATE_DECL(MPID_STATE_MPI_COMM_SPAWN_MULTIPLE);
 
