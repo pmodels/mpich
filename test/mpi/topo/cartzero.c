fdc4629e5031c005b533cb9d8b7e2e92df875bb7
William Gropp <gropp@mcs.anl.gov>
2008-04-02 19:29:16 +0000

    [svn-r782] Added test for zero-dimensional cartesian

---
+++ test/mpi/topo/cartzero.c
@@ -0,0 +1,62 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ *
+ *  (C) 2008 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+#include "mpi.h"
+#include <stdio.h>
+#include "mpitest.h"
+
+/*  
+    Check that the MPI implementation properly handles zero-dimensional
+    Cartesian communicators - the original standard implies that these
+    should be consistent with higher dimensional topologies and thus 
+    these should work with any MPI implementation.  MPI 2.1 made this
+    requirement explicit.
+*/
+int main( int argc, char *argv[] )
+{
+    int errs = 0;
+    int size, rank;
+    int dims[2], periods[2];
+    MPI_Comm comm;
+
+    MTest_Init( &argc, &argv );
+
+    /* Create a new cartesian communicator in a subset of the processes */
+    MPI_Comm_size( MPI_COMM_WORLD, &size );
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+    if (size < 2) {
+	fprintf( stderr, "This test needs at least 2 processes\n" );
+	MPI_Abort( MPI_COMM_WORLD, 1 );
+    }
+    /* None of these values should be relevant */
+    dims[0]    = 1;
+    periods[0] = 1;
+    MPI_Cart_create( MPI_COMM_WORLD, 0, dims, periods, 0, &comm );
+
+    if (comm != MPI_COMM_NULL) {
+	int csize;
+	MPI_Comm_size( comm, &csize );
+	if (csize != 1) {
+	    errs++;
+	    fprintf( stderr, 
+	     "Sizes is wrong in cart communicator.  Is %d, should be 1\n", 
+	     csize ); 
+	}
+	MPI_Barrier( comm );
+
+	MPI_Comm_free( &comm );
+    } 
+    else if (rank == 0) {
+	errs++;
+	fprintf( stderr, "Communicator returned is null!" );
+    }
+
+    MTest_Finalize( errs );
+
+    MPI_Finalize();
+
+    return 0;
+}
