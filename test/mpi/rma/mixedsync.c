15707753b80530af31ea45d6274e34ff54bde076
William Gropp <gropp@mcs.anl.gov>
2008-05-05 17:58:28 +0000

    [svn-r896] Added two rma tests - communication with self, and mixing of rma syncrhonization modes.  These are based on gaps in the coverage analysis.  The mixedsync test occasionally fails on my mac, probably because of a race that exposes a problem in the code that the coverage test identified as untested

---
+++ test/mpi/rma/mixedsync.c
@@ -0,0 +1,108 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ *
+ *  (C) 2003 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "mpitest.h"
+
+static char MTEST_Descrip[] = "Mix synchronization types";
+
+void delay( double time )
+{
+    double t1;
+    t1 = MPI_Wtime();
+    while (MPI_Wtime() - t1 < time) ;
+}
+
+int main( int argc, char *argv[] )
+{
+    int      errs = 0, err;
+    int      crank, csize, source, dest;
+    int      *buf0, *buf1, *buf2, count0, count1, count2, count, i;
+    MPI_Comm comm;
+    MPI_Win  win;
+    int      *winbuf;
+
+    MTest_Init( &argc, &argv );
+
+    comm = MPI_COMM_WORLD;
+
+    count0 = 1000;
+    count1 = 1;
+    count2 = 100;
+
+    count = count0 + count1 + count2 + 2;
+    
+    /* Allocate and initialize the local buffers */
+    buf0 = (int *)malloc( count0 * sizeof(int) );
+    buf1 = (int *)malloc( count1 * sizeof(int) );
+    buf2 = (int *)malloc( count2 * sizeof(int) );
+    if (!buf0 || !buf1 || !buf2) {
+	fprintf( stderr, "Unable to allocated buf0-2\n" );
+	MPI_Abort( MPI_COMM_WORLD, 1 );
+    }
+    for (i=0; i<count0; i++) buf0[i] = i;
+    for (i=0; i<count1; i++) buf1[i] = i + count0;
+    for (i=0; i<count2; i++) buf2[i] = i + count0 + count1;
+
+    /* Allocate the window buffer and create the memory window. */
+    MPI_Alloc_mem( count*sizeof(int), MPI_INFO_NULL, &winbuf );
+    if (!winbuf) {
+	fprintf( stderr, "Unable to allocate %d words\n", count );
+	MPI_Abort( MPI_COMM_WORLD, 0 );
+    }
+    MPI_Win_create( winbuf, count*sizeof(int), sizeof(int), MPI_INFO_NULL, 
+		    comm, &win );
+
+    MPI_Comm_size( comm, &csize );
+    MPI_Comm_rank( comm, &crank );
+    dest   = 0;
+    source = 1;
+
+    /* Perform several communication operations, mixing synchronization
+       types.  Use multiple communication to avoid the single-operation
+       optimization that may be present. */
+    MPI_Barrier( comm );
+    if (crank == source) {
+	MPI_Win_lock( MPI_LOCK_EXCLUSIVE, dest, 0, win );
+	MPI_Put( buf0, count0, MPI_INT, dest, 0, count0, MPI_INT, win );
+	MPI_Put( buf1, count1, MPI_INT, dest, count0, count1, MPI_INT, win );
+	MPI_Put( buf2, count2, MPI_INT, dest, count0+count1, count2, MPI_INT, win );
+	MPI_Win_unlock( dest, win );
+    }
+    else if (crank == dest) {
+	/* Just delay a bit */
+	delay( 0.0001 );
+    }
+    MPI_Win_fence( 0, win );
+    if (crank == source) {
+	MPI_Put( buf0, count0, MPI_INT, dest, 1, count0, MPI_INT, win );
+	MPI_Put( buf1, count1, MPI_INT, dest, 1+count0, count1, MPI_INT, win );
+	MPI_Put( buf2, count2, MPI_INT, dest, 1+count0+count1, count2, MPI_INT, win );
+    }
+    MPI_Win_fence( 0, win );
+
+    /* Check results */
+    if (crank == dest) {
+	for (i=0; i<count0+count1+count2; i++) {
+	    if (winbuf[1+i] != i) {
+		errs++;
+		if (errs < 10) {
+		    fprintf( stderr, "winbuf[%d] = %d, expected %d\n",
+			     1+i, winbuf[1+i], i ); fflush(stderr);
+		}
+	    }
+	}
+    }
+
+    MPI_Win_free( &win );
+    MPI_Free_mem( winbuf );
+
+    MTest_Finalize( errs );
+
+    return 0;
+}
---
+++ test/mpi/rma/selfrma.c
@@ -0,0 +1,106 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ *
+ *  (C) 2003 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "mpitest.h"
+
+static char MTEST_Descrip[] = "RMA to self";
+
+int main( int argc, char *argv[] )
+{
+    int errs = 0, err;
+    int rank, size, source, dest, i, j;
+    MPI_Comm      comm;
+    MPI_Win       win;
+    int           *winbuf, count;
+    int           *sbuf, scount, vcount;
+    MPI_Datatype  vectype, itype;
+
+    MTest_Init( &argc, &argv );
+
+    comm = MPI_COMM_WORLD;
+
+    MPI_Comm_rank( comm, &rank );
+    MPI_Comm_size( comm, &size );
+
+    /* Allocate and initialize sbuf */
+    sbuf = (int *)malloc( scount * sizeof(int) );
+    if (!sbuf) {
+	fprintf( stderr, "Could not allocate send buffer f size %d\n", 
+		 scount );
+	MPI_Abort( MPI_COMM_WORLD, 0 );
+    }
+    for (i=0; i<scount; i++) sbuf[i] = i;
+
+    MPI_Alloc_mem( count*sizeof(int), MPI_INFO_NULL, &winbuf );
+
+    /* This is a simple vector type */
+    vcount = count / 4;
+    MPI_Type_vector( vcount, 1, 2, MPI_INT, &vectype );
+    MPI_Type_commit( &vectype );
+    MPI_Win_create( winbuf, count * sizeof(int), sizeof(int), MPI_INFO_NULL, 
+		    comm, &win );
+
+    /* Check with different combination of types, including non-contig on 
+       both sides */
+    
+    /* Clear winbuf */
+    memset( winbuf, 0, count*sizeof(int) );
+    MPI_Win_lock( MPI_LOCK_EXCLUSIVE, rank, 0, win );
+    MPI_Put( sbuf, 1, vectype, 0, rank, 1, vectype, win );
+    MPI_Win_unlock( rank, win );
+    /* Check results */
+    j = 0;
+    for (i=0; i<vcount; i++) {
+	if (winbuf[j] != sbuf[j]) {
+	    errs ++;
+	    fprintf( stderr, "VecPut: winbuf[%d] = %d, should = %d\n", 
+		     winbuf[j], j, sbuf[j] );
+	}
+	j += 2;
+    }
+
+    memset( winbuf, 0, count*sizeof(int) );
+    MPI_Win_lock( MPI_LOCK_SHARED, rank, 0, win );
+    MPI_Accumulate( sbuf, 1, vectype, 0, rank, 1, vectype, MPI_SUM, win );
+    MPI_Win_unlock( rank, win );
+    /* Check results */
+    j = 0;
+    for (i=0; i<vcount; i++) {
+	if (winbuf[j] != sbuf[j]) {
+	    errs ++;
+	    fprintf( stderr, "VecAcc: winbuf[%d] = %d, should = %d\n", 
+		     winbuf[j], j, sbuf[j] );
+	}
+	j += 2;
+    }
+
+    /* Now, use get to fetch back the results that we just wrote */
+    memset( sbuf, 0, count*sizeof(int) );
+    MPI_Win_lock( MPI_LOCK_SHARED, rank, 0, win );
+    MPI_Get( sbuf, 1, vectype, 0, rank, 1, vectype, win );
+    MPI_Win_unlock( rank, win );
+    /* Check results */
+    j = 0;
+    for (i=0; i<vcount; i++) {
+	if (winbuf[j] != sbuf[j]) {
+	    errs ++;
+	    fprintf( stderr, "VecAcc: winbuf[%d] = %d, should = %d\n", 
+		     winbuf[j], j, sbuf[j] );
+	}
+	j += 2;
+    }
+
+    MPI_Win_free( &win );
+    MPI_Free_mem( winbuf );
+    free( sbuf );
+
+    MTest_Finalize( errs );
+
+    return 0;
+}
