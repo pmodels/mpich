6b0e6747df8037388724eb13322094744b20eedb
Anthony Chan <chan@mcs.anl.gov>
2008-07-07 20:28:50 +0000

    [svn-r1029] fixed non-ANSI feature introduced in r1008.

--- src/mpe2/src/wrappers/src/trace_mpi_core.c
+++ src/mpe2/src/wrappers/src/trace_mpi_core.c
@@ -1894,15 +1894,15 @@ MPI_Status * status;
     Trace the beginning and ending of MPI_Recv.
 */
 
-  sprintf( msg, "Starting MPI_Recv with count = %d, source = %d, tag = %d...", 
-	   count, source, tag );
-  TRACE_PRINTF( msg );
-
 #ifdef HAVE_MPI_STATUS_IGNORE
   MPI_Status    tmp_status;
   if (status == MPI_STATUS_IGNORE)
       status = &tmp_status;
 #endif
+
+  sprintf( msg, "Starting MPI_Recv with count = %d, source = %d, tag = %d...", 
+	   count, source, tag );
+  TRACE_PRINTF( msg );
   
   returnVal = PMPI_Recv( buf, count, datatype, source, tag, comm, status );
 
