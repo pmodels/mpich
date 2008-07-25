90835891907b548054ccca394e9256a53d014183
Rajeev Thakur <thakur@mcs.anl.gov>
2008-07-25 18:38:30 +0000

    [svn-r1155] removed generated files

--- src/binding/f90/mpi.f90
+++
@@ -1,9 +0,0 @@
-!  (C) 2007 by Argonne National Laboratory.
-!      See COPYRIGHT in top-level directory.
-
-       MODULE MPI
-!      This module was created by the script buildiface
-       USE MPI_CONSTANTS
-       USE MPI_SIZEOFS
-       USE MPI_BASE
-       END MODULE MPI
--- src/binding/f90/mpi_constants.f90
+++
@@ -1,7 +0,0 @@
-!  (C) 2007 by Argonne National Laboratory.
-!      See COPYRIGHT in top-level directory.
-
-        MODULE MPI_CONSTANTS
-        IMPLICIT NONE
-        INCLUDE 'mpifnoext.h'
-        END MODULE MPI_CONSTANTS
