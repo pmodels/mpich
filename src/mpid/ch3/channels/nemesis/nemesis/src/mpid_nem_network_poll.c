ee93c2a8e46e8c66393da56aba8f844544ee6a31
Darius Buntinas <buntinas@mcs.anl.gov>
2008-07-30 18:14:51 +0000

    [svn-r1202] removed routing logging macros from poll to reduce noise in log

--- src/mpid/ch3/channels/nemesis/nemesis/src/mpid_nem_network_poll.c
+++ src/mpid/ch3/channels/nemesis/nemesis/src/mpid_nem_network_poll.c
@@ -13,10 +13,5 @@
 #define FCNAME MPIDI_QUOTE(FUNCNAME)
 int MPID_nem_network_poll (MPID_nem_poll_dir_t in_or_out)
 {
-    MPIDI_STATE_DECL(MPID_STATE_MPID_NEM_NETWORK_POLL);
-
-    MPIDI_FUNC_ENTER(MPID_STATE_MPID_NEM_NETWORK_POLL);
-
-    MPIDI_FUNC_EXIT(MPID_STATE_MPID_NEM_NETWORK_POLL);
     return MPID_nem_net_module_poll (in_or_out);
 }
