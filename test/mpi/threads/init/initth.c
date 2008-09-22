989aa503858ec6d42f24766c4999983f393241b7
William Gropp <gropp@mcs.anl.gov>
2008-09-22 08:55:10 +0000

    [svn-r3170] Added an additional thread test and fixed runtests targets to work within the thread test directories

---
+++ test/mpi/threads/init/initth.c
@@ -0,0 +1,22 @@
+/* */
+#include "mpi.h"
+
+int main( int argc, char *argv[] )
+{
+    int provided, rank;
+
+    MPI_Init_thread( &argc, &argv, MPI_THREAD_MULTIPLE, &provided );
+
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+
+    MPI_Finalize();
+
+    /* The test here is a simple one that Finalize exits, so the only action
+       is to write no errors */
+    if (rank == 0) {
+	printf( " No Errors\n" );
+    }
+
+    return 0;
+
+}
