2bbf805c0528db2559c7d0aa13922826254231ce
William Gropp <gropp@mcs.anl.gov>
2008-09-24 06:37:47 +0000

    [svn-r3183] Minor change in constant to address compiler warning

--- src/nameserv/file/file_nameserv.c
+++ src/nameserv/file/file_nameserv.c
@@ -71,7 +71,7 @@ int MPID_NS_Create( const MPID_Info *info_ptr, MPID_NS_Handle *handle_ptr )
     
     if (stat( (*handle_ptr)->dirname, &st ) || !S_ISDIR(st.st_mode) ) {
 	/* This mode is rwx by owner only.  */
-	if (mkdir( (*handle_ptr)->dirname, 00700 )) {
+	if (mkdir( (*handle_ptr)->dirname, 0000700 )) {
 	    /* FIXME : An error.  Ignore most ? 
 	     For example, ignore EEXIST?  */
 	    ;
