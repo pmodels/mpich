241145aabd70aac3f00bd29016201dfb633626d0
Jayesh Krishna <jayesh@mcs.anl.gov>
2008-10-23 20:36:23 +0000

    [svn-r3360] Running dlltool from bash was just masking the error. Specifying the path to gnu assembler explicitly

--- maint/makegcclibs.bat
+++ maint/makegcclibs.bat
@@ -5,11 +5,11 @@ Release\impgen.exe ..\lib\mpich2mpi.dll > mpich2mpi.def
 REM Release\impgen.exe ..\lib\mpich2mpid.dll > mpich2mpid.def
 Release\impgen.exe ..\lib\fmpich2g.dll > fmpich2gcc.def
 REM Release\impgen.exe ..\lib\fmpich2gd.dll > fmpich2gccd.def
-bash -c dlltool --dllname mpich2.dll --def mpich2gcc.def --output-lib ..\lib\libmpich2.a >> ..\make.log
+dlltool --as /usr/bin/as --dllname mpich2.dll --def mpich2gcc.def --output-lib ..\lib\libmpich2.a >> ..\make.log
 REM dlltool --dllname mpich2d.dll --def mpich2gccd.def --output-lib ..\lib\libmpich2d.a
-bash -c dlltool --dllname mpich2mpi.dll --def mpich2mpi.def --output-lib ..\lib\libmpi.a >> ..\make.log
+dlltool --as /usr/bin/as --dllname mpich2mpi.dll --def mpich2mpi.def --output-lib ..\lib\libmpi.a >> ..\make.log
 REM dlltool --dllname mpich2mpid.dll --def mpich2mpid.def --output-lib ..\lib\libmpid.a
-bash -c dlltool --dllname fmpich2g.dll --def fmpich2gcc.def --output-lib ..\lib\libfmpich2g.a >> ..\make.log
+dlltool --as /usr/bin/as --dllname fmpich2g.dll --def fmpich2gcc.def --output-lib ..\lib\libfmpich2g.a >> ..\make.log
 REM dlltool --dllname fmpich2gd.dll --def fmpich2gccd.def --output-lib ..\lib\libfmpich2gd.a
 REM Building MPI CXX Interface lib
 g++ -I ../src/include -I ../src/include/win32 -c ../src/binding/cxx/initcxx.cxx -o initcxx.o >> ..\make.log
