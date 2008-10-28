1e7d533c42b82cdddd2f58436ab74f70a115f889
William Gropp <gropp@mcs.anl.gov>
2008-10-28 16:12:51 +0000

    [svn-r3396] Added support for C types in Fortran, along with tests

---
+++ test/mpi/f77/ext/ctypesfromc.c
@@ -0,0 +1,110 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ *
+ *  (C) 2008 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+/*
+ * This file contains the C routines used in testing that all C datatypes
+ * are available in Fortran and have the correct values.
+ *
+ * The tests follow this pattern:
+ *
+ *  Fortran main program
+ *     calls the c routine f2ctype with each of the C types and the name of 
+ *     the type.  That c routine using MPI_Type_f2c to convert the 
+ *     Fortran handle to a C handle, and then compares it to the corresponding
+ *     C type, which is found by looking up the C handle by name
+ *
+ *     C routine uses xxx_f2c routine to get C handle, checks some
+ *     properties (i.e., size and rank of communicator, contents of datatype)
+ *
+ *     Then the Fortran main program calls a C routine that provides
+ *     a handle, and the Fortran program performs similar checks.
+ *
+ * We also assume that a C int is a Fortran integer.  If this is not the
+ * case, these tests must be modified.
+ */
+
+/* style: allow:fprintf:10 sig:0 */
+#include <stdio.h>
+#include "mpi.h"
+#include "../../include/mpitestconf.h"
+#include <string.h>
+
+/* Create an array with all of the MPI names in it */
+/* This is extracted from the test in test/mpi/types/typename.c ; only the
+   C types are included. */
+
+typedef struct mpi_names_t { MPI_Datatype dtype; const char *name; } mpi_names_t;
+
+/* The MPI standard specifies that the names must be the MPI names,
+   not the related language names (e.g., MPI_CHAR, not char) */
+
+static mpi_names_t mpi_names[] = {
+    { MPI_CHAR, "MPI_CHAR" },
+    { MPI_SIGNED_CHAR, "MPI_SIGNED_CHAR" },
+    { MPI_UNSIGNED_CHAR, "MPI_UNSIGNED_CHAR" },
+    { MPI_WCHAR, "MPI_WCHAR" },
+    { MPI_SHORT, "MPI_SHORT" },
+    { MPI_UNSIGNED_SHORT, "MPI_UNSIGNED_SHORT" },
+    { MPI_INT, "MPI_INT" },
+    { MPI_UNSIGNED, "MPI_UNSIGNED" },
+    { MPI_LONG, "MPI_LONG" },
+    { MPI_UNSIGNED_LONG, "MPI_UNSIGNED_LONG" },
+    { MPI_FLOAT, "MPI_FLOAT" },
+    { MPI_DOUBLE, "MPI_DOUBLE" },
+    { MPI_FLOAT_INT, "MPI_FLOAT_INT" },
+    { MPI_DOUBLE_INT, "MPI_DOUBLE_INT" },
+    { MPI_LONG_INT, "MPI_LONG_INT" },
+    { MPI_SHORT_INT, "MPI_SHORT_INT" },
+    { MPI_2INT, "MPI_2INT" },
+    { MPI_LONG_DOUBLE, "MPI_LONG_DOUBLE" },
+    { MPI_LONG_LONG_INT, "MPI_LONG_LONG_INT" }, 
+    { MPI_LONG_LONG, "MPI_LONG_LONG" },
+    { MPI_UNSIGNED_LONG_LONG, "MPI_UNSIGNED_LONG_LONG" }, 
+    { MPI_LONG_DOUBLE_INT, "MPI_LONG_DOUBLE_INT" },
+    { 0, (char *)0 },  /* Sentinal used to indicate the last element */
+};
+
+/* 
+   Name mapping.  All routines are created with names that are lower case
+   with a single trailing underscore.  This matches many compilers.
+   We use #define to change the name for Fortran compilers that do
+   not use the lowercase/underscore pattern 
+*/
+
+#ifdef F77_NAME_UPPER
+#define f2ctype_ F2CTYPE
+
+#elif defined(F77_NAME_LOWER) || defined(F77_NAME_MIXED)
+/* Mixed is ok because we use lowercase in all uses */
+#define f2ctype_ f2ctype
+
+#elif defined(F77_NAME_LOWER_2USCORE) || defined(F77_NAME_LOWER_USCORE) || \
+      defined(F77_NAME_MIXED_USCORE)
+/* Else leave name alone (routines have no underscore, so both
+   of these map to a lowercase, single underscore) */
+#else 
+#error 'Unrecognized Fortran name mapping'
+#endif
+
+/* Prototypes to keep compilers happy */
+int f2ctype_( MPI_Fint *, MPI_Fint * );
+
+/* */
+int f2ctype_( MPI_Fint *fhandle, MPI_Fint *typeidx )
+{
+    int i, errs = 0;
+    MPI_Datatype ctype;
+
+    /* printf( "Testing %s\n", mpi_names[*typeidx].name ); */
+    ctype = MPI_Type_f2c( *fhandle );
+    if (ctype != mpi_names[*typeidx].dtype) {
+	errs++;
+	printf( "C and Fortran types for %s do not match f=%d, c=%d.\n",
+		mpi_names[*typeidx].name, *fhandle, MPI_Type_c2f( ctype ) );
+    }
+    
+    return errs;
+}
---
+++ test/mpi/f77/ext/ctypesinf.f
@@ -0,0 +1,53 @@
+      program main
+      include 'mpif.h'
+      integer ierr
+      integer errs, toterrs, wrank
+      integer f2ctype
+C
+      call mpi_init( ierr )
+      call mpi_comm_rank( MPI_COMM_WORLD, wrank, ierr )
+C
+      errs = 0
+C
+      errs = errs + f2ctype( MPI_CHAR, 0 )
+      errs = errs + f2ctype( MPI_SIGNED_CHAR, 1 )
+      errs = errs + f2ctype( MPI_UNSIGNED_CHAR, 2 )
+      errs = errs + f2ctype( MPI_WCHAR, 3 )
+      errs = errs + f2ctype( MPI_SHORT, 4 )
+      errs = errs + f2ctype( MPI_UNSIGNED_SHORT, 5 )
+      errs = errs + f2ctype( MPI_INT, 6 )
+      errs = errs + f2ctype( MPI_UNSIGNED, 7 )
+      errs = errs + f2ctype( MPI_LONG, 8 )
+      errs = errs + f2ctype( MPI_UNSIGNED_LONG, 9 )
+      errs = errs + f2ctype( MPI_FLOAT, 10 )
+      errs = errs + f2ctype( MPI_DOUBLE, 11 )
+      errs = errs + f2ctype( MPI_FLOAT_INT, 12 )
+      errs = errs + f2ctype( MPI_DOUBLE_INT, 13 )
+      errs = errs + f2ctype( MPI_LONG_INT, 14 )
+      errs = errs + f2ctype( MPI_SHORT_INT, 15 )
+      errs = errs + f2ctype( MPI_2INT, 16 )
+      if (MPI_LONG_DOUBLE .ne. MPI_TYPE_NULL) then
+          errs = errs + f2ctype( MPI_LONG_DOUBLE, 17 )
+          errs = errs + f2ctype( MPI_LONG_DOUBLE_INT, 21 )
+      endif
+      if (MPI_LONG_LONG .ne. MPI_TYPE_NULL) then
+          errs = errs + f2ctype( MPI_LONG_LONG_INT, 18 )
+          errs = errs + f2ctype( MPI_LONG_LONG, 19 )
+          errs = errs + f2ctype( MPI_UNSIGNED_LONG_LONG, 20 )
+      endif
+C
+C Summarize the errors
+C
+      call mpi_allreduce( errs, toterrs, 1, MPI_INTEGER, MPI_SUM,
+     $     MPI_COMM_WORLD, ierr )
+      if (wrank .eq. 0) then
+         if (toterrs .eq. 0) then
+            print *, ' No Errors'
+         else
+            print *, ' Found ', toterrs, ' errors'
+         endif
+      endif
+
+      call mpi_finalize( ierr )
+
+      end 
