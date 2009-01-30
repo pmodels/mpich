d8e4d50770afd4eb16daf83988c19927f094877a
Pavan Balaji <balaji@mcs.anl.gov>
2009-01-30 04:35:57 +0000

    [svn-r3764] This commit has three parts:
    1. Added a dependency for utils in the perf makefile. Without this MTest features cannot be used.
    
    2. Enabled the performance tests for the nightlies.
    
    3. Added a new timer test that ensures that MPI_Wtime() does not always return a zero (as Anthony noticed in the IBM BG/P builds).
    
    No reviewer.

---
+++ test/mpi/perf/timer.c
@@ -0,0 +1,27 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ *  (C) 2006 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
+#include "mpi.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    double t1, t2;
+    int errs = 0;
+
+    MTest_Init(&argc,&argv);
+
+    t1 = MPI_Wtime();
+    t2 = MPI_Wtime();
+
+    if (t1 == t2)
+        errs++;
+
+    MTest_Finalize(errs);
+    MPI_Finalize();
+
+    return 0;
+}
