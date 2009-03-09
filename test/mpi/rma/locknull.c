1098b9617ffa20de6402d4da5a59f0864032fa74
Rajeev Thakur <thakur@mcs.anl.gov>
2009-03-09 22:19:42 +0000

    [svn-r3989] added include string.h

--- test/mpi/rma/locknull.c
+++ test/mpi/rma/locknull.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mpitest.h"
+#include <string.h>
 
 static char MTEST_Descrip[] = "Locks with no RMA operations";
 
--- test/mpi/rma/selfrma.c
+++ test/mpi/rma/selfrma.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mpitest.h"
+#include <string.h>
 
 static char MTEST_Descrip[] = "RMA to self";
 
