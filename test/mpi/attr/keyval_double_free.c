d4647b3f034bd606900d33f9e6d2fdd2d9388714
David Goodell <goodell@mcs.anl.gov>
2009-03-12 17:42:59 +0000

    [svn-r4028] Add a new test for double-freeing attriute keyvals.
    No reviewer.

---
+++ test/mpi/attr/keyval_double_free.c
@@ -0,0 +1,37 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ *
+ *  (C) 2009 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+#include <mpi.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* tests multiple invocations of Keyval_free on the same keyval */
+
+int delete_fn(MPI_Comm comm, int keyval, void *attr, void *extra) {
+    MPI_Keyval_free(&keyval);
+}
+
+int main (int argc, char **argv)
+{
+    MPI_Comm duped;
+    int keyval = MPI_KEYVAL_INVALID;
+    int keyval_copy = MPI_KEYVAL_INVALID;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_dup(MPI_COMM_SELF, &duped);
+
+    MPI_Keyval_create(MPI_NULL_COPY_FN, delete_fn,  &keyval, NULL);
+    keyval_copy = keyval;
+
+    MPI_Attr_put(MPI_COMM_SELF, keyval, NULL);
+    MPI_Attr_put(duped, keyval, NULL);
+
+    MPI_Comm_free(&duped);         /* first MPI_Keyval_free */
+    MPI_Keyval_free(&keyval);      /* second MPI_Keyval_free */
+    MPI_Keyval_free(&keyval_copy); /* third MPI_Keyval_free */
+    printf(" No Errors\n");
+    MPI_Finalize();                /* fourth MPI_Keyval_free */
+    return 0;
+}
