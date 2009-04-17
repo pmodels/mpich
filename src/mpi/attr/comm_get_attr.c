99952dd2f59c578627181547e2a3eeae9065309f
William Gropp <gropp@mcs.anl.gov>
2009-04-17 14:47:22 +0000

    [svn-r4256] Clean out old attribute code (#if 0ed for several releases)

--- src/mpi/attr/comm_get_attr.c
+++ src/mpi/attr/comm_get_attr.c
@@ -110,9 +110,6 @@ int MPIR_CommGetAttr( MPI_Comm comm, int comm_keyval, void *attribute_val,
 	   On some 64-bit plaforms, such as Solaris-SPARC, using an MPI_Fint
 	   will cause the value to placed into the high, rather than low,
 	   end of the output value. */
-#if 0
-	MPIR_Pint  *attr_int = (MPIR_Pint *)attribute_val;
-#endif
 #endif
 	*flag = 1;
 
@@ -192,70 +189,6 @@ int MPIR_CommGetAttr( MPI_Comm comm, int comm_keyval, void *attribute_val,
 		*attr_val_p = &attr_copy.appnum;
 	    }
 	    break;
-#if 0
-#ifdef HAVE_FORTRAN_BINDING
-	case 2: /* Fortran TAG_UB */
-	    *attr_int = attr_copy.tag_ub;
-	    break;
-	case 4: /* Fortran HOST */
-	    *attr_int = attr_copy.host;
-	    break;
-	case 6: /* Fortran IO */
-	    *attr_int = attr_copy.io;
-	    break;
-	case 8: /* Fortran WTIME */
-	    *attr_int = attr_copy.wtime_is_global;
-	    break;
-	case 10: /* UNIVERSE_SIZE */
-	    /* This is a special case.  If universe is not set, then we
-	       attempt to get it from the device.  If the device is doesn't
-	       supply a value, then we set the flag accordingly */
-	    if (attr_copy.universe >= 0)
-	    { 
-		*attr_int = attr_copy.universe;
-	    }
-	    else if (attr_copy.universe == MPIR_UNIVERSE_SIZE_NOT_AVAILABLE)
-	    {
-		*flag = 0;
-	    }
-	    else
-	    {
-		mpi_errno = MPID_Get_universe_size(&attr_copy.universe);
-		/* --BEGIN ERROR HANDLING-- */
-		if (mpi_errno != MPI_SUCCESS)
-		{
-		    attr_copy.universe = MPIR_UNIVERSE_SIZE_NOT_AVAILABLE;
-		    goto fn_fail;
-		}
-		/* --END ERROR HANDLING-- */
-		
-		if (attr_copy.universe >= 0)
-		{
-		    *attr_int = attr_copy.universe;
-		}
-		else
-		{
-		    attr_copy.universe = MPIR_UNIVERSE_SIZE_NOT_AVAILABLE;
-		    *flag = 0;
-		}
-	    }
-	    break;
-	case 12: /* LASTUSEDCODE */
-	    *attr_int = attr_copy.lastusedcode;
-	    break;
-	case 14: /* APPNUM */
-	    /* This is another special case.  If appnum is negative,
-	       we take that as indicating no value of APPNUM, and set
-	       the flag accordingly */
-	    if (attr_copy.appnum < 0) {
-		*flag = 0;
-	    }
-	    else {
-		*attr_int = attr_copy.appnum;
-	    }
-	    break;
-#endif
-#endif
 	}
 	/* All of the predefined attributes are INTEGER; since we've set 
 	   the output value as the pointer to these, we need to dereference
