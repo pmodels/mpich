00a864e6cf0d3d96c86a08559174f68cb3c765d2
Robert Latham <robl@mcs.anl.gov>
2009-05-08 14:47:14 +0000

    [svn-r4443] update declaration of heapsort funcs to match renamed version

--- src/mpi/romio/adio/include/heap-sort.h
+++ src/mpi/romio/adio/include/heap-sort.h
@@ -14,9 +14,9 @@ typedef struct {
 /*static inline int parent(heap_t *heap, int i);
 static inline int left(heap_t *heap, int i);
 static inline int right(heap_t *heap, int i); */
-void free_heap(heap_t *heap);
-int create_heap(heap_t *heap, int size);
-void heap_insert(heap_t *heap, ADIO_Offset offset, int proc,
+void ADIOI_Heap_free(heap_t *heap);
+int ADIOI_Heap_create(heap_t *heap, int size);
+void ADIOI_Heap_insert(heap_t *heap, ADIO_Offset offset, int proc,
 		 ADIO_Offset reg_max_len);
-void heap_extract_min(heap_t *heap, ADIO_Offset* key, int *proc,
+void ADIOI_Heap_extract_min(heap_t *heap, ADIO_Offset* key, int *proc,
 		      ADIO_Offset *reg_max_len);
