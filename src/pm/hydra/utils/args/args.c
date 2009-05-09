ecc4c817f3bd502f931c51031d06f372360c510a
Pavan Balaji <balaji@mcs.anl.gov>
2009-05-09 05:22:03 +0000

    [svn-r4473] Hackish fix for ticket #544. If mpiexec is used from the user's path, there is no good way to find the location of pmi_proxy. We search through the user's path to find it. This seems to be similar to what SMPD is doing as well.

--- src/pm/hydra/utils/args/args.c
+++ src/pm/hydra/utils/args/args.c
@@ -6,10 +6,21 @@
 
 #include "hydra_utils.h"
 
+static int exists(char *filename)
+{
+    struct stat file_stat;
+
+    if ((stat(filename, &file_stat) < 0) || !(S_ISREG(file_stat.st_mode))) {
+        return 0; /* no such file, or not a regular file */
+    }
+
+    return 1;
+}
+
 HYD_Status HYDU_get_base_path(char *execname, char *wdir, char **path)
 {
     char *loc, *post;
-    char *path_str[HYD_NUM_TMP_STRINGS];
+    char *user_path = NULL, *tmp[HYD_NUM_TMP_STRINGS], *path_loc = NULL, *test_loc;
     HYD_Status status = HYD_SUCCESS;
 
     HYDU_FUNC_ENTER();
@@ -18,6 +29,35 @@ HYD_Status HYDU_get_base_path(char *execname, char *wdir, char **path)
     post = HYDU_strdup(execname);
     loc = strrchr(post, '/');
     if (!loc) { /* If there is no path */
+        /* The executable is somewhere in the user's path. We need to
+         * find it. */
+        if (getenv("PATH")) { /* If the PATH environment exists */
+            user_path = HYDU_strdup(getenv("PATH"));
+            test_loc = strtok(user_path, ";:");
+            do {
+                tmp[0] = test_loc;
+                tmp[1] = "/";
+                tmp[2] = execname;
+                tmp[3] = NULL;
+
+                status = HYDU_str_alloc_and_join(tmp, &path_loc);
+                HYDU_ERR_POP(status, "unable to join strings\n");
+
+                if (exists(path_loc)) {
+                    tmp[0] = test_loc;
+                    tmp[1] = "/";
+                    tmp[2] = NULL;
+
+                    status = HYDU_str_alloc_and_join(tmp, path);
+                    HYDU_ERR_POP(status, "unable to join strings\n");
+
+                    goto fn_exit; /* We are done */
+                }
+            } while ((test_loc = strtok(NULL, ";:")));
+        }
+
+        /* There is either no PATH environment or we could not find
+         * the file in the PATH. Just return an empty path */
         *path = HYDU_strdup("");
     }
     else {      /* There is a path */
@@ -25,11 +65,11 @@ HYD_Status HYDU_get_base_path(char *execname, char *wdir, char **path)
 
         /* Check if its absolute or relative */
         if (post[0] != '/') {   /* relative */
-            path_str[0] = wdir;
-            path_str[1] = "/";
-            path_str[2] = post;
-            path_str[3] = NULL;
-            status = HYDU_str_alloc_and_join(path_str, path);
+            tmp[0] = wdir;
+            tmp[1] = "/";
+            tmp[2] = post;
+            tmp[3] = NULL;
+            status = HYDU_str_alloc_and_join(tmp, path);
             HYDU_ERR_POP(status, "unable to join strings\n");
         }
         else {  /* absolute */
@@ -40,6 +80,10 @@ HYD_Status HYDU_get_base_path(char *execname, char *wdir, char **path)
   fn_exit:
     if (post)
         HYDU_FREE(post);
+    if (user_path)
+        HYDU_FREE(user_path);
+    if (path_loc)
+        HYDU_FREE(path_loc);
     HYDU_FUNC_EXIT();
     return status;
 
