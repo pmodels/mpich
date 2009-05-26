2937785460a292e13f50b9dc2de92a67bd0520fa
William Gropp <gropp@mcs.anl.gov>
2009-05-26 13:55:26 +0000

    [svn-r4623] Check that the timings in twovec ar sensible (they often are too small relative to MPI_Wtick)

--- test/mpi/perf/twovec.c
+++ test/mpi/perf/twovec.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* FIXME: This comment is wrong - the test appears to only create a datatype */
 /* This does a transpose with a get operation, fence, and derived
    datatypes. Uses vector and hvector (Example 3.32 from MPI 1.1
    Standard). Run on 2 processes */
@@ -14,13 +15,15 @@
 int main(int argc, char *argv[]) 
 { 
     MPI_Datatype column, xpose;
-    double t[5], ttmp, tmin, tmax;
+    double t[5], ttmp, tmin, tmax, ttick;
     static int sizes[5] = { 10, 100, 1000, 10000, 20000 };
-    int i, isMonotone, errs=0, nrows, ncols;
+    int i, isMonotone, errs=0, nrows, ncols, isvalid;
  
     MPI_Init(&argc,&argv); 
 
-     for (i=0; i<5; i++) {
+    ttick = MPI_Wtick();
+
+    for (i=0; i<5; i++) {
          nrows = ncols = sizes[i];
          ttmp = MPI_Wtime();
          /* create datatype for one column */
@@ -37,25 +40,39 @@ int main(int argc, char *argv[])
         nearly independent of size */
      tmin = 10000;
      tmax = 0;
+     isvalid = 1;
      for (i=0; i<5; i++) {
-         if (t[i] < tmin) tmin = t[i];
-         if (t[i] > tmax) tmax = t[i];
+	 if (t[i] < 10*ttick) {
+	     /* Timing is invalid - resolution is too low */
+	     isvalid = 0;
+	 }
+	 else {
+	     if (t[i] < tmin) tmin = t[i];
+	     if (t[i] > tmax) tmax = t[i];
+	 }
      }
-     /* Monotone times are a warning */
-     isMonotone = 1;
-     for (i=1; i<5; i++) {
-        if (t[i] < t[i-1]) isMonotone = 0;
+     if (isvalid) {
+	 /* Monotone times are a warning */
+	 isMonotone = 1;
+	 for (i=1; i<5; i++) {
+	     if (t[i] < t[i-1]) isMonotone = 0;
+	 }
+	 if (tmax > 100 * tmin) {
+	     errs++;
+	     fprintf( stderr, "Range of times appears too large\n" );
+	     if (isMonotone) {
+		 fprintf( stderr, "Vector types may use processing proportion to count\n" );
+	     }
+	     for (i=0; i<5; i++) {
+		 fprintf( stderr, "n = %d, time = %f\n", sizes[i], t[i] );
+	     }
+	     fflush(stderr);
+	 }
      }
-     if (tmax > 100 * tmin) {
-         errs++;
-         fprintf( stderr, "Range of times appears too large\n" );
-         if (isMonotone) {
-             fprintf( stderr, "Vector types may use processing proportion to count\n" );
-         }
-         for (i=0; i<5; i++) {
-              fprintf( stderr, "n = %d, time = %f\n", sizes[i], t[i] );
-         }
-         fflush(stderr);
+     else {
+	 fprintf( stderr, "Timing failed - recorded times are too small relative to MPI_Wtick\n" );
+	 /* Note that this is not an error in the MPI implementation - it is a 
+	    failure in the test */
      }
 
     if (errs) {
