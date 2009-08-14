b6f3d9a5e5cd3fb63338f2b9d3eeb39e652467fa
David Goodell <goodell@mcs.anl.gov>
2009-08-14 21:52:16 +0000

    [svn-r5220] Add a new MTEST_ macro to simplify MPI version number checks.
    No reviewer.

--- test/mpi/include/mpitest.h
+++ test/mpi/include/mpitest.h
@@ -73,4 +73,30 @@ const char *MTestGetWinName( void );
 void MTestFreeWin( MPI_Win * );
 #endif
 
+/* These macros permit overrides via:
+ *     make CPPFLAGS='-DMTEST_MPI_VERSION=X -DMTEST_MPI_SUBVERSION=Y'
+ * where X and Y are the major and minor versions of the MPI standard that is
+ * being tested.  The override should work similarly if added to the CPPFLAGS at
+ * configure time. */
+#ifndef MTEST_MPI_VERSION
+#define MTEST_MPI_VERSION MPI_VERSION
+#endif
+#ifndef MTEST_MPI_SUBVERSION
+#define MTEST_MPI_SUBVERSION MPI_SUBVERSION
+#endif
+
+/* Makes it easier to conditionally compile test code for a particular minimum
+ * version of the MPI Standard.  Right now there is only a MIN flavor but it
+ * would be easy to add MAX or EXACT flavors if they become necessary at some
+ * point.  Example usage:
+ ------8<-------
+#if MTEST_HAVE_MIN_MPI_VERSION(2,2)
+... test for some feature that is only available in MPI-2.2 or later ...
+#endif
+ ------8<-------
+ */
+#define MTEST_HAVE_MIN_MPI_VERSION(major_,minor_) \
+    (MTEST_MPI_VERSION == (major_) && MTEST_MPI_SUBVERSION >= (minor_)) ||   \
+    (MTEST_MPI_VERSION > (major_))
+
 #endif
