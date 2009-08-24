02cc8a0ce267c13e4db5e9acd278d57af9e36e45
David Goodell <goodell@mcs.anl.gov>
2009-08-24 19:16:39 +0000

    [svn-r5261] Clean up the probe-intercomm test.
    It made some potentially unsafe assumptions about the behavior of
    MTestGetIntercomm and it had several unused variables left over from a
    previous incarnation of this test.
    
    No reviewer.

--- test/mpi/comm/probe-intercomm.c
+++ test/mpi/comm/probe-intercomm.c
@@ -18,45 +18,50 @@ int main( int argc, char *argv[] )
     int errs = 0, recvlen, isLeft;
     MPI_Status status;
     int rank, size;
-    MPI_Comm  dupcomm, clustercomm, intercomm;
+    MPI_Comm  intercomm;
     char buf[MAX_DATA_LEN];
+    const char *test_str = "test";
 
     MTest_Init( &argc, &argv );
-
 
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
     MPI_Comm_size( MPI_COMM_WORLD, &size );
 
-    if (size != 2) {
-	fprintf( stderr, "This test requires two processes." );
+    if (size < 2) {
+	fprintf( stderr, "This test requires at least two processes." );
 	MPI_Abort( MPI_COMM_WORLD, 1 );
     }
 
-    MPI_Comm_dup(MPI_COMM_WORLD, &dupcomm);
-    MPI_Comm_split(dupcomm, 0, rank, &clustercomm);
+    while (MTestGetIntercomm( &intercomm, &isLeft, 2 )) {
+        if (intercomm == MPI_COMM_NULL) continue;
 
-    if(rank > 0){
-	MPI_Comm_free(&clustercomm); 
+        MPI_Comm_rank(intercomm, &rank);
+
+        /* 0 ranks on each side communicate, everyone else does nothing */
+        if(rank == 0) {
+            if (isLeft) {
+                recvlen = -1;
+                MPI_Probe(0, 0, intercomm, &status);
+                MPI_Get_count(&status, MPI_CHAR, &recvlen);
+                if (recvlen != (strlen(test_str) + 1)) {
+                    printf(" Error: recvlen (%d) != strlen(\"%s\")+1 (%d)\n", recvlen, test_str, (int)strlen(test_str) + 1);
+                    ++errs;
+                }
+                buf[0] = '\0';
+                MPI_Recv(buf, recvlen, MPI_CHAR, 0, 0, intercomm, &status);
+                if (strcmp(test_str,buf)) {
+                    printf(" Error: strcmp(test_str,buf)!=0\n");
+                    ++errs;
+                }
+            }
+            else {
+                strncpy(buf, test_str, 5);
+                MPI_Send(buf, strlen(buf)+1, MPI_CHAR, 0, 0, intercomm);
+            }
+        }
+        MTestFreeComm(&intercomm);
     }
 
-    isLeft = rank % 2;
-    MTestGetIntercomm(&intercomm, &isLeft, 2);
-
-    if(rank == 0){
-	MPI_Probe(0, 0, intercomm, &status);
-	MPI_Get_count(&status, MPI_CHAR, &recvlen);
-	buf[0] = '\0';
-	MPI_Recv(buf, recvlen, MPI_CHAR, 0, 0, intercomm, &status);
-    }else{
-	strncpy(buf, "test\0", 5);
-	MPI_Send(buf, strlen(buf)+1, MPI_CHAR, 0, 0, intercomm);
-    }
-
-    MPI_Comm_free( &dupcomm );
-    if(rank == 0){
-        MPI_Comm_free( &clustercomm);
-    }
-    MPI_Comm_free( &intercomm);
     MTest_Finalize( errs );
     MPI_Finalize();
     return 0;
