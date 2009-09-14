8889ec9249210e437ced4db95801a542ea60f1ef
Pavan Balaji <balaji@mcs.anl.gov>
2009-09-14 23:37:40 +0000

    [svn-r5341] Added a version of the failing test reported by Bryan Putnam to the test suite.

---
+++ test/mpi/datatype/subarray.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include "mpi.h"
+
+#define X 64
+#define Y 8
+#define Z 512
+
+double array[X][Y][Z];
+
+int main(int argc, char *argv[])
+{
+    int myrank;
+    MPI_Datatype subarray;
+    int array_size[] = {X, Y, Z};
+    int array_subsize[] = {X/2, Y/2, Z};
+    int array_start[] = {0, 0, 0};
+ 
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+
+    MPI_Type_create_subarray(3, array_size, array_subsize, array_start, MPI_ORDER_C,
+                             MPI_DOUBLE, &subarray);
+    MPI_Type_commit(&subarray);
+
+    if(myrank == 0)
+        MPI_Send(array, 1, subarray, 1, 0, MPI_COMM_WORLD);
+    else
+        MPI_Recv(array, 1, subarray, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        
+    MPI_Finalize();
+
+    return 0;
+}
