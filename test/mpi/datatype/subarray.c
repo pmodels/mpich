a2e9bdc51a1e0ade4956fa99f08fb191f56abf2b
Pavan Balaji <balaji@mcs.anl.gov>
2009-09-15 14:04:03 +0000

    [svn-r5342] Forgot to add the copyright notice.

--- test/mpi/datatype/subarray.c
+++ test/mpi/datatype/subarray.c
@@ -1,3 +1,9 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ *  (C) 2001 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include "mpi.h"
