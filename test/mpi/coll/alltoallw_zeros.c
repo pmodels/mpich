be1b08804f0a1463bcdc3d95f7cb055c28e5e0af
David Goodell <goodell@mcs.anl.gov>
2009-09-23 07:27:12 +0000

    [svn-r5363] Minor cleanups to the alltoallw_zeros test from r5362.
    Buffers weren't being freed and the MPI_IN_PLACE argument to
    MPI_Alltoall{v,w} required an MPI-2.2 #ifdef guard.
    
    No reviewer.

--- test/mpi/coll/alltoallw_zeros.c
+++ test/mpi/coll/alltoallw_zeros.c
@@ -18,6 +18,8 @@
 
 #include <mpi.h>
 
+#include "mpitest.h"
+
 int main(int argc, char *argv[])
 {
     int sendbuf, recvbuf;
@@ -70,21 +72,24 @@ int main(int argc, char *argv[])
     MPI_Alltoallw(&sendbuf, sendcounts, sdispls, sendtypes, &recvbuf, recvcounts, rdispls, recvtypes, MPI_COMM_WORLD);
     MPI_Alltoallw(&sendbuf, recvcounts, rdispls, recvtypes, &recvbuf, sendcounts, sdispls, sendtypes, MPI_COMM_WORLD);
 
+#if MTEST_HAVE_MIN_MPI_VERSION(2,2)
     /* pass MPI_IN_PLACE and different but compatible types rank is even/odd */
     if (rank % 2)
         MPI_Alltoallw(MPI_IN_PLACE, NULL, NULL, NULL, &recvbuf, recvcounts, rdispls, recvtypes, MPI_COMM_WORLD);
     else
         MPI_Alltoallw(MPI_IN_PLACE, NULL, NULL, NULL, &recvbuf, sendcounts, sdispls, sendtypes, MPI_COMM_WORLD);
+#endif
 
     /* now the same for Alltoallv instead of Alltoallw */
     MPI_Alltoallv(&sendbuf, sendcounts, sdispls, sendtypes[0], &recvbuf, recvcounts, rdispls, recvtypes[0], MPI_COMM_WORLD);
     MPI_Alltoallv(&sendbuf, recvcounts, rdispls, recvtypes[0], &recvbuf, sendcounts, sdispls, sendtypes[0], MPI_COMM_WORLD);
 
+#if MTEST_HAVE_MIN_MPI_VERSION(2,2)
     if (rank % 2)
         MPI_Alltoallv(MPI_IN_PLACE, NULL, NULL, MPI_DATATYPE_NULL, &recvbuf, recvcounts, rdispls, recvtypes[0], MPI_COMM_WORLD);
     else
         MPI_Alltoallv(MPI_IN_PLACE, NULL, NULL, MPI_DATATYPE_NULL, &recvbuf, sendcounts, sdispls, sendtypes[0], MPI_COMM_WORLD);
-
+#endif
 
     MPI_Type_free(&sendtype);
 
@@ -92,6 +97,13 @@ int main(int argc, char *argv[])
         printf(" No Errors\n");
 
 fn_exit:
+    if (rdispls)    free(rdispls);
+    if (sdispls)    free(sdispls);
+    if (recvcounts) free(recvcounts);
+    if (sendcounts) free(sendcounts);
+    if (recvtypes)  free(recvtypes);
+    if (sendtypes)  free(sendtypes);
+
     MPI_Finalize();
 
     return 0;
