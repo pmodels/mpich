09ca8251f6a581fbcfbc31fc1eb3e60e6d4bb981
William Gropp <gropp@mcs.anl.gov>
2009-10-15 15:29:05 +0000

    [svn-r5463] Added a test program for use of MPI_Comm_disconnect, provided by the Parallel R group that thought they had a problem with MPI_Comm_disconnect

---
+++ test/mpi/manual/spawntest_child.c
@@ -0,0 +1,42 @@
+#include "mpi.h"
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+  int my_rank; 
+  int partner;
+  int size, i,t;
+  char greeting[100];
+  MPI_Comm parentcomm;
+  MPI_Comm allcomm;
+  
+  MPI_Init(&argc, &argv); 
+  
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
+  MPI_Comm_size(MPI_COMM_WORLD, &size); 
+  
+  MPI_Comm_get_parent(&parentcomm);
+  
+  if ( parentcomm == MPI_COMM_NULL )
+    {
+      fprintf(stdout, "parentcomm is null\n");
+    }
+  
+  MPI_Intercomm_merge( parentcomm, 1, &allcomm);
+  
+  /* Without the Free of allcomm, the children *must not exit* until the
+     master calls MPI_Finalize. */
+  MPI_Barrier( allcomm );
+  /* According to 10.5.4, case 1b in MPI2.2, the children and master are
+     still connected unless MPI_Comm_disconnect is used with allcomm. 
+     MPI_Comm_free is not sufficient */
+  MPI_Comm_free( &allcomm );
+  MPI_Comm_disconnect( &parentcomm );
+  
+  fprintf(stdout, "%s:%d\n", __FILE__, __LINE__ );fflush(stdout);
+  MPI_Finalize();
+  
+  fprintf(stdout, "%d:child exiting.\n", my_rank );fflush(stdout);
+  return 0;
+}
---
+++ test/mpi/manual/spawntest_master.c
@@ -0,0 +1,51 @@
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_SPAWNS 4
+
+int main( int argc, char *argv[] )
+{
+  int np = NUM_SPAWNS;
+  int my_rank,i, size, newsize;
+  int errcodes[NUM_SPAWNS];
+  MPI_Comm allcomm;
+  MPI_Comm intercomm;
+
+  MPI_Init( &argc, &argv );
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  
+  
+  MPI_Comm_spawn( "./spawntest_child", MPI_ARGV_NULL, np, MPI_INFO_NULL, 0,
+		  MPI_COMM_WORLD, &intercomm, errcodes );
+  
+  if ( intercomm == MPI_COMM_NULL )
+    {
+      fprintf(stdout, "intercomm is null\n");
+    }
+  
+  MPI_Intercomm_merge(intercomm, 0, &allcomm);
+  
+  MPI_Comm_rank(allcomm, &my_rank);
+  MPI_Comm_size(allcomm, &size);
+  
+  /* Without the Free of allcomm, the children *must not exit* until the
+     master calls MPI_Finalize. */
+  MPI_Barrier( allcomm );
+  /* According to 10.5.4, case 1b in MPI2.2, the children and master are
+     still connected unless MPI_Comm_disconnect is used with allcomm. 
+     MPI_Comm_free is not sufficient */
+  MPI_Comm_free( &allcomm );
+  MPI_Comm_disconnect( &intercomm );
+  
+  fprintf(stdout, "%s:%d: Sleep starting; children should exit\n",
+	  __FILE__, __LINE__ );fflush(stdout);
+  sleep(30);
+  fprintf(stdout, 
+	  "%s:%d: Sleep done; all children should have already exited\n", 
+	  __FILE__, __LINE__ );fflush(stdout);
+  
+  MPI_Finalize();
+  return 0;
+}
