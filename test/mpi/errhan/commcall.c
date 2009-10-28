dcfa99ca82d5cfaa9b77b2d62170f4c3aed90333
David Goodell <goodell@mcs.anl.gov>
2009-10-28 23:24:42 +0000

    [svn-r5604] Add errhandler resetting to the commcall test.
    This is a regression test for the bad BUILTIN check in
    MPI_Comm_set_errhandler.  This diff is best viewed ignoring whitespace.
    
    Reviewed by buntinas@.

--- test/mpi/errhan/commcall.c
+++ test/mpi/errhan/commcall.c
@@ -31,6 +31,7 @@ int main( int argc, char *argv[] )
     MPI_Comm       comm;
     MPI_Errhandler newerr;
     int            i;
+    int            reset_handler;
 
     MTest_Init( &argc, &argv );
 
@@ -48,32 +49,39 @@ int main( int argc, char *argv[] )
     }
 
     /* Here we apply the test to many copies of a communicator */
-    for (i=0; i<1000; i++) {
-	MPI_Comm comm2;
-	calls = 0;
-	MPI_Comm_dup( MPI_COMM_WORLD, &comm );
-	mycomm = comm;
-	MPI_Comm_create_errhandler( eh, &newerr );
+    for (reset_handler = 0; reset_handler <= 1; ++reset_handler) {
+        for (i=0; i<1000; i++) {
+            MPI_Comm comm2;
+            calls = 0;
+            MPI_Comm_dup( MPI_COMM_WORLD, &comm );
+            mycomm = comm;
+            MPI_Comm_create_errhandler( eh, &newerr );
 
-	MPI_Comm_set_errhandler( comm, newerr );
-	MPI_Comm_call_errhandler( comm, MPI_ERR_OTHER );
-	if (calls != 1) {
-	    errs++;
-	    printf( "Error handler not called\n" );
-	}
-	MPI_Comm_dup( comm, &comm2 );
-	calls = 0;
-	mycomm = comm2;
-	/* comm2 must inherit the error handler from comm */
-	MPI_Comm_call_errhandler( comm2, MPI_ERR_OTHER );
-	MPI_Errhandler_free( &newerr );
-	if (calls != 1) {
-	    errs++;
-	    printf( "Error handler not called\n" );
-	}
+            MPI_Comm_set_errhandler( comm, newerr );
+            MPI_Comm_call_errhandler( comm, MPI_ERR_OTHER );
+            if (calls != 1) {
+                errs++;
+                printf( "Error handler not called\n" );
+            }
+            MPI_Comm_dup( comm, &comm2 );
+            calls = 0;
+            mycomm = comm2;
+            /* comm2 must inherit the error handler from comm */
+            MPI_Comm_call_errhandler( comm2, MPI_ERR_OTHER );
+            if (calls != 1) {
+                errs++;
+                printf( "Error handler not called\n" );
+            }
 
-	MPI_Comm_free( &comm );
-	MPI_Comm_free( &comm2 );
+            if (reset_handler) {
+                /* extra checking of the reference count handling */
+                MPI_Comm_set_errhandler( comm, MPI_ERRORS_ARE_FATAL );
+            }
+            MPI_Errhandler_free( &newerr );
+
+            MPI_Comm_free( &comm );
+            MPI_Comm_free( &comm2 );
+        }
     }
 
     MTest_Finalize( errs );
