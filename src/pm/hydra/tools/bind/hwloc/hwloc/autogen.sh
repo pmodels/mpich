9a28d4945f294c0849e74a79e43d885f2e03ee13
William Gropp <gropp@mcs.anl.gov>
2009-12-17 16:59:11 +0000

    [svn-r5985] Use standard environment variables for LIBTOOLIZE and AUTORECONF as well as the MPICH2_xxx versions

--- src/pm/hydra/tools/bind/hwloc/hwloc/autogen.sh
+++ src/pm/hydra/tools/bind/hwloc/hwloc/autogen.sh
@@ -1,5 +1,20 @@
 :
-libtoolize=${MPICH2_LIBTOOLIZE:-libtoolize}
-autoreconf=${MPICH2_AUTORECONF:-autoreconf}
+# This file is used to ensure that the rebuild of the autotools files
+# when run from within MPICH2 by maint/updatefiles will use the 
+# correct versions of the autotools (many systems have versions installed
+# in /usr/bin or /usr/local/bin that are too old to permit successful
+# building of the autotools)
+# Allow the following choices for libtoolize (similarly for autoreconf):
+# LIBTOOLIZE if set, else MPICH2_LIBTOOLIZE if set, else libtoolize
+if [ "X$LIBTOOLIZE" != "X" ] ; then
+    libtoolize=$LIBTOOLIZE
+else
+    libtoolize=${MPICH2_LIBTOOLIZE:-libtoolize}
+fi
+if [ "X$AUTORECONF" != "X" ] ; then
+    autoreconf=$AUTORECONF
+else
+    autoreconf=${MPICH2_AUTORECONF:-autoreconf}
+fi
 $libtoolize
 $autoreconf -vif
