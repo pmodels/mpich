5779a4869bbbd8a4566e58f23d30dbf2c5a6cf4f
Jayesh Krishna <jayesh@mcs.anl.gov>
2010-02-04 21:48:02 +0000

    [svn-r6218] Parsing the node info correctly. Also see ticket #999

--- src/pm/smpd/smpd_ccp_util.c
+++ src/pm/smpd/smpd_ccp_util.c
@@ -10,7 +10,7 @@
 int smpd_get_ccp_nodes(int *np, smpd_host_node_t **host_node_ptr_p)
 {
     smpd_host_node_t *host_node_ptr=NULL, **host_list_tail_p=NULL;
-    int smpd_node_cnt = 0;
+    int smpd_node_cnt = 0, np_total, i;
     char *p=NULL, *tok=NULL, *next_tok=NULL;
     char seps[] = " ,\t\n";
     
@@ -28,8 +28,11 @@ int smpd_get_ccp_nodes(int *np, smpd_host_node_t **host_node_ptr_p)
     host_list_tail_p = host_node_ptr_p;
     
     *np = -1;
+    np_total = 0;
 
-    /* Get the CCP nodes list */
+    /* Get the CCP nodes list 
+     * CCP_NODES = <NUM_OF_HOSTS> <HOST1> <NP_HOST1> <HOST2> <NP_HOST2> ...
+     */
     p = MPIU_Strdup(getenv("CCP_NODES"));
     if(p == NULL){
         smpd_err_printf("Error: Unable to get the list of CCP nodes\n");
@@ -38,7 +41,35 @@ int smpd_get_ccp_nodes(int *np, smpd_host_node_t **host_node_ptr_p)
     }
     smpd_dbg_printf("CCP_NODES = %s\n", p);
     tok = strtok_s(p, seps, &next_tok);
-    while(tok != NULL){
+
+    if(tok == NULL){
+        smpd_err_printf("Error: Unable to parse th list of CCP nodes\n");
+        MPIU_Free(p);
+        smpd_exit_fn(FCNAME);
+        return SMPD_FAIL;
+    }
+    
+    smpd_node_cnt = atoi(tok);
+    for(i=0; i<smpd_node_cnt; i++){
+        char *host;
+        int np_host;
+
+        host = strtok_s(NULL, seps, &next_tok);
+        if(host == NULL){
+            smpd_err_printf("Error: Unable to parse the list of CCP nodes\n");
+            MPIU_Free(p);
+            smpd_exit_fn(FCNAME);
+            return SMPD_FAIL;
+        }
+        tok = strtok_s(NULL, seps, &next_tok);
+        if(tok == NULL){
+            smpd_err_printf("Error: Unable to parse the list of CCP nodes\n");
+            MPIU_Free(p);
+            smpd_exit_fn(FCNAME);
+            return SMPD_FAIL;
+        }
+        np_host = atoi(tok);
+        np_total += np_host;
         /* Allocate memory & set node name */
         host_node_ptr = (smpd_host_node_t *)MPIU_Malloc(sizeof(smpd_host_node_t));
         if(host_node_ptr == NULL){
@@ -52,22 +83,17 @@ int smpd_get_ccp_nodes(int *np, smpd_host_node_t **host_node_ptr_p)
         host_node_ptr->right = NULL;
         host_node_ptr->connected = SMPD_FALSE;
         host_node_ptr->connect_cmd_tag = -1;
-        host_node_ptr->nproc = 1;
+        host_node_ptr->nproc = np_host;
         host_node_ptr->alt_host[0] = '\0';
 
-        MPIU_Strncpy(host_node_ptr->host, tok, SMPD_MAX_HOST_LENGTH);
+        MPIU_Strncpy(host_node_ptr->host, host, SMPD_MAX_HOST_LENGTH);
 
         /* Add the node to the tail of the list */
-
         *host_list_tail_p = host_node_ptr;
         host_list_tail_p = &(host_node_ptr->next);
-
-        smpd_node_cnt++;
-        /* Next node name */
-        tok = strtok_s(NULL, seps, &next_tok);
     }
 
-    *np = smpd_node_cnt;
+    *np = np_total;
     MPIU_Free(p);
     smpd_exit_fn(FCNAME);
     return SMPD_SUCCESS;
