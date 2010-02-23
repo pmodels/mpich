5471d38ca911867ae0428121d2767546e6ee6d56
Robert Latham <robl@mcs.anl.gov>
2010-02-23 17:37:38 +0000

    [svn-r6324] With recent code submissions and testing by the community, tests all pass, so might be ok to auto-detect lustre file systems now.

--- src/mpi/romio/adio/common/ad_fstype.c
+++ src/mpi/romio/adio/common/ad_fstype.c
@@ -346,17 +346,14 @@ static void ADIO_FileSysType_fncall(char *filename, int *fstype, int *error_code
     }
 # endif
 
-/*#if defined(LINUX) && defined(ROMIO_LUSTRE)*/
-#if 0
-    /* disable lustre auto-detection until we figure out why collective i/o
-     * broken */
 #ifdef ROMIO_LUSTRE
-#define LL_SUPER_MAGIC 0x0BD00BD0
+# ifndef LL_SUPER_MAGIC
+#  define LL_SUPER_MAGIC 0x0BD00BD0
+# endif
     if (fsbuf.f_type == LL_SUPER_MAGIC) {
 	*fstype = ADIO_LUSTRE;
 	return;
     }
-# endif
 #endif
 
 # ifdef PAN_KERNEL_FS_CLIENT_SUPER_MAGIC
