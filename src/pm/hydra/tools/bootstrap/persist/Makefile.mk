d4fbcdda9643c049df18c906a8aa6306053dda46
Pavan Balaji <balaji@mcs.anl.gov>
2010-04-17 23:38:38 +0000

    [svn-r6493] Change the name of the persistent bootstrap server from hydserv to hydra_persist to avoid namespace conflicts.

--- src/pm/hydra/tools/bootstrap/persist/Makefile.mk
+++ src/pm/hydra/tools/bootstrap/persist/Makefile.mk
@@ -4,13 +4,13 @@
 #     See COPYRIGHT in top-level directory.
 #
 
-bin_PROGRAMS += hydserv
+bin_PROGRAMS += hydra_persist
 
-hydserv_SOURCES = $(top_srcdir)/tools/bootstrap/persist/persist_server.c
-hydserv_CFLAGS = $(AM_CFLAGS)
-hydserv_LDFLAGS = $(external_ldflags) -L$(top_builddir)
-hydserv_LDADD = -lhydra $(external_libs)
-hydserv_DEPENDENCIES = libhydra.la
+hydra_persist_SOURCES = $(top_srcdir)/tools/bootstrap/persist/persist_server.c
+hydra_persist_CFLAGS = $(AM_CFLAGS)
+hydra_persist_LDFLAGS = $(external_ldflags) -L$(top_builddir)
+hydra_persist_LDADD = -lhydra $(external_libs)
+hydra_persist_DEPENDENCIES = libhydra.la
 
 libhydra_la_SOURCES += $(top_srcdir)/tools/bootstrap/persist/persist_init.c \
 	$(top_srcdir)/tools/bootstrap/persist/persist_launch.c \
