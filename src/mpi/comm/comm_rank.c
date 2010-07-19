b71000b04b3030878f29e7a948c5a8bedcdc8b35
David Goodell <goodell@mcs.anl.gov>
2010-07-19 19:37:00 +0000

    [svn-r6860] MPI_Comm_rank was missing its ALLFUNC critical section
    Reviewed by buntinas@.

--- src/mpi/comm/comm_rank.c
+++ src/mpi/comm/comm_rank.c
@@ -54,6 +54,7 @@ int MPI_Comm_rank( MPI_Comm comm, int *rank )
 
     MPIR_ERRTEST_INITIALIZED_ORDIE();
     
+    MPIU_THREAD_CS_ENTER(ALLFUNC,);
     MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_COMM_RANK);
 
     /* Validate parameters, especially handles needing to be converted */
@@ -96,6 +97,7 @@ int MPI_Comm_rank( MPI_Comm comm, int *rank )
   fn_exit:
 #endif
     MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_COMM_RANK);
+    MPIU_THREAD_CS_EXIT(ALLFUNC,);
     return mpi_errno;
 
     /* --BEGIN ERROR HANDLING-- */
