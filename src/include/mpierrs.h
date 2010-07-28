99595a2ad11a80a7511e150ca7be781bb0aefb33
David Goodell <goodell@mcs.anl.gov>
2010-07-28 21:35:39 +0000

    [svn-r6951] fix problems with the **memcpyalias error message
    This required moving CHKMEMCPYANDSTMT above the "/* --BEGIN ERROR MACROS-- */"
    line.  This lets maint/extracterrmsgs know that **memcpyalias is
    actually used and avoids an "(unknown)(): Internal MPI error!" message
    when memcpy arguments are aliased in MPIR_Localcopy.
    
    Also fix a format string mismatch.
    
    No reviewer.

--- src/include/mpierrs.h
+++ src/include/mpierrs.h
@@ -375,6 +375,12 @@
     }                                                                   \
 }
 
+/* some simple memcpy aliasing checks */
+#define MPIU_ERR_CHKMEMCPYANDSTMT(err_,stmt_,src_,dst_,len_) \
+        MPIU_ERR_CHKANDSTMT3(MPIU_MEM_RANGES_OVERLAP((dst_),(len_),(src_),(len_)),err_,MPI_ERR_INTERN,stmt_,"**memcpyalias","**memcpyalias %p %p %L",(src_),(dst_),(long long)(len_))
+#define MPIU_ERR_CHKMEMCPYANDJUMP(err_,src_,dst_,len_) \
+        MPIU_ERR_CHKMEMCPYANDSTMT((err_),goto fn_fail,(src_),(dst_),(len_))
+
 /* Special MPI error "class/code" for out of memory */
 /* FIXME: not yet done */
 #define MPIR_ERR_MEMALLOCFAILED MPI_ERR_INTERN
@@ -394,12 +400,6 @@
     MPIU_ERR_SETFATALANDSTMT(err_,MPI_ERR_OTHER,goto fn_fail,"**fail")
 #define MPIU_ERR_POPFATAL_LABEL(err_, label_) \
     MPIU_ERR_SETFATALANDSTMT(err_,MPI_ERR_OTHER,goto label_,"**fail")
-
-/* some simple memcpy aliasing checks */
-#define MPIU_ERR_CHKMEMCPYANDSTMT(err_,stmt_,src_,dst_,len_) \
-        MPIU_ERR_CHKANDSTMT3(MPIU_MEM_RANGES_OVERLAP((dst_),(len_),(src_),(len_)),mpi_errno,MPI_ERR_INTERN,stmt_,"**memcpyalias","**memcpy %p %p %L",(src_),(dst_),(len_))
-#define MPIU_ERR_CHKMEMCPYANDJUMP(err_,src_,dst_,len_) \
-        MPIU_ERR_CHKMEMCPYANDSTMT((err_),goto fn_fail,(src_),(dst_),(len_))
 
 /* If you add any macros to this list, make sure that you update
  maint/extracterrmsgs to handle the additional macros (see the hash 
