0a2c57c4ec5dde0effbcbb7dc0a44b31840c2bcf
Pavan Balaji <balaji@mcs.anl.gov>
2010-09-01 01:56:50 +0000

    [svn-r7141] Cleanup the code that waits for the completion of processes. This now cleanly decouples the part that is waiting for the application processes to terminate vs. the part that is waiting for the proxies to terminate.

--- src/pm/hydra/pm/pmiserv/pmiserv_pmci.c
+++ src/pm/hydra/pm/pmiserv/pmiserv_pmci.c
@@ -36,6 +36,9 @@ static HYD_status cleanup_procs(void)
     else {
         HYDU_dump_noprefix(stdout, "Ctrl-C caught... forcing cleanup\n");
 
+        /* Something has gone really wrong! Ask the bootstrap server
+         * to forcefully cleanup the proxies, but this may leave some
+         * of the application processes still running. */
         status = HYDT_bsci_cleanup_procs();
         HYDU_ERR_POP(status, "error cleaning up processes\n");
     }
@@ -317,30 +320,28 @@ HYD_status HYD_pmci_wait_for_completion(int timeout)
 
     HYDU_FUNC_ENTER();
 
-    status = HYDT_bsci_wait_for_completion(timeout);
-    if (status == HYD_TIMED_OUT) {
-        status = HYD_pmcd_pmiserv_cleanup();
-        HYDU_ERR_POP(status, "cleanup of processes failed\n");
-    }
-    HYDU_ERR_POP(status, "bootstrap server returned error waiting for completion\n");
-
-    /* Wait for the processes to terminate */
-    status = HYDT_bsci_wait_for_completion(-1);
-    HYDU_ERR_POP(status, "bootstrap server returned error waiting for completion\n");
-
-    /* If we didn't get a user abort signal yet, wait for the exit
-     * status'es */
+    /* We first wait for the exit statuses to arrive till the timeout
+     * period */
     for (pg = &HYD_handle.pg_list; pg; pg = pg->next) {
         pg_scratch = (struct HYD_pmcd_pmi_pg_scratch *) pg->pg_scratch;
 
         while (pg_scratch->control_listen_fd != HYD_FD_CLOSED) {
-            status = HYDT_dmx_wait_for_event(-1);
+            status = HYDT_dmx_wait_for_event(timeout);
+            if (status == HYD_TIMED_OUT) {
+                status = HYD_pmcd_pmiserv_cleanup();
+                HYDU_ERR_POP(status, "cleanup of processes failed\n");
+            }
             HYDU_ERR_POP(status, "error waiting for event\n");
         }
 
         status = HYD_pmcd_pmi_free_pg_scratch(pg);
         HYDU_ERR_POP(status, "error freeing PG scratch space\n");
     }
+
+    /* Either all application processes exited or we have timed
+     * out. We now wait for all the proxies to terminate. */
+    status = HYDT_bsci_wait_for_completion(-1);
+    HYDU_ERR_POP(status, "bootstrap server returned error waiting for completion\n");
 
   fn_exit:
     HYDU_FUNC_EXIT();
