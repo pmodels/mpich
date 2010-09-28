47defb54621a2fa7089aa8bc4d3bdf2da5c4945b
William Gropp <gropp@mcs.anl.gov>
2010-09-28 14:24:50 +0000

    [svn-r7250] Improved test of MPI_SIZEOF for Fortran

--- test/mpi/f90/datatype/sizeof.f90
+++ test/mpi/f90/datatype/sizeof.f90
@@ -12,25 +12,25 @@
       integer ierr, errs
       integer rank, size, mpisize
       logical verbose
-      real    r1
-      double precision d1
-      complex c1
-      integer i1
-      character ch1
-      logical l1
+      real    r1,r1v(2)
+      double precision d1,d1v(3)
+      complex c1,c1v(4)
+      integer i1,i1v(5)
+      character ch1,ch1v(6)
+      logical l1,l1v(7)
 
       verbose = .false.
       errs = 0
       call mtest_init ( ierr )
       call mpi_comm_rank( MPI_COMM_WORLD, rank, ierr )
 
-!
+! Test of scalar types
       call mpi_sizeof( r1, size, ierr )
       call mpi_type_size( MPI_REAL, mpisize, ierr )
       if (size .ne. mpisize) then
          errs = errs + 1
-         print *, "Size of MPI_REAL = ", mpisize, " but MPI_SIZEOF gives ", &
-              size
+         print *, "Size of MPI_REAL = ", mpisize,                         &
+     &            " but MPI_SIZEOF gives ", size
       endif
 
       call mpi_sizeof( d1, size, ierr )
@@ -45,16 +45,16 @@
       call mpi_type_size( MPI_INTEGER, mpisize, ierr )
       if (size .ne. mpisize) then
          errs = errs + 1
-         print *, "Size of MPI_INTEGER = ", mpisize, " but MPI_SIZEOF gives ", &
-              size
+         print *, "Size of MPI_INTEGER = ", mpisize,                      &
+     &            " but MPI_SIZEOF gives ", size
       endif
 
       call mpi_sizeof( c1, size, ierr )
       call mpi_type_size( MPI_COMPLEX, mpisize, ierr )
       if (size .ne. mpisize) then
          errs = errs + 1
-         print *, "Size of MPI_COMPLEX = ", mpisize, " but MPI_SIZEOF gives ", &
-              size
+         print *, "Size of MPI_COMPLEX = ", mpisize,                      &
+     &            " but MPI_SIZEOF gives ", size
       endif
 
       call mpi_sizeof( ch1, size, ierr )
@@ -69,8 +69,57 @@
       call mpi_type_size( MPI_LOGICAL, mpisize, ierr )
       if (size .ne. mpisize) then
          errs = errs + 1
-         print *, "Size of MPI_LOGICAL = ", mpisize, " but MPI_SIZEOF gives ", &
-              size
+         print *, "Size of MPI_LOGICAL = ", mpisize,                        &
+     &        " but MPI_SIZEOF gives ", size
+      endif
+!
+! Test of vector types (1-dimensional)
+      call mpi_sizeof( r1v, size, ierr )
+      call mpi_type_size( MPI_REAL, mpisize, ierr )
+      if (size .ne. mpisize) then
+         errs = errs + 1
+         print *, "Size of MPI_REAL = ", mpisize,                         &
+     &            " but MPI_SIZEOF gives ", size
+      endif
+
+      call mpi_sizeof( d1v, size, ierr )
+      call mpi_type_size( MPI_DOUBLE_PRECISION, mpisize, ierr )
+      if (size .ne. mpisize) then
+         errs = errs + 1
+         print *, "Size of MPI_DOUBLE_PRECISION = ", mpisize, &
+              " but MPI_SIZEOF gives ", size
+      endif
+
+      call mpi_sizeof( i1v, size, ierr )
+      call mpi_type_size( MPI_INTEGER, mpisize, ierr )
+      if (size .ne. mpisize) then
+         errs = errs + 1
+         print *, "Size of MPI_INTEGER = ", mpisize,                      &
+     &            " but MPI_SIZEOF gives ", size
+      endif
+
+      call mpi_sizeof( c1v, size, ierr )
+      call mpi_type_size( MPI_COMPLEX, mpisize, ierr )
+      if (size .ne. mpisize) then
+         errs = errs + 1
+         print *, "Size of MPI_COMPLEX = ", mpisize,                      &
+     &            " but MPI_SIZEOF gives ", size
+      endif
+
+      call mpi_sizeof( ch1v, size, ierr )
+      call mpi_type_size( MPI_CHARACTER, mpisize, ierr )
+      if (size .ne. mpisize) then
+         errs = errs + 1
+         print *, "Size of MPI_CHARACTER = ", mpisize, &
+              " but MPI_SIZEOF gives ", size
+      endif
+
+      call mpi_sizeof( l1v, size, ierr )
+      call mpi_type_size( MPI_LOGICAL, mpisize, ierr )
+      if (size .ne. mpisize) then
+         errs = errs + 1
+         print *, "Size of MPI_LOGICAL = ", mpisize,                        &
+     &        " but MPI_SIZEOF gives ", size
       endif
 
       call mtest_finalize( errs )
