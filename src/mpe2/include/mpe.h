7105841c764fddec0244f5c86ac1dd0f1c895c27
Anthony Chan <chan@mcs.anl.gov>
2010-10-21 18:20:19 +0000

    [svn-r7347] added the missing file that should be part of r5822 for req#941.

--- src/mpe2/include/mpe.h
+++ src/mpe2/include/mpe.h
@@ -10,18 +10,10 @@
 #ifndef _MPE_INCLUDE
 #define _MPE_INCLUDE
 
-#if defined(__cplusplus)
-extern "C" {
-#endif
-
 #include "mpe_misc.h"
 #include "mpe_log.h"
 #ifdef MPE_GRAPHICS
 #include "mpe_graphics.h"
-#endif
-
-#if defined(__cplusplus)
-};
 #endif
 
 #endif
