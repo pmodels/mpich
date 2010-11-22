7e40a574312de19b7282f3be110c11eaa5f97564
Pavan Balaji <balaji@mcs.anl.gov>
2010-11-22 15:56:08 +0000

    [svn-r7477] There is no need to set the incoming socket to blocking mode. We should set all sockets to non-blocking.

--- src/pm/hydra/utils/sock/sock.c
+++ src/pm/hydra/utils/sock/sock.c
@@ -286,28 +286,6 @@ static HYD_status set_nonblock(int fd)
     goto fn_exit;
 }
 
-static HYD_status set_block(int fd)
-{
-    int flags;
-    HYD_status status = HYD_SUCCESS;
-
-    HYDU_FUNC_ENTER();
-
-    if ((flags = fcntl(fd, F_GETFL, 0)) == -1)
-        flags = 0;
-#if defined O_NONBLOCK
-    if (fcntl(fd, F_SETFL, flags & ~O_NONBLOCK) < 0)
-        HYDU_ERR_SETANDJUMP(status, HYD_SOCK_ERROR, "fcntl failed on %d\n", fd);
-#endif /* O_NONBLOCK */
-
-  fn_exit:
-    HYDU_FUNC_EXIT();
-    return status;
-
-  fn_fail:
-    goto fn_exit;
-}
-
 static HYD_status alloc_fwd_hash(struct fwd_hash **fwd_hash, int in, int out)
 {
     HYD_status status = HYD_SUCCESS;
@@ -323,7 +301,7 @@ static HYD_status alloc_fwd_hash(struct fwd_hash **fwd_hash, int in, int out)
 
     (*fwd_hash)->next = NULL;
 
-    status = set_block(in);
+    status = set_nonblock(in);
     HYDU_ERR_POP(status, "unable to set out-socket to non-blocking\n");
 
     status = set_nonblock(out);
