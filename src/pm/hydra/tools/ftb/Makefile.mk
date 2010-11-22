197fa147b8b60ec38163ac3ab9215aaf3cdb6390
Pavan Balaji <balaji@mcs.anl.gov>
2010-11-22 15:54:33 +0000

    [svn-r7474] Squash the FTB warnings.

--- src/pm/hydra/tools/ftb/Makefile.mk
+++ src/pm/hydra/tools/ftb/Makefile.mk
@@ -8,4 +8,6 @@ AM_CPPFLAGS += -I$(top_srcdir)/tools/ftb
 
 if hydra_have_ftb
 libhydra_la_SOURCES += $(top_srcdir)/tools/ftb/hydt_ftb.c
+else
+libhydra_la_SOURCES += $(top_srcdir)/tools/ftb/hydt_ftb_dummy.c
 endif
--- src/pm/hydra/tools/ftb/hydt_ftb.h
+++ src/pm/hydra/tools/ftb/hydt_ftb.h
@@ -9,32 +9,13 @@
 
 #if defined ENABLE_FTB
 #include <libftb.h>
-
 #define HYDT_FTB_MAX_PAYLOAD_DATA FTB_MAX_PAYLOAD_DATA
+#else
+#define HYDT_FTB_MAX_PAYLOAD_DATA (1024)
+#endif /* ENABLE_FTB */
 
 HYD_status HYDT_ftb_init(void);
 HYD_status HYDT_ftb_finalize(void);
 HYD_status HYDT_ftb_publish(const char *event_name, const char *event_payload);
-
-#else
-
-#define HYDT_FTB_MAX_PAYLOAD_DATA (1024)
-
-static HYD_status HYDT_ftb_init(void)
-{
-    return HYD_SUCCESS;
-}
-
-static HYD_status HYDT_ftb_finalize(void)
-{
-    return HYD_SUCCESS;
-}
-
-static HYD_status HYDT_ftb_publish(const char *event_name, const char *event_payload)
-{
-    return HYD_SUCCESS;
-}
-
-#endif /* ENABLE_FTB */
 
 #endif /* HYDT_FTB_H_INCLUDED */
