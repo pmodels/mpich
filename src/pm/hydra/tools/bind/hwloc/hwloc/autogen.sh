6add62bdfb750ae2b7208f2ffb5f5327ca624ad6
Pavan Balaji <balaji@mcs.anl.gov>
2010-12-27 08:42:15 +0000

    [svn-r7615] Use the MPICH2-specific autogen.sh for hwloc.

--- src/pm/hydra/tools/bind/hwloc/hwloc/autogen.sh
+++ src/pm/hydra/tools/bind/hwloc/hwloc/autogen.sh
@@ -1,2 +1,11 @@
-:
-autoreconf -ivf
+#! /bin/sh
+
+if [ -n "$MPICH2_AUTOTOOLS_DIR" ] ; then
+    libtoolize=${MPICH2_AUTOTOOLS_DIR}/libtoolize
+    autoreconf=${MPICH2_AUTOTOOLS_DIR}/autoreconf
+else
+    libtoolize=${LIBTOOLIZE:-libtoolize}
+    autoreconf=${AUTORECONF:-autoreconf}
+fi
+
+$autoreconf -vif
