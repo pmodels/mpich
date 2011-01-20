4e71fee18dc33f66cf779f43146bb2b6e026949e
David Goodell <goodell@mcs.anl.gov>
2011-01-20 22:08:33 +0000

    [svn-r7776] fix bad copyright dates in NBC sources caused by the code generation script
    Reviewed by balaji@.

--- src/mpi/coll/ireduce.c
+++ src/mpi/coll/ireduce.c
@@ -1,6 +1,6 @@
 /* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
 /*
- *  (C) 110 by Argonne National Laboratory.
+ *  (C) 2010 by Argonne National Laboratory.
  *      See COPYRIGHT in top-level directory.
  */
 
