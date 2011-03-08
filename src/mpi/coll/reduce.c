c73de10d25292d390b689c8b2848405bf0dec5d1
Darius Buntinas <buntinas@mcs.anl.gov>
2011-03-08 17:20:46 +0000

    [svn-r8162] repositioned braces around ifdefs added in r8135 to fix compile errors when c++ bindings are disabled

--- src/mpi/coll/reduce.c
+++ src/mpi/coll/reduce.c
@@ -192,8 +192,9 @@ static int MPIR_Reduce_binomial (
                         (*MPIR_Process.cxx_call_op_fn)( tmp_buf, recvbuf, 
                                                         count, datatype, uop );
                     }
-                    else {
+                    else
 #endif
+                    {
 #if defined(HAVE_FORTRAN_BINDING) && !defined(HAVE_FINT_IS_INT)
 			if (is_f77_uop) {
 			    MPI_Fint lcount = (MPI_Fint)count;
@@ -214,8 +215,9 @@ static int MPIR_Reduce_binomial (
                         (*MPIR_Process.cxx_call_op_fn)( recvbuf, tmp_buf,
                                                         count, datatype, uop );
                     }
-                    else {
+                    else
 #endif
+                    {
 #if defined(HAVE_FORTRAN_BINDING) && !defined(HAVE_FINT_IS_INT)
 			if (is_f77_uop) {
 			    /* In this case, the integer types do not match */
@@ -376,7 +378,7 @@ static int MPIR_Reduce_redscat_gather (
         else
             is_commutative = 1;
         
-#ifdef HAVE_CXX_BINDING            
+#ifdef HAVE_CXX_BINDING
             if (op_ptr->language == MPID_LANG_CXX) {
                 uop = (MPI_User_function *) op_ptr->function.c_function;
                 is_cxx_uop = 1;
@@ -478,7 +480,7 @@ static int MPIR_Reduce_redscat_gather (
                                                 datatype,
                                                 uop ); 
             }
-            else 
+            else
 #endif
                 (*uop)(tmp_buf, recvbuf, &count, &datatype);
 
@@ -565,7 +567,7 @@ static int MPIR_Reduce_redscat_gather (
                                                disps[recv_idx]*extent, 
                                                recv_cnt, datatype, uop);
             }
-            else 
+            else
 #endif
                 (*uop)((char *) tmp_buf + disps[recv_idx]*extent,
                        (char *) recvbuf + disps[recv_idx]*extent, 
