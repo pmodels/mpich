a2ccaae37d9d99e8385e1ecc3ded35763550786a
Robert Latham <robl@mcs.anl.gov>
2011-04-21 16:07:37 +0000

    [svn-r8462] read-and-broadcast when everyone reads same region
    another workaroudn for bluegene out of memory conditions: when all processes
    wish to read the same data, just do a read-and-broadcast instead of the full
    two-phase algorithm.

--- src/mpi/romio/adio/ad_bgl/ad_bgl_rdcoll.c
+++ src/mpi/romio/adio/ad_bgl/ad_bgl_rdcoll.c
@@ -93,6 +93,8 @@ extern void ADIOI_Calc_my_off_len(ADIO_File fd, int bufcount, MPI_Datatype
 
 static int ADIOI_too_much_memory_for_alltoallv(int nprocs, 
 		int * send_size, int *recv_size);
+static int ADIOI_allsame_access(ADIO_File fd, ADIO_Offset *st_offsets, 
+		ADIO_Offset *end_offsets);
 
 void ADIOI_BGL_ReadStridedColl(ADIO_File fd, void *buf, int count,
 			       MPI_Datatype datatype, int file_ptr_type,
@@ -252,6 +254,30 @@ void ADIOI_BGL_ReadStridedColl(ADIO_File fd, void *buf, int count,
 
 	return;
     }
+
+    /* one more check: if we have a "everyone reads the same data" workload (a
+     * config file, an initial mesh dataset, etc), then the full two-phase
+     * algorithm is overkill and we should just read-and-broadcast */
+
+    if (ADIOI_allsame_access(fd, st_offsets, end_offsets)) {
+	*error_code = MPI_SUCCESS;
+        if (myrank == fd->hints->ranklist[0])  {
+            ADIO_ReadContig(fd, buf, count, datatype, file_ptr_type, 
+			    offset, status, error_code);
+	}
+	MPI_Bcast(buf, count, datatype, fd->hints->ranklist[0], fd->comm);
+#ifdef HAVE_STATUS_SET_BYTES
+	MPI_Type_size(datatype, &size);
+	bufsize = size * count;
+	MPIR_Status_set_bytes(status, datatype, bufsize);
+	ADIOI_Free(offset_list);
+	ADIOI_Free(len_list);
+	ADIOI_Free(st_offsets);
+	ADIOI_Free(end_offsets);
+#endif
+	return;
+    }
+
 
 #if BGL_PROFILE 
     BGLMPIO_T_CIO_SET_GET( 0, r, 1, 1, 1, BGLMPIO_CIO_FD_PART, BGLMPIO_CIO_PATANA )
@@ -1192,4 +1218,29 @@ static int ADIOI_too_much_memory_for_alltoallv(int nprocs,
 
 	if (mem_required > threshold) return 1;
 	return 0;
+}
+
+static int ADIOI_allsame_access(ADIO_File fd, ADIO_Offset *st_offsets, 
+		ADIO_Offset *end_offsets) 
+{
+    /* TODO: The general solution would look at the file type and use this
+     * approach for any file view.  The list of start and end offsets, however,
+     * only gives us enough information to make this decision for
+     * contig-in-file. */
+    int filetype_is_contig;
+    int allsame=0, i, nprocs;
+
+    ADIOI_Datatype_iscontig(fd->filetype, &filetype_is_contig);
+    MPI_Comm_size(fd->comm, &nprocs);
+
+    if (filetype_is_contig) {
+	for(i=1, allsame=1; i<nprocs; i++) {
+            if( (st_offsets[i-1] != st_offsets[i]) || 
+			    (end_offsets[i-1] != end_offsets[i]) ) {
+	        allsame=0;
+	        break;
+	    }
+	}
+    }
+    return (allsame);
 }
