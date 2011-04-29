bea9be880cc8798b00ef9b9e53ed12bd2c048b39
Pavan Balaji <balaji@mcs.anl.gov>
2011-04-29 22:27:31 +0000

    [svn-r8507] Improved checks to see if a process actually terminated badly. Thanks to Yauheni Zelenko for reporting this issue.

--- src/pm/hydra/pm/pmiserv/pmiserv_cb.c
+++ src/pm/hydra/pm/pmiserv/pmiserv_cb.c
@@ -255,7 +255,7 @@ static HYD_status control_cb(int fd, HYD_event_t events, void *userp)
          * processes */
         if (HYD_server_info.user_global.auto_cleanup) {
             for (i = 0; i < proxy->proxy_process_count; i++) {
-                if (proxy->exit_status[i]) {
+                if (!WIFEXITED(proxy->exit_status[i])) {
                     HYDU_dump_noprefix
                         (stdout, "\n====================================================");
                     HYDU_dump_noprefix(stdout, "=================================\n");
