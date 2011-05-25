b2e0c03a8ee3fa45e8a5bbee729c913ad0cf352c
David Goodell <goodell@mcs.anl.gov>
2011-05-25 18:02:12 +0000

    [svn-r8612] add conditional `extern "C" {}' guards in mpix.h for C++ compilers
    The extensions in mpix.h are all written in C and are exported as C
    symbol names.  When mpix.h is included by a C++ compiler, the C++ must
    understand that these are C prototypes and not attempt to generate
    undefined symbols that point to mangled C++ versions of the symbol
    names, otherwise an error will occur when the application is linked.
    
    No reviewer.

--- src/include/mpix.h
+++ src/include/mpix.h
@@ -9,6 +9,17 @@
 
 #include "mpi.h"
 
+/* Prototypes in this file must be interpreted as C routines when this header
+ * file is compiled by a C++ compiler.  "mpi.h" already does this for the main
+ * MPI_ routines. */
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
 int MPIX_Group_comm_create(MPI_Comm old_comm, MPI_Group group, int tag, MPI_Comm * new_comm);
+
+#if defined(__cplusplus)
+}
+#endif
 
 #endif /* MPIX_H_INCLUDED */
