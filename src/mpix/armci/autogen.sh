676961613ab7f1cc359bf1412aaf6916d765a669
Anthony Chan <chan@mcs.anl.gov>
2011-08-11 17:25:21 +0000

    [svn-r8887] Fixed autogen.sh so that it is consistent with other autogen.sh in mpich2.

--- src/mpix/armci/autogen.sh
+++ src/mpix/armci/autogen.sh
@@ -1,3 +1,11 @@
 #! /bin/sh
 
-autoreconf -vif
+if [ -n "$MPICH2_AUTOTOOLS_DIR" ] ; then
+    libtoolize=${MPICH2_AUTOTOOLS_DIR}/libtoolize
+    autoreconf=${MPICH2_AUTOTOOLS_DIR}/autoreconf
+else
+    libtoolize=${LIBTOOLIZE:-libtoolize}
+    autoreconf=${AUTORECONF:-autoreconf}
+fi
+
+$autoreconf -vif
