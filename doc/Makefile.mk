c35edd7fddcb18fd6869af5639a023419f042d28
David Goodell <goodell@mcs.anl.gov>
2011-11-12 02:38:13 +0000

    [svn-r9162] and the umpteenth forgotten file is: doc/Makefile.mk
    Missed from r9158.  Thanks to Anthony and Pavan for catching it.
    
    No reviewer.

---
+++ doc/Makefile.mk
@@ -0,0 +1,29 @@
+## -*- Mode: Makefile; -*-
+## vim: set ft=automake :
+##
+## (C) 2011 by Argonne National Laboratory.
+##     See COPYRIGHT in top-level directory.
+##
+
+DEVELOPER_SUBDIRS = doc/pmi doc/namepub
+DOC_SUBDIRS += doc/mansrc doc/userguide doc/installguide doc/smpd doc/logging \
+	       doc/windev doc/design
+
+userdocs:
+	for dir in $(DOC_SUBDIRS) ; do \
+	    (cd $$dir && $(MAKE) ) ; done
+
+devdocs:
+	for dir in $(DEVELOPER_SUBDIRS) ; do \
+	    (cd $$dir && $(MAKE) ) ; done
+
+install-devdocs:
+	if [ -z "$(DEV_INSTALL_PREFIX)" ] ; then \
+	    echo "DEV_INSTALL_PREFIX must be defined" ; \
+	    exit 1 ; \
+	fi
+	for dir in $(DEVELOPER_SUBDIRS) ; do \
+	        export DEV_INSTALL_PREFIX ; \
+	        ( cd $$dir && $(MAKE) install-devdocs ) ; \
+	done
+
