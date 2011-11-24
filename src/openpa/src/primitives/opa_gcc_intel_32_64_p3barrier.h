a42a6699b8e765c4f736b75a2bb597dde27e1cab
Pavan Balaji <balaji@mcs.anl.gov>
2011-11-24 22:40:26 +0000

    [svn-r9227] Updated OPA external link to r128 to get the comment describing the fix in r127.

--- src/openpa/src/primitives/opa_gcc_intel_32_64_p3barrier.h
+++ src/openpa/src/primitives/opa_gcc_intel_32_64_p3barrier.h
@@ -7,9 +7,12 @@
 #ifndef OPA_GCC_INTEL_32_64_P3BARRIER_H_INCLUDED
 #define OPA_GCC_INTEL_32_64_P3BARRIER_H_INCLUDED
 
-/* Pentium III and earlier processors have store barriers (sfence),
-   but no full barrier or load barriers.  However, the lock prefix
-   orders loads and stores (essentially doing a full barrier).  */
+/* Some Pentium III and earlier processors have store barriers
+   (sfence), but no full barrier or load barriers.  Some other
+   x86-like processors don't seem to have sfence either. The lock
+   prefix orders loads and stores (essentially doing a full
+   barrier). We force everything to a full barrier for compatibility
+   with such processors. */
 
 #define OPA_write_barrier OPA_read_write_barrier
 #define OPA_read_barrier OPA_read_write_barrier
