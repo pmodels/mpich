249d18553c68a2afe3a44f213a5adb5aa7b8ef3f
Pavan Balaji <balaji@mcs.anl.gov>
2011-11-24 20:50:10 +0000

    [svn-r9226] Update OPA external link to get r127 for the sfence fix.

--- src/openpa/src/primitives/opa_gcc_intel_32_64_p3barrier.h
+++ src/openpa/src/primitives/opa_gcc_intel_32_64_p3barrier.h
@@ -11,18 +11,12 @@
    but no full barrier or load barriers.  However, the lock prefix
    orders loads and stores (essentially doing a full barrier).  */
 
-#define OPA_write_barrier() __asm__ __volatile__ ( "sfence" ::: "memory" )
-
-static inline void OPA_read_barrier(void) {
-    int a = 0;
-    __asm__ __volatile__ ("lock orl $0, %0" : "+m" (a));
-}
+#define OPA_write_barrier OPA_read_write_barrier
+#define OPA_read_barrier OPA_read_write_barrier
 
 static inline void OPA_read_write_barrier(void) {
     int a = 0;
     __asm__ __volatile__ ("lock orl $0, %0" : "+m" (a));
 }
-
-
 
 #endif /* OPA_GCC_INTEL_32_64_P3BARRIER_H_INCLUDED */
