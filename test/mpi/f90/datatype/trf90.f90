f62493b8f4427f391543f6b1f59c6f55a50f167f
David Goodell <goodell@mcs.anl.gov>
2011-12-16 22:35:24 +0000

    [svn-r9376] fix up f90/datatype/trf90 for the MPICH2 test suite
    Output " No errors" and make sure to free the contig that was
    created.
    
    No reviewer.

--- test/mpi/f90/datatype/trf90.f90
+++ test/mpi/f90/datatype/trf90.f90
@@ -3,16 +3,22 @@
 !  (C) 2011 by Argonne National Laboratory.
 !      See COPYRIGHT in top-level directory.
 !
-! Based on a isample program that triggered a segfault in MPICH2
+! Based on a sample program that triggered a segfault in MPICH2
 program testf90_mpi
   use mpi
   implicit none
+  integer errs
   integer :: rk_mpi, ierr, ctype
 
-  call MPI_Init(ierr)
+  errs = 0
+  call mtest_init(ierr)
+
   call MPI_Type_create_f90_real(15, MPI_UNDEFINED, rk_mpi, ierr)
   call MPI_Type_contiguous(19, rk_mpi, ctype, ierr)
   call MPI_Type_commit(ctype, ierr)
+  call MPI_Type_free(ctype, ierr)
+
+  call mtest_finalize(errs)
   call MPI_Finalize(ierr)
 
 end program testf90_mpi
