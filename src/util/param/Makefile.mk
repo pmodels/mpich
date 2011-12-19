466e931958235d720675567ea6bae0b8a935d1f5
David Goodell <goodell@mcs.anl.gov>
2011-12-19 20:53:50 +0000

    [svn-r9390] force params rebuild when the genparams script is updated
    No reviewer.

--- src/util/param/Makefile.mk
+++ src/util/param/Makefile.mk
@@ -12,7 +12,7 @@ dist_noinst_DATA += src/util/param/params.yml
 
 if MAINTAINER_MODE
 # normally built by maint/updatefiles, but this rebuild rule is here
-src/util/param/param_vals.c: src/util/param/params.yml
+src/util/param/param_vals.c: src/util/param/params.yml $(top_srcdir)/maint/genparams
 	( cd $(top_srcdir) && ./maint/genparams )
 endif MAINTAINER_MODE
 
