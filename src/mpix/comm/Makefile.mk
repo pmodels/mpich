4400a94ba5e44bb245e9eb26f62b19e5a95337e7
Darius Buntinas <buntinas@mcs.anl.gov>
2011-12-19 23:22:58 +0000

    [svn-r9401] removed missing file from Makefile.mk

--- src/mpix/comm/Makefile.mk
+++ src/mpix/comm/Makefile.mk
@@ -5,11 +5,6 @@
 ##     See COPYRIGHT in top-level directory.
 ##
 
-# The group_comm code does not currently have a PMPIX version.  If/when it does,
-# it should be moved to the "mpi_sources" variable.
-lib_lib@MPILIBNAME@_la_SOURCES +=   \
-    src/mpix/comm/group_comm.c
-
 mpi_sources +=					\
     src/mpix/comm/comm_group_failed.c		\
     src/mpix/comm/comm_remote_group_failed.c
