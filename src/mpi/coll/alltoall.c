dd7e55f8de33cfd8b6eb61b75714e9ad088235df
Pavan Balaji <balaji@mcs.anl.gov>
2012-02-28 05:32:10 +0000

    [svn-r9544] Bug-fix: For MPI_ALLTOALL, do error checking on sendcount and sendtype only when the send buffer is not MPI_IN_PLACE.  See tt#1564.

--- src/mpi/coll/alltoall.c
+++ src/mpi/coll/alltoall.c
@@ -784,15 +784,19 @@ int MPI_Alltoall(void *sendbuf, int sendcount, MPI_Datatype sendtype,
 	    
             MPID_Comm_valid_ptr( comm_ptr, mpi_errno );
             if (mpi_errno != MPI_SUCCESS) goto fn_fail;
-	    MPIR_ERRTEST_COUNT(sendcount, mpi_errno);
-	    MPIR_ERRTEST_COUNT(recvcount, mpi_errno);
-	    MPIR_ERRTEST_DATATYPE(sendtype, "sendtype", mpi_errno);
-	    MPIR_ERRTEST_DATATYPE(recvtype, "recvtype", mpi_errno);
-            if (HANDLE_GET_KIND(sendtype) != HANDLE_KIND_BUILTIN) {
-                MPID_Datatype_get_ptr(sendtype, sendtype_ptr);
-                MPID_Datatype_valid_ptr( sendtype_ptr, mpi_errno );
-                MPID_Datatype_committed_ptr( sendtype_ptr, mpi_errno );
+
+            if (sendbuf != MPI_IN_PLACE) {
+                MPIR_ERRTEST_COUNT(sendcount, mpi_errno);
+                MPIR_ERRTEST_DATATYPE(sendtype, "sendtype", mpi_errno);
+                if (HANDLE_GET_KIND(sendtype) != HANDLE_KIND_BUILTIN) {
+                    MPID_Datatype_get_ptr(sendtype, sendtype_ptr);
+                    MPID_Datatype_valid_ptr( sendtype_ptr, mpi_errno );
+                    MPID_Datatype_committed_ptr( sendtype_ptr, mpi_errno );
+                }
             }
+
+	    MPIR_ERRTEST_COUNT(recvcount, mpi_errno);
+	    MPIR_ERRTEST_DATATYPE(recvtype, "recvtype", mpi_errno);
             if (HANDLE_GET_KIND(recvtype) != HANDLE_KIND_BUILTIN) {
                 MPID_Datatype_get_ptr(recvtype, recvtype_ptr);
                 MPID_Datatype_valid_ptr( recvtype_ptr, mpi_errno );
