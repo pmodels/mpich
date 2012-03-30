16e67ba90cb8873a99fa94410a7bc4cf4ff46215
David Goodell <goodell@mcs.anl.gov>
2012-03-30 21:55:36 +0000

    [svn-r9650] tt#1584: make qdemo and tvtest programs work
    You must "make" them explicitly, and only after MPICH2 has been
    installed.
    
    No reviewer.

--- src/mpi/debugger/Makefile.mk
+++ src/mpi/debugger/Makefile.mk
@@ -13,11 +13,10 @@ noinst_HEADERS += src/mpi/debugger/mpich2_dll_defs.h
 
 # workaround the fact that autoconf is missing per-object flags by using a dummy
 # convenience library that isn't installed
-noinst_LTLIBRARIES = lib/libdbginitdummy.la
-lib_libdbginitdummy_la_SOURCES = src/mpi/debugger/dbginit.c
-lib_libdbginitdummy_la_CFLAGS = -g
+noinst_LTLIBRARIES += src/mpi/debugger/libdbginitdummy.la
+src_mpi_debugger_libdbginitdummy_la_SOURCES = src/mpi/debugger/dbginit.c
+src_mpi_debugger_libdbginitdummy_la_CFLAGS = -g
 lib_lib@MPILIBNAME@_la_LIBADD += $(top_builddir)/src/mpi/debugger/libdbginitdummy.la
-
 
 lib_LTLIBRARIES += lib/libtvmpich2.la
 # There is no static debugger interface library
@@ -25,17 +24,24 @@ lib_libtvmpich2_la_SOURCES = src/mpi/debugger/dll_mpich2.c
 lib_libtvmpich2_la_CFLAGS = -g
 lib_libtvmpich2_la_LDFLAGS = -g
 
-# FIXME work is needed here to force these to build late, after mpicc is working
-#
 # tvtest builds a main program that uses the routines in dll_mpich2 to 
 # access the internal structure of an MPICH2 program.  This is only a partial
 # test, but it allows a developer to check out the basic functioning of 
 # dll_mpich2 (but without loading it).
-# tvtest can no longer be built using the mpich libraries - mpich2 no longer
-# places all necessary object files into the MPILIBNAME or PMPILIBNAME libraries
-#tvtest_SOURCES = tvtest.c dll_mpich2.c dbgstub.c
-#qdemo_SOURCES = qdemo.c
-#EXTRA_PROGRAMS = tvtest qdemo
+#
+# NB: these tests can only be built *after* a "make install" step.  They must be
+# built explicitly
+src/mpi/debugger/tvtest: src/mpi/debugger/tvtest.o src/mpi/debugger/dbgstub.o
+	$(bindir)/mpicc -o $@ $^ -ltvmpich2
+
+src/mpi/debugger/tvtest.o: src/mpi/debugger/tvtest.c
+	$(bindir)/mpicc -c -o $@ $<
+
+# no rule for dbgstub.o, it needs to be built by suffix rules in the usual
+# fashion because it needs direct access to MPICH2 internal headers
+
+src/mpi/debugger/qdemo: src/mpi/debugger/qdemo.c
+	$(bindir)/mpicc -o $@ $^
 
 endif BUILD_DEBUGGER_DLL
 
