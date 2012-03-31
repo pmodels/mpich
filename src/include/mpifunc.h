56c130cb528fc477dcb7c967b27a759967507ac4
Pavan Balaji <balaji@mcs.anl.gov>
2012-03-31 23:06:29 +0000

    [svn-r9662] Fix an error that got introduced in r9659.

--- src/include/mpifunc.h
+++ src/include/mpifunc.h
@@ -8,6 +8,7 @@
 #define MPIFUNC_H_INCLUDED
 
 /* state declaration macros */
+#if defined(USE_DBG_LOGGING) || defined(MPICH_DEBUG_MEMARENA)
 #define MPIR_STATE_DECL(a)
 #define MPID_MPI_STATE_DECL(a)		MPIR_STATE_DECL(a)
 #define MPID_MPI_INIT_STATE_DECL(a)	MPIR_STATE_DECL(a)
@@ -15,6 +16,11 @@
 #define MPIDI_STATE_DECL(a)		MPIR_STATE_DECL(a)
 #define MPIDI_INIT_STATE_DECL(a)	MPIR_STATE_DECL(a)
 #define MPIDI_FINALIZE_STATE_DECL(a)	MPIR_STATE_DECL(a)
+
+/* Tell the package to define the rest of the enter/exit macros in
+   terms of these */
+#define NEEDS_FUNC_ENTER_EXIT_DEFS 1
+#endif /* USE_DBG_LOGGING || MPICH_DEBUG_MEMARENA */
 
 /* function enter and exit macros */
 #if defined(USE_DBG_LOGGING)
@@ -28,9 +34,5 @@
 #elif defined(MPICH_DEBUG_MEMARENA)
 #define MPIR_FUNC_EXIT(a) MPIU_DBG_MSG(ROUTINE_EXIT,TYPICAL,"Leaving "#a)
 #endif
-
-/* Tell the package to define the rest of the enter/exit macros in
-   terms of these */
-#define NEEDS_FUNC_ENTER_EXIT_DEFS 1
 
 #endif /* MPIFUNC_H_INCLUDED */
