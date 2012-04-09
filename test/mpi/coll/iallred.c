3e6cc34f6d703cc1fa80f8a73b054b1ac4a2ba23
David Goodell <goodell@mcs.anl.gov>
2012-04-09 22:58:49 +0000

    [svn-r9686] tt#1601: disable "COLL_FT_RET" by default
    The problem is that the (previously on by default) collective error
    propagation functionality is implemented in a way that is fundamentally
    incompatible with the way that nonblocking collectives are implemented.
    
    Thanks to Jed Brown for the bug report and reproducer.
    
    No reviewer.

---
+++ test/mpi/coll/iallred.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <assert.h>
+#include "mpi.h"
+
+/* Since MPICH2 is currently the only NBC implementation in existence, just use
+ * this quick-and-dirty #ifdef to decide whether to test the nonblocking
+ * collectives.  Eventually we can add a configure option or configure test, or
+ * the MPI-3 standard will be released and these can be gated on a MPI_VERSION
+ * check */
+#if !defined(USE_STRICT_MPI) && defined(MPICH2)
+#define TEST_NBC_ROUTINES 1
+#endif
+
+int main(int argc, char *argv[])
+{
+    MPI_Request request;
+    int size, rank;
+    int one = 1, two = 2, isum, sum;
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    assert(size == 2);
+#if defined(TEST_NBC_ROUTINES)
+    MPIX_Iallreduce(&one,&isum,1,MPI_INT,MPI_SUM,MPI_COMM_WORLD,&request);
+    MPI_Allreduce(&two,&sum,1,MPI_INT,MPI_SUM,MPI_COMM_WORLD);
+    MPI_Wait(&request,MPI_STATUS_IGNORE);
+
+    assert(isum == 2);
+    assert(sum == 4);
+    if (rank == 0)
+        printf(" No errors\n");
+#endif
+
+    MPI_Finalize();
+    return 0;
+}
+
