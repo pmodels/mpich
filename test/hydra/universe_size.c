425e7467239e5340cbb8dd334af3b0b0f06bd9d5
Pavan Balaji <balaji@mcs.anl.gov>
2012-05-03 21:52:58 +0000

    [svn-r9833] Started a directory for hydra-specific tests.

---
+++ test/hydra/universe_size.c
@@ -0,0 +1,41 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ *
+ *  (C) 2001 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
+#include <stdio.h>
+#include "mpi.h"
+
+int main(int argc, char **argv)
+{
+    int rc, flag, vval, size;
+    void *v;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    rc = MPI_Attr_get(MPI_COMM_WORLD, MPI_UNIVERSE_SIZE, &v, &flag);
+    if (rc) {
+        fprintf(stderr, "MPI_UNIVERSE_SIZE missing\n");
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+    else {
+        /* MPI_UNIVERSE_SIZE need not be set */
+        if (flag) {
+            vval = *(int *) v;
+            if (vval < 5) {
+                fprintf(stderr, "MPI_UNIVERSE_SIZE = %d, less than expected (%d)\n", vval,
+                        size);
+                MPI_Abort(MPI_COMM_WORLD, 1);
+            }
+        }
+    }
+
+    MPI_Finalize();
+
+    printf(" No errors\n");
+
+    return 0;
+}
