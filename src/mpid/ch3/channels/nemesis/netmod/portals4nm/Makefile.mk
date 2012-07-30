b7d25398e84b28204635a3348dc115470452d0d1
Darius Buntinas <buntinas@mcs.anl.gov>
2012-07-30 20:28:00 +0000

    [svn-r10064] updated paths in portals netmod makefile

--- src/mpid/ch3/channels/nemesis/netmod/portals4nm/Makefile.mk
+++ src/mpid/ch3/channels/nemesis/netmod/portals4nm/Makefile.mk
@@ -8,12 +8,12 @@
 if BUILD_NEMESIS_NETMOD_PORTALS4NM
 
 lib_lib@MPILIBNAME@_la_SOURCES +=                                   \
-    src/mpid/ch3/channels/nemesis/nemesis/netmod/portals4nm/ptlnm_init.c \
-    src/mpid/ch3/channels/nemesis/nemesis/netmod/portals4nm/ptlnm_poll.c \
-    src/mpid/ch3/channels/nemesis/nemesis/netmod/portals4nm/ptlnm_send.c 
+    src/mpid/ch3/channels/nemesis/netmod/portals4nm/ptlnm_init.c \
+    src/mpid/ch3/channels/nemesis/netmod/portals4nm/ptlnm_poll.c \
+    src/mpid/ch3/channels/nemesis/netmod/portals4nm/ptlnm_send.c 
 
 noinst_HEADERS +=                                                \
-    src/mpid/ch3/channels/nemesis/nemesis/netmod/portals4nm/ptlnm_impl.h 
+    src/mpid/ch3/channels/nemesis/netmod/portals4nm/ptlnm_impl.h 
 
 endif BUILD_NEMESIS_NETMOD_PORTALS4NM
 
