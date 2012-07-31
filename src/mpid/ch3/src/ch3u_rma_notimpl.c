cb7594d5bf901a733c4a1539836d183d93a6909a
James Dinan <dinan@mcs.anl.gov>
2012-07-31 21:27:09 +0000

    [svn-r10075] Fixed symbol concatenation and added FUNCNAME macros to quiet warnings during autogen.

--- src/mpid/ch3/src/ch3u_rma_notimpl.c
+++ src/mpid/ch3/src/ch3u_rma_notimpl.c
@@ -7,17 +7,20 @@
 #include "mpidi_ch3_impl.h"
 #include "mpidrma.h"
 
-
+/* FUNCNAME macros are included for the MPIDI_FUNC_NOTIMPL macro to keep the
+ * state checker from emitting warnings */
+#undef FUNCNAME
+#define FUNCNAME MPIDI_FUNC_NOTIMPL
 #define MPIDI_FUNC_NOTIMPL(state_name)                          \
     int mpi_errno = MPI_SUCCESS;                                \
                                                                 \
-    MPIDI_STATE_DECL(MPID_STATE_MPIDI_state_name);              \
-    MPIDI_RMA_FUNC_ENTER(MPID_STATE_MPIDI_state_name);          \
+    MPIDI_STATE_DECL(MPID_STATE_MPIDI_##state_name);            \
+    MPIDI_RMA_FUNC_ENTER(MPID_STATE_MPIDI_##state_name);        \
                                                                 \
     MPIU_ERR_SETANDJUMP(mpi_errno, MPI_ERR_OTHER, "**notimpl"); \
                                                                 \
  fn_exit:                                                       \
-    MPIDI_RMA_FUNC_EXIT(MPID_STATE_MPIDI_state_name);           \
+    MPIDI_RMA_FUNC_EXIT(MPID_STATE_MPIDI_##state_name);         \
     return mpi_errno;                                           \
     /* --BEGIN ERROR HANDLING-- */                              \
  fn_fail:                                                       \
