50d7c0f642ffe0fb63c230f061d0c5c24a9498ca
David Goodell <goodell@mcs.anl.gov>
2012-08-08 22:43:31 +0000

    [svn-r10117] tt#1671: new longdouble test and allred tweak
    Based on suggestions from Jim Hoekstra at Iowa State University.
    
    No reviewer.

--- test/mpi/coll/allred.c
+++ test/mpi/coll/allred.c
@@ -3,6 +3,10 @@
  *  (C) 2001 by Argonne National Laboratory.
  *      See COPYRIGHT in top-level directory.
  */
+/*      Warning - this test will fail for MPI_PROD & maybe MPI_SUM
+ *        if more than 10 MPI processes are used.  Loss of precision
+ *        will occur as the number of processors is increased.
+ */
 
 #include "mpi.h"
 #include "mpitest.h"
@@ -306,12 +310,14 @@ struct double_test { double a; int b; };
 
 #if MTEST_HAVE_MIN_MPI_VERSION(2,2) && defined(HAVE_FLOAT__COMPLEX) \
     && defined(HAVE_DOUBLE__COMPLEX) \
-    && defined(HAVE_LONG_DOUBLE__COMPLEX) 
-#define test_types_set4(op, post)                                         \
-    do {                                                                  \
-        op##_test##post(float _Complex, MPI_C_FLOAT_COMPLEX);             \
-        op##_test##post(double _Complex, MPI_C_DOUBLE_COMPLEX);           \
-        op##_test##post(long double _Complex, MPI_C_LONG_DOUBLE_COMPLEX); \
+    && defined(HAVE_LONG_DOUBLE__COMPLEX)
+#define test_types_set4(op, post)                                             \
+    do {                                                                      \
+        op##_test##post(float _Complex, MPI_C_FLOAT_COMPLEX);                 \
+        op##_test##post(double _Complex, MPI_C_DOUBLE_COMPLEX);               \
+        if (MPI_C_LONG_DOUBLE_COMPLEX != MPI_DATATYPE_NULL) {                 \
+            op##_test##post(long double _Complex, MPI_C_LONG_DOUBLE_COMPLEX); \
+        }                                                                     \
     } while (0)
 
 #else
---
+++ test/mpi/datatype/longdouble.c
@@ -0,0 +1,65 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2012 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "mpi.h"
+#include "mpitest.h"
+
+/* Some MPI implementations should not support MPI_LONG_DOUBLE because it has
+ * different representations/sizes among several concurrently supported
+ * compilers.  For example, a 16-byte GCC implementation and an 8-byte Cray
+ * compiler implementation.
+ *
+ * This test ensures that simplistic build logic/configuration did not result in
+ * a defined, yet incorrectly sized, MPI predefined datatype for long double and
+ * long double _Complex.  See tt#1671 for more info.
+ *
+ * Based on a test suggested by Jim Hoekstra @ Iowa State University. */
+
+int main(int argc, char *argv[])
+{
+    int rank, size, i, type_size;
+    int errs = 0;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0) {
+#ifdef HAVE_LONG_DOUBLE
+        if (MPI_LONG_DOUBLE != MPI_DATATYPE_NULL) {
+            MPI_Type_size(MPI_LONG_DOUBLE, &type_size);
+            if (type_size != sizeof(long double)) {
+                printf("type_size != sizeof(long double) : (%zd != %zd)\n",
+                       type_size, sizeof(long double));
+                ++errs;
+            }
+        }
+#endif
+#if defined(HAVE_LONG_DOUBLE__COMPLEX) && defined(USE_LONG_DOUBLE_COMPLEX)
+        if (MPI_C_LONG_DOUBLE_COMPLEX != MPI_DATATYPE_NULL) {
+            MPI_Type_size(MPI_C_LONG_DOUBLE_COMPLEX, &type_size);
+            if (type_size != sizeof(long double _Complex)) {
+                printf("type_size != sizeof(long double _Complex) : (%zd != %zd)\n",
+                       type_size, sizeof(long double _Complex));
+                ++errs;
+            }
+        }
+#endif
+        if (errs) {
+            printf("found %d errors\n", errs);
+        }
+        else {
+            printf(" No errors\n");
+        }
+    }
+
+    MPI_Finalize();
+    return 0;
+}
+
