ce15ef645b936edb268c44a9c570574432d704f2
Pavan Balaji <balaji@mcs.anl.gov>
2012-09-03 22:30:40 +0000

    [svn-r10184] Pull OPA r146 to pick up some of the recent fixes.

--- src/openpa/autogen.sh
+++ src/openpa/autogen.sh
@@ -6,12 +6,4 @@ else
     autoreconf=${AUTORECONF:-autoreconf}
 fi
 
-$autoreconf ${autoreconf_args:-"-vif"}
-
-# fix depcomp to support pgcc correctly
-if grep "pgcc)" confdb/depcomp 2>&1 >/dev/null ; then :
-else
-    echo 'patching "confdb/depcomp" to support pgcc'
-    patch -f -p0 < confdb/depcomp_pgcc.patch
-fi
-
+$autoreconf ${autoreconf_args:-"-vif"} || exit 1
