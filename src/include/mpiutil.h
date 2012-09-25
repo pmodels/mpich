0b2cca6dbd2c75c4947026e11fd8b10b1bac0c18
Pavan Balaji <balaji@mcs.anl.gov>
2012-09-25 20:30:47 +0000

    [svn-r10269] IBM-contribution: Support likely/unlikely for xlc as well.
    Reviewed by buntinas.

--- src/include/mpiutil.h
+++ src/include/mpiutil.h
@@ -214,7 +214,7 @@ int MPIR_Assert_fail_fmt(const char *cond, const char *file_name, int line_num, 
  * These macros are not namespaced because the namespacing is cumbersome.
  */
 /* safety guard for now, add a configure check in the future */
-#if defined(__GNUC__) && (__GNUC__ >= 3)
+#if ( defined(__GNUC__) && (__GNUC__ >= 3) ) || defined(__xlc__)
 #  define unlikely(x_) __builtin_expect(!!(x_),0)
 #  define likely(x_)   __builtin_expect(!!(x_),1)
 #else
