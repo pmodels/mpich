f8ccd5365d698b0d3307ea264b5c7f36d7d2e2bc
Pavan Balaji <balaji@mcs.anl.gov>
2012-10-04 20:23:29 +0000

    [svn-r10332] IBM-contributed patch: Added support for non-versioned libraries
    Added a configure flag --enable-versioning, which is on by default.
    This causes libtool to version shared libraries, using libtool
    conventions.  Some platforms may use other versioning schemes, for
    example PE MPI uses directory structure layout to version libraries.
    In addition, PE must generate consistent, non-versioned "libmpi.so"
    output from ldd on the final link of the application to allow for 2
    way binary compatibility with other MPI implementations.
    
    Reviewed by goodell.

--- src/binding/cxx/Makefile.mk
+++ src/binding/cxx/Makefile.mk
@@ -15,7 +15,7 @@ lib_LTLIBRARIES += lib/lib@MPICXXLIBNAME@.la
 
 lib_lib@MPICXXLIBNAME@_la_SOURCES = \
     src/binding/cxx/initcxx.cxx
-lib_lib@MPICXXLIBNAME@_la_LDFLAGS = -version-info $(ABIVERSION)
+lib_lib@MPICXXLIBNAME@_la_LDFLAGS = $(ABIVERSIONFLAGS)
 
 # Update output files if the buildiface script or mpi.h.in is updated.  Use the
 # buildiface-stamp to deal with the &ReplaceIfDifferent logic
