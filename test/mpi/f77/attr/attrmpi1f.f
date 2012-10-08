2ad4c0550c60533cd9174f54508ff06220431891
William Gropp <gropp@mcs.anl.gov>
2012-10-08 16:19:01 +0000

    [svn-r10342] Added test of the MPI-1 version of the Fortran attributes.  This is needed to ensure that these work correctly without needing to run the MPICH-1 or Intel tests.  Added because the current MPICH2 build appears to fail to handle the MPI-1 Fortran attribute put/get correctly

---
+++ test/mpi/f77/attr/attrmpi1f.f
@@ -0,0 +1,58 @@
+C -*- Mode: Fortran; -*- 
+C
+C  (C) 2003 by Argonne National Laboratory.
+C      See COPYRIGHT in top-level directory.
+C
+      program main
+      implicit none
+      include 'mpif.h'
+      integer value, wsize, wrank, extra, mykey
+      integer rvalue, ncomm
+      logical flag
+      integer ierr, errs
+C
+      errs = 0
+      call mtest_init( ierr )
+      call mpi_comm_size( MPI_COMM_WORLD, wsize, ierr )
+      call mpi_comm_rank( MPI_COMM_WORLD, wrank, ierr )
+C
+C     Simple attribute put and get
+C
+      call mpi_keyval_create( MPI_NULL_COPY_FN, MPI_NULL_DELETE_FN,
+     $     mykey, extra,ierr ) 
+      call mpi_attr_get( MPI_COMM_WORLD, mykey, value, flag, ierr )
+      if (flag) then
+         errs = errs + 1
+         print *,
+     $       "Did not get flag==.false. for attribute that was not set"
+      endif
+C
+      value = 1234567
+      call mpi_attr_put( MPI_COMM_WORLD, mykey, value, ierr )
+      call mpi_attr_get( MPI_COMM_WORLD, mykey, rvalue, flag, ierr )
+      if (.not. flag) then
+         errs = errs + 1
+         print *, "Did not find attribute after set"
+      else
+         if (rvalue .ne. value) then
+            errs = errs + 1
+            print *, "Attribute value ", rvalue, " should be ", value
+         endif
+      endif
+      value = -123456
+      call mpi_attr_put( MPI_COMM_WORLD, mykey, value, ierr )
+      call mpi_attr_get( MPI_COMM_WORLD, mykey, rvalue, flag, ierr )
+      if (.not. flag) then
+         errs = errs + 1
+         print *, "Did not find attribute after set (neg)"
+      else
+         if (rvalue .ne. value) then
+            errs = errs + 1
+            print *, "Neg Attribute value ", rvalue, " should be ",value
+         endif
+      endif
+C      
+      call mpi_keyval_free( mykey, ierr )
+      call mtest_finalize( errs )
+      call mpi_finalize( ierr )
+      end
