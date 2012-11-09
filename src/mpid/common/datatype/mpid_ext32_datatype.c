0b798b7ae5e0db21f1ac0d0b19f39ff7a4e10b6e
Pavan Balaji <balaji@mcs.anl.gov>
2012-11-09 14:50:07 +0000

    [svn-r10595] Updated external32 sizes for the new datatypes added in MPI-3 (pg. 536 of the MPI-3 standard).  Fixes ticket #1711.
    Reviewed by robl.

--- src/mpid/common/datatype/mpid_ext32_datatype.c
+++ src/mpid/common/datatype/mpid_ext32_datatype.c
@@ -25,16 +25,22 @@ static external32_basic_size_t external32_basic_size_array[] =
     { MPI_BYTE, 1 },
     { MPI_CHAR, 1 },
     { MPI_UNSIGNED_CHAR, 1 },
+    { MPI_SIGNED_CHAR, 1 },
+    { MPI_WCHAR, 2 },
     { MPI_SHORT, 2 },
     { MPI_UNSIGNED_SHORT, 2 },
     { MPI_INT, 4 },
     { MPI_UNSIGNED, 4 },
     { MPI_LONG, 4 },
     { MPI_UNSIGNED_LONG, 4 },
+    { MPI_LONG_LONG, 8 },
+    { MPI_LONG_LONG_INT, 8 },
+    { MPI_UNSIGNED_LONG_LONG, 8 },
     { MPI_FLOAT, 4 },
     { MPI_DOUBLE, 8 },
     { MPI_LONG_DOUBLE, 16 },
-    { MPI_CHARACTER, 1 },
+
+    { MPI_C_BOOL, 1 },
     { MPI_INT8_T, 1 },
     { MPI_INT16_T, 2 },
     { MPI_INT32_T, 4 },
@@ -43,33 +49,40 @@ static external32_basic_size_t external32_basic_size_array[] =
     { MPI_UINT16_T, 2 },
     { MPI_UINT32_T, 4 },
     { MPI_UINT64_T, 8 },
-
-    /* official MPI-2.2 doc says "4" for MPI_C_BOOL, but the MPI Forum
-     * immediately approved errata changing this to "1" */
-    { MPI_C_BOOL, 1 },
-
+    { MPI_AINT, 8 },
+    { MPI_COUNT, 8 },
+    { MPI_OFFSET, 8 },
+    { MPI_C_COMPLEX, 2*4 },
     { MPI_C_FLOAT_COMPLEX, 2*4 },
     { MPI_C_DOUBLE_COMPLEX, 2*8 },
     { MPI_C_LONG_DOUBLE_COMPLEX, 2*16 },
+
+    { MPI_CHARACTER, 1 },
     { MPI_LOGICAL, 4 },
     { MPI_INTEGER, 4 },
     { MPI_REAL, 4 },
     { MPI_DOUBLE_PRECISION, 8 },
     { MPI_COMPLEX, 8 },
     { MPI_DOUBLE_COMPLEX, 16 },
-#ifdef HAVE_FORTRAN_BINDING
-    { MPI_SIGNED_CHAR, 1 },
-    { MPI_WCHAR, 2 },
+
     { MPI_INTEGER1, 1 },
     { MPI_INTEGER2, 2 },
     { MPI_INTEGER4, 4 },
     { MPI_INTEGER8, 8 },
-    { MPI_UNSIGNED_LONG_LONG, 8 },
+    { MPI_INTEGER16, 16 },
+
     { MPI_REAL4, 4 },
     { MPI_REAL8, 8 },
     { MPI_REAL16, 16 },
-#endif
-    { MPI_LONG_LONG, 8 }
+
+    { MPI_COMPLEX8, 2*4 },
+    { MPI_COMPLEX16, 2*8 },
+    { MPI_COMPLEX32, 2*16 },
+
+    { MPI_CXX_BOOL, 1 },
+    { MPI_CXX_FLOAT_COMPLEX, 2*4 },
+    { MPI_CXX_DOUBLE_COMPLEX, 2*8 },
+    { MPI_CXX_LONG_DOUBLE_COMPLEX, 2*16 }
 };
 
 MPI_Aint MPIDI_Datatype_get_basic_size_external32(MPI_Datatype el_type)
