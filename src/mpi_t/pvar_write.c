2cfb60178370094f4df8ba43e535f987ff405bb3
Ralf Gunter <ralfgunter@gmail.com>
2012-11-29 14:25:05 -0600

    MPI_T_pvar_read/write now respect the handle creator's count variable

--- src/mpi_t/pvar_write.c
+++ src/mpi_t/pvar_write.c
@@ -37,7 +37,7 @@ int MPIR_T_pvar_write_impl(MPI_T_pvar_session session, MPI_T_pvar_handle handle,
     /* the extra indirection through "info" might be too costly for some tools,
      * consider moving this value to or caching it in the handle itself */
     if (likely(handle->info->impl_kind == MPIR_T_PVAR_IMPL_SIMPLE)) {
-        MPIU_Memcpy(handle->handle_state, buf, handle->bytes);
+        MPIU_Memcpy(handle->handle_state, buf, handle->count * handle->bytes);
     }
     else {
         MPIU_Assertp(FALSE); /* _IMPL_CB not yet implemented */
