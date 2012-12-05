3a53d084f7ec585eca41e380cac6f4b601a88eae
Dave Goodell <goodell@mcs.anl.gov>
2012-12-05 16:06:56 -0600

    delete src/openpa before re-adding via git-subtree

--- src/openpa/autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -n "$MPICH2_AUTOTOOLS_DIR" ] ; then
-    autoreconf=${MPICH2_AUTOTOOLS_DIR}/autoreconf
-else
-    autoreconf=${AUTORECONF:-autoreconf}
-fi
-
-$autoreconf ${autoreconf_args:-"-vif"} || exit 1
