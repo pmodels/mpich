fe07232f0142cbbd03a6f5fc7d4c02cdf17022be
James Dinan <dinan@mcs.anl.gov>
2013-01-13 19:49:16 -0600

    Fixed error checks in shr. mem. win. creation
    Replaced SETANDJUMP with CHKANDJUMP, so we actually check for a pthread error
    before signalling an MPI error.
    
    Reviewer: none

--- src/mpid/ch3/channels/nemesis/include/mpidi_ch3_impl.h
+++ src/mpid/ch3/channels/nemesis/include/mpidi_ch3_impl.h
@@ -85,48 +85,48 @@ int MPIDI_CH3_SHM_Win_free(MPID_Win **win_ptr);
 
 /* Shared memory window atomic/accumulate mutex implementation */
 
-#define MPIDI_CH3I_SHM_MUTEX_LOCK(win_ptr)                                      \
-    do {                                                                        \
-        int pt_err = pthread_mutex_lock((win_ptr)->shm_mutex);                  \
-        if (pt_err) {                                                           \
-            MPIU_ERR_SETANDJUMP1(mpi_errno, MPI_ERR_OTHER, "**pthread_lock",    \
-                                 "**pthread_lock %s", strerror(pt_err));        \
-        }                                                                       \
+#define MPIDI_CH3I_SHM_MUTEX_LOCK(win_ptr)                                              \
+    do {                                                                                \
+        int pt_err = pthread_mutex_lock((win_ptr)->shm_mutex);                          \
+        if (pt_err) {                                                                   \
+            MPIU_ERR_CHKANDJUMP1(pt_err, mpi_errno, MPI_ERR_OTHER, "**pthread_lock",    \
+                                 "**pthread_lock %s", strerror(pt_err));                \
+        }                                                                               \
     } while (0)
 
-#define MPIDI_CH3I_SHM_MUTEX_UNLOCK(win_ptr)                                    \
-    do {                                                                        \
-        int pt_err = pthread_mutex_unlock((win_ptr)->shm_mutex);                \
-        if (pt_err) {                                                           \
-            MPIU_ERR_SETANDJUMP1(mpi_errno, MPI_ERR_OTHER, "**pthread_unlock",  \
-                                 "**pthread_unlock %s", strerror(pt_err));      \
-        }                                                                       \
+#define MPIDI_CH3I_SHM_MUTEX_UNLOCK(win_ptr)                                            \
+    do {                                                                                \
+        int pt_err = pthread_mutex_unlock((win_ptr)->shm_mutex);                        \
+        if (pt_err) {                                                                   \
+            MPIU_ERR_CHKANDJUMP1(pt_err, mpi_errno, MPI_ERR_OTHER, "**pthread_unlock",  \
+                                 "**pthread_unlock %s", strerror(pt_err));              \
+        }                                                                               \
     } while (0)
 
-#define MPIDI_CH3I_SHM_MUTEX_INIT(win_ptr)                                      \
-    do {                                                                        \
-        int pt_err;                                                             \
-        pthread_mutexattr_t attr;                                               \
-                                                                                \
-        pt_err = pthread_mutexattr_init(&attr);                                 \
-        MPIU_ERR_SETANDJUMP1(mpi_errno, MPI_ERR_OTHER, "**pthread_mutex",       \
-                             "**pthread_mutex %s", strerror(pt_err));           \
-        pt_err = pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED);   \
-        MPIU_ERR_SETANDJUMP1(mpi_errno, MPI_ERR_OTHER, "**pthread_mutex",       \
-                             "**pthread_mutex %s", strerror(pt_err));           \
-        pt_err = pthread_mutex_init((win_ptr)->shm_mutex, &attr);               \
-        MPIU_ERR_SETANDJUMP1(mpi_errno, MPI_ERR_OTHER, "**pthread_mutex",       \
-                             "**pthread_mutex %s", strerror(pt_err));           \
-        pt_err = pthread_mutexattr_destroy(&attr);                              \
-        MPIU_ERR_SETANDJUMP1(mpi_errno, MPI_ERR_OTHER, "**pthread_mutex",       \
-                             "**pthread_mutex %s", strerror(pt_err));           \
+#define MPIDI_CH3I_SHM_MUTEX_INIT(win_ptr)                                              \
+    do {                                                                                \
+        int pt_err;                                                                     \
+        pthread_mutexattr_t attr;                                                       \
+                                                                                        \
+        pt_err = pthread_mutexattr_init(&attr);                                         \
+        MPIU_ERR_CHKANDJUMP1(pt_err, mpi_errno, MPI_ERR_OTHER, "**pthread_mutex",       \
+                             "**pthread_mutex %s", strerror(pt_err));                   \
+        pt_err = pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED);           \
+        MPIU_ERR_CHKANDJUMP1(pt_err, mpi_errno, MPI_ERR_OTHER, "**pthread_mutex",       \
+                             "**pthread_mutex %s", strerror(pt_err));                   \
+        pt_err = pthread_mutex_init((win_ptr)->shm_mutex, &attr);                       \
+        MPIU_ERR_CHKANDJUMP1(pt_err, mpi_errno, MPI_ERR_OTHER, "**pthread_mutex",       \
+                             "**pthread_mutex %s", strerror(pt_err));                   \
+        pt_err = pthread_mutexattr_destroy(&attr);                                      \
+        MPIU_ERR_CHKANDJUMP1(pt_err, mpi_errno, MPI_ERR_OTHER, "**pthread_mutex",       \
+                             "**pthread_mutex %s", strerror(pt_err));                   \
     } while (0);
 
-#define MPIDI_CH3I_SHM_MUTEX_DESTROY(win_ptr)                                   \
-    do {                                                                        \
-        int pt_err = pthread_mutex_destroy((win_ptr)->shm_mutex);               \
-        MPIU_ERR_SETANDJUMP1(mpi_errno, MPI_ERR_OTHER, "**pthread_mutex",       \
-                             "**pthread_mutex %s", strerror(pt_err));           \
+#define MPIDI_CH3I_SHM_MUTEX_DESTROY(win_ptr)                                           \
+    do {                                                                                \
+        int pt_err = pthread_mutex_destroy((win_ptr)->shm_mutex);                       \
+        MPIU_ERR_CHKANDJUMP1(pt_err, mpi_errno, MPI_ERR_OTHER, "**pthread_mutex",       \
+                             "**pthread_mutex %s", strerror(pt_err));                   \
     } while (0);
 
 #endif /* !defined(MPICH_MPIDI_CH3_IMPL_H_INCLUDED) */
