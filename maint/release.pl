pmodels/mpich

commit dd76a0f2f90bf4d79ac75a5d4419e498f9770c30

Fix typo -- the performance tests were not correctly disabled in released versions.
Reviewed by dinan.

maint/release.pl
@@ -254,7 +254,7 @@ print("done\n");
 # Disable unnecessary tests in the release tarball
 print("===> Disabling unnecessary tests in the main package... ");
 chdir($expdir);
-run_cmd("perl -p -i -e 's/^perf\$/#perf/' test/mpi/testlist.in");
+run_cmd("perl -p -i -e 's/^@perfdir@\$/#@perfdir@/' test/mpi/testlist.in");
 run_cmd("perl -p -i -e 's/^large_message /#large_message /' test/mpi/pt2pt/testlist");
 run_cmd("perl -p -i -e 's/^large-count /#large-count /' test/mpi/datatype/testlist");
 print("done\n");

commit 38fb9ec634e018b373513d0014029c3ead06ed4e

Fix typo introduced in dd76a0f that's causing the nightly tests to fail.
No reviewer.

maint/release.pl
@@ -254,7 +254,7 @@ print("done\n");
 # Disable unnecessary tests in the release tarball
 print("===> Disabling unnecessary tests in the main package... ");
 chdir($expdir);
-run_cmd("perl -p -i -e 's/^@perfdir@\$/#@perfdir@/' test/mpi/testlist.in");
+run_cmd("perl -p -i -e 's/^\@perfdir\@\$/#\@perfdir\@/' test/mpi/testlist.in");
 run_cmd("perl -p -i -e 's/^large_message /#large_message /' test/mpi/pt2pt/testlist");
 run_cmd("perl -p -i -e 's/^large-count /#large-count /' test/mpi/datatype/testlist");
 print("done\n");