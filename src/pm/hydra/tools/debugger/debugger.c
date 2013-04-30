pmodels/mpich

commit e04dd4b64ff618f2df58789265b741a8e9fab081

Bug-fix: correct MPIR_proctable setup when we use wraparound ranks.
As reported by John DelSignore @ Roguewave, when we wraparound ranks
among nodes, we were not allocating the MPIR_proctable structure in
rank order.  We were always grouping them per node.

Reviewed by dinan.

src/pm/hydra/tools/debugger/debugger.c
@@ -28,7 +28,7 @@ HYD_status HYDT_dbg_setup_procdesc(struct HYD_pg * pg)
 {
     struct HYD_proxy *proxy;
     struct HYD_exec *exec;
-    int i, j, np;
+    int i, j, k, np, round;
     HYD_status status = HYD_SUCCESS;
 
     HYDU_FUNC_ENTER();
@@ -36,10 +36,27 @@ HYD_status HYDT_dbg_setup_procdesc(struct HYD_pg * pg)
     HYDU_MALLOC(MPIR_proctable, struct MPIR_PROCDESC *,
                 pg->pg_process_count * sizeof(struct MPIR_PROCDESC), status);
 
-    for (proxy = pg->proxy_list, i = 0; proxy; proxy = proxy->next) {
+    round = 0;
+    /* We need to allocate the MPIR_proctable in COMM_WORLD rank
+     * order.  We do this in multiple rounds.  In each round, we
+     * allocate the proctable entries for the executables on the proxy
+     * that form a contiguous rank list.  Then we move to the next
+     * proxy.  When we run out of proxies, we go back to the first
+     * proxy and find the next set of contiguous ranks on that
+     * proxy. */
+    for (proxy = pg->proxy_list, i = 0;; proxy = proxy->next) {
         j = 0;
+        k = 0;
+        if (!proxy) {
+            proxy = pg->proxy_list;
+            round++;
+        }
         for (exec = proxy->exec_list; exec; exec = exec->next) {
             for (np = 0; np < exec->proc_count; np++) {
+                if (k + np >= ((round + 1) * proxy->node->core_count))
+                    break;
+                if (k + np < round * proxy->node->core_count)
+                    continue;
                 MPIR_proctable[i].host_name = HYDU_strdup(proxy->node->hostname);
                 MPIR_proctable[i].pid = proxy->pid[j++];
                 if (exec->exec[0])
@@ -48,7 +65,11 @@ HYD_status HYDT_dbg_setup_procdesc(struct HYD_pg * pg)
                     MPIR_proctable[i].executable_name = NULL;
                 i++;
             }
+            k += exec->proc_count;
         }
+
+        if (i >= pg->pg_process_count)
+            break;
     }
 
     MPIR_proctable_size = pg->pg_process_count;

commit a9640dc49d25f95ffbccd04476e42c5c23d0c96e

Fix PID calculation error introduced in [e04dd4b6].
No reviewer.

src/pm/hydra/tools/debugger/debugger.c
@@ -58,7 +58,8 @@ HYD_status HYDT_dbg_setup_procdesc(struct HYD_pg * pg)
                 if (k + np < round * proxy->node->core_count)
                     continue;
                 MPIR_proctable[i].host_name = HYDU_strdup(proxy->node->hostname);
-                MPIR_proctable[i].pid = proxy->pid[j++];
+                MPIR_proctable[i].pid = proxy->pid[(proxy->node->core_count * round) + j];
+                j++;
                 if (exec->exec[0])
                     MPIR_proctable[i].executable_name = HYDU_strdup(exec->exec[0]);
                 else