43bd72a48283612bd86cf8603ce99cb2104a64c8
Pavan Balaji <balaji@mcs.anl.gov>
2013-05-05 13:47:02 -0500

    Die on autoreconf error instead of chugging along.
    No reviewer.

--- src/pm/hydra/autogen.sh
+++ src/pm/hydra/autogen.sh
@@ -8,9 +8,12 @@ fi
 
 echo "=== running autoreconf in 'mpl' ==="
 (cd mpl && $autoreconf ${autoreconf_args:-"-vif"}) || exit 1
+
 echo "=== running autoreconf in 'tools/topo/hwloc/hwloc' ==="
 (cd tools/topo/hwloc/hwloc && $autoreconf ${autoreconf_args:-"-vif"}) || exit 1
-$autoreconf ${autoreconf_args:-"-vif"}
+
+echo "=== running autoreconf in '.' ==="
+$autoreconf ${autoreconf_args:-"-vif"} || exit 1
 
 # Remove the autom4te.cache folders for a release-like structure.
 find . -name autom4te.cache | xargs rm -rf
