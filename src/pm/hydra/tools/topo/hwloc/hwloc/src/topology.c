b61fc70291f34acf1995ca3988207af5038f927b
Pavan Balaji <balaji@mcs.anl.gov>
2013-05-19 00:17:55 -0500

    Hydra-specific patches for hwloc.
    Picked up the relevant parts of [a460c1ee], [dab3166d], and [1eb07c6e].
    
    Signed-off-by: Antonio J. Pena <apenya@mcs.anl.gov>

--- src/pm/hydra/tools/topo/hwloc/hwloc/src/topology.c
+++ src/pm/hydra/tools/topo/hwloc/hwloc/src/topology.c
@@ -1055,17 +1055,19 @@ hwloc_insert_object_by_parent(struct hwloc_topology *topology, hwloc_obj_t paren
     if (obj->cpuset && (!curcpuset || hwloc__object_cpusets_compare_first(obj, *current) < 0)) {
       static int reported = 0;
       if (!reported && !hwloc_hide_errors()) {
-	char *a = "NULL", *b;
-	if (curcpuset)
-	  hwloc_bitmap_asprintf(&a, curcpuset);
+	char *a, *b;
 	hwloc_bitmap_asprintf(&b, obj->cpuset);
         fprintf(stderr, "****************************************************************************\n");
         fprintf(stderr, "* hwloc has encountered an out-of-order topology discovery.\n");
-        fprintf(stderr, "* An object with (complete) cpuset %s was inserted after object with %s\n", b, a);
+	if (curcpuset) {
+	  hwloc_bitmap_asprintf(&a, curcpuset);
+	  fprintf(stderr, "* An object with (complete) cpuset %s was inserted after object with %s\n", b, a);
+	  free(a);
+	} else {
+	  fprintf(stderr, "* An object with (complete) cpuset %s was inserted after object with NULL\n", b);
+	}
         fprintf(stderr, "* Please check that your input topology (XML file, etc.) is valid.\n");
         fprintf(stderr, "****************************************************************************\n");
-	if (curcpuset)
-	  free(a);
 	free(b);
 	reported = 1;
       }
