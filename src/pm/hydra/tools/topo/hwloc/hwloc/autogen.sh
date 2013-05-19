a460c1eeca56cc4c7f61fddcdb2724bd605fdea2
Pavan Balaji <balaji@mcs.anl.gov>
2013-05-19 00:17:55 -0500

    Hydra-specific patches for hwloc.
    Picked up the relevant parts of [2bda6bc7], [49b74176] and [59cbc846].
    
    No reviewer.

--- src/pm/hydra/tools/topo/hwloc/hwloc/autogen.sh
+++ src/pm/hydra/tools/topo/hwloc/hwloc/autogen.sh
@@ -1,2 +1,9 @@
-:
-autoreconf -ivf
+#!/bin/sh
+
+if [ -n "$MPICH_AUTOTOOLS_DIR" ] ; then
+    autoreconf=${MPICH_AUTOTOOLS_DIR}/autoreconf
+else
+    autoreconf=${AUTORECONF:-autoreconf}
+fi
+
+$autoreconf ${autoreconf_args:-"-ivf"}
