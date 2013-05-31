32dc8c1b689e98ed6a1289b11c81245312fdf761
Rajeev Thakur <thakur@mcs.anl.gov>
2013-05-31 17:09:06 -0500

    remove check for committed type in Type_size_x. Fixes ticket #1774

--- src/mpi/datatype/type_size_x.c
+++ src/mpi/datatype/type_size_x.c
@@ -95,7 +95,6 @@ int MPI_Type_size_x(MPI_Datatype datatype, MPI_Count *size)
                 MPID_Datatype *datatype_ptr = NULL;
                 MPID_Datatype_get_ptr(datatype, datatype_ptr);
                 MPID_Datatype_valid_ptr(datatype_ptr, mpi_errno);
-                MPID_Datatype_committed_ptr(datatype_ptr, mpi_errno);
             }
 
             /* TODO more checks may be appropriate (counts, in_place, buffer aliasing, etc) */
