8448badc4099f6b0ded16e02aeb0ecd60178ffa7
Pavan Balaji <balaji@mcs.anl.gov>
2013-07-08 18:46:16 -0500

    Remove unused mpicc.conf.in and friends.

--- src/env/Makefile.mk
+++ src/env/Makefile.mk
@@ -19,29 +19,20 @@ endif BUILD_PROFILING_LIB
 
 src_env_mpichversion_LDFLAGS = $(mpich_libtool_static_flag)
 
-sysconf_DATA += src/env/mpicc.conf
-
 if BUILD_F77_BINDING
 bin_SCRIPTS += src/env/mpif77
-sysconf_DATA += src/env/mpif77.conf
 endif BUILD_F77_BINDING
 
 if BUILD_F90_LIB
 bin_SCRIPTS += src/env/mpif90
-sysconf_DATA += src/env/mpif90.conf
 endif BUILD_F90_LIB
 
 if BUILD_CXX_LIB
 bin_SCRIPTS += src/env/mpicxx
-sysconf_DATA += src/env/mpicxx.conf
 endif BUILD_CXX_LIB
 
 # create a local copy of the compiler wrapper that will actually be installed
 if BUILD_BASH_SCRIPTS
-src/env/mpicc.bash: $(top_builddir)/src/env/mpicc.conf
-src/env/mpicxx.bash: $(top_builddir)/src/env/mpicxx.conf
-src/env/mpif77.bash: $(top_builddir)/src/env/mpif77.conf
-src/env/mpif90.bash: $(top_builddir)/src/env/mpif90.conf
 src/env/mpicc: $(top_builddir)/src/env/mpicc.bash
 	cp -p $? $@
 src/env/mpicxx: $(top_builddir)/src/env/mpicxx.bash
@@ -51,10 +42,6 @@ src/env/mpif77: $(top_builddir)/src/env/mpif77.bash
 src/env/mpif90: $(top_builddir)/src/env/mpif90.bash
 	cp -p $? $@
 else !BUILD_BASH_SCRIPTS
-src/env/mpicc.sh: $(top_builddir)/src/env/mpicc.conf
-src/env/mpicxx.sh: $(top_builddir)/src/env/mpicxx.conf
-src/env/mpif77.sh: $(top_builddir)/src/env/mpif77.conf
-src/env/mpif90.sh: $(top_builddir)/src/env/mpif90.conf
 src/env/mpicc: $(top_builddir)/src/env/mpicc.sh
 	cp -p $? $@
 src/env/mpicxx: $(top_builddir)/src/env/mpicxx.sh
@@ -81,4 +68,3 @@ wrapper_doc_src = src/env/mpicc.txt \
                   src/env/mpiexec.txt
 doc1_src_txt += $(wrapper_doc_src)
 EXTRA_DIST += $(wrapper_doc_src)
-
