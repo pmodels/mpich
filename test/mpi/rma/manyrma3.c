602605b67eab52c0d4eaf6b2a3b2580a7f1da0d6
Pavan Balaji <balaji@mcs.anl.gov>
2013-08-01 14:01:15 -0500

    Created a new test for a large number of outstanding RMA operations.
    This seems to fail with MPICH for >= 4096 back-to-back RMA operations.
    
    Signed-off-by: Xin Zhao <xinzhao3@illinois.edu>

---
+++ test/mpi/rma/manyrma3.c
@@ -0,0 +1,35 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2010 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
+#include "mpi.h"
+#include <stdio.h>
+
+#define MAX_COUNT 4096
+
+int main(int argc, char *argv[])
+{
+    int i, winbuf, one = 1, rank;
+    MPI_Win win;
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    MPI_Win_create(&winbuf, sizeof(int), sizeof(int), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+
+    MPI_Win_fence(0, win);
+    for (i = 0; i < MAX_COUNT; i++)
+        MPI_Accumulate(&one, 1, MPI_INT, 0, 0, 1, MPI_INT, MPI_SUM, win);
+    MPI_Win_fence(0, win);
+
+    MPI_Win_free(&win);
+
+    if (rank == 0)
+        printf(" No Errors\n");
+
+    MPI_Finalize();
+
+    return 0;
+}
