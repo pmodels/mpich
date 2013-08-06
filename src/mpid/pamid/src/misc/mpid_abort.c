254aa2cdaba145bf9c7f6a42665cae32d1b31685
Michael Blocksome <blocksom@us.ibm.com>
2013-08-06 11:32:15 -0500

    Clean up pamid MPID_Abort() logic
    Removed the processing of the (undocumented) environment variable
    'PAMID_CORE_ON_ABORT' which was being checked to determine if the user
    does *not* want the process to core dump. On Blue Gene/Q the core dump
    was accomplished by calling 'abort()' which sends SIGSBRT to all
    processes and all processes would then write a core file. This is not
    scalable.
    
    Instead, MPID_Abort() will invoke 'exit(1)' which will terminate all
    processes in the job. This behavior is identical for both the POE and
    the Blue Gene/Q control systems.
    
    On Blue Gene/Q the user may replicate the previous core dump behavior by
    using the environment variables 'BG_COREDUMPONERROR=1' or
    'BG_COREDUMPONEXIT=1'.
    
    Finally, the 'DYNAMIC_TASKING' #ifdef is moved up so it is checked first.
    'MPIDI_NO_ASSERT' and 'DYNAMIC_TASKING' are typically defined for PE. It
    appears that the dynamic tasking code was never being invoked.
    
    (ibm) CPS 99YURA
    
    Signed-off-by: Bob Cernohous <bobc@us.ibm.com>

--- src/mpid/pamid/src/misc/mpid_abort.c
+++ src/mpid/pamid/src/misc/mpid_abort.c
@@ -27,8 +27,8 @@
  *
  * \param[in] comm      The communicator associated with the failure (can be null).
  * \param[in] mpi_errno The MPI error associated with the failure (can be zero).
- * \param[in] exit_code The requested exit code, however BG features imply that exit(1) will always be used.
- * \param[in] error_msg The message to display (may be NULL_
+ * \param[in] exit_code The requested exit code.
+ * \param[in] error_msg The message to display (may be NULL)
  *
  * This is the majority of the call to MPID_Abort().  The only
  * difference is that it does not call exit.  That allows it to be
@@ -74,26 +74,28 @@ void MPIDI_Abort_core(MPID_Comm * comm, int mpi_errno, int exit_code, const char
  * \brief The central parts of the MPID_Abort call
  * \param[in] comm      The communicator associated with the failure (can be null).
  * \param[in] mpi_errno The MPI error associated with the failure (can be zero).
- * \param[in] exit_code The requested exit code, however BG features imply that exit(1) will always be used.
- * \param[in] error_msg The message to display (may be NULL_
- * \returns MPI_ERR_INTERN
+ * \param[in] exit_code The requested exit code.
+ * \param[in] error_msg The message to display (may be NULL)
+ * \return MPI_ERR_INTERN
  *
  * This function MUST NEVER return.
  */
 int MPID_Abort(MPID_Comm * comm, int mpi_errno, int exit_code, const char *error_msg)
 {
-  char* env     = getenv("PAMID_CORE_ON_ABORT");
   MPIDI_Abort_core(comm, mpi_errno, exit_code, error_msg);
-
-#ifdef MPIDI_NO_ASSERT
-  exit(1);
-#endif
-  if (env != NULL)
-    if ( (strncasecmp("no", env, 2)==0) || (strncasecmp("exit", env, 4)==0) || (strncmp("0", env, 1)==0) )
-      exit(1);
 
 #ifdef DYNAMIC_TASKING
   return PMI2_Abort(1,error_msg);
+#else
+  /* The POE and BGQ control systems both catch the exit value for additional
+   * processing. If a process exits with '1' then all processes in the job
+   * are terminated. The requested error code is lost in this process however
+   * this is acceptable, but not desirable, behavior according to the MPI
+   * standard.
+   *
+   * On BGQ, the user may force the process (rank) that exited with '1' to core
+   * dump by setting the environment variable 'BG_COREDUMPONERROR=1'.
+   */
+  exit(1);
 #endif
-  abort();
 }
