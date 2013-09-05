98431efc18fa6cefdeb6d5a8d6815abce8d88216
Huiwei Lu <huiweilu@mcs.anl.gov>
2013-09-05 18:19:43 -0500

    Adding a test case for MPI_Comm_idup
    To cover the case when multiple communicators are created by
    MPI_Comm_idup.
    
    Signed-off-by: Pavan Balaji <balaji@mcs.anl.gov>

---
+++ test/mpi/comm/comm_idup_mul.c
@@ -0,0 +1,39 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2012 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
+/*
+ * Test creating multiple communicators with MPI_Comm_idup.
+ */
+
+#include <stdio.h>
+#include <mpi.h>
+
+#define NUM_ITER    2
+
+int main(int argc, char **argv)
+{
+    int i, rank;
+    MPI_Comm comms[NUM_ITER];
+    MPI_Request req[NUM_ITER];
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    for (i = 0; i < NUM_ITER; i++)
+        MPI_Comm_idup(MPI_COMM_WORLD, &comms[i], &req[i]);
+
+    MPI_Waitall(NUM_ITER, req, MPI_STATUSES_IGNORE);
+
+    for (i = 0; i < NUM_ITER; i++)
+        MPI_Comm_free(&comms[i]);
+
+    if (rank == 0)
+        printf(" No Errors\n");
+
+    MPI_Finalize();
+
+    return 0;
+}
