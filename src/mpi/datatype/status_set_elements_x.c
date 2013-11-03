a5aa7920ba7045ff66a3c3f5cfec7d8037b40fea
Pavan Balaji <balaji@mcs.anl.gov>
2013-11-03 12:00:00 -0600

    Fix assertion for large counts.
    We were checking for (size of datatype < MPIR_COUNT_MAX / count)
    earlier.  This is not a good test when the count is very large.  This
    patch modifies the assertion to (count * size of datatype <
    MPIR_COUNT_MAX) so we don't run into integer division issues.
    
    Signed-off-by: Rob Latham <robl@mcs.anl.gov>

--- src/mpi/datatype/status_set_elements_x.c
+++ src/mpi/datatype/status_set_elements_x.c
@@ -38,7 +38,7 @@ int MPIR_Status_set_elements_x_impl(MPI_Status *status, MPI_Datatype datatype, M
     /* overflow check, should probably be a real error check? */
     if (count != 0) {
         MPIU_Assert(size_x >= 0 && count > 0);
-        MPIU_Assert(size_x < MPIR_COUNT_MAX / count);
+        MPIU_Assert(count * size_x < MPIR_COUNT_MAX);
     }
 
     MPIR_STATUS_SET_COUNT(*status, size_x * count);
