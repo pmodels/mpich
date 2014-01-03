888b22681de14054d20e2dd26a9bb456657cd6af
Pavan Balaji <balaji@mcs.anl.gov>
2014-01-02 21:33:52 -0600

    Add primes to cover till 2 billion MPI ranks.
    Fixes #1992.
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpi/topo/dims_create.c
+++ src/mpi/topo/dims_create.c
@@ -36,34 +36,14 @@ PMPI_LOCAL int MPIR_ChooseFactors( int, Factors [], int, int, int [] );
 #undef MPI_Dims_create
 #define MPI_Dims_create PMPI_Dims_create
 
-/* Return the factors of n and their multiplicity in factors; the number of 
-   distinct factors is the return value and the total number of factors,
-   including multiplicities, is returned in ndivisors */
-#define NUM_PRIMES 168
-  static int primes[NUM_PRIMES] = 
-	   {2,    3,    5,    7,   11,   13,   17,   19,   23,   29, 
-	   31,   37,   41,   43,   47,   53,   59,   61,   67,   71, 
-	   73,   79,   83,   89,   97,  101,  103,  107,  109,  113, 
-	  127,  131,  137,  139,  149,  151,  157,  163,  167,  173, 
-	  179,  181,  191,  193,  197,  199,  211,  223,  227,  229, 
-	  233,  239,  241,  251,  257,  263,  269,  271,  277,  281, 
-	  283,  293,  307,  311,  313,  317,  331,  337,  347,  349, 
-	  353,  359,  367,  373,  379,  383,  389,  397,  401,  409, 
-	  419,  421,  431,  433,  439,  443,  449,  457,  461,  463, 
-	  467,  479,  487,  491,  499,  503,  509,  521,  523,  541, 
-	  547,  557,  563,  569,  571,  577,  587,  593,  599,  601, 
-	  607,  613,  617,  619,  631,  641,  643,  647,  653,  659, 
-	  661,  673,  677,  683,  691,  701,  709,  719,  727,  733, 
-	  739,  743,  751,  757,  761,  769,  773,  787,  797,  809, 
-	  811,  821,  823,  827,  829,  839,  853,  857,  859,  863, 
-	  877,  881,  883,  887,  907,  911,  919,  929,  937,  941, 
-	  947,  953,  967,  971,  977,  983,  991,  997};
+#include "primes.h"
 
 PMPI_LOCAL int MPIR_Factor( int n, Factors factors[], int *ndivisors )
 {
     int n_tmp, n_root;
     int i, nfactors=0, nall=0;
     int cnt;
+    int NUM_PRIMES = sizeof(primes) / sizeof(int);
 
     /* Start from an approximate of the square root of n, by first finding
        the power of 2 at least as large as n.  The approximate root is then
