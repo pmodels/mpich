3457b353765078d5a37be8b043811fc7e547bcd6
Ken Raffenetti <raffenet@mcs.anl.gov>
2014-01-27 14:00:42 -0600

    add manual test for process affinity
    Add a number of synthetic topologies and reference binding bitmaps to
    the implementation specific tests directory. Running the proc_binding.sh
    script will show any errors in hydra's binding logic.
    
    Signed-off-by: Pavan Balaji <balaji@mcs.anl.gov>

---
+++ test/mpi/impls/hydra/dummy.c
@@ -0,0 +1,11 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2013 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+/* */
+
+int main( int argc, char *argv[] )
+{
+    return 0;
+}
