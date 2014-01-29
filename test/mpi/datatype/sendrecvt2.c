229f3126763c8275460dcf6ed916350634ad68c8
William Gropp <wgropp@illinois.edu>
2014-01-29 12:04:51 -0600

    Add sendrecv2 and sendrecv4 test from MPICH-1
    This test moves the sendrecv2 and sendrecv4 test from MPICH-1 to the new
    MPICH tests.  It has been updated to use the MTest framework.  This test
    uses the datatypes created by the dtypes.c file (also moved from
    MPICH-1).
    
    These tests involve creating a structure type with an explicit offset of
    a variety of types, and then communicated with MPI_BOTTOM as the buffer
    location.  Several of these tests failed in a recent update to the Dataloop
    code for optimization of performance.
    
    Signed-off-by: Rob Latham <robl@mcs.anl.gov>

---
+++ test/mpi/datatype/sendrecvt2.c
@@ -0,0 +1,110 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2014 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+#include "mpi.h"
+#include "mpitest.h"
+#include <stdio.h>
+#include "dtypes.h"
+
+/*
+   This program is from mpich/tsuite/pt2pt and should be changed there only.
+   It needs gcomm and dtype from mpich/tsuite, and can be run with
+   any number of processes > 1.
+ */
+int main( int argc, char **argv)
+{
+    MPI_Datatype *types;
+    void         **inbufs, **outbufs;
+    int          *counts, *bytesize, ntype;
+    MPI_Comm     comm;
+    int          ncomm = 20, rank, np, partner, tag, count;
+    int          i, j, k, err, toterr, world_rank, errloc;
+    MPI_Status   status;
+    char         *obuf;
+    char         myname[MPI_MAX_OBJECT_NAME];
+    int          mynamelen;
+
+    MTest_Init( &argc, &argv );
+
+    /*
+     * Check for -basiconly to select only the simple datatypes
+     */
+    for (i=1; i<argc; i++) {
+	if (!argv[i]) break;
+	if (strcmp( argv[i], "-basiconly" ) == 0) {
+	    MTestDatatype2BasicOnly();
+	}
+    }
+
+    MTestDatatype2Allocate( &types, &inbufs, &outbufs, &counts, &bytesize,
+			    &ntype );
+    MTestDatatype2Generate( types, inbufs, outbufs, counts, bytesize, &ntype );
+
+    MPI_Comm_rank( MPI_COMM_WORLD, &world_rank );
+
+ /* Test over a wide range of datatypes and communicators */
+    err = 0;
+    tag = 0;
+    while (MTestGetIntracomm( &comm, 2 )) {
+	if (comm == MPI_COMM_NULL) continue;
+	MPI_Comm_rank( comm, &rank );
+	MPI_Comm_size( comm, &np );
+	if (np < 2) continue;
+	if (world_rank == 0)
+	    MTestPrintfMsg( 10, "Testing communicator number %s\n",
+			    MTestGetIntracommName() );
+
+	tag++;
+	for (j=0; j<ntype; j++) {
+	    MPI_Type_get_name( types[j], myname, &mynamelen );
+	    if (world_rank == 0)
+		MTestPrintfMsg( 10, "Testing type %s\n", myname );
+	    if (rank == 0) {
+		partner = np - 1;
+		MPI_Send( inbufs[j], counts[j], types[j], partner, tag, comm );
+            }
+	    else if (rank == np-1) {
+		partner = 0;
+		obuf = outbufs[j];
+		for (k=0; k<bytesize[j]; k++)
+		    obuf[k] = 0;
+		MPI_Recv( outbufs[j], counts[j], types[j], partner, tag,
+			  comm, &status );
+		/* Test correct */
+		MPI_Get_count( &status, types[j], &count );
+		if (count != counts[j]) {
+		    fprintf( stderr,
+		     "Error in counts (got %d expected %d) with type %s\n",
+			 count, counts[j], myname );
+		    err++;
+                }
+		if (status.MPI_SOURCE != partner) {
+		    fprintf( stderr,
+			"Error in source (got %d expected %d) with type %s\n",
+			 status.MPI_SOURCE, partner, myname );
+		    err++;
+                }
+		if ((errloc = MTestDatatype2Check( inbufs[j], outbufs[j],
+						   bytesize[j] ))) {
+		    char *p1, *p2;
+		    fprintf( stderr,
+		    "Error in data with type %s (type %d on %d) at byte %d\n",
+			     myname, j, world_rank, errloc - 1 );
+		    p1 = (char *)inbufs[j];
+		    p2 = (char *)outbufs[j];
+		    fprintf( stderr,
+			"Got %x expected %x\n", p1[errloc-1], p2[errloc-1] );
+		    err++;
+                }
+            }
+	}
+	MTestFreeComm( &comm );
+    }
+
+    MTestDatatype2Free( types, inbufs, outbufs, counts, bytesize, ntype );
+    MTest_Finalize( err );
+    MPI_Finalize();
+    return MTestReturnValue( err );
+}
---
+++ test/mpi/datatype/sendrecvt4.c
@@ -0,0 +1,163 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2014 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+#include "mpi.h"
+#include "mpitest.h"
+#include <stdio.h>
+#include "dtypes.h"
+
+
+/*
+   This program is from mpich/tsuite/pt2pt and should be changed there only.
+   It needs gcomm and dtype from mpich/tsuite, and can be run with
+   any number of processes > 1.
+
+   This version sends and receives EVERYTHING from MPI_BOTTOM, by putting
+   the data into a structure.
+
+   This code isn't quite correct, since the MPI_Type_struct that is
+   created for the type may not have the correct extent.
+   One possible change is to make the struct type include the count, and
+   send/receive one instance of the data item.
+
+   The GenerateData call should return extents; when the extent of the
+   created structure doesn't match, we can at least issue an error message.
+ */
+int main( int argc, char **argv )
+{
+    MPI_Datatype *types;
+    void         **inbufs, **outbufs;
+    int          *counts, *bytesize, ntype;
+    MPI_Comm     comm;
+    int          ncomm = 20, rank, np, partner, tag, count;
+    int          j, k, err, toterr, world_rank, errloc;
+    MPI_Status   status;
+    char         *obuf;
+    MPI_Datatype offsettype;
+    int          blen;
+    MPI_Aint     displ, extent, natural_extent;
+    char         myname[MPI_MAX_OBJECT_NAME];
+    int          mynamelen;
+
+    MTest_Init( &argc, &argv );
+
+    MTestDatatype2Allocate( &types, &inbufs, &outbufs, &counts, &bytesize,
+			    &ntype );
+    MTestDatatype2Generate( types, inbufs, outbufs, counts, bytesize, &ntype );
+
+    MPI_Comm_rank( MPI_COMM_WORLD, &world_rank );
+
+    /* Test over a wide range of datatypes and communicators */
+    err = 0;
+    tag = 0;
+    while (MTestGetIntracomm( &comm, 2 )) {
+	if (comm == MPI_COMM_NULL) continue;
+	MPI_Comm_rank( comm, &rank );
+	MPI_Comm_size( comm, &np );
+	if (np < 2) continue;
+	tag++;
+	for (j=0; j<ntype; j++) {
+	    MPI_Type_get_name( types[j], myname, &mynamelen );
+	    if (world_rank == 0)
+		MTestPrintfMsg( 10, "Testing type %s\n", myname );
+	    if (rank == 0) {
+		MPI_Get_address( inbufs[j], &displ );
+		blen = 1;
+		MPI_Type_create_struct( 1, &blen, &displ, types + j,
+					&offsettype );
+		MPI_Type_commit( &offsettype );
+		/* Warning: if the type has an explicit MPI_UB, then using a
+		   simple shift of the offset won't work.  For now, we skip
+		   types whose extents are negative; the correct solution is
+		   to add, where required, an explicit MPI_UB */
+		MPI_Type_extent( offsettype, &extent );
+		if (extent < 0) {
+		    if (world_rank == 0)
+			MTestPrintfMsg( 10,
+			"... skipping (appears to have explicit MPI_UB\n" );
+		    MPI_Type_free( &offsettype );
+		    continue;
+		}
+		MPI_Type_extent( types[j], &natural_extent );
+		if (natural_extent != extent) {
+		    MPI_Type_free( &offsettype );
+		    continue;
+		}
+		partner = np - 1;
+		MPI_Send( MPI_BOTTOM, counts[j], offsettype, partner, tag,
+			  comm );
+		MPI_Type_free( &offsettype );
+            }
+	    else if (rank == np-1) {
+		partner = 0;
+		obuf = outbufs[j];
+		for (k=0; k<bytesize[j]; k++)
+		    obuf[k] = 0;
+		MPI_Get_address( outbufs[j], &displ );
+		blen = 1;
+		MPI_Type_create_struct( 1, &blen, &displ, types + j,
+					&offsettype );
+		MPI_Type_commit( &offsettype );
+		/* Warning: if the type has an explicit MPI_UB, then using a
+		   simple shift of the offset won't work.  For now, we skip
+		   types whose extents are negative; the correct solution is
+		   to add, where required, an explicit MPI_UB */
+		MPI_Type_extent( offsettype, &extent );
+		if (extent < 0) {
+		    MPI_Type_free( &offsettype );
+		    continue;
+		}
+		MPI_Type_extent( types[j], &natural_extent );
+		if (natural_extent != extent) {
+		    MPI_Type_free( &offsettype );
+		    continue;
+		}
+		MPI_Recv( MPI_BOTTOM, counts[j], offsettype,
+			  partner, tag, comm, &status );
+		/* Test for correctness */
+		MPI_Get_count( &status, types[j], &count );
+		if (count != counts[j]) {
+		    fprintf( stderr,
+			"Error in counts (got %d expected %d) with type %s\n",
+			 count, counts[j], myname );
+		    err++;
+                }
+		if (status.MPI_SOURCE != partner) {
+		    fprintf( stderr,
+			"Error in source (got %d expected %d) with type %s\n",
+			 status.MPI_SOURCE, partner, myname );
+		    err++;
+                }
+		if ((errloc = MTestDatatype2Check( inbufs[j], outbufs[j],
+						   bytesize[j] ))) {
+		    fprintf( stderr,
+                  "Error in data with type %s (type %d on %d) at byte %d\n",
+			 myname, j, world_rank, errloc - 1 );
+		    if (err < 10) {
+			/* Give details on only the first 10 errors */
+			unsigned char *in_p = (unsigned char *)inbufs[j],
+			    *out_p = (unsigned char *)outbufs[j];
+			int jj;
+			jj = errloc - 1;
+			jj &= 0xfffffffc; /* lop off a few bits */
+			in_p += jj;
+			out_p += jj;
+			fprintf( stderr, "%02x%02x%02x%02x should be %02x%02x%02x%02x\n",
+				 out_p[0], out_p[1], out_p[2], out_p[3],
+				 in_p[0], in_p[1], in_p[2], in_p[3] );
+		    }
+		    err++;
+                }
+		MPI_Type_free( &offsettype );
+            }
+	}
+	MTestFreeComm( &comm );
+    }
+
+    MTestDatatype2Free( types, inbufs, outbufs, counts, bytesize, ntype );
+    MTest_Finalize( err );
+    MPI_Finalize();
+    return MTestReturnValue( err );
+}
