bc39d1b02dec4719db957286fb17f3730100da8e
Ken Raffenetti <raffenet@mcs.anl.gov>
2014-02-17 09:00:11 -0600

    add missing copyright statements
    Signed-off-by: Pavan Balaji <balaji@mcs.anl.gov>

--- src/pm/hydra/examples/print_cpus_allowed.c
+++ src/pm/hydra/examples/print_cpus_allowed.c
@@ -1,3 +1,9 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *
+ *  (C) 2014 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
 #define PATH_MAX 1000
 
 #include "mpi.h"
