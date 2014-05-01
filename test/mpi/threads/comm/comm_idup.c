7e44a0f14764b56823811ff8235ce802f5bdfae6
Huiwei Lu <huiweilu@mcs.anl.gov>
2014-04-30 23:10:57 -0500

    Adds two tests to MPI_Comm_idup
    Two multi-threaded tests are added for MPI_Comm_idup in
    test/mpi/threads/comm: ctxidup.c and comm_idup.c. The former has passed
    the test but the latter still not (marked in testlist as xfail).
    
    Signed-off-by: Pavan Balaji <balaji@anl.gov>

---
+++ test/mpi/threads/comm/comm_idup.c
@@ -0,0 +1,98 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2012 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
+#include <stdio.h>
+#include <mpi.h>
+#include "mpitest.h"
+#include "mpithreadtest.h"
+
+#define NUM_THREADS 2
+#define NUM_ITER    1
+
+#define check(X_)       \
+    do {                \
+        if (!(X_)) {    \
+            printf("[%s:%d] -- Assertion failed: %s\n", __FILE__, __LINE__, #X_);\
+            MPI_Abort(MPI_COMM_WORLD, 1); \
+        }               \
+    } while (0)
+
+MPI_Comm               comms[NUM_THREADS];
+MTEST_THREAD_LOCK_TYPE comm_lock;
+int                    rank, size;
+int                    verbose = 0;
+
+MTEST_THREAD_RETURN_TYPE test_comm_dup(void *arg)
+{
+    int rank;
+    int i;
+    MPI_Request req;
+
+    MPI_Comm_rank(comms[*(int*)arg], &rank);
+
+    for (i = 0; i < NUM_ITER; i++) {
+        MPI_Comm    comm, self_dup;
+
+        if (*(int*)arg == rank) {
+            MTestSleep(1);
+        }
+
+        MTest_thread_lock(&comm_lock);
+        if (verbose) printf("%d: Thread %d - COMM_IDUP %d start\n", rank, *(int*)arg, i);
+        MPI_Comm_idup(MPI_COMM_SELF, &self_dup, &req);
+        MPI_Wait(&req, MPI_STATUS_IGNORE);
+        if (verbose) printf("\t%d: Thread %d - COMM_IDUP %d finish\n", rank, *(int*)arg, i);
+        MTest_thread_unlock(&comm_lock);
+        MPI_Comm_free(&self_dup);
+
+        if (verbose) printf("%d: Thread %d - comm_idup %d start\n", rank, *(int*)arg, i);
+        MPI_Comm_idup(comms[*(int*)arg], &comm, &req);
+        MPI_Wait(&req, MPI_STATUS_IGNORE);
+        MPI_Comm_free(&comm);
+        if (verbose) printf("\t%d: Thread %d - comm_idup %d finish\n", rank, *(int*)arg, i);
+    }
+
+    if (verbose) printf("%d: Thread %d - Done.\n", rank, *(int*)arg);
+    return (MTEST_THREAD_RETURN_TYPE)0;
+}
+
+
+int main(int argc, char **argv)
+{
+    int         thread_args[NUM_THREADS];
+    int         i, provided;
+
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+
+    check(provided == MPI_THREAD_MULTIPLE);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    MTest_thread_lock_create(&comm_lock);
+
+    for (i = 0; i < NUM_THREADS; i++) {
+        MPI_Comm_dup(MPI_COMM_WORLD, &comms[i]);
+    }
+
+    for (i = 0; i < NUM_THREADS; i++) {
+        thread_args[i] = i;
+        MTest_Start_thread( test_comm_dup, (void *)&thread_args[i] );
+    }
+
+    MTest_Join_threads();
+
+    for (i = 0; i < NUM_THREADS; i++) {
+        MPI_Comm_free(&comms[i]);
+    }
+
+    if (rank == 0)
+        printf(" No Errors\n");
+
+    MPI_Finalize();
+
+    return 0;
+}
---
+++ test/mpi/threads/comm/ctxidup.c
@@ -0,0 +1,104 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *
+ *  (C) 2006 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "mpitest.h"
+#include "mpithreadtest.h"
+
+/*
+ * static char MTEST_Descrip[] = "Creating communications concurrently in different threads";
+ */
+
+MTEST_THREAD_RETURN_TYPE dup_thread(void *);
+
+MTEST_THREAD_RETURN_TYPE dup_thread(void *p)
+{
+    int rank;
+    int buffer[1];
+    MPI_Comm *comm2_ptr = (MPI_Comm *)p;
+    MPI_Comm comm3;
+    MPI_Request req;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank & 0x1) {
+        /* If odd, wait for message */
+        MPI_Recv( buffer, 0, MPI_INT, rank, 0, MPI_COMM_WORLD,
+                MPI_STATUS_IGNORE );
+    }
+    MPI_Comm_idup( *comm2_ptr, &comm3, &req );
+    MPI_Wait(&req, MPI_STATUS_IGNORE);
+
+    MPI_Barrier( comm3 );
+    MPI_Recv( buffer, 0, MPI_INT, rank, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
+
+    MPI_Comm_free( &comm3 );
+    /* Tell the main thread that we're done */
+    MPI_Send( buffer, 0, MPI_INT, rank, 2, MPI_COMM_WORLD );
+
+    return (MTEST_THREAD_RETURN_TYPE)0;
+}
+
+int main( int argc, char *argv[] )
+{
+    int rank, size;
+    int provided;
+    int buffer[1];
+    MPI_Comm comm1, comm2, comm4;
+
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    /* Check that we're multi-threaded */
+    if (provided != MPI_THREAD_MULTIPLE) {
+        if (rank == 0) {
+            printf("MPI_Init_thread must return MPI_THREAD_MULTIPLE in order for this test to run.\n");
+            fflush(stdout);
+        }
+        MPI_Finalize();
+        return 1;
+    }
+
+    /* The test is this:
+       The main thread on ODD processors tells the other thread to start
+       a comm dup(on comm2), then starts a comm dup(on comm1) after a delay.
+       The main thread on even processors starts a comm dup(on comm1)
+
+       The second thread on ODD processors waits until it gets a message
+       (from the same process) before starting the comm dup on comm2.
+       */
+
+    /* Create two communicators */
+    MPI_Comm_dup( MPI_COMM_WORLD, &comm1 );
+    MPI_Comm_dup( MPI_COMM_WORLD, &comm2 );
+
+    /* Start a thread that will perform a dup comm2 */
+    MTest_Start_thread( dup_thread, (void *)&comm2 );
+
+    /* If we're odd, send to our new thread and then delay */
+    if (rank & 0x1) {
+        MPI_Ssend( buffer, 0, MPI_INT, rank, 0, MPI_COMM_WORLD );
+        MTestSleep(1);
+    }
+    MPI_Comm_dup( comm1, &comm4 );
+
+    /* Tell the threads to exit after we've created our new comm */
+    MPI_Barrier( comm4 );
+    MPI_Ssend( buffer, 0, MPI_INT, rank, 1, MPI_COMM_WORLD );
+    MPI_Recv( buffer, 0, MPI_INT, rank, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
+
+    MPI_Comm_free( &comm4 );
+    MPI_Comm_free( &comm1 );
+    MPI_Comm_free( &comm2 );
+
+    MTest_Finalize(0);
+    MPI_Finalize();
+    return 0;
+}
