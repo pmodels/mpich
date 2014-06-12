aa1b034353ca4c1ffbfe4d25cb69b62bbe9dee77
Su Huang <suhuang@us.ibm.com>
2014-06-12 09:10:04 -0400

    MPI_Iallgather has no validity check for sendcount
    (ibm) D198243
    
    Signed-off-by: Michael Blocksome <blocksom@us.ibm.com>
    Signed-off-by: Wesley Bland <wbland@anl.gov>

--- src/mpi/coll/iallgather.c
+++ src/mpi/coll/iallgather.c
@@ -673,6 +673,7 @@ int MPI_Iallgather(const void *sendbuf, int sendcount, MPI_Datatype sendtype,
         {
             if (sendbuf != MPI_IN_PLACE)
                 MPIR_ERRTEST_DATATYPE(sendtype, "sendtype", mpi_errno);
+            MPIR_ERRTEST_COUNT(sendcount, mpi_errno);
             MPIR_ERRTEST_DATATYPE(recvtype, "recvtype", mpi_errno);
             MPIR_ERRTEST_COMM(comm, mpi_errno);
 
