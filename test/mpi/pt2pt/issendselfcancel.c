8eef807cf5a6a61b8d6a1289f52d900afe779c10
Wesley Bland <wbland@anl.gov>
2014-07-10 16:41:34 -0500

    Add a test for the bug in #2129
    Test this case so we don't have another regression. Thanks for test case
    contributed by Lisandro Dalcin (dalcinl@gmail.com).
    
    See #2129
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

---
+++ test/mpi/pt2pt/issendselfcancel.c
@@ -0,0 +1,36 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2014 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+#import <stdio.h>
+#import <stdlib.h>
+#import <assert.h>
+#import "mpi.h"
+
+int main(int argc, char **argv)
+{
+    MPI_Request req;
+    MPI_Status status;
+
+    MPI_Init(NULL, NULL);
+
+    MPI_Issend(NULL, 0, MPI_BYTE, 0, 123, MPI_COMM_SELF, &req);
+
+    MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_SELF, &status);
+    assert(status.MPI_SOURCE == 0);
+    assert(status.MPI_TAG == 123);
+
+    MPI_Cancel(&req);
+    assert(req != MPI_REQUEST_NULL);
+
+    MPI_Request_free(&req);
+
+    MPI_Irecv(NULL, 0, MPI_BYTE, 0, 123, MPI_COMM_SELF, &req);
+    MPI_Cancel(&req);
+    MPI_Wait(&req, &status);
+
+    printf(" No Errors\n");
+
+    MPI_Finalize();
+}
