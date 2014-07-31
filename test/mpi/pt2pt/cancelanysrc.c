b95c5fa2fd3aff6c235ab9e5ae9afba3b142198a
Ken Raffenetti <raffenet@mcs.anl.gov>
2014-07-31 15:10:49 -0500

    add test for canceling anysource recv
    Canceling an anysource can be tricky if the netmod does matching. There
    are additional functions that must be working in order for the cancel to
    be correctly reported. This test will exercise that functionality.
    
    Signed-off-by: Wesley Bland <wbland@anl.gov>

---
+++ test/mpi/pt2pt/cancelanysrc.c
@@ -0,0 +1,48 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2014 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "mpi.h"
+
+int main(int argc, char **argv) {
+    int size, rank, msg, cancelled;
+    MPI_Request request;
+    MPI_Status status;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (size != 2) {
+        fprintf(stderr,"ERROR: must be run with 2 processes");
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    if (rank == 0) {
+        msg = -1;
+        /* Post, then cancel MPI_ANY_SOURCE recv */
+        MPI_Irecv(&msg, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &request);
+        MPI_Cancel(&request);
+        MPI_Wait(&request, &status);
+        MPI_Test_cancelled(&status, &cancelled);
+        assert(cancelled);
+
+        MPI_Barrier(MPI_COMM_WORLD);
+        MPI_Irecv(&msg, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &request);
+        MPI_Wait(&request, &status);
+        assert(msg == 42);
+    } else {
+        MPI_Barrier(MPI_COMM_WORLD);
+        msg = 42;
+        MPI_Send(&msg, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+
+    if (rank == 0)
+        printf(" No Errors\n");
+
+    MPI_Finalize();
+}
