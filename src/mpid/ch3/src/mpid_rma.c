32596b62b8d4b85cca3424a85fae216843b20e06
Xin Zhao <xinzhao3@illinois.edu>
2014-09-24 21:28:10 -0500

    Revert "Bug-fix: waiting for ACKs for Active Target Synchronization."
    This reverts commit 7418944673507d4fd3d3025e9e5ecfde809e9210.

--- src/mpid/ch3/src/mpid_rma.c
+++ src/mpid/ch3/src/mpid_rma.c
@@ -299,7 +299,6 @@ static int win_init(MPI_Aint size, int disp_unit, int create_flavor, int model,
     (*win_ptr)->epoch_count         = 0;
     (*win_ptr)->at_rma_ops_list     = NULL;
     (*win_ptr)->shm_allocated       = FALSE;
-    (*win_ptr)->ack_counter         = 0;
 
     /* Initialize the passive target lock state */
     MPIU_CHKPMEM_MALLOC((*win_ptr)->targets, struct MPIDI_Win_target_state *,
