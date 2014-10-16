aad52bcc7414ecf80a8242b5177d0b33f3067b5e
Pavan Balaji <balaji@anl.gov>
2014-10-16 18:20:30 -0500

    Delete the unsupported mx and wintcp netmods.
    Signed-off-by: Wesley Bland <wbland@anl.gov>

--- src/mpid/ch3/channels/nemesis/netmod/Makefile.mk
+++ src/mpid/ch3/channels/nemesis/netmod/Makefile.mk
@@ -6,7 +6,6 @@
 ##     See COPYRIGHT in top-level directory.
 ##
 
-include $(top_srcdir)/src/mpid/ch3/channels/nemesis/netmod/mx/Makefile.mk
 include $(top_srcdir)/src/mpid/ch3/channels/nemesis/netmod/tcp/Makefile.mk
 include $(top_srcdir)/src/mpid/ch3/channels/nemesis/netmod/none/Makefile.mk
 include $(top_srcdir)/src/mpid/ch3/channels/nemesis/netmod/newmad/Makefile.mk
