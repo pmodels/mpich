2f896172913cc63d7c03e832e10c0e62710ae5dc
Min Si <msi@il.is.s.u-tokyo.ac.jp>
2014-10-21 12:48:44 -0500

    Separate and rewrite mtest datatype.
    This patch separates mtest datatype from file mtest.c and then rewrites
    the whole structure for applying various test patterns and datatypes.
    
    We separate mtest datatype funcs and test generators.
    1. In mtest_datatype_gen.c, we generate test cases for both basic and
    derived datatype.
    2. In mtest_datatype.c, we define the MTestDatatype creator,
    init/free/check functions for each derived datatype in order to reuse in
    multiple test cases generated by 1.
    
    About test case definition:
    1. For every basic datatype, we only define one test case using
    the same type for both send and receive buffers.
    2. For every derived datatype, we test ddt send buffer and receive
    buffer separately, each with contig buffer on the other side. We define
    following four different ddt structures for each test:
    	a.large block length
    	b.large count
    	c.large block length and large stride
    	d.large count and large stride
    
    Signed-off-by: Wesley Bland <wbland@anl.gov>

---
+++ test/mpi/util/mtest_datatype.h
@@ -0,0 +1,48 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *
+ *  (C) 2014 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
+#ifndef MTEST_DATATYPE_H_
+#define MTEST_DATATYPE_H_
+
+#include "mpi.h"
+#include "mpitestconf.h"
+#include "mpitest.h"
+
+/* Provide backward portability to MPI 1 */
+#ifndef MPI_VERSION
+#define MPI_VERSION 1
+#endif
+
+enum MTEST_BASIC_DT {
+    MTEST_BDT_INT,
+    MTEST_BDT_DOUBLE,
+    MTEST_BDT_FLOAT_INT,
+    MTEST_BDT_SHORT,
+    MTEST_BDT_LONG,
+    MTEST_BDT_CHAR,
+    MTEST_BDT_UINT64_T,
+    MTEST_BDT_FLOAT,
+    MTEST_BDT_BYTE,
+    MTEST_BDT_MAX
+};
+
+enum MTEST_DERIVED_DT {
+    MTEST_DDT_CONTIGUOUS,
+    MTEST_DDT_VECTOR,
+    MTEST_DDT_INDEXED,
+    MTEST_DDT_MAX
+};
+
+typedef int (*MTestDdtCreator) (int, int, int, MPI_Datatype, const char *, MTestDatatype *);
+
+extern void MTestTypeCreatorInit(MTestDdtCreator * creators);
+extern void *MTestTypeInitRecv(MTestDatatype * mtype);
+
+extern int MTestTypeBasicCreate(MPI_Datatype oldtype, MTestDatatype * mtype);
+extern int MTestTypeDupCreate(MPI_Datatype oldtype, MTestDatatype * mtype);
+
+#endif /* MTEST_DATATYPE_H_ */
