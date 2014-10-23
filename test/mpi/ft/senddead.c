45ceb3d01f3f3bc0214f99e31f1a63da55dddb2e
Wesley Bland <wbland@anl.gov>
2014-10-23 12:33:59 -0500

    Improve the senddead test
    Added another send call to this test to take out the race condition. Now, the
    test should fail under any circumstances if the send request isn't being
    cleaned up correctly.
    
    Signed-off-by: Huiwei Lu <huiweilu@mcs.anl.gov>

--- test/mpi/ft/senddead.c
+++ test/mpi/ft/senddead.c
@@ -37,6 +37,13 @@ int main(int argc, char **argv)
         MPI_Error_class(err, &errclass);
         if ((err) && (errclass != MPIX_ERR_PROC_FAILED)) {
             fprintf(stderr, "Wrong error code (%d) returned. Expected MPIX_ERR_PROC_FAILED\n", errclass);
+        }
+#endif
+        err = MPI_Send(buf, 100000, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+#if defined (MPICH) && (MPICH_NUMVERSION >= 30100102)
+        MPI_Error_class(err, &errclass);
+        if ((err) && (errclass != MPIX_ERR_PROC_FAILED)) {
+            fprintf(stderr, "Wrong error code (%d) returned. Expected MPIX_ERR_PROC_FAILED\n", errclass);
         } else {
             printf(" No Errors\n");
             fflush(stdout);
