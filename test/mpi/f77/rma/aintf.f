16fbce5e976bd9f6345036bb86abfd01446400c2
Junchao Zhang <jczhang@mcs.anl.gov>
2014-10-28 20:19:08 -0500

    Add C/Fortran tests for MPI_Aint_add/diff
    The F77/90 test is marked as xfail for a bug at
    https://trac.mpich.org/projects/mpich/ticket/1877
    
    Signed-off-by: Wesley Bland <wbland@anl.gov>

---
+++ test/mpi/f77/rma/aintf.f
@@ -0,0 +1,85 @@
+! -*- Mode: Fortran; -*-
+!
+!
+!  (C) 2014 by Argonne National Laboratory.
+!      See COPYRIGHT in top-level directory.
+!
+
+! This program tests MPI_Aint_add/diff in MPI-3.1.
+! The two functions are often used in RMA code.
+! See https://svn.mpi-forum.org/trac/mpi-forum-web/ticket/349
+
+      program main
+      implicit none
+      include 'mpif.h'
+      integer :: rank, nproc
+      integer :: ierr, errs
+      integer :: array(0:1023)
+      integer :: val, target_rank;
+      integer(kind=MPI_ADDRESS_KIND) :: bases(0:1), disp, offset
+      integer(kind=MPI_ADDRESS_KIND) :: winsize
+      integer :: win
+      integer :: intsize
+
+      integer(kind=MPI_ADDRESS_KIND), external :: MPIX_Aint_add
+      integer(kind=MPI_ADDRESS_KIND), external :: MPIX_Aint_diff
+
+      errs = 0
+      call mtest_init(ierr);
+      call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr)
+      call MPI_Comm_size(MPI_COMM_WORLD, nproc, ierr)
+
+      if (rank == 0 .and. nproc /= 2) then
+        print *, 'Must run with 2 ranks'
+        call MPI_Abort(MPI_COMM_WORLD, 1, ierr)
+      endif
+
+! Get the base address in the middle of the array
+      if (rank == 0) then
+        target_rank = 1
+        array(0) = 1234
+        call MPI_Get_address(array(512), bases(0), ierr)
+      else if (rank == 1) then
+        target_rank = 0
+        array(1023) = 1234
+        call MPI_Get_address(array(512), bases(1), ierr)
+      endif
+
+! Exchange bases
+      call MPI_Type_size(MPI_INT, intsize, ierr);
+
+      call MPI_Allgather(MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, bases,
+     &                   1, MPI_AINT, MPI_COMM_WORLD, ierr)
+
+      call MPI_Win_create_dynamic(MPI_INFO_NULL,
+     &                            MPI_COMM_WORLD, win, ierr)
+
+      winsize = intsize*1024
+      call MPI_Win_attach(win, array, winsize, ierr)
+
+! Do MPI_Aint addressing arithmetic
+      if (rank == 0) then
+        disp = intsize*511
+        offset = MPIX_Aint_add(bases(1), disp)
+      else if (rank == 1) then
+        disp = intsize*512
+        offset = MPIX_Aint_diff(bases(0), disp)
+      endif
+
+! Get value and verify it
+      call MPI_Win_fence(MPI_MODE_NOPRECEDE, win, ierr)
+      call MPI_Get(val, 1, MPI_INT, target_rank,
+     &             offset, 1, MPI_INT, win, ierr)
+      call MPI_Win_fence(MPI_MODE_NOSUCCEED, win, ierr)
+
+      if (val /= 1234) then
+        errs = errs + 1
+        print *, rank, ' -- Got', val, 'expected 1234'
+      endif
+
+      call MPI_Win_detach(win, array, ierr)
+      call MPI_Win_free(win, ierr)
+
+      call MTest_Finalize(errs)
+      call MPI_Finalize(ierr);
+      end
---
+++ test/mpi/rma/aint.c
@@ -0,0 +1,80 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *
+ *  (C) 2014 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
+/*
+ * This program tests MPI_Aint_add/diff in MPI-3.1.
+ * The two functions are often used in RMA code.
+ * See https://svn.mpi-forum.org/trac/mpi-forum-web/ticket/349
+ */
+
+#include <stdio.h>
+#include <mpi.h>
+#include "mpitest.h"
+
+int main(int argc, char **argv)
+{
+    int rank, nproc;
+    int errs = 0;
+    int array[1024];
+    int val = 0;
+    int target_rank;
+    MPI_Aint bases[2];
+    MPI_Aint disp, offset;
+    MPI_Win  win;
+
+    MTest_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+
+    if (rank == 0 && nproc != 2) {
+        MTestError("Must run with 2 ranks\n");
+    }
+
+    /* Get the base address in the middle of the array */
+    if (rank == 0) {
+        target_rank = 1;
+        array[0] = 1234;
+        MPI_Get_address(&array[512], &bases[0]);
+    } else if (rank == 1) {
+        target_rank = 0;
+        array[1023] = 1234;
+        MPI_Get_address(&array[512], &bases[1]);
+    }
+
+    /* Exchange bases */
+    MPI_Allgather(MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, bases, 1, MPI_AINT, MPI_COMM_WORLD);
+
+    MPI_Win_create_dynamic(MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+    MPI_Win_attach(win, array, sizeof(int)*1024);
+
+    /* Do MPI_Aint addressing arithmetic */
+    if (rank == 0) {
+        disp = sizeof(int)*511;
+        offset = MPIX_Aint_add(bases[1], disp); /* offset points to array[1023]*/
+    } else if (rank == 1) {
+        disp = sizeof(int)*512;
+        offset = MPIX_Aint_diff(bases[0], disp); /* offset points to array[0] */
+    }
+
+    /* Get val and verify it */
+    MPI_Win_fence(MPI_MODE_NOPRECEDE, win);
+    MPI_Get(&val, 1, MPI_INT, target_rank, offset, 1, MPI_INT, win);
+    MPI_Win_fence(MPI_MODE_NOSUCCEED, win);
+
+    if (val != 1234) {
+        errs++;
+        printf("%d -- Got %d, expected 1234\n", rank, val);
+    }
+
+    MPI_Win_detach(win, array);
+    MPI_Win_free(&win);
+
+    MTest_Finalize(errs);
+    MPI_Finalize();
+    return 0;
+}
