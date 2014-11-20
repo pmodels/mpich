77401b9511f55f8d2300fb217a2e2b8122b57832
James Dinan <james.dinan@intel.com>
2014-11-20 08:27:49 -0500

    Add dynamic err code, predefined err class test
    Test for correct error class when a dynamic error code is created from a
    predefined error class.
    
    Signed-off-by: Wesley Bland <wbland@anl.gov>

---
+++ test/mpi/errhan/dynamic_errcode_predefined_errclass.c
@@ -0,0 +1,30 @@
+/*
+ *  (C) 2006 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ *
+ *  Portions of this code were written by Intel Corporation.
+ *  Copyright (C) 2011-2012 Intel Corporation.  Intel provides this material
+ *  to Argonne National Laboratory subject to Software Grant and Corporate
+ *  Contributor License Agreement dated February 8, 2012.
+ */
+
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char **argv) {
+    int errcode, errclass;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Add_error_code(MPI_ERR_ARG, &errcode);
+    MPI_Error_class(errcode, &errclass);
+
+    if (errclass != MPI_ERR_ARG) {
+        printf("ERROR: Got 0x%x, expected 0x%x\n", errclass, MPI_ERR_ARG);
+    } else {
+        printf( " No Errors\n" );
+    }
+
+    MPI_Finalize();
+    return 0;
+}
