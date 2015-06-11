a8ac5b4e166db86c09732524a6c4a600718ee7e6
Pavan Balaji <balaji@anl.gov>
2015-06-11 01:30:22 -0500

    Remove the IB netmod.
    Signed-off-by: Sangmin Seo <sseo@anl.gov>

--- src/mpid/ch3/channels/nemesis/netmod/Makefile.mk
+++ src/mpid/ch3/channels/nemesis/netmod/Makefile.mk
@@ -9,7 +9,6 @@
 include $(top_srcdir)/src/mpid/ch3/channels/nemesis/netmod/tcp/Makefile.mk
 include $(top_srcdir)/src/mpid/ch3/channels/nemesis/netmod/none/Makefile.mk
 include $(top_srcdir)/src/mpid/ch3/channels/nemesis/netmod/portals4/Makefile.mk
-include $(top_srcdir)/src/mpid/ch3/channels/nemesis/netmod/ib/Makefile.mk
 include $(top_srcdir)/src/mpid/ch3/channels/nemesis/netmod/mxm/Makefile.mk
 include $(top_srcdir)/src/mpid/ch3/channels/nemesis/netmod/ofi/Makefile.mk
 include $(top_srcdir)/src/mpid/ch3/channels/nemesis/netmod/llc/Makefile.mk
