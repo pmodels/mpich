8519d2bb2866c72c32ce6efa202ee5c126e98716
Michael Blocksome <blocksom@us.ibm.com>
2015-07-07 20:57:08 +0000

    Move the PAMID GPID routines from mpidpre.h to mpidpost.h
    The 'MPID_Comm' struct, which is used by the PAMID GPID
    routines, is not yet defined when mpidpre.h is included.
    
    Signed-off-by: Antonio J. Pena <apenya@mcs.anl.gov>

--- src/mpid/pamid/include/mpidpost.h
+++ src/mpid/pamid/include/mpidpost.h
@@ -40,6 +40,65 @@
 #ifdef DYNAMIC_TASKING
 #define MPID_ICCREATE_REMOTECOMM_HOOK(_p,_c,_np,_gp,_r) \
      MPID_PG_ForwardPGInfo(_p,_c,_np,_gp,_r)
-#endif
+#else /* ! DYNAMIC_TASKING */
+/* If DYNAMIC_TASKING is not defined, PAMID does not provide its own GPID routines,
+   so provide one here. **/
 
+/* FIXME: A temporary version for lpids within my comm world */
+static inline int MPID_GPID_GetAllInComm( MPID_Comm *comm_ptr, int local_size,
+                                          int local_gpids[], int *singlePG )
+{
+    int i;
+    int *gpid = local_gpids;
+
+    for (i=0; i<comm_ptr->local_size; i++) {
+        *gpid++ = 0;
+        (void)MPID_VCR_Get_lpid( comm_ptr->vcr[i], gpid );
+        gpid++;
+    }
+    *singlePG = 1;
+    return 0;
+}
+
+/* FIXME: A temp for lpids within my comm world */
+static inline int MPID_GPID_ToLpidArray( int size, int gpid[], int lpid[] )
+{
+    int i;
+
+    for (i=0; i<size; i++) {
+        lpid[i] = *++gpid;  gpid++;
+    }
+    return 0;
+}
+/* FIXME: for MPI1, all process ids are relative to MPI_COMM_WORLD.
+   For MPI2, we'll need to do something more complex */
+static inline int MPID_VCR_CommFromLpids( MPID_Comm *newcomm_ptr,
+                                          int size, const int lpids[] )
+{
+    MPID_Comm *commworld_ptr;
+    int i;
+
+    commworld_ptr = MPIR_Process.comm_world;
+    /* Setup the communicator's vc table: remote group */
+    MPID_VCRT_Create( size, &newcomm_ptr->vcrt );
+    MPID_VCRT_Get_ptr( newcomm_ptr->vcrt, &newcomm_ptr->vcr );
+    for (i=0; i<size; i++) {
+        /* For rank i in the new communicator, find the corresponding
+           rank in the comm world (FIXME FOR MPI2) */
+        /* printf( "[%d] Remote rank %d has lpid %d\n",
+           MPIR_Process.comm_world->rank, i, lpids[i] ); */
+        if (lpids[i] < commworld_ptr->remote_size) {
+            MPID_VCR_Dup( commworld_ptr->vcr[lpids[i]],
+                          &newcomm_ptr->vcr[i] );
+        }
+        else {
+            /* We must find the corresponding vcr for a given lpid */
+            /* FIXME: Error */
+            return 1;
+            /* MPID_VCR_Dup( ???, &newcomm_ptr->vcr[i] ); */
+        }
+    }
+    return 0;
+}
+#endif /* DYNAMIC_TASKING */
 #endif
