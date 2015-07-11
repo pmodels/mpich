7b38ef031693eb89bd30f4e2e4658752ae73f9e6
Pavan Balaji <balaji@anl.gov>
2015-07-11 13:04:53 -0500

    Added a script to detect git conflicts.
    With the project including a large number of reference objects, our
    current usage of 8 for the abbreviation length might not be always
    sufficient.  Currently, with this length, we have no conflicts and a
    probability of collision at 1.96%.

---
+++ maint/git-conflicts.bash
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+max_abbrev=12
+
+total=`git rev-list --all | wc -l`
+
+echo "Conflicts:"
+echo "Format:   [abbrev length]: number of conflicts / total number of refs (percentage conflicts)"
+for ((x = 1 ; x <= ${max_abbrev} ; x++)) ; do
+    conflicts=`git rev-list --all --abbrev=$x --abbrev-commit | sort | uniq -D -w$x | wc -l`
+    percent=`echo "scale=8 ; 100 * $conflicts / $total" | bc | awk '{ printf("%1.2f\n", $0); }'`
+    echo "    [$x]: $conflicts / $total ($percent %)"
+    if test "$conflicts" = "0" ; then break ; fi
+done
+
+##
+## probability calculation is based on this random link, that I found
+## online:
+## http://blog.cuviper.com/2013/11/10/how-short-can-git-abbreviate/
+##
+## I have not verified the accuracy of the calculation at that link,
+## but if it is online, it must be true, of course. ;-)
+##
+
+echo
+echo "Probability of Collision:"
+echo "Format:   [abbrev length]: probability of collision"
+for ((; x <= ${max_abbrev} ; x++)) ; do
+    percent=`echo "scale=8 ; 100 * ($total ^ 2) / (16 ^ $x)" | bc | awk '{ printf("%1.2f\n", $0); }'`
+    echo "    [$x]: $percent %"
+done
