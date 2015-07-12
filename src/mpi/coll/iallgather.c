c0feceddc9b281fcac62078053f82a1e19c9a903
Lena Oden <loden@anl.gov>
2015-07-12 18:01:13 -0500

    Bugfix for MPI_Iallgather for intercommunicators.
     This Fixes Iallgather for inter-communicators. It is wrong
     to alias-checking for inter-communicators, since the local
     and the destination group in inter-communicators are not
     the same and the processes never sends messages to itself
     using an inter-communicator.
    
    Fixes #2284
    
    Signed-off-by: Huiwei Lu <huiweilu@mcs.anl.gov>

--- src/mpi/coll/iallgather.c
+++ src/mpi/coll/iallgather.c
@@ -718,7 +718,7 @@ int MPI_Iallgather(const void *sendbuf, int sendcount, MPI_Datatype sendtype,
             MPIR_ERRTEST_ARGNULL(request,"request", mpi_errno);
 
             /* catch common aliasing cases */
-            if (recvbuf != MPI_IN_PLACE && sendtype == recvtype && sendcount == recvcount && sendcount != 0) {
+            if (comm_ptr->comm_kind == MPID_INTRACOMM && recvbuf != MPI_IN_PLACE && sendtype == recvtype && sendcount == recvcount && sendcount != 0) {
                 int recvtype_size;
                 MPID_Datatype_get_size_macro(recvtype, recvtype_size);
                 MPIR_ERRTEST_ALIAS_COLL(sendbuf, (char*)recvbuf + comm_ptr->rank*recvcount*recvtype_size, mpi_errno);
