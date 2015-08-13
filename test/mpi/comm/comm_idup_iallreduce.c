46ea748cff16d1aeb759fe92f9df097729b9d835
Yanfei Guo <yguo@anl.gov>
2015-08-13 11:38:25 -0500

    Add the missing return 0 for idup-tests;
    Some of the comm_idup tests were missing the return 0;
    statement.
    
    Signed-off-by: Lena Oden <loden@anl.gov>

--- test/mpi/comm/comm_idup_iallreduce.c
+++ test/mpi/comm/comm_idup_iallreduce.c
@@ -222,5 +222,5 @@ int main(int argc, char **argv)
 
     MTest_Finalize(errs);
     MPI_Finalize();
-
+    return 0;
 }
--- test/mpi/comm/comm_idup_nb.c
+++ test/mpi/comm/comm_idup_nb.c
@@ -190,7 +190,7 @@ int main(int argc, char **argv)
     }
     MTest_Finalize(errs);
     MPI_Finalize();
-
+    return 0;
 
 
 }
