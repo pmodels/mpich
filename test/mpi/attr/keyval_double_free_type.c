424119523f35e81302997ebc18ae0230eb11505c
Yanfei Guo <yguo@anl.gov>
2015-09-18 13:10:18 -0500

    Fix bug in attr double freeing tests and reenable the tests
    Added the additional type/win to make sure that the keyval is not
    deallocated before test is finished.
    
    Signed-off-by: Pavan Balaji <balaji@anl.gov>

--- test/mpi/attr/keyval_double_free_type.c
+++ test/mpi/attr/keyval_double_free_type.c
@@ -21,20 +21,24 @@ int delete_fn(MPI_Comm comm, int keyval, void *attr, void *extra)
 int main(int argc, char **argv)
 {
     MPI_Datatype type;
+    MPI_Datatype type_dup;
     int keyval = MPI_KEYVAL_INVALID;
     int keyval_copy = MPI_KEYVAL_INVALID;
     int errs = 0;
 
     MTest_Init(&argc, &argv);
     MPI_Type_dup(MPI_INT, &type);
+    MPI_Type_dup(MPI_INT, &type_dup);
 
     MPI_Type_create_keyval(MPI_NULL_COPY_FN, delete_fn, &keyval, NULL);
     keyval_copy = keyval;
     MPI_Type_set_attr(type, keyval, NULL);
+    MPI_Type_set_attr(type_dup, keyval, NULL);
 
     MPI_Type_free(&type);      /* first MPI_Type_free_keyval */
     MPI_Type_free_keyval(&keyval);   /* second MPI_Type_free_keyval */
     MPI_Type_free_keyval(&keyval_copy);      /* third MPI_Type_free_keyval */
+    MPI_Type_free(&type_dup);      /* fourth MPI_Type_free_keyval */
     MTest_Finalize(errs);
     MPI_Finalize();
     return 0;
--- test/mpi/attr/keyval_double_free_win.c
+++ test/mpi/attr/keyval_double_free_win.c
@@ -9,6 +9,9 @@
 #include <stdio.h>
 #include "mpitest.h"
 
+#define NUM_WIN 2
+#define DATA_SZ sizeof(int)
+
 /* tests multiple invocations of MPI_Win_free_keyval on the same keyval */
 
 int delete_fn(MPI_Comm comm, int keyval, void *attr, void *extra);
@@ -20,24 +23,30 @@ int delete_fn(MPI_Comm comm, int keyval, void *attr, void *extra)
 
 int main(int argc, char **argv)
 {
-    void *base_ptr = NULL;
-    MPI_Win window;
+    void *base_ptr[NUM_WIN];
+    MPI_Win windows[NUM_WIN];
     int keyval = MPI_KEYVAL_INVALID;
     int keyval_copy = MPI_KEYVAL_INVALID;
     int errs = 0;
 
     MTest_Init(&argc, &argv);
-    MPI_Alloc_mem(sizeof(int), MPI_INFO_NULL, &base_ptr);
-    MPI_Win_create(base_ptr, sizeof(int), 1, MPI_INFO_NULL, MPI_COMM_WORLD, &window);
+    MPI_Alloc_mem(DATA_SZ, MPI_INFO_NULL, &base_ptr[0]);
+    MPI_Win_create(base_ptr[0], DATA_SZ, 1, MPI_INFO_NULL, MPI_COMM_WORLD, &windows[0]);
+    MPI_Alloc_mem(DATA_SZ, MPI_INFO_NULL, &base_ptr[1]);
+    MPI_Win_create(base_ptr[1], DATA_SZ, 1, MPI_INFO_NULL, MPI_COMM_WORLD, &windows[1]);
 
     MPI_Win_create_keyval(MPI_NULL_COPY_FN, delete_fn, &keyval, NULL);
     keyval_copy = keyval;
-    MPI_Win_set_attr(window, keyval, NULL);
 
-    MPI_Win_free(&window);      /* first MPI_Win_free_keyval */
-    MPI_Free_mem(base_ptr);
+    MPI_Win_set_attr(windows[0], keyval, NULL);
+    MPI_Win_set_attr(windows[1], keyval, NULL);
+
+    MPI_Win_free(&windows[0]);      /* first MPI_Win_free_keyval */
+    MPI_Free_mem(base_ptr[0]);
     MPI_Win_free_keyval(&keyval);   /* second MPI_Win_free_keyval */
     MPI_Win_free_keyval(&keyval_copy);      /* third MPI_Win_free_keyval */
+    MPI_Win_free(&windows[1]);      /* fourth MPI_Win_free_keyval */
+    MPI_Free_mem(base_ptr[1]);
     MTest_Finalize(errs);
     MPI_Finalize();
     return 0;
