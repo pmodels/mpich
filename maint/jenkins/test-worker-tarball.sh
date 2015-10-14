e1ddafb8b2b7301b84e77c11918faf89a5b583d4
Yanfei Guo <yguo@anl.gov>
2015-10-14 14:31:06 -0500

    maint/jenkins: build mpich-tarball on /sandbox
    Creating mpich tarball on local disk instead of NFS.
    
    No reviewer.

--- maint/jenkins/test-worker-tarball.sh
+++ maint/jenkins/test-worker-tarball.sh
@@ -9,11 +9,25 @@ export CXX=g++
 export FC=gfortran
 
 cd $WORKSPACE
-git clean -x -d -f
+TMP_WORKSPACE=$(mktemp -d /sandbox/jenkins.tmp.XXXXXXXX)
 
 mkdir build
-cd build
+
+cp -a $WORKSPACE/* $TMP_WORKSPACE/
+pushd $TMP_WORKSPACE
+
+git clean -x -d -f
+
+pushd build
 
 ../maint/release.pl --branch=master --version=master --git-repo=git://git.mpich.org/mpich.git
+
+popd
+
+cp -a $TMP_WORKSPACE/build/mpich-master.tar.gz $WORKSPACE/build
+
+popd
+
+rm -rf $TMP_WORKSPACE
 
 exit 0
