77592165aa38afc218e2f3f5913be4a7e3ef923c
Rob Latham <robl@mcs.anl.gov>
2015-12-09 16:41:36 -0600

    some backtrace cleanup to remove warnings
    - use preprocessor to compile just one of our backtrace options
    - configure checks if we need a prototype or not
    
    Signed-off-by: Sangmin Seo <sseo@anl.gov>

--- src/mpl/include/mplutil.h
+++ src/mpl/include/mplutil.h
@@ -13,6 +13,19 @@
 extern "C" {
 #endif
 
+#if !MPL_HAVE_DECL_BACKTRACE_CREATE_STATE
+struct backtrace_state;
+typedef void (*backtrace_error_callback)(void *data, const char *msg,
+                                         int errnum);
+
+extern struct backtrace_state *backtrace_create_state(
+        const char *filename, int threaded,
+        backtrace_error_callback error_callback, void *data);
+#endif
+#if !MPL_HAVE_DECL_BACKTRACE_PRINT
+extern void backtrace_print(struct backtrace_state *state, int skip, FILE *);
+#endif
+
 #define MPL_BACKTRACE_BUFFER_LEN 1024
 #define MPL_MAX_TRACE_DEPTH 32
 void MPL_backtrace_show(FILE *output);
