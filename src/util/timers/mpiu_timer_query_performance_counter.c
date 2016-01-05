173b39f23c2fbadd3c24f1172f56c0ae51915268
Pavan Balaji <balaji@anl.gov>
2016-01-05 03:47:30 -0600

    timers: remove namespacing for local variable.
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/util/timers/mpiu_timer_query_performance_counter.c
+++ src/util/timers/mpiu_timer_query_performance_counter.c
@@ -10,26 +10,27 @@ MPL_SUPPRESS_OSX_HAS_NO_SYMBOLS_WARNING;
 
 #if MPICH_TIMER_KIND == MPIU_TIMER_KIND__QUERYPERFORMANCECOUNTER
 
-double MPIU_Seconds_per_tick = 0.0;     /* High performance counter frequency */
+static double seconds_per_tick = 0.0;     /* High performance counter frequency */
+
 int MPIU_Wtime_init(void)
 {
     LARGE_INTEGER n;
     QueryPerformanceFrequency(&n);
-    MPIU_Seconds_per_tick = 1.0 / (double) n.QuadPart;
+    seconds_per_tick = 1.0 / (double) n.QuadPart;
 
     return MPIU_TIMER_SUCCESS;
 }
 
 int MPIU_Wtick(double *wtick)
 {
-    *wtick = MPIU_Seconds_per_tick;
+    *wtick = seconds_per_tick;
 
     return MPIU_TIMER_SUCCESS;
 }
 
 int MPIU_Wtime_todouble(MPIU_Time_t * t, double *val)
 {
-    *val = (double) t->QuadPart * MPIU_Seconds_per_tick;
+    *val = (double) t->QuadPart * seconds_per_tick;
 
     return MPIU_TIMER_SUCCESS;
 }
@@ -38,7 +39,7 @@ int MPIU_Wtime_diff(MPIU_Time_t * t1, MPIU_Time_t * t2, double *diff)
 {
     LARGE_INTEGER n;
     n.QuadPart = t2->QuadPart - t1->QuadPart;
-    *diff = (double) n.QuadPart * MPIU_Seconds_per_tick;
+    *diff = (double) n.QuadPart * seconds_per_tick;
 
     return MPIU_TIMER_SUCCESS;
 }
