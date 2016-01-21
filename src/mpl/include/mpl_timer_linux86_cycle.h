4ad77ec00ceb95abeb92d0684725b3eb8b5197ba
Charles J Archer <charles.j.archer@intel.com>
2016-01-20 17:47:26 -0800

    x86 cycle timer warning and asm fixes
    Signed-off-by: Pavan Balaji <balaji@anl.gov>

--- src/mpl/include/mpl_timer_linux86_cycle.h
+++ src/mpl/include/mpl_timer_linux86_cycle.h
@@ -16,23 +16,23 @@ static inline int MPL_Wtime(MPL_Time_t *timeval)
    the rdtscp instruction which is synchronizing, we use this when we
    can. */
 #ifdef MPL_LINUX86_CYCLE_RDTSCP
-    __asm__ __volatile__("push %%rbx ; cpuid ; rdtsc ; pop %%rbx ; shl $32, %%rdx; or %%rdx, %%rax" : "=a" (*timeval) : : "ecx", "rdx");
+    __asm__ __volatile__("push %%rbx ; cpuid ; rdtsc ; pop %%rbx ; shl $32, %%rdx; or %%rdx, %%rax" : "=a" (timeval) : : "ecx", "rdx");
 
 #elif defined(MPL_LINUX86_CYCLE_CPUID_RDTSC64)
 /* Here we have to save the rbx register for when the compiler is
    generating position independent code (e.g., when it's generating
    shared libraries) */
-    __asm__ __volatile__("push %%rbx ; cpuid ; rdtsc ; pop %%rbx" : "=A" (*timeval) : : "ecx");
+    __asm__ __volatile__("push %%rbx ; cpuid ; rdtsc ; pop %%rbx" : "=A" (timeval) : : "ecx");
 
 #elif defined(MPL_LINUX86_CYCLE_CPUID_RDTSC32)
 /* Here we have to save the ebx register for when the compiler is
    generating position independent code (e.g., when it's generating
    shared libraries) */
-    __asm__ __volatile__("push %%ebx ; cpuid ; rdtsc ; pop %%ebx" : "=A" (*timeval) : : "ecx");
+    __asm__ __volatile__("push %%ebx ; cpuid ; rdtsc ; pop %%ebx" : "=A" (timeval) : : "ecx");
 
 #elif defined(MPL_LINUX86_CYCLE_RDTSC)
 /* The configure test using cpuid must have failed, try just rdtsc by itself */
-    __asm__ __volatile__("rdtsc" : "=A" (*timeval));
+    __asm__ __volatile__("rdtsc" : "=A" (timeval));
 
 #else
 #error Dont know which Linux timer to use
