802c218a6bb2c15d0765672379d63ab56ba3e581
Pavan Balaji <balaji@anl.gov>
2016-02-02 16:42:59 -0600

    sock: move utils to sock channel.
    These utils are not used anywhere and will not be used anywhere in the
    future, at least in the CH3 device.  It's highly unlikely they will be
    used in CH4, but if they are, we'll probably need to rewrite them to be
    closer to what we have with nemesis.
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpid/ch3/channels/sock/src/Makefile.mk
+++ src/mpid/ch3/channels/sock/src/Makefile.mk
@@ -12,6 +12,7 @@ errnames_txt_files += src/mpid/ch3/channels/sock/src/errnames.txt
 if BUILD_CH3_SOCK
 
 mpi_core_sources +=   \
+    src/mpid/ch3/channels/sock/src/sock.c \
     src/mpid/ch3/channels/sock/src/ch3_finalize.c    \
     src/mpid/ch3/channels/sock/src/ch3_init.c     \
     src/mpid/ch3/channels/sock/src/ch3_isend.c     \
@@ -20,6 +21,10 @@ mpi_core_sources +=   \
     src/mpid/ch3/channels/sock/src/ch3_istartmsgv.c    \
     src/mpid/ch3/channels/sock/src/ch3_progress.c       \
     src/mpid/ch3/channels/sock/src/ch3_win_fns.c
+
+noinst_HEADERS += \
+    src/mpid/ch3/channels/sock/include/mpidu_sock.h \
+    src/mpid/ch3/channels/sock/include/mpidu_socki.h
 
 endif BUILD_CH3_SOCK
 
