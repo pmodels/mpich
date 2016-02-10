8dced2e4176daf36f81d0263a4dbc42994b4ede2
Min Si <msi@il.is.s.u-tokyo.ac.jp>
2016-02-10 19:31:16 +0900

    Ensure alignment access for received packet header.
    Multiple packets may be contiguously stored in the internal receive
    buffer (i.e., TCP netmod), thus some packets may start from unaligned
    address. BUS error can happen when receiving progress accesses such
    packet header on alignment-sensitive platform (e.g., SPARC).
    
    This patch fixes this bug in three steps:
    (1) Check platform at configure time, enable NEEDS_STRICT_ALIGNMENT if
    it is SPARC (more platforms can be added in future);
    (2) For every received packet through netmod, copy the header into a
    local temporary buffer which is naturally aligned;
    (3) Pass the pointer to temporary header buffer to all packet handler
    functions as the packet header thus ensuring aligned accessing.
    
    Signed-off-by: Pavan Balaji <balaji@anl.gov>

--- src/mpid/ch3/channels/nemesis/src/ch3_progress.c
+++ src/mpid/ch3/channels/nemesis/src/ch3_progress.c
@@ -756,9 +756,21 @@ int MPID_nem_handle_pkt(MPIDI_VC_t *vc, char *buf, MPIDI_msg_sz_t buflen)
             {
                 MPIDI_msg_sz_t len;
                 MPIDI_CH3_Pkt_t *pkt = (MPIDI_CH3_Pkt_t *)buf;
+#ifdef NEEDS_STRICT_ALIGNMENT
+                MPIDI_CH3_Pkt_t aligned_buf;
+#endif
 
                 MPIU_DBG_MSG(CH3_CHANNEL, VERBOSE, "received new message");
 
+#ifdef NEEDS_STRICT_ALIGNMENT
+                /* Copy packet header to temporary buffer to ensure aligned access.
+                 * Note that we intentionally perform the extra copy on all platforms
+                 * that require strict alignment without checking whether the original
+                 * buffer is aligned. This is because we cannot find a good way to
+                 * get correct alignment for general structure on all platforms. */
+                MPIR_Memcpy(&aligned_buf, buf, sizeof(MPIDI_CH3_Pkt_t));
+                pkt = &aligned_buf;
+#endif
                 /* deduct packet header */
                 buflen -= sizeof(MPIDI_CH3_Pkt_t);
                 buf += sizeof(MPIDI_CH3_Pkt_t);
@@ -794,6 +806,9 @@ int MPID_nem_handle_pkt(MPIDI_VC_t *vc, char *buf, MPIDI_msg_sz_t buflen)
             MPIDI_msg_sz_t copylen;
             MPIDI_msg_sz_t datalen = 0;
             MPIDI_CH3_Pkt_t *pkt = (MPIDI_CH3_Pkt_t *)vc_ch->pending_pkt;
+
+            /* pending_pkt is allocated by malloc which guarantees the allocated
+             * space is suitably aligned for storage of any type of object. */
 
             MPIU_DBG_MSG(CH3_CHANNEL, VERBOSE, "received header fragment");
 
