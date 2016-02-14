7f697e13fb5729d8fa2e2ad09933be7edee3e680
Pavan Balaji <balaji@anl.gov>
2016-02-13 20:10:08 -0600

    Bug-fix: release RMA progress hook correctly.
    We should check for both active and inactive lists.  This was a
    bug causing the RMA progress hook to get released even when one of the
    lists was not NULL.
    
    Signed-off-by: Xin Zhao <xinzhao3@illinois.edu>

--- src/mpid/ch3/src/mpidi_rma.c
+++ src/mpid/ch3/src/mpidi_rma.c
@@ -193,7 +193,7 @@ int MPID_Win_free(MPID_Win ** win_ptr)
     MPIU_Assert((*win_ptr)->active == FALSE);
     MPL_DL_DELETE(MPIDI_RMA_Win_inactive_list_head, (*win_ptr));
 
-    if (MPIDI_RMA_Win_inactive_list_head == NULL && MPIDI_RMA_Win_inactive_list_head == NULL) {
+    if (MPIDI_RMA_Win_inactive_list_head == NULL && MPIDI_RMA_Win_active_list_head == NULL) {
         /* this is the last window, de-register RMA progress hook */
         mpi_errno = MPID_Progress_deregister_hook(MPIDI_CH3I_RMA_Progress_hook_id);
         if (mpi_errno != MPI_SUCCESS) {
