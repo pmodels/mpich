edc26f3feae67dcc88edc4f6c865d8547aa74775
Sangmin Seo <sseo@anl.gov>
2016-02-16 17:31:00 +0000

    Hydra-specific patches for hwloc.
    Applied [23821aa1487b] and [637cf86f6f7d].
    
    NOTE for dropped patches:
    - [5ce7102445fe]: hwloc-1.11.2 includes the same patch.
    - [23cbd2969684]: picked up a different patch in hwloc-1.11.2.
    
    Signed-off-by: Pavan Balaji <balaji@anl.gov>

--- src/pm/hydra/tools/topo/hwloc/hwloc/autogen.sh
+++ src/pm/hydra/tools/topo/hwloc/hwloc/autogen.sh
@@ -1,2 +1,9 @@
-:
-autoreconf ${autoreconf_args:-"-ivf"}
+#!/bin/sh
+
+if [ -n "$MPICH_AUTOTOOLS_DIR" ] ; then
+    autoreconf=${MPICH_AUTOTOOLS_DIR}/autoreconf
+else
+    autoreconf=${AUTORECONF:-autoreconf}
+fi
+
+$autoreconf ${autoreconf_args:-"-ivf"}
