79dc3d7b6d6d1f87801b0e76554c11bdffbd436a
Pavan Balaji <balaji@anl.gov>
2016-02-19 17:43:41 -0600

    maint: improvements to the git-conflicts script.
    Display probability of collision in the next year, which is a more
    "human understandable" metric than the (currently) low number for the
    next commit.
    
    No reviewer.

--- maint/git-conflicts.bash
+++ maint/git-conflicts.bash
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 max_abbrev=12
+scale=10
 
 total=`git rev-list --all | wc -l`
 
@@ -8,7 +9,7 @@ echo "Conflicts:"
 echo "Format:   [abbrev length]: number of conflicts / total number of refs (percentage conflicts)"
 for ((x = 1 ; x <= ${max_abbrev} ; x++)) ; do
     conflicts=`git rev-list --all --abbrev=$x --abbrev-commit | sort | uniq -D -w$x | wc -l`
-    percent=`echo "scale=8 ; 100 * $conflicts / $total" | bc | awk '{ printf("%1.2f\n", $0); }'`
+    percent=`echo "scale=$scale ; 100 * $conflicts / $total" | bc | awk '{ printf("%1.2f\n", $0); }'`
     echo "    [$x]: $conflicts / $total ($percent %)"
     if test "$conflicts" = "0" ; then break ; fi
 done
@@ -22,10 +23,22 @@ done
 ## but if it is online, it must be true, of course. ;-)
 ##
 
+min_abbrev=$x
+
 echo
 echo "Probability of collision on your next commit:"
 echo "Format:   [abbrev length]: probability of collision"
-for ((; x <= ${max_abbrev} ; x++)) ; do
-    percent=`echo "scale=8 ; 100 * ($total) / (16 ^ $x)" | bc | awk '{ printf("%1.4f\n", $0); }'`
+for ((x = ${min_abbrev}; x <= ${max_abbrev} ; x++)) ; do
+    percent=`echo "scale=$scale ; 100 * ($total) / (16 ^ $x)" | bc | awk '{ printf("%1.4f\n", $0); }'`
+    echo "    [$x]: $percent %"
+done
+
+commits_last_year=`git rev-list --all --since="1 year ago" | wc -l`
+
+echo
+echo "Probability of collision in the next year (based on commits last year):"
+echo "Format:   [abbrev length]: probability of collision"
+for ((x = ${min_abbrev}; x <= ${max_abbrev} ; x++)) ; do
+    percent=`echo "scale=$scale ; 100 * (1 - ((1 - ($total / (16 ^ $x))) ^ ${commits_last_year}))" | bc | awk '{ printf("%1.4f\n", $0); }'`
     echo "    [$x]: $percent %"
 done
