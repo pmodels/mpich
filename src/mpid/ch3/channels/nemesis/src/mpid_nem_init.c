hajimefu/mpich-mirror

mpid/common/shm: remove dependency on CH3
Moving MPID_nem_barrier and MPID_nem_seg from CH3 to MPIDU_shm. Revising
interface to make it work with CH3.

src/mpid/ch3/channels/nemesis/src/mpid_nem_init.c
@@ -167,7 +167,7 @@ MPID_nem_init(int pg_rank, MPIDI_PG_t *pg_p, int has_parent ATTRIBUTE((unused)))
 #endif /* MEM_REGION_IN_HEAP */
 
     MPID_nem_mem_region.num_seg        = 7;
-    MPIU_CHKPMEM_MALLOC (MPID_nem_mem_region.seg, MPID_nem_seg_info_ptr_t, MPID_nem_mem_region.num_seg * sizeof(MPID_nem_seg_info_t), mpi_errno, "mem_region segments");
+    MPIU_CHKPMEM_MALLOC (MPID_nem_mem_region.seg, MPIDU_shm_seg_info_ptr_t, MPID_nem_mem_region.num_seg * sizeof(MPIDU_shm_seg_info_t), mpi_errno, "mem_region segments");
     MPID_nem_mem_region.rank           = pg_rank;
     MPID_nem_mem_region.num_local      = num_local;
     MPID_nem_mem_region.num_procs      = num_procs;
@@ -260,7 +260,19 @@ MPID_nem_init(int pg_rank, MPIDI_PG_t *pg_p, int has_parent ATTRIBUTE((unused)))
     if (mpi_errno) MPIR_ERR_POP(mpi_errno);
 
     /* Actually allocate the segment and assign regions to the pointers */
-    mpi_errno = MPIDU_Seg_commit(&MPID_nem_mem_region.memory, num_local, local_rank);
+    mpi_errno = MPIDU_Seg_commit(&MPID_nem_mem_region.memory, &MPID_nem_mem_region.barrier,
+                                 num_local, local_rank, MPID_nem_mem_region.local_procs[0],
+                                 MPID_nem_mem_region.rank);
+    /* check_alloc steps */
+    if (MPID_nem_mem_region.memory.symmetrical == 1) {
+        MPID_nem_asymm_base_addr = NULL;
+    } else {
+        MPID_nem_asymm_base_addr = MPID_nem_mem_region.memory.base_addr;
+#ifdef MPID_NEM_SYMMETRIC_QUEUES
+        MPIR_ERR_INTERNALANDJUMP(mpi_errno, "queues are not symmetrically allocated as expected");
+#endif
+    }
+
     if (mpi_errno) MPIR_ERR_POP (mpi_errno);
 
     /* init shared collectives barrier region */
@@ -268,7 +280,7 @@ MPID_nem_init(int pg_rank, MPIDI_PG_t *pg_p, int has_parent ATTRIBUTE((unused)))
     if (mpi_errno) MPIR_ERR_POP (mpi_errno);
 
     /* local procs barrier */
-    mpi_errno = MPIDU_shm_barrier();
+    mpi_errno = MPIDU_shm_barrier(MPID_nem_mem_region.barrier, num_local);
     if (mpi_errno) MPIR_ERR_POP (mpi_errno);
 
     /* find our cell region */
@@ -343,7 +355,7 @@ MPID_nem_init(int pg_rank, MPIDI_PG_t *pg_p, int has_parent ATTRIBUTE((unused)))
 
     
     /* local barrier */
-    mpi_errno = MPIDU_shm_barrier();
+    mpi_errno = MPIDU_shm_barrier(MPID_nem_mem_region.barrier, num_local);
     if (mpi_errno) MPIR_ERR_POP(mpi_errno);
 
     
@@ -394,11 +406,11 @@ MPID_nem_init(int pg_rank, MPIDI_PG_t *pg_p, int has_parent ATTRIBUTE((unused)))
     MPL_free(publish_bc_orig);
 
 
-    mpi_errno = MPIDU_shm_barrier();
+    mpi_errno = MPIDU_shm_barrier(MPID_nem_mem_region.barrier, num_local);
     if (mpi_errno) MPIR_ERR_POP(mpi_errno);
     mpi_errno = MPID_nem_mpich_init();
     if (mpi_errno) MPIR_ERR_POP(mpi_errno);
-    mpi_errno = MPIDU_shm_barrier();
+    mpi_errno = MPIDU_shm_barrier(MPID_nem_mem_region.barrier, num_local);
     if (mpi_errno) MPIR_ERR_POP(mpi_errno);
 #ifdef ENABLE_CHECKPOINTING
     mpi_errno = MPIDI_nem_ckpt_init();

src/mpid/common/shm/mpidu_shm_barrier.c
@@ -4,8 +4,7 @@
  *      See COPYRIGHT in top-level directory.
  */
 
-#include "mpidimpl.h"
-#include "mpiu_os_wrappers.h"
+#include "mpidu_shm_impl.h"
 
 static int sense;
 static int barrier_init = 0;
@@ -14,22 +13,23 @@ static int barrier_init = 0;
 #define FUNCNAME MPIDU_shm_barrier_init
 #undef FCNAME
 #define FCNAME MPL_QUOTE(FUNCNAME)
-int MPIDU_shm_barrier_init(MPID_nem_barrier_t *barrier_region, int init_values)
+int MPIDU_shm_barrier_init(MPIDU_shm_barrier_t *barrier_region,
+                           MPIDU_shm_barrier_ptr_t *barrier, int init_values)
 {
-    MPIDI_STATE_DECL(MPID_STATE_MPID_NEM_BARRIER_INIT);
+    MPIDI_STATE_DECL(MPID_STATE_MPIDU_SHM_BARRIER_INIT);
 
-    MPIDI_FUNC_ENTER(MPID_STATE_MPID_NEM_BARRIER_INIT);
+    MPIDI_FUNC_ENTER(MPID_STATE_MPIDU_SHM_BARRIER_INIT);
     
-    MPID_nem_mem_region.barrier = barrier_region;
+    *barrier = barrier_region;
     if (init_values) {
-        OPA_store_int(&MPID_nem_mem_region.barrier->val, 0);
-        OPA_store_int(&MPID_nem_mem_region.barrier->wait, 0);
+        OPA_store_int(&(*barrier)->val, 0);
+        OPA_store_int(&(*barrier)->wait, 0);
         OPA_write_barrier();
     }
     sense = 0;
     barrier_init = 1;
 
-    MPIDI_FUNC_EXIT(MPID_STATE_MPID_NEM_BARRIER_INIT);
+    MPIDI_FUNC_EXIT(MPID_STATE_MPIDU_SHM_BARRIER_INIT);
 
     return MPI_SUCCESS;
 }
@@ -39,34 +39,34 @@ int MPIDU_shm_barrier_init(MPID_nem_barrier_t *barrier_region, int init_values)
 #undef FCNAME
 #define FCNAME MPL_QUOTE(FUNCNAME)
 /* FIXME: this is not a scalable algorithm because everyone is polling on the same cacheline */
-int MPIDU_shm_barrier(void)
+int MPIDU_shm_barrier(MPIDU_shm_barrier_t *barrier, int num_local)
 {
     int mpi_errno = MPI_SUCCESS;
-    MPIDI_STATE_DECL(MPID_STATE_MPID_NEM_BARRIER);
+    MPIDI_STATE_DECL(MPID_STATE_MPIDU_SHM_BARRIER);
 
-    MPIDI_FUNC_ENTER(MPID_STATE_MPID_NEM_BARRIER);
+    MPIDI_FUNC_ENTER(MPID_STATE_MPIDU_SHM_BARRIER);
 
-    if (MPID_nem_mem_region.num_local == 1)
+    if (num_local == 1)
         goto fn_exit;
 
     MPIR_ERR_CHKINTERNAL(!barrier_init, mpi_errno, "barrier not initialized");
 
-    if (OPA_fetch_and_incr_int(&MPID_nem_mem_region.barrier->val) == MPID_nem_mem_region.num_local - 1)
+    if (OPA_fetch_and_incr_int(&barrier->val) == num_local - 1)
     {
-	OPA_store_int(&MPID_nem_mem_region.barrier->val, 0);
-	OPA_store_int(&MPID_nem_mem_region.barrier->wait, 1 - sense);
+	OPA_store_int(&barrier->val, 0);
+	OPA_store_int(&barrier->wait, 1 - sense);
         OPA_write_barrier();
     }
     else
     {
 	/* wait */
-	while (OPA_load_int(&MPID_nem_mem_region.barrier->wait) == sense)
-            MPL_sched_yield(); /* skip */
+	while (OPA_load_int(&barrier->wait) == sense)
+            MPIU_PW_Sched_yield(); /* skip */
     }
     sense = 1 - sense;
 
  fn_fail:
  fn_exit:
-    MPIDI_FUNC_EXIT(MPID_STATE_MPID_NEM_BARRIER);
+    MPIDI_FUNC_EXIT(MPID_STATE_MPIDU_SHM_BARRIER);
     return mpi_errno;
 }

pmodels/mpich

src/mpid/ch3/channels/nemesis/src/mpid_nem_init.c
@@ -167,7 +167,7 @@ MPID_nem_init(int pg_rank, MPIDI_PG_t *pg_p, int has_parent ATTRIBUTE((unused)))
 #endif /* MEM_REGION_IN_HEAP */
 
     MPID_nem_mem_region.num_seg        = 7;
-    MPIR_CHKPMEM_MALLOC (MPID_nem_mem_region.seg, MPID_nem_seg_info_ptr_t, MPID_nem_mem_region.num_seg * sizeof(MPID_nem_seg_info_t), mpi_errno, "mem_region segments");
+    MPIR_CHKPMEM_MALLOC (MPID_nem_mem_region.seg, MPIDU_shm_seg_info_ptr_t, MPID_nem_mem_region.num_seg * sizeof(MPIDU_shm_seg_info_t), mpi_errno, "mem_region segments");
     MPID_nem_mem_region.rank           = pg_rank;
     MPID_nem_mem_region.num_local      = num_local;
     MPID_nem_mem_region.num_procs      = num_procs;
@@ -260,7 +260,19 @@ MPID_nem_init(int pg_rank, MPIDI_PG_t *pg_p, int has_parent ATTRIBUTE((unused)))
     if (mpi_errno) MPIR_ERR_POP(mpi_errno);
 
     /* Actually allocate the segment and assign regions to the pointers */
-    mpi_errno = MPIDU_Seg_commit(&MPID_nem_mem_region.memory, num_local, local_rank);
+    mpi_errno = MPIDU_Seg_commit(&MPID_nem_mem_region.memory, &MPID_nem_mem_region.barrier,
+                                 num_local, local_rank, MPID_nem_mem_region.local_procs[0],
+                                 MPID_nem_mem_region.rank);
+    /* check_alloc steps */
+    if (MPID_nem_mem_region.memory.symmetrical == 1) {
+        MPID_nem_asymm_base_addr = NULL;
+    } else {
+        MPID_nem_asymm_base_addr = MPID_nem_mem_region.memory.base_addr;
+#ifdef MPID_NEM_SYMMETRIC_QUEUES
+        MPIR_ERR_INTERNALANDJUMP(mpi_errno, "queues are not symmetrically allocated as expected");
+#endif
+    }
+
     if (mpi_errno) MPIR_ERR_POP (mpi_errno);
 
     /* init shared collectives barrier region */
@@ -268,7 +280,7 @@ MPID_nem_init(int pg_rank, MPIDI_PG_t *pg_p, int has_parent ATTRIBUTE((unused)))
     if (mpi_errno) MPIR_ERR_POP (mpi_errno);
 
     /* local procs barrier */
-    mpi_errno = MPIDU_shm_barrier();
+    mpi_errno = MPIDU_shm_barrier(MPID_nem_mem_region.barrier, num_local);
     if (mpi_errno) MPIR_ERR_POP (mpi_errno);
 
     /* find our cell region */
@@ -343,7 +355,7 @@ MPID_nem_init(int pg_rank, MPIDI_PG_t *pg_p, int has_parent ATTRIBUTE((unused)))
 
     
     /* local barrier */
-    mpi_errno = MPIDU_shm_barrier();
+    mpi_errno = MPIDU_shm_barrier(MPID_nem_mem_region.barrier, num_local);
     if (mpi_errno) MPIR_ERR_POP(mpi_errno);
 
     
@@ -394,11 +406,11 @@ MPID_nem_init(int pg_rank, MPIDI_PG_t *pg_p, int has_parent ATTRIBUTE((unused)))
     MPL_free(publish_bc_orig);
 
 
-    mpi_errno = MPIDU_shm_barrier();
+    mpi_errno = MPIDU_shm_barrier(MPID_nem_mem_region.barrier, num_local);
     if (mpi_errno) MPIR_ERR_POP(mpi_errno);
     mpi_errno = MPID_nem_mpich_init();
     if (mpi_errno) MPIR_ERR_POP(mpi_errno);
-    mpi_errno = MPIDU_shm_barrier();
+    mpi_errno = MPIDU_shm_barrier(MPID_nem_mem_region.barrier, num_local);
     if (mpi_errno) MPIR_ERR_POP(mpi_errno);
 #ifdef ENABLE_CHECKPOINTING
     mpi_errno = MPIDI_nem_ckpt_init();

src/mpid/common/shm/mpidu_shm_barrier.c
@@ -4,8 +4,7 @@
  *      See COPYRIGHT in top-level directory.
  */
 
-#include "mpidimpl.h"
-#include "mpiu_os_wrappers.h"
+#include "mpidu_shm_impl.h"
 
 static int sense;
 static int barrier_init = 0;
@@ -14,22 +13,23 @@ static int barrier_init = 0;
 #define FUNCNAME MPIDU_shm_barrier_init
 #undef FCNAME
 #define FCNAME MPL_QUOTE(FUNCNAME)
-int MPIDU_shm_barrier_init(MPID_nem_barrier_t *barrier_region, int init_values)
+int MPIDU_shm_barrier_init(MPIDU_shm_barrier_t *barrier_region,
+                           MPIDU_shm_barrier_ptr_t *barrier, int init_values)
 {
-    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPID_NEM_BARRIER_INIT);
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDU_SHM_BARRIER_INIT);
 
-    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPID_NEM_BARRIER_INIT);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDU_SHM_BARRIER_INIT);
     
-    MPID_nem_mem_region.barrier = barrier_region;
+    *barrier = barrier_region;
     if (init_values) {
-        OPA_store_int(&MPID_nem_mem_region.barrier->val, 0);
-        OPA_store_int(&MPID_nem_mem_region.barrier->wait, 0);
+        OPA_store_int(&(*barrier)->val, 0);
+        OPA_store_int(&(*barrier)->wait, 0);
         OPA_write_barrier();
     }
     sense = 0;
     barrier_init = 1;
 
-    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPID_NEM_BARRIER_INIT);
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDU_SHM_BARRIER_INIT);
 
     return MPI_SUCCESS;
 }
@@ -39,34 +39,34 @@ int MPIDU_shm_barrier_init(MPID_nem_barrier_t *barrier_region, int init_values)
 #undef FCNAME
 #define FCNAME MPL_QUOTE(FUNCNAME)
 /* FIXME: this is not a scalable algorithm because everyone is polling on the same cacheline */
-int MPIDU_shm_barrier(void)
+int MPIDU_shm_barrier(MPIDU_shm_barrier_t *barrier, int num_local)
 {
     int mpi_errno = MPI_SUCCESS;
-    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPID_NEM_BARRIER);
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDU_SHM_BARRIER);
 
-    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPID_NEM_BARRIER);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDU_SHM_BARRIER);
 
-    if (MPID_nem_mem_region.num_local == 1)
+    if (num_local == 1)
         goto fn_exit;
 
     MPIR_ERR_CHKINTERNAL(!barrier_init, mpi_errno, "barrier not initialized");
 
-    if (OPA_fetch_and_incr_int(&MPID_nem_mem_region.barrier->val) == MPID_nem_mem_region.num_local - 1)
+    if (OPA_fetch_and_incr_int(&barrier->val) == num_local - 1)
     {
-	OPA_store_int(&MPID_nem_mem_region.barrier->val, 0);
-	OPA_store_int(&MPID_nem_mem_region.barrier->wait, 1 - sense);
+	OPA_store_int(&barrier->val, 0);
+	OPA_store_int(&barrier->wait, 1 - sense);
         OPA_write_barrier();
     }
     else
     {
 	/* wait */
-	while (OPA_load_int(&MPID_nem_mem_region.barrier->wait) == sense)
-            MPL_sched_yield(); /* skip */
+	while (OPA_load_int(&barrier->wait) == sense)
+            MPIU_PW_Sched_yield(); /* skip */
     }
     sense = 1 - sense;
 
  fn_fail:
  fn_exit:
-    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPID_NEM_BARRIER);
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDU_SHM_BARRIER);
     return mpi_errno;
 }