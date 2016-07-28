29fb99df33bd82bc2f0d68d0c088af60ca556d7d
Charles J Archer <charles.j.archer@intel.com>
2016-07-28 16:56:22 -0700

    Symbol visibility
    This patch implements shared library best practice with regard to symbol
    visibility. MPICH is compiled to hide all function and variable symbols,
    except those decorated with MPICH_API_PUBLIC. Compiler support is tested
    for, and visibility disabled if not available. A configure flag,
    --enable-visibility, is added to control this option. Note that some
    symbols must be exposed from libmpi.so that are not obviously user
    visible, but are needed by the language binding libraries.
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/include/mpii_f77interface.h
+++ src/include/mpii_f77interface.h
@@ -10,13 +10,13 @@
 /* These functions are provided by the MPICH code for the Fortran interface,
    and provide the interfaces needed to keep track of which MPI internal
    objects need to have Fortran or Fortran 90 characteristics */
-void MPII_Keyval_set_fortran( int );
-void MPII_Keyval_set_fortran90( int );
-void MPII_Grequest_set_lang_f77( MPI_Request greq );
+MPICH_API_PUBLIC void MPII_Keyval_set_fortran( int );
+MPICH_API_PUBLIC void MPII_Keyval_set_fortran90( int );
+MPICH_API_PUBLIC void MPII_Grequest_set_lang_f77( MPI_Request greq );
 #if defined(HAVE_FORTRAN_BINDING) && !defined(HAVE_FINT_IS_INT)
-void MPII_Op_set_fc( MPI_Op );
+MPICH_API_PUBLIC void MPII_Op_set_fc( MPI_Op );
 typedef void (MPII_F77_User_function) ( void *, void *, MPI_Fint *, MPI_Fint * );
-void MPII_Errhandler_set_fc( MPI_Errhandler );
+MPICH_API_PUBLIC void MPII_Errhandler_set_fc( MPI_Errhandler );
 #endif
 
 #define MPII_ATTR_C_TO_FORTRAN(ATTR) ((ATTR)+1)
