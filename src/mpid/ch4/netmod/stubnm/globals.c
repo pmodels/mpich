d17a96a5c1cde4d2051000ebf4e059274cbb4c04
Wesley Bland <wesley.bland@intel.com>
2016-09-13 11:57:36 -0500

    CH4/STUBNM: Remove type_dup_hook pointer
    This pointer was accidentally re-inserted by `6370ba6ce119`, but it is
    no longer used and should be removed.
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpid/ch4/netmod/stubnm/globals.c
+++ src/mpid/ch4/netmod/stubnm/globals.c
@@ -142,7 +142,6 @@ MPIDI_NM_native_funcs_t MPIDI_NM_native_stubnm_funcs = {
     MPIDI_NM_mpi_iscatter,
     MPIDI_NM_mpi_iscatterv,
     MPIDI_NM_mpi_type_create_hook,
-    MPIDI_NM_mpi_type_dup_hook,
     MPIDI_NM_mpi_type_free_hook,
     MPIDI_NM_mpi_op_create_hook,
     MPIDI_NM_mpi_op_free_hook,
