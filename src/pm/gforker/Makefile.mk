bbce217f972d814daea190d45c4db72b89fdc75a
Ken Raffenetti <raffenet@mcs.anl.gov>
2016-10-20 14:12:46 -0500

    config: Cleanup MPL substitutions in Makefiles
    Use the MPL library determined by configure in all cases. Avoid
    cluttering up LDFLAGS in embedded mode.
    
    Signed-off-by: Pavan Balaji <balaji@anl.gov>

--- src/pm/gforker/Makefile.mk
+++ src/pm/gforker/Makefile.mk
@@ -16,7 +16,7 @@ if BUILD_PM_GFORKER
 if PRIMARY_PM_GFORKER
 bin_PROGRAMS += src/pm/gforker/mpiexec
 src_pm_gforker_mpiexec_SOURCES = src/pm/gforker/mpiexec.c 
-src_pm_gforker_mpiexec_LDADD = src/pm/util/libmpiexec.la -l$(MPLLIBNAME)
+src_pm_gforker_mpiexec_LDADD = src/pm/util/libmpiexec.la $(mpllib)
 src_pm_gforker_mpiexec_LDFLAGS = $(mpllibdir)
 EXTRA_src_pm_gforker_mpiexec_DEPENDENCIES = $(mpllib)
 # we may not want to add AM_CPPFLAGS for this program
@@ -24,7 +24,7 @@ src_pm_gforker_mpiexec_CPPFLAGS = $(common_pm_includes) $(AM_CPPFLAGS)
 else !PRIMARY_PM_GFORKER
 bin_PROGRAMS += src/pm/gforker/mpiexec.gforker
 src_pm_gforker_mpiexec_gforker_SOURCES = src/pm/gforker/mpiexec.c 
-src_pm_gforker_mpiexec_gforker_LDADD = src/pm/util/libmpiexec.la -l$(MPLLIBNAME)
+src_pm_gforker_mpiexec_gforker_LDADD = src/pm/util/libmpiexec.la $(mpllib)
 src_pm_gforker_mpiexec_gforker_LDFLAGS = $(mpllibdir)
 EXTRA_src_pm_gforker_mpiexec_gforker_DEPENDENCIES = $(mpllib)
 # we may not want to add AM_CPPFLAGS for this program
