52a7f6da0f436fefbe4d77100f68983e5f7eca08
Lena Oden <loden@anl.gov>
2016-11-01 16:41:32 +0000

    CH4/UCX: Avoid double-buffering with am_isendv
    Implement real am_isendv functionality.
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpid/ch4/netmod/ucx/ucx_am.h
+++ src/mpid/ch4/netmod/ucx/ucx_am.h
@@ -182,30 +182,80 @@ static inline int MPIDI_NM_am_isendv(int rank,
 {
     int mpi_errno = MPI_SUCCESS;
     size_t am_hdr_sz = 0, i;
-    char *am_hdr_buf;
+    MPIDI_UCX_ucp_request_t *ucp_request;
+    ucp_ep_h ep;
+    uint64_t ucx_tag;
+    char *send_buf;
+    size_t data_sz;
+    MPI_Aint dt_true_lb, last;
+    MPIR_Datatype *dt_ptr;
+    int dt_contig;
+    MPIDI_UCX_am_header_t ucx_hdr;
+
 
     MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_NM_AM_ISENDV);
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_NM_AM_ISENDV);
 
+    ep = MPIDI_UCX_COMM_TO_EP(comm, rank);
+    ucx_tag = MPIDI_UCX_init_tag(0, 0, MPIDI_UCX_AM_TAG);
+
+    MPIDI_Datatype_get_info(count, datatype, dt_contig, data_sz, dt_ptr, dt_true_lb);
     for (i = 0; i < iov_len; i++) {
         am_hdr_sz += am_hdr[i].iov_len;
     }
 
-    am_hdr_buf = (char *) MPL_malloc(am_hdr_sz);
-    MPIR_Assert(am_hdr_buf);
+    /* copy headers to send buffer */
+    send_buf = MPL_malloc(data_sz + am_hdr_sz + sizeof(ucx_hdr));
+    ucx_hdr.handler_id = handler_id;
+    ucx_hdr.data_sz = data_sz;
+    MPIR_Memcpy(send_buf, &ucx_hdr, sizeof(ucx_hdr));
     am_hdr_sz = 0;
-
     for (i = 0; i < iov_len; i++) {
-        MPIR_Memcpy(am_hdr_buf + am_hdr_sz, am_hdr[i].iov_base, am_hdr[i].iov_len);
+        MPIR_Memcpy(send_buf + am_hdr_sz + sizeof(ucx_hdr), am_hdr[i].iov_base, am_hdr[i].iov_len);
         am_hdr_sz += am_hdr[i].iov_len;
     }
 
-    mpi_errno = MPIDI_NM_am_isend(rank, comm, handler_id, am_hdr_buf, am_hdr_sz,
-                                  data, count, datatype, sreq, netmod_context);
+    if (dt_contig) {
+        MPIR_Memcpy(send_buf + am_hdr_sz + sizeof(ucx_hdr), (char *) data + dt_true_lb, data_sz);
+    } else {
+        size_t segment_first;
+        struct MPIDU_Segment *segment_ptr;
+        segment_ptr = MPIDU_Segment_alloc();
+        MPIR_ERR_CHKANDJUMP1(segment_ptr == NULL, mpi_errno,
+                             MPI_ERR_OTHER, "**nomem", "**nomem %s", "MPIDU_Segment_alloc");
+        MPIDU_Segment_init(data, count, datatype, segment_ptr, 0);
+        segment_first = 0;
+        last = data_sz;
+        MPIDU_Segment_pack(segment_ptr, segment_first, &last,
+                           send_buf + sizeof(ucx_hdr) + am_hdr_sz);
+        MPIDU_Segment_free(segment_ptr);
+    }
+    ucp_request = (MPIDI_UCX_ucp_request_t *) ucp_tag_send_nb(ep, send_buf,
+                                                              data_sz + am_hdr_sz + sizeof(ucx_hdr),
+                                                              ucp_dt_make_contig(1), ucx_tag,
+                                                              &MPIDI_UCX_am_isend_callback);
+    MPIDI_CH4_UCX_REQUEST(ucp_request);
 
-    MPL_free(am_hdr_buf);
+    /* send is done. free all resources and complete the request */
+    if (ucp_request == NULL) {
+        MPL_free(send_buf);
+        MPIDIG_global.origin_cbs[handler_id] (sreq);
+        goto fn_exit;
+    }
+
+    /* set the ch4r request inside the UCP request */
+    sreq->dev.ch4.am.netmod_am.ucx.pack_buffer = send_buf;
+    sreq->dev.ch4.am.netmod_am.ucx.handler_id = handler_id;
+    ucp_request->req = sreq;
+    ucp_request_release(ucp_request);
+
+fn_exit:
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_NM_AM_ISENDV);
     return mpi_errno;
+
+fn_fail:
+    goto fn_exit;
+
 }
 
 
