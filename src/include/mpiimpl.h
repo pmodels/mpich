25b116858f607f598ffc1449404354225d0b8834
Halim Amer <aamer@anl.gov>
2016-11-02 18:20:38 -0500

    Remove the 32B alignment for MPID_Request
    The MPID_Request structure is aligned to 32B and allocated through the
    request memory object pool. The alignment was introduced for
    potentially reducing false sharing in thread_multiple mode. Indirect
    blocks in the pool, however, are dynamically allocated without
    using aligned memory allocation. This does not guarantee 32B
    alignment and causes segfaults when touching requests in indirect
    blocks. To this point, there is no evidence of significant performance
    improvement with this alignment. Thus this patch drops this alignment.
    
    Fixes #2350
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/include/mpiimpl.h
+++ src/include/mpiimpl.h
@@ -1528,7 +1528,7 @@ typedef struct MPID_Request {
 #ifdef MPID_DEV_REQUEST_DECL
     MPID_DEV_REQUEST_DECL
 #endif
-} MPID_Request ATTRIBUTE((__aligned__(32)));
+} MPID_Request;
 
 extern MPIU_Object_alloc_t MPID_Request_mem;
 /* Preallocated request objects */
