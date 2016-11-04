248984a757a2fba601f53a7a5f942c69feb48873
Pavan Balaji <balaji@anl.gov>
2016-11-03 23:50:01 -0500

    Rename MPID_Request_free_hook to MPID_Request_destroy_hook.
    This hook is only called when the request is finally being destroyed,
    not for every free (which is only a ref-count decrement).

--- src/mpid/ch4/include/mpidpre.h
+++ src/mpid/ch4/include/mpidpre.h
@@ -196,7 +196,7 @@ typedef struct {
 
 MPL_STATIC_INLINE_PREFIX void MPID_Request_create_hook(struct MPIR_Request *req);
 
-MPL_STATIC_INLINE_PREFIX void MPID_Request_free_hook(struct MPIR_Request *req);
+MPL_STATIC_INLINE_PREFIX void MPID_Request_destroy_hook(struct MPIR_Request *req);
 
 typedef struct MPIDI_CH4U_win_shared_info {
     uint32_t disp_unit;
