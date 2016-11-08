ddfa8551839b279e16a79fecc540948a502b8965
Pavan Balaji <balaji@anl.gov>
2016-11-08 12:57:22 -0600

    Initial draft of the SHM glue layer.
    This layer is the first point of entry to the shmmods.  It uses the
    message information (e.g., message size) to decide on which shmmod to
    use.
    
    This commit comments out the glue layer since the shmmod namespacing
    needs to be resolved before it can be enabled.

---
+++ src/mpid/ch4/shm/src/Makefile.mk
@@ -0,0 +1,8 @@
+## -*- Mode: Makefile; -*-
+## vim: set ft=automake :
+##
+## (C) 2016 by Argonne National Laboratory.
+##     See COPYRIGHT in top-level directory.
+##
+
+noinst_HEADERS += src/mpid/ch4/shm/src/shm.h
