pmodels/mpich

commit c21c488f6b9e9c964fc9f6b6b57a564359bbfcce

CH4: Move callback registration into generic layer
Callback registration was originally defined by the netmod layer based
on an assumption that netmods may be able to do sophisticated active
message implementations using hardware support. However, that API
inhibited sharing of common code between network and shared memory
transports. For now, we move generic callback registration to a common
layer and punt more sophisticated schemes down the road.

Signed-off-by: Signed-off-by: Pavan Balaji <balaji@anl.gov>

src/mpid/ch4/generic/mpidig_globals.c
@@ -1,0 +1,15 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2006 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ *
+ *  Portions of this code were written by Intel Corporation.
+ *  Copyright (C) 2011-2016 Intel Corporation.  Intel provides this material
+ *  to Argonne National Laboratory subject to Software Grant and Corporate
+ *  Contributor License Agreement dated February 8, 2012.
+ */
+
+#include "mpidimpl.h"
+#include "mpidig.h"
+
+MPIDIG_global_t MPIDIG_global;

src/mpid/ch4/generic/Makefile.mk
@@ -13,4 +13,8 @@
 AM_CPPFLAGS += -I$(top_srcdir)/src/mpid/ch4/generic
 
 noinst_HEADERS += src/mpid/ch4/generic/mpidig_send.h \
-	src/mpid/ch4/generic/mpidig_recv.h
+                  src/mpid/ch4/generic/mpidig_recv.h \
+                  src/mpid/ch4/generic/mpidig.h
+
+mpi_core_sources += src/mpid/ch4/generic/mpidig_globals.c \
+                    src/mpid/ch4/generic/mpidig_init.c

src/mpid/ch4/generic/mpidig.h
@@ -1,0 +1,41 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2006 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ *
+ *  Portions of this code were written by Intel Corporation.
+ *  Copyright (C) 2011-2016 Intel Corporation.  Intel provides this material
+ *  to Argonne National Laboratory subject to Software Grant and Corporate
+ *  Contributor License Agreement dated February 8, 2012.
+ */
+#ifndef MPIDIG_TYPES_H_INCLUDED
+#define MPIDIG_TYPES_H_INCLUDED
+
+#include <mpidimpl.h>
+
+#define MPIDI_AM_HANDLERS_MAX (64)
+
+typedef int (*MPIDIG_am_target_cmpl_cb) (MPIR_Request * req);
+typedef int (*MPIDIG_am_origin_cb) (MPIR_Request * req);
+
+/* Callback function setup by handler register function */
+/* for short cases, output arguments are NULL */
+typedef int (*MPIDIG_am_target_msg_cb)
+ (int handler_id, void *am_hdr, void **data,    /* data should be iovs if *is_contig is false */
+  size_t * data_sz, int *is_contig, MPIDIG_am_target_cmpl_cb * target_cmpl_cb,        /* completion handler */
+  MPIR_Request ** req);         /* if allocated, need pointer to completion function */
+
+typedef struct MPIDIG_global_t {
+    MPIDIG_am_target_msg_cb target_msg_cbs[MPIDI_AM_HANDLERS_MAX];
+    MPIDIG_am_origin_cb origin_cbs[MPIDI_AM_HANDLERS_MAX];
+} MPIDIG_global_t;
+extern MPIDIG_global_t MPIDIG_global;
+
+int MPIDIG_am_reg_cb(int handler_id,
+                     MPIDIG_am_origin_cb origin_cb,
+                     MPIDIG_am_target_msg_cb target_msg_cb);
+int MPIDIG_init(MPIR_Comm * comm_world, MPIR_Comm * comm_self,
+                int num_contexts, void **netmod_contexts);
+void MPIDIG_finalize(void);
+
+#endif /* MPIDIG_TYPES_H_INCLUDED */

src/mpid/ch4/generic/mpidig_init.c
@@ -1,0 +1,259 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2016 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ *
+ *  Portions of this code were written by Intel Corporation.
+ *  Copyright (C) 2011-2016 Intel Corporation.  Intel provides this material
+ *  to Argonne National Laboratory subject to Software Grant and Corporate
+ *  Contributor License Agreement dated February 8, 2012.
+ */
+
+#include "mpidimpl.h"
+#include "mpidig.h"
+
+#undef FUNCNAME
+#define FUNCNAME MPIDIG_am_reg_cb
+#undef FCNAME
+#define FCNAME MPL_QUOTE(FUNCNAME)
+int MPIDIG_am_reg_cb(int handler_id,
+                     MPIDIG_am_origin_cb origin_cb, MPIDIG_am_target_msg_cb target_msg_cb)
+{
+    int mpi_errno = MPI_SUCCESS;
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_NETMOD_REG_HDR_HANDLER);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_NETMOD_REG_HDR_HANDLER);
+
+    MPIDIG_global.target_msg_cbs[handler_id] = target_msg_cb;
+    MPIDIG_global.origin_cbs[handler_id] = origin_cb;
+  fn_exit:
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_NETMOD_REG_HDR_HANDLER);
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
+}
+
+#undef FUNCNAME
+#define FUNCNAME MPIDIG_init
+#undef FCNAME
+#define FCNAME MPL_QUOTE(FUNCNAME)
+int MPIDIG_init(MPIR_Comm * comm_world, MPIR_Comm * comm_self,
+                int num_contexts, void **netmod_contexts)
+{
+    int mpi_errno = MPI_SUCCESS;
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDIG_INIT);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDIG_INIT);
+
+    MPIDI_CH4_Global.is_ch4u_initialized = 0;
+
+    MPIDI_CH4_Global.comm_req_lists = (MPIDI_CH4U_comm_req_list_t *)
+        MPL_calloc(MPIR_MAX_CONTEXT_MASK * MPIR_CONTEXT_INT_BITS,
+                   sizeof(MPIDI_CH4U_comm_req_list_t));
+#ifndef MPIDI_CH4U_USE_PER_COMM_QUEUE
+    MPIDI_CH4_Global.posted_list = NULL;
+    MPIDI_CH4_Global.unexp_list = NULL;
+#endif
+
+    MPIDI_CH4_Global.cmpl_list = NULL;
+    OPA_store_int(&MPIDI_CH4_Global.exp_seq_no, 0);
+    OPA_store_int(&MPIDI_CH4_Global.nxt_seq_no, 0);
+
+    MPIDI_CH4_Global.buf_pool = MPIDI_CH4U_create_buf_pool(MPIDI_CH4I_BUF_POOL_NUM,
+                                                           MPIDI_CH4I_BUF_POOL_SZ);
+    MPIR_Assert(MPIDI_CH4_Global.buf_pool);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_SEND, &MPIDI_send_origin_cb, &MPIDI_send_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_SEND_LONG_REQ, NULL /* Injection only */ ,
+                                 &MPIDI_send_long_req_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_SEND_LONG_ACK, NULL /* Injection only */ ,
+                                 &MPIDI_send_long_ack_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_SEND_LONG_LMT,
+                                 &MPIDI_send_long_lmt_origin_cb,
+                                 &MPIDI_send_long_lmt_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_SSEND_REQ,
+                                 &MPIDI_send_origin_cb, &MPIDI_ssend_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_SSEND_ACK,
+                                 &MPIDI_ssend_ack_origin_cb, &MPIDI_ssend_ack_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno =
+        MPIDIG_am_reg_cb(MPIDI_CH4U_PUT_REQ, &MPIDI_put_origin_cb, &MPIDI_put_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_PUT_ACK, NULL, &MPIDI_put_ack_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno =
+        MPIDIG_am_reg_cb(MPIDI_CH4U_GET_REQ, &MPIDI_get_origin_cb, &MPIDI_get_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_GET_ACK,
+                                 &MPIDI_get_ack_origin_cb, &MPIDI_get_ack_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_CSWAP_REQ,
+                                 &MPIDI_cswap_origin_cb, &MPIDI_cswap_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_CSWAP_ACK,
+                                 &MPIDI_cswap_ack_origin_cb, &MPIDI_cswap_ack_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno =
+        MPIDIG_am_reg_cb(MPIDI_CH4U_ACC_REQ, &MPIDI_acc_origin_cb, &MPIDI_acc_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_GET_ACC_REQ,
+                                 &MPIDI_get_acc_origin_cb, &MPIDI_get_acc_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_ACC_ACK, NULL, &MPIDI_acc_ack_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_GET_ACC_ACK,
+                                 &MPIDI_get_acc_ack_origin_cb, &MPIDI_get_acc_ack_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_WIN_COMPLETE, NULL, &MPIDI_win_ctrl_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_WIN_POST, NULL, &MPIDI_win_ctrl_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_WIN_LOCK, NULL, &MPIDI_win_ctrl_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_WIN_LOCK_ACK, NULL, &MPIDI_win_ctrl_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_WIN_UNLOCK, NULL, &MPIDI_win_ctrl_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_WIN_UNLOCK_ACK, NULL, &MPIDI_win_ctrl_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_WIN_LOCKALL, NULL, &MPIDI_win_ctrl_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_WIN_LOCKALL_ACK, NULL, &MPIDI_win_ctrl_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_WIN_UNLOCKALL, NULL, &MPIDI_win_ctrl_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_WIN_UNLOCKALL_ACK, NULL, &MPIDI_win_ctrl_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_PUT_IOV_REQ,
+                                 &MPIDI_put_iov_origin_cb, &MPIDI_put_iov_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_PUT_IOV_ACK, NULL, &MPIDI_put_iov_ack_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_PUT_DAT_REQ,
+                                 &MPIDI_put_data_origin_cb, &MPIDI_put_data_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_ACC_IOV_REQ,
+                                 &MPIDI_acc_iov_origin_cb, &MPIDI_acc_iov_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_GET_ACC_IOV_REQ,
+                                 &MPIDI_get_acc_iov_origin_cb, &MPIDI_get_acc_iov_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_ACC_IOV_ACK, NULL, &MPIDI_acc_iov_ack_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_GET_ACC_IOV_ACK,
+                                 NULL, &MPIDI_get_acc_iov_ack_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_ACC_DAT_REQ,
+                                 &MPIDI_acc_data_origin_cb, &MPIDI_acc_data_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDIG_am_reg_cb(MPIDI_CH4U_GET_ACC_DAT_REQ,
+                                 &MPIDI_get_acc_data_origin_cb, &MPIDI_get_acc_data_target_msg_cb);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDI_CH4U_init_comm(comm_world);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    mpi_errno = MPIDI_CH4U_init_comm(comm_self);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    MPIDI_CH4_Global.win_hash = NULL;
+
+    MPIDI_CH4_Global.is_ch4u_initialized = 1;
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDIG_INIT);
+
+  fn_exit:
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
+}
+
+#undef FUNCNAME
+#define FUNCNAME MPIDIG_finalize
+#undef FCNAME
+#define FCNAME MPL_QUOTE(FUNCNAME)
+void MPIDIG_finalize(void)
+{
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDIG_FINALIZE);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDIG_FINALIZE);
+
+    MPIDI_CH4_Global.is_ch4u_initialized = 0;
+    MPL_HASH_CLEAR(dev.ch4u.hash_handle, MPIDI_CH4_Global.win_hash);
+    MPIDI_CH4R_destroy_buf_pool(MPIDI_CH4_Global.buf_pool);
+    MPL_free(MPIDI_CH4_Global.comm_req_lists);
+
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDIG_FINALIZE);
+}

src/mpid/ch4/netmod/include/netmod.h
@@ -16,24 +16,11 @@
 
 #define MPIDI_MAX_NETMOD_STRING_LEN 64
 
-typedef int (*MPIDI_NM_am_target_cmpl_cb) (MPIR_Request * req);
-typedef int (*MPIDI_NM_am_origin_cb) (MPIR_Request * req);
-
-/* Callback function setup by handler register function */
-/* for short cases, output arguments are NULL */
-typedef int (*MPIDI_NM_am_target_msg_cb)
- (int handler_id, void *am_hdr, void **data,    /* data should be iovs if *is_contig is false */
-  size_t * data_sz, int *is_contig, MPIDI_NM_am_target_cmpl_cb * target_cmpl_cb,        /* completion handler */
-  MPIR_Request ** req);         /* if allocated, need pointer to completion function */
-
 typedef int (*MPIDI_NM_mpi_init_t) (int rank, int size, int appnum, int *tag_ub,
                                     MPIR_Comm * comm_world, MPIR_Comm * comm_self, int spawned,
                                     int num_contexts, void **netmod_contexts);
 typedef int (*MPIDI_NM_mpi_finalize_t) (void);
 typedef int (*MPIDI_NM_progress_t) (void *netmod_context, int blocking);
-typedef int (*MPIDI_NM_am_reg_cb_t) (int handler_id,
-                                     MPIDI_NM_am_origin_cb origin_cb,
-                                     MPIDI_NM_am_target_msg_cb target_msg_cb);
 typedef int (*MPIDI_NM_mpi_comm_connect_t) (const char *port_name, MPIR_Info * info, int root,
                                             MPIR_Comm * comm, MPIR_Comm ** newcomm_ptr);
 typedef int (*MPIDI_NM_mpi_comm_disconnect_t) (MPIR_Comm * comm_ptr);
@@ -377,7 +364,6 @@ typedef struct MPIDI_NM_funcs {
     MPIDI_NM_am_request_init_t am_request_init;
     MPIDI_NM_am_request_finalize_t am_request_finalize;
     /* Active Message Routines */
-    MPIDI_NM_am_reg_cb_t am_reg_cb;
     MPIDI_NM_am_send_hdr_t am_send_hdr;
     MPIDI_NM_am_isend_t am_isend;
     MPIDI_NM_am_isendv_t am_isendv;
@@ -511,11 +497,6 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_NM_mpi_init_hook(int rank, int size, int appn
 MPL_STATIC_INLINE_PREFIX int MPIDI_NM_mpi_finalize_hook(void) MPL_STATIC_INLINE_SUFFIX;
 MPL_STATIC_INLINE_PREFIX int MPIDI_NM_progress(void *netmod_context,
                                                int blocking) MPL_STATIC_INLINE_SUFFIX;
-MPL_STATIC_INLINE_PREFIX int MPIDI_NM_am_reg_cb(int handler_id,
-                                                MPIDI_NM_am_origin_cb
-                                                origin_cb,
-                                                MPIDI_NM_am_target_msg_cb
-                                                target_msg_cb) MPL_STATIC_INLINE_SUFFIX;
 MPL_STATIC_INLINE_PREFIX int MPIDI_NM_mpi_comm_connect(const char *port_name, MPIR_Info * info,
                                                        int root, MPIR_Comm * comm,
                                                        MPIR_Comm **

src/mpid/ch4/netmod/include/netmod_impl.h
@@ -35,13 +35,6 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_NM_progress(void *netmod_context, int blockin
     return MPIDI_NM_func->progress(netmod_context, blocking);
 };
 
-MPL_STATIC_INLINE_PREFIX int MPIDI_NM_am_reg_cb(int handler_id,
-                                                MPIDI_NM_am_origin_cb
-                                                origin_cb, MPIDI_NM_am_target_msg_cb target_msg_cb)
-{
-    return MPIDI_NM_func->am_reg_cb(handler_id, origin_cb, target_msg_cb);
-};
-
 MPL_STATIC_INLINE_PREFIX int MPIDI_NM_mpi_comm_connect(const char *port_name, MPIR_Info * info,
                                                        int root, MPIR_Comm * comm,
                                                        MPIR_Comm ** newcomm_ptr)

src/mpid/ch4/netmod/ofi/ofi_am.h
@@ -27,32 +27,6 @@ static inline void MPIDI_NM_am_request_finalize(MPIR_Request * req)
 }
 
 #undef FUNCNAME
-#define FUNCNAME MPIDI_NM_am_reg_cb
-#undef FCNAME
-#define FCNAME MPL_QUOTE(FUNCNAME)
-static inline int MPIDI_NM_am_reg_cb(int handler_id,
-                                     MPIDI_NM_am_origin_cb origin_cb,
-                                     MPIDI_NM_am_target_msg_cb target_msg_cb)
-{
-    int mpi_errno = MPI_SUCCESS;
-    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_NETMOD_REG_HDR_HANDLER);
-    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_NETMOD_REG_HDR_HANDLER);
-
-    if (handler_id > MPIDI_OFI_MAX_AM_HANDLERS) {
-        mpi_errno = MPI_ERR_OTHER;
-        goto fn_fail;
-    }
-
-    MPIDI_Global.target_msg_cbs[handler_id] = target_msg_cb;
-    MPIDI_Global.origin_cbs[handler_id] = origin_cb;
-  fn_exit:
-    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_NETMOD_REG_HDR_HANDLER);
-    return mpi_errno;
-  fn_fail:
-    goto fn_exit;
-}
-
-#undef FUNCNAME
 #define FUNCNAME MPIDI_NM_am_isend
 #undef FCNAME
 #define FCNAME MPL_QUOTE(FUNCNAME)

src/mpid/ch4/netmod/ofi/ofi_am_events.h
@@ -25,7 +25,7 @@ static inline int MPIDI_OFI_handle_short_am(MPIDI_OFI_am_header_t * msg_hdr)
     void *in_data;
 
     size_t data_sz, in_data_sz;
-    MPIDI_NM_am_target_cmpl_cb target_cmpl_cb;
+    MPIDIG_am_target_cmpl_cb target_cmpl_cb;
     struct iovec *iov;
     int i, is_contig, iov_len;
     size_t done, curr_len, rem;
@@ -36,9 +36,9 @@ static inline int MPIDI_OFI_handle_short_am(MPIDI_OFI_am_header_t * msg_hdr)
     p_data = in_data = (char *) msg_hdr->payload + msg_hdr->am_hdr_sz;
     in_data_sz = data_sz = msg_hdr->data_sz;
 
-    MPIDI_Global.target_msg_cbs[msg_hdr->handler_id] (msg_hdr->handler_id, msg_hdr->payload,
-                                                      &p_data, &data_sz, &is_contig,
-                                                      &target_cmpl_cb, &rreq);
+    MPIDIG_global.target_msg_cbs[msg_hdr->handler_id] (msg_hdr->handler_id, msg_hdr->payload,
+                                                       &p_data, &data_sz,
+                                                       &is_contig, &target_cmpl_cb, &rreq);
 
     if (!rreq)
         goto fn_exit;
@@ -101,13 +101,13 @@ static inline int MPIDI_OFI_handle_short_am_hdr(MPIDI_OFI_am_header_t * msg_hdr,
 {
     int mpi_errno = MPI_SUCCESS;
     MPIR_Request *rreq = NULL;
-    MPIDI_NM_am_target_cmpl_cb target_cmpl_cb = NULL;
+    MPIDIG_am_target_cmpl_cb target_cmpl_cb = NULL;
 
     MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_OFI_HANDLE_SHORT_AM_HDR);
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_OFI_HANDLE_SHORT_AM_HDR);
 
-    MPIDI_Global.target_msg_cbs[msg_hdr->handler_id] (msg_hdr->handler_id, am_hdr,
-                                                      NULL, NULL, NULL, &target_cmpl_cb, &rreq);
+    MPIDIG_global.target_msg_cbs[msg_hdr->handler_id] (msg_hdr->handler_id, am_hdr,
+                                                       NULL, NULL, NULL, &target_cmpl_cb, &rreq);
 
     if (!rreq)
         goto fn_exit;
@@ -218,16 +218,16 @@ static inline int MPIDI_OFI_do_handle_long_am(MPIDI_OFI_am_header_t * msg_hdr,
     MPIR_Request *rreq;
     void *p_data;
     size_t data_sz, rem, done, curr_len, in_data_sz;
-    MPIDI_NM_am_target_cmpl_cb target_cmpl_cb;
+    MPIDIG_am_target_cmpl_cb target_cmpl_cb;
     struct iovec *iov;
 
     MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_OFI_DO_HANDLE_LONG_AM);
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_OFI_DO_HANDLE_LONG_AM);
 
     in_data_sz = data_sz = msg_hdr->data_sz;
-    MPIDI_Global.target_msg_cbs[msg_hdr->handler_id] (msg_hdr->handler_id, am_hdr,
-                                                      &p_data, &data_sz, &is_contig,
-                                                      &target_cmpl_cb, &rreq);
+    MPIDIG_global.target_msg_cbs[msg_hdr->handler_id] (msg_hdr->handler_id, am_hdr,
+                                                       &p_data, &data_sz, &is_contig,
+                                                       &target_cmpl_cb, &rreq);
 
     if (!rreq)
         goto fn_exit;
@@ -367,7 +367,7 @@ static inline int MPIDI_OFI_handle_lmt_ack(MPIDI_OFI_am_header_t * msg_hdr)
 
     handler_id = MPIDI_OFI_AMREQUEST_HDR(sreq, msg_hdr).handler_id;
     MPIDI_OFI_am_request_complete(sreq);
-    mpi_errno = MPIDI_Global.origin_cbs[handler_id] (sreq);
+    mpi_errno = MPIDIG_global.origin_cbs[handler_id] (sreq);
 
     if (mpi_errno)
         MPIR_ERR_POP(mpi_errno);

commit 3ebb53fdd57be8c14bed32dd2847facc2808a808

CH4/generic: Initialize global struct
Builds with weak symbol support disabled broke with commit
[c21c488f6b9e]. Initializing the generic global struct makes linking
MPI/PMPI statically happy again in these configurations.

No reviewer.

src/mpid/ch4/generic/mpidig_globals.c
@@ -12,4 +12,4 @@
 #include "mpidimpl.h"
 #include "mpidig.h"
 
-MPIDIG_global_t MPIDIG_global;
+MPIDIG_global_t MPIDIG_global = { 0 };