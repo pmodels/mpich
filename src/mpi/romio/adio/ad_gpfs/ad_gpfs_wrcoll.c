61a1fd5df595d345a4fdfec867ee1bb9d2f64b72
Paul Coffman <pcoffman@anl.gov>
2016-11-18 17:32:10 +0000

    ROMIO/BGQ: Remove MPIX calls
    Without PAMID the MPIX calls for BGQ partition info within ROMIO for
    aggregator placement will not be defined.  Replace these calls with
    SPI and functional equivalents, and remove unneeded info that MPIX
    was also gathering.
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpi/romio/adio/ad_gpfs/ad_gpfs_wrcoll.c
+++ src/mpi/romio/adio/ad_gpfs/ad_gpfs_wrcoll.c
@@ -18,7 +18,7 @@
 #include "ad_gpfs_aggrs.h"
 
 #ifdef BGQPLATFORM
-#include <mpix.h>
+#include "bg/ad_bg_pset.h"
 #endif
 
 #ifdef AGGREGATION_PROFILE
@@ -565,7 +565,7 @@ static int gpfs_find_access_for_ion(ADIO_File fd,
 	ADIO_Offset *fd_start, ADIO_Offset *fd_end,
 	ADIO_Offset *start, ADIO_Offset *end)
 {
-    int my_ionode = MPIX_IO_node_id();
+    int my_ionode = BGQ_IO_node_id();
     int *rank_to_ionode;
     int i, nprocs, rank;
     ADIO_Offset group_start=LLONG_MAX, group_end=0;
