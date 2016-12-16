13a516c41109f0aaf9b720a93f7e7330fd1e74ac
Ken Raffenetti <raffenet@mcs.anl.gov>
2016-12-16 14:00:15 -0600

    CH4/UCX: Fix improbe/imrecv
    Fix remaining issues with the improbe/imrecv functionality in the UCX
    netmod after refactoring.
    
     1. Eliminate extra request. Convert already allocated MPROBE request
        to RECV request and give back to upper layer instead.
     2. Implement new mrecv completion callback. Because we already have
        our request allocated, we need different logic.
    
    Signed-off-by: Lena Oden <loden@anl.gov>

--- src/mpid/ch4/netmod/ucx/ucx_recv.h
+++ src/mpid/ch4/netmod/ucx/ucx_recv.h
@@ -51,6 +51,44 @@ static inline void MPIDI_UCX_recv_cmpl_cb(void *request, ucs_status_t status,
 }
 
 #undef FUNCNAME
+#define FUNCNAME MPIDI_UCX_mrecv_cmpl_cb
+#undef FCNAME
+#define FCNAME MPL_QUOTE(FUNCNAME)
+static inline void MPIDI_UCX_mrecv_cmpl_cb(void *request, ucs_status_t status,
+                                           ucp_tag_recv_info_t * info)
+{
+    MPIDI_UCX_ucp_request_t *ucp_request = (MPIDI_UCX_ucp_request_t *) request;
+
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_UCX_MRECV_CMPL_CB);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_UCX_MRECV_CMPL_CB);
+
+    if (ucp_request->req) {
+        MPIR_Request *rreq = ucp_request->req;
+        MPIDI_CH4U_request_complete(rreq);
+        ucp_request->req = NULL;
+        ucp_request_release(ucp_request);
+
+        if (unlikely(status == UCS_ERR_MESSAGE_TRUNCATED)) {
+            rreq->status.MPI_ERROR = MPI_ERR_TRUNCATE;
+        } else {
+            rreq->status.MPI_ERROR = MPI_SUCCESS;
+            rreq->status.MPI_SOURCE = MPIDI_UCX_get_source(info->sender_tag);
+            rreq->status.MPI_TAG = MPIDI_UCX_get_tag(info->sender_tag);
+            MPIR_STATUS_SET_COUNT(rreq->status, info->length);
+        }
+    } else {
+        if (unlikely(status == UCS_ERR_MESSAGE_TRUNCATED)) {
+            ucp_request->req = (void *)UCS_ERR_MESSAGE_TRUNCATED;
+        } else {
+            ucp_request->req = MPL_malloc(sizeof(ucp_tag_recv_info_t));
+            memcpy(ucp_request->req, info, sizeof(ucp_tag_recv_info_t));
+        }
+    }
+
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_UCX_MRECV_CMPL_CB);
+}
+
+#undef FUNCNAME
 #define FUNCNAME MPIDI_UCX_recv
 #undef FCNAME
 #define FCNAME MPL_QUOTE(FUNCNAME)
@@ -123,7 +161,6 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_NM_mpi_imrecv(void *buf,
     int mpi_errno = MPI_SUCCESS;
     size_t data_sz;
     int dt_contig;
-    MPIR_Request *req;
     MPI_Aint dt_true_lb;
     MPIDI_UCX_ucp_request_t *ucp_request;
     MPIR_Datatype *dt_ptr;
@@ -136,7 +173,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_NM_mpi_imrecv(void *buf,
                                                            data_sz,
                                                            ucp_dt_make_contig(1),
                                                            MPIDI_UCX_REQ(message).a.message_handler,
-                                                           &MPIDI_UCX_recv_cmpl_cb);
+                                                           &MPIDI_UCX_mrecv_cmpl_cb);
     } else {
         MPIDU_Datatype_add_ref(dt_ptr);
         ucp_request =
@@ -144,20 +181,30 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_NM_mpi_imrecv(void *buf,
                                                            buf, count,
                                                            dt_ptr->dev.netmod.ucx.ucp_datatype,
                                                            MPIDI_UCX_REQ(message).a.message_handler,
-                                                           &MPIDI_UCX_recv_cmpl_cb);
+                                                           &MPIDI_UCX_mrecv_cmpl_cb);
     }
     MPIDI_CH4_UCX_REQUEST(ucp_request);
 
     if (ucp_request->req) {
-        req = ucp_request->req;
+        if (unlikely((ucs_status_t)ucp_request->req == UCS_ERR_MESSAGE_TRUNCATED)) {
+            message->status.MPI_ERROR = MPI_ERR_TRUNCATE;
+        } else {
+            ucp_tag_recv_info_t *info = ucp_request->req;
+            message->status.MPI_ERROR = MPI_SUCCESS;
+            message->status.MPI_SOURCE = MPIDI_UCX_get_source(info->sender_tag);
+            message->status.MPI_TAG = MPIDI_UCX_get_tag(info->sender_tag);
+            MPIR_STATUS_SET_COUNT(message->status, info->length);
+            MPL_free(ucp_request->req);
+        }
+        MPIDI_CH4U_request_complete(message);
         ucp_request->req = NULL;
+        ucp_request_release(ucp_request);
     } else {
-        req = MPIR_Request_create(MPIR_REQUEST_KIND__RECV);
-        MPIR_Request_add_ref(req);
-        MPIDI_UCX_REQ(req).a.ucp_request = ucp_request;
-        ucp_request->req = req;
+        MPIDI_UCX_REQ(message).a.ucp_request = ucp_request;
+        ucp_request->req = message;
     }
-    *rreqp = req;
+    message->kind = MPIR_REQUEST_KIND__RECV;
+    *rreqp = message;
 
  fn_exit:
     return mpi_errno;
