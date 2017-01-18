6db5cbf1f18f491d978dbd4f404f618576f1f581
Pavan Balaji <balaji@anl.gov>
2017-01-18 16:00:28 +0900

    MPID_Abort fixes.
    The previous fixes to MPID_Abort were causing it to fail to build on a
    mac.  This patch allows MPID_Abort to be defined in a C file, with a
    prototype in the overall header to allow all files to access it
    correctly.
    
    This fix is not yet complete.  The build still fails inexplicably if
    the MPID_Abort is moved to a different file.  But at least it gets the
    build going for now.
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpid/ch4/src/ch4_init.h
+++ src/mpid/ch4/src/ch4_init.h
@@ -450,50 +450,6 @@ MPL_STATIC_INLINE_PREFIX int MPID_Get_processor_name(char *name, int namelen, in
 }
 
 #undef FUNCNAME
-#define FUNCNAME MPID_Abort
-#undef FCNAME
-#define FCNAME MPL_QUOTE(FUNCNAME)
-MPL_STATIC_INLINE_PREFIX int MPID_Abort(MPIR_Comm * comm,
-                                         int mpi_errno, int exit_code, const char *error_msg)
-{
-    char sys_str[MPI_MAX_ERROR_STRING + 5] = "";
-    char comm_str[MPI_MAX_ERROR_STRING] = "";
-    char world_str[MPI_MAX_ERROR_STRING] = "";
-    char error_str[2 * MPI_MAX_ERROR_STRING + 128];
-    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPID_ABORT);
-    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPID_ABORT);
-
-    if (MPIR_Process.comm_world) {
-        int rank = MPIR_Process.comm_world->rank;
-        snprintf(world_str, sizeof(world_str), " on node %d", rank);
-    }
-
-    if (comm) {
-        int rank = comm->rank;
-        int context_id = comm->context_id;
-        snprintf(comm_str, sizeof(comm_str), " (rank %d in comm %d)", rank, context_id);
-    }
-
-    if (!error_msg)
-        error_msg = "Internal error";
-
-    if (mpi_errno != MPI_SUCCESS) {
-        char msg[MPI_MAX_ERROR_STRING] = "";
-        MPIR_Err_get_string(mpi_errno, msg, MPI_MAX_ERROR_STRING, NULL);
-        snprintf(sys_str, sizeof(msg), " (%s)", msg);
-    }
-    MPL_snprintf(error_str, sizeof(error_str), "Abort(%d)%s%s: %s%s\n",
-                 exit_code, world_str, comm_str, error_msg, sys_str);
-    MPL_error_printf("%s", error_str);
-
-    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPID_ABORT);
-    fflush(stderr);
-    fflush(stdout);
-    PMI_Abort(exit_code, error_msg);
-    return 0;
-}
-
-#undef FUNCNAME
 #define FUNCNAME MPID_Alloc_mem
 #undef FCNAME
 #define FCNAME MPL_QUOTE(FUNCNAME)
