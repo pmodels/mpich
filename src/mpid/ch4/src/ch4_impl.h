84e1dcd43b4cf9b0dcc0e8ae5ec16e379954117a
Halim Amer <aamer@anl.gov>
2017-02-08 15:33:38 +0000

    endpoints: use only ep 0 for now
    The hash functions are broken. They at least do not take into account
    wild cards with possibly other issues. Ignore them for now and proper hash
    functions will be developed later

--- src/mpid/ch4/src/ch4_impl.h
+++ src/mpid/ch4/src/ch4_impl.h
@@ -743,13 +743,21 @@ static inline void MPIDI_win_check_group_local_completed(MPIR_Win * win,
 
 static inline void MPIDI_find_tag_ep(MPIR_Comm* comm, int target_rank, int tag, int* ep_idx)
 {
+#if 0
     *ep_idx = ((comm->context_id + target_rank + tag) % MPIDI_CH4_Global.n_netmod_eps) & INT_MAX;
+#else
+    *ep_idx = 0;
+#endif
     MPIR_Assert(*ep_idx >= 0);
 }
 
 static inline void MPIDI_find_rma_ep(MPIR_Win* win, int target_rank, int* ep_idx)
 {
+#if 0
     *ep_idx = ((win->comm_ptr->context_id + target_rank) % MPIDI_CH4_Global.n_netmod_eps) & INT_MAX;
+#else
+    *ep_idx = 0;
+#endif
     MPIR_Assert(*ep_idx >= 0);
 }
 
