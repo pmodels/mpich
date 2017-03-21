b64a0a942ff6a8669df4605d43ed95953f18c952
Akhil Langer <akhil.langer@intel.com>
2017-03-21 15:14:22 -0500

    ch4/generic/coll: Add knomial trees in treebasic algorithms
    Adapted treebasic algos to reuse code from schedules/tree_util.h
    instead of rewriting knomial trees

--- src/mpid/ch4/generic/coll/algorithms/treebasic/post.h
+++ src/mpid/ch4/generic/coll/algorithms/treebasic/post.h
@@ -1,5 +1,7 @@
+#define COLL_MAX_TREE_BREADTH 100
 #define COLL_NAME TREEBASIC_
 #define COLL_NAME_LC treebasic
 #include "../../src/coll_namespace_pre.h"
 #include "./coll.h"
 #include "../../src/coll_namespace_post.h"
+#undef COLL_MAX_TREE_BREADTH
