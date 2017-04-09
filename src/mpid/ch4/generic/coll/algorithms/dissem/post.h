66ab854fefe27e3da6479fa09d572f4ae36e9ed6
Akhil Langer <akhil.langer@intel.com>
2017-04-08 22:40:46 -0500

    ch4/coll: Move src/coll_common_impl.h inside algorithms/common
    This is where it belongs

--- src/mpid/ch4/generic/coll/algorithms/dissem/post.h
+++ src/mpid/ch4/generic/coll/algorithms/dissem/post.h
@@ -1,7 +1,7 @@
 #define COLL_NAME DISSEM_
 #define COLL_NAME_LC dissem
 #include "../../src/coll_namespace_def.h"
-#include "../../src/coll_common_impl.h"
+#include "../common/common_fns.h"
 #include "../common/schedule_dissem.h"
 #include "./coll.h"
 #include "../../src/coll_namespace_undef.h"
--- src/mpid/ch4/generic/coll/algorithms/recexch/post.h
+++ src/mpid/ch4/generic/coll/algorithms/recexch/post.h
@@ -1,7 +1,7 @@
 #define COLL_NAME RECEXCH_
 #define COLL_NAME_LC recexch
 #include "../../src/coll_namespace_def.h"
-#include "../../src/coll_common_impl.h"
+#include "../common/common_fns.h"
 #include "../common/schedule_recexch.h"
 #include "./coll.h"
 #include "../../src/coll_namespace_undef.h"
