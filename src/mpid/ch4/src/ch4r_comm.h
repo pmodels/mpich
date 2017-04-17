c5fff8a7ca5ed3ced6584cf9eb34112ca33d4a92
Yanfei Guo <yguo@anl.gov>
2017-04-17 16:00:38 -0500

    CH4: Change namespace for LUT/MLUT allocation functions.
    These functions will be used in the netmod layer for creating the
    intercomm of dynproc. Move them to MPIDIU_ namespace.
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpid/ch4/src/ch4r_comm.h
+++ src/mpid/ch4/src/ch4r_comm.h
@@ -180,4 +180,113 @@ MPL_STATIC_INLINE_PREFIX int MPIDIU_Intercomm_map_bcast_intra(MPIR_Comm *local_c
     goto fn_exit;
 }
 
+#undef FUNCNAME
+#define FUNCNAME MPIDIU_alloc_lut
+#undef FCNAME
+#define FCNAME MPL_QUOTE(FUNCNAME)
+static inline int MPIDIU_alloc_lut(MPIDI_rank_map_lut_t ** lut, int size)
+{
+    int mpi_errno = MPI_SUCCESS;
+    MPIDI_rank_map_lut_t *new_lut = NULL;
+
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_ALLOC_LUT);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_ALLOC_LUT);
+
+    new_lut = (MPIDI_rank_map_lut_t *) MPL_malloc(sizeof(MPIDI_rank_map_lut_t)
+                                                  + size * sizeof(MPIDI_lpid_t));
+    if (new_lut == NULL) {
+        *lut = NULL;
+        MPIR_ERR_SETANDJUMP(mpi_errno, MPI_ERR_OTHER, "**nomem");
+    }
+
+    MPIR_Object_set_ref(new_lut, 1);
+    *lut = new_lut;
+
+    MPL_DBG_MSG_FMT(MPIDI_CH4_DBG_MEMORY, VERBOSE,
+                    (MPL_DBG_FDEST, "alloc lut %p, size %ld, refcount=%d",
+                     new_lut, size * sizeof(MPIDI_lpid_t), MPIR_Object_get_ref(new_lut)));
+  fn_exit:
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_ALLOC_LUT);
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
+}
+
+#undef FUNCNAME
+#define FUNCNAME MPIDIU_release_lut
+#undef FCNAME
+#define FCNAME MPL_QUOTE(FUNCNAME)
+static inline int MPIDIU_release_lut(MPIDI_rank_map_lut_t * lut)
+{
+    int mpi_errno = MPI_SUCCESS;
+    int count = 0;
+
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDIU_RELEASE_LUT);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDIU_RELEASE_LUT);
+
+    MPIR_Object_release_ref(lut, &count);
+    MPL_DBG_MSG_FMT(MPIDI_CH4_DBG_MEMORY, VERBOSE, (MPL_DBG_FDEST, "dec ref to lut %p", lut));
+    if (count == 0) {
+        MPL_free(lut);
+        MPL_DBG_MSG_FMT(MPIDI_CH4_DBG_MEMORY, VERBOSE, (MPL_DBG_FDEST, "free lut %p", lut));
+    }
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDIU_RELEASE_LUT);
+    return mpi_errno;
+}
+
+#undef FUNCNAME
+#define FUNCNAME MPIDIU_alloc_mlut
+#undef FCNAME
+#define FCNAME MPL_QUOTE(FUNCNAME)
+static inline int MPIDIU_alloc_mlut(MPIDI_rank_map_mlut_t ** mlut, int size)
+{
+    int mpi_errno = MPI_SUCCESS;
+    MPIDI_rank_map_mlut_t *new_mlut = NULL;
+
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDIU_ALLOC_MLUT);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDIU_ALLOC_MLUT);
+
+    new_mlut = (MPIDI_rank_map_mlut_t *) MPL_malloc(sizeof(MPIDI_rank_map_mlut_t)
+                                                    + size * sizeof(MPIDI_gpid_t));
+    if (new_mlut == NULL) {
+        *mlut = NULL;
+        MPIR_ERR_SETANDJUMP(mpi_errno, MPI_ERR_OTHER, "**nomem");
+    }
+
+    MPIR_Object_set_ref(new_mlut, 1);
+    *mlut = new_mlut;
+
+    MPL_DBG_MSG_FMT(MPIDI_CH4_DBG_MEMORY, VERBOSE,
+                    (MPL_DBG_FDEST, "alloc mlut %p, size %ld, refcount=%d",
+                     new_mlut, size * sizeof(MPIDI_gpid_t), MPIR_Object_get_ref(new_mlut)));
+  fn_exit:
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDIU_ALLOC_MLUT);
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
+}
+
+#undef FUNCNAME
+#define FUNCNAME MPIDIU_release_mlut
+#undef FCNAME
+#define FCNAME MPL_QUOTE(FUNCNAME)
+static inline int MPIDIU_release_mlut(MPIDI_rank_map_mlut_t * mlut)
+{
+    int mpi_errno = MPI_SUCCESS;
+    int count = 0;
+
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDIU_RELEASE_MLUT);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDIU_RELEASE_MLUT);
+
+    MPIR_Object_release_ref(mlut, &count);
+    MPL_DBG_MSG_FMT(MPIDI_CH4_DBG_MEMORY, VERBOSE, (MPL_DBG_FDEST, "dec ref to mlut %p", mlut));
+    if (count == 0) {
+        MPL_free(mlut);
+        MPL_DBG_MSG_FMT(MPIDI_CH4_DBG_MEMORY, VERBOSE, (MPL_DBG_FDEST, "free mlut %p", mlut));
+    }
+
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDIU_RELEASE_MLUT);
+    return mpi_errno;
+}
+
 #endif /* ifndef CH4R_COMM_H_INCLUDED */
