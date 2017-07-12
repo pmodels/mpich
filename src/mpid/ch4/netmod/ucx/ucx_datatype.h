08a7e57ba5b03138fd2ab18845addb0b47e4f82a
Ken Raffenetti <raffenet@mcs.anl.gov>
2017-07-12 11:04:47 -0500

    ch4/ucx: Define dt ops struct in C file
    Avoid defining this struct everywhere the device headers are
    included. Fixes pmodels/mpich#2427
    
    No reviewer.

--- src/mpid/ch4/netmod/ucx/ucx_datatype.h
+++ src/mpid/ch4/netmod/ucx/ucx_datatype.h
@@ -103,17 +103,6 @@ static inline void MPIDI_UCX_Finish_pack(void *state)
 
 }
 
-
-static ucp_generic_dt_ops_t MPIDI_UCX_datatype_ops = {
-    .start_pack = MPIDI_UCX_Start_pack,
-    .start_unpack = MPIDI_UCX_Start_unpack,
-    .packed_size = MPIDI_UCX_Packed_size,
-    .pack = MPIDI_UCX_Pack,
-    .unpack = MPIDI_UCX_Unpack,
-    .finish = MPIDI_UCX_Finish_pack
-};
-
-
 static inline int MPIDI_NM_mpi_type_free_hook(MPIR_Datatype * datatype_p)
 {
 
