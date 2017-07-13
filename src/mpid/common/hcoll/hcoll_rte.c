ecc2e7a70136099b8514c97fd83b1e534f005624
Valentin Petrov <valentinp@mellanox.com>
2017-07-13 15:17:26 -0500

    hcoll: Support only simple datatypes
    Complex datatype handling will be added back in a later commit.
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpid/common/hcoll/hcoll_rte.c
+++ src/mpid/common/hcoll/hcoll_rte.c
@@ -97,21 +97,6 @@ void hcoll_rte_fns_setup(void)
     init_module_fns();
 }
 
-/* This function converts dte_general_representation data into regular iovec array which is
-  used in rml
-  */
-static inline int count_total_dte_repeat_entries(struct dte_data_representation_t *data)
-{
-    unsigned int i;
-
-    struct dte_generalized_iovec_t *dte_iovec = data->rep.general_rep->data_representation.data;
-    int total_entries_number = 0;
-    for (i = 0; i < dte_iovec->repeat_count; i++) {
-        total_entries_number += dte_iovec->repeat[i].n_elements;
-    }
-    return total_entries_number;
-}
-
 #undef FUNCNAME
 #define FUNCNAME recv_nb
 #undef FCNAME
@@ -126,53 +111,25 @@ static int recv_nb(struct dte_data_representation_t data,
     MPI_Datatype dtype;
     MPIR_Request *request;
     MPIR_Comm *comm;
-    int context_offset;
     size_t size;
     mpi_errno = MPI_SUCCESS;
-    context_offset = MPIR_CONTEXT_INTRA_COLL;
     comm = (MPIR_Comm *) grp_h;
     if (!ec_h.handle) {
         MPIR_ERR_SETANDJUMP2(mpi_errno, MPI_ERR_OTHER, "**hcoll_wrong_arg",
                              "**hcoll_wrong_arg %p %d", ec_h.handle, ec_h.rank);
     }
 
-    if (HCOL_DTE_IS_INLINE(data)) {
-        if (!buffer && !HCOL_DTE_IS_ZERO(data)) {
-            MPIR_ERR_SETANDJUMP(mpi_errno, MPI_ERR_OTHER, "**null_buff_ptr");
-        }
-        size = (size_t) data.rep.in_line_rep.data_handle.in_line.packed_size * count / 8;
-        dtype = MPI_CHAR;
-        mpi_errno = MPID_Irecv(buffer, size, dtype, ec_h.rank, tag, comm, context_offset, &request);
-        req->data = (void *) request;
-        req->status = HCOLRTE_REQUEST_ACTIVE;
+    MPIR_Assert(HCOL_DTE_IS_INLINE(data));
+    if (!buffer && !HCOL_DTE_IS_ZERO(data)) {
+        MPIR_ERR_SETANDJUMP(mpi_errno, MPI_ERR_OTHER, "**null_buff_ptr");
     }
-    else {
-        int total_entries_number;
-        int i;
-        unsigned int j;
-        void *buf;
-        uint64_t len;
-        int repeat_count;
-        struct dte_struct_t *repeat;
-        if (NULL != buffer) {
-            /* We have a full data description & buffer pointer simultaneously.
-             * It is ambiguous. Throw a warning since the user might have made a
-             * mistake with data reps */
-            MPL_DBG_MSG(MPIR_DBG_HCOLL, VERBOSE, "Warning: buffer_pointer != NULL for NON-inline data "
-                         "representation: buffer_pointer is ignored");
-        }
-        total_entries_number = count_total_dte_repeat_entries(&data);
-        repeat = data.rep.general_rep->data_representation.data->repeat;
-        repeat_count = data.rep.general_rep->data_representation.data->repeat_count;
-        for (i = 0; i < repeat_count; i++) {
-            for (j = 0; j < repeat[i].n_elements; j++) {
-                char *repeat_unit = (char *) &repeat[i];
-                buf = (void *) (repeat_unit + repeat[i].elements[j].base_offset);
-                len = repeat[i].elements[j].packed_size;
-                recv_nb(DTE_BYTE, len, buf, ec_h, grp_h, tag, req);
-            }
-        }
-    }
+    size = (size_t) data.rep.in_line_rep.data_handle.in_line.packed_size * count / 8;
+    dtype = MPI_CHAR;
+    request = NULL;
+    mpi_errno = MPIC_Irecv(buffer, size, dtype, ec_h.rank, tag, comm, &request);
+    MPIR_Assert(request);
+    req->data = (void *) request;
+    req->status = HCOLRTE_REQUEST_ACTIVE;
   fn_exit:
     return mpi_errno;
   fn_fail:
@@ -193,53 +150,26 @@ static int send_nb(dte_data_representation_t data,
     MPI_Datatype dtype;
     MPIR_Request *request;
     MPIR_Comm *comm;
-    int context_offset;
     size_t size;
     mpi_errno = MPI_SUCCESS;
-    context_offset = MPIR_CONTEXT_INTRA_COLL;
     comm = (MPIR_Comm *) grp_h;
     if (!ec_h.handle) {
         MPIR_ERR_SETANDJUMP2(mpi_errno, MPI_ERR_OTHER, "**hcoll_wrong_arg",
                              "**hcoll_wrong_arg %p %d", ec_h.handle, ec_h.rank);
     }
 
-    if (HCOL_DTE_IS_INLINE(data)) {
-        if (!buffer && !HCOL_DTE_IS_ZERO(data)) {
-            MPIR_ERR_SETANDJUMP(mpi_errno, MPI_ERR_OTHER, "**null_buff_ptr");
-        }
-        size = (size_t) data.rep.in_line_rep.data_handle.in_line.packed_size * count / 8;
-        dtype = MPI_CHAR;
-        mpi_errno = MPID_Isend(buffer, size, dtype, ec_h.rank, tag, comm, context_offset, &request);
-        req->data = (void *) request;
-        req->status = HCOLRTE_REQUEST_ACTIVE;
+    MPIR_Assert(HCOL_DTE_IS_INLINE(data));
+    if (!buffer && !HCOL_DTE_IS_ZERO(data)) {
+        MPIR_ERR_SETANDJUMP(mpi_errno, MPI_ERR_OTHER, "**null_buff_ptr");
     }
-    else {
-        int total_entries_number;
-        int i;
-        unsigned int j;
-        void *buf;
-        uint64_t len;
-        int repeat_count;
-        struct dte_struct_t *repeat;
-        if (NULL != buffer) {
-            /* We have a full data description & buffer pointer simultaneously.
-             * It is ambiguous. Throw a warning since the user might have made a
-             * mistake with data reps */
-            MPL_DBG_MSG(MPIR_DBG_HCOLL, VERBOSE, "Warning: buffer_pointer != NULL for NON-inline data "
-                         "representation: buffer_pointer is ignored");
-        }
-        total_entries_number = count_total_dte_repeat_entries(&data);
-        repeat = data.rep.general_rep->data_representation.data->repeat;
-        repeat_count = data.rep.general_rep->data_representation.data->repeat_count;
-        for (i = 0; i < repeat_count; i++) {
-            for (j = 0; j < repeat[i].n_elements; j++) {
-                char *repeat_unit = (char *) &repeat[i];
-                buf = (void *) (repeat_unit + repeat[i].elements[j].base_offset);
-                len = repeat[i].elements[j].packed_size;
-                send_nb(DTE_BYTE, len, buf, ec_h, grp_h, tag, req);
-            }
-        }
-    }
+    size = (size_t) data.rep.in_line_rep.data_handle.in_line.packed_size * count / 8;
+    dtype = MPI_CHAR;
+    request = NULL;
+    MPIR_Errflag_t err = MPIR_ERR_NONE;
+    mpi_errno = MPIC_Isend(buffer, size, dtype, ec_h.rank, tag, comm, &request, &err);
+    MPIR_Assert(request);
+    req->data = (void *) request;
+    req->status = HCOLRTE_REQUEST_ACTIVE;
   fn_exit:
     return mpi_errno;
   fn_fail:
