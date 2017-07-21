d15849e314d7f77d525b0748e01a8cd6a9dbd9bc
Neelima <nbayyapu@anl.gov>
2017-07-21 16:16:43 -0500

    test/mpi: Add tests for NULL argument errors
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

---
+++ test/mpi/errors/datatype/type_get_true_extent_test.c
@@ -0,0 +1,31 @@
+#include <mpi.h>
+#include <stdio.h>
+#include "mpitest.h"
+
+int main(int argc, char *argv[])
+{
+    int rank, size;
+    MPI_Datatype type;
+    MPI_Aint lb;
+    int errs = 0, errclass, mpi_errno;
+
+    MTest_Init(&argc, &argv);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Errhandler_set(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
+
+    type = MPI_INT;
+    MPI_Type_size(type, &size);
+    /* Checking type_get_true_extent for NULL variable */
+    mpi_errno = MPI_Type_get_true_extent(type, &lb, NULL);
+    MPI_Error_class(mpi_errno, &errclass);
+    if (errclass != MPI_ERR_ARG)
+        ++errs;
+
+    MPI_Type_free(&type);
+    MTest_Finalize(errs);
+    MPI_Finalize();
+    return 0;
+}
+
