08b02b6510aa753818bba525071645a8f53b2bde
Neelima <nbayyapu@anl.gov>
2017-07-28 16:42:10 -0500

    comm: Add NULL argument check to MPI_Comm_split
    Missed this function in [7ca4f0b5bbe9].
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpi/comm/comm_split.c
+++ src/mpi/comm/comm_split.c
@@ -468,6 +468,7 @@ int MPI_Comm_split(MPI_Comm comm, int color, int key, MPI_Comm *newcomm)
             MPIR_Comm_valid_ptr( comm_ptr, mpi_errno, FALSE );
 	    /* If comm_ptr is not valid, it will be reset to null */
             if (mpi_errno) goto fn_fail;
+            MPIR_ERRTEST_ARGNULL(newcomm, "newcomm", mpi_errno);
         }
         MPID_END_ERROR_CHECKS;
     }
