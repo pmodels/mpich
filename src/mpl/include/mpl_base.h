d88d8d10e55ba449f62a60e952d1d4e281517de6
Akhil Langer <akhil.langer@intel.com>
2017-08-30 16:45:21 +0000

    Define MPL_UNUSED macro for unused functions
    Prefix unused functions with this macro to suppress
    "unused function" warnings

--- src/mpl/include/mpl_base.h
+++ src/mpl/include/mpl_base.h
@@ -52,6 +52,7 @@
 #  endif /* MPL_HAVE_GCC_ATTRIBUTE */
 #endif /* ATTRIBUTE */
 
+#define MPL_UNUSED ATTRIBUTE((unused))
 #define MPL_STATIC_INLINE_PREFIX ATTRIBUTE((always_inline)) static inline
 #define MPL_STATIC_INLINE_SUFFIX ATTRIBUTE((always_inline))
 
