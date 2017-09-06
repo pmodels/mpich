68842fe5459c10fe10689aa118602b93cc56a6e1
Mohamad Chaarawi <mohamad.chaarawi@intel.com>
2017-09-06 20:42:44 +0000

    fix error codes conversion from DAOS to MPI
    Signed-off-by: Mohamad Chaarawi <mohamad.chaarawi@intel.com>

--- src/mpi/romio/adio/ad_daos/ad_daos_common.c
+++ src/mpi/romio/adio/ad_daos/ad_daos_common.c
@@ -7,25 +7,25 @@ int ADIOI_DAOS_error_convert(int error)
 {
     switch (error)
     {
-	case DER_NO_PERM:
+	case -DER_NO_PERM:
 	    return MPI_ERR_ACCESS;
-	case DER_ENOENT:
-        case DER_NONEXIST:
+	case -DER_ENOENT:
+        case -DER_NONEXIST:
 	    return MPI_ERR_NO_SUCH_FILE;
-	case DER_IO:
+	case -DER_IO:
 	    return MPI_ERR_IO;
-	case DER_EXIST:
+	case -DER_EXIST:
 	    return MPI_ERR_FILE_EXISTS;
-	case DER_NOTDIR:
+	case -DER_NOTDIR:
 	    return MPI_ERR_BAD_FILE;
-	case DER_INVAL:
-	case DER_STALE:
+	case -DER_INVAL:
+	case -DER_STALE:
 	    return MPI_ERR_FILE;
-	case DER_NOSPACE:
+	case -DER_NOSPACE:
 	    return MPI_ERR_NO_SPACE;
-	case DER_NOSYS:
+	case -DER_NOSYS:
 	    return MPI_ERR_UNSUPPORTED_OPERATION;
-	case DER_NOMEM:
+	case -DER_NOMEM:
 	    return MPI_ERR_INTERN;
 	default:
 	    return MPI_UNDEFINED;
