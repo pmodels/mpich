48867674a2dd68d9f8a6f0b96c6f7af6d453b9d2
Pavan Balaji <balaji@anl.gov>
2017-10-12 09:59:57 -0500

    autogen: Get rid of MPICH_AUTOTOOLS_DIR.
    Unused environment variable that requires us to maintain unnecessary
    patches in third-party software, such as openpa and hwloc.
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/pm/hydra/tools/topo/hwloc/hwloc/autogen.sh
+++ src/pm/hydra/tools/topo/hwloc/hwloc/autogen.sh
@@ -1,9 +1,2 @@
-#!/bin/sh
-
-if [ -n "$MPICH_AUTOTOOLS_DIR" ] ; then
-    autoreconf=${MPICH_AUTOTOOLS_DIR}/autoreconf
-else
-    autoreconf=${AUTORECONF:-autoreconf}
-fi
-
-$autoreconf ${autoreconf_args:-"-ivf"}
+:
+autoreconf ${autoreconf_args:-"-ivf"}
