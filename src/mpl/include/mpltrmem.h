5f9583f6f4ecb1e8412f5e5e82ff2654893e31ef
Min Si <msi@il.is.s.u-tokyo.ac.jp>
2017-10-20 10:15:43 -0500

    Define MPL memory alignment union type.
    Many internal structures and buffers (e.g., MPL memory tracing header,
    RMA packet immediate data) need to follow the alignment rules for any
    predefined types. This union helps wrap up them with all C types that
    possibly require the largest alignment bytes (e.g., long double might
    require 16bytes with clang compiler).
    
    Signed-off-by: Pavan Balaji <balaji@anl.gov>

--- src/mpl/include/mpltrmem.h
+++ src/mpl/include/mpltrmem.h
@@ -7,6 +7,72 @@
 #ifndef MPLTRMEM_H_INCLUDED
 #define MPLTRMEM_H_INCLUDED
 
+/* --------------------------------------------------------------------
+ * MPL memory alignment union
+ * The MPL_mem_alignment_t union is used to help internal structures or buffers
+ * follow the alignment rules for all predefined datatypes. */
+
+#if 0 /* sample usage : wrap up sample_t structure. */
+typedef union {
+    sample_t var;
+    MPL_mem_alignment_t alignment;
+} aligned_sample_t;
+#endif
+
+/* Union including all C types that possibly require the largest alignment bytes.
+ * Note that we do not need other Fortran/CXX predefined types because all of them
+ * are internally translated to appropriate C types. */
+typedef union {
+    /* Integer types.
+     * We only list signed types here, because an unsigned type always require
+     * the same alignment as its signed version. Fix me if this theory is wrong.*/
+    long long_align;
+#ifdef HAVE_LONG_LONG
+    long long ll_align;
+#endif
+#ifdef HAVE_INT32_T
+    int32_t int32_t_align;
+#endif
+#ifdef HAVE_INT64_T
+    int64_t int64_t_align;
+#endif
+
+    /* Logical type */
+#ifdef HAVE__BOOL
+    _Bool bool_align;
+#endif
+
+    /* Floating-point types */
+    double double_align;
+#ifdef HAVE_LONG_DOUBLE
+    long double ld_align;
+#endif
+
+    /* Complex types */
+#ifdef HAVE_DOUBLE__COMPLEX
+    double _Complex d_complex_align;
+#endif
+#ifdef HAVE_LONG_DOUBLE__COMPLEX
+    long double _Complex ld_complex_align;
+#endif
+    /* MPICH handles Fortran/CXX complex types as structure (see src/include/oputil.h).
+     * Because some platform may have special alignment rule for structures,
+     * we include them as well. */
+    struct {
+        double re;
+        double im;
+    } mpl_d_complex_align;
+#ifdef HAVE_LONG_DOUBLE
+    struct {
+        long double re;
+        long double im;
+    } mpl_ld_complex_align;
+#endif
+} MPL_mem_alignment_t;
+
+/* END of MPL memory alignment union
+ * -------------------------------------------------------------------- */
+
 /* FIXME: Consider an option of specifying __attribute__((malloc)) for
    gcc - this lets gcc-style compilers know that the returned pointer
    does not alias any pointer prior to the call.
