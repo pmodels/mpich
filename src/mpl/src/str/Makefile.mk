e6fb716d3aec8b98be3848cb8b406e4d17cbc4c1
Pavan Balaji <balaji@anl.gov>
2017-10-30 13:55:03 -0500

    mpl: converted mpl to a submodule
    Signed-off-by: Yanfei Guo <yguo@anl.gov>

--- src/mpl/src/str/Makefile.mk
+++
@@ -1,7 +0,0 @@
-# -*- Mode: Makefile; -*-
-#
-# (C) 2016 by Argonne National Laboratory.
-#     See COPYRIGHT in top-level directory.
-#
-
-lib@MPLLIBNAME@_la_SOURCES += src/str/mpl_str.c src/str/mpl_argstr.c
