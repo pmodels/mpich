24faa8ed65b2cdc8e43260680603bd700c6bbd10
Pavan Balaji <balaji@anl.gov>
2017-11-02 11:16:50 -0500

    hwloc: added a submodule for hwloc 1.11.8

--- autogen.sh
+++ autogen.sh
@@ -166,7 +166,7 @@ export do_build_configure
 MAKE=${MAKE-make}
 
 # external packages that require autogen.sh to be run for each of them
-externals="src/pm/hydra src/mpi/romio src/openpa src/mpid/ch4/netmod/ucx/ucx src/mpid/ch4/netmod/ofi/libfabric"
+externals="src/pm/hydra src/mpi/romio src/openpa src/hwloc src/mpid/ch4/netmod/ucx/ucx src/mpid/ch4/netmod/ofi/libfabric"
 
 # amdirs are the directories that make use of autoreconf
 amdirs=". src/mpl src/util/logging/rlog"
