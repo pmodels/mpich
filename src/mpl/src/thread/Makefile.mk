677dc09e07e27831b8f1ea3d4ae9bc7662eb6d24
Yanfei Guo <yguo@anl.gov>
2017-11-08 09:21:55 -0600

    mpl: Adding support for Argobots.
    Adds supports for Argobots thread interface, including: creation,
    lock/unlock, yielding and condition.
    
    Argobots can be enabled by using --with-thread-package=argobots
    
    This commit takes the final codes from mpich-dev/mpi-argobots branch
    without preserving its detail history. The original contributors are
    Halim Amer, Pavan Balaji, Huiwei Lu and Sangmin Seo.
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>
    Signed-off-by: Yanfei Guo <yguo@anl.gov>

--- src/mpl/src/thread/Makefile.mk
+++ src/mpl/src/thread/Makefile.mk
@@ -9,4 +9,5 @@ lib@MPLLIBNAME@_la_SOURCES +=		\
 	src/thread/mpl_thread.c		\
 	src/thread/mpl_thread_win.c	\
 	src/thread/mpl_thread_solaris.c	\
+	src/thread/mpl_thread_argobots.c	\
 	src/thread/mpl_thread_posix.c
