e3e30ea48aee44a61d49c111a3fac42ea74a1abf
Pavan Balaji <balaji@anl.gov>
2017-12-21 21:09:23 -0600

    coll: remove redundant undefs
    Signed-off-by: Wesley Bland <wesley.bland@intel.com>

--- src/mpi/coll/bcast/bcast_nb.c
+++ src/mpi/coll/bcast/bcast_nb.c
@@ -7,7 +7,6 @@
 #include "mpiimpl.h"
 
 #undef FUNCNAME
-#undef FUNCNAME
 #define FUNCNAME MPIR_Bcast_nb
 #undef FCNAME
 #define FCNAME MPL_QUOTE(FUNCNAME)
--- src/mpi/coll/exscan/exscan_nb.c
+++ src/mpi/coll/exscan/exscan_nb.c
@@ -7,7 +7,6 @@
 #include "mpiimpl.h"
 
 #undef FUNCNAME
-#undef FUNCNAME
 #define FUNCNAME MPIR_Exscan_nb
 #undef FCNAME
 #define FCNAME MPL_QUOTE(FUNCNAME)
