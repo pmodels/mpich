0e862f687784da796cb7d0eff6734e87bd99ae63
Rubasri Kalidas <rubasri.kalidas@intel.com>
2017-12-22 08:57:02 -0600

    test/coll: Add collective algorithm testing
    Add tests for specific bcast algos selected through CVARs
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

---
+++ test/mpi/coll/test_coll_algos.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+# (C) 2018 by Argonne National Laboratory.
+#     See COPYRIGHT in top-level directory.
+#
+nl='
+'
+
+coll_algo_tests=""
+
+########## Add tests for bcast algorithms ############
+
+#disable device collectives for bcast to test MPIR algorithms
+testing_env="env=MPIR_CVAR_BCAST_DEVICE_COLLECTIVE=0 "
+
+#test nb algorithms
+testing_env+="env=MPIR_CVAR_BCAST_INTRA_ALGORITHM=nb "
+testing_env+="env=MPIR_CVAR_IBCAST_DEVICE_COLLECTIVE=0 "
+algo_names="tree_kary tree_knomial"
+kvalues="3"
+
+for algo_name in ${algo_names}; do
+    for kval in ${kvalues}; do
+        #set the environment
+        env="${testing_env} env=MPIR_CVAR_IBCAST_INTRA_ALGORITHM=${algo_name} "
+        env+="env=MPIR_CVAR_IBCAST_TREE_KVAL=${kval}"
+
+        coll_algo_tests+="bcasttest 10 ${env}${nl}"
+        coll_algo_tests+="bcast_full 4 timeLimit=600 ${env}${nl}"
+        coll_algo_tests+="bcast_min_datatypes 10 timeLimit=1200 ${env}${nl}"
+        coll_algo_tests+="bcast_comm_world 10 timeLimit=1200 ${env}${nl}"
+        coll_algo_tests+="bcastzerotype 5 ${env}${nl}"
+    done
+done
+
+export coll_algo_tests
