f0a6c54689ebfc979ee3e792ebaa462fdbbde389
Pavan Balaji <balaji@anl.gov>
2017-12-24 05:06:39 -0600

    coll: split intercomm scatter algorithms to separate files.
    Signed-off-by: Wesley Bland <wesley.bland@intel.com>

--- src/mpi/coll/scatter/Makefile.mk
+++ src/mpi/coll/scatter/Makefile.mk
@@ -8,4 +8,5 @@ mpi_sources +=                     		\
 mpi_core_sources +=									\
 	src/mpi/coll/scatter/scatter_nb.c			\
 	src/mpi/coll/scatter/scatter_intra_binomial.c			\
-	src/mpi/coll/scatter/scatter_inter_generic.c
+	src/mpi/coll/scatter/scatter_inter_linear.c \
+	src/mpi/coll/scatter/scatter_inter_remote_send_local_scatter.c
