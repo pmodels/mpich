12c3fef0d75cea6908f2437edd5438a69ae94f70
Akhil Langer <akhil.langer@intel.com>
2017-12-30 12:13:45 -0600

    Move request related API impl to new folder
    Added a new high level 'request' folder. All the `MPI_Request/Status`
    related functions were in pt2pt folder. But requests are also used
    for collectives, rma, etc. There is enough requests related API to
    call for its own folder. Moreover, with persistent collectives
    implementation, some collectives specific cases will be added to
    request functions. Hence, they do not fit well inside the pt2pt folder.
    
    Signed-off-by: Pavan Balaji <balaji@anl.gov>

--- src/mpi/pt2pt/Makefile.mk
+++ src/mpi/pt2pt/Makefile.mk
@@ -10,9 +10,6 @@ mpi_sources +=                           \
     src/mpi/pt2pt/bsend_init.c           \
     src/mpi/pt2pt/bufattach.c            \
     src/mpi/pt2pt/buffree.c              \
-    src/mpi/pt2pt/cancel.c               \
-    src/mpi/pt2pt/greq_start.c           \
-    src/mpi/pt2pt/greq_complete.c        \
     src/mpi/pt2pt/ibsend.c               \
     src/mpi/pt2pt/improbe.c              \
     src/mpi/pt2pt/imrecv.c               \
@@ -26,32 +23,17 @@ mpi_sources +=                           \
     src/mpi/pt2pt/probe.c                \
     src/mpi/pt2pt/recv.c                 \
     src/mpi/pt2pt/recv_init.c            \
-    src/mpi/pt2pt/request_free.c         \
-    src/mpi/pt2pt/request_get_status.c   \
     src/mpi/pt2pt/rsend.c                \
     src/mpi/pt2pt/rsend_init.c           \
     src/mpi/pt2pt/send.c                 \
     src/mpi/pt2pt/send_init.c            \
     src/mpi/pt2pt/sendrecv.c             \
     src/mpi/pt2pt/sendrecv_rep.c         \
-    src/mpi/pt2pt/status_set_cancelled.c \
     src/mpi/pt2pt/ssend.c                \
-    src/mpi/pt2pt/ssend_init.c           \
-    src/mpi/pt2pt/start.c                \
-    src/mpi/pt2pt/startall.c             \
-    src/mpi/pt2pt/test.c                 \
-    src/mpi/pt2pt/test_cancelled.c       \
-    src/mpi/pt2pt/testall.c              \
-    src/mpi/pt2pt/testany.c              \
-    src/mpi/pt2pt/testsome.c             \
-    src/mpi/pt2pt/wait.c                 \
-    src/mpi/pt2pt/waitall.c              \
-    src/mpi/pt2pt/waitany.c              \
-    src/mpi/pt2pt/waitsome.c
+    src/mpi/pt2pt/ssend_init.c
 
 mpi_core_sources += \
-    src/mpi/pt2pt/bsendutil.c     \
-    src/mpi/pt2pt/mpir_request.c
+    src/mpi/pt2pt/bsendutil.c
 
 noinst_HEADERS += src/mpi/pt2pt/bsendutil.h
 
