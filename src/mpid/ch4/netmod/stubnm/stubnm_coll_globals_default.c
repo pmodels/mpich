b08aefe6df1dbe578c8ec0f74a9b51ded5f4b38e
Pavan Balaji <balaji@anl.gov>
2018-01-10 14:57:08 -0600

    ch4: distinguish the collective name from the algo name.
    Signed-off-by: Wesley Bland <wesley.bland@intel.com>

--- src/mpid/ch4/netmod/stubnm/stubnm_coll_globals_default.c
+++ src/mpid/ch4/netmod/stubnm/stubnm_coll_globals_default.c
@@ -1,43 +1,43 @@
 /* Barrier default STUBNM containers initialization*/
-const MPIDI_STUBNM_coll_algo_container_t STUBNM_barrier_recursive_doubling_cnt =
+const MPIDI_STUBNM_coll_algo_container_t STUBNM_barrier__recursive_doubling_cnt =
     {
-        .id = MPIDI_STUBNM_barrier_recursive_doubling_id
+        .id = MPIDI_STUBNM_barrier__recursive_doubling_id
     };
 
 /* Bcast default STUBNM containers initialization*/
-const MPIDI_STUBNM_coll_algo_container_t STUBNM_bcast_binomial_cnt =
+const MPIDI_STUBNM_coll_algo_container_t STUBNM_bcast__binomial_cnt =
     {
-        .id = MPIDI_STUBNM_bcast_binomial_id
+        .id = MPIDI_STUBNM_bcast__binomial_id
     };
 
-const MPIDI_STUBNM_coll_algo_container_t STUBNM_bcast_scatter_recursive_doubling_allgather_cnt =
+const MPIDI_STUBNM_coll_algo_container_t STUBNM_bcast__scatter_recursive_doubling_allgather_cnt =
     {
-        .id = MPIDI_STUBNM_bcast_scatter_recursive_doubling_allgather_id
+        .id = MPIDI_STUBNM_bcast__scatter_recursive_doubling_allgather_id
     };
 
-const MPIDI_STUBNM_coll_algo_container_t STUBNM_bcast_scatter_ring_allgather_cnt =
+const MPIDI_STUBNM_coll_algo_container_t STUBNM_bcast__scatter_ring_allgather_cnt =
     {
-        .id = MPIDI_STUBNM_bcast_scatter_ring_allgather_id
+        .id = MPIDI_STUBNM_bcast__scatter_ring_allgather_id
     };
 
 /* Reduce default STUBNM containers initialization*/
-const MPIDI_STUBNM_coll_algo_container_t STUBNM_reduce_reduce_scatter_gather_cnt =
+const MPIDI_STUBNM_coll_algo_container_t STUBNM_reduce__reduce_scatter_gather_cnt =
     {
-        .id = MPIDI_STUBNM_reduce_reduce_scatter_gather_id
+        .id = MPIDI_STUBNM_reduce__reduce_scatter_gather_id
     };
 
-const MPIDI_STUBNM_coll_algo_container_t STUBNM_reduce_binomial_cnt =
+const MPIDI_STUBNM_coll_algo_container_t STUBNM_reduce__binomial_cnt =
     {
-        .id = MPIDI_STUBNM_reduce_binomial_id
+        .id = MPIDI_STUBNM_reduce__binomial_id
     };
 
 /* Allreduce default STUBNM containers initialization*/
-const MPIDI_STUBNM_coll_algo_container_t STUBNM_allreduce_recursive_doubling_cnt =
+const MPIDI_STUBNM_coll_algo_container_t STUBNM_allreduce__recursive_doubling_cnt =
     {
-        .id = MPIDI_STUBNM_allreduce_recursive_doubling_id
+        .id = MPIDI_STUBNM_allreduce__recursive_doubling_id
     };
 
-const MPIDI_STUBNM_coll_algo_container_t STUBNM_allreduce_reduce_scatter_allgather_cnt =
+const MPIDI_STUBNM_coll_algo_container_t STUBNM_allreduce__reduce_scatter_allgather_cnt =
     {
-        .id = MPIDI_STUBNM_allreduce_reduce_scatter_allgather_id
+        .id = MPIDI_STUBNM_allreduce__reduce_scatter_allgather_id
     };
