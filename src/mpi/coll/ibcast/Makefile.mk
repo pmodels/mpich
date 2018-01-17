909e10980e8e5e548dcb938f0711408419ab0abb
Pavan Balaji <balaji@anl.gov>
2018-01-17 17:14:01 -0600

    coll: white space changes
    Signed-off-by: Akhil Langer <akhil.langer@intel.com>

--- src/mpi/coll/ibcast/Makefile.mk
+++ src/mpi/coll/ibcast/Makefile.mk
@@ -1,15 +1,15 @@
 # mpi_sources includes only the routines that are MPI function entry points
 # The code for the MPI operations (e.g., MPI_SUM) is not included in
 # mpi_sources
-mpi_sources +=                                  \
+mpi_sources += \
     src/mpi/coll/ibcast/ibcast.c
 
-mpi_core_sources +=                        \
-    src/mpi/coll/ibcast/ibcast_intra_binomial.c  \
-    src/mpi/coll/ibcast/ibcast_intra_scatter_ring_allgather.c      \
-    src/mpi/coll/ibcast/ibcast_intra_scatter_recursive_doubling_allgather.c      \
-    src/mpi/coll/ibcast/ibcast_intra_smp.c      \
-    src/mpi/coll/ibcast/ibcast_inter_flat.c      \
+mpi_core_sources += \
+    src/mpi/coll/ibcast/ibcast_intra_binomial.c                             \
+    src/mpi/coll/ibcast/ibcast_intra_scatter_ring_allgather.c               \
+    src/mpi/coll/ibcast/ibcast_intra_scatter_recursive_doubling_allgather.c \
+    src/mpi/coll/ibcast/ibcast_intra_smp.c                                  \
+    src/mpi/coll/ibcast/ibcast_inter_flat.c                                 \
     src/mpi/coll/ibcast/ibcast_utils.c
 
 noinst_HEADERS += \
