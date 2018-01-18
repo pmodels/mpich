434933f0f1225752fb540196a08421f77e32f476
Ken Raffenetti <raffenet@mcs.anl.gov>
2018-01-18 10:27:48 -0600

    pm/hydra: Update embedded hwloc settings
    Use the recommended method from
    https://www.open-mpi.org/projects/hwloc/doc/v1.11.8/a00304.php.
    
    Signed-off-by: Pavan Balaji <balaji@anl.gov>

--- src/pm/hydra/tools/topo/hwloc/Makefile.mk
+++ src/pm/hydra/tools/topo/hwloc/Makefile.mk
@@ -9,12 +9,11 @@ libhydra_la_SOURCES += tools/topo/hwloc/topo_hwloc.c
 noinst_HEADERS += tools/topo/hwloc/topo_hwloc.h
 
 if hydra_use_embedded_hwloc
-AM_CPPFLAGS += -I$(top_srcdir)/tools/topo/hwloc/hwloc/include \
-	-I$(top_builddir)/tools/topo/hwloc/hwloc/include
+AM_CPPFLAGS += @HWLOC_EMBEDDED_CPPFLAGS@
+AM_CFLAGS += @HWLOC_EMBEDDED_CFLAGS@
 
 # Append hwloc to the external subdirs, so it gets built first
 external_subdirs += tools/topo/hwloc/hwloc
 external_dist_subdirs += tools/topo/hwloc/hwloc
-external_ldflags += -L$(top_builddir)/tools/topo/hwloc/hwloc/src
-external_libs += -lhwloc_embedded
+external_libs += @HWLOC_EMBEDDED_LDADD@ @HWLOC_EMBEDDED_LIBS@
 endif
