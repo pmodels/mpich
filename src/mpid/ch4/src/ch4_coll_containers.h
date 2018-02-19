23acffd5bd22fbf44022a481c44d9ad10738ed9e
Alexey Malkhanov <alexey.malhanov@intel.com>
2018-02-19 15:01:32 +0300

    ch4/coll: Rename file coll_tuning_types.h
    Rename coll_tuning_types.h -> ch4_coll_containers.h and
    move it from src/mpid/ch4/include to src/mpid/ch4/src
    to unify the files structure for ch4, ofi and shm layers
    
    Signed-off-by: Pavan Balaji <balaji@anl.gov>

--- src/mpid/ch4/src/ch4_coll_containers.h
+++ src/mpid/ch4/src/ch4_coll_containers.h
@@ -1,5 +1,5 @@
-#ifndef COLL_TUNING_TYPES_H_INCLUDED
-#define COLL_TUNING_TYPES_H_INCLUDED
+#ifndef CH4_COLL_CONTAINERS_H_INCLUDED
+#define CH4_COLL_CONTAINERS_H_INCLUDED
 
 /* Barrier CH4 level containers declaration */
 extern const MPIDI_coll_algo_container_t CH4_barrier_composition_alpha_cnt;
@@ -23,4 +23,4 @@ extern const MPIDI_coll_algo_container_t CH4_allreduce_composition_beta_cnt;
 extern const MPIDI_coll_algo_container_t CH4_allreduce_composition_gamma_cnt;
 extern const MPIDI_coll_algo_container_t CH4_allreduce_intercomm_cnt;
 
-#endif /*COLL_TUNING_TYPES_H_INCLUDED */
+#endif /* CH4_COLL_CONTAINERS_H_INCLUDED */
