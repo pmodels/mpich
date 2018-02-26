a5aece857672177da6f183f272aabb2877317723
Kavitha Tiptur Madhu <kmadhu@anl.gov>
2018-02-26 10:01:51 -0600

    comm_split_type: pci device support
    Signed-off-by: Pavan Balaji <balaji@anl.gov>

--- src/mpi/init/initthread.c
+++ src/mpi/init/initthread.c
@@ -340,6 +340,7 @@ int MPIR_Init_thread(int *argc, char ***argv, int required, int *provided)
     MPIR_Process.bindset = hwloc_bitmap_alloc();
     hwloc_topology_init(&MPIR_Process.topology);
     MPIR_Process.bindset_is_valid = 0;
+    hwloc_topology_set_io_types_filter(MPIR_Process.topology, HWLOC_TYPE_FILTER_KEEP_ALL);
     if (!hwloc_topology_load(MPIR_Process.topology)) {
         MPIR_Process.bindset_is_valid =
             !hwloc_get_proc_cpubind(MPIR_Process.topology, getpid(), MPIR_Process.bindset,
