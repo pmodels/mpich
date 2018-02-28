ce1607eb9667c1096a7b41ad908844f3968f6ff9
Wei-keng Liao <wkliao@users.noreply.github.com>
2018-02-28 14:39:55 -0600

    ROMIO: small messaging optimization
    combined two separated send/recv for offsets and lengths into one
    
    Signed-off-by: Rob Latham <robl@mcs.anl.gov>

--- src/mpi/romio/adio/ad_lustre/ad_lustre_aggregate.c
+++ src/mpi/romio/adio/ad_lustre/ad_lustre_aggregate.c
@@ -209,8 +209,8 @@ void ADIOI_LUSTRE_Calc_my_req(ADIO_File fd, ADIO_Offset * offset_list,
     for (i = 0; i < nprocs; i++) {
         if (count_my_req_per_proc[i]) {
             my_req[i].offsets = (ADIO_Offset *)
-                ADIOI_Malloc(count_my_req_per_proc[i] * sizeof(ADIO_Offset));
-            my_req[i].lens = ADIOI_Malloc(count_my_req_per_proc[i] * sizeof(ADIO_Offset));
+                ADIOI_Malloc(count_my_req_per_proc[i] * 2 * sizeof(ADIO_Offset));
+            my_req[i].lens = my_req[i].offsets + count_my_req_per_proc[i];
             count_my_req_procs++;
         }
         my_req[i].count = 0;    /* will be incremented where needed later */
