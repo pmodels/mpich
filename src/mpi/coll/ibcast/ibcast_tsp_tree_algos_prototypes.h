ed5c058853519ce8875d0f4dfd20a2ecdef9d1a3
Akhil Langer <akhil.langer@intel.com>
2018-03-09 19:18:01 -0600

    coll: Rename ibcast tsp algo files
    Rename the files to specify that they are tree based transport algorithm
    files. Going forward we are adding more algorithms, each type
    being in its own file.
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpi/coll/ibcast/ibcast_tsp_tree_algos_prototypes.h
+++ src/mpi/coll/ibcast/ibcast_tsp_tree_algos_prototypes.h
@@ -9,7 +9,7 @@
  *  Contributor License Agreement dated February 8, 2012.
  */
 
-/* Header protection (i.e., IBCAST_TSP_ALGOS_PROTOTYPES_H_INCLUDED) is
+/* Header protection (i.e., IBCAST_TSP_TREE_ALGOS_PROTOTYPES_H_INCLUDED) is
  * intentionally omitted since this header might get included multiple
  * times within the same .c file. */
 
