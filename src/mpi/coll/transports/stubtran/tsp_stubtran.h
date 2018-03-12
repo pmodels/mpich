cf82e3063296496e67e18c85ec7f3e89b97d7200
Akhil Langer <akhil.langer@intel.com>
2018-03-11 20:26:34 -0500

    coll: Use UT_array for children array in tree
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpi/coll/transports/stubtran/tsp_stubtran.h
+++ src/mpi/coll/transports/stubtran/tsp_stubtran.h
@@ -27,7 +27,7 @@ int MPII_Stubutil_sched_create(MPII_Stubutil_sched_t * sched, int tag);
 int MPII_Stubutil_sched_isend(const void *buf, int count, MPI_Datatype dt, int dest, int tag,
                               MPIR_Comm * comm_ptr, MPII_Stubutil_sched_t * sched,
                               int n_invtcs, int *invtcs);
-int MPII_Stubutil_sched_imcast(const void *buf, int count, MPI_Datatype dt, int *destinations,
+int MPII_Stubutil_sched_imcast(const void *buf, int count, MPI_Datatype dt, UT_array * destinations,
                                int num_destinations, int tag, MPIR_Comm * comm_ptr,
                                MPII_Stubutil_sched_t * sched, int n_invtcs, int *invtcs);
 int MPII_Stubutil_sched_irecv(void *buf, int count, MPI_Datatype datatype, int source, int tag,
