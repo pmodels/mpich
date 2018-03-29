975e8e0b0f48b4e7280f990ff5db31069d55cd11
Yanfei Guo <yguo@anl.gov>
2018-03-29 03:15:36 -0500

    ch4/posix: return non-catastrophic error for request allocation failure
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpid/ch4/shm/posix/posix_impl.h
+++ src/mpid/ch4/shm/posix/posix_impl.h
@@ -106,6 +106,7 @@ typedef struct {
 #define MPIDI_POSIX_REQUEST_CREATE_SREQ(sreq_)                  \
     {                                                           \
         (sreq_) = MPIR_Request_create(MPIR_REQUEST_KIND__SEND); \
+        MPIR_ERR_CHKANDSTMT((sreq_) == NULL, mpi_errno, MPIX_ERR_NOREQ, goto fn_fail, "**nomemreq"); \
         MPIR_Request_add_ref((sreq_));                          \
         (sreq_)->u.persist.real_request   = NULL;               \
     }
@@ -113,6 +114,7 @@ typedef struct {
 #define MPIDI_POSIX_REQUEST_CREATE_RREQ(rreq_)                  \
     {                                                           \
         (rreq_) = MPIR_Request_create(MPIR_REQUEST_KIND__RECV); \
+        MPIR_ERR_CHKANDSTMT((rreq_) == NULL, mpi_errno, MPIX_ERR_NOREQ, goto fn_fail, "**nomemreq"); \
         MPIR_Request_add_ref((rreq_));                          \
         (rreq_)->u.persist.real_request   = NULL;               \
     }
