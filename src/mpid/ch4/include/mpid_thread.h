276eb7e324dd33fc5d33efa00a0ce02aa504a4e4
Hajime Fujita <hajime.fujita@intel.com>
2018-04-12 15:54:00 -0500

    ch4: Add THREAD_SAFE macro
    This patch adds a macro to implement a branch between different
    MT models.
    
    Co-authored-by: Halim Amer <aamer@anl.gov>
    Co-authored-by: Chongxiao Cao <chongxiao.cao@intel.com>

--- src/mpid/ch4/include/mpid_thread.h
+++ src/mpid/ch4/include/mpid_thread.h
@@ -38,6 +38,22 @@ typedef MPIDU_Thread_mutex_t MPID_Thread_mutex_t;
 #define MPID_THREAD_CS_TRYENTER    MPIDU_THREAD_CS_TRYENTER
 #define MPID_THREAD_CS_EXIT        MPIDU_THREAD_CS_EXIT
 #define MPID_THREAD_CS_YIELD       MPIDU_THREAD_CS_YIELD
+
+#define MPID_THREAD_SAFE_BEGIN(name, mutex, cs_acq)                     \
+do {                                                                    \
+    if (MPIDI_CH4_MT_MODEL == MPIDI_CH4_MT_TRYLOCK) {                   \
+        MPID_THREAD_CS_TRYENTER(name, mutex, cs_acq);                   \
+    } else if (MPIDI_CH4_MT_MODEL == MPIDI_CH4_MT_HANDOFF) {            \
+        cs_acq = 0;                                                     \
+    } else {                                                            \
+        /* Direct */                                                    \
+        MPIDU_THREAD_CS_ENTER(name, mutex);                             \
+        cs_acq = 1;                                                     \
+    }                                                                   \
+} while (0)
+
+#define MPID_THREAD_SAFE_END(name, mutex) MPIDU_THREAD_CS_EXIT(name, mutex)
+
 #define MPID_Thread_mutex_create   MPIDU_Thread_mutex_create
 #define MPID_Thread_mutex_destroy  MPIDU_Thread_mutex_destroy
 #define MPID_Thread_mutex_lock     MPIDU_Thread_mutex_lock
