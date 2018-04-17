afcaad083cc12312adc2a592de64fe343f08dcfe
Hajime Fujita <hajime.fujita@intel.com>
2018-04-17 16:52:02 -0500

    ch4/ofi: Remove redundant assertion
    MPIR_Object_release_ref returns a boolean, so this assertion
    does not make sense.
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpid/ch4/netmod/ofi/ofi_impl.h
+++ src/mpid/ch4/netmod/ofi/ofi_impl.h
@@ -323,7 +323,6 @@ MPL_STATIC_INLINE_PREFIX void MPIDI_OFI_win_request_complete(MPIDI_OFI_win_reque
     int in_use;
     MPIR_Assert(HANDLE_GET_MPI_KIND(req->handle) == MPIR_REQUEST);
     MPIR_Object_release_ref(req, &in_use);
-    MPIR_Assert(in_use >= 0);
     if (!in_use) {
         MPL_free(req->noncontig);
         MPIR_Handle_obj_free(&MPIR_Request_mem, (req));
