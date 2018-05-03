570bd7b8e71dc575f695028f0244ab5e967b8fa1
Hoang-Vu Dang <dang.hvu@gmail.com>
2018-05-03 16:11:47 -0500

    request: Simplify MPID_Wait logic for generalized request
    There is no point for checking wait_fn, we already make progress.
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpi/request/wait.c
+++ src/mpi/request/wait.c
@@ -45,9 +45,6 @@ int MPIR_Wait_impl(MPIR_Request * request_ptr, MPI_Status * status)
                 (request_ptrs[i]->u.ureq.greq_fns->poll_fn) (request_ptrs[i]->u.ureq.
                                                              greq_fns->grequest_extra_state,
                                                              &array_of_statuses[i]);
-        }
-        if (request_ptr->kind == MPIR_REQUEST_KIND__GREQUEST &&
-            request_ptr->u.ureq.greq_fns->wait_fn != NULL) {
             if (mpi_errno) {
                 /* --BEGIN ERROR HANDLING-- */
                 MPID_Progress_end(&progress_state);
