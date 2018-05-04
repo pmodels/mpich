1773703b815d3aafbbf13158e54c41d516440273
Wesley Bland <wesley.bland@intel.com>
2018-05-04 10:20:37 -0500

    ch4/shm: Allow non-direct builds
    Make changes to the structure of the CH4 shared memory modules to allow
    non-direct builds (where the shared memory module code is inlined
    "directly" into the MPI-level code). This makes the "glue" code (now
    just called `src`) required and makes the POSIX code required as well
    (in fact, it's not used directly in the CH4-level code in places).
    
    This simplifies the autoconf code since we will always assume that the
    `shm/src` code will be the starting point for shared memory calls. In
    the future, when we have multiple submodules in shared memory that can
    be built together, we will probably need to reintroduce some of the
    autoconf magic within the shared memory code itself (instead of at the
    CH4 layer).
    
    This assumes that the decision to inline will always happen at the CH4
    layer (and not inside the shared memory code itself). When we have
    multiple shared memory modules in the future, they should all be inlined
    underneath the shared memory code (if they will be inlined at all).
    
    Signed-off-by: Min Si <msi@anl.gov>
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

---
+++ src/mpid/ch4/include/shmpre.h
@@ -0,0 +1,21 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2006 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ *
+ *  Portions of this code were written by Intel Corporation.
+ *  Copyright (C) 2011-2016 Intel Corporation.  Intel provides this material
+ *  to Argonne National Laboratory subject to Software Grant and Corporate
+ *  Contributor License Agreement dated February 8, 2012.
+ */
+#ifndef SHMPRE_H_INCLUDED
+#define SHMPRE_H_INCLUDED
+
+/* *INDENT-OFF* */
+#include "../shm/src/shm_pre.h"
+/* *INDENT-ON* */
+
+#define MPIDI_SHM_REQUEST_DECL       MPIDI_POSIX_request_t posix;
+#define MPIDI_SHM_COMM_DECL          MPIDI_POSIX_comm_t posix;
+
+#endif /* SHMPRE_H_INCLUDED */
---
+++ src/mpid/ch4/shm/src/shm_pre.h
@@ -0,0 +1,20 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2006 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ *
+ *  Portions of this code were written by Intel Corporation.
+ *  Copyright (C) 2011-2016 Intel Corporation.  Intel provides this material
+ *  to Argonne National Laboratory subject to Software Grant and Corporate
+ *  Contributor License Agreement dated February 8, 2012.
+ */
+
+#ifndef SHM_PRE_H_INCLUDED
+#define SHM_PRE_H_INCLUDED
+
+#include <mpi.h>
+
+#include "../posix/posix_pre.h"
+
+#include "shm_coll_params.h"
+#endif /* SHM_PRE_H_INCLUDED */
