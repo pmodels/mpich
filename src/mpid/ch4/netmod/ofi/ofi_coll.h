57143c9dce31bbbde70242589a57884c21eb37f6
Marat Shamshetdinov <marat.shamshetdinov@intel.com>
2018-05-08 21:36:55 +0300

    ch4/coll: Add Ibcast CS for ofi netmod
    Ibcast collective selection for ofi netmod.

--- src/mpid/ch4/netmod/ofi/ofi_coll.h
+++ src/mpid/ch4/netmod/ofi/ofi_coll.h
@@ -1070,17 +1070,55 @@ static inline int MPIDI_NM_mpi_ibarrier(MPIR_Comm * comm, MPIR_Request ** req)
 #define FCNAME MPL_QUOTE(FUNCNAME)
 static inline int MPIDI_NM_mpi_ibcast(void *buffer, int count, MPI_Datatype datatype,
                                       int root, MPIR_Comm * comm_ptr, MPIR_Request ** request,
-                                      const void *ch4_algo_parameters_container_in
-                                      ATTRIBUTE((unused)))
+                                      const void *ch4_algo_parameters_container_in)
 {
-    int mpi_errno;
+    int mpi_errno = MPI_SUCCESS;
+    const MPIDI_OFI_coll_algo_container_t *nm_algo_parameters_container_out = NULL;
+
     MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_NM_MPI_IBCAST);
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_NM_MPI_IBCAST);
 
-    mpi_errno = MPIR_Ibcast_impl(buffer, count, datatype, root, comm_ptr, request);
+    nm_algo_parameters_container_out =
+        MPIDI_OFI_Ibcast_select(buffer, count, datatype, root, comm_ptr, request,
+                                ch4_algo_parameters_container_in);
+
+    switch (nm_algo_parameters_container_out->id) {
+        case MPIDI_OFI_Ibcast_intra_nbc_binomial_id:
+            mpi_errno =
+                MPIR_Ibcast_intra_nbc_binomial(buffer, count, datatype, root, comm_ptr, request);
+            break;
+        case MPIDI_OFI_Ibcast_intra_nbc_scatter_recursive_doubling_allgather_id:
+            mpi_errno =
+                MPIR_Ibcast_intra_nbc_scatter_recursive_doubling_allgather(buffer, count, datatype,
+                                                                           root, comm_ptr, request);
+            break;
+        case MPIDI_OFI_Ibcast_intra_nbc_scatter_ring_allgather_id:
+            mpi_errno =
+                MPIR_Ibcast_intra_nbc_scatter_ring_allgather(buffer, count, datatype, root,
+                                                             comm_ptr, request);
+            break;
+        case MPIDI_OFI_Ibcast_intra_tree_knomial_id:
+            mpi_errno =
+                MPIR_Ibcast_intra_tree_knomial(buffer, count, datatype, root, comm_ptr, request);
+            break;
+        case MPIDI_OFI_Ibcast_intra_tree_kary_id:
+            mpi_errno =
+                MPIR_Ibcast_intra_tree_kary(buffer, count, datatype, root, comm_ptr, request);
+            break;
+        default:
+            mpi_errno = MPIR_Ibcast_impl(buffer, count, datatype, root, comm_ptr, request);
+            break;
+    }
+
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
 
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_NM_MPI_IBCAST);
+
+  fn_exit:
     return mpi_errno;
+  fn_fail:
+    goto fn_exit;
 }
 
 #undef FUNCNAME
