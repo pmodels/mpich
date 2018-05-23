c22621c4cbf0155ae067fab761ce752ae4ebb3ff
Min Si <msi@anl.gov>
2018-05-23 17:41:07 -0500

    ch4: defines generic collective shm segment creation
    MPL_shm* is a consistent way to allocate shared memory segment. However,
    it cannot use MPI collectives internally, thus the caller has to
    exchange segment handlers and ensure synchronization. This patch defines
    a generic shm segment creation routine in CH4, which collectively
    allocates a shared segment in the given communicator. MPL_shm* and
    collectives are internally used.
    
    The symmetric_flag is defined to create symmetric shm segment. It is
    left as future work.
    
    Signed-off-by: Chongxiao Cao <chongxiao.cao@intel.com>

--- src/mpid/ch4/src/ch4_impl.h
+++ src/mpid/ch4/src/ch4_impl.h
@@ -935,4 +935,89 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_CH4U_wait_am_acc(MPIR_Win * win, int target_r
   fn_fail:
     goto fn_exit;
 }
+
+/* Collectively allocate shared memory region.
+ * MPL_shm routines and MPI collectives are internally used.
+ *
+ * TODO: If symmetric_flag is true, then the routine will try to get
+ * a symmetric address and give up after tried predefined times. */
+#undef FUNCNAME
+#define FUNCNAME MPIDI_CH4U_allocate_shm_segment
+#undef FCNAME
+#define FCNAME MPL_QUOTE(FUNCNAME)
+static inline int MPIDI_CH4U_allocate_shm_segment(MPIR_Comm * shm_comm_ptr,
+                                                  MPI_Aint shm_segment_len, int symmetric_flag,
+                                                  MPL_shm_hnd_t * shm_segment_hdl_ptr,
+                                                  void **base_ptr)
+{
+    MPIR_Errflag_t errflag = MPIR_ERR_NONE;
+    int mpi_errno = MPI_SUCCESS;
+    MPL_shm_hnd_t shm_segment_handle;
+    void *map_ptr = NULL;
+
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_CH4R_ALLOCATE_SHM_SEGMENT);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_CH4R_ALLOCATE_SHM_SEGMENT);
+
+    mpi_errno = MPL_shm_hnd_init(&shm_segment_handle);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    if (shm_comm_ptr->rank == 0) {
+        char *serialized_hnd_ptr = NULL;
+
+        /* create shared memory region for all processes in win and map */
+        mpi_errno = MPL_shm_seg_create_and_attach(shm_segment_handle,
+                                                  shm_segment_len, (char **) &map_ptr, 0);
+        if (mpi_errno)
+            MPIR_ERR_POP(mpi_errno);
+
+        /* serialize handle and broadcast it to the other processes in win */
+        mpi_errno = MPL_shm_hnd_get_serialized_by_ref(shm_segment_handle, &serialized_hnd_ptr);
+        if (mpi_errno)
+            MPIR_ERR_POP(mpi_errno);
+
+        mpi_errno = MPIR_Bcast(serialized_hnd_ptr, MPL_SHM_GHND_SZ, MPI_CHAR, 0,
+                               shm_comm_ptr, &errflag);
+        MPIR_ERR_CHKANDJUMP(errflag, mpi_errno, MPI_ERR_OTHER, "**coll_fail");
+
+        /* wait for other processes to attach to win */
+        mpi_errno = MPIR_Barrier(shm_comm_ptr, &errflag);
+        MPIR_ERR_CHKANDJUMP(errflag, mpi_errno, MPI_ERR_OTHER, "**coll_fail");
+
+        /* unlink shared memory region so it gets deleted when all processes exit */
+        mpi_errno = MPL_shm_seg_remove(shm_segment_handle);
+        if (mpi_errno)
+            MPIR_ERR_POP(mpi_errno);
+    } else {
+        char serialized_hnd[MPL_SHM_GHND_SZ] = { 0 };
+
+        /* get serialized handle from rank 0 and deserialize it */
+        mpi_errno = MPIR_Bcast(serialized_hnd, MPL_SHM_GHND_SZ, MPI_CHAR, 0,
+                               shm_comm_ptr, &errflag);
+        MPIR_ERR_CHKANDJUMP(errflag, mpi_errno, MPI_ERR_OTHER, "**coll_fail");
+
+        mpi_errno = MPL_shm_hnd_deserialize(shm_segment_handle, serialized_hnd,
+                                            strlen(serialized_hnd));
+        if (mpi_errno)
+            MPIR_ERR_POP(mpi_errno);
+
+        /* attach to shared memory region created by rank 0 */
+        mpi_errno = MPL_shm_seg_attach(shm_segment_handle, shm_segment_len, (char **) &map_ptr, 0);
+        if (mpi_errno)
+            MPIR_ERR_POP(mpi_errno);
+
+        mpi_errno = MPIR_Barrier(shm_comm_ptr, &errflag);
+        MPIR_ERR_CHKANDJUMP(errflag, mpi_errno, MPI_ERR_OTHER, "**coll_fail");
+    }
+
+    *shm_segment_hdl_ptr = shm_segment_handle;
+    *base_ptr = map_ptr;
+
+  fn_exit:
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_CH4R_ALLOCATE_SHM_SEGMENT);
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
+}
+
 #endif /* CH4_IMPL_H_INCLUDED */
