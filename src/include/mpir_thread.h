24341aaa816f395ca4d12d3e098cdf0a1e00ed6e
Hoang-Vu Dang <dang.hvu@gmail.com>
2018-05-30 17:09:27 -0500

    mpi: implement MDTA configuration and data structure
    MDTA code optimizes blocking call for global granularity:
    1) allow threads to wait in a synchronization object associated with
    request pointers that they are waiting for, to reduce contention in the CS.
    2) keep track of how many threads are being in the progress engine to
    make sure at least one thread makes progress.
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/include/mpir_thread.h
+++ src/include/mpir_thread.h
@@ -10,6 +10,7 @@
 
 #include "mpichconfconst.h"
 #include "mpichconf.h"
+#include "utlist.h"
 
 typedef struct {
     int thread_provided;        /* Provided level of thread support */
@@ -26,6 +27,24 @@ typedef struct {
 #endif                          /* MPICH_IS_THREADED */
 } MPIR_Thread_info_t;
 extern MPIR_Thread_info_t MPIR_ThreadInfo;
+
+#ifdef MPICH_THREAD_USE_MDTA
+typedef struct MPIR_Thread_sync {
+    struct MPIR_Thread_sync *next;
+    struct MPIR_Thread_sync *prev;
+    int is_server;
+    int is_initialized;
+    OPA_int_t count;
+    MPID_Thread_cond_t cond;
+} MPIR_Thread_sync_t;
+
+typedef struct MPIR_Thread_sync_list {
+    MPIR_Thread_sync_t *head;
+} MPIR_Thread_sync_list_t;
+
+extern MPIR_Thread_sync_list_t sync_wait_list;
+extern OPA_int_t num_server_thread;
+#endif
 
 /* ------------------------------------------------------------------------- */
 /* thread-local storage macros */
@@ -47,6 +66,10 @@ typedef struct {
 
 #if (MPICH_THREAD_LEVEL == MPI_THREAD_MULTIPLE)
     int lock_depth;
+
+#ifdef MPICH_THREAD_USE_MDTA
+    MPIR_Thread_sync_t sync;
+#endif
 #endif
 } MPIR_Per_thread_t;
 
@@ -81,5 +104,66 @@ extern MPID_Thread_mutex_t MPIR_THREAD_POBJ_PMI_MUTEX;
 #define MPIR_THREAD_POBJ_COMM_MUTEX(_comm_ptr) _comm_ptr->mutex
 #define MPIR_THREAD_POBJ_WIN_MUTEX(_win_ptr)   _win_ptr->mutex
 #endif
+
+#ifdef MPICH_THREAD_USE_MDTA
+
+MPL_STATIC_INLINE_PREFIX void MPIR_Thread_sync_signal(MPIR_Thread_sync_t * sync, const int force)
+{
+    int rc;
+    if (force) {
+        MPID_Thread_cond_signal(&sync->cond, &rc);
+        return;
+    }
+
+    OPA_decr_int(&(sync->count));
+    if (OPA_load_int(&(sync->count)) == 0) {
+        MPID_Thread_cond_signal(&sync->cond, &rc);
+    }
+}
+
+MPL_STATIC_INLINE_PREFIX void MPIR_Thread_sync_alloc(MPIR_Thread_sync_t ** sync, const int count)
+{
+    int rc;
+    MPIR_Per_thread_t *per_thread = NULL;
+    MPID_THREADPRIV_KEY_GET_ADDR(MPIR_ThreadInfo.isThreaded, MPIR_Per_thread_key,
+                                 MPIR_Per_thread, per_thread, &rc);
+    *sync = &per_thread->sync;
+    (*sync)->is_server = FALSE;
+    OPA_store_int(&((*sync)->count), count);
+    if (unlikely(!(*sync)->is_initialized)) {
+        MPID_Thread_cond_create(&((*sync)->cond), &rc);
+        (*sync)->is_initialized = TRUE;
+    }
+}
+
+MPL_STATIC_INLINE_PREFIX void MPIR_Thread_sync_free(MPIR_Thread_sync_t * sync)
+{
+    if (sync->is_server) {
+        OPA_decr_int(&num_server_thread);
+        sync->is_server = FALSE;
+    }
+    if (OPA_load_int(&num_server_thread) == 0 && sync_wait_list.head != NULL) {
+        MPIR_Thread_sync_signal(sync_wait_list.head, /* force */ 1);
+    }
+}
+
+MPL_STATIC_INLINE_PREFIX void MPIR_Thread_sync_wait(MPIR_Thread_sync_t * sync)
+{
+    int rc;
+
+    if (!sync->is_server && OPA_load_int(&num_server_thread) == 0) {
+        OPA_incr_int(&num_server_thread);
+        sync->is_server = TRUE;
+        return;
+    }
+
+    if (!sync->is_server && OPA_load_int(&sync->count) > 0) {
+        DL_APPEND(sync_wait_list.head, sync);
+        MPID_Thread_cond_wait(&sync->cond, &MPIR_THREAD_GLOBAL_ALLFUNC_MUTEX, &rc);
+        DL_DELETE(sync_wait_list.head, sync);
+    }
+}
+
+#endif /* MPICH_THREAD_USE_MDTA */
 
 #endif /* MPIR_THREAD_H_INCLUDED */
