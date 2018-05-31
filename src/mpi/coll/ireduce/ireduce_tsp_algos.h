2d6398347e119270c368f3ab6160d1f9dea998fa
Akhil Langer <akhil.langer@intel.com>
2018-05-31 08:53:47 -0500

    coll: Use MPIR_Sched_next_tag
    Instead of MPIDU_Sched_next_tag, for consistency with other usage.
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpi/coll/ireduce/ireduce_tsp_algos.h
+++ src/mpi/coll/ireduce/ireduce_tsp_algos.h
@@ -268,7 +268,7 @@ int MPIR_TSP_Ireduce_intra_tree(const void *sendbuf, void *recvbuf, int count,
 
     /* For correctness, transport based collectives need to get the
      * tag from the same pool as schedule based collectives */
-    mpi_errno = MPIDU_Sched_next_tag(comm, &tag);
+    mpi_errno = MPIR_Sched_next_tag(comm, &tag);
     if (mpi_errno)
         MPIR_ERR_POP(mpi_errno);
 
