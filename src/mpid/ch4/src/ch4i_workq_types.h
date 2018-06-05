9c2c2fd7ff2778e25ef0367130676f348e6d1bd3
Halim Amer <aamer@anl.gov>
2018-06-05 12:54:00 -0500

    ch4: Add work queue infrastructure
    This patch adds a basic infrastructure for work queue, like
    enqueue/dequeue functions and work dispatch function.
    
    Co-authored-by: Hajime Fujita <hajime.fujita@intel.com>
    Signed-off-by: Yanfei Guo <yguo@anl.gov>

--- src/mpid/ch4/src/ch4i_workq_types.h
+++ src/mpid/ch4/src/ch4i_workq_types.h
@@ -25,6 +25,8 @@ MPL_STATIC_INLINE_PREFIX void MPIDI_workq_dequeue(MPIDI_workq_t * q, void **pp)
 {
 }
 
+#define MPIDI_WORKQ_ELEMT_PREALLOC 64   /* Number of elements to preallocate in the "direct" block */
+
 typedef enum MPIDI_workq_op MPIDI_workq_op_t;
 
 /* Indentifies the delegated operation */
