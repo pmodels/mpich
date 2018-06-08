cdbbb99013f909b6f19095dab4c15547b0d69060
Taru Doodi <taru.doodi@intel.com>
2018-06-08 17:07:13 -0500

    coll: Add ring algorithm for allreduce collective
    Co-authored-by: Rubasri Kalidas <rubasri.kalidas@intel.com>
    Co-authored-by: Akhil Langer <akhil.langer@intel.com>
    Signed-off-by: Hui Zhou <hzhou321@anl.gov>

--- src/mpi/coll/iallreduce/Makefile.mk
+++ src/mpi/coll/iallreduce/Makefile.mk
@@ -24,4 +24,5 @@ mpi_core_sources +=                                         \
     src/mpi/coll/iallreduce/iallreduce_gentran_algos.c                    \
     src/mpi/coll/iallreduce/iallreduce_intra_recexch_single_buffer.c    \
     src/mpi/coll/iallreduce/iallreduce_intra_recexch_multiple_buffer.c \
-    src/mpi/coll/iallreduce/iallreduce_intra_tree.c
+    src/mpi/coll/iallreduce/iallreduce_intra_tree.c \
+    src/mpi/coll/iallreduce/iallreduce_intra_ring.c
