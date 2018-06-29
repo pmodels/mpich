7437c45042fe79552de3269d56d14d662cbec253
Akhil Langer <akhil.langer@intel.com>
2018-06-29 13:55:39 -0500

    coll: Add CVAR to select tree type for reduce
    Each tree type, for example, kary, knomial, had different
    algorithms to their name - tree_kary, tree_knomial, respectively.
    This is not very scalable as we add more tree types. This commit
    adds a single function to select tree algo and then the tree
    type can be selected through a CVAR - MPIR_CVAR_IREDUCE_TREE_TYPE.
    With this change, as we add more tree types, we do not have to
    add new functions to the code to run that tree_type algorithm.
    It can be selected just through the CVAR.
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpi/coll/ireduce/ireduce_intra_tree.c
+++ src/mpi/coll/ireduce/ireduce_intra_tree.c
@@ -17,17 +17,17 @@
 #include "tsp_undef.h"
 
 #undef FUNCNAME
-#define FUNCNAME MPIR_Ireduce_intra_tree_kary
+#define FUNCNAME MPIR_Ireduce_intra_tree
 #undef FCNAME
 #define FCNAME MPL_QUOTE(FUNCNAME)
-int MPIR_Ireduce_intra_tree_kary(const void *sendbuf, void *recvbuf, int count,
-                                 MPI_Datatype datatype, MPI_Op op, int root, MPIR_Comm * comm_ptr,
-                                 MPIR_Request ** request)
+int MPIR_Ireduce_intra_tree(const void *sendbuf, void *recvbuf, int count,
+                            MPI_Datatype datatype, MPI_Op op, int root, MPIR_Comm * comm_ptr,
+                            MPIR_Request ** request)
 {
     int mpi_errno = MPI_SUCCESS;
 
     mpi_errno = MPII_Gentran_Ireduce_intra_tree(sendbuf, recvbuf, count, datatype, op, root,
-                                                comm_ptr, request, TREE_TYPE_KARY,
+                                                comm_ptr, request, MPIR_CVAR_IREDUCE_TREE_TYPE,
                                                 MPIR_CVAR_IREDUCE_TREE_KVAL,
                                                 MPIR_CVAR_IREDUCE_TREE_PIPELINE_CHUNK_SIZE);
 
