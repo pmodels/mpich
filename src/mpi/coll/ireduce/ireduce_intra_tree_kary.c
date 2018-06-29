126b3150500d2724daf92e9262ffd8486f080354
Akhil Langer <akhil.langer@intel.com>
2018-06-29 13:50:35 -0500

    coll: Fix function name in FUNCNAME macro
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpi/coll/ireduce/ireduce_intra_tree_kary.c
+++ src/mpi/coll/ireduce/ireduce_intra_tree_kary.c
@@ -17,7 +17,7 @@
 #include "tsp_undef.h"
 
 #undef FUNCNAME
-#define FUNCNAME MPIR_Ireduce_intra_tree_knomial
+#define FUNCNAME MPIR_Ireduce_intra_tree_kary
 #undef FCNAME
 #define FCNAME MPL_QUOTE(FUNCNAME)
 int MPIR_Ireduce_intra_tree_kary(const void *sendbuf, void *recvbuf, int count,
