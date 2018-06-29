f683d51c7bff2d8aa0d16b6ac42076f2fb6e1071
Akhil Langer <akhil.langer@intel.com>
2018-06-29 15:57:06 -0500

    coll: Add knomial_2 tree type
    This knomial tree is of the same structure as the binomial
    tree in the old collectives algorithms in MPICH. We noted
    that this type of tree gives better performance than
    knomial_1 tree type in some cases, although it is difficult
    to explain why. We, therefore, want to keep both
    knomial_1 and knomial_2 tree types. The difference the two tree types
    is documented through tree examples in treeutil.c file
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpi/coll/algorithms/treealgo/treeutil.h
+++ src/mpi/coll/algorithms/treealgo/treeutil.h
@@ -19,4 +19,8 @@ int MPII_Treeutil_tree_kary_init(int rank, int nranks, int k, int root, MPII_Tre
 int MPII_Treeutil_tree_knomial_1_init(int rank, int nranks, int k, int root,
                                       MPII_Treealgo_tree_t * ct);
 
+/* Generate knomial_2 tree information for rank 'rank' */
+int MPII_Treeutil_tree_knomial_2_init(int rank, int nranks, int k, int root,
+                                      MPII_Treealgo_tree_t * ct);
+
 #endif /* TREEUTIL_H_INCLUDED */
