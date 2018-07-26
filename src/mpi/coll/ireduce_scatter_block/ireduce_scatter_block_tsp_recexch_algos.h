9d4742422ed83a15ad3bbbb0594cc955fc56146a
Akhil Langer <akhil.langer@intel.com>
2018-07-26 10:18:28 -0500

    coll: Use MPIR_Sched_next_tag and catch returned error
    Also fixes Coverity error #187198
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpi/coll/ireduce_scatter_block/ireduce_scatter_block_tsp_recexch_algos.h
+++ src/mpi/coll/ireduce_scatter_block/ireduce_scatter_block_tsp_recexch_algos.h
@@ -230,7 +230,9 @@ int MPIR_TSP_Ireduce_scatter_block_intra_recexch(const void *sendbuf, void *recv
 
     /* For correctness, transport based collectives need to get the
      * tag from the same pool as schedule based collectives */
-    MPIDU_Sched_next_tag(comm, &tag);
+    mpi_errno = MPIR_Sched_next_tag(comm, &tag);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
 
     mpi_errno =
         MPIR_TSP_Ireduce_scatter_block_sched_intra_recexch(sendbuf, recvbuf, recvcount, datatype,
