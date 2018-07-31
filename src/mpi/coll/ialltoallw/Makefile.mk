33a19e2607facd50c0fba92468def20e57d0a46c
Akhil Langer <akhil.langer@intel.com>
2018-07-31 12:33:40 -0500

    coll: Add Alltoallw inplace algorithm

--- src/mpi/coll/ialltoallw/Makefile.mk
+++ src/mpi/coll/ialltoallw/Makefile.mk
@@ -20,4 +20,5 @@ mpi_core_sources +=                               \
     src/mpi/coll/ialltoallw/ialltoallw_intra_blocked.c  \
     src/mpi/coll/ialltoallw/ialltoallw_inter_pairwise_exchange.c	\
     src/mpi/coll/ialltoallw/ialltoallw_gentran_algos.c	\
-    src/mpi/coll/ialltoallw/ialltoallw_intra_gentran_blocked.c
+    src/mpi/coll/ialltoallw/ialltoallw_intra_gentran_blocked.c	\
+    src/mpi/coll/ialltoallw/ialltoallw_intra_gentran_inplace.c
