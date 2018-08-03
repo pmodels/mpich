44b8a00e0c5df8c0c2412b6bcd30f54f5c3ea216
Akhil Langer <akhil.langer@intel.com>
2018-08-03 16:07:24 -0500

    coll: Implement gentran_linear algo for nhb_allgather
    Implement linear algo for ineighbor_allgather using
    the new collectives scheduler.

---
+++ src/mpi/coll/ineighbor_allgather/ineighbor_allgather_tsp_linear_algos.h
@@ -0,0 +1,121 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2006 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ *
+ *  Portions of this code were written by Intel Corporation.
+ *  Copyright (C) 2011-2017 Intel Corporation.  Intel provides this material
+ *  to Argonne National Laboratory subject to Software Grant and Corporate
+ *  Contributor License Agreement dated February 8, 2012.
+ */
+
+/* Header protection (i.e., INEIGHBOR_ALLGATHER_TSP_LINEAR_ALGOS_H_INCLUDED) is
+ * intentionally omitted since this header might get included multiple
+ * times within the same .c file. */
+
+#include "algo_common.h"
+#include "treealgo.h"
+#include "tsp_namespace_def.h"
+
+/* Routine to schedule a pipelined tree based broadcast */
+#undef FUNCNAME
+#define FUNCNAME MPIR_TSP_Ineighbor_allgather_sched_allcomm_tree
+#undef FCNAME
+#define FCNAME MPL_QUOTE(FUNCNAME)
+int MPIR_TSP_Ineighbor_allgather_sched_allcomm_linear(const void *sendbuf, int sendcount,
+                                                      MPI_Datatype sendtype, void *recvbuf,
+                                                      int recvcount, MPI_Datatype recvtype,
+                                                      MPIR_Comm * comm_ptr,
+                                                      MPIR_TSP_sched_t * sched)
+{
+    int mpi_errno = MPI_SUCCESS;
+    int indegree, outdegree, weighted;
+    int k, l;
+    int *srcs, *dsts;
+    int tag;
+    MPI_Aint recvtype_extent;
+    MPIR_CHKLMEM_DECL(2);
+
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIR_TSP_INEIGHBOR_ALLGATHER_SCHED_INTRA_LINEAR);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIR_TSP_INEIGHBOR_ALLGATHER_SCHED_INTRA_LINEAR);
+
+    MPIR_Datatype_get_extent_macro(recvtype, recvtype_extent);
+
+    /* This is the largest offset we add to recvbuf */
+    MPIR_Ensure_Aint_fits_in_pointer(MPIR_VOID_PTR_CAST_TO_MPI_AINT recvbuf +
+                                     (comm_ptr->local_size * recvcount * recvtype_extent));
+
+    mpi_errno = MPIR_Topo_canon_nhb_count(comm_ptr, &indegree, &outdegree, &weighted);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+    MPIR_CHKLMEM_MALLOC(srcs, int *, indegree * sizeof(int), mpi_errno, "srcs", MPL_MEM_COMM);
+    MPIR_CHKLMEM_MALLOC(dsts, int *, outdegree * sizeof(int), mpi_errno, "dsts", MPL_MEM_COMM);
+    mpi_errno = MPIR_Topo_canon_nhb(comm_ptr,
+                                    indegree, srcs, MPI_UNWEIGHTED,
+                                    outdegree, dsts, MPI_UNWEIGHTED);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    /* For correctness, transport based collectives need to get the
+     * tag from the same pool as schedule based collectives */
+    mpi_errno = MPIR_Sched_next_tag(comm_ptr, &tag);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    for (k = 0; k < outdegree; ++k) {
+        MPIR_TSP_sched_isend(sendbuf, sendcount, sendtype, dsts[k], tag, comm_ptr, sched, 0, NULL);
+    }
+
+    for (l = 0; l < indegree; ++l) {
+        char *rb = ((char *) recvbuf) + l * recvcount * recvtype_extent;
+        MPIR_TSP_sched_irecv(rb, recvcount, recvtype, srcs[l], tag, comm_ptr, sched, 0, NULL);
+    }
+
+  fn_exit:
+    MPIR_CHKLMEM_FREEALL();
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIR_TSP_INEIGHBOR_ALLGATHER_SCHED_INTRA_LINEAR);
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
+}
+
+
+/* Non-blocking linear algo based neighbor_allgather */
+#undef FUNCNAME
+#define FUNCNAME MPIR_TSP_Ineighbor_allgather_allcomm_linear
+#undef FCNAME
+#define FCNAME MPL_QUOTE(FUNCNAME)
+int MPIR_TSP_Ineighbor_allgather_allcomm_linear(const void *sendbuf, int sendcount,
+                                                MPI_Datatype sendtype, void *recvbuf, int recvcount,
+                                                MPI_Datatype recvtype, MPIR_Comm * comm_ptr,
+                                                MPIR_Request ** req)
+{
+    int mpi_errno = MPI_SUCCESS;
+    MPIR_TSP_sched_t *sched;
+    *req = NULL;
+
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIR_TSP_IBCAST_INTRA_TREE);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIR_TSP_IBCAST_INTRA_TREE);
+
+    /* generate the schedule */
+    sched = MPL_malloc(sizeof(MPIR_TSP_sched_t), MPL_MEM_COLL);
+    MPIR_TSP_sched_create(sched);
+
+    /* schedule pipelined tree algo */
+    mpi_errno = MPIR_TSP_Ineighbor_allgather_sched_allcomm_linear(sendbuf, sendcount, sendtype,
+                                                                  recvbuf, recvcount, recvtype,
+                                                                  comm_ptr, sched);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+    /* start and register the schedule */
+    mpi_errno = MPIR_TSP_sched_start(sched, comm_ptr, req);
+    if (mpi_errno)
+        MPIR_ERR_POP(mpi_errno);
+
+  fn_exit:
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIR_TSP_INEIGHBOR_ALLGATHER_INTRA_LINEAR);
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
+}
