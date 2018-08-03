1be1084b7ed66d33e62f6bc951ede93513ee06b0
Min Si <msi@anl.gov>
2018-08-03 15:09:48 -0500

    ch4/rma: return correct iov at target put callback
    In MPIDI_put_target_msg_cb, the current code did not return newly
    allocated iov buffer when target datatype is noncontig. Instead, it
    returned the payload part associated with transport message. It may
    cause undefined behavior because the payload part maybe reset before the
    put transfer is done (e.g., SHM AM/iqueue divides large put into several
    cells). This patch fixes it.
    
    Signed-off-by: Wesley Bland <wesley.bland@intel.com>

--- src/mpid/ch4/src/ch4r_rma_target_callbacks.h
+++ src/mpid/ch4/src/ch4r_rma_target_callbacks.h
@@ -1651,7 +1651,7 @@ static inline int MPIDI_put_target_msg_cb(int handler_id, void *am_hdr,
         MPIDI_CH4U_REQUEST(rreq, req->preq.dt_iov) = dt_iov;
         MPIDI_CH4U_REQUEST(rreq, req->preq.n_iov) = msg_hdr->n_iov;
         *is_contig = 0;
-        *data = iov;
+        *data = dt_iov;
         *p_data_sz = msg_hdr->n_iov;
         goto fn_exit;
     }
