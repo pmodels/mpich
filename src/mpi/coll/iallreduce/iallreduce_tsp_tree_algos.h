779d7da1a502dc1f74ae6d5f271138876c11883e
Akhil Langer <akhil.langer@intel.com>
2018-08-03 12:26:18 -0500

    coll: Initialize pointers
    Fixes coverity errors
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpi/coll/iallreduce/iallreduce_tsp_tree_algos.h
+++ src/mpi/coll/iallreduce/iallreduce_tsp_tree_algos.h
@@ -41,7 +41,7 @@ int MPIR_TSP_Iallreduce_sched_intra_tree(const void *sendbuf, void *recvbuf, int
     MPII_Treealgo_tree_t my_tree;
     void **child_buffer;        /* Buffer array in which data from children is received */
     void *reduce_buffer;        /* Buffer in which allreduced data is present */
-    int *vtcs, *recv_id, *reduce_id;    /* Arrays to store graph vertex ids */
+    int *vtcs = NULL, *recv_id = NULL, *reduce_id = NULL;       /* Arrays to store graph vertex ids */
     int send_id, sink_id, bcast_recv_id;
     int nvtcs;
     int buffer_per_child = MPIR_CVAR_IALLREDUCE_TREE_BUFFER_PER_CHILD;
