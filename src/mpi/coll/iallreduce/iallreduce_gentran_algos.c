745af79882d689753c7c6b49621231e4fc74db7b
Rubasri Kalidas <rubasri.kalidas@intel.com>
2018-08-13 09:34:57 -0500

    coll: Add recexch reduce scatter - recexch allgatherv algorithm for allreduce
    This commit adds allreduce using reduce scatter
    followed by allgatherv (recursive exchange). This algorithm
    allows us to have 'K' number of processes participating
    in each exchange.

--- src/mpi/coll/iallreduce/iallreduce_gentran_algos.c
+++ src/mpi/coll/iallreduce/iallreduce_gentran_algos.c
@@ -32,4 +32,9 @@
 #include "iallreduce_tsp_recursive_exchange_common.h"
 #include "iallreduce_tsp_recursive_exchange_common_undef.h"
 
+/* instantiate iallreduce recexch_reduce_scatter_recexch_allgatherv algorithms for the gentran transport */
+#include "iallreduce_tsp_recexch_reduce_scatter_recexch_allgatherv_algos_prototypes.h"
+#include "iallreduce_tsp_recexch_reduce_scatter_recexch_allgatherv_algos.h"
+#include "iallreduce_tsp_recexch_reduce_scatter_recexch_allgatherv_algos_undef.h"
+
 #include "tsp_undef.h"
