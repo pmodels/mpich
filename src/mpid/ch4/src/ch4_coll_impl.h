567ec78abd62fa7b7187f43fcbcd7a0a111c016d
Surabhi Jain <surabhi.jain@intel.com>
2018-08-16 14:26:55 -0500

    ch4/coll: Rename compositions of intra-reduce
    Rename (alpha, beta) compositions for Reduce to (beta, gamma).
    Composition alpha is added in the next commit, to better match the alpha
    composition for Bcast
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpid/ch4/src/ch4_coll_impl.h
+++ src/mpid/ch4/src/ch4_coll_impl.h
@@ -438,18 +438,18 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_Allreduce_inter_composition_alpha(const void 
 }
 
 #undef FUNCNAME
-#define FUNCNAME MPIDI_Reduce_intra_composition_alpha
+#define FUNCNAME MPIDI_Reduce_intra_composition_beta
 #undef FCNAME
 #define FCNAME MPL_QUOTE(FUNCNAME)
-MPL_STATIC_INLINE_PREFIX int MPIDI_Reduce_intra_composition_alpha(const void *sendbuf,
-                                                                  void *recvbuf, int count,
-                                                                  MPI_Datatype datatype,
-                                                                  MPI_Op op, int root,
-                                                                  MPIR_Comm * comm,
-                                                                  MPIR_Errflag_t * errflag,
-                                                                  const
-                                                                  MPIDI_coll_algo_container_t
-                                                                  * ch4_algo_parameters_container)
+MPL_STATIC_INLINE_PREFIX int MPIDI_Reduce_intra_composition_beta(const void *sendbuf,
+                                                                 void *recvbuf, int count,
+                                                                 MPI_Datatype datatype,
+                                                                 MPI_Op op, int root,
+                                                                 MPIR_Comm * comm,
+                                                                 MPIR_Errflag_t * errflag,
+                                                                 const
+                                                                 MPIDI_coll_algo_container_t
+                                                                 * ch4_algo_parameters_container)
 {
     int mpi_errno = MPI_SUCCESS;
     int mpi_errno_ret = MPI_SUCCESS;
@@ -592,18 +592,18 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_Reduce_intra_composition_alpha(const void *se
 
 
 #undef FUNCNAME
-#define FUNCNAME MPIDI_Reduce_intra_composition_beta
+#define FUNCNAME MPIDI_Reduce_intra_composition_gamma
 #undef FCNAME
 #define FCNAME MPL_QUOTE(FUNCNAME)
-MPL_STATIC_INLINE_PREFIX int MPIDI_Reduce_intra_composition_beta(const void *sendbuf,
-                                                                 void *recvbuf, int count,
-                                                                 MPI_Datatype datatype,
-                                                                 MPI_Op op, int root,
-                                                                 MPIR_Comm * comm,
-                                                                 MPIR_Errflag_t * errflag,
-                                                                 const
-                                                                 MPIDI_coll_algo_container_t
-                                                                 * ch4_algo_parameters_container)
+MPL_STATIC_INLINE_PREFIX int MPIDI_Reduce_intra_composition_gamma(const void *sendbuf,
+                                                                  void *recvbuf, int count,
+                                                                  MPI_Datatype datatype,
+                                                                  MPI_Op op, int root,
+                                                                  MPIR_Comm * comm,
+                                                                  MPIR_Errflag_t * errflag,
+                                                                  const
+                                                                  MPIDI_coll_algo_container_t
+                                                                  * ch4_algo_parameters_container)
 {
     int mpi_errno = MPI_SUCCESS;
     const void *reduce_container = MPIDI_coll_get_next_container(ch4_algo_parameters_container);
