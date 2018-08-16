517a673b1a8d471c5298876069334e1fc9883325
Surabhi Jain <surabhi.jain@intel.com>
2018-08-16 14:26:55 -0500

    ch4/coll: Rename compositions of intra-reduce
    Rename (alpha, beta) compositions for Reduce to (beta, gamma).
    Composition alpha is added in the next commit, to better match the alpha
    composition for Bcast
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpid/ch4/src/ch4_coll_params.h
+++ src/mpid/ch4/src/ch4_coll_params.h
@@ -41,19 +41,19 @@ typedef union {
 } MPIDI_Bcast_params_t;
 
 typedef enum {
-    MPIDI_Reduce_intra_composition_alpha_id,
     MPIDI_Reduce_intra_composition_beta_id,
+    MPIDI_Reduce_intra_composition_gamma_id,
     MPIDI_Reduce_inter_composition_alpha_id,
 } MPIDI_Reduce_id_t;
 
 typedef union {
-    struct MPIDI_Reduce_alpha {
+    struct MPIDI_Reduce_beta {
         int node_reduce;
         int roots_reduce;
-    } ch4_reduce_alpha;
-    struct MPIDI_Reduce_beta {
-        int reduce;
     } ch4_reduce_beta;
+    struct MPIDI_Reduce_gamma {
+        int reduce;
+    } ch4_reduce_gamma;
 } MPIDI_Reduce_params_t;
 
 typedef enum {
