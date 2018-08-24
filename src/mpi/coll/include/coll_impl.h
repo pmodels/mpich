b95269071e19e33b937ec681d3282d3c1deff2e2
Akhil Langer <akhil.langer@intel.com>
2018-08-23 20:22:23 -0500

    coll: Implement gentran_linear algo for nhb_alltoallv

--- src/mpi/coll/include/coll_impl.h
+++ src/mpi/coll/include/coll_impl.h
@@ -380,6 +380,7 @@ extern MPIR_Ineighbor_alltoall_inter_algo_t MPIR_Ineighbor_alltoall_inter_algo_c
 typedef enum MPIR_Ineighbor_alltoallv_intra_algo_t {
     MPIR_INEIGHBOR_ALLTOALLV_INTRA_ALGO_AUTO,
     MPIR_INEIGHBOR_ALLTOALLV_INTRA_ALGO_LINEAR,
+    MPIR_INEIGHBOR_ALLTOALLV_INTRA_ALGO_GENTRAN_LINEAR,
 } MPIR_Ineighbor_alltoallv_intra_algo_t;
 extern MPIR_Ineighbor_alltoallv_intra_algo_t MPIR_Ineighbor_alltoallv_intra_algo_choice;
 
