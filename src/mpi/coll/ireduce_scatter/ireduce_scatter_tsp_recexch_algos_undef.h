1812ba330a5de3a26d22b258fb06155b0b60c9e0
Akhil Langer <akhil.langer@intel.com>
2018-08-24 11:41:27 -0500

    coll: Fix comment in reduce, reduce_scatter(_block) algo
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpi/coll/ireduce_scatter/ireduce_scatter_tsp_recexch_algos_undef.h
+++ src/mpi/coll/ireduce_scatter/ireduce_scatter_tsp_recexch_algos_undef.h
@@ -9,7 +9,7 @@
  *  Contributor License Agreement dated February 8, 2012.
  */
 
-/* Header protection (i.e., IREDUCE_SCATTER_TSP_ALGOS_PROTOTYPES_H_INCLUDED) is
+/* Header protection (i.e., IREDUCE_SCATTER_TSP_RECEXCH_ALGOS_PROTOTYPES_H_INCLUDED) is
  * intentionally omitted since this header might get included multiple
  * times within the same .c file. */
 
