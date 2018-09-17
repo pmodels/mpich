0246dd92b14e8d2ddff1757a9707debc5bac6b3e
Halim Amer <aamer@anl.gov>
2018-09-17 20:43:20 +0000

    pt2pt: cleanup waiting for completion in Mrecv
    Mrecv waits for completion in the device layer, which is inconsistent
    with the other blocking recv operations. This patch moves waiting for
    completion to where it belong: the MPI layer.
    
    Signed-off-by: Hajime Fujita <hajime.fujita@intel.com>

--- src/mpid/ch3/src/mpid_mrecv.c
+++ src/mpid/ch3/src/mpid_mrecv.c
@@ -11,11 +11,10 @@
 #undef FCNAME
 #define FCNAME MPL_QUOTE(FUNCNAME)
 int MPID_Mrecv(void *buf, int count, MPI_Datatype datatype,
-               MPIR_Request *message, MPI_Status *status)
+               MPIR_Request *message, MPI_Status *status, MPIR_Request **rreq)
 {
     int mpi_errno = MPI_SUCCESS;
-    int active_flag;            /* dummy for MPIR_Request_completion_processing */
-    MPIR_Request *rreq = NULL;
+    *rreq = NULL;
 
     if (message == NULL) {
         /* treat as though MPI_MESSAGE_NO_PROC was passed */
@@ -27,29 +26,8 @@ int MPID_Mrecv(void *buf, int count, MPI_Datatype datatype,
      * optimization potential in that case.  MPID_Recv exists to prevent
      * creating a request unnecessarily for messages that are already present
      * and eligible for immediate completion. */
-    mpi_errno = MPID_Imrecv(buf, count, datatype, message, &rreq);
+    mpi_errno = MPID_Imrecv(buf, count, datatype, message, rreq);
     if (mpi_errno) MPIR_ERR_POP(mpi_errno);
-
-    if (!MPIR_Request_is_complete(rreq)) {
-        MPID_Progress_state progress_state;
-
-        MPID_Progress_start(&progress_state);
-        while (!MPIR_Request_is_complete(rreq))
-        {
-            mpi_errno = MPID_Progress_wait(&progress_state);
-            if (mpi_errno) {
-                /* --BEGIN ERROR HANDLING-- */
-                MPID_Progress_end(&progress_state);
-                MPIR_ERR_POP(mpi_errno);
-                /* --END ERROR HANDLING-- */
-            }
-        }
-        MPID_Progress_end(&progress_state);
-    }
-    mpi_errno = MPIR_Request_completion_processing(rreq, status, &active_flag);
-    MPIR_Request_free(rreq);
-    if (mpi_errno)
-        MPIR_ERR_POP(mpi_errno);
 
 fn_exit:
     return mpi_errno;
