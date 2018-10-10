b03ead79e81c6671a9c463ec4a57d9628b635860
Sagar Thapaliya <sagar.thapaliya@intel.com>
2018-10-10 17:43:59 -0500

    ch4/ofi: Migrate eagain comm hint to new infrastructure
    This patch replaces the MPIR based support for the comm hint
    eagain with the newly introduced MPID based hint support.

--- src/mpid/ch4/netmod/ofi/ofi_comm.c
+++ src/mpid/ch4/netmod/ofi/ofi_comm.c
@@ -49,12 +49,31 @@ int MPIDI_OFI_mpi_comm_free_hook(MPIR_Comm * comm)
     return mpi_errno;
 }
 
+MPL_STATIC_INLINE_PREFIX int MPIDI_OFI_set_hint_eagain(MPIR_Comm * comm_ptr, const char *value)
+{
+    if (!strncmp(value, "true", strlen("true")))
+        MPIDI_OFI_COMM(comm_ptr).eagain = TRUE;
+    if (!strncmp(value, "false", strlen("false")))
+        MPIDI_OFI_COMM(comm_ptr).eagain = FALSE;
+
+    return MPI_SUCCESS;
+}
+
 int MPIDI_NM_mpi_comm_set_info_mutable(MPIR_Comm * comm, MPIR_Info * info)
 {
     int mpi_errno = MPI_SUCCESS;
     MPIR_Info *curr_info;
     MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_NM_MPI_COMM_SET_INFO_MUTABLE);
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_NM_MPI_COMM_SET_INFO_MUTABLE);
+
+    /* Set infohints supported by OFI, while ignoring the unsupported ones */
+    LL_FOREACH(info, curr_info) {
+        if (curr_info->key == NULL)
+            continue;
+        if (!strcmp(curr_info->key, "eagain")) {
+            MPIDI_OFI_set_hint_eagain(comm, curr_info->value);
+        }
+    }
 
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_NM_MPI_COMM_SET_INFO_MUTABLE);
     return mpi_errno;
@@ -73,8 +92,15 @@ int MPIDI_NM_mpi_comm_set_info_immutable(MPIR_Comm * comm, MPIR_Info * info)
 int MPIDI_NM_mpi_comm_get_info(MPIR_Comm * comm, MPIR_Info ** info_p_p)
 {
     int mpi_errno = MPI_SUCCESS;
+    char hint_val_str[MPI_MAX_INFO_VAL];
     MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_NM_MPI_COMM_GET_INFO);
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_NM_MPI_COMM_GET_INFO);
+
+    if (MPIDI_OFI_COMM(comm).eagain)
+        MPL_snprintf(hint_val_str, MPI_MAX_INFO_VAL, "true");
+    else
+        MPL_snprintf(hint_val_str, MPI_MAX_INFO_VAL, "false");
+    mpi_errno = MPIR_Info_set_impl(*info_p_p, "eagain", hint_val_str);
 
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_NM_MPI_COMM_GET_INFO);
     return mpi_errno;
