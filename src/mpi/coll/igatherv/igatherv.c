13a8bc79d5aefbf8c037f1f64158ee77200ccdf1
Ken Raffenetti <raffenet@mcs.anl.gov>
2018-10-16 20:29:31 -0500

    coll: Always return a request in NBCs
    Returning MPI_REQUEST_NULL led to user confusion. See discussion
    http://lists.mpich.org/pipermail/discuss/2015-December/004393.html. Create
    a complete request and return it if the underlying operation completes
    immediately.
    
    Fixes pmodels/mpich#2319
    
    Signed-off-by: Hui Zhou <hzhou321@anl.gov>

--- src/mpi/coll/igatherv/igatherv.c
+++ src/mpi/coll/igatherv/igatherv.c
@@ -426,11 +426,11 @@ int MPI_Igatherv(const void *sendbuf, int sendcount, MPI_Datatype sendtype, void
     if (mpi_errno)
         MPIR_ERR_POP(mpi_errno);
 
+    /* create a complete request, if needed */
+    if (!request_ptr)
+        request_ptr = MPIR_Request_create_complete(MPIR_REQUEST_KIND__COLL);
     /* return the handle of the request to the user */
-    if (request_ptr)
-        *request = request_ptr->handle;
-    else
-        *request = MPI_REQUEST_NULL;
+    *request = request_ptr->handle;
 
     /* ... end of body of routine ... */
 
