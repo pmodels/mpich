ea6a9d358ae4b6b15837efab924308e989793842
Taru Doodi <taru.doodi@intel.com>
2018-10-22 16:57:41 -0500

    coll: add utility function to check if displacements are ordered
    Signed-off-by: Hui Zhou <hzhou321@anl.gov>

--- src/mpi/coll/iallgatherv/Makefile.mk
+++ src/mpi/coll/iallgatherv/Makefile.mk
@@ -23,4 +23,8 @@ mpi_core_sources +=                                           \
     src/mpi/coll/iallgatherv/iallgatherv_gentran_algos.c                    \
     src/mpi/coll/iallgatherv/iallgatherv_intra_recexch_distance_doubling.c    \
     src/mpi/coll/iallgatherv/iallgatherv_intra_recexch_distance_halving.c    \
-    src/mpi/coll/iallgatherv/iallgatherv_intra_gentran_ring.c
+    src/mpi/coll/iallgatherv/iallgatherv_intra_gentran_ring.c        \
+    src/mpi/coll/iallgatherv/iallgatherv_utils.c
+
+noinst_HEADERS += \
+    src/mpi/coll/iallgatherv/iallgatherv.h
