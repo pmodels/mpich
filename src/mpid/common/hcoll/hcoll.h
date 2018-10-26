911151dfac8e3571ca3e5c9f2bc34ce9100ee932
Ken Raffenetti <raffenet@mcs.anl.gov>
2018-10-26 10:08:22 -0500

    hcoll: Add support for more collectives
    Add in functions for hcoll Reduce, Alltoall, and Alltoallv.
    
    Signed-off-by: Tomislav Janjusic <tomislavj@mellanox.com>

--- src/mpid/common/hcoll/hcoll.h
+++ src/mpid/common/hcoll/hcoll.h
@@ -27,10 +27,17 @@ int hcoll_comm_destroy(MPIR_Comm * comm, void *param);
 int hcoll_Barrier(MPIR_Comm * comm_ptr, MPIR_Errflag_t * err);
 int hcoll_Bcast(void *buffer, int count, MPI_Datatype datatype, int root,
                 MPIR_Comm * comm_ptr, MPIR_Errflag_t * err);
+int hcoll_Reduce(const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op,
+                 int root, MPIR_Comm * comm_ptr, MPIR_Errflag_t * err);
 int hcoll_Allgather(const void *sbuf, int scount, MPI_Datatype sdtype,
                     void *rbuf, int rcount, MPI_Datatype rdtype, MPIR_Comm * comm_ptr,
                     MPIR_Errflag_t * err);
 int hcoll_Allreduce(const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op,
+                    MPIR_Comm * comm_ptr, MPIR_Errflag_t * err);
+int hcoll_Alltoall(const void *sbuf, int scount, MPI_Datatype sdtype, void *rbuf, int rcount,
+                   MPI_Datatype rdtype, MPIR_Comm * comm_ptr, MPIR_Errflag_t * err);
+int hcoll_Alltoallv(const void *sbuf, const int *scounts, const int *sdispls, MPI_Datatype sdtype,
+                    void *rbuf, const int *rcounts, const int *rdispls, MPI_Datatype rdtype,
                     MPIR_Comm * comm_ptr, MPIR_Errflag_t * err);
 
 int hcoll_do_progress(int *made_progress);
