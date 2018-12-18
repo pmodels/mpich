ae3cd7411363f6279fb2f3e07998fa0827aae061
Min Si <msi@anl.gov>
2018-12-18 15:08:40 -0600

    test/rma: extend acc_pairtype to use win_allocate
    Win_allocate enables SHM RMA by default, thus adding such option in test
    can also check the SHM RMA routine.
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- test/mpi/rma/acc_pairtype.c
+++ test/mpi/rma/acc_pairtype.c
@@ -34,21 +34,36 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     MPI_Alloc_mem(sizeof(long_double_int_t) * DATA_SIZE, MPI_INFO_NULL, &orig_buf);
-    MPI_Alloc_mem(sizeof(long_double_int_t) * DATA_SIZE, MPI_INFO_NULL, &tar_buf);
-
     for (i = 0; i < DATA_SIZE; i++) {
         orig_buf[i].a = 1.0;
         orig_buf[i].b = 1;
-        tar_buf[i].a = 0;
-        tar_buf[i].b = 0;
     }
 
     MPI_Type_vector(5 /* count */ , 3 /* blocklength */ , 5 /* stride */ , MPI_LONG_DOUBLE_INT,
                     &vector_dtp);
     MPI_Type_commit(&vector_dtp);
 
+#ifdef USE_WIN_ALLOCATE
+    MPI_Win_allocate(sizeof(long_double_int_t) * DATA_SIZE, sizeof(long_double_int_t),
+                     MPI_INFO_NULL, MPI_COMM_WORLD, &tar_buf, &win);
+    /* Reset window buffer */
+    MPI_Win_lock(MPI_LOCK_SHARED, rank, 0, win);
+    for (i = 0; i < DATA_SIZE; i++) {
+        tar_buf[i].a = 0;
+        tar_buf[i].b = 0;
+    }
+    MPI_Win_unlock(rank, win);
+    MPI_Barrier(MPI_COMM_WORLD);
+#else
+    MPI_Alloc_mem(sizeof(long_double_int_t) * DATA_SIZE, MPI_INFO_NULL, &tar_buf);
+    /* Reset window buffer */
+    for (i = 0; i < DATA_SIZE; i++) {
+        tar_buf[i].a = 0;
+        tar_buf[i].b = 0;
+    }
     MPI_Win_create(tar_buf, sizeof(long_double_int_t) * DATA_SIZE, sizeof(long_double_int_t),
                    MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+#endif
 
     if (rank == 0) {
         MPI_Win_lock(MPI_LOCK_SHARED, 1, 0, win);
@@ -56,9 +71,10 @@ int main(int argc, char *argv[])
         MPI_Win_unlock(1, win);
     }
 
-    MPI_Win_free(&win);
+    MPI_Barrier(MPI_COMM_WORLD);
 
     if (rank == 1) {
+        MPI_Win_lock(MPI_LOCK_SHARED, 1, 0, win);
         for (i = 0; i < DATA_SIZE; i++) {
             if (i % 5 < 3) {
                 if (tar_buf[i].a != 1.0 || tar_buf[i].b != 1) {
@@ -70,12 +86,16 @@ int main(int argc, char *argv[])
                 }
             }
         }
+        MPI_Win_unlock(1, win);
     }
 
+    MPI_Win_free(&win);
     MPI_Type_free(&vector_dtp);
 
     MPI_Free_mem(orig_buf);
+#ifndef USE_WIN_ALLOCATE
     MPI_Free_mem(tar_buf);
+#endif
 
     MTest_Finalize(errs);
     return MTestReturnValue(errs);
