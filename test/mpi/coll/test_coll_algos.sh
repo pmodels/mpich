46194398ee34719e74eb55a5fd310ec18adb4be1
Ken Raffenetti <raffenet@mcs.anl.gov>
2018-12-20 11:11:29 -0600

    test/coll: Fix extended test generation
    String equality is done with "=". -eq is for integers.
    
    Signed-off-by: Surabhi Jain <surabhi.jain@intel.com>

--- test/mpi/coll/test_coll_algos.sh
+++ test/mpi/coll/test_coll_algos.sh
@@ -22,7 +22,7 @@ kvalues="3"
 
 for algo_name in ${algo_names}; do
     for kval in ${kvalues}; do
-        if [ ${algo_name} -eq "tree" ]; then
+        if [ ${algo_name} = "tree" ]; then
             for tree_type in ${tree_types}; do
                 #set the environment
                 env="${testing_env} env=MPIR_CVAR_IBCAST_INTRA_ALGORITHM=${algo_name} "
@@ -57,7 +57,7 @@ tree_types="kary knomial_1 knomial_2"
 kvalues="3"
 
 for algo_name in ${algo_names}; do
-    if [ ${algo_name} -eq "tree" ]; then
+    if [ ${algo_name} = "tree" ]; then
         for tree_type in ${tree_types}; do
             for kval in ${kvalues}; do
                 #set the environment
