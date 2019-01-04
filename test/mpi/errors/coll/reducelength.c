05f0eb177d573a53e445a9b908bf682a99da9dcf
Rashid Kaleem <rashid.kaleem@intel.com>
2019-01-04 16:04:59 -0600

    test: Add collectives error propagation tests
    Add tests for error propagation in collectives.
    1) allreducelength - ranks use different lengths in allreduce
    2) reducelength - ranks use different lengths in reduce
    3) reduceop - ranks use different operator in reduce
    4) gatherlength - rank use different lengths
    5) allgatherlength - rank use different lengths
    6) scatterlength - rank use different lengths
    7) alltoalllength - rank use different lengths
    Currently, all tests have been xfailed until the fix has been
    patched in.

---
+++ test/mpi/errors/coll/reducelength.c
@@ -0,0 +1,55 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2019 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+#include <stdio.h>
+#include "mpi.h"
+#include "mpitest.h"
+#include "mpicolltest.h"
+
+/* Very simple test that Reduce handled mismatched lengths.
+   Extended from bcastlength.c
+*/
+
+int verbose = 0;
+
+int main(int argc, char *argv[])
+{
+    int buf[10], recvbuf[10], ierr, errs = 0;
+    int rank;
+    char str[MPI_MAX_ERROR_STRING + 1];
+    int slen;
+
+    MTEST_VG_MEM_INIT(buf, 10 * sizeof(int));
+
+    MTest_Init(&argc, &argv);
+
+    MPI_Errhandler_set(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) {
+        ierr = MTest_Reduce(buf, recvbuf, 10, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    } else {
+        ierr = MTest_Reduce(buf, recvbuf, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    }
+    if (ierr == MPI_SUCCESS) {
+        if (rank == 0) {
+            /* The root process may not detect that a too-short buffer
+             * was provided by the non-root processes, but those processes
+             * should detect this. */
+            errs++;
+            printf("Did not detect mismatched length (short) on process %d\n", rank);
+        }
+    } else {
+        if (verbose) {
+            MPI_Error_string(ierr, str, &slen);
+            printf("Found expected error; message is: %s\n", str);
+        }
+    }
+
+    MPI_Errhandler_set(MPI_COMM_WORLD, MPI_ERRORS_ARE_FATAL);
+
+    MTest_Finalize(errs);
+    return MTestReturnValue(errs);
+}
