40451ada66cda2119d34cd8c1eee7c93dc5088ad
Wesley Bland <wesley.bland@intel.com>
2019-01-08 15:03:55 -0600

    ch4/posix: Avoid include guard warnings
    When you have two lines that look like the usual include guards, but
    don't match, some compilers give warnings to tell you that you might
    have messed up.
    
    To avoid this, add an extra #ifndef to check for the same macro that is
    being defined on the next line.
    
    Signed-off-by: Pavan Balaji <balaji@anl.gov>

--- src/mpid/ch4/shm/posix/eager/stub/func_table.c
+++ src/mpid/ch4/shm/posix/eager/stub/func_table.c
@@ -9,6 +9,8 @@
  */
 
 #ifndef POSIX_EAGER_INLINE
+
+#ifndef POSIX_EAGER_DISABLE_INLINES
 #define POSIX_EAGER_DISABLE_INLINES
 
 #include <mpidimpl.h>
@@ -29,4 +31,5 @@ MPIDI_POSIX_eager_funcs_t MPIDI_POSIX_eager_stub_funcs = {
     MPIDI_POSIX_eager_recv_completed_hook
 };
 
+#endif /* POSIX_EAGER_DIABLE_INLINES */
 #endif /* POSIX_EAGER_INLINE */
