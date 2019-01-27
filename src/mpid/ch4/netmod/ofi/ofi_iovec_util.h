6e1ff1e35138562b59148bbf80c535e49f1c8a73
Pavan Balaji <balaji@anl.gov>
2019-01-27 17:26:35 -0600

    fixup! datatypes: use segment pointers instead of the full structure.

--- src/mpid/ch4/netmod/ofi/ofi_iovec_util.h
+++ src/mpid/ch4/netmod/ofi/ofi_iovec_util.h
@@ -129,17 +129,17 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_OFI_segment_next(MPIDI_OFI_seg_state_t * stat
     switch (side) {
         case MPIDI_OFI_SEGMENT_ORIGIN:
             last = state->origin_end;
-            seg = &state->origin_seg;
+            seg = state->origin_seg;
             cursor = &state->origin_cursor;
             break;
         case MPIDI_OFI_SEGMENT_TARGET:
             last = state->target_end;
-            seg = &state->target_seg;
+            seg = state->target_seg;
             cursor = &state->target_cursor;
             break;
         case MPIDI_OFI_SEGMENT_RESULT:
             last = state->result_end;
-            seg = &state->result_seg;
+            seg = state->result_seg;
             cursor = &state->result_cursor;
             break;
         default:
@@ -179,11 +179,13 @@ MPL_STATIC_INLINE_PREFIX void MPIDI_OFI_init_seg_state(MPIDI_OFI_seg_state_t * s
 
     seg_state->origin_cursor = 0;
     MPIDI_Datatype_check_size(origin_type, origin_count, seg_state->origin_end);
-    MPIR_Segment_init(origin, origin_count, origin_type, &seg_state->origin_seg);
+    seg_state->origin_seg = MPIR_Segment_alloc();
+    MPIR_Segment_init(origin, origin_count, origin_type, seg_state->origin_seg);
 
     seg_state->target_cursor = 0;
     MPIDI_Datatype_check_size(target_type, target_count, seg_state->target_end);
-    MPIR_Segment_init((const void *) target, target_count, target_type, &seg_state->target_seg);
+    seg_state->target_seg = MPIR_Segment_alloc();
+    MPIR_Segment_init((const void *) target, target_count, target_type, seg_state->target_seg);
 
     MPIDI_OFI_INIT_SEG_STATE(target, TARGET);
     MPIDI_OFI_INIT_SEG_STATE(origin, ORIGIN);
@@ -213,15 +215,18 @@ MPL_STATIC_INLINE_PREFIX void MPIDI_OFI_init_seg_state2(MPIDI_OFI_seg_state_t * 
 
     seg_state->origin_cursor = 0;
     MPIDI_Datatype_check_size(origin_type, origin_count, seg_state->origin_end);
-    MPIR_Segment_init(origin, origin_count, origin_type, &seg_state->origin_seg);
+    seg_state->origin_seg = MPIR_Segment_alloc();
+    MPIR_Segment_init(origin, origin_count, origin_type, seg_state->origin_seg);
 
     seg_state->target_cursor = 0;
     MPIDI_Datatype_check_size(target_type, target_count, seg_state->target_end);
-    MPIR_Segment_init((const void *) target, target_count, target_type, &seg_state->target_seg);
+    seg_state->target_seg = MPIR_Segment_alloc();
+    MPIR_Segment_init((const void *) target, target_count, target_type, seg_state->target_seg);
 
     seg_state->result_cursor = 0;
     MPIDI_Datatype_check_size(result_type, result_count, seg_state->result_end);
-    MPIR_Segment_init(result, result_count, result_type, &seg_state->result_seg);
+    seg_state->result_seg = MPIR_Segment_alloc();
+    MPIR_Segment_init(result, result_count, result_type, seg_state->result_seg);
 
     MPIDI_OFI_INIT_SEG_STATE(target, TARGET);
     MPIDI_OFI_INIT_SEG_STATE(origin, ORIGIN);
