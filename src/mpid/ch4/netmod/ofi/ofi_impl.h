9d54e194e6409b6205e0b7875e507ba7cc3911be
Yanfei Guo <yguo@anl.gov>
2019-01-31 10:44:41 -0600

    ch4/ofi: MR key allocation rework
    This commit simplified the MR key allocation in OFI. The OFI only
    requires MR key to be unique. The key is used in RMA windows and the LMT
    protocol. This patch does two things:
    
    1. Rework the allocator to support 64bit keys that can be directly used
    in OFI memory registration.
    
    2. Using one global allocator for all MR keys. The key no longer have
    complex layout of context ID, key type and unique number.
    
    The rational of this patch are as follow.
    
    1. There is no need to have separate allocator for window id and RMA id
    in LMT. As the only requirement of uniqueness which can be guaranteed
    with only on allocator. This also removes the need of having key type
    field.
    
    2. The per-comm allocator design is overcomplicating things with little
    benefit. Since the key allocation only happens at window creation or
    latency insensitive code path (large message), using per-comm allocator
    offers little to none performance edge.
    
    Author: Alexander Sannikov <alexander.sannikov@intel.com>
    Co-Authored-by: Yanfei Guo <yguo@anl.gov>
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- src/mpid/ch4/netmod/ofi/ofi_impl.h
+++ src/mpid/ch4/netmod/ofi/ofi_impl.h
@@ -327,10 +327,10 @@ int MPIDI_OFI_control_handler(int handler_id, void *am_hdr,
                               MPIDIG_am_target_cmpl_cb * target_cmpl_cb, MPIR_Request ** req);
 int MPIDI_OFI_control_dispatch(void *buf);
 void MPIDI_OFI_index_datatypes(void);
-void MPIDI_OFI_index_allocator_create(void **_indexmap, int start, MPL_memory_class class);
-int MPIDI_OFI_index_allocator_alloc(void *_indexmap, MPL_memory_class class);
-void MPIDI_OFI_index_allocator_free(void *_indexmap, int index);
-void MPIDI_OFI_index_allocator_destroy(void *_indexmap);
+void MPIDI_OFI_mr_key_allocator_init();
+uint64_t MPIDI_OFI_mr_key_alloc();
+void MPIDI_OFI_mr_key_free(uint64_t index);
+void MPIDI_OFI_mr_key_allocator_destroy();
 
 /* Common Utility functions used by the
  * C and C++ components
--- src/mpid/ch4/netmod/ofi/ofi_send.h
+++ src/mpid/ch4/netmod/ofi/ofi_send.h
@@ -349,12 +349,8 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_OFI_send_normal(const void *buf, MPI_Aint cou
 
         if (MPIDI_OFI_ENABLE_MR_SCALABLE) {
             /* Set up a memory region for the lmt data transfer */
-            ctrl.rma_key =
-                MPIDI_OFI_index_allocator_alloc(MPIDI_OFI_COMM(comm).rma_id_allocator, MPL_MEM_RMA);
-            MPIR_Assert(ctrl.rma_key < MPIDI_Global.max_huge_rmas);
-            rma_key = MPIDI_OFI_rma_key_pack(comm->context_id, MPIDI_OFI_KEY_TYPE_HUGE_RMA,
-                                             ctrl.rma_key);
-            ctrl.rma_key = rma_key;
+            ctrl.rma_key = MPIDI_OFI_mr_key_alloc();
+            rma_key = ctrl.rma_key;
         }
 
         MPIDI_OFI_CALL_NOLOCK(fi_mr_reg(MPIDI_Global.domain,    /* In:  Domain Object       */
