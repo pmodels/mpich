ce8bcd0017846caaad4eff43e74112c7da7b23a4
Wesley Bland <wesley.bland@intel.com>
2019-02-26 13:26:15 -0600

    Revert "mtest: Add testing for allreduce ring algorithm"
    This reverts commit 6b3b8dd365c017913c141be9190433e56683479f.
    
    Signed-off-by: Pavan Balaji <balaji@anl.gov>

--- test/mpi/coll/test_coll_algos.sh
+++ test/mpi/coll/test_coll_algos.sh
@@ -90,34 +90,18 @@ testing_env="env=MPIR_CVAR_ALLREDUCE_DEVICE_COLLECTIVE=0 "
 #test nb algorithms
 testing_env+="env=MPIR_CVAR_ALLREDUCE_INTRA_ALGORITHM=nb "
 testing_env+="env=MPIR_CVAR_IALLREDUCE_DEVICE_COLLECTIVE=0 "
-algo_names="recexch_single_buffer recexch_multiple_buffer tree ring"
+algo_names="recexch_single_buffer recexch_multiple_buffer tree"
 tree_types="kary knomial_1 knomial_2"
 kvalues="2 3 4"
 
 for algo_name in ${algo_names}; do
-    if [ "${algo_name}" != "ring" ]; then
-        for kval in ${kvalues}; do
-            if [ "${algo_name}" = "tree" ]; then
-                for tree_type in ${tree_types}; do
-                    #set the environment
-                    env="${testing_env} env=MPIR_CVAR_IALLREDUCE_INTRA_ALGORITHM=${algo_name} "
-                    env+="env=MPIR_CVAR_IALLREDUCE_TREE_TYPE=${tree_type} env=MPIR_CVAR_IALLREDUCE_TREE_PIPELINE_CHUNK_SIZE=4096 "
-                    env+="env=MPIR_CVAR_IALLREDUCE_TREE_KVAL=${kval} "
-
-                    coll_algo_tests+="allred 4 arg=100 ${env}${nl}"
-                    coll_algo_tests+="allred 7 ${env}${nl}"
-                    coll_algo_tests+="allredmany 4 ${env}${nl}"
-                    coll_algo_tests+="allred2 4 ${env}${nl}"
-                    coll_algo_tests+="allred3 10 ${env}${nl}"
-                    coll_algo_tests+="allred4 4 ${env}${nl}"
-                    coll_algo_tests+="allred5 5 ${env}${nl}"
-                    coll_algo_tests+="allred6 4 ${env}${nl}"
-                    coll_algo_tests+="allred6 7 ${env}${nl}"
-                done
-            else #test recurisive exchange algorithms
+    for kval in ${kvalues}; do
+        if [ "${algo_name}" = "tree" ]; then
+            for tree_type in ${tree_types}; do
                 #set the environment
                 env="${testing_env} env=MPIR_CVAR_IALLREDUCE_INTRA_ALGORITHM=${algo_name} "
-                env+="env=MPIR_CVAR_IALLREDUCE_RECEXCH_KVAL=${kval} "
+                env+="env=MPIR_CVAR_IALLREDUCE_TREE_TYPE=${tree_type} env=MPIR_CVAR_IALLREDUCE_TREE_PIPELINE_CHUNK_SIZE=4096 "
+                env+="env=MPIR_CVAR_IALLREDUCE_TREE_KVAL=${kval} "
 
                 coll_algo_tests+="allred 4 arg=100 ${env}${nl}"
                 coll_algo_tests+="allred 7 ${env}${nl}"
@@ -128,22 +112,23 @@ for algo_name in ${algo_names}; do
                 coll_algo_tests+="allred5 5 ${env}${nl}"
                 coll_algo_tests+="allred6 4 ${env}${nl}"
                 coll_algo_tests+="allred6 7 ${env}${nl}"
-            fi
-        done
-    else #test ring algorithm
-        #set the environment
-        env="${testing_env} env=MPIR_CVAR_IALLREDUCE_INTRA_ALGORITHM=${algo_name} "
+            done
+        else
+            #set the environment
+            env="${testing_env} env=MPIR_CVAR_IALLREDUCE_INTRA_ALGORITHM=${algo_name} "
+            env+="env=MPIR_CVAR_IALLREDUCE_RECEXCH_KVAL=${kval} "
 
-        coll_algo_tests+="allred 4 arg=100 ${env}${nl}"
-        coll_algo_tests+="allred 7 ${env}${nl}"
-        coll_algo_tests+="allredmany 4 ${env}${nl}"
-        coll_algo_tests+="allred2 4 ${env}${nl}"
-        coll_algo_tests+="allred3 10 ${env}${nl}"
-        coll_algo_tests+="allred4 4 ${env}${nl}"
-        coll_algo_tests+="allred5 5 ${env}${nl}"
-        coll_algo_tests+="allred6 4 ${env}${nl}"
-        coll_algo_tests+="allred6 7 ${env}${nl}"
-    fi
+            coll_algo_tests+="allred 4 arg=100 ${env}${nl}"
+            coll_algo_tests+="allred 7 ${env}${nl}"
+            coll_algo_tests+="allredmany 4 ${env}${nl}"
+            coll_algo_tests+="allred2 4 ${env}${nl}"
+            coll_algo_tests+="allred3 10 ${env}${nl}"
+            coll_algo_tests+="allred4 4 ${env}${nl}"
+            coll_algo_tests+="allred5 5 ${env}${nl}"
+            coll_algo_tests+="allred6 4 ${env}${nl}"
+            coll_algo_tests+="allred6 7 ${env}${nl}"
+        fi
+    done
 done
 
 ######### Add tests for Allgather algorithms ###########
