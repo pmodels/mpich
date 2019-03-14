7741379b9cb375ee8fd0c506436c6c22706ebba3
Min Si <msi@anl.gov>
2019-03-14 17:14:50 -0500

    ch4/xpmem: enable xpmem-based win_create mem sharing
    The xpmem shmmod enables memory sharing for win_create at creation time,
    and reuses POSIX's RMA operation routines.

--- src/mpid/ch4/shm/xpmem/Makefile.mk
+++ src/mpid/ch4/shm/xpmem/Makefile.mk
@@ -14,5 +14,6 @@ noinst_HEADERS += src/mpid/ch4/shm/xpmem/shm_inline.h      \
                   src/mpid/ch4/shm/xpmem/xpmem_pre.h
 
 mpi_core_sources += src/mpid/ch4/shm/xpmem/globals.c       \
-                    src/mpid/ch4/shm/xpmem/xpmem_init.c
+                    src/mpid/ch4/shm/xpmem/xpmem_init.c    \
+                    src/mpid/ch4/shm/xpmem/xpmem_win.c
 endif
