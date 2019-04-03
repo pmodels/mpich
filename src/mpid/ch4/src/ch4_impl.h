92c88a57b3aa2dc671c99f7035c4c78a447de76a
Rohit Zambre <rzambre@uci.edu>
2019-04-03 00:45:45 +0000

    ch4/progress: Add infrastructure to choose global or per-VCI progress
    This commit puts the current (global) progress code into a separate
    global progress function, `MPIDI_Progress_test_global`. Similarly,
    it introduces a new function for per-VCI progress,
    `MPIDI_Progress_test_vci`.
    
    `MPIDI_Progress_test` will now first check if it needs to perform a
    global progress with the `progress_type` argument. If yes, it will
    ignore the rest of the function arguments and call the global
    progress function.
    
    [Note that global progress is separate from MPIDI_PROGRESS_ALL. We
    can have global progress without progressing certain types of
    hooks. For example, when the netmod needs to call the progress
    function, it could call global progress but it cannot progress
    MPIDI_PROGRESS_HOOKS.]
    
    If `progress_type` is MPIDI_PROGRESS_TYPE__VCI, then the progress
    function will progress only one VCI that is pointed to by the
    `vci` argument.
    
    Additionally, this commit also puts the current definitions of
    the hook flags into an enum.

--- src/mpid/ch4/src/ch4_impl.h
+++ src/mpid/ch4/src/ch4_impl.h
@@ -14,8 +14,10 @@
 #include "ch4_types.h"
 #include "mpidig.h"
 
-MPL_STATIC_INLINE_PREFIX int MPIDI_Progress_test(int flags);
-MPL_STATIC_INLINE_PREFIX int MPIDI_Progress_test_vci(int flags, int vci);
+MPL_STATIC_INLINE_PREFIX int MPIDI_Progress_test(MPIDI_hook_flags_t hook_flags,
+                                                 MPIDI_progress_type_t progress_type, int vci);
+MPL_STATIC_INLINE_PREFIX int MPIDI_Progress_test_global(MPIDI_hook_flags_t hook_flags);
+MPL_STATIC_INLINE_PREFIX int MPIDI_Progress_test_vci(MPIDI_hook_flags_t hook_flags, int vci);
 MPL_STATIC_INLINE_PREFIX int MPIDI_Progress_test_hooks(void);
 int MPIDIG_get_context_index(uint64_t context_id);
 uint64_t MPIDIG_generate_win_id(MPIR_Comm * comm_ptr);
