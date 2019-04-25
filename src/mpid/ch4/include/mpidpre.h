957d7c6fb20c1f70450397856be53fcc31900b14
Rohit Zambre <rzambre@uci.edu>
2019-04-25 19:15:13 +0000

    ch4: Define the VCI, the VCI pool and its management functions
    VCI - Virtual Communication Interface is CH4's communication
    object that has various types (MPIDI_vci_type_t), resources
    (MPIDI_vci_resource_t), and properties (MPIDI_vci_property_t).
    
    Each VCI has its own lock, a lightweight request for
    operations that complete immediately on the VCI, a VNI, and
    a VSI.
    
    CH4 maintains a pool of VCIs that is created during init time.
    This pool is simply an array of VCIs, the memory for which is
    allocated dynamically to accommodate for the maximum number of
    VCIs possible. VCIs from this pool will be dynamically allocated
    to objects like communicators during communicator creation time.
    
    Since multiple communicators can be created simultaneously, the
    VCI pool has its own lock that is taken to provide mutual
    exclusion during the allocation or deallocation (free) for a VCI.
    This lock also maintains the thread safety of the
    allocation/deallocation of VNIs and VSIs since the VNIs and VSIs
    are allocated/freed only when a VCI is allocated/freed.
    
    The CH4 request also now stores the VCI that was used for the
    request.

--- src/mpid/ch4/include/mpidpre.h
+++ src/mpid/ch4/include/mpidpre.h
@@ -519,10 +519,14 @@ typedef struct {
 typedef enum {
     MPIDI_VCI_RESOURCE__TX = 0x1,       /* Can send */
     MPIDI_VCI_RESOURCE__RX = 0x2,       /* Can receive */
+    MPIDI_VCI_RESOURCE__NM = 0x4,       /* Can perform netmod operations */
+    MPIDI_VCI_RESOURCE__SHM = 0x8,      /* Can perform shmmod operations */
 } MPIDI_vci_resource_t;
 
 #define MPIDI_VCI_RESOURCE__GENERIC MPIDI_VCI_RESOURCE__TX | \
-                                    MPIDI_VCI_RESOURCE__RX
+                                    MPIDI_VCI_RESOURCE__RX | \
+                                    MPIDI_VCI_RESOURCE__NM | \
+                                    MPIDI_VCI_RESOURCE__SHM
 
 /* VCI properties */
 typedef enum {
@@ -540,6 +544,35 @@ typedef enum {
                                     MPIDI_VCI_PROPERTY__WAR | \
                                     MPIDI_VCI_PROPERTY__RAW | \
                                     MPIDI_VCI_PROPERTY__WAW
+
+/* VCI types */
+typedef enum {
+    MPIDI_VCI_TYPE__SHARED = 0x1,       /* VCI can be used by multiple objects */
+    MPIDI_VCI_TYPE__EXCLUSIVE = 0x2,    /* VCI will be used by only one object */
+} MPIDI_vci_type_t;
+
+ /* VCI */
+typedef struct MPIDI_vci {
+    MPID_Thread_mutex_t lock;   /* lock to protect the objects in this VCI */
+    int ref_count;              /* number of objects referring to this VCI */
+    int is_free;                /* flag to check if this VCI is free or not */
+    int vni;                    /* index to the VNI in the netmod's pool */
+    int vsi;                    /* index to the VSI in the shmmod's pool */
+} MPIDI_vci_t;
+
+ /* VCI pool */
+typedef struct MPIDI_vci_pool {
+    int next_free_vci;
+    int max_vcis;
+    MPID_Thread_mutex_t lock;   /* lock to protect the VCI pool */
+    MPIDI_vci_t *vci;           /* array of VCIs */
+} MPIDI_vci_pool_t;
+
+#define MPIDI_VCI_ROOT 0
+#define MPIDI_VCI_INVALID -1
+
+#define MPIDI_VCI_POOL(field) MPIDI_global.vci_pool.field
+#define MPIDI_VCI(i) MPIDI_VCI_POOL(vci)[i]
 
 typedef struct MPIDI_av_entry {
     union {
