766c342e37134863219d4d4dc7b2b52048f1275b
Ken Raffenetti <raffenet@mcs.anl.gov>
2019-05-22 16:43:56 -0500

    mpl/bt: Remove support for libbacktrace and libunwind
    These libraries are not regularly tested on our systems, and are the
    subject of some user-reported compilation failures. Since this is just
    nice-to-have debug information, remove support and rely on execinfo
    when available.

--- src/mpl/src/bt/mpl_bt.c
+++ src/mpl/src/bt/mpl_bt.c
@@ -7,80 +7,8 @@
 #include "mpl.h"
 #ifdef MPL_HAVE_BACKTRACE
 #include <execinfo.h>
-#endif
 
-#ifdef MPL_HAVE_LIBBACKTRACE
-#include <backtrace.h>
-#endif
-
-#ifdef MPL_HAVE_LIBUNWIND
-#define UNW_LOCAL_ONLY
-#include <libunwind.h>
-#endif
-
-
-/* freebsd and linux have slightly different backtrace routines.
- * solaris uses something totally different: a 'walkcontext' routine
- * which takes a function pointer.  solaris 'walkcontext' is simliar to
- * libbacktrace, shipped with gcc-4.8 and newer.  both share features
- * with libunwind.
- *
- * For the case of "display the call stack to this point" the various
- * approaches share a common pattern:
- * - initialize the library
- * - get the stack
- * - decode the stack
- *
- * but for now we'll simply dispatch to one of several appraoches
- * depending on what configure found
- *
- */
-
-#ifdef MPL_HAVE_LIBBACKTRACE
-
-static inline void backtrace_libback(FILE * output)
-{
-    struct backtrace_state *btstate;
-    btstate = backtrace_create_state(NULL, 1, NULL, NULL);
-    backtrace_print(btstate, 0, output);
-}
-
-/* we need not only the symbols but the header file too (for the cursor and
- * context), so tighten up when we take the libunwind path.  Thanks
- * Siegmar.Gross@informatik.hs-fulda.de for the bug report about systems with
- * libunwind libraries but no libunwind development headers */
-#elif defined MPL_HAVE_LIBUNWIND
-static inline void backtrace_libunwind(FILE * output)
-{
-    unw_cursor_t cursor;
-    unw_context_t uc;
-    unw_word_t ip, offset;
-    int ret, chars = 0;
-    char buffer[MPL_BACKTRACE_BUFFER_LEN];
-    char backtrace_buffer[MPL_BACKTRACE_BUFFER_LEN];
-
-    unw_getcontext(&uc);
-    unw_init_local(&cursor, &uc);
-    while (unw_step(&cursor) > 0) {
-        unw_get_reg(&cursor, UNW_REG_IP, &ip);
-        unw_get_proc_name(&cursor, buffer, MPL_BACKTRACE_BUFFER_LEN, &offset);
-        ret = MPL_snprintf(backtrace_buffer + chars,
-                           MPL_BACKTRACE_BUFFER_LEN - chars,
-                           "0x%lx %s() + 0x%lx\n", (long) ip, buffer, (long) offset);
-        if (ret + chars >= MPL_BACKTRACE_BUFFER_LEN) {
-            /* the extra new line will be more readable than a merely
-             * truncated string */
-            backtrace_buffer[MPL_BACKTRACE_BUFFER_LEN - 2] = '\n';
-            backtrace_buffer[MPL_BACKTRACE_BUFFER_LEN - 1] = '\0';
-            break;
-        }
-        chars += ret;
-    }
-    fprintf(output, "%s", backtrace_buffer);
-}
-
-#elif defined MPL_HAVE_BACKTRACE
-static inline void backtrace_libc(FILE * output)
+void MPL_backtrace_show(FILE * output)
 {
 #ifndef MPL_MAX_TRACE_DEPTH
 #define MPL_MAX_TRACE_DEPTH 32
@@ -109,24 +37,8 @@ static inline void backtrace_libc(FILE * output)
     MPL_free(stack_strs);
 }
 #else
-static inline void backtrace_unsupported(FILE * output)
+void MPL_backtrace_show(FILE * output)
 {
     fprintf(output, "No backtrace info available\n");
 }
 #endif
-
-/* Pick one of the many ways one could dump out a call stack*/
-void MPL_backtrace_show(FILE * output)
-{
-#ifdef MPL_HAVE_LIBBACKTRACE
-    backtrace_libback(output);
-#elif defined MPL_HAVE_LIBUNWIND
-    /* libunwind is not able to get line numbers without forking off to
-     * addr2line (?)*/
-    backtrace_libunwind(output);
-#elif defined MPL_HAVE_BACKTRACE
-    backtrace_libc(output);
-#else
-    backtrace_unsupported(output);
-#endif
-}
