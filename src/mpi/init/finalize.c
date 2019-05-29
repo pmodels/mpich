pmodels/mpich

commit 7cf2e519c7b3118607645e17a55f1ab6cf825c1f

init: use thread init/finalize interfaces in MPICH
MPI_Init and MPI_Finalize call thread initialization and
finalization functions, which some threading libraries require.

Accordingly, this patch removes Argobots-specific code in MPI_Init.

Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

src/mpi/init/finalize.c
@@ -331,6 +331,14 @@ int MPI_Finalize(void)
     }
 #endif
 
+    /* Finalize the threading library after releasing all synchronization
+     * objects (e.g., mutexes) */
+    {
+        int thread_err;
+        MPL_thread_finalize(&thread_err);
+        MPIR_Assert(thread_err == 0);
+    }
+
     /* ... end of body of routine ... */
   fn_exit:
     MPIR_FUNC_TERSE_FINALIZE_EXIT(MPID_STATE_MPI_FINALIZE);

src/mpi/init/initthread.c
@@ -345,15 +345,15 @@ int MPIR_Init_thread(int *argc, char ***argv, int required, int *provided)
     int exit_init_cs_on_failure = 0;
     MPIR_Info *info_ptr;
 
-#if (MPL_THREAD_PACKAGE_NAME == MPL_THREAD_PACKAGE_ARGOBOTS)
-    int rc = ABT_initialized();
-    if (rc != ABT_SUCCESS) {
-        mpi_errno = MPIR_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE,
-                                         "MPI_Init_thread", __LINE__, MPI_ERR_OTHER,
-                                         "**argobots_uninitialized", 0);
-        goto fn_fail;
+    /* The threading library must be initialized at the very beginning because
+     * it manages all synchronization objects (e.g., mutexes) that will be
+     * initialized later */
+    {
+        int thread_err;
+        MPL_thread_init(&thread_err);
+        if (thread_err)
+            goto fn_fail;
     }
-#endif
 
 #ifdef HAVE_HWLOC
     MPIR_Process.bindset = hwloc_bitmap_alloc();

commit 93e031aabe2a7ee36d07ff6c8f1b2734c053c50b

init: Fix single threaded builds
Guard thread library initialization and finalization in case it is not
needed. Single thread build configurations with thread package "none"
were broken by [7cf2e519c7b3].

Co-authored-by: Shintaro Iwasaki <siwasaki@anl.gov>

src/mpi/init/finalize.c
@@ -331,6 +331,7 @@ int MPI_Finalize(void)
     }
 #endif
 
+#if defined(MPICH_IS_THREADED)
     /* Finalize the threading library after releasing all synchronization
      * objects (e.g., mutexes) */
     {
@@ -338,6 +339,7 @@ int MPI_Finalize(void)
         MPL_thread_finalize(&thread_err);
         MPIR_Assert(thread_err == 0);
     }
+#endif
 
     /* ... end of body of routine ... */
   fn_exit:

src/mpi/init/initthread.c
@@ -346,7 +346,6 @@ int MPIR_Init_thread(int *argc, char ***argv, int required, int *provided)
     MPIR_Info *info_ptr;
 #if defined(MPICH_IS_THREADED)
     bool cs_initialized = false;
-#endif
 
     /* The threading library must be initialized at the very beginning because
      * it manages all synchronization objects (e.g., mutexes) that will be
@@ -357,6 +356,7 @@ int MPIR_Init_thread(int *argc, char ***argv, int required, int *provided)
         if (thread_err)
             goto fn_fail;
     }
+#endif
 
 #ifdef HAVE_HWLOC
     MPIR_Process.bindset = hwloc_bitmap_alloc();