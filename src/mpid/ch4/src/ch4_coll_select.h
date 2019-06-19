b752b74a5428bdf0b3831d6fa5606f8575c4eaa3
Rubasri Kalidas <rubasri.kalidas@intel.com>
2019-06-19 17:18:15 -0500

    ch4/coll: Change composition container to array of containers

--- src/mpid/ch4/src/ch4_coll_select.h
+++ src/mpid/ch4/src/ch4_coll_select.h
@@ -30,10 +30,10 @@ MPIDIG_coll_algo_generic_container_t *MPIDI_Barrier_select(MPIR_Comm * comm,
 
     if (MPIR_CVAR_ENABLE_SMP_COLLECTIVES && MPIR_CVAR_ENABLE_SMP_BARRIER &&
         MPIR_Comm_is_node_aware(comm)) {
-        return &MPIDI_Barrier_intra_composition_alpha_cnt;
+        return MPIDI_Barrier_intra_composition_alpha_cnt;
     }
 
-    return &MPIDI_Barrier_intra_composition_beta_cnt;
+    return MPIDI_Barrier_intra_composition_beta_cnt;
 }
 
 MPL_STATIC_INLINE_PREFIX const
@@ -57,14 +57,14 @@ MPIDIG_coll_algo_generic_container_t *MPIDI_Bcast_select(void *buffer,
         nbytes <= MPIR_CVAR_MAX_SMP_BCAST_MSG_SIZE && MPIR_Comm_is_node_aware(comm)) {
         if ((nbytes < MPIR_CVAR_BCAST_SHORT_MSG_SIZE) ||
             (comm->local_size < MPIR_CVAR_BCAST_MIN_PROCS)) {
-            return &MPIDI_Bcast_intra_composition_alpha_cnt;
+            return MPIDI_Bcast_intra_composition_alpha_cnt;
         } else {
             if (nbytes < MPIR_CVAR_BCAST_LONG_MSG_SIZE && MPL_is_pof2(comm->local_size, NULL)) {
-                return &MPIDI_Bcast_intra_composition_beta_cnt;
+                return MPIDI_Bcast_intra_composition_beta_cnt;
             }
         }
     }
-    return &MPIDI_Bcast_intra_composition_gamma_cnt;
+    return MPIDI_Bcast_intra_composition_gamma_cnt;
 }
 
 MPL_STATIC_INLINE_PREFIX const
@@ -96,7 +96,7 @@ MPIDIG_coll_algo_generic_container_t *MPIDI_Allreduce_select(const void *sendbuf
               MPIDI_POSIX_Reduce_algo_choice == MPIDI_POSIX_Reduce_intra_release_gather_id)) {
             /* gamma is selected if the msg size is less than threshold or release_gather based
              * shm_bcast and shm_reduce is not chosen for msg sizes larger than threshold */
-            return &MPIDI_Allreduce_intra_composition_gamma_cnt;
+            return MPIDI_Allreduce_intra_composition_gamma_cnt;
         }
     }
 #endif
@@ -105,10 +105,10 @@ MPIDIG_coll_algo_generic_container_t *MPIDI_Allreduce_select(const void *sendbuf
         nbytes = MPIR_CVAR_MAX_SMP_ALLREDUCE_MSG_SIZE ? type_size * count : 0;
         if (MPIR_Comm_is_node_aware(comm) && is_commutative &&
             nbytes <= MPIR_CVAR_MAX_SMP_ALLREDUCE_MSG_SIZE) {
-            return &MPIDI_Allreduce_intra_composition_alpha_cnt;
+            return MPIDI_Allreduce_intra_composition_alpha_cnt;
         }
     }
-    return &MPIDI_Allreduce_intra_composition_beta_cnt;
+    return MPIDI_Allreduce_intra_composition_beta_cnt;
 }
 
 MPL_STATIC_INLINE_PREFIX const
@@ -137,13 +137,13 @@ MPIDIG_coll_algo_generic_container_t *MPIDI_Reduce_select(const void *sendbuf,
         if (MPIR_Comm_is_node_aware(comm) && is_commutative &&
             nbytes <= MPIR_CVAR_MAX_SMP_REDUCE_MSG_SIZE) {
             if (nbytes <= MPIR_CVAR_REDUCE_SHORT_MSG_SIZE) {
-                return &MPIDI_Reduce_intra_composition_beta_cnt;
+                return MPIDI_Reduce_intra_composition_beta_cnt;
             } else {
-                return &MPIDI_Reduce_intra_composition_alpha_cnt;
+                return MPIDI_Reduce_intra_composition_alpha_cnt;
             }
         }
     }
-    return &MPIDI_Reduce_intra_composition_gamma_cnt;
+    return MPIDI_Reduce_intra_composition_gamma_cnt;
 }
 
 MPL_STATIC_INLINE_PREFIX const
@@ -161,7 +161,7 @@ MPIDIG_coll_algo_generic_container_t *MPIDI_Gather_select(const void *sendbuf,
         return &MPIDI_Gather_inter_composition_alpha_cnt;
     }
 
-    return &MPIDI_Gather_intra_composition_alpha_cnt;
+    return MPIDI_Gather_intra_composition_alpha_cnt;
 }
 
 MPL_STATIC_INLINE_PREFIX const
@@ -180,7 +180,7 @@ MPIDIG_coll_algo_generic_container_t *MPIDI_Gatherv_select(const void *sendbuf,
         return &MPIDI_Gatherv_inter_composition_alpha_cnt;
     }
 
-    return &MPIDI_Gatherv_intra_composition_alpha_cnt;
+    return MPIDI_Gatherv_intra_composition_alpha_cnt;
 }
 
 MPL_STATIC_INLINE_PREFIX const
@@ -199,7 +199,7 @@ MPIDIG_coll_algo_generic_container_t *MPIDI_Scatter_select(const void *sendbuf,
         return &MPIDI_Scatter_inter_composition_alpha_cnt;
     }
 
-    return &MPIDI_Scatter_intra_composition_alpha_cnt;
+    return MPIDI_Scatter_intra_composition_alpha_cnt;
 }
 
 MPL_STATIC_INLINE_PREFIX const
@@ -219,7 +219,7 @@ MPIDIG_coll_algo_generic_container_t *MPIDI_Scatterv_select(const void *sendbuf,
         return &MPIDI_Scatterv_inter_composition_alpha_cnt;
     }
 
-    return &MPIDI_Scatterv_intra_composition_alpha_cnt;
+    return MPIDI_Scatterv_intra_composition_alpha_cnt;
 }
 
 MPL_STATIC_INLINE_PREFIX const
@@ -236,7 +236,7 @@ MPIDIG_coll_algo_generic_container_t *MPIDI_Alltoall_select(const void *sendbuf,
         return &MPIDI_Alltoall_inter_composition_alpha_cnt;
     }
 
-    return &MPIDI_Alltoall_intra_composition_alpha_cnt;
+    return MPIDI_Alltoall_intra_composition_alpha_cnt;
 }
 
 MPL_STATIC_INLINE_PREFIX const
@@ -254,7 +254,7 @@ MPIDIG_coll_algo_generic_container_t *MPIDI_Alltoallv_select(const void *sendbuf
         return &MPIDI_Alltoallv_inter_composition_alpha_cnt;
     }
 
-    return &MPIDI_Alltoallv_intra_composition_alpha_cnt;
+    return MPIDI_Alltoallv_intra_composition_alpha_cnt;
 }
 
 MPL_STATIC_INLINE_PREFIX const
@@ -272,7 +272,7 @@ MPIDIG_coll_algo_generic_container_t *MPIDI_Alltoallw_select(const void *sendbuf
         return &MPIDI_Alltoallw_inter_composition_alpha_cnt;
     }
 
-    return &MPIDI_Alltoallw_intra_composition_alpha_cnt;
+    return MPIDI_Alltoallw_intra_composition_alpha_cnt;
 }
 
 MPL_STATIC_INLINE_PREFIX const
@@ -286,7 +286,7 @@ MPIDIG_coll_algo_generic_container_t *MPIDI_Allgather_select(const void *sendbuf
         return &MPIDI_Allgather_inter_composition_alpha_cnt;
     }
 
-    return &MPIDI_Allgather_intra_composition_alpha_cnt;
+    return MPIDI_Allgather_intra_composition_alpha_cnt;
 }
 
 MPL_STATIC_INLINE_PREFIX const
@@ -302,7 +302,7 @@ MPIDIG_coll_algo_generic_container_t *MPIDI_Allgatherv_select(const void *sendbu
         return &MPIDI_Allgatherv_inter_composition_alpha_cnt;
     }
 
-    return &MPIDI_Allgatherv_intra_composition_alpha_cnt;
+    return MPIDI_Allgatherv_intra_composition_alpha_cnt;
 }
 
 MPL_STATIC_INLINE_PREFIX const
@@ -318,7 +318,7 @@ MPIDIG_coll_algo_generic_container_t *MPIDI_Reduce_scatter_select(const void *se
         return &MPIDI_Reduce_scatter_inter_composition_alpha_cnt;
     }
 
-    return &MPIDI_Reduce_scatter_intra_composition_alpha_cnt;
+    return MPIDI_Reduce_scatter_intra_composition_alpha_cnt;
 }
 
 MPL_STATIC_INLINE_PREFIX const
@@ -335,7 +335,7 @@ MPIDIG_coll_algo_generic_container_t *MPIDI_Reduce_scatter_block_select(const vo
         return &MPIDI_Reduce_scatter_block_inter_composition_alpha_cnt;
     }
 
-    return &MPIDI_Reduce_scatter_block_intra_composition_alpha_cnt;
+    return MPIDI_Reduce_scatter_block_intra_composition_alpha_cnt;
 }
 
 MPL_STATIC_INLINE_PREFIX const
@@ -347,10 +347,10 @@ MPIDIG_coll_algo_generic_container_t *MPIDI_Scan_select(const void *sendbuf,
                                                         MPIR_Errflag_t * errflag)
 {
     if (MPII_Comm_is_node_consecutive(comm)) {
-        return &MPIDI_Scan_intra_composition_alpha_cnt;
+        return MPIDI_Scan_intra_composition_alpha_cnt;
     }
 
-    return &MPIDI_Scan_intra_composition_beta_cnt;
+    return MPIDI_Scan_intra_composition_beta_cnt;
 }
 
 MPL_STATIC_INLINE_PREFIX const
@@ -361,7 +361,7 @@ MPIDIG_coll_algo_generic_container_t *MPIDI_Exscan_select(const void *sendbuf,
                                                           MPI_Op op, MPIR_Comm * comm,
                                                           MPIR_Errflag_t * errflag)
 {
-    return &MPIDI_Exscan_intra_composition_alpha_cnt;
+    return MPIDI_Exscan_intra_composition_alpha_cnt;
 }
 
 #endif /* CH4_COLL_SELECT_H_INCLUDED */
