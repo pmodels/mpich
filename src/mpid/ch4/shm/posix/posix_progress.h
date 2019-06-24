f53e68462332f3cf36b44e6ece8495a07c1ecab6
Wesley Bland <wesley.bland@intel.com>
2019-06-24 15:15:00 -0500

    Remove stray FCNAME/FUNCNAME additions

--- src/mpid/ch4/shm/posix/posix_progress.h
+++ src/mpid/ch4/shm/posix/posix_progress.h
@@ -98,7 +98,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_progress_recv(int blocking)
 
                 if (in_total_data_sz > p_data_sz) {
                     rreq->status.MPI_ERROR =
-                        MPIR_Err_create_code(rreq->status.MPI_ERROR, MPIR_ERR_RECOVERABLE, FCNAME,
+                        MPIR_Err_create_code(rreq->status.MPI_ERROR, MPIR_ERR_RECOVERABLE, __func__,
                                              __LINE__, MPI_ERR_TRUNCATE, "**truncate",
                                              "**truncate %d %d %d %d", rreq->status.MPI_SOURCE,
                                              rreq->status.MPI_TAG, p_data_sz, in_total_data_sz);
@@ -157,7 +157,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_progress_recv(int blocking)
             if (in_total_data_sz > recv_data_sz) {
                 rreq->status.MPI_ERROR = MPIR_Err_create_code(rreq->status.MPI_ERROR,
                                                               MPIR_ERR_RECOVERABLE,
-                                                              FCNAME, __LINE__, MPI_ERR_TRUNCATE,
+                                                              __func__, __LINE__, MPI_ERR_TRUNCATE,
                                                               "**truncate",
                                                               "**truncate %d %d %d %d",
                                                               rreq->status.MPI_SOURCE,
