dc7c4305f8d693b691cc0c07ad2b3a43d9f7ef11
Rohit Zambre <rzambre@uci.edu>
2019-07-11 19:30:07 +0000

    ch4/ofi: Process completion events only after ensuring progress
    The purpose of `fi_cq_read` (which occurs in `MPIDI_OFI_PROGRESS`)
    is to process any completion events that might be associated with
    some RMA operations (such as Rput, Rget, etc.)
    
    Currently, the RMA progress engine performs both `fi_cntr_read`
    and `fi_cq_read` in each iteration. However, calling `fi_cq_read`
    before the `fi_cntr` has reached the desired value is wasteful
    since both the calls ensure progress of operations (although the
    OFI provider might implement network progress inside each function
    at different frequencies). Additionally, operations such as Put
    and Get do not have any completion events associated with them.
    Hence, calling `fi_cq_read` is wasteful for such operations too.
    
    Current bug: if the code path gets to `fi_cntr_wait`, `fi_cq_read`
    is not called to process any potential completion events.
    
    This commit does the following:
    
    1) Removes wasteful calls to `fi_cq_read` until the `fi_cntr` has
    reached the target value. Once the counter has reached the target
    value, the CQ is polled to process any completion events. The
    maximum number of times this CQ needs to be polled is
    deterministic and depends on how many operations are progressed
    during `fi_cntr_read`.
    
    2) Introduces a new function -- MPIDI_OFI_progress_test to read
    the output of `fi_cq_read` (MPIDI_OFI_progress internally uses
    this new function). If there are no completion events to be
    processed, we don't need to poll the CQ for the maximum possible
    number of times. This helps operations such as Put that don't
    need completion processing.
    
    3) Fixes the bug mentioned above.

--- src/mpid/ch4/netmod/ofi/ofi_progress.h
+++ src/mpid/ch4/netmod/ofi/ofi_progress.h
@@ -15,26 +15,67 @@
 #include "ofi_events.h"
 #include "ofi_am_events.h"
 
-MPL_STATIC_INLINE_PREFIX int MPIDI_NM_progress(int vni, int blocking)
+/* Successful execution of this function could be any of the following
+ * - mpi_errno = MPI_SUCCESS while handling cq entries or errors
+ * - fi_cq_read returns -FI_EAGAIN
+ * Erroneous execution of this function is when mpi_errno != MPI_SUCESS
+ * while handling cq entries or errors.
+ * Upon successful execution, this function will return the number of
+ * completions read during fi_cq_read (0 if -FI_EAGAIN).
+ * Upon erroneous execution, this function will return the mpi error
+ * codes in the negative integer space to not interfere with the positive
+ * values from successful execution.
+ * This function will return no negative values that corresponding to OFI
+ * error codes. They will be absorbed i.e. 0 will be returned for -FI_EAGAIN
+ * and the other fi_cq error codes will be handled.
+ */
+MPL_STATIC_INLINE_PREFIX int MPIDI_OFI_progress_test(int vni)
 {
     int mpi_errno;
     struct fi_cq_tagged_entry wc[MPIDI_OFI_NUM_CQ_ENTRIES];
-    ssize_t ret;
+    int ret;
+
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_OFI_PROGRESS_TEST);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_OFI_PROGRESS_TEST);
+
+    if (unlikely(MPIDI_OFI_get_buffered(wc, 1))) {
+        mpi_errno = MPIDI_OFI_handle_cq_entries(wc, 1);
+        if (mpi_errno != MPI_SUCCESS)
+            goto fn_fail;
+        ret = 1;
+    } else if (likely(1)) {
+        ret = fi_cq_read(MPIDI_OFI_CTX(vni).cq, (void *) wc, MPIDI_OFI_NUM_CQ_ENTRIES);
+
+        if (likely(ret > 0)) {
+            mpi_errno = MPIDI_OFI_handle_cq_entries(wc, ret);
+            if (mpi_errno != MPI_SUCCESS)
+                goto fn_fail;
+        } else if (ret == -FI_EAGAIN)
+            ret = 0;
+        else {
+            mpi_errno = MPIDI_OFI_handle_cq_error(vni, ret);
+            if (mpi_errno != MPI_SUCCESS)
+                goto fn_fail;
+        }
+    }
+
+  fn_exit:
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_OFI_PROGRESS_TEST);
+    return ret;
+  fn_fail:
+    ret = -mpi_errno;
+    goto fn_exit;
+}
+
+MPL_STATIC_INLINE_PREFIX int MPIDI_NM_progress(int vni, int blocking)
+{
+    int mpi_errno = MPI_SUCCESS, ret;
     MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_NM_PROGRESS);
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_NM_PROGRESS);
 
-    if (unlikely(MPIDI_OFI_get_buffered(wc, 1)))
-        mpi_errno = MPIDI_OFI_handle_cq_entries(wc, 1);
-    else if (likely(1)) {
-        ret = fi_cq_read(MPIDI_OFI_CTX(vni).cq, (void *) wc, MPIDI_OFI_NUM_CQ_ENTRIES);
-
-        if (likely(ret > 0))
-            mpi_errno = MPIDI_OFI_handle_cq_entries(wc, ret);
-        else if (ret == -FI_EAGAIN)
-            mpi_errno = MPI_SUCCESS;
-        else
-            mpi_errno = MPIDI_OFI_handle_cq_error(vni, ret);
-    }
+    ret = MPIDI_OFI_progress_test(vni);
+    if (ret < 0)
+        mpi_errno = -ret;
 
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_NM_PROGRESS);
 
--- src/mpid/ch4/netmod/ofi/ofi_win.h
+++ src/mpid/ch4/netmod/ofi/ofi_win.h
@@ -18,23 +18,20 @@ static inline int MPIDI_OFI_win_progress_fence(MPIR_Win * win)
 {
     int mpi_errno = MPI_SUCCESS;
     int itercount = 0;
-    int ret;
-    uint64_t tcount, donecount;
+    int ret, max_ofi_progress;
+    uint64_t tcount, firstcount, donecount;
     MPIDI_OFI_win_request_t *r;
 
     MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_OFI_WIN_PROGRESS_FENCE);
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_OFI_WIN_PROGRESS_FENCE);
 
     tcount = *MPIDI_OFI_WIN(win).issued_cntr;
-    donecount = fi_cntr_read(MPIDI_OFI_WIN(win).cmpl_cntr);
+    firstcount = donecount = fi_cntr_read(MPIDI_OFI_WIN(win).cmpl_cntr);
 
     MPIR_Assert(donecount <= tcount);
 
     while (tcount > donecount) {
         MPIR_Assert(donecount <= tcount);
-        MPID_THREAD_CS_EXIT(VCI, MPIDI_VCI(MPIDI_VCI_ROOT).lock);
-        MPIDI_OFI_PROGRESS();
-        MPID_THREAD_CS_ENTER(VCI, MPIDI_VCI(MPIDI_VCI_ROOT).lock);
         donecount = fi_cntr_read(MPIDI_OFI_WIN(win).cmpl_cntr);
         itercount++;
 
@@ -49,6 +46,25 @@ static inline int MPIDI_OFI_win_progress_fence(MPIR_Win * win)
             itercount = 0;
         }
     }
+
+    /* We need to poll the CQ to process any completion events corresponding
+     * to the progress made by reading the counter.
+     * Since we don't know what the value of the fi_cntr was before reading
+     * it, we might have made progress by reading it. Hence, we need to poll
+     * the CQ at least and then for `max_ofi_progress` times, if needed */
+    itercount = 0;
+    max_ofi_progress =
+        (tcount - firstcount + MPIDI_OFI_NUM_CQ_ENTRIES - 1) / MPIDI_OFI_NUM_CQ_ENTRIES;
+    do {
+        ret = MPIDI_OFI_progress_test(0);
+        if (ret == 0) {
+            break;
+        } else if (ret < 0) {
+            mpi_errno = -ret;
+            goto fn_exit;
+        }
+        itercount++;
+    } while (itercount < max_ofi_progress);
 
     r = MPIDI_OFI_WIN(win).syncQ;
 
