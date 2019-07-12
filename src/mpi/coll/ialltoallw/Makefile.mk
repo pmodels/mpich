abc523353f338897cfdff9325eaa96ee84489aeb
Rubasri Kalidas <rubasri.kalidas@intel.com>
2019-07-12 13:24:36 -0500

    coll: Rename sched algorithm files to match the algorithm names

--- src/mpi/coll/ialltoallw/Makefile.mk
+++ src/mpi/coll/ialltoallw/Makefile.mk
@@ -16,9 +16,9 @@ mpi_sources +=                                    \
     src/mpi/coll/ialltoallw/ialltoallw.c
 
 mpi_core_sources +=                               \
-    src/mpi/coll/ialltoallw/ialltoallw_intra_inplace.c  \
-    src/mpi/coll/ialltoallw/ialltoallw_intra_blocked.c  \
-    src/mpi/coll/ialltoallw/ialltoallw_inter_pairwise_exchange.c	\
+    src/mpi/coll/ialltoallw/ialltoallw_intra_sched_inplace.c  \
+    src/mpi/coll/ialltoallw/ialltoallw_intra_sched_blocked.c  \
+    src/mpi/coll/ialltoallw/ialltoallw_inter_sched_pairwise_exchange.c	\
     src/mpi/coll/ialltoallw/ialltoallw_gentran_algos.c	\
     src/mpi/coll/ialltoallw/ialltoallw_intra_gentran_blocked.c	\
     src/mpi/coll/ialltoallw/ialltoallw_intra_gentran_inplace.c
