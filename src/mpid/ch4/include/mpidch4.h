5ca9c037a414a877c99c54a1364e4666170c49d0
Hui Zhou <hzhou321@anl.gov>
2019-07-22 21:04:41 +0000

    ch4: refactor MPID_Wait functions into ch4_wait.h

--- src/mpid/ch4/include/mpidch4.h
+++ src/mpid/ch4/include/mpidch4.h
@@ -285,6 +285,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_av_is_local(MPIDI_av_entry_t * av);
 #include "ch4_proc.h"
 #include "ch4_coll.h"
 #include "ch4_vci.h"
+#include "ch4_wait.h"
 
 #define MPIDI_MAX_NETMOD_STRING_LEN 64
 extern int MPIDI_num_netmods;
