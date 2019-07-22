e662dd4a7a4f5eee4c18e49be031fbf5cb4b6f80
Yanfei Guo <yanf.guo@gmail.com>
2019-07-22 17:25:39 +0000

    pmi/bgq: add dummy implementation for PMI_Spawn_multiple.
    Spawn is not supported on BGQ. Redirect the call to PMI_Abort.

---
+++ src/pmi/bgq/Makefile.mk
@@ -0,0 +1,17 @@
+## -*- Mode: Makefile; -*-
+## vim: set ft=automake :
+##
+## (C) 2011 by Argonne National Laboratory.
+##     See COPYRIGHT in top-level directory.
+##
+
+if BUILD_PMI_BGQ
+
+mpi_core_sources +=       \
+    src/pmi/bgq/bgq_pmi.c
+
+noinst_HEADERS +=
+
+AM_CPPFLAGS += -I$(top_srcdir)/src/pmi/bgq
+
+endif BUILD_PMI_BGQ
