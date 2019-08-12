6271b58fb81fdb1d05529cc63c63d949c87077de
Min Si <msi@anl.gov>
2019-08-12 16:54:33 -0500

    mpidu/shm: introduce query API to return item addr
    The original get API only returns target item address. Thus, the calling
    routine has to first get the address and then copy the value (two
    steps). This patch renames the get API to "query", and changes the get
    routine to return the actual target value.

--- src/mpid/common/shm/mpidu_init_shm.h
+++ src/mpid/common/shm/mpidu_init_shm.h
@@ -18,6 +18,7 @@ int MPIDU_Init_shm_init(int rank, int size, int *nodemap);
 int MPIDU_Init_shm_finalize(void);
 int MPIDU_Init_shm_barrier(void);
 int MPIDU_Init_shm_put(void *orig, size_t len);
-int MPIDU_Init_shm_get(int local_rank, void **target);
+int MPIDU_Init_shm_get(int local_rank, size_t len, void *target);
+int MPIDU_Init_shm_query(int local_rank, void **target_addr);
 
 #endif /* MPIDU_INIT_SHM_H_INCLUDED */
