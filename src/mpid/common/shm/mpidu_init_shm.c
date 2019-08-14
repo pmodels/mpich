5290f48abc3d31b17946275db0dacd38feaeeae0
Hui Zhou <hzhou321@anl.gov>
2019-08-14 15:38:53 -0500

    init: add proposed init_shm implementation

--- src/mpid/common/shm/mpidu_init_shm.c
+++ src/mpid/common/shm/mpidu_init_shm.c
@@ -7,6 +7,63 @@
 #include "mpidu_init_shm.h"
 #include "build_nodemap.h"
 
+/******************************************************/
+/* NOTE: Proof of concept (I don't quite understand the need for current complexity */
+
+#define INIT_SHM_NAME "/dev/shm/mpich-init-shm"
+
+struct MPIDU_init_block {
+    char bc[MPIDU_MAX_BC_SIZE];
+    /* each feature can add their field here */
+};
+
+struct MPIDU_init_shm {
+    MPIDU_shm_barrier barrier;
+    struct MPIDU_init_block node_block[0]; /* FIXME: use C99 flexible array member with empty brackets */
+};
+
+static int init_shm_fd;
+static struct MPIDU_init_shm *init_shm;
+static int init_shm_size;
+
+int MPIDU_init_shm_init(void)
+{
+    int mpi_errno = MPI_SUCCESS;
+    /* NOTE: potentially we still need create unique handle and use PMI to exchange it */
+    /* It'd be nice if PMI provide us a unique id */
+    init_shm_fd = shm_open(INIT_SHM_NAME, O_CREAT | O_EXCL | ORDWR, 0600);
+    MPIR_ERR_CHKANDJUMP(init_shm_fd < 0, mpi_errno, MPI_ERR_OTHER, "**init_shm_init");
+
+    init_shm_size = sizeof(struct MPIDU_init_shm) +
+                    sizeof(struct MPIDU_Init_shm_block) * MPIR_Process.local_size;
+    ftruncate(init_shm_fd, init_shm_size);
+    init_shm = (struct MPIDU_Init_shm_block *) mmap(0, total_size, PROT_READ | PROT_WRITE,
+                                                           MAP_SHARED, init_shm_fd, 0);
+    mpi_errno = MPIDU_init_shm_barrier_init(init_shm->barrier);
+    MPIR_ERR_CHECK(mpi_errno);
+
+    MPIDU_init_shm_barrier();
+
+  fn_exit:  
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
+}
+
+int MPIDU_init_shm_finish(void)
+{
+    munmap(init_shm, init_shm_size);
+    close(init_shm_fd);
+    return MPI_SUCCESS;
+}
+
+int MPIDU_init_shm_barrier(void)
+{
+    /* to be implemented */
+    return MPI_SUCCESS;
+}
+
+/******************************************************/
 static int local_size;
 static int my_local_rank;
 static MPIDU_shm_seg_t memory;
