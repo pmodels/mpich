a3858b00abbdf54f6bc440c6e500c2ed653e33bf
Hui Zhou <hzhou321@anl.gov>
2019-08-20 08:41:06 -0500

    pmi: wrap pmi init/finalize functions
    MPIR_pmi_init will get process information in MPIR_Process, abstracting
    away the difference in various PMI APIs.
    
    In addition to MPIR_pmi_init/MPIR_pmi_finalize, we also provide
    MPIR_pmi_max_val_size and MPIR_pmi_jobid as getters for PMI private values.

--- src/util/mpir_pmi.c
+++ src/util/mpir_pmi.c
@@ -6,3 +6,139 @@
 
 #include <mpir_pmi.h>
 #include <mpiimpl.h>
+
+static int pmi_max_key_size;
+static int pmi_max_val_size;
+
+#ifdef USE_PMI1_API
+static int pmi_max_kvs_name_length;
+static char *pmi_kvs_name;
+#elif defined USE_PMI2_API
+static char *pmi_jobid;
+#elif defined USE_PMIX_API
+static pmix_proc_t pmix_proc;
+static pmix_proc_t pmix_wcproc;
+#endif
+
+int MPIR_pmi_init(void)
+{
+    int mpi_errno = MPI_SUCCESS;
+    int pmi_errno;
+
+    int has_parent, rank, size, appnum;
+
+#ifdef USE_PMI1_API
+    pmi_errno = PMI_Init(&has_parent);
+    MPIR_ERR_CHKANDJUMP1(pmi_errno != PMI_SUCCESS, mpi_errno, MPI_ERR_OTHER,
+                         "**pmi_init", "**pmi_init %d", pmi_errno);
+    pmi_errno = PMI_Get_rank(&rank);
+    MPIR_ERR_CHKANDJUMP1(pmi_errno != PMI_SUCCESS, mpi_errno, MPI_ERR_OTHER,
+                         "**pmi_get_rank", "**pmi_get_rank %d", pmi_errno);
+    pmi_errno = PMI_Get_size(&size);
+    MPIR_ERR_CHKANDJUMP1(pmi_errno != PMI_SUCCESS, mpi_errno, MPI_ERR_OTHER,
+                         "**pmi_get_size", "**pmi_get_size %d", pmi_errno);
+    pmi_errno = PMI_Get_appnum(&appnum);
+    MPIR_ERR_CHKANDJUMP1(pmi_errno != PMI_SUCCESS, mpi_errno, MPI_ERR_OTHER,
+                         "**pmi_get_appnum", "**pmi_get_appnum %d", pmi_errno);
+
+    pmi_errno = PMI_KVS_Get_name_length_max(&pmi_max_kvs_name_length);
+    MPIR_ERR_CHKANDJUMP1(pmi_errno != PMI_SUCCESS, mpi_errno, MPI_ERR_OTHER,
+                         "**pmi_kvs_get_name_length_max",
+                         "**pmi_kvs_get_name_length_max %d", pmi_errno);
+    pmi_kvs_name = (char *) MPL_malloc(pmi_max_kvs_name_length, MPL_MEM_OTHER);
+    pmi_errno = PMI_KVS_Get_my_name(pmi_kvs_name, pmi_max_kvs_name_length);
+    MPIR_ERR_CHKANDJUMP1(pmi_errno != PMI_SUCCESS, mpi_errno, MPI_ERR_OTHER,
+                         "**pmi_kvs_get_my_name", "**pmi_kvs_get_my_name %d", pmi_errno);
+
+    pmi_errno = PMI_KVS_Get_key_length_max(&pmi_max_key_size);
+    MPIR_ERR_CHKANDJUMP1(pmi_errno != PMI_SUCCESS, mpi_errno, MPI_ERR_OTHER,
+                         "**pmi_kvs_get_key_length_max",
+                         "**pmi_kvs_get_key_length_max %d", pmi_errno);
+    pmi_errno = PMI_KVS_Get_value_length_max(&pmi_max_val_size);
+    MPIR_ERR_CHKANDJUMP1(pmi_errno != PMI_SUCCESS, mpi_errno, MPI_ERR_OTHER,
+                         "**pmi_kvs_get_value_length_max",
+                         "**pmi_kvs_get_value_length_max %d", pmi_errno);
+
+#elif defined USE_PMI2_API
+    pmi_max_key_size = PMI2_MAX_KEYLEN;
+    pmi_max_val_size = PMI2_MAX_VALLEN;
+
+    pmi_errno = PMI2_Init(&has_parent, &size, &rank, &appnum);
+    MPIR_ERR_CHKANDJUMP1(pmi_errno != PMI2_SUCCESS, mpi_errno, MPI_ERR_OTHER,
+                         "**pmi_init", "**pmi_init %d", pmi_errno);
+
+    pmi_jobid = (char *) MPL_malloc(PMI2_MAX_VALLEN, MPL_MEM_OTHER);
+    pmi_errno = PMI2_Job_GetId(pmi_jobid, PMI2_MAX_VALLEN);
+    MPIR_ERR_CHKANDJUMP1(pmi_errno != PMI2_SUCCESS, mpi_errno, MPI_ERR_OTHER,
+                         "**pmi_job_getid", "**pmi_job_getid %d", pmi_errno);
+
+#elif defined USE_PMIX_API
+    pmi_max_key_size = PMIX_MAX_KEYLEN;
+    pmi_max_val_size = 1024;    /* this is what PMI2_MAX_VALLEN currently set to */
+
+    pmix_value_t *pvalue = NULL;
+
+    pmi_errno = PMIx_Init(&pmix_proc, NULL, 0);
+    MPIR_ERR_CHKANDJUMP1(pmi_errno != PMIX_SUCCESS, mpi_errno, MPI_ERR_OTHER,
+                         "**pmix_init", "**pmix_init %d", pmi_errno);
+
+    rank = pmix_proc.rank;
+    PMIX_PROC_CONSTRUCT(&pmix_wcproc);
+    MPL_strncpy(pmix_wcproc.nspace, pmix_proc.nspace, PMIX_MAX_NSLEN);
+    pmix_wcproc.rank = PMIX_RANK_WILDCARD;
+
+    pmi_errno = PMIx_Get(&pmix_wcproc, PMIX_JOB_SIZE, NULL, 0, &pvalue);
+    MPIR_ERR_CHKANDJUMP1(pmi_errno != PMIX_SUCCESS, mpi_errno, MPI_ERR_OTHER,
+                         "**pmix_get", "**pmix_get %d", pmi_errno);
+    size = pvalue->data.uint32;
+    PMIX_VALUE_RELEASE(pvalue);
+
+    /* appnum, has_parent is not set for now */
+    appnum = 0;
+    has_parent = 0;
+
+    MPIR_Process.pmix_proc = pmix_proc;
+    MPIR_Process.pmix_wcproc = pmix_wcproc;
+
+#endif
+    MPIR_Process.has_parent = has_parent;
+    MPIR_Process.rank = rank;
+    MPIR_Process.size = size;
+    MPIR_Process.appnum = appnum;
+
+  fn_exit:
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
+}
+
+void MPIR_pmi_finalize(void)
+{
+#ifdef USE_PMI1_API
+    PMI_Finalize();
+    MPL_free(pmi_kvs_name);
+#elif defined(USE_PMI2_API)
+    PMI2_Finalize();
+    MPL_free(pmi_jobid);
+#elif defined(USE_PMIX_API)
+    PMIx_Finalize(NULL, 0);
+    /* pmix_proc does not need free */
+#endif
+}
+
+/* getters for internal constants */
+int MPIR_pmi_max_val_size(void)
+{
+    return pmi_max_val_size;
+}
+
+const char *MPIR_pmi_job_id(void)
+{
+#ifdef USE_PMI1_API
+    return (const char *) pmi_kvs_name;
+#elif defined USE_PMI2_API
+    return (const char *) pmi_jobid;
+#elif defined USE_PMIX_API
+    return (const char *) pmix_proc.nspace;
+#endif
+}
