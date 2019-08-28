44d5f53799c90e0b64745d881a4c67808ba186d0
Ken Raffenetti <raffenet@mcs.anl.gov>
2019-08-28 09:57:59 -0500

    init: Move some MPIR initialization out of the devices
    Move these init functions up a layer to eliminate duplication.

--- src/mpid/ch3/src/mpid_init.c
+++ src/mpid/ch3/src/mpid_init.c
@@ -103,11 +103,6 @@ int MPID_Init(int *argc, char ***argv, int requested, int *provided)
 
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPID_INIT);
 
-    /* Call any and all MPID_Init type functions */
-    MPIR_Err_init();
-    MPIR_Datatype_init();
-    MPIR_Group_init();
-
     /* initialization routine for ch3u_comm.c */
     mpi_errno = MPIDI_CH3I_Comm_init();
     MPIR_ERR_CHECK(mpi_errno);
