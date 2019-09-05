a167341c29ac5902b7f71e5f13d602d7038508db
Surabhi Jain <surabhi.jain@intel.com>
2019-09-05 16:07:46 -0500

    posix/coll: Use MPL atomics in release_gather instead of izem

--- src/mpid/ch4/shm/posix/posix_comm.c
+++ src/mpid/ch4/shm/posix/posix_comm.c
@@ -10,9 +10,7 @@
 
 #include "mpidimpl.h"
 #include "posix_noinline.h"
-#ifdef ENABLE_IZEM_ATOMIC
 #include "release_gather.h"
-#endif
 
 int MPIDI_POSIX_mpi_comm_create_hook(MPIR_Comm * comm)
 {
@@ -20,12 +18,10 @@ int MPIDI_POSIX_mpi_comm_create_hook(MPIR_Comm * comm)
     MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_POSIX_MPI_COMM_CREATE_HOOK);
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_POSIX_MPI_COMM_CREATE_HOOK);
 
-#ifdef ENABLE_IZEM_ATOMIC
     /* Release_gather primitives based collective algorithm works for Intra-comms only */
     if (comm->comm_kind == MPIR_COMM_KIND__INTRACOMM) {
         mpi_errno = MPIDI_POSIX_mpi_release_gather_comm_init_null(comm);
     }
-#endif
 
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_POSIX_MPI_COMM_CREATE_HOOK);
     return mpi_errno;
@@ -37,12 +33,10 @@ int MPIDI_POSIX_mpi_comm_free_hook(MPIR_Comm * comm)
     MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_POSIX_MPI_COMM_FREE_HOOK);
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_POSIX_MPI_COMM_FREE_HOOK);
 
-#ifdef ENABLE_IZEM_ATOMIC
     /* Release_gather primitives based collective algorithm works for Intra-comms only */
     if (comm->comm_kind == MPIR_COMM_KIND__INTRACOMM) {
         mpi_errno = MPIDI_POSIX_mpi_release_gather_comm_free(comm);
     }
-#endif
 
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_POSIX_MPI_COMM_FREE_HOOK);
     return mpi_errno;
