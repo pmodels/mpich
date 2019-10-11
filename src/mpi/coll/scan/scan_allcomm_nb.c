a0ba149f0328ec75b16c19a5755a7cb38c440add
Gengbin Zheng <gengbin.zheng@intel.com>
2019-10-11 10:26:18 -0500

    mpi/coll: fix for blocking collectives that call nonblocking
    Call MPIC_wait instead of MPIR_Wait. calling MPIR_Wait requires
    convert MPIR_Request to its handle, and get pointer back again.
    During finalize, this process may be broken.
    Switch to call MPIC_Wait, which takes MPIR_Request directly
    as a parameter.

--- src/mpi/coll/scan/scan_allcomm_nb.c
+++ src/mpi/coll/scan/scan_allcomm_nb.c
@@ -9,17 +9,15 @@ int MPIR_Scan_allcomm_nb(const void *sendbuf, void *recvbuf, MPI_Aint count, MPI
                          MPI_Op op, MPIR_Comm * comm_ptr, MPIR_Errflag_t * errflag)
 {
     int mpi_errno = MPI_SUCCESS;
-    MPI_Request req = MPI_REQUEST_NULL;
     MPIR_Request *req_ptr = NULL;
 
     /* just call the nonblocking version and wait on it */
     mpi_errno = MPIR_Iscan(sendbuf, recvbuf, count, datatype, op, comm_ptr, &req_ptr);
     MPIR_ERR_CHECK(mpi_errno);
-    if (req_ptr)
-        req = req_ptr->handle;
 
-    mpi_errno = MPIR_Wait(&req, MPI_STATUS_IGNORE);
+    mpi_errno = MPIC_Wait(req_ptr, errflag);
     MPIR_ERR_CHECK(mpi_errno);
+    MPIR_Request_free(req_ptr);
 
   fn_exit:
     return mpi_errno;
