3da1c83ecf18fdc98b104b727287a10235255487
Yanfei Guo <yguo@anl.gov>
2019-10-23 22:45:53 -0500

    coll: remove redundant checking.

--- src/mpi/coll/ireduce_scatter_block/ireduce_scatter_block.c
+++ src/mpi/coll/ireduce_scatter_block/ireduce_scatter_block.c
@@ -383,7 +383,7 @@ int MPI_Ireduce_scatter_block(const void *sendbuf, void *recvbuf,
                 MPIR_Op *op_ptr = NULL;
                 MPIR_Op_get_ptr(op, op_ptr);
                 MPIR_Op_valid_ptr(op_ptr, mpi_errno);
-            } else if (HANDLE_IS_BUILTIN(op)) {
+            } else {
                 mpi_errno = (*MPIR_OP_HDL_TO_DTYPE_FN(op)) (datatype);
             }
             if (mpi_errno != MPI_SUCCESS)
