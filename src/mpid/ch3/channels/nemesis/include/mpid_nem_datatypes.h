eab678f11d8c0ed5f6ff631d87b3fd7d944a46cc
Hui Zhou <hzhou321@anl.gov>
2019-10-31 17:15:45 -0500

    ch3: use SIZEOF_VOID_P instead of SIZEOF_OPA_PTR_T
    SIZEOF_OPA_PTR_T is not defined. It probably once defined but got
    refactored away at some point. To support it will be quite messy as the
    configure don't really decide which opa atomic primitive it is being
    used -- opa_primitives.h does.
    
    For practical purpose, this is usually the same as SIZEOF_VOID_P. And
    since we are going to replace openpa with mpl atomics, let's take the
    cheap fix for now. We need ensure the size of MPL_Atomic_ptr_t is the
    same as void * in the future.

--- src/mpid/ch3/channels/nemesis/include/mpid_nem_datatypes.h
+++ src/mpid/ch3/channels/nemesis/include/mpid_nem_datatypes.h
@@ -116,12 +116,12 @@
    
 */
 
-#if (SIZEOF_OPA_PTR_T > 8)
-#  if (SIZEOF_OPA_PTR_T > 16)
+#if (SIZEOF_VOID_P > 8)
+#  if (SIZEOF_VOID_P > 16)
 #    error unexpected size for OPA_ptr_t
 #  endif
 #  define MPID_NEM_CELL_HEAD_LEN  16 /* We use this to keep elements 64-bit aligned */
-#else /* (SIZEOF_OPA_PTR_T <= 8) */
+#else /* (SIZEOF_VOID_P <= 8) */
 #  define MPID_NEM_CELL_HEAD_LEN  8 /* We use this to keep elements 64-bit aligned */
 #endif
 
@@ -188,7 +188,7 @@ MPID_nem_cell_rel_ptr_t;
 typedef struct MPID_nem_cell
 {
     MPID_nem_cell_rel_ptr_t next;
-#if (MPID_NEM_CELL_HEAD_LEN > SIZEOF_OPA_PTR_T)
+#if (MPID_NEM_CELL_HEAD_LEN > SIZEOF_VOID_P)
     char padding[MPID_NEM_CELL_HEAD_LEN - sizeof(MPID_nem_cell_rel_ptr_t)];
 #endif
     volatile MPID_nem_pkt_t pkt;
@@ -225,11 +225,11 @@ typedef struct MPID_nem_queue
 {
     MPID_nem_cell_rel_ptr_t head;
     MPID_nem_cell_rel_ptr_t tail;
-#if (MPID_NEM_CACHE_LINE_LEN > (2 * SIZEOF_OPA_PTR_T))
+#if (MPID_NEM_CACHE_LINE_LEN > (2 * SIZEOF_VOID_P))
     char padding1[MPID_NEM_CACHE_LINE_LEN - 2 * sizeof(MPID_nem_cell_rel_ptr_t)];
 #endif
     MPID_nem_cell_rel_ptr_t my_head;
-#if (MPID_NEM_CACHE_LINE_LEN > SIZEOF_OPA_PTR_T)
+#if (MPID_NEM_CACHE_LINE_LEN > SIZEOF_VOID_P)
     char padding2[MPID_NEM_CACHE_LINE_LEN - sizeof(MPID_nem_cell_rel_ptr_t)];
 #endif
 #if !defined(MPID_NEM_USE_LOCK_FREE_QUEUES)
