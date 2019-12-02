05d1142208cb9bb2b6efeb3bbc93db73ec4050c3
Giuseppe Congiu <gcongiu@anl.gov>
2019-12-02 10:22:24 -0600

    ch4/shm: uninline fastbox initialization code
    Uninline fastbox init code using the same scheme used by netmod and
    shmmod.

---
+++ src/mpid/ch4/shm/posix/eager/src/Makefile.mk
@@ -0,0 +1,8 @@
+## -*- Mode: Makefile; -*-
+## vim: set ft=automake :
+##
+## (C) 2020 by Argonne National Laboratory.
+##     See COPYRIGHT in top-level directory.
+##
+
+mpi_core_sources+=src/mpid/ch4/shm/posix/eager/src/posix_eager_impl.c
