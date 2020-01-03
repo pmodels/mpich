fb1d35bf4b8052a1500a48672495e44852a9a4ca
Giuseppe Congiu <gcongiu@anl.gov>
2020-01-03 16:53:38 +0100

    ch4/am: implement callbacks for rendezvous protocol

--- src/mpid/ch4/am/rndv/rndv.c
+++ src/mpid/ch4/am/rndv/rndv.c
@@ -50,6 +50,12 @@ int MPIDI_am_rndv_init(void)
 {
     int mpi_errno = MPI_SUCCESS;
 
+    /* register callback functions */
+    rts_hnd = MPIDI_am_register_handler(&am_rndv_rts_cb);
+    cts_hnd = MPIDI_am_register_handler(&am_rndv_cts_cb);
+    dst_hnd = MPIDI_am_register_handler(&am_rndv_send_dst_cb);
+    src_hnd = MPIDI_am_register_completion(&am_rndv_send_src_cb);
+
     return mpi_errno;
 }
 
@@ -79,7 +85,137 @@ int MPIDI_am_rndv_init(void)
 static int am_rndv_rts_cb(am_rndv_hdr_t * header, size_t payload_sz, void *payload)
 {
     int mpi_errno = MPI_SUCCESS;
+    MPIR_Request *rreq = NULL;
+    MPIR_Comm *root_comm = NULL;
+    am_rndv_cts_hdr_t cts_hdr;
 
+#ifndef MPIDI_CH4_DIRECT_NETMOD
+    MPIR_Request *anysource_partner = NULL;
+#endif
+
+    am_rndv_rts_hdr_t *rts_hdr = (am_rndv_rts_hdr_t *) header;
+    root_comm = MPIDIG_context_id_to_comm(rts_hdr->context_id);
+
+  root_comm_retry:
+    if (root_comm) {
+        /* MPIDI_CS_ENTER(); */
+#ifdef MPIDI_CH4_DIRECT_NETMOD
+        rreq = MPIDIG_dequeue_posted(rts_hdr->src, rts_hdr->tag, rts_hdr->context_id,
+                                     &MPIDIG_COMM(root_comm, posted_list));
+#else /* MPIDI_CH4_DIRECT_NETMOD */
+        int continue_matching = 1;
+        while (continue_matching) {
+            rreq =
+                MPIDIG_dequeue_posted(rts_hdr->src_rank, rts_hdr->tag, rts_hdr->context_id,
+                                      &MPIDIG_COMM(root_comm, posted_list));
+
+            if (rreq && MPIDI_REQUEST_ANYSOURCE_PARTNER(rreq)) {
+                anysource_partner = MPIDI_REQUEST_ANYSOURCE_PARTNER(rreq);
+
+                mpi_errno = MPIDI_anysource_matched(anysource_partner,
+                                                    MPIDI_REQUEST(rreq, is_local) ?
+                                                    MPIDI_SHM : MPIDI_NETMOD, &continue_matching);
+
+                MPIR_ERR_CHECK(mpi_errno);
+
+                MPIDI_REQUEST_ANYSOURCE_PARTNER(rreq) = NULL;
+                MPIDI_REQUEST_ANYSOURCE_PARTNER(anysource_partner) = NULL;
+            }
+
+            break;
+        }
+#endif /* MPIDI_CH4_DIRECT_NETMOD */
+        /* MPIDI_CS_EXIT(); */
+    }
+
+    if (rreq == NULL) {
+        rreq = MPIDIG_request_create(MPIR_REQUEST_KIND__RECV, 2);
+        MPIR_ERR_CHKANDSTMT(rreq == NULL, mpi_errno, MPIX_ERR_NOREQ, goto fn_fail, "**nomemreq");
+
+        MPIDIG_REQUEST(rreq, buffer) = NULL;
+        MPIDIG_REQUEST(rreq, datatype) = MPI_BYTE;
+        MPIDIG_REQUEST(rreq, count) = rts_hdr->data_sz;
+        MPIDIG_REQUEST(rreq, req->status) |= MPIDIG_REQ_LONG_RTS;
+        MPIDIG_REQUEST(rreq, req->rreq.peer_req_ptr) = rts_hdr->sreq_ptr;
+        MPIDIG_REQUEST(rreq, rank) = rts_hdr->src_rank;
+        MPIDIG_REQUEST(rreq, tag) = rts_hdr->tag;
+        MPIDIG_REQUEST(rreq, context_id) = rts_hdr->context_id;
+        MPIDIG_REQUEST(rreq, req->status) |= MPIDIG_REQ_IN_PROGRESS;
+
+#ifndef MPIDI_CH4_DIRECT_NETMOD
+        MPIDI_REQUEST(rreq, is_local) = rts_hdr->is_local;
+#endif
+        MPID_THREAD_CS_ENTER(VCI, MPIDIU_THREAD_MPIDIG_GLOBAL_MUTEX);
+        if (root_comm) {
+            MPIR_Comm_add_ref(root_comm);
+            MPIDIG_enqueue_unexp(rreq, &MPIDIG_COMM(root_comm, unexp_list));
+        } else {
+            MPIR_Comm *root_comm_again;
+            /* This branch means that last time we checked, there was no communicator
+             * associated with the arriving message.
+             * In a multi-threaded environment, it is possible that the communicator
+             * has been created since we checked root_comm last time.
+             * If that is the case, the new message must be put into a queue in
+             * the new communicator. Otherwise that message will be lost forever.
+             * Here that strategy is to query root_comm again, and if found,
+             * simply re-execute the per-communicator enqueue logic above. */
+            root_comm_again = MPIDIG_context_id_to_comm(rts_hdr->context_id);
+            if (unlikely(root_comm_again != NULL)) {
+                MPID_THREAD_CS_EXIT(VCI, MPIDIU_THREAD_MPIDIG_GLOBAL_MUTEX);
+                MPL_free(MPIDIG_REQUEST(rreq, buffer));
+                MPIR_Request_free(rreq);
+                MPID_Request_complete(rreq);
+                rreq = NULL;
+                root_comm = root_comm_again;
+                goto root_comm_retry;
+            }
+            MPIDIG_enqueue_unexp(rreq,
+                                 MPIDIG_context_id_to_uelist(MPIDIG_REQUEST(rreq, context_id)));
+        }
+        MPID_THREAD_CS_EXIT(VCI, MPIDIU_THREAD_MPIDIG_GLOBAL_MUTEX);
+    } else {
+        /* Matching receive was posted, tell the netmod */
+        MPIR_Comm_release(root_comm);   /* -1 for posted_list */
+        MPIDIG_REQUEST(rreq, req->status) |= MPIDIG_REQ_LONG_RTS;
+        MPIDIG_REQUEST(rreq, req->rreq.peer_req_ptr) = rts_hdr->sreq_ptr;
+        MPIDIG_REQUEST(rreq, rank) = rts_hdr->src_rank;
+        MPIDIG_REQUEST(rreq, tag) = rts_hdr->tag;
+        MPIDIG_REQUEST(rreq, context_id) = rts_hdr->context_id;
+        MPIDIG_REQUEST(rreq, req->status) |= MPIDIG_REQ_IN_PROGRESS;
+        /* Mark `match_req` as NULL so that we know nothing else to complete when
+         * `unexp_req` finally completes. (See MPIDI_recv_target_cmpl_cb) */
+        MPIDIG_REQUEST(rreq, req->rreq.match_req) = NULL;
+
+        /* prepare clear to send header */
+        cts_hdr.sreq_ptr = rts_hdr->sreq_ptr;
+        cts_hdr.rreq_ptr = rreq;
+
+#ifndef MPIDI_CH4_DIRECT_NETMOD
+        if (MPIDI_REQUEST(rreq, is_local)) {
+            mpi_errno =
+                MPIDI_SHM_am_send_header(rts_hdr->src_rank, cts_hnd, 0, sizeof(am_rndv_cts_hdr_t),
+                                         &cts_hdr, rreq);
+        } else
+#endif
+        {
+            mpi_errno =
+                MPIDI_NM_am_send_header(rts_hdr->src_rank, cts_hnd, 0, sizeof(am_rndv_cts_hdr_t),
+                                        &cts_hdr, rreq);
+        }
+
+        MPIR_ERR_CHECK(mpi_errno);
+
+#ifndef MPIDI_CH4_DIRECT_NETMOD
+        if (unlikely(anysource_partner)) {
+            anysource_partner->status = rreq->status;
+        }
+#endif /* MPIDI_CH4_DIRECT_NETMOD */
+    }
+
+  fn_exit:
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
     return mpi_errno;
 }
 
@@ -99,8 +235,61 @@ static int am_rndv_rts_cb(am_rndv_hdr_t * header, size_t payload_sz, void *paylo
 static int am_rndv_cts_cb(am_rndv_hdr_t * header, size_t payload_sz, void *payload)
 {
     int mpi_errno = MPI_SUCCESS;
+    int is_contig;
+    int is_local;
+    size_t data_sz;
+    MPI_Aint dt_true_lb;
+    MPIR_Datatype *dt_ptr = NULL;
+    am_rndv_msg_hdr_t msg_hdr;
 
+    am_rndv_cts_hdr_t *cts_hdr = (am_rndv_cts_hdr_t *) header;
+    MPIR_Request *sreq = (MPIR_Request *) cts_hdr->sreq_ptr;
+    MPIR_Assert(sreq != NULL);
+
+    /* prepare message header */
+    msg_hdr.rreq_ptr = cts_hdr->rreq_ptr;
+
+    is_local = MPIDI_REQUEST(sreq, is_local);
+    MPIDI_Datatype_get_info(MPIDIG_REQUEST(sreq, req->rndv_req).count,
+                            MPIDIG_REQUEST(sreq, req->rndv_req).datatype,
+                            is_contig, data_sz, dt_ptr, dt_true_lb);
+
+    if (is_contig) {
+        if (is_local) {
+            mpi_errno =
+                MPIDI_SHM_am_send_contig(MPIDIG_REQUEST(sreq, rank), dst_hnd, src_hnd,
+                                         sizeof(am_rndv_msg_hdr_t), &msg_hdr, data_sz,
+                                         MPIDIG_REQUEST(sreq, req->rndv_req).src_buf, sreq);
+        } else {
+            mpi_errno =
+                MPIDI_NM_am_send_contig(MPIDIG_REQUEST(sreq, rank), dst_hnd, src_hnd,
+                                        sizeof(am_rndv_msg_hdr_t), &msg_hdr, data_sz,
+                                        MPIDIG_REQUEST(sreq, req->rndv_req).src_buf, sreq);
+        }
+    } else {
+        if (is_local) {
+            mpi_errno =
+                MPIDI_SHM_am_send_noncontig(MPIDIG_REQUEST(sreq, rank),
+                                            MPIDIG_REQUEST(sreq, req->rndv_req).src_buf,
+                                            MPIDIG_REQUEST(sreq, req->rndv_req).count,
+                                            MPIDIG_REQUEST(sreq, req->rndv_req).datatype, dst_hnd,
+                                            src_hnd, sizeof(am_rndv_msg_hdr_t), &msg_hdr, sreq);
+        } else {
+            mpi_errno = MPIDI_NM_am_send_noncontig(MPIDIG_REQUEST(sreq, rank),
+                                                   MPIDIG_REQUEST(sreq, req->rndv_req).src_buf,
+                                                   MPIDIG_REQUEST(sreq, req->rndv_req).count,
+                                                   MPIDIG_REQUEST(sreq, req->rndv_req).datatype,
+                                                   dst_hnd, src_hnd, sizeof(am_rndv_msg_hdr_t),
+                                                   &msg_hdr, sreq);
+        }
+    }
+
+    MPIR_ERR_CHECK(mpi_errno);
+
+  fn_exit:
     return mpi_errno;
+  fn_fail:
+    goto fn_exit;
 }
 
 /* am_rndv_send_src_cb - send message (MPIDI_AM_SEND_MSG) handling in sender
