41889e2914aadcbd23a33811f9141fddc9490bce
Yanfei Guo <yguo@anl.gov>
2020-01-07 10:40:17 -0600

    ch4/stubnm: update function signature

--- src/mpid/ch4/netmod/stubnm/stubnm_am.h
+++ src/mpid/ch4/netmod/stubnm/stubnm_am.h
@@ -13,37 +13,28 @@
 
 #include "stubnm_impl.h"
 
-static inline int MPIDI_NM_am_isend(int rank,
-                                    MPIR_Comm * comm,
-                                    int handler_id,
-                                    const void *am_hdr,
-                                    size_t am_hdr_sz,
-                                    const void *data,
-                                    MPI_Count count, MPI_Datatype datatype, MPIR_Request * sreq)
+static inline int MPIDI_NM_am_isend(int rank, MPIR_Comm * comm, int handler_id, const void *am_hdr,
+                                    size_t am_hdr_sz, const void *data, MPI_Count count,
+                                    MPI_Datatype datatype, MPIR_Request * sreq,
+                                    MPIDI_av_entry_t * av)
 {
     MPIR_Assert(0);
     return MPI_SUCCESS;
 }
 
-static inline int MPIDI_NM_am_isendv(int rank,
-                                     MPIR_Comm * comm,
-                                     int handler_id,
-                                     struct iovec *am_hdr,
-                                     size_t iov_len,
-                                     const void *data,
-                                     MPI_Count count, MPI_Datatype datatype, MPIR_Request * sreq)
+static inline int MPIDI_NM_am_isendv(int rank, MPIR_Comm * comm, int handler_id,
+                                     struct iovec *am_hdr, size_t iov_len, const void *data,
+                                     MPI_Count count, MPI_Datatype datatype, MPIR_Request * sreq,
+                                     MPIDI_av_entry_t * av)
 {
     MPIR_Assert(0);
     return MPI_SUCCESS;
 }
 
-static inline int MPIDI_NM_am_isend_reply(MPIR_Context_id_t context_id, int src_rank,
-                                          int handler_id,
-                                          const void *am_hdr,
-                                          size_t am_hdr_sz,
-                                          const void *data,
-                                          MPI_Count count,
-                                          MPI_Datatype datatype, MPIR_Request * sreq)
+static inline int MPIDI_NM_am_isend_reply(int src_rank, MPIR_Comm * comm, int handler_id,
+                                          const void *am_hdr, size_t am_hdr_sz, const void *data,
+                                          MPI_Count count, MPI_Datatype datatype,
+                                          MPIR_Request * sreq, MPIDI_av_entry_t * av)
 {
     MPIR_Assert(0);
     return MPI_SUCCESS;
@@ -55,16 +46,16 @@ static inline size_t MPIDI_NM_am_hdr_max_sz(void)
     return 0;
 }
 
-static inline int MPIDI_NM_am_send_hdr(int rank,
-                                       MPIR_Comm * comm,
-                                       int handler_id, const void *am_hdr, size_t am_hdr_sz)
+static inline int MPIDI_NM_am_send_hdr(int rank, MPIR_Comm * comm, int handler_id,
+                                       const void *am_hdr, size_t am_hdr_sz, MPIDI_av_entry_t * av)
 {
     MPIR_Assert(0);
     return MPI_SUCCESS;
 }
 
-static inline int MPIDI_NM_am_send_hdr_reply(MPIR_Context_id_t context_id, int src_rank,
-                                             int handler_id, const void *am_hdr, size_t am_hdr_sz)
+static inline int MPIDI_NM_am_send_hdr_reply(int src_rank, MPIR_Comm * comm, int handler_id,
+                                             const void *am_hdr, size_t am_hdr_sz,
+                                             MPIDI_av_entry_t * av)
 {
     MPIR_Assert(0);
     return MPI_SUCCESS;
