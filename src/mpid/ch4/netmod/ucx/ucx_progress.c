c380e265ab46a8cc5968ee25c6aa15eed3001bac
Ken Raffenetti <raffenet@mcs.anl.gov>
2020-01-08 08:09:50 -0600

    ch4/ucx: Add comments explaining active message handling

--- src/mpid/ch4/netmod/ucx/ucx_progress.c
+++ src/mpid/ch4/netmod/ucx/ucx_progress.c
@@ -10,7 +10,7 @@
 #include "mpidimpl.h"
 #include "ucx_impl.h"
 
-static void *am_buf;
+static void *am_buf; /* message buffer has global scope because ucx executes am_handler */
 static void am_handler(void *request, ucs_status_t status, ucp_tag_recv_info_t * info)
 {
     MPIR_Request *rreq = NULL;
@@ -83,24 +83,29 @@ int MPIDI_UCX_progress(int vci, int blocking)
     ucp_tag_recv_info_t info;
     MPIDI_UCX_ucp_request_t *ucp_request;
 
-    /* check for active messages */
+    /* TODO: test UCX active message APIs instead of layering over tagged */
     while (true) {
+        /* check for pending active messages */
         ucp_tag_message_h message_handle =
             ucp_tag_probe_nb(MPIDI_UCX_global.worker, MPIDI_UCX_AM_TAG, MPIDI_UCX_AM_TAG, 1,
                              &info);
         if (message_handle == NULL)
             break;
 
+        /* message is available. allocate a buffer and start receiving it */
         am_buf = MPL_malloc(info.length, MPL_MEM_BUFFER);
         ucp_request = (MPIDI_UCX_ucp_request_t *) ucp_tag_msg_recv_nb(MPIDI_UCX_global.worker,
                                                                       am_buf,
                                                                       info.length,
                                                                       ucp_dt_make_contig(1),
                                                                       message_handle, &am_handler);
+
+        /* block until receive completes and am_handler executes */
         while (!ucp_request_is_completed(ucp_request)) {
             ucp_worker_progress(MPIDI_UCX_global.worker);
         }
 
+        /* free resources for handled message */
         ucp_request_release(ucp_request);
         MPL_free(am_buf);
     }
