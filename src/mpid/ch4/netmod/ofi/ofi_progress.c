9a78aea2ac0080c98a735511d7f9bcb77b2af84c
Rohit Zambre <rzambre@uci.edu>
2020-01-09 22:24:03 +0000

    ch4/ofi: Use VNI for internal communication
    Currently, there is a one-to-one mapping between VCIs and VNIs
    in OFI. A VNI in OFI is based on either endpoints or scalable
    endpoints.

--- src/mpid/ch4/netmod/ofi/ofi_progress.c
+++ src/mpid/ch4/netmod/ofi/ofi_progress.c
@@ -15,23 +15,25 @@
 
 int MPIDI_OFI_progress(int vci, int blocking)
 {
-    int mpi_errno;
+    int mpi_errno, vni;
     struct fi_cq_tagged_entry wc[MPIDI_OFI_NUM_CQ_ENTRIES];
     ssize_t ret;
     MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_OFI_PROGRESS);
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_OFI_PROGRESS);
 
+    vni = vci;
+
     if (unlikely(MPIDI_OFI_get_buffered(wc, 1)))
         mpi_errno = MPIDI_OFI_handle_cq_entries(wc, 1);
     else if (likely(1)) {
-        ret = fi_cq_read(MPIDI_OFI_global.ctx[vci].cq, (void *) wc, MPIDI_OFI_NUM_CQ_ENTRIES);
+        ret = fi_cq_read(MPIDI_OFI_global.ctx[vni].cq, (void *) wc, MPIDI_OFI_NUM_CQ_ENTRIES);
 
         if (likely(ret > 0))
             mpi_errno = MPIDI_OFI_handle_cq_entries(wc, ret);
         else if (ret == -FI_EAGAIN)
             mpi_errno = MPI_SUCCESS;
         else
-            mpi_errno = MPIDI_OFI_handle_cq_error(vci, ret);
+            mpi_errno = MPIDI_OFI_handle_cq_error(vni, ret);
     }
 
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_OFI_PROGRESS);
