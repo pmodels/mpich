266e2fa4bf175070a6faca46a022591fb942e5e3
Hui Zhou <hzhou@hzsolutions.net>
2020-01-22 13:56:09 -0600

    ch4/ofi: simplify lightweight send request creation
    The original code was complicated because we assume an asynchronous
    lightweight send, which is not the case. Anyway, the whole reason we
    have lightweight request is for cases we do not do asynchronous
    progress, so it appears that the complications was unnecessary.

--- src/mpid/ch4/netmod/ofi/ofi_send.h
+++ src/mpid/ch4/netmod/ofi/ofi_send.h
@@ -371,10 +371,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_OFI_send(const void *buf, MPI_Aint count, MPI
                                                comm->rank, dst_rank, tag, comm,
                                                context_offset, addr);
         if (!noreq) {
-            MPIDI_OFI_SEND_REQUEST_CREATE_LW_CONDITIONAL(*request);
-            /* If we set CC>0 in case of injection, we need to decrement the CC
-             * to tell the main thread we completed the injection. */
-            MPIDI_OFI_SEND_REQUEST_COMPLETE_LW_CONDITIONAL(*request);
+            *request = MPIR_Request_create_complete(MPIR_REQUEST_KIND__SEND);
         }
     } else {
         mpi_errno = MPIDI_OFI_send_normal(buf, count, datatype, dst_rank, tag, comm,
