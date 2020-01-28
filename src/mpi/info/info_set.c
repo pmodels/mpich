pmodels/mpich

commit 0171ea2442b5d34ca664025050475a0ac195a937

warnings: check returns from MPIR_Info_set_impl
Coverity warns that we throw away the error return.

src/mpi/info/info_set.c
@@ -102,7 +102,8 @@ int MPI_Info_set(MPI_Info info, const char *key, const char *value)
 #endif /* HAVE_ERROR_CHECKING */
 
     /* ... body of routine ...  */
-    MPIR_Info_set_impl(info_ptr, key, value);
+    mpi_errno = MPIR_Info_set_impl(info_ptr, key, value);
+    MPIR_ERR_CHECK(mpi_errno);
     /* ... end of body of routine ... */
 
 #ifdef HAVE_ERROR_CHECKING

src/mpi/comm/commutil.c
@@ -130,14 +130,21 @@ int MPII_Comm_set_hints(MPIR_Comm * comm_ptr, MPIR_Info * info)
 
 int MPII_Comm_get_hints(MPIR_Comm * comm_ptr, MPIR_Info * info)
 {
+    int mpi_errno = MPI_SUCCESS;
+
     char hint_val_str[MPI_MAX_INFO_VAL];
     for (int i = 0; i < next_comm_hint_index; i++) {
         if (MPIR_comm_hint_list[i].key) {
             get_string_value(hint_val_str, MPIR_comm_hint_list[i].type, comm_ptr->hints[i]);
-            MPIR_Info_set_impl(info, MPIR_comm_hint_list[i].key, hint_val_str);
+            mpi_errno = MPIR_Info_set_impl(info, MPIR_comm_hint_list[i].key, hint_val_str);
+            MPIR_ERR_CHECK(mpi_errno);
         }
     }
-    return MPI_SUCCESS;
+
+  fn_exit:
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
 }
 
 int MPII_Comm_check_hints(MPIR_Comm * comm)

src/mpid/ch3/src/ch3u_win_fns.c
@@ -430,7 +430,7 @@ int MPID_Win_get_info(MPIR_Win * win, MPIR_Info ** info_used)
                 c += snprintf(buf + c, BUFSIZE - c, "%swaw", (c > 0) ? "," : "");
         }
 
-        MPIR_Info_set_impl(*info_used, "accumulate_ordering", buf);
+        mpi_errno = MPIR_Info_set_impl(*info_used, "accumulate_ordering", buf);
         MPIR_ERR_CHECK(mpi_errno);
 #undef BUFSIZE
     }

commit 9bf64d1e40e0e14ef3706e98cb7234d13daf141f

mpi/info: Fix preprocessor guards for error checking
We always check the return code from MPIR_Info_set_impl, so the
failure and exit labels must always be defined. Fixes build error with
--disable-error-checking introduced in [0171ea2442b5].

src/mpi/info/info_set.c
@@ -106,26 +106,24 @@ int MPI_Info_set(MPI_Info info, const char *key, const char *value)
     MPIR_ERR_CHECK(mpi_errno);
     /* ... end of body of routine ... */
 
-#ifdef HAVE_ERROR_CHECKING
   fn_exit:
-#endif
     MPIR_FUNC_TERSE_EXIT(MPID_STATE_MPI_INFO_SET);
     MPID_THREAD_CS_EXIT(GLOBAL, MPIR_THREAD_GLOBAL_ALLFUNC_MUTEX);
     MPID_THREAD_CS_EXIT(VCI, MPIR_THREAD_VCI_GLOBAL_MUTEX);
     return mpi_errno;
 
-#ifdef HAVE_ERROR_CHECKING
   fn_fail:
     /* --BEGIN ERROR HANDLING-- */
+#ifdef HAVE_ERROR_CHECKING
     {
         mpi_errno =
             MPIR_Err_create_code(mpi_errno, MPIR_ERR_RECOVERABLE, __func__, __LINE__, MPI_ERR_OTHER,
                                  "**mpi_info_set", "**mpi_info_set %I %s %s", info, key, value);
     }
+#endif
     mpi_errno = MPIR_Err_return_comm(NULL, __func__, mpi_errno);
     goto fn_exit;
     /* --END ERROR HANDLING-- */
-#endif
 }
 
 