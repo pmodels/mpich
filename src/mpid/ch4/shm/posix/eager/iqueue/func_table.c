9d1f9746d5d49329e9f18247b90d5dc3c03e4744
Wesley Bland <wesley.bland@intel.com>
2020-01-31 09:28:29 -0600

    ch4/iqueue: Update Intel Copyrights

--- src/mpid/ch4/shm/posix/eager/iqueue/func_table.c
+++ src/mpid/ch4/shm/posix/eager/iqueue/func_table.c
@@ -3,7 +3,7 @@
  *      See COPYRIGHT in top-level directory.
  *
  *  Portions of this code were written by Intel Corporation.
- *  Copyright (C) 2011-2017 Intel Corporation.  Intel provides this material
+ *  Copyright (C) 2011-2020 Intel Corporation.  Intel provides this material
  *  to Argonne National Laboratory subject to Software Grant and Corporate
  *  Contributor License Agreement dated February 8, 2012.
  */
