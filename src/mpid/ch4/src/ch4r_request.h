a9b41a368b84660570a1eda5d2bed8e894e27944
Hui Zhou <hzhou321@anl.gov>
2020-02-03 14:03:59 -0600

    ch4: add convenience functions for handling any source
    Add routines to centralize handling of anysource partner handling.
    Provide better comments to explain the logic.

--- src/mpid/ch4/src/ch4r_request.h
+++ src/mpid/ch4/src/ch4r_request.h
@@ -140,4 +140,86 @@ static inline int MPIDI_anysource_matched(MPIR_Request * rreq, int caller, int *
     return mpi_errno;
 }
 
+#ifndef MPIDI_CH4_DIRECT_NETMOD
+/* mpi-layer request handle is connected to one of the request (the one with shm),
+ * so we can't simply free one at matching time. Both need be completed and freed
+ * at the same time.
+ *
+ * At matching time, we cancel one of them (but not freed).
+ * At completion time, we complete and free both.
+ */
+static inline int MPIDI_anysrc_try_cancel(MPIR_Request * rreq, int *is_cancelled)
+{
+    int mpi_errno = MPI_SUCCESS;
+
+    *is_cancelled = 1;  /* overwrite if cancel fails */
+
+    MPIR_Request *anysrc_partner = MPIDI_REQUEST_ANYSOURCE_PARTNER(rreq);
+    if (unlikely(anysrc_partner)) {
+        if (!MPIR_STATUS_GET_CANCEL_BIT(anysrc_partner->status)) {
+            if (MPIDI_REQUEST(rreq, is_local)) {
+                /* SHM, cancel NM partner */
+                mpi_errno = MPIDI_NM_mpi_cancel_recv(anysrc_partner);
+                MPIR_ERR_CHECK(mpi_errno);
+                if (!MPIR_STATUS_GET_CANCEL_BIT(anysrc_partner->status)) {
+                    /* failed, cancel SHM rreq instead
+                     * NOTE: comm and datatype will be defreferenced at caller site
+                     */
+                    MPIR_STATUS_SET_CANCEL_BIT(rreq->status, TRUE);
+                    *is_cancelled = 0;
+                } else {
+                    /* NM partner is not user-visible, free it now, which means
+                     * explicit SHM code can skip MPIDI_anysrc_free_partner
+                     */
+                    MPIDI_REQUEST_ANYSOURCE_PARTNER(rreq) = NULL;
+                    MPIDI_REQUEST_ANYSOURCE_PARTNER(anysrc_partner) = NULL;
+                    /* cancel freed it once, freed once more on behalf of mpi-layer */
+                    MPIR_Request_free(anysrc_partner);
+                }
+            } else {
+                /* NM, cancel SHM partner */
+                int c;
+                /* prevent free, we'll complete it separately */
+                MPIR_cc_incr(anysrc_partner->cc_ptr, &c);
+                mpi_errno = MPIDI_SHM_mpi_cancel_recv(anysrc_partner);
+                MPIR_ERR_CHECK(mpi_errno);
+
+                /* NOTE: We assume the cancel is always successful.
+                 *       This is because SHM won't match unless NM cancels.
+                 *       We only reach here when NM is still active.
+                 */
+            }
+        }
+    }
+
+  fn_exit:
+    return MPI_SUCCESS;
+  fn_fail:
+    goto fn_exit;
+}
+
+static inline void MPIDI_anysrc_free_partner(MPIR_Request * rreq)
+{
+    MPIR_Request *anysrc_partner = MPIDI_REQUEST_ANYSOURCE_PARTNER(rreq);
+    if (unlikely(anysrc_partner)) {
+        if (!MPIDI_REQUEST(rreq, is_local)) {
+            /* NM, complete and free SHM partner */
+            MPIDI_REQUEST_ANYSOURCE_PARTNER(rreq) = NULL;
+            MPIDI_REQUEST_ANYSOURCE_PARTNER(anysrc_partner) = NULL;
+            MPID_Request_complete(anysrc_partner);
+            /* copy status to SHM partner */
+            anysrc_partner->status = rreq->status;
+            /* free NM request on behalf of mpi-layer
+             * final free for NM request happen at call-site MPID_Request_complete
+             * final free for SHM partner happen at mpi-layer
+             */
+            MPIR_Request_free(rreq);
+        } else {
+            /* SHM, NM partner should already been freed (this branch can't happen) */
+            MPIR_Assert(0);
+        }
+    }
+}
+#endif /* MPIDI_CH4_DIRECT_NETMOD */
+
 #endif /* CH4R_REQUEST_H_INCLUDED */
