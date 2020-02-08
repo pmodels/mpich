314509735706d4dc7336529b47133aaf92b4509b
Hui Zhou <hzhou321@anl.gov>
2020-02-07 23:09:59 -0500

    ch4/am: add new file mpidig_msg.h
    This file hosts supporting inline routines that are used for generic
    layer message matching and data copying. It is mainly used to support
    generic layer am protocols.
    
    Due to some transport need asynchronous payload transfer, the target
    message callback often need to be split into process, data-copy, and
    state cleanup stages. The data-copying part may be transport-specific
    (eg rdma-read), but the data structure and flow are common. The
    immediate goal is to refactor most of common code into mpidig_msg.h and
    minimize the interface exposed to transport. This will reduce the code
    duplications among netmods and shmmods. Once we moved the data structure
    and semantics internal, we can easily modify and experiment with
    different data transfer model, such as between using iov and directly
    using MPIR_Typerep_unpace.

---
+++ src/mpid/ch4/generic/mpidig_msg.h
@@ -0,0 +1,15 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2020 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+#ifndef MPIDIG_MSG_H_INCLUDED
+#define MPIDIG_MSG_H_INCLUDED
+
+/* This file is for supporting routines used for generic layer message matching
+ * and data transfer. They are used by protocols such as send, ssend, and send_long.
+ * Other protocols that share semantic with message transport may also use these
+ * routines to avoid code duplications.
+ */
+
+#endif /* MPIDIG_MSG_H_INCLUDED */
