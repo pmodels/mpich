609a75cbd66388c68ef423c24bcc91ca316cc7e5
Hui Zhou <hzhou321@anl.gov>
2020-02-12 23:03:45 -0500

    ch4/ofi: update callback interface for OFI internal control

--- src/mpid/ch4/netmod/ofi/ofi_impl.h
+++ src/mpid/ch4/netmod/ofi/ofi_impl.h
@@ -190,9 +190,8 @@ MPL_STATIC_INLINE_PREFIX void MPIDI_OFI_cntr_incr()
 /* Externs:  see util.c for definition */
 int MPIDI_OFI_handle_cq_error_util(int ep_idx, ssize_t ret);
 int MPIDI_OFI_retry_progress(void);
-int MPIDI_OFI_control_handler(int handler_id, void *am_hdr,
-                              void **data, size_t * data_sz, int is_local, int *is_contig,
-                              MPIR_Request ** req);
+int MPIDI_OFI_control_handler(int handler_id, void *am_hdr, void *data, MPI_Aint data_sz,
+                              int is_local, int is_async, MPIR_Request ** req);
 int MPIDI_OFI_control_dispatch(void *buf);
 void MPIDI_OFI_index_datatypes(void);
 int MPIDI_OFI_mr_key_allocator_init(void);
