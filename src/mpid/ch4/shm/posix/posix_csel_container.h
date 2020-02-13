6e8688d7758db1a873d7e8eb80141f41787719e0
Surabhi Jain <surabhi.jain@intel.com>
2020-02-13 11:31:40 -0600

    posix/coll: Add release_gather based intra-node barrier
    Implement barrier as a gather step followed by release step in the
    release_gather framework.

--- src/mpid/ch4/shm/posix/posix_csel_container.h
+++ src/mpid/ch4/shm/posix/posix_csel_container.h
@@ -4,6 +4,7 @@
 typedef struct {
     enum {
         MPIDI_POSIX_CSEL_CONTAINER_TYPE__ALGORITHM__MPIDI_POSIX_mpi_bcast_release_gather,
+        MPIDI_POSIX_CSEL_CONTAINER_TYPE__ALGORITHM__MPIDI_POSIX_mpi_barrier_release_gather,
         MPIDI_POSIX_CSEL_CONTAINER_TYPE__ALGORITHM__MPIDI_POSIX_mpi_allreduce_release_gather,
         MPIDI_POSIX_CSEL_CONTAINER_TYPE__ALGORITHM__MPIDI_POSIX_mpi_reduce_release_gather,
     } id;
