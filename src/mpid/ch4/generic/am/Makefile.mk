469d2304cc5c45ef1a1a5ec645c4f17dda3438e4
Giuseppe Congiu <gcongiu@anl.gov>
2020-02-29 21:41:38 -0600

    mpidig: move am to separate subdir
    The generic layer should provide functionalities that go beyond active
    message. For this reason we prepare the ground for such new
    functionalities, e.g. GPU support, by moving the AM code in a separate
    subdirectory.

---
+++ src/mpid/ch4/generic/am/Makefile.mk
@@ -0,0 +1,21 @@
+## -*- Mode: Makefile; -*-
+## vim: set ft=automake :
+##
+## (C) 2016 by Argonne National Laboratory.
+##     See COPYRIGHT in top-level directory.
+##
+##  Portions of this code were written by Intel Corporation.
+##  Copyright (C) 2011-2016 Intel Corporation.  Intel provides this material
+##  to Argonne National Laboratory subject to Software Grant and Corporate
+##  Contributor License Agreement dated February 8, 2012.
+##
+
+AM_CPPFLAGS += -I$(top_srcdir)/src/mpid/ch4/generic/am
+
+noinst_HEADERS += src/mpid/ch4/generic/am/mpidig_send.h \
+                  src/mpid/ch4/generic/am/mpidig_recv.h \
+                  src/mpid/ch4/generic/am/mpidig_msg.h \
+                  src/mpid/ch4/generic/am/mpidig.h
+
+mpi_core_sources += src/mpid/ch4/generic/am/mpidig_globals.c \
+                    src/mpid/ch4/generic/am/mpidig_init.c
