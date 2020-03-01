5b29b2065c3c254c56b6b4e23c8abeb9f670d149
Pavan Balaji <balaji@anl.gov>
2020-03-01 02:21:57 -0600

    ch4/posix: use the new csel infrastructure
    Integrate support for the new json selection infrastructure into
    CH4/POSIX.  This patch uses an empty json file, which is equivalent to
    the current default.

--- src/mpid/ch4/shm/posix/posix_comm.c
+++ src/mpid/ch4/shm/posix/posix_comm.c
@@ -21,10 +21,14 @@ int MPIDI_POSIX_mpi_comm_commit_pre_hook(MPIR_Comm * comm)
     /* Release_gather primitives based collective algorithm works for Intra-comms only */
     if (comm->comm_kind == MPIR_COMM_KIND__INTRACOMM) {
         mpi_errno = MPIDI_POSIX_mpi_release_gather_comm_init_null(comm);
+        MPIR_ERR_CHECK(mpi_errno);
     }
 
+  fn_exit:
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_POSIX_MPI_COMM_COMMIT_PRE_HOOK);
     return mpi_errno;
+  fn_fail:
+    goto fn_exit;
 }
 
 int MPIDI_POSIX_mpi_comm_commit_post_hook(MPIR_Comm * comm)
@@ -34,8 +38,16 @@ int MPIDI_POSIX_mpi_comm_commit_post_hook(MPIR_Comm * comm)
     MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_POSIX_MPI_COMM_COMMIT_POST_HOOK);
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_POSIX_MPI_COMM_COMMIT_POST_HOOK);
 
+    /* prune selection tree */
+    mpi_errno = MPIR_Csel_prune(MPIDI_global.shm.posix.csel_root, comm,
+                                &MPIDI_POSIX_COMM(comm)->csel_comm);
+    MPIR_ERR_CHECK(mpi_errno);
+
+  fn_exit:
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_POSIX_MPI_COMM_COMMIT_POST_HOOK);
     return mpi_errno;
+  fn_fail:
+    goto fn_exit;
 }
 
 int MPIDI_POSIX_mpi_comm_free_hook(MPIR_Comm * comm)
@@ -44,11 +56,20 @@ int MPIDI_POSIX_mpi_comm_free_hook(MPIR_Comm * comm)
     MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_POSIX_MPI_COMM_FREE_HOOK);
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_POSIX_MPI_COMM_FREE_HOOK);
 
+    if (MPIDI_POSIX_COMM(comm)->csel_comm) {
+        mpi_errno = MPIR_Csel_free(MPIDI_POSIX_COMM(comm)->csel_comm);
+        MPIR_ERR_CHECK(mpi_errno);
+    }
+
     /* Release_gather primitives based collective algorithm works for Intra-comms only */
     if (comm->comm_kind == MPIR_COMM_KIND__INTRACOMM) {
         mpi_errno = MPIDI_POSIX_mpi_release_gather_comm_free(comm);
+        MPIR_ERR_CHECK(mpi_errno);
     }
 
+  fn_exit:
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_POSIX_MPI_COMM_FREE_HOOK);
     return mpi_errno;
+  fn_fail:
+    goto fn_exit;
 }
