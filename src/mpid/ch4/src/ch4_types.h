d2faa2c94fc75771c8e7a246471b12b9e3d3e631
Hui Zhou <hzhou321@anl.gov>
2020-03-07 14:27:11 -0600

    ch4: convert progress_count from OPA to MPL atomics

--- src/mpid/ch4/src/ch4_types.h
+++ src/mpid/ch4/src/ch4_types.h
@@ -323,7 +323,7 @@ typedef struct MPIDI_CH4_Global_t {
     volatile int sigusr1_count;
     int my_sigusr1_count;
 #endif
-    OPA_int_t progress_count;
+    MPL_atomic_int_t progress_count;
 
     MPID_Thread_mutex_t vci_lock;
     int n_vcis;
