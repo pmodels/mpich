fbd33376facc000b38bf3a7285ecafa17682b6f2
Shintaro Iwasaki <siwasaki@anl.gov>
2020-03-10 12:09:43 -0500

    mpl/atomic: declare MPL_emulation_lock only when needed
    MPL_emulation_lock is used only when lock-based atomic is enabled.

--- src/mpl/src/atomic/mpl_atomic.c
+++ src/mpl/src/atomic/mpl_atomic.c
@@ -4,16 +4,17 @@
  *      See COPYRIGHT in top-level directory.
  */
 
+#include "mpl_atomic.h"
+
+#ifdef MPL_USE_LOCK_BASED_PRIMITIVES
+
 /* FIXME For now we rely on pthreads for our IPC locks.  This is fairly
  * portable, although it is obviously not 100% portable.  We need to figure out
  * how to support other threading packages and lock implementations, such as the
  * BG/P lockbox. */
 
-#include "mpl_atomic.h"
-
-#ifdef MPL_HAVE_PTHREAD_H
 #include <pthread.h>
 
 pthread_mutex_t MPL_emulation_lock = PTHREAD_MUTEX_INITIALIZER;
 
-#endif /* MPL_HAVE_PTHREAD_H */
+#endif /* MPL_USE_LOCK_BASED_PRIMITIVES */
