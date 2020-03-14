66fe66a4619aebb46070d4ee0636ae40935c5645
Pavan Balaji <balaji@anl.gov>
2020-03-13 19:58:57 -0500

    copyright: add copyright to edited files
    Signed-off-by: Pavan Balaji <balaji@anl.gov>

--- src/mpl/include/utarray.h
+++ src/mpl/include/utarray.h
@@ -1,3 +1,9 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2001 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
 /*
 Copyright (c) 2008-2011, Troy D. Hanson   http://uthash.sourceforge.net
 All rights reserved.
