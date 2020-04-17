a03b0561e646320d923f829d460009824d0f6d94
Pavan Balaji <balaji@anl.gov>
2020-04-17 14:57:23 -0500

    typerep: prereq to allow typerep to have multiple backends

---
+++ src/mpi/datatype/typerep/src/Makefile.mk
@@ -0,0 +1,12 @@
+##
+## Copyright (C) by Argonne National Laboratory
+##     See COPYRIGHT in top-level directory
+##
+
+mpi_core_sources += \
+    src/mpi/datatype/typerep/src/typerep_dataloop_flatten.c \
+    src/mpi/datatype/typerep/src/typerep_dataloop_pack.c \
+    src/mpi/datatype/typerep/src/typerep_dataloop_pack_external.c \
+    src/mpi/datatype/typerep/src/typerep_dataloop_iov.c \
+    src/mpi/datatype/typerep/src/typerep_dataloop_create.c \
+    src/mpi/datatype/typerep/src/typerep_dataloop_debug.c
