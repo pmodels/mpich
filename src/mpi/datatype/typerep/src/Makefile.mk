c3378653685f613ae57f2c9655afab4ccab9332c
Pavan Balaji <balaji@anl.gov>
2020-04-17 15:38:21 -0500

    typerep: extend typerep API to allow for yaksa integration

--- src/mpi/datatype/typerep/src/Makefile.mk
+++ src/mpi/datatype/typerep/src/Makefile.mk
@@ -4,9 +4,10 @@
 ##
 
 mpi_core_sources += \
+    src/mpi/datatype/typerep/src/typerep_dataloop_init.c \
     src/mpi/datatype/typerep/src/typerep_dataloop_flatten.c \
     src/mpi/datatype/typerep/src/typerep_dataloop_pack.c \
     src/mpi/datatype/typerep/src/typerep_dataloop_pack_external.c \
     src/mpi/datatype/typerep/src/typerep_dataloop_iov.c \
-    src/mpi/datatype/typerep/src/typerep_dataloop_create.c \
+    src/mpi/datatype/typerep/src/typerep_dataloop_commit.c \
     src/mpi/datatype/typerep/src/typerep_dataloop_debug.c
