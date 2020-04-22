5901e2001cf4fd55fe826831ca01c16a4f7c9cac
Surabhi Jain <surabhi.jain@intel.com>
2020-04-22 13:27:48 -0500

    posix/coll: Add mpir csel containers
    Handle the case when json picks any mpir algorithm over posix
    algorithms. We cannot expect that the json will return an empty
    container for this case.

--- src/mpid/ch4/shm/posix/posix_csel_container.h
+++ src/mpid/ch4/shm/posix/posix_csel_container.h
@@ -9,9 +9,13 @@
 typedef struct {
     enum {
         MPIDI_POSIX_CSEL_CONTAINER_TYPE__ALGORITHM__MPIDI_POSIX_mpi_bcast_release_gather,
+        MPIDI_POSIX_CSEL_CONTAINER_TYPE__ALGORITHM__MPIR_Bcast_impl,
         MPIDI_POSIX_CSEL_CONTAINER_TYPE__ALGORITHM__MPIDI_POSIX_mpi_barrier_release_gather,
+        MPIDI_POSIX_CSEL_CONTAINER_TYPE__ALGORITHM__MPIR_Barrier_impl,
         MPIDI_POSIX_CSEL_CONTAINER_TYPE__ALGORITHM__MPIDI_POSIX_mpi_allreduce_release_gather,
+        MPIDI_POSIX_CSEL_CONTAINER_TYPE__ALGORITHM__MPIR_Allreduce_impl,
         MPIDI_POSIX_CSEL_CONTAINER_TYPE__ALGORITHM__MPIDI_POSIX_mpi_reduce_release_gather,
+        MPIDI_POSIX_CSEL_CONTAINER_TYPE__ALGORITHM__MPIR_Reduce_impl,
     } id;
 } MPIDI_POSIX_csel_container_s;
 
