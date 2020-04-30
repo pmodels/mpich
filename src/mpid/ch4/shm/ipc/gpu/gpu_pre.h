171f1ed814abb1ed5ab5d85a6166dfb995c47669
Giuseppe Congiu <gcongiu@anl.gov>
2020-04-30 11:48:12 -0500

    ipc/gpu: define gpu submodule
    Define GPU IPC submodule. Currently GPU submodule provides very minimal
    functionalities for handling GPU IPC memory handles, e.g., get, open and
    close, which in turn return a handle for a buffer, map it into another
    process address space and unmap it from another process address space.
    
    Handles are not currently cached. Future support will add memory handles
    caching in the receiver so that remote memory can be mapped once and
    accessed multiple times when the handle does not change.

---
+++ src/mpid/ch4/shm/ipc/gpu/gpu_pre.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) by Argonne National Laboratory
+ *     See COPYRIGHT in top-level directory
+ */
+
+#ifndef GPU_PRE_H_INCLUDED
+#define GPU_PRE_H_INCLUDED
+
+typedef struct MPIDI_GPU_mem_handle {
+    MPL_gpu_ipc_mem_handle_t ipc_handle;
+} MPIDI_GPU_mem_handle_t;
+
+typedef struct MPIDI_GPU_mem_seg {
+    void *vaddr;
+} MPIDI_GPU_mem_seg_t;
+
+#endif /* GPU_PRE_H_INCLUDED */
