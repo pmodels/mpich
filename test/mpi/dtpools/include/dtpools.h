d8dd756eadd706febd9778ba17a315ba8b3c0ac1
Hui Zhou <hzhou321@anl.gov>
2020-05-03 17:00:17 -0500

    dtpools: add DTP_obj_create_idx
    Each DTP datatype can be reliablly recreated by setting the internal
    random index. This PR provides the interface to generate specific
    datatype matching the given random index for more specific
    reproducibility.

--- test/mpi/dtpools/include/dtpools.h
+++ test/mpi/dtpools/include/dtpools.h
@@ -36,6 +36,7 @@ int DTP_pool_create(const char *basic_type_str, MPI_Aint basic_type_count, int s
 int DTP_pool_free(DTP_pool_s dtp);
 
 int DTP_obj_create(DTP_pool_s dtp, DTP_obj_s * obj, MPI_Aint maxbufsize);
+int DTP_obj_create_idx(DTP_pool_s dtp, DTP_obj_s * obj, MPI_Aint maxbufsize, int rand_idx);
 int DTP_obj_free(DTP_obj_s obj);
 int DTP_obj_get_description(DTP_obj_s obj, char **desc);
 
