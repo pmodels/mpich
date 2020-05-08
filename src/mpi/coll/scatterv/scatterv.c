acc95513a9aad1006c028b91d6387b0c8150b8b0
Hui Zhou <hzhou321@anl.gov>
2020-05-08 08:12:18 -0500

    coll: unreachable statement
    Looks like we have covered all error cases in the branches so the last
    check is not needed. Coverity reports unreachable statement here.

--- src/mpi/coll/scatterv/scatterv.c
+++ src/mpi/coll/scatterv/scatterv.c
@@ -370,9 +370,6 @@ int MPI_Scatterv(const void *sendbuf, const int *sendcounts, const int *displs,
                     MPIR_ERRTEST_USERBUFFER(recvbuf, recvcount, recvtype, mpi_errno);
                 }
             }
-
-            if (mpi_errno != MPI_SUCCESS)
-                goto fn_fail;
         }
         MPID_END_ERROR_CHECKS;
     }
