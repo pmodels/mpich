c554fe0f955569f599d5a031cc6483c77474f56c
Min Si <msi@anl.gov>
2020-05-31 17:13:29 -0500

    ch4/ipc: fix stub noninline functions

--- src/mpid/ch4/shm/ipc/gpu/gpu_noinline.h
+++ src/mpid/ch4/shm/ipc/gpu/gpu_noinline.h
@@ -12,8 +12,17 @@
 int MPIDI_GPU_mpi_init_hook(int rank, int size, int *tag_bits);
 int MPIDI_GPU_mpi_finalize_hook(void);
 #else
-#define MPIDI_GPU_mpi_init_hook(...) MPI_SUCCESS
-#define MPIDI_GPU_mpi_finalize_hook(...) MPI_SUCCESS
+
+/* stub noninline functions */
+MPL_STATIC_INLINE_PREFIX int MPIDI_GPU_mpi_init_hook(int rank, int size, int *tag_bits)
+{
+    return MPI_SUCCESS;
+}
+
+MPL_STATIC_INLINE_PREFIX int MPIDI_GPU_mpi_finalize_hook(void)
+{
+    return MPI_SUCCESS;
+}
 #endif
 
 #endif /* GPU_NOINLINE_H_INCLUDED */
