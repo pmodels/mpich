af4c22858b06b2343b4717ae53d5a41be48b4ddd
Min Si <msi@anl.gov>
2020-06-08 12:54:59 -0500

    ch4/shm/xpmem: hide xpmem internal types and header
    xpmem_type.h should not be exposed at xpmem_pre.h. Thus we use a void*
    type for MPIDI_XPMEM_mem_seg_t in xpmem_pre.h and cast internally. We
    also move xpmem.h to internal. This patch also fixes clang warning
    "redefinition of typedef."

--- src/mpid/ch4/shm/ipc/xpmem/xpmem_pre.h
+++ src/mpid/ch4/shm/ipc/xpmem/xpmem_pre.h
@@ -6,19 +6,13 @@
 #ifndef XPMEM_PRE_H_INCLUDED
 #define XPMEM_PRE_H_INCLUDED
 
-#ifdef MPIDI_CH4_SHM_ENABLE_XPMEM
-#include <xpmem.h>
-#endif
-
-typedef struct MPIDI_XPMEMI_seg MPIDI_XPMEMI_seg_t;
-
 /* memory handle definition */
 typedef struct {
     uint64_t src_offset;
 } MPIDI_XPMEM_mem_handle_t;
 
 typedef struct {
-    MPIDI_XPMEMI_seg_t *seg_ptr;
+    void *seg_ptr;
 } MPIDI_XPMEM_mem_seg_t;
 
 /* request extension definition */
