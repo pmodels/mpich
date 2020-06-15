5658c6a9145e74d9fabc4c541a3d1265560cc372
Ken Raffenetti <raffenet@mcs.anl.gov>
2020-06-14 19:47:33 -0500

    test/mpi: Enable GPU buffers for coll dtpools tests
    Enable testing of collective buffers in GPU memory. Only bcast is
    covered at this time.

--- test/mpi/maint/gentests_dtp.sh
+++ test/mpi/maint/gentests_dtp.sh
@@ -191,6 +191,15 @@ while read -r line ; do
                     echo "${testname} $procs arg=-type=${type} arg=-count=${sendcount} arg=-seed=$seed arg=-testsize=${gputestsize} ${other_args} arg=-origmem=device arg=-targetmem=device $timelimit" >> ${builddir}/${testdir}/testlist.gpu
                     seed=$((seed + 1))
                     fi
+            elif [ $testdir = "coll" ] ; then
+                echo "${testname} $procs arg=-type=${type} arg=-count=${sendcount} arg=-seed=$seed arg=-testsize=${testsize} ${other_args} arg=-evenmemtype=host arg=-oddmemtype=host $timelimit" >> ${builddir}/${testdir}/testlist.dtp
+                seed=$((seed + 1))
+                echo "${testname} $procs arg=-type=${type} arg=-count=${sendcount} arg=-seed=$seed arg=-testsize=${gputestsize} ${other_args} arg=-evenmemtype=host arg=-oddmemtype=device $timelimit" >> ${builddir}/${testdir}/testlist.gpu
+                seed=$((seed + 1))
+                echo "${testname} $procs arg=-type=${type} arg=-count=${sendcount} arg=-seed=$seed arg=-testsize=${gputestsize} ${other_args} arg=-evenmemtype=device arg=-oddmemtype=reg_host $timelimit" >> ${builddir}/${testdir}/testlist.gpu
+                seed=$((seed + 1))
+                echo "${testname} $procs arg=-type=${type} arg=-count=${sendcount} arg=-seed=$seed arg=-testsize=${gputestsize} ${other_args} arg=-evenmemtype=device arg=-oddmemtype=device $timelimit" >> ${builddir}/${testdir}/testlist.gpu
+                seed=$((seed + 1))
             else
                 echo "${testname} $procs arg=-type=${type} arg=-count=${sendcount} arg=-seed=$seed arg=-testsize=${testsize} ${other_args} $timelimit" >> ${builddir}/${testdir}/testlist.dtp
                 seed=$((seed + 1))
