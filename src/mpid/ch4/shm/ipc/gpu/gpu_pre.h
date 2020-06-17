2ddbc273db78f99f2bea6ae58293884715a83437
Kaiming Ouyang <kouya001@ucr.edu>
2020-06-17 14:15:55 -0500

    ch4/gpu: move GPU get_attr/attach/close function to .c file
    MPIDI_GPU_get_mem_attr, MPIDI_GPU_attach_mem and MPIDI_GPU_close_mem
    function performance is not critical, so they should be moved to .c file.

--- src/mpid/ch4/shm/ipc/gpu/gpu_pre.h
+++ src/mpid/ch4/shm/ipc/gpu/gpu_pre.h
@@ -6,6 +6,8 @@
 #ifndef GPU_PRE_H_INCLUDED
 #define GPU_PRE_H_INCLUDED
 
+#include "mpl.h"
+
 typedef struct MPIDI_GPU_mem_handle {
     MPL_gpu_ipc_mem_handle_t ipc_handle;
 } MPIDI_GPU_mem_handle_t;
