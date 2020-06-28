4d78c44525ccceff3fbb3352628be3a9ddc69fc5
Kaiming Ouyang <kouya001@ucr.edu>
2020-06-28 00:43:09 -0500

    ch4/gpu: invalidate cached handle buffer after cuda free
    after user calls cudaFree/cuMemFree, sender will delete all cached
    ipc handle related to freed buffer; then, during next communication,
    it will tell receiver to invalidate the corresponding cached ipc
    handle to assure stale cached ones are not used anymore

--- src/mpid/ch4/shm/ipc/gpu/gpu_pre.h
+++ src/mpid/ch4/shm/ipc/gpu/gpu_pre.h
@@ -8,6 +8,11 @@
 
 #include "mpl.h"
 
+enum {
+    MPIDI_GPU_IPC_HANDLE_VALID,
+    MPIDI_GPU_IPC_HANDLE_REMAP_REQUIRED,
+};
+
 typedef struct MPIDI_GPU_ipc_handle {
     MPL_gpu_ipc_mem_handle_t ipc_handle;
     int global_dev_id;
@@ -15,6 +20,7 @@ typedef struct MPIDI_GPU_ipc_handle {
     uintptr_t len;
     int node_rank;
     uintptr_t offset;
+    int handle_status;
 } MPIDI_GPU_ipc_handle_t;
 
 #endif /* GPU_PRE_H_INCLUDED */
