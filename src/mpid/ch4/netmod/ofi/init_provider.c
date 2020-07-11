fd4ac86fe20d7044c41e7d6ba54796db6516c337
Hui Zhou <hzhou321@anl.gov>
2020-07-11 12:11:44 -0500

    ch4/ofi: consolidate init_provider and probe settings
    The initial provider probe with NULL hints does not indicate whether
    full atomic ordering is supported. Patch it with extra trials. It is
    necessary to consolidate the code to carry out this patch.

--- src/mpid/ch4/netmod/ofi/init_provider.c
+++ src/mpid/ch4/netmod/ofi/init_provider.c
@@ -6,9 +6,7 @@
 #include "mpidimpl.h"
 #include "ofi_impl.h"
 
-/* find best matching provider and populate hints */
-static int find_provider(struct fi_info *hints);
-/* picks one matching provider from the list or return NULL */
+static int find_provider(void);
 static struct fi_info *pick_provider_from_list(const char *provname, struct fi_info *prov_list);
 
 /* pick provider, set MPIDI_OFI_global.prov_use, and update global settings */
@@ -16,69 +14,41 @@ int MPIDI_OFI_init_provider(void)
 {
     int mpi_errno = MPI_SUCCESS;
 
-    struct fi_info *prov = NULL;
-
     /* First, find the provider and prepare the hints */
-    struct fi_info *hints = fi_allocinfo();
-    MPIR_Assert(hints != NULL);
-
-    mpi_errno = find_provider(hints);
+    mpi_errno = find_provider();
     MPIR_ERR_CHECK(mpi_errno);
 
-    /* Second, get the actual fi_info * prov */
-    MPIDI_OFI_CALL(fi_getinfo(MPIDI_OFI_get_fi_version(), NULL, NULL, 0ULL, hints, &prov), getinfo);
-    MPIR_ERR_CHKANDJUMP(prov == NULL, mpi_errno, MPI_ERR_OTHER, "**ofid_getinfo");
-    if (!MPIDI_OFI_ENABLE_RUNTIME_CHECKS) {
-        int set_number = MPIDI_OFI_get_set_number(prov->fabric_attr->prov_name);
-        MPIR_ERR_CHKANDJUMP(MPIDI_OFI_SET_NUMBER != set_number,
-                            mpi_errno, MPI_ERR_OTHER, "**ofi_provider_mismatch");
-    }
-
-    /* Third, update global settings */
-    struct fi_info *prov_use = fi_dupinfo(prov);
-    MPIR_Assert(prov_use);
-    MPIDI_OFI_global.prov_use = prov_use;
-
-    /* stick to the original auto progress setting */
-    prov_use->domain_attr->data_progress = hints->domain_attr->data_progress;
-    prov_use->domain_attr->control_progress = hints->domain_attr->control_progress;
-
-    /* NOTE: MPIDI_OFI_global.settings are not used when !MPIDI_OFI_ENABLE_RUNTIME_CHECKS,
-     * but harmless to get updated together with rest of the global settings */
-    MPIDI_OFI_update_global_settings(prov_use);
+    MPIDI_OFI_update_global_settings(MPIDI_OFI_global.prov_use);
 
     if (MPIR_CVAR_CH4_OFI_CAPABILITY_SETS_DEBUG && MPIR_Process.rank == 0) {
         MPIDI_OFI_dump_global_settings();
     }
 
   fn_exit:
-    if (prov)
-        fi_freeinfo(prov);
-
-    /* prov_name is from MPL_strdup, can't let fi_freeinfo to free it */
-    MPL_free(hints->fabric_attr->prov_name);
-    hints->fabric_attr->prov_name = NULL;
-    fi_freeinfo(hints);
-
     return mpi_errno;
   fn_fail:
     goto fn_exit;
 }
 
-/* Pick best matching provider and populate the hints accordingly */
-static int find_provider(struct fi_info *hints)
+/* find best matching provider and set MPIDI_OFI_global.prov_use */
+static int find_provider(void)
 {
     int mpi_errno = MPI_SUCCESS;
 
     const char *provname = MPIR_CVAR_OFI_USE_PROVIDER;
     int ofi_version = MPIDI_OFI_get_fi_version();
 
+    int ret;                    /* return from fi_getinfo() */
+    struct fi_info *prov_list = NULL;   /* RUNTIME_CHECK first fi_getinfo() */
+    struct fi_info *prov = NULL;        /* final fi_getinfo() with hints */
+
+    struct fi_info *hints = fi_allocinfo();
+    MPIR_Assert(hints != NULL);
+
     if (MPIDI_OFI_ENABLE_RUNTIME_CHECKS) {
-        struct fi_info *prov_list, *prov_use;
         MPIDI_OFI_CALL(fi_getinfo(ofi_version, NULL, NULL, 0ULL, NULL, &prov_list), getinfo);
 
-        prov_use = pick_provider_from_list(provname, prov_list);
-
+        struct fi_info *prov_use = pick_provider_from_list(provname, prov_list);
         MPIR_ERR_CHKANDJUMP(prov_use == NULL, mpi_errno, MPI_ERR_OTHER, "**ofid_getinfo");
 
         /* OFI provider doesn't expose FI_DIRECTED_RECV by default for performance consideration.
@@ -99,7 +69,23 @@ static int find_provider(struct fi_info *hints)
         hints->caps = prov_use->caps;
         hints->addr_format = prov_use->addr_format;
 
-        fi_freeinfo(prov_list);
+        /* try with the initial optimal hints */
+        ret = fi_getinfo(ofi_version, NULL, NULL, 0ULL, hints, &prov);
+        if (ret || prov == NULL) {
+            /* relax msg_order */
+            hints->tx_attr->msg_order = prov_use->tx_attr->msg_order;
+            ret = fi_getinfo(ofi_version, NULL, NULL, 0ULL, hints, &prov);
+        }
+        if (ret || prov == NULL) {
+            /* some provider still expects FI_MR_BASIC */
+            if (prov_use->domain_attr->mr_mode & FI_MR_BASIC) {
+                hints->domain_attr->mr_mode = FI_MR_BASIC;
+            }
+            ret = fi_getinfo(ofi_version, NULL, NULL, 0ULL, hints, &prov);
+        }
+
+        MPIR_ERR_CHKANDJUMP(prov == NULL, mpi_errno, MPI_ERR_OTHER, "**ofid_getinfo");
+
     } else {
         /* Make sure that the user-specified provider matches the configure-specified provider. */
         MPIR_ERR_CHKANDJUMP(provname != NULL &&
@@ -108,8 +94,35 @@ static int find_provider(struct fi_info *hints)
         /* Initialize hints based on configure time macros) */
         MPIDI_OFI_init_hints(hints);
         hints->fabric_attr->prov_name = provname ? MPL_strdup(provname) : NULL;
+
+        ret = fi_getinfo(ofi_version, NULL, NULL, 0ULL, hints, &prov);
+        MPIR_ERR_CHKANDJUMP(prov == NULL, mpi_errno, MPI_ERR_OTHER, "**ofid_getinfo");
+
+        int set_number = MPIDI_OFI_get_set_number(prov->fabric_attr->prov_name);
+        MPIR_ERR_CHKANDJUMP(MPIDI_OFI_SET_NUMBER != set_number,
+                            mpi_errno, MPI_ERR_OTHER, "**ofi_provider_mismatch");
     }
+
+    MPIDI_OFI_global.prov_use = fi_dupinfo(prov);
+    MPIR_Assert(MPIDI_OFI_global.prov_use);
+
+    /* stick to the original auto progress setting */
+    MPIDI_OFI_global.prov_use->domain_attr->data_progress = hints->domain_attr->data_progress;
+    MPIDI_OFI_global.prov_use->domain_attr->control_progress = hints->domain_attr->control_progress;
+
   fn_exit:
+    if (prov_list) {
+        fi_freeinfo(prov_list);
+    }
+    if (prov) {
+        fi_freeinfo(prov);
+    }
+
+    /* prov_name is from MPL_strdup, can't let fi_freeinfo to free it */
+    MPL_free(hints->fabric_attr->prov_name);
+    hints->fabric_attr->prov_name = NULL;
+    fi_freeinfo(hints);
+
     return mpi_errno;
   fn_fail:
     goto fn_exit;
