5f09297b9603609d0926cea57ab2e01e4dbd6a06
Chongxiao Cao <chongxiao.cao@intel.com>
2020-07-23 14:20:05 -0600

    test/mpi: add ZE support for gpu test alloc and copy functions
    Level Zero resource is lazy initialized in first memory allocation call.
    Limitation: we can only use device 0 for Level Zero.

--- test/mpi/util/mtest_common.c
+++ test/mpi/util/mtest_common.c
@@ -194,11 +194,22 @@ int MTestIsBasicDtype(MPI_Datatype type)
 
 /* ------------------------------------------------------------------------ */
 /* Utilities to support device memory allocation */
+#if defined(HAVE_CUDA) || defined(HAVE_ZE)
+#include <assert.h>
 #ifdef HAVE_CUDA
 #include <cuda_runtime_api.h>
-#include <assert.h>
+#endif
 int ndevices = -1;
 int device_id = -1;
+#ifdef HAVE_ZE
+#include "level_zero/ze_api.h"
+ze_driver_handle_t driver = NULL;
+ze_device_handle_t *device = NULL;
+ze_result_t zerr = ZE_RESULT_SUCCESS;
+ze_command_list_handle_t command_list = NULL;
+ze_event_pool_handle_t event_pool = NULL;
+ze_event_handle_t event = NULL;
+#endif
 #endif
 
 /* allocates memory of specified type */
@@ -220,6 +231,89 @@ void MTestAlloc(size_t size, mtest_mem_type_e type, void **hostbuf, void **devic
     }
 #endif
 
+#ifdef HAVE_ZE
+    if (device_id == -1) {
+        /* Initilize ZE driver and device only at first call. */
+        device_id = 0;
+        zerr = zeInit(0);
+        assert(zerr == ZE_RESULT_SUCCESS);
+
+        /* Get driver for Intel GPUs by first discovering all the drivers,
+         * and then picks the first driver that supports GPU devices */
+        uint32_t num_drivers = 0;
+        zerr = zeDriverGet(&num_drivers, NULL);
+        assert(zerr == ZE_RESULT_SUCCESS);
+
+        ze_driver_handle_t *all_drivers =
+            (ze_driver_handle_t *) malloc(num_drivers * sizeof(ze_driver_handle_t));
+        zerr = zeDriverGet(&num_drivers, all_drivers);
+        assert(zerr == ZE_RESULT_SUCCESS);
+
+        /* Find a driver for GPU device, and get handles to each of the
+         * available devices */
+        for (int i = 0; i < num_drivers; i++) {
+            uint32_t num_devices = 0;
+            zerr = zeDeviceGet(all_drivers[i], &num_devices, NULL);
+            assert(zerr == ZE_RESULT_SUCCESS);
+
+            ze_device_handle_t *all_devices =
+                (ze_device_handle_t *) malloc(num_devices * sizeof(ze_device_handle_t));
+            zerr = zeDeviceGet(all_drivers[i], &num_devices, all_devices);
+            assert(zerr == ZE_RESULT_SUCCESS);
+
+            for (int j = 0; j < num_devices; j++) {
+                ze_device_properties_t device_properties;
+                zerr = zeDeviceGetProperties(all_devices[j], &device_properties);
+                assert(zerr == ZE_RESULT_SUCCESS);
+
+                if (ZE_DEVICE_TYPE_GPU == device_properties.type) {
+                    driver = all_drivers[i];
+                    ndevices = num_devices;
+                    device = all_devices;
+                    break;
+                }
+            }
+
+            if (NULL != driver) {
+                break;
+            } else {
+                free(all_devices);
+            }
+        }
+
+        free(all_drivers);
+
+        /* Create command list, command queue, event pool and event, for device 0 only. */
+        ze_command_queue_desc_t descriptor;
+        descriptor.version = ZE_COMMAND_QUEUE_DESC_VERSION_CURRENT;
+        descriptor.flags = 0;
+        descriptor.mode = ZE_COMMAND_QUEUE_MODE_ASYNCHRONOUS;
+        descriptor.priority = ZE_COMMAND_QUEUE_PRIORITY_NORMAL;
+        descriptor.ordinal = 0; /* computeQueueGroupOrdinal */
+
+        zerr = zeCommandListCreateImmediate(device[0], &descriptor, &command_list);
+        assert(zerr == ZE_RESULT_SUCCESS);
+
+        /* Create event pool and event */
+        ze_event_pool_desc_t pool_desc;
+        pool_desc.version = ZE_EVENT_POOL_DESC_VERSION_CURRENT;
+        pool_desc.flags = 0;
+        pool_desc.count = 1;
+
+        zerr = zeEventPoolCreate(driver, &pool_desc, 1, &(device[0]), &event_pool);
+        assert(zerr == ZE_RESULT_SUCCESS);
+
+        ze_event_desc_t event_desc = {
+            ZE_EVENT_DESC_VERSION_CURRENT,
+            0,
+            ZE_EVENT_SCOPE_FLAG_NONE,
+            ZE_EVENT_SCOPE_FLAG_HOST
+        };
+        zerr = zeEventCreate(event_pool, &event_desc, &event);
+        assert(zerr == ZE_RESULT_SUCCESS);
+    }
+#endif
+
     if (type == MTEST_MEM_TYPE__UNREGISTERED_HOST) {
         *devicebuf = malloc(size);
         if (hostbuf)
@@ -236,6 +330,46 @@ void MTestAlloc(size_t size, mtest_mem_type_e type, void **hostbuf, void **devic
             cudaMallocHost(hostbuf, size);
         device_id++;
         device_id %= ndevices;
+#endif
+
+#ifdef HAVE_ZE
+    } else if (type == MTEST_MEM_TYPE__REGISTERED_HOST) {
+        size_t mem_alignment;
+        ze_host_mem_alloc_desc_t host_desc;
+        host_desc.flags = ZE_HOST_MEM_ALLOC_FLAG_DEFAULT;
+        host_desc.version = ZE_HOST_MEM_ALLOC_DESC_VERSION_CURRENT;
+
+        /* Currently ZE ignores this augument and uses an internal alignment
+         * value. However, this behavior can change in the future. */
+        mem_alignment = 1;
+        zerr = zeDriverAllocHostMem(driver, &host_desc, size, mem_alignment, devicebuf);
+        assert(zerr == ZE_RESULT_SUCCESS);
+
+        if (hostbuf)
+            *hostbuf = *devicebuf;
+    } else if (type == MTEST_MEM_TYPE__DEVICE) {
+        size_t mem_alignment;
+        ze_device_mem_alloc_desc_t device_desc;
+        device_desc.flags = ZE_DEVICE_MEM_ALLOC_FLAG_DEFAULT;
+        device_desc.ordinal = 0;        /* We currently support a single memory type */
+        device_desc.version = ZE_DEVICE_MEM_ALLOC_DESC_VERSION_CURRENT;
+        /* Currently ZE ignores this augument and uses an internal alignment
+         * value. However, this behavior can change in the future. */
+        mem_alignment = 1;
+        zerr =
+            zeDriverAllocDeviceMem(driver, &device_desc, size, mem_alignment, device[device_id],
+                                   devicebuf);
+        assert(zerr == ZE_RESULT_SUCCESS);
+
+        if (hostbuf) {
+            ze_host_mem_alloc_desc_t host_desc;
+            host_desc.flags = ZE_HOST_MEM_ALLOC_FLAG_DEFAULT;
+            host_desc.version = ZE_HOST_MEM_ALLOC_DESC_VERSION_CURRENT;
+            zerr = zeDriverAllocHostMem(driver, &host_desc, size, mem_alignment, hostbuf);
+            assert(zerr == ZE_RESULT_SUCCESS);
+        }
+        /* FIXME: currently ZE only support device 0, so we cannot change device during test.
+         * Shifting device_id similar to CUDA should be added in future. */
 #endif
     } else {
         fprintf(stderr, "ERROR: unsupported memory type %d\n", type);
@@ -256,14 +390,36 @@ void MTestFree(mtest_mem_type_e type, void *hostbuf, void *devicebuf)
             cudaFreeHost(hostbuf);
         }
 #endif
+#ifdef HAVE_ZE
+    } else if (type == MTEST_MEM_TYPE__REGISTERED_HOST) {
+        zerr = zeDriverFreeMem(driver, devicebuf);
+        assert(zerr == ZE_RESULT_SUCCESS);
+    } else if (type == MTEST_MEM_TYPE__DEVICE) {
+        zerr = zeDriverFreeMem(driver, devicebuf);
+        assert(zerr == ZE_RESULT_SUCCESS);
+        if (hostbuf) {
+            zerr = zeDriverFreeMem(driver, hostbuf);
+            assert(zerr == ZE_RESULT_SUCCESS);
+        }
+#endif
     }
 }
 
 void MTestCopyContent(const void *sbuf, void *dbuf, size_t size, mtest_mem_type_e type)
 {
-#ifdef HAVE_CUDA
     if (type == MTEST_MEM_TYPE__DEVICE) {
+#ifdef HAVE_CUDA
         cudaMemcpy(dbuf, sbuf, size, cudaMemcpyDefault);
-    }
 #endif
+#ifdef HAVE_ZE
+        zerr = zeCommandListReset(command_list);
+        assert(zerr == ZE_RESULT_SUCCESS);
+        zerr = zeCommandListAppendMemoryCopy(command_list, dbuf, sbuf, size, NULL);
+        assert(zerr == ZE_RESULT_SUCCESS);
+        zerr = zeCommandListAppendSignalEvent(command_list, event);
+        assert(zerr == ZE_RESULT_SUCCESS);
+        zerr = zeEventHostSynchronize(event, UINT32_MAX);
+        assert(zerr == ZE_RESULT_SUCCESS);
+#endif
+    }
 }
