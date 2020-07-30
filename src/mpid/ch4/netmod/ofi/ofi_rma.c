cbede03f1ee06568243318ab236ebd2deef3b50c
Ken Raffenetti <raffenet@mcs.anl.gov>
2020-07-30 14:17:36 -0500

    ch4/ofi: Make pack buffer pool independent of active messages
    Pack buffers from this pool are utilized in RMA code. Rename the pool
    and associated CVARs to be independent of actives messages. Move
    init/finalize outside of AM conditionals.

--- src/mpid/ch4/netmod/ofi/ofi_rma.c
+++ src/mpid/ch4/netmod/ofi/ofi_rma.c
@@ -41,7 +41,7 @@ void MPIDI_OFI_complete_chunks(MPIDI_OFI_win_request_t * winreq)
         }
 
         MPIDI_OFI_pack_chunk *next = chunk->next;
-        MPIDU_genq_private_pool_free_cell(MPIDI_OFI_global.am_pack_buf_pool, chunk->pack_buffer);
+        MPIDU_genq_private_pool_free_cell(MPIDI_OFI_global.pack_buf_pool, chunk->pack_buffer);
         MPL_free(chunk);
         chunk = next;
     }
@@ -193,7 +193,7 @@ static int issue_packed_put(MPIR_Win * win, MPIDI_OFI_win_request_t * req)
     int j = req->noncontig.put.target.iov_cur;
     size_t msg_len;
     while (req->noncontig.put.origin.pack_offset < req->noncontig.put.origin.total_bytes) {
-        MPIDU_genq_private_pool_alloc_cell(MPIDI_OFI_global.am_pack_buf_pool, &pack_buffer);
+        MPIDU_genq_private_pool_alloc_cell(MPIDI_OFI_global.pack_buf_pool, &pack_buffer);
         if (pack_buffer == NULL)
             break;
 
@@ -280,7 +280,7 @@ static int issue_packed_get(MPIR_Win * win, MPIDI_OFI_win_request_t * req)
     int j = req->noncontig.get.target.iov_cur;
     size_t msg_len;
     while (req->noncontig.get.origin.pack_offset < req->noncontig.get.origin.total_bytes) {
-        MPIDU_genq_private_pool_alloc_cell(MPIDI_OFI_global.am_pack_buf_pool, &pack_buffer);
+        MPIDU_genq_private_pool_alloc_cell(MPIDI_OFI_global.pack_buf_pool, &pack_buffer);
         if (pack_buffer == NULL)
             break;
 
