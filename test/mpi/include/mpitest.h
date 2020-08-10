d0bbfe632215b150e8387350638e24ce8abe4531
Hui Zhou <hzhou321@anl.gov>
2020-08-10 13:48:23 -0500

    test: add parameter to radomize intercomm generation
    Raname the utility function name to MTestCommRandomize to reflect its
    generalize purpose of randomize context_id even for the case of
    intracomm.
    
    Automatically clean up comm radomization inside MTestFinalize.
    
    Add two more tests, icsend and icprobe with radomize argument so we have
    tests cover both with or without the randomization.

--- test/mpi/include/mpitest.h
+++ test/mpi/include/mpitest.h
@@ -32,8 +32,6 @@ void MTestGetDbgInfo(int *dbgflag, int *verbose);
 
 int MTestGetIntracomm(MPI_Comm *, int);
 int MTestGetIntracommGeneral(MPI_Comm *, int, int);
-void MTestGetIntercomm_start(void);
-void MTestGetIntercomm_finish(void);
 int MTestGetIntercomm(MPI_Comm *, int *, int);
 int MTestGetComm(MPI_Comm *, int);
 int MTestTestIntercomm(MPI_Comm intercomm);
@@ -42,6 +40,7 @@ int MTestTestComm(MPI_Comm comm);
 const char *MTestGetIntracommName(void);
 const char *MTestGetIntercommName(void);
 void MTestFreeComm(MPI_Comm *);
+void MTestCommRandomize(void);
 
 int MTestSpawnPossible(int *);
 
