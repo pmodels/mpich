pmodels/mpich

commit e3b828f55c01ae3cdd07074c4661a406e7e8ca27

ch4/posix: fix req_hdr size initialization

src/mpid/ch4/shm/posix/posix_am_impl.h
@@ -48,7 +48,7 @@ static inline int MPIDI_POSIX_am_init_req_hdr(const void *am_hdr,
         MPIR_ERR_CHKANDJUMP(!req_hdr, mpi_errno, MPI_ERR_OTHER, "**nomem");
 
         req_hdr->am_hdr = (void *) &req_hdr->am_hdr_buf[0];
-        req_hdr->am_hdr_sz = MPIDI_POSIX_MAX_AM_HDR_SIZE;
+        req_hdr->am_hdr_sz = am_hdr_sz;
 
         req_hdr->pack_buffer = NULL;
 

commit 8675e70659babb205f6519455fa980b9cd6e4bf6

ch4/posix: fix checking for am_hdr_sz
The posix req_hdr need to do a malloc if the am_hdr is larger than the
preallocated space from the pool. This checking was comparing against
the am_hdr_sz stored in the req_hdr which originally set to
MPIDI_POSIX_MAX_AM_HDR_SIZE.

A previous commit e3b828f55c01 changed the setting to the actual
am_hdr_sz which makes the following `am_hdr_sz > req_hdr->am_hdr_sz`
always false and cause faliure in RMA (which relies on large am_hdr_sz
handling in the isendv case). Also, in original code, the
req_hdr->am_hdr_sz was set to the actual am_hdr_sz later in this
function, so commit e3b828f55c01 is seemingly unnecessary.

This commit fixes the failure caused by e3b828f55c01 by fixing the
aforemention comparison to `am_hdr_sz > MPIDI_POSIX_MAX_AM_HDR_SIZE`. I
think it is better to keep e3b828f55c01 and apply this commit as it
results in reliable code---comparison will work without relying some
variable being set in a certain way.

src/mpid/ch4/shm/posix/posix_am_impl.h
@@ -51,24 +51,23 @@ static inline int MPIDI_POSIX_am_init_req_hdr(const void *am_hdr,
         req_hdr->am_hdr_sz = am_hdr_sz;
 
         req_hdr->pack_buffer = NULL;
-
     }
 
     /* If the header is larger than what we'd preallocated, get rid of the preallocated buffer and
      * create a new one of the correct size. */
-    if (am_hdr_sz > req_hdr->am_hdr_sz) {
+    if (am_hdr_sz > MPIDI_POSIX_MAX_AM_HDR_SIZE) {
         if (req_hdr->am_hdr != &req_hdr->am_hdr_buf[0])
             MPL_free(req_hdr->am_hdr);
 
         req_hdr->am_hdr = MPL_malloc(am_hdr_sz, MPL_MEM_SHM);
         MPIR_ERR_CHKANDJUMP(!(req_hdr->am_hdr), mpi_errno, MPI_ERR_OTHER, "**nomem");
+        req_hdr->am_hdr_sz = am_hdr_sz;
     }
 
     if (am_hdr) {
         MPIR_Typerep_copy(req_hdr->am_hdr, am_hdr, am_hdr_sz);
     }
 
-    req_hdr->am_hdr_sz = am_hdr_sz;
     *req_hdr_ptr = req_hdr;
 
   fn_exit: