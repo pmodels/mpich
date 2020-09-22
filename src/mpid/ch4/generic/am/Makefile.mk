14555709a7f8daa7f8a2df8a19e763e560245808
Yanfei Guo <yguo@anl.gov>
2020-09-22 11:12:57 -0500

    ch4/generic: add general request cache
    The request cache is needed for the case of PIPELINE recv.
    In PIPELINE send operation, the sender of only send the am_hdr on the
    first segment. Therefore, the receiver will need to save the rreq
    returned from the target callback for the subsequent segments. This
    request cache functionality provides a mapping between a globally unique
    key (for example, global rank of the src) and the rreq.

--- src/mpid/ch4/generic/am/Makefile.mk
+++ src/mpid/ch4/generic/am/Makefile.mk
@@ -9,6 +9,7 @@ noinst_HEADERS += src/mpid/ch4/generic/am/mpidig_am_send.h \
                   src/mpid/ch4/generic/am/mpidig_am_recv.h \
                   src/mpid/ch4/generic/am/mpidig_am_msg.h \
                   src/mpid/ch4/generic/am/mpidig_am_send_utils.h \
+                  src/mpid/ch4/generic/am/mpidig_am_req_cache.h \
                   src/mpid/ch4/generic/am/mpidig_am.h
 
 mpi_core_sources += src/mpid/ch4/generic/am/mpidig_am_globals.c \
