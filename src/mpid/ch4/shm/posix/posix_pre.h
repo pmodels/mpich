b4b65ec28b9d846ba8e0f2ec5679bbb702ecfc5f
Yanfei Guo <yguo@anl.gov>
2020-10-13 18:04:51 -0500

    ch4/posix: remove data_sz from message header
    The total message data size was also used to differentiate header-only,
    short, pipeline messages. Since data_sz is being removed, a am_type is
    added for differentiate them.

--- src/mpid/ch4/shm/posix/posix_pre.h
+++ src/mpid/ch4/shm/posix/posix_pre.h
@@ -14,7 +14,8 @@
 #define MPIDI_POSIX_AM_KIND_BITS  (1)   /* 0 or 1 */
 #define MPIDI_POSIX_AM_HANDLER_ID_BITS  (7)     /* up to 64 */
 #define MPIDI_POSIX_AM_HDR_SZ_BITS      (8)
-#define MPIDI_POSIX_AM_DATA_SZ_BITS     (48)
+#define MPIDI_POSIX_AM_TYPE_BITS     (8)
+#define MPIDI_POSIX_AM_UNUSED_BITS     (40)
 
 #define MPIDI_POSIX_AM_MSG_HEADER_SIZE  (sizeof(MPIDI_POSIX_am_header_t))
 #define MPIDI_POSIX_MAX_IOV_NUM         (3)     /* am_hdr, [padding], payload */
@@ -29,6 +30,12 @@ typedef enum {
     MPIDI_POSIX_EAGER_RECV_POSTED_HOOK_STATE_REGISTERED,
     MPIDI_POSIX_EAGER_RECV_POSTED_HOOK_STATE_FINALIZED
 } MPIDI_POSIX_EAGER_recv_posted_hook_state_t;
+
+typedef enum {
+    MPIDI_POSIX_AM_TYPE__HDR,
+    MPIDI_POSIX_AM_TYPE__SHORT,
+    MPIDI_POSIX_AM_TYPE__PIPELINE
+} MPIDI_POSIX_am_type_t;
 
 struct MPIR_Request;
 
@@ -61,7 +68,8 @@ typedef struct MPIDI_POSIX_am_header {
     MPIDI_POSIX_am_header_kind_t kind:MPIDI_POSIX_AM_KIND_BITS;
     uint32_t handler_id:MPIDI_POSIX_AM_HANDLER_ID_BITS;
     uint64_t am_hdr_sz:MPIDI_POSIX_AM_HDR_SZ_BITS;
-    uint64_t data_sz:MPIDI_POSIX_AM_DATA_SZ_BITS;
+    uint64_t am_type:MPIDI_POSIX_AM_TYPE_BITS;
+    uint64_t unused:MPIDI_POSIX_AM_UNUSED_BITS;
 } MPIDI_POSIX_am_header_t;
 
 typedef struct MPIDI_POSIX_am_request_header {
