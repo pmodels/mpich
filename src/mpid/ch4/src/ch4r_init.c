083b1f20d248f23f1c57e709f079e72303dcc049
Pavan Balaji <balaji@anl.gov>
2020-11-13 01:28:06 +0000

    ch4: cleanup MPID_Alloc_mem code
    The current MPID_Alloc_mem code is completely bogus.
    
    1. All allocations are immediately forwarded to the netmod.  Shmmods
    are completely ignored.
    
    2. If the user requests for bound HBM or DDR memory, this is
    completely ignored in the primary path.  Only if the netmod fallsback
    to the generic layer are these requests even considered.
    
    3. It maintains a linked list of memory allocations and always
    searches through them for frees.  While this function is not
    performance critical, maintaining an O(N) search-time data structure
    for this seems silly when one could have just as easily used a hashmap
    to make the lookup faster.
    
    4. The linked list insertions and deletions are not protected by the
    per-object lock.  This works fine for the global lock, but would be
    broken for per-object locks.

--- src/mpid/ch4/src/ch4r_init.c
+++ src/mpid/ch4/src/ch4r_init.c
@@ -88,72 +88,14 @@ int MPIDIG_destroy_comm(MPIR_Comm * comm)
     return mpi_errno;
 }
 
-/* Linked list internally used to keep track of
- * allocated memory for which memory binding is
- * requested by the user. */
-typedef struct mem_node {
-    void *ptr;
-    size_t size;
-    struct mem_node *next;
-} mem_node_t;
-
-static mem_node_t *mem_list_head = NULL;
-static mem_node_t *mem_list_tail = NULL;
-
 void *MPIDIG_mpi_alloc_mem(size_t size, MPIR_Info * info_ptr)
 {
     MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDIG_MPI_ALLOC_MEM);
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDIG_MPI_ALLOC_MEM);
     void *p;
-    MPIR_hwtopo_type_e mem_type = MPIR_HWTOPO_TYPE__DDR;
-    MPIR_hwtopo_gid_t mem_gid = MPIR_HWTOPO_GID_ROOT;
-    int flag = 0;
-    char hint_str[MPI_MAX_INFO_VAL + 1];
 
-    /* retrieve requested memory type for allocation */
-    if (info_ptr) {
-        MPIR_Info_get_impl(info_ptr, "mpich_buf_type", MPI_MAX_INFO_VAL, hint_str, &flag);
-    }
+    p = MPL_malloc(size, MPL_MEM_USER);
 
-    if (flag) {
-        if (!strcmp(hint_str, "ddr"))
-            mem_type = MPIR_HWTOPO_TYPE__DDR;
-        else if (!strcmp(hint_str, "hbm")) {
-            mem_type = MPIR_HWTOPO_TYPE__HBM;
-        } else {
-            mem_type = MPIR_HWTOPO_TYPE__DDR;
-        }
-        mem_gid = MPIR_hwtopo_get_obj_by_type(mem_type);
-    }
-
-    if (mem_gid != MPIR_HWTOPO_GID_ROOT) {
-        /* requested memory type is available in the system and process is bound
-         * to the corresponding device; allocate memory and bind it to device. */
-        p = MPL_mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0,
-                     MPL_MEM_USER);
-        MPIR_hwtopo_mem_bind(p, size, mem_gid);
-
-        /* keep track of bound memory for freeing it later */
-        mem_node_t *el = MPL_malloc(sizeof(*el), MPL_MEM_OTHER);
-        el->ptr = p;
-        el->size = size;
-        LL_APPEND(mem_list_head, mem_list_tail, el);
-    } else if (mem_type != MPIR_HWTOPO_TYPE__DDR) {
-        /* if mem_gid = MPIR_HWTOPO_GID_ROOT and mem_type is non-default (DDR)
-         * it can mean either that the requested memory type is not available
-         * in the system or the requested memory type is available but there
-         * are many devices of such type and the process requesting memory is
-         * not bound to any of them. Regardless the reason we do not fall back
-         * to the default allocation and return a NULL pointer to the upper layer
-         * instead. */
-        p = NULL;
-    } else {
-        /* if mem_gid = MPIR_HWTOPO_GID_ROOT and mem_type is default (DDR) it
-         * means that we cannot bind memory to a single device explicitly. In
-         * this case we still allocate memory and leave the binding to the OS
-         * (first touch policy in Linux). */
-        p = MPL_malloc(size, MPL_MEM_USER);
-    }
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDIG_MPI_ALLOC_MEM);
     return p;
 }
@@ -163,22 +105,9 @@ int MPIDIG_mpi_free_mem(void *ptr)
     int mpi_errno = MPI_SUCCESS;
     MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDIG_MPI_FREE_MEM);
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDIG_MPI_FREE_MEM);
-    mem_node_t *el = NULL;
 
-    /* scan memory list for allocations */
-    LL_FOREACH(mem_list_head, el) {
-        if (el->ptr == ptr) {
-            LL_DELETE(mem_list_head, mem_list_tail, el);
-            break;
-        }
-    }
+    MPL_free(ptr);
 
-    if (el) {
-        MPL_munmap(el->ptr, el->size, MPL_MEM_USER);
-        MPL_free(el);
-    } else {
-        MPL_free(ptr);
-    }
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDIG_MPI_FREE_MEM);
     return mpi_errno;
 }
