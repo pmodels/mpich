b2ac4a82ec48286b037f13658130ccabbbef7a51
Yanfei Guo <yguo@anl.gov>
2020-11-22 22:48:36 -0600

    ch4/shm: add interface for am_recv
    This interface is expected to be used by MPIDIG to trigger receive
    operation when a extend header is provided. For example, this header can
    contain handle for GPU IPC or XPMEM IPC.

--- src/mpid/ch4/shm/include/shm.h
+++ src/mpid/ch4/shm/include/shm.h
@@ -41,6 +41,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_am_isend_reply(MPIR_Context_id_t context_
                                                       size_t am_hdr_sz, const void *data,
                                                       MPI_Count count, MPI_Datatype datatype,
                                                       MPIR_Request * sreq) MPL_STATIC_INLINE_SUFFIX;
+MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_am_recv(MPIR_Request * rreq) MPL_STATIC_INLINE_SUFFIX;
 MPL_STATIC_INLINE_PREFIX size_t MPIDI_SHM_am_hdr_max_sz(void) MPL_STATIC_INLINE_SUFFIX;
 MPL_STATIC_INLINE_PREFIX size_t MPIDI_SHM_am_eager_limit(void) MPL_STATIC_INLINE_SUFFIX;
 MPL_STATIC_INLINE_PREFIX size_t MPIDI_SHM_am_eager_buf_limit(void) MPL_STATIC_INLINE_SUFFIX;
