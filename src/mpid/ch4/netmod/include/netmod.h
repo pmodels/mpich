5d6099356b028e87068c4f73ca61477a58970c90
Yanfei Guo <yguo@anl.gov>
2020-11-22 22:48:36 -0600

    ch4/netmod: add interface for am_recv
    This interface allows CH4 to explicitly trigger the netmod to do am recv
    operation. Current use case is to start the ZCOPY recv, but this could
    be expand to other recvs.

--- src/mpid/ch4/netmod/include/netmod.h
+++ src/mpid/ch4/netmod/include/netmod.h
@@ -48,6 +48,7 @@ typedef int (*MPIDI_NM_am_isend_reply_t) (MPIR_Context_id_t context_id, int src_
                                           int handler_id, const void *am_hdr, size_t am_hdr_sz,
                                           const void *data, MPI_Count count, MPI_Datatype datatype,
                                           MPIR_Request * sreq);
+typedef int (*MPIDI_NM_am_recv_t) (MPIR_Request * rreq);
 typedef size_t(*MPIDI_NM_am_hdr_max_sz_t) (void);
 typedef size_t(*MPIDI_NM_am_eager_limit_t) (void);
 typedef size_t(*MPIDI_NM_am_eager_buf_limit_t) (void);
@@ -399,6 +400,7 @@ typedef struct MPIDI_NM_funcs {
     MPIDI_NM_am_isendv_t am_isendv;
     MPIDI_NM_am_send_hdr_reply_t am_send_hdr_reply;
     MPIDI_NM_am_isend_reply_t am_isend_reply;
+    MPIDI_NM_am_recv_t am_recv;
     MPIDI_NM_am_hdr_max_sz_t am_hdr_max_sz;
     MPIDI_NM_am_eager_limit_t am_eager_limit;
     MPIDI_NM_am_eager_buf_limit_t am_eager_buf_limit;
@@ -547,6 +549,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_NM_am_isend_reply(MPIR_Context_id_t context_i
                                                      size_t am_hdr_sz, const void *data,
                                                      MPI_Count count, MPI_Datatype datatype,
                                                      MPIR_Request * sreq) MPL_STATIC_INLINE_SUFFIX;
+MPL_STATIC_INLINE_PREFIX int MPIDI_NM_am_recv(MPIR_Request * rreq) MPL_STATIC_INLINE_SUFFIX;
 MPL_STATIC_INLINE_PREFIX size_t MPIDI_NM_am_hdr_max_sz(void) MPL_STATIC_INLINE_SUFFIX;
 MPL_STATIC_INLINE_PREFIX size_t MPIDI_NM_am_eager_limit(void) MPL_STATIC_INLINE_SUFFIX;
 MPL_STATIC_INLINE_PREFIX size_t MPIDI_NM_am_eager_buf_limit(void) MPL_STATIC_INLINE_SUFFIX;
--- src/mpid/ch4/netmod/include/netmod_impl.h
+++ src/mpid/ch4/netmod/include/netmod_impl.h
@@ -92,6 +92,19 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_NM_am_isend_reply(MPIR_Context_id_t context_i
     return ret;
 }
 
+MPL_STATIC_INLINE_PREFIX int MPIDI_NM_am_recv(MPIR_Request * rreq)
+{
+    int ret;
+
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_AM_ZCOPY_START);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_AM_ZCOPY_START);
+
+    ret = MPIDI_NM_func->am_recv(rreq);
+
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_AM_ZCOPY_START);
+    return ret;
+}
+
 MPL_STATIC_INLINE_PREFIX size_t MPIDI_NM_am_hdr_max_sz(void)
 {
     int ret;
