6678d268eb3d34e5222c5ba0c538ca085f7e5a46
Hui Zhou <hzhou321@anl.gov>
2020-11-23 20:00:29 -0600

    autogen: autogen mpi c bindings
    Only the functions included in src/binding/c/*_api.txt will get
    generated.
    
    src/binding/c/Makefile.mk will be generated by maint/gen_binding_c.py.
    
    We also added a README to explain how the generation works.

--- src/binding/Makefile.mk
+++ src/binding/Makefile.mk
@@ -3,6 +3,7 @@
 ##     See COPYRIGHT in top-level directory
 ##
 
+include $(top_srcdir)/src/binding/c/Makefile.mk
 include $(top_srcdir)/src/binding/cxx/Makefile.mk
 include $(top_srcdir)/src/binding/fortran/mpif_h/Makefile.mk
 include $(top_srcdir)/src/binding/fortran/use_mpi/Makefile.mk
