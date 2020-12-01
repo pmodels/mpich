c5aa2739373440e70aaad8f391e53be122c1996e
Hui Zhou <hzhou321@anl.gov>
2020-12-01 09:10:39 -0600

    binding/c: update python scripts
    Skip MPIR_ERRTEST_INITIALIZED_ORDIE when skip initcheck is specified.
    
    Allow splitting manpage notes into two parts. Now in the config file we
    can list man page notes in multiple /* */ blocks. The first block goes
    first, the rest of the block will go after autogenerated notes. This
    allows better use of directives.
    
    Allow specify custom error_checking, handle_ptr, and early_return code.
    
    Add a directive "extra" to generate additional stuff. For example, extra
    man notes items such as collops, SignalSafe, NotThreadSafe, etc.
    Specifying SignalSafe or NotThreadSafe will automatically skip ThreadSafe in the
    man page notes.
    
    Add extra directive to generate extra code such as setup_intercomm_localcomm.
    
    Add skip directives to skip warnings for certain parameter validations.
    For example, ".skip: validate-COLOR, validate-KEY" skips validation for
    the color, key parameter for the specific function MPI_Comm_split.
    
    Generate functions in alphabetic order of dir. It allows easier time
    tracking progress and debug anomallies.
    
    Add .replace directives to generate simpler code that just call PMPI version
    of the replacement functions. It also adds the notes about deprecated or
    removed functions.
    
    Add ".impl: direct" directives to allow specifying the entire function
    body, including logging and validations with the body of routines. This
    is to allow simple handling of specific functions such as MPI_Aint_add
    by directly list the short code rather than complicating the rules in
    the scripts.
    
    Add ".impl: mpid" directives to allow body of routines to call MPID_
    routine instead of MPIR impl routine.
    
    Support polymorph functions, This is to support functions like
    MPI_Comm_set_attr where C and Fortran may call the function with different
    parameter type, thus called polymorphism. Essetially, just use an internal
    function with an extra parameter to distinguish the parameter types.
    
    Add function to generate body of routines for collectives. Collectives
    need check for CVARs to switch between direct MPID call or MPIR impl
    calls.
    
    Generate code to publish handle output for handle allocation functions.
    Multiple handles output (only the case of MPI_Comm_idup[_with_info])
    also work.
    
    Fix fn_fail handling for MPI_Initialized/Finalized.
    
    Fix up misc validation errors.
    
    Allow custom data types.
    
    Move the code that writes file to top level function.
    
    Move copyright code to __init__.py.
    
    Generate prototypes for mpir implement functions.
    
    Generate mpi errnames.

--- maint/gen_binding_c.py
+++ maint/gen_binding_c.py
@@ -5,28 +5,47 @@
 
 from local_python import MPI_API_Global as G
 from local_python.mpi_api import load_mpi_api
-from local_python.binding_c import dump_mpi_c, dump_Makefile_mk
+from local_python.binding_c import *
 from local_python import RE
-import os
 import glob
 
 def main():
+    # -- Loading APIs --
+    print("Loading maint/mpi_standard_api.txt ...")
     load_mpi_api("maint/mpi_standard_api.txt")
 
-    os.chdir("src/binding/c")
-    api_files = glob.glob("*_api.txt")
+    binding_dir = "src/binding/c"
+    api_files = glob.glob("%s/*_api.txt" % binding_dir)
     for f in api_files:
-        if RE.match(r'(\w+)_api.txt', f):
+        if RE.match(r'.*\/(\w+)_api.txt', f):
             # The name in eg pt2pt_api.txt indicates the output folder.
             # Only the api functions with output folder will get generated.
             # This allows simple control of what functions to generate.
+            print("Loading %s ..." % f)
             load_mpi_api(f, RE.m.group(1))
 
-    for func in G.FUNCS.values():
-        if 'dir' in func:
-            dump_mpi_c(func)
+    # -- Generating code --
+    func_list = [f for f in G.FUNCS.values() if 'dir' in f]
+    func_list.sort(key = lambda f: f['dir'])
+    for func in func_list:
+        if RE.search(r'not_implemented', func['attrs']):
+            print("  skip %s (not_implemented)" % func['name'])
+            pass
+        else:
+            G.out = []
+            G.err_codes = {}
+            mapping = G.MAPS['SMALL_C_KIND_MAP']
 
-    dump_Makefile_mk()
+            # dumps the code to G.out array
+            dump_mpi_c(func, mapping)
+
+            file_path = get_func_file_path(func, binding_dir)
+            dump_c_file(file_path, G.out)
+
+            # add to mpi_sources for dump_Makefile_mk()
+            G.mpi_sources.append(file_path)
+
+    dump_Makefile_mk("src/binding/c/Makefile.mk")
 
 # ---------------------------------------------------------
 if __name__ == "__main__":
