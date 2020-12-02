8a70da40c65bea5628126cd114484b1994782f52
Hui Zhou <hzhou321@anl.gov>
2020-12-02 09:11:20 -0600

    binding/c: autogen coll api
    Note that collectives will call MPIR_Xxx rather than calling
    MPIR_Xxx_impl. The prototypes for MPIR_Xxx_impl is still generated for
    collectives since MPIR_Xxx is just a wrapper function that will always
    call MPIR_Xxx_impl in some branch.

--- src/mpi/coll/Makefile.mk
+++ src/mpi/coll/Makefile.mk
@@ -62,53 +62,6 @@ include $(top_srcdir)/src/mpi/coll/algorithms/Makefile.mk
 
 AM_CPPFLAGS += -I$(top_srcdir)/src/mpi/coll/include/
 
-mpi_sources += \
-    src/mpi/coll/allgather.c \
-    src/mpi/coll/allgatherv.c \
-    src/mpi/coll/allreduce.c \
-    src/mpi/coll/alltoall.c \
-    src/mpi/coll/alltoallv.c \
-    src/mpi/coll/alltoallw.c \
-    src/mpi/coll/barrier.c \
-    src/mpi/coll/bcast.c \
-    src/mpi/coll/exscan.c \
-    src/mpi/coll/gather.c \
-    src/mpi/coll/gatherv.c \
-    src/mpi/coll/iallgather.c \
-    src/mpi/coll/iallgatherv.c \
-    src/mpi/coll/iallreduce.c \
-    src/mpi/coll/ialltoall.c \
-    src/mpi/coll/ialltoallv.c \
-    src/mpi/coll/ialltoallw.c \
-    src/mpi/coll/ibarrier.c \
-    src/mpi/coll/ibcast.c \
-    src/mpi/coll/iexscan.c \
-    src/mpi/coll/igather.c \
-    src/mpi/coll/igatherv.c \
-    src/mpi/coll/ineighbor_allgather.c \
-    src/mpi/coll/ineighbor_allgatherv.c \
-    src/mpi/coll/ineighbor_alltoall.c \
-    src/mpi/coll/ineighbor_alltoallv.c \
-    src/mpi/coll/ineighbor_alltoallw.c \
-    src/mpi/coll/ireduce.c \
-    src/mpi/coll/ireduce_scatter.c \
-    src/mpi/coll/ireduce_scatter_block.c \
-    src/mpi/coll/iscan.c \
-    src/mpi/coll/iscatter.c \
-    src/mpi/coll/iscatterv.c \
-    src/mpi/coll/neighbor_allgather.c \
-    src/mpi/coll/neighbor_allgatherv.c \
-    src/mpi/coll/neighbor_alltoall.c \
-    src/mpi/coll/neighbor_alltoallv.c \
-    src/mpi/coll/neighbor_alltoallw.c \
-    src/mpi/coll/reduce.c \
-    src/mpi/coll/reduce_local.c \
-    src/mpi/coll/reduce_scatter.c \
-    src/mpi/coll/reduce_scatter_block.c \
-    src/mpi/coll/scan.c \
-    src/mpi/coll/scatter.c \
-    src/mpi/coll/scatterv.c
-
 mpi_core_sources += \
     src/mpi/coll/helper_fns.c     \
     src/mpi/coll/nbcutil.c
