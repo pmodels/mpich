6697b9c99f0e902f372f3bdbc2818b181f46fa1a
Hui Zhou <hzhou321@anl.gov>
2020-12-01 22:48:02 -0600

    binding/c: autogen init api

--- src/mpi/init/Makefile.mk
+++ src/mpi/init/Makefile.mk
@@ -3,16 +3,6 @@
 ##     See COPYRIGHT in top-level directory
 ##
 
-mpi_sources +=                 \
-    src/mpi/init/abort.c       \
-    src/mpi/init/init.c        \
-    src/mpi/init/initialized.c \
-    src/mpi/init/initthread.c  \
-    src/mpi/init/ismain.c      \
-    src/mpi/init/finalize.c    \
-    src/mpi/init/finalized.c   \
-    src/mpi/init/querythread.c
-
 mpi_core_sources += \
     src/mpi/init/init_impl.c      \
     src/mpi/init/mpir_init.c      \
