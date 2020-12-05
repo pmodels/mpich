3aaed70b514987127e3a9c77a0389962a9a209ad
Hui Zhou <hzhou321@anl.gov>
2020-12-04 18:04:01 -0600

    binding/c: autogen group api

--- src/mpi/group/Makefile.mk
+++ src/mpi/group/Makefile.mk
@@ -3,20 +3,6 @@
 ##     See COPYRIGHT in top-level directory
 ##
 
-mpi_sources +=                            \
-    src/mpi/group/group_compare.c         \
-    src/mpi/group/group_difference.c      \
-    src/mpi/group/group_excl.c            \
-    src/mpi/group/group_free.c            \
-    src/mpi/group/group_incl.c            \
-    src/mpi/group/group_intersection.c    \
-    src/mpi/group/group_range_excl.c      \
-    src/mpi/group/group_range_incl.c      \
-    src/mpi/group/group_rank.c            \
-    src/mpi/group/group_size.c            \
-    src/mpi/group/group_translate_ranks.c \
-    src/mpi/group/group_union.c
-
 mpi_core_sources += \
     src/mpi/group/group_impl.c \
     src/mpi/group/grouputil.c
