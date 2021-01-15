7f2e85e6df4ee0eb82904fdf2241f5f322ceb103
Hui Zhou <hzhou321@anl.gov>
2021-01-14 22:10:37 -0600

    typerep/yaksa: always pass info with yaksa pack/unpack
    Since we already queried pointer device attribute, we should always pass
    the info hint to yaksa to avoid additional pointer query inside yaksa.
    
    The code is locally restructured to always get info hints from
    attributes.

--- src/mpi/datatype/typerep/src/typerep_yaksa_pack.c
+++ src/mpi/datatype/typerep/src/typerep_yaksa_pack.c
@@ -31,12 +31,17 @@ int MPIR_Typerep_copy(void *outbuf, const void *inbuf, MPI_Aint num_bytes)
     } else {
         yaksa_request_t request;
         uintptr_t actual_pack_bytes;
+
+        yaksa_info_t info = MPII_yaksa_get_info(&inattr, &outattr);
         rc = yaksa_ipack(inbuf, num_bytes, YAKSA_TYPE__BYTE, 0, outbuf, num_bytes,
-                         &actual_pack_bytes, NULL, YAKSA_OP__REPLACE, &request);
+                         &actual_pack_bytes, info, YAKSA_OP__REPLACE, &request);
         MPIR_ERR_CHKANDJUMP(rc, mpi_errno, MPI_ERR_INTERN, "**yaksa");
         MPIR_Assert(actual_pack_bytes == num_bytes);
 
         rc = yaksa_request_wait(request);
+        MPIR_ERR_CHKANDJUMP(rc, mpi_errno, MPI_ERR_INTERN, "**yaksa");
+
+        rc = MPII_yaksa_free_info(info);
         MPIR_ERR_CHKANDJUMP(rc, mpi_errno, MPI_ERR_INTERN, "**yaksa");
     }
 
@@ -128,21 +133,48 @@ int MPIR_Typerep_pack(const void *inbuf, MPI_Aint incount, MPI_Datatype datatype
 
     MPIR_Assert(datatype != MPI_DATATYPE_NULL);
 
-    /* fast-path for contig CPU buffers */
-    bool fast = fastpath_memcpy(inbuf, outbuf, datatype, incount, inoffset, max_pack_bytes,
-                                actual_pack_bytes, MEMCPY_DIR__PACK);
-    if (fast)
+    int is_contig = 0;
+    const void *inbuf_ptr;      /* adjusted by true_lb */
+    MPI_Aint total_size = 0;
+    if (HANDLE_IS_BUILTIN(datatype)) {
+        is_contig = 1;
+        inbuf_ptr = inbuf;
+        total_size = incount * MPIR_Datatype_get_basic_size(datatype);
+    } else {
+        MPIR_Datatype *dtp;
+        MPIR_Datatype_get_ptr(datatype, dtp);
+        is_contig = dtp->is_contig;
+        inbuf_ptr = (const char *) inbuf + dtp->true_lb;
+        total_size = incount * dtp->size;
+    }
+
+    MPL_pointer_attr_t inattr, outattr;
+    MPIR_GPU_query_pointer_attr(inbuf_ptr, &inattr);
+    MPIR_GPU_query_pointer_attr(outbuf, &outattr);
+
+    if (is_contig &&
+        (inattr.type == MPL_GPU_POINTER_UNREGISTERED_HOST ||
+         inattr.type == MPL_GPU_POINTER_REGISTERED_HOST) &&
+        (outattr.type == MPL_GPU_POINTER_UNREGISTERED_HOST ||
+         outattr.type == MPL_GPU_POINTER_REGISTERED_HOST)) {
+        *actual_pack_bytes = MPL_MIN(total_size - inoffset, max_pack_bytes);
+        MPIR_Memcpy(outbuf, (const char *) inbuf_ptr + inoffset, *actual_pack_bytes);
         goto fn_exit;
+    }
 
     yaksa_type_t type = MPII_Typerep_get_yaksa_type(datatype);
+    yaksa_info_t info = MPII_yaksa_get_info(&inattr, &outattr);
 
     yaksa_request_t request;
     uintptr_t real_pack_bytes;
     rc = yaksa_ipack(inbuf, incount, type, inoffset, outbuf, max_pack_bytes, &real_pack_bytes,
-                     NULL, YAKSA_OP__REPLACE, &request);
+                     info, YAKSA_OP__REPLACE, &request);
     MPIR_ERR_CHKANDJUMP(rc, mpi_errno, MPI_ERR_INTERN, "**yaksa");
 
     rc = yaksa_request_wait(request);
+    MPIR_ERR_CHKANDJUMP(rc, mpi_errno, MPI_ERR_INTERN, "**yaksa");
+
+    rc = MPII_yaksa_free_info(info);
     MPIR_ERR_CHKANDJUMP(rc, mpi_errno, MPI_ERR_INTERN, "**yaksa");
 
     *actual_pack_bytes = (MPI_Aint) real_pack_bytes;
@@ -170,27 +202,50 @@ int MPIR_Typerep_unpack(const void *inbuf, MPI_Aint insize, void *outbuf, MPI_Ai
 
     MPIR_Assert(datatype != MPI_DATATYPE_NULL);
 
-    /* fast-path for contig CPU buffers */
-    bool fast = fastpath_memcpy(inbuf, outbuf, datatype, outcount, outoffset, insize,
-                                actual_unpack_bytes, MEMCPY_DIR__UNPACK);
-    if (fast)
+    int is_contig = 0;
+    const void *outbuf_ptr;     /* adjusted by true_lb */
+    MPI_Aint total_size = 0;
+    if (HANDLE_IS_BUILTIN(datatype)) {
+        is_contig = 1;
+        outbuf_ptr = outbuf;
+        total_size = outcount * MPIR_Datatype_get_basic_size(datatype);
+    } else {
+        MPIR_Datatype *dtp;
+        MPIR_Datatype_get_ptr(datatype, dtp);
+        is_contig = dtp->is_contig;
+        outbuf_ptr = (char *) outbuf + dtp->true_lb;
+        total_size = outcount * dtp->size;
+    }
+
+    MPL_pointer_attr_t inattr, outattr;
+    MPIR_GPU_query_pointer_attr(inbuf, &inattr);
+    MPIR_GPU_query_pointer_attr(outbuf_ptr, &outattr);
+
+    if (is_contig &&
+        (inattr.type == MPL_GPU_POINTER_UNREGISTERED_HOST ||
+         inattr.type == MPL_GPU_POINTER_REGISTERED_HOST) &&
+        (outattr.type == MPL_GPU_POINTER_UNREGISTERED_HOST ||
+         outattr.type == MPL_GPU_POINTER_REGISTERED_HOST)) {
+        *actual_unpack_bytes = MPL_MIN(total_size - outoffset, insize);
+        MPIR_Memcpy((char *) outbuf_ptr + outoffset, inbuf, *actual_unpack_bytes);
         goto fn_exit;
+    }
 
     yaksa_type_t type = MPII_Typerep_get_yaksa_type(datatype);
+    yaksa_info_t info = MPII_yaksa_get_info(&inattr, &outattr);
 
-    uintptr_t size;
-    rc = yaksa_type_get_size(type, &size);
-    MPIR_ERR_CHKANDJUMP(rc, mpi_errno, MPI_ERR_INTERN, "**yaksa");
-
-    uintptr_t real_insize = MPL_MIN(insize, size * outcount);
+    uintptr_t real_insize = MPL_MIN(total_size - outoffset, insize);
 
     yaksa_request_t request;
     uintptr_t real_unpack_bytes;
     rc = yaksa_iunpack(inbuf, real_insize, outbuf, outcount, type, outoffset, &real_unpack_bytes,
-                       NULL, YAKSA_OP__REPLACE, &request);
+                       info, YAKSA_OP__REPLACE, &request);
     MPIR_ERR_CHKANDJUMP(rc, mpi_errno, MPI_ERR_INTERN, "**yaksa");
 
     rc = yaksa_request_wait(request);
+    MPIR_ERR_CHKANDJUMP(rc, mpi_errno, MPI_ERR_INTERN, "**yaksa");
+
+    rc = MPII_yaksa_free_info(info);
     MPIR_ERR_CHKANDJUMP(rc, mpi_errno, MPI_ERR_INTERN, "**yaksa");
 
     *actual_unpack_bytes = (MPI_Aint) real_unpack_bytes;
