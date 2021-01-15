ad89a55463788f6a4ec47c504a12dc9b16f7efdc
Hui Zhou <hzhou321@anl.gov>
2021-01-14 21:38:35 -0600

    mpl/gpu: add device attr inside MPL_pointer_attr_t
    To avoid multiple query of device pointer attribute, for example,
    between mpich and yaksa, we need cache or pass the queried attribute in
    hash or info hint. We need have the attr inside the MPL_pointer_attr_t
    to be able to do that.

--- src/mpl/include/mpl_gpu_fallback.h
+++ src/mpl/include/mpl_gpu_fallback.h
@@ -8,6 +8,7 @@
 
 typedef int MPL_gpu_ipc_mem_handle_t;
 typedef int MPL_gpu_device_handle_t;
+typedef int MPL_gpu_device_attr;        /* dummy type */
 #define MPL_GPU_DEVICE_INVALID -1
 
 #endif /* ifndef MPL_GPU_CUDA_H_INCLUDED */
