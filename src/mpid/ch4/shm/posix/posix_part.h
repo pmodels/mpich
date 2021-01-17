b966cce9b99fd65267b625e6645f49822b9ac560
Min Si <msi@anl.gov>
2021-01-17 14:53:58 -0600

    ch4/posix: add partitioned pt2pt
    All functions fallback to ch4 generic routine.

---
+++ src/mpid/ch4/shm/posix/posix_part.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) by Argonne National Laboratory
+ *     See COPYRIGHT in top-level directory
+ */
+
+#ifndef POSIX_PART_H_INCLUDED
+#define POSIX_PART_H_INCLUDED
+
+#include "posix_impl.h"
+
+MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_part_start(MPIR_Request * request)
+{
+    return MPIDIG_part_start(request, 1 /*is_local */);
+}
+
+MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_pready_range(int partition_low,
+                                                          int partition_high,
+                                                          MPIR_Request * request)
+{
+    return MPIDIG_mpi_pready_range(partition_low, partition_high, request, 1 /*is_local */);
+}
+
+MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_pready_list(int length, int array_of_partitions[],
+                                                         MPIR_Request * request)
+{
+    return MPIDIG_mpi_pready_list(length, array_of_partitions, request, 1 /*is_local */);
+}
+
+MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_parrived(MPIR_Request * request,
+                                                      int partition, int *flag)
+{
+    return MPIDIG_mpi_parrived(request, partition, flag);
+}
+
+#endif /* POSIX_PART_H_INCLUDED */
