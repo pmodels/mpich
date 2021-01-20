b2b0055345ce1ae34c9c4006031a31580f0fa966
Hui Zhou <hzhou321@anl.gov>
2021-01-20 09:01:18 -0600

    mpi/init: move MPIR_Init_thread and MPIR_Finalize together
    Init and Finalize often need matching code, moving the code into the
    same unit makes checking easier. Another motivation is both need manage
    init state, init counter and use shared init lock. Having them together
    allows use of static variables rather than maintaining these init-only
    variables in a global scope.

--- src/mpi/init/Makefile.mk
+++ src/mpi/init/Makefile.mk
@@ -7,7 +7,6 @@ mpi_core_sources += \
     src/mpi/init/init_impl.c      \
     src/mpi/init/init_util.c      \
     src/mpi/init/mpir_init.c      \
-    src/mpi/init/mpir_finalize.c  \
     src/mpi/init/globals.c        \
     src/mpi/init/initinfo.c       \
     src/mpi/init/local_proc_attrs.c \
