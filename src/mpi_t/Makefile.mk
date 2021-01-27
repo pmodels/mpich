a74d69ad846993ac6533b06c3229b8eef141a7e2
Wesley Bland <wesley.bland@intel.com>
2021-01-27 09:21:56 -0600

    qmpi: Add QMPI Support

--- src/mpi_t/Makefile.mk
+++ src/mpi_t/Makefile.mk
@@ -3,10 +3,13 @@
 ##     See COPYRIGHT in top-level directory
 ##
 
+AM_CPPFLAGS += -I$(top_srcdir)/src/mpi_t
+
 mpi_core_sources += \
         src/mpi_t/mpit_finalize.c       \
         src/mpi_t/mpit_initthread.c     \
         src/mpi_t/mpit_impl.c \
         src/mpi_t/pvar_impl.c \
         src/mpi_t/mpit.c \
-        src/mpi_t/events_impl.c
+        src/mpi_t/events_impl.c \
+        src/mpi_t/qmpi_register.c
