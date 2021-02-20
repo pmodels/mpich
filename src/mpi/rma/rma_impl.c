26bc35a7ed863c872e91c838e554b91e995c81e9
Hui Zhou <hzhou321@anl.gov>
2021-02-20 09:18:22 -0600

    rma: wrap MPIR_Win_shared_query_impl
    Now that internally we use MPI_Aint for output parameter disp_unit, it
    is easier to handle with an impl wrapper so the scripts and
    automantically handle pointer conversions.

--- src/mpi/rma/rma_impl.c
+++ src/mpi/rma/rma_impl.c
@@ -19,3 +19,9 @@ int MPIR_Win_set_name_impl(MPIR_Win * win_ptr, const char *win_name)
 
     return MPI_SUCCESS;
 }
+
+int MPIR_Win_shared_query_impl(MPIR_Win * win_ptr, int rank, MPI_Aint * size, MPI_Aint * disp_unit,
+                               void *baseptr)
+{
+    return MPID_Win_shared_query(win_ptr, rank, size, disp_unit, baseptr);
+}
