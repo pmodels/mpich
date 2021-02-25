ed58065b58b90438ad12b100f7c6189827993ae8
Jayyee-HPC <jie.yang@beboplogin4.lcrc.anl.gov>
2021-02-25 15:12:38 -0600

    Using uthash to compute the init hash values

--- src/mpi/coll/coll_length_check.c
+++ src/mpi/coll/coll_length_check.c
@@ -5,49 +5,47 @@
 
 #include "mpiimpl.h"
 
-#define MPI_DATATYPE_HASH_UNDEFINED (0b10101010<<8) + 0b00000000
+#define MPI_DATATYPE_HASH_UNDEFINED 0xFFFFFFFF
+#define CHAR_BIT 8
 
-typedef struct type_hash {
-    MPI_Datatype type;
-    const unsigned int hash_value;     /* used in hash generator */
-} type_hash;
+typedef struct Type_Sig {
+    unsigned long num_types;
+    unsigned long hash_value;   /* used in hash generator */
+} Type_Sig;
 
-/* -- Define initial hash values for MPI basic datatypes */
-static type_hash mpi_type_hashs[] = {
-    {MPI_CHAR, (0b10101010<<8) + 0b00000001},
-    {MPI_SHORT, (0b10101010<<8) + 0b00000010},
-    {MPI_INT, (0b10101010<<8) + 0b00000011},
-    {MPI_LONG, (0b10101010<<8) + 0b00000100},
-    {MPI_LONG_LONG, (0b10101010<<8) + 0b00000101},
-    {MPI_SIGNED_CHAR, (0b10101010<<8) + 0b00000110},
-    {MPI_UNSIGNED_CHAR, (0b10101010<<8) + 0b00000111},
-    {MPI_UNSIGNED_SHORT, (0b10101010<<8) + 0b00001000},
-    {MPI_UNSIGNED, (0b10101010<<8) + 0b00001001},
-    {MPI_UNSIGNED_LONG, (0b10101010<<8) + 0b00001010},
-    {MPI_UNSIGNED_LONG_LONG, (0b10101010<<8) + 0b00001011},
-    {MPI_FLOAT, (0b10101010<<8) + 0b00001100},
-    {MPI_DOUBLE, (0b10101010<<8) + 0b00001101},
-    {MPI_LONG_DOUBLE, (0b10101010<<8) + 0b00001110},
-    {MPI_COMPLEX, (0b10101010<<8) + 0b00001111},
-    {MPI_DOUBLE_COMPLEX, (0b10101010<<8) + 0b00010000},
-    {MPI_LONG_DOUBLE_COMPLEX    , (0b10101010<<8) + 0b00010001},
-    {MPI_WCHAR, (0b10101010<<8) + 0b00010010},
-    {MPI_BYTE, (0b10101010<<8) + 0b00010011},
-    {MPI_PACKED, (0b10101010<<8) + 0b00100001}
-    //TODO fortran datatypes   
-};
-
-
-//Get the initial hash value for a MPI_Datatype
-unsigned int MPIR_Datatype_Hash_builtin_search_by_datatype(MPI_Datatype datatype)
+//Example 1001 << 1 -> 0011
+unsigned long MPIR_Circular_Left_Shift(unsigned long val, unsigned long nums_left_shift)
 {
-    int i;
-    unsigned int  hash_value_to_return= MPI_DATATYPE_HASH_UNDEFINED;
-    for (i = 0; i < sizeof(mpi_type_hashs) / sizeof(type_hash); ++i) {
-        if (mpi_type_hashs[i].type == datatype){
-            hash_value_to_return = mpi_type_hashs[i].hash_value;
-            break;
-        }
-    }
-    return hash_value_to_return;
+    unsigned long return_val;
+
+    return_val = (val << nums_left_shift) | 
+    		(val >> (sizeof(val)*CHAR_BIT - nums_left_shift));
+
+    return return_val;
+}
+
+//(a, n) op (b,m) = (a XOR (B Circular_Left_Shift n), n+m)
+Type_Sig MPIR_Datatype_Sig_Hash_Generator(Type_Sig  type_sig_a, Type_Sig type_sig_b)
+{
+    Type_Sig  type_sig_to_return;
+    unsigned long temp_hash_value;
+
+    type_sig_to_return.num_types = type_sig_a.num_types + type_sig_b.num_types;
+
+    temp_hash_value = MPIR_Circular_Left_Shift(type_sig_b.hash_value, type_sig_a.num_types);
+
+    type_sig_to_return.hash_value = type_sig_b.hash_value ^ temp_hash_value;
+
+    return type_sig_to_return;
+}
+
+//Generate the hash info for basic MPI_Datatypes
+Type_Sig MPIR_Datatype_Init_Hash_Generator(MPI_Datatype type)
+{
+	Type_Sig temp_hash;
+	temp_hash.num_types = 1;
+
+	HASH_VALUE(&type, sizeof(MPI_Datatype), temp_hash.hash_value);
+	
+	return temp_hash;
 }
