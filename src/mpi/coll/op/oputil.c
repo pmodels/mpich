f66856d4af53c9a25f08745ad6af1ccb98a02b48
Jayyee-HPC <jie.yang@beboplogin3.lcrc.anl.gov>
2021-03-05 11:06:00 -0600

    Move MPIR_Op_table to oputil.c from allreduce/allreduce.c

--- src/mpi/coll/op/oputil.c
+++ src/mpi/coll/op/oputil.c
@@ -24,7 +24,33 @@ static op_name_t mpi_ops[] = {
     {MPI_MINLOC, "minloc"},
     {MPI_MAXLOC, "maxloc"},
     {MPI_REPLACE, "replace"},
-    {MPI_NO_OP, "no_op"}
+    {MPI_NO_OP, "no_op"},
+    {MPIX_EQUAL, "equal"}
+};
+
+
+/* The order of entries in this table must match the definitions in
+   mpi.h.in */
+MPI_User_function *MPIR_Op_table[] = {
+    NULL, MPIR_MAXF,
+    MPIR_MINF, MPIR_SUM,
+    MPIR_PROD, MPIR_LAND,
+    MPIR_BAND, MPIR_LOR, MPIR_BOR,
+    MPIR_LXOR, MPIR_BXOR,
+    MPIR_MINLOC, MPIR_MAXLOC,
+    MPIR_REPLACE, MPIR_NO_OP,
+    MPIR_EQUAL
+};
+
+MPIR_Op_check_dtype_fn *MPIR_Op_check_dtype_table[] = {
+    NULL, MPIR_MAXF_check_dtype,
+    MPIR_MINF_check_dtype, MPIR_SUM_check_dtype,
+    MPIR_PROD_check_dtype, MPIR_LAND_check_dtype,
+    MPIR_BAND_check_dtype, MPIR_LOR_check_dtype, MPIR_BOR_check_dtype,
+    MPIR_LXOR_check_dtype, MPIR_BXOR_check_dtype,
+    MPIR_MINLOC_check_dtype, MPIR_MAXLOC_check_dtype,
+    MPIR_REPLACE_check_dtype, MPIR_NO_OP_check_dtype,
+    MPIR_EQUAL_check_dtype
 };
 
 MPI_Datatype MPIR_Op_builtin_search_by_shortname(const char *short_name)
