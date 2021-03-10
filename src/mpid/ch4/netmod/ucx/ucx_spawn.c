5d81ffb81d58a3613524734d89e5ca8c0f1df2d3
Hui Zhou <hzhou321@anl.gov>
2021-03-10 11:19:45 -0600

    ch4/ucx: return error for non-supported functions
    Ucx netmod currently does not support MPI_Comm_connect and
    MPI_Comm_accept. Return error rather success and fail later
    mysteriously.

--- src/mpid/ch4/netmod/ucx/ucx_spawn.c
+++ src/mpid/ch4/netmod/ucx/ucx_spawn.c
@@ -9,7 +9,9 @@
 int MPIDI_UCX_mpi_open_port(MPIR_Info * info_ptr, char *port_name)
 {
     int mpi_errno = MPI_SUCCESS;
-    port_name[0] = '\0';
+
+    MPIR_ERR_SET(mpi_errno, MPI_ERR_OTHER, "**ucx_nm_notsupported");
+
   fn_exit:
     return mpi_errno;
   fn_fail:
@@ -20,6 +22,9 @@ int MPIDI_UCX_mpi_open_port(MPIR_Info * info_ptr, char *port_name)
 int MPIDI_UCX_mpi_close_port(const char *port_name)
 {
     int mpi_errno = MPI_SUCCESS;
+
+    MPIR_ERR_SET(mpi_errno, MPI_ERR_OTHER, "**ucx_nm_notsupported");
+
   fn_exit:
     return mpi_errno;
   fn_fail:
@@ -31,6 +36,8 @@ int MPIDI_UCX_mpi_comm_connect(const char *port_name, MPIR_Info * info, int root
                                MPIR_Comm * comm_ptr, MPIR_Comm ** newcomm)
 {
     int mpi_errno = MPI_SUCCESS;
+
+    MPIR_ERR_SET(mpi_errno, MPI_ERR_OTHER, "**ucx_nm_notsupported");
 
   fn_exit:
     return mpi_errno;
@@ -63,13 +70,9 @@ int MPIDI_UCX_mpi_comm_accept(const char *port_name, MPIR_Info * info, int root,
 {
     int mpi_errno = MPI_SUCCESS;
 
-    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_UCX_MPI_COMM_ACCEPT);
-    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_UCX_MPI_COMM_ACCEPT);
-
-
+    MPIR_ERR_SET(mpi_errno, MPI_ERR_OTHER, "**ucx_nm_notsupported");
 
   fn_exit:
-    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_UCX_MPI_COMM_ACCEPT);
     return mpi_errno;
   fn_fail:
     goto fn_exit;
