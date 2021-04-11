df64741a8c40ddf6777030aba507ef0daa20dcd0
Hui Zhou <hzhou321@anl.gov>
2021-04-10 19:38:01 -0500

    spell: use "BigSur" to avoid spellcheck
    The Sur in "MacOs Big Sur" trips spell checker. Rather than add
    exception, which will prevent legitimate spell errors, use BigSur
    instead.

--- autogen.sh
+++ autogen.sh
@@ -1033,7 +1033,7 @@ if [ "$do_build_configure" = "yes" ] ; then
                 else
                     echo "failed"
                 fi
-                echo_n "Patching libtool.m4 for compatibility macOS Big Sur..."
+                echo_n "Patching libtool.m4 for compatibility macOS BigSur..."
                 patch -N -s -l $amdir/confdb/libtool.m4 maint/patches/optional/confdb/big-sur.patch
                 if [ $? -eq 0 ] ; then
                     macos_patch_requires_rebuild=yes
