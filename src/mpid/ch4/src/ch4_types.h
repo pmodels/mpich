222c60ca9c32bff7f805bb8d54786be15d0e1cc9
Shintaro Iwasaki <siwasaki@anl.gov>
2021-04-19 08:13:28 -0500

    progress: move progress_count to MPIDI_vci_t
    Accessing MPIDI_global.progress_counts[vci] in parallel causes cache
    line conflicts.  This patch fixes this issue by declaring
    progress_count in MPIDI_vci_t.

--- src/mpid/ch4/src/ch4_types.h
+++ src/mpid/ch4/src/ch4_types.h
@@ -242,6 +242,11 @@ typedef union MPIDI_vci {
     struct {
         int attr;
         MPID_Thread_mutex_t lock;
+        /* The progress counts are mostly accessed in a VCI critical section and thus updated in a
+         * relaxed manner.  MPL_atomic_int_t is used here only for MPIDI_set_progress_vci() and
+         * MPIDI_set_progress_vci_n(), which access these progress counts outside a VCI critical
+         * section. */
+        MPL_atomic_int_t progress_count;
     } vci;
     char pad[MPL_CACHELINE_SIZE];
 } MPIDI_vci_t;
@@ -282,11 +287,6 @@ typedef struct MPIDI_CH4_Global_t {
 
     int n_vcis;
     MPIDI_vci_t vci[MPIDI_CH4_MAX_VCIS];
-    /* The progress counts are mostly accessed in a VCI critical section and thus updated in a
-     * relaxed manner.  MPL_atomic_int_t is used here only for MPIDI_set_progress_vci() and
-     * MPIDI_set_progress_vci_n(), which access these progress counts outside a VCI critical
-     * section. */
-    MPL_atomic_int_t progress_counts[MPIDI_CH4_MAX_VCIS];
 
 #if defined(MPIDI_CH4_USE_WORK_QUEUES)
     /* TODO: move into MPIDI_vci to have per-vci workqueue */
