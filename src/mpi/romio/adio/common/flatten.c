ac441a43d29378fa7e3fea988f959be0747769ea
Hui Zhou <hzhou321@anl.gov>
2021-04-29 21:31:39 -0500

    misc: remove MPL_FALLTHROUGH
    The fallthrough attribute is less supported than the marker comment,
    i.e. /* fall through */. Besides, the current test in configure doesn't
    really work due to error:
    
        a label can only be part of a statement and a declaration is
        not a statement

--- src/mpi/romio/adio/common/flatten.c
+++ src/mpi/romio/adio/common/flatten.c
@@ -532,8 +532,7 @@ void ADIOI_Flatten(MPI_Datatype datatype, ADIOI_Flatlist_node * flat,
 #if defined HAVE_DECL_MPI_COMBINER_HINDEXED_BLOCK && HAVE_DECL_MPI_COMBINER_HINDEXED_BLOCK
         case MPI_COMBINER_HINDEXED_BLOCK:
             is_hindexed_block = 1;
-            /* deliberate fall-through */
-            MPL_FALLTHROUGH;
+            /* fall through */
 #endif
         case MPI_COMBINER_INDEXED_BLOCK:
 #ifdef FLATTEN_DEBUG
