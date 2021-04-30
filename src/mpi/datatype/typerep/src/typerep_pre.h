8a2eaa75d0edaa32427b94f7d681f82d919c2483
Min Si <msi@anl.gov>
2021-04-30 15:30:56 -0500

    datatype/typerep: define nonblocking pack/unpack/copy
    Define nonblocking i{pack,unpack,copy} APIs and corresponding wait.
    
    In yaksa, the nonblocking version only issues yaksa pack/unpack but does
    not wait for completion. It returns a typerep request. The new wait API
    is used to wait on the typerep request, which internally calls
    yaksa_request_wait.
    
    In dataloop, the nonblocking version is actually blocking. Thus, wait is
    a no-op and a blocking routine can simply call the corresponding
    nonblocking one.

--- src/mpi/datatype/typerep/src/typerep_pre.h
+++ src/mpi/datatype/typerep/src/typerep_pre.h
@@ -9,4 +9,13 @@
 #define MPICH_DATATYPE_ENGINE_YAKSA    (1)
 #define MPICH_DATATYPE_ENGINE_DATALOOP (2)
 
+#if (MPICH_DATATYPE_ENGINE == MPICH_DATATYPE_ENGINE_YAKSA)
+#include "yaksa.h"
+typedef yaksa_request_t MPIR_Typerep_req;
+#define MPIR_TYPEREP_REQ_NULL YAKSA_REQUEST__NULL
+#else
+typedef int MPIR_Typerep_req;   /* unused in dataloop */
+#define MPIR_TYPEREP_REQ_NULL 0
+#endif
+
 #endif /* TYPEREP_PRE_H_INCLUDED */
