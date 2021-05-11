cfe7ef85ba05337b5e8af6b144cf942a3979ce8c
Ken Raffenetti <raffenet@mcs.anl.gov>
2021-05-11 16:22:32 -0500

    Revert "hydra: update configure of mpl and hwloc"
    This reverts commit 0bd6e16fe2c478a0edd7b8572a14aba20de80456.

--- src/pm/hydra/tools/topo/hwloc/Makefile.mk
+++ src/pm/hydra/tools/topo/hwloc/Makefile.mk
@@ -6,3 +6,13 @@
 libhydra_la_SOURCES += tools/topo/hwloc/topo_hwloc.c
 
 noinst_HEADERS += tools/topo/hwloc/topo_hwloc.h
+
+if HYDRA_USE_EMBEDDED_HWLOC
+AM_CPPFLAGS += @HWLOC_EMBEDDED_CPPFLAGS@
+AM_CFLAGS += @HWLOC_EMBEDDED_CFLAGS@
+
+# Append hwloc to the external subdirs, so it gets built first
+external_subdirs += tools/topo/hwloc/hwloc
+external_dist_subdirs += tools/topo/hwloc/hwloc
+external_libs += @HWLOC_EMBEDDED_LDADD@ @HWLOC_EMBEDDED_LIBS@
+endif
