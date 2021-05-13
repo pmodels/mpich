d789bff998f43dfe6600ed9c001cc337670d687a
Yanfei Guo <yguo@anl.gov>
2021-05-13 16:43:17 -0500

    refactor pipeline

--- src/mpid/ch4/netmod/ucx/ucx_am_impl.h
+++ src/mpid/ch4/netmod/ucx/ucx_am_impl.h
@@ -35,16 +35,9 @@ MPL_STATIC_INLINE_PREFIX void MPIDI_UCX_am_isend_short_callback(void *request, u
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_UCX_AM_ISEND_SHORT_CALLBACK);
 }
 
-MPL_STATIC_INLINE_PREFIX void MPIDI_UCX_am_isend_pipeline_callback(void *request,
-                                                                   ucs_status_t status)
+MPL_STATIC_INLINE_PREFIX void MPIDI_UCX_am_isend_pipeline_req_cmpl(MPIDI_UCX_am_request_t * req,
+                                                                   int handler_id)
 {
-    MPIDI_UCX_ucp_request_t *ucp_request = (MPIDI_UCX_ucp_request_t *) request;
-    MPIDI_UCX_am_request_t *req = (MPIDI_UCX_am_request_t *) ucp_request->req;
-    int handler_id = req->handler_id;
-
-    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_UCX_AM_ISEND_SHORT_CALLBACK);
-    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_UCX_AM_ISEND_SHORT_CALLBACK);
-
     MPID_Request_complete(req->sreq);
 
     if (req->is_gpu_pack_buffer) {
@@ -58,6 +51,18 @@ MPL_STATIC_INLINE_PREFIX void MPIDI_UCX_am_isend_pipeline_callback(void *request
         MPIDIG_global.origin_cbs[handler_id] (req->sreq);
     }
     MPIDU_genq_private_pool_free_cell(MPIDI_UCX_global.am_hdr_buf_pool, (void *) req);
+}
+
+MPL_STATIC_INLINE_PREFIX void MPIDI_UCX_am_isend_pipeline_callback(void *request,
+                                                                   ucs_status_t status)
+{
+    MPIDI_UCX_ucp_request_t *ucp_request = (MPIDI_UCX_ucp_request_t *) request;
+    MPIDI_UCX_am_request_t *req = (MPIDI_UCX_am_request_t *) ucp_request->req;
+
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_UCX_AM_ISEND_SHORT_CALLBACK);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_UCX_AM_ISEND_SHORT_CALLBACK);
+
+    MPIDI_UCX_am_isend_pipeline_req_cmpl(req, req->handler_id);
     ucp_request->req = NULL;
 
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_UCX_AM_ISEND_SHORT_CALLBACK);
@@ -95,6 +100,42 @@ MPL_STATIC_INLINE_PREFIX MPIDI_UCX_ucp_request_t
     goto fn_exit;
 }
 
+MPL_STATIC_INLINE_PREFIX MPIDI_UCX_ucp_request_t
+    * MPIDI_UCX_ucp_am_send_iov(ucp_ep_h ep, const void *ucx_hdr, MPI_Aint ucx_hdr_sz,
+                                const void *am_hdr, MPI_Aint am_hdr_sz, void *data,
+                                MPI_Aint data_sz, MPIDI_UCX_am_request_t * send_req,
+                                int ucx_am_handler_id, ucp_send_callback_t cb)
+{
+    MPIDI_UCX_ucp_request_t *ucp_request;
+    char *pack_buf = NULL;
+    MPI_Aint header_sz = 0;
+
+    header_sz = ucx_hdr_sz + am_hdr_sz;
+    pack_buf = (char *) MPL_malloc(header_sz, MPL_MEM_OTHER);
+
+    MPIR_Memcpy(pack_buf, ucx_hdr, ucx_hdr_sz);
+    if (am_hdr_sz) {
+        MPIR_Memcpy(pack_buf + ucx_hdr_sz, am_hdr, am_hdr_sz);
+    }
+
+    send_req->iov[0].buffer = pack_buf;
+    send_req->iov[0].length = header_sz;
+    send_req->iov[1].buffer = data;
+    send_req->iov[1].length = data_sz;
+
+    send_req->pack_buffer = pack_buf;
+    send_req->is_gpu_pack_buffer = false;
+
+    ucp_request =
+        (MPIDI_UCX_ucp_request_t *) ucp_am_send_nb(ep, ucx_am_handler_id, send_req->iov, 2,
+                                                   ucp_dt_make_iov(), cb, 0);
+
+  fn_exit:
+    return ucp_request;
+  fn_fail:
+    goto fn_exit;
+}
+
 MPL_STATIC_INLINE_PREFIX int MPIDI_UCX_do_am_isend_short(int rank, MPIR_Comm * comm, int handler_id,
                                                          const void *am_hdr, MPI_Aint am_hdr_sz,
                                                          const void *data, MPI_Aint count,
@@ -108,7 +149,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_UCX_do_am_isend_short(int rank, MPIR_Comm * c
     char *send_buf;
     int dt_contig = 0;
     MPI_Aint data_sz;
-    MPI_Aint dt_true_lb, last;
+    MPI_Aint dt_true_lb = 0, last;
     bool need_packing = false;
 
     MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_UCX_DO_AM_ISEND_SHORT);
@@ -240,6 +281,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_UCX_do_am_isend_pipeline(int rank, MPIR_Comm 
      * we need to skip some code path in the scenario. Also am_hdr, am_hdr_sz and data_sz are
      * ignored when issue_deferred is set to true. They should have been saved in the request. */
 
+    /* STEP1: check buffer/DT type */
     if (!issue_deferred) {
         MPIDI_Datatype_check_contig_size(datatype, count, dt_contig, data_sz);
 
@@ -261,6 +303,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_UCX_do_am_isend_pipeline(int rank, MPIR_Comm 
         /* we are issuing deferred op. If the offset == 0, this is the first segment and we need to
          * send am_hdr */
         offset = MPIDIG_am_send_async_get_offset(sreq);
+        /* if we are not issuing the first segment, skip am_hdr */
         local_am_hdr_sz = offset ? 0 : am_hdr_sz;
         send_size = MPIDI_UCX_DEFAULT_SHORT_SEND_SIZE - local_am_hdr_sz
             - sizeof(MPIDI_UCX_am_header_t);
@@ -279,6 +322,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_UCX_do_am_isend_pipeline(int rank, MPIR_Comm 
 
     ep = MPIDI_UCX_COMM_TO_EP(comm, rank, 0, 0);
 
+    /* STEP2: getting pack buffer if needed and send */
     if (need_packing) {
         /* FIXME: currently we always do packing, also for high density types. However,
          * we should not do packing unless needed. Also, for large low-density types
@@ -293,87 +337,56 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_UCX_do_am_isend_pipeline(int rank, MPIR_Comm 
                 goto fn_exit;
             }
         }
-        char *data_buf = (char *) send_buf + sizeof(ucx_hdr);
-        if (local_am_hdr_sz) {
-            MPIR_Memcpy(data_buf, am_hdr, local_am_hdr_sz);
-            data_buf += local_am_hdr_sz;
-        }
+        char *data_buf = (char *) send_buf + sizeof(ucx_hdr) + local_am_hdr_sz;
         mpi_errno = MPIR_Typerep_pack(buf, count, datatype, offset, (void *) data_buf, send_size,
                                       &packed_size);
         MPIR_ERR_CHECK(mpi_errno);
         send_size = packed_size;
 
         ucx_hdr.data_sz = send_size;
-        MPIR_Memcpy(send_buf, &ucx_hdr, sizeof(ucx_hdr));
 
         mpi_errno = MPIDU_genq_private_pool_alloc_cell(MPIDI_UCX_global.am_hdr_buf_pool,
                                                        (void **) &send_req);
         MPIR_Assert(send_req);
-
         MPIR_cc_incr(sreq->cc_ptr, &c);
-        ucp_request =
-            (MPIDI_UCX_ucp_request_t *) ucp_am_send_nb(ep, MPIDI_UCX_AM_HANDLER_ID__PIPELINE,
-                                                       send_buf, send_size + local_am_hdr_sz +
-                                                       sizeof(ucx_hdr), ucp_dt_make_contig(1),
-                                                       &MPIDI_UCX_am_isend_pipeline_callback, 0);
+        send_req->sreq = sreq;
+        send_req->pack_buffer = send_buf;
+        send_req->is_gpu_pack_buffer = true;
+
+        ucp_request = MPIDI_UCX_ucp_am_send_buffered(ep, &ucx_hdr, sizeof(ucx_hdr), am_hdr,
+                                                     local_am_hdr_sz, data_buf,
+                                                     send_size, send_req,
+                                                     MPIDI_UCX_AM_HANDLER_ID__PIPELINE,
+                                                     &MPIDI_UCX_am_isend_pipeline_callback);
     } else {
         MPIDI_Datatype_check_lb(datatype, dt_true_lb);
-        int hdr_size = sizeof(MPIDI_UCX_am_header_t) + local_am_hdr_sz;
-        /* send_buf here is only for ucx_hdr and am_hdr */
-        send_buf = MPL_malloc(hdr_size, MPL_MEM_OTHER);
-        MPIR_Memcpy(send_buf, &ucx_hdr, sizeof(ucx_hdr));
-        if (local_am_hdr_sz) {
-            MPIR_Memcpy(send_buf + sizeof(MPIDI_UCX_am_header_t), am_hdr, local_am_hdr_sz);
-        }
-
         mpi_errno = MPIDU_genq_private_pool_alloc_cell(MPIDI_UCX_global.am_hdr_buf_pool,
                                                        (void **) &send_req);
         MPIR_Assert(send_req);
-
-        send_req->iov[0].buffer = send_buf;
-        send_req->iov[0].length = hdr_size;
-        send_req->iov[1].buffer = (char *) buf + dt_true_lb + offset;
-        send_req->iov[1].length = send_size;
-
         MPIR_cc_incr(sreq->cc_ptr, &c);
-        ucp_request =
-            (MPIDI_UCX_ucp_request_t *) ucp_am_send_nb(ep, MPIDI_UCX_AM_HANDLER_ID__PIPELINE,
-                                                       send_req->iov, 2, ucp_dt_make_iov(),
-                                                       &MPIDI_UCX_am_isend_pipeline_callback, 0);
+        send_req->sreq = sreq;
+
+        ucp_request = MPIDI_UCX_ucp_am_send_iov(ep, &ucx_hdr, sizeof(ucx_hdr), am_hdr,
+                                                local_am_hdr_sz, (char *) buf + dt_true_lb + offset,
+                                                send_size, send_req,
+                                                MPIDI_UCX_AM_HANDLER_ID__PIPELINE,
+                                                &MPIDI_UCX_am_isend_pipeline_callback);
     }
 
     MPIDIG_am_send_async_issue_seg(sreq, send_size);
     MPIR_ERR_CHECK(mpi_errno);
 
+    /* STEP3: handles immediate send completion or preseve info for callback */
     if (ucp_request == NULL) {
-        /* send is done. free all resources and complete the request */
-        MPID_Request_complete(sreq);
-        if (need_packing) {
-            MPIDU_genq_private_pool_free_cell(MPIDI_UCX_global.pack_buf_pool, (void *) send_buf);
-        } else {
-            MPL_free(send_buf);
-        }
-        MPIDU_genq_private_pool_free_cell(MPIDI_UCX_global.am_hdr_buf_pool, (void *) send_req);
-        int is_done = MPIDIG_am_send_async_finish_seg(sreq);
-        if (is_done) {
-            if (issue_deferred) {
-                DL_DELETE(MPIDI_UCX_global.deferred_am_isend_q,
-                          MPIDI_UCX_AMREQUEST(sreq, deferred_req));
-                MPL_free(MPIDI_UCX_AMREQUEST(sreq, deferred_req));
-            }
-            mpi_errno = MPIDIG_global.origin_cbs[handler_id] (sreq);
-            goto fn_exit;
-        }
+        MPIDI_UCX_am_isend_pipeline_req_cmpl(send_req, handler_id);
     } else {
         /* send is not done. remember resources in send_req (not the sreq) */
-        send_req->pack_buffer = send_buf;
         send_req->handler_id = handler_id;
-        send_req->is_gpu_pack_buffer = need_packing;
-        send_req->sreq = sreq;
         ucp_request->req = (MPIR_Request *) send_req;
         ucp_request_release(ucp_request);
     }
 
+    /* STEP4: cleanup deferred req if needed */
     /* if there IS MORE DATA to be sent and we ARE NOT called for issue deferred op, enqueue.
      * if there NO MORE DATA and we ARE called for issuing deferred op, pipeline is done, dequeue
      * skip for all other cases */
@@ -384,6 +397,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_UCX_do_am_isend_pipeline(int rank, MPIR_Comm 
     } else if (issue_deferred) {
         DL_DELETE(MPIDI_UCX_global.deferred_am_isend_q, MPIDI_UCX_AMREQUEST(sreq, deferred_req));
         MPL_free(MPIDI_UCX_AMREQUEST(sreq, deferred_req));
+        MPID_Request_complete(sreq);
     }
 
   fn_exit:
@@ -409,6 +423,8 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_UCX_do_am_isend_pipeline(int rank, MPIR_Comm 
     MPIDI_UCX_AMREQUEST(sreq, deferred_req)->am_hdr_sz = am_hdr_sz;
     MPIR_Memcpy(MPIDI_UCX_AMREQUEST(sreq, deferred_req)->am_hdr, am_hdr, am_hdr_sz);
     DL_APPEND(MPIDI_UCX_global.deferred_am_isend_q, MPIDI_UCX_AMREQUEST(sreq, deferred_req));
+    /* NOTE: need to hold a ref to the sreq for the deferred_req */
+    MPIR_cc_incr(sreq->cc_ptr, &c);
     goto fn_exit;
 }
 
