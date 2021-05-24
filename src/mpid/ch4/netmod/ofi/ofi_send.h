3c37aaae9b34c799fd77fe9c8aad9cbf1918f667
Ken Raffenetti <raffenet@mcs.anl.gov>
2021-05-24 15:28:31 -0500

    ch4/ofi: Make huge protocol tracking lists global
    Rather than keep these lists on a per-comm basis, use global
    lists. This will help simplify the code without any significant cost,
    since lookups are hash-based.

--- src/mpid/ch4/netmod/ofi/ofi_send.h
+++ src/mpid/ch4/netmod/ofi/ofi_send.h
@@ -317,7 +317,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_OFI_send_normal(const void *buf, MPI_Aint cou
                                      NULL), mr_reg);    /* In:  context             */
         }
         /* Create map to the memory region */
-        MPIDIU_map_set(MPIDI_OFI_COMM(comm).huge_send_counters, sreq->handle, huge_send_mrs,
+        MPIDIU_map_set(MPIDI_OFI_global.huge_send_counters, sreq->handle, huge_send_mrs,
                        MPL_MEM_BUFFER);
         if (MPIDI_OFI_ENABLE_MR_PROV_KEY) {
             /* MR_BASIC */
