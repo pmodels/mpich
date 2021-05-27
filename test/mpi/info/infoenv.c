6ebc196b34f62a4eb51f347e8ad05169f233130e
Wesley Bland <wesley.bland@intel.com>
2021-05-27 12:02:35 -0500

    test: Improve infoenv test to include multi-NIC

--- test/mpi/info/infoenv.c
+++ test/mpi/info/infoenv.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 {
     char value[MPI_MAX_INFO_VAL];
     const char *keys[] = { "command", "argv", "maxprocs", "soft", "host", "arch", "wdir", "file",
-        "thread_level", 0
+        "thread_level", "num_nics", "num_close_nics", 0
     };
     int flag, i;
 
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
     for (i = 0; keys[i]; i++) {
         MPI_Info_get(MPI_INFO_ENV, keys[i], MPI_MAX_INFO_VAL, value, &flag);
         if (flag && verbose)
-            printf("command: %s\n", value);
+            printf("%s: %s\n", keys[i], value);
     }
 
     MTest_Finalize(0);
