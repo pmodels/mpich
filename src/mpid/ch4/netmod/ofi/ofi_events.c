pmodels/mpich

commit 90a02540728c9ce30d990312740f538d0883c15b

ch4/ofi: Remove context_id->comm lookup in huge path
Use the communcicator stored in the received request, rather than
lookup via context_id. Comm and striping information is set when a
huge message is posted, so we can safely remove setting the same info
when an unexpected message arrives.

src/mpid/ch4/netmod/ofi/ofi_events.c
@@ -64,7 +64,7 @@ static int peek_event(struct fi_cq_tagged_entry *wc, MPIR_Request * rreq)
             recv_elem = (MPIDI_OFI_huge_recv_t *) MPL_calloc(sizeof(*recv_elem), 1, MPL_MEM_COMM);
             MPIR_ERR_CHKANDJUMP(recv_elem == NULL, mpi_errno, MPI_ERR_OTHER, "**nomem");
             recv_elem->peek = true;
-            MPIR_Comm *comm_ptr = MPIDIG_context_id_to_comm(MPIDI_OFI_CONTEXT_MASK & wc->tag);
+            MPIR_Comm *comm_ptr = rreq->comm;
             recv_elem->comm_ptr = comm_ptr;
             MPIDIU_map_set(MPIDI_OFI_global.huge_recv_counters, rreq->handle, recv_elem,
                            MPL_MEM_BUFFER);
@@ -220,6 +220,11 @@ static int recv_huge_event(struct fi_cq_tagged_entry *wc, MPIR_Request * rreq)
     recv_elem->localreq = rreq;
     recv_elem->done_fn = MPIDI_OFI_recv_event;
     recv_elem->wc = *wc;
+    if (MPIDI_OFI_COMM(comm_ptr).enable_striping) {
+        recv_elem->cur_offset = MPIDI_OFI_STRIPE_CHUNK_SIZE;
+    } else {
+        recv_elem->cur_offset = MPIDI_OFI_global.max_msg_size;
+    }
     MPIDI_OFI_get_huge_event(NULL, (MPIR_Request *) recv_elem);
 
   fn_exit:

src/mpid/ch4/netmod/ofi/util.c
@@ -167,13 +167,9 @@ void MPIDI_OFI_mr_key_allocator_destroy(void)
 static int MPIDI_OFI_get_huge(MPIDI_OFI_send_control_t * info)
 {
     MPIDI_OFI_huge_recv_t *recv_elem = NULL;
-    MPIR_Comm *comm_ptr;
     int mpi_errno = MPI_SUCCESS;
     MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_OFI_GET_HUGE);
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_OFI_GET_HUGE);
-
-    /* Look up the communicator */
-    comm_ptr = MPIDIG_context_id_to_comm(info->comm_id);
 
     /* If there has been a posted receive, search through the list of unmatched
      * receives to find the one that goes with the incoming message. */
@@ -230,13 +226,7 @@ static int MPIDI_OFI_get_huge(MPIDI_OFI_send_control_t * info)
     }
 
     recv_elem->event_id = MPIDI_OFI_EVENT_GET_HUGE;
-    if (MPIDI_OFI_COMM(comm_ptr).enable_striping) {
-        recv_elem->cur_offset = MPIDI_OFI_STRIPE_CHUNK_SIZE;
-    } else {
-        recv_elem->cur_offset = MPIDI_OFI_global.max_msg_size;
-    }
     recv_elem->remote_info = *info;
-    recv_elem->comm_ptr = comm_ptr;
     recv_elem->next = NULL;
     MPIDI_OFI_get_huge_event(NULL, (MPIR_Request *) recv_elem);
 

commit f8b951b2ce1c92007d5461bfe6442c8ec05d84e9

ch4/ofi: Fix huge message striping
90a02540728c broke huge message striping because it started filling in
the current offset earlier than was previously expected. This meant that
the control message describing the message hadn't yet arrived.

Change the huge message receive protocol to use both the current offset
and the message size to make sure the message has arrived.

src/mpid/ch4/netmod/ofi/ofi_events.c
@@ -321,9 +321,9 @@ int MPIDI_OFI_get_huge_event(struct fi_cq_tagged_entry *wc, MPIR_Request * req)
     MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_OFI_GET_HUGE_EVENT);
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_OFI_GET_HUGE_EVENT);
 
-    if (recv_elem->localreq && recv_elem->cur_offset != 0) {    /* If this is true, then the message has a posted
-                                                                 * receive already and we'll be able to find the
-                                                                 * struct describing the transfer. */
+    /* If this is true, then the message has a posted receive already and we'll be able to find the
+     * struct describing the transfer. */
+    if (recv_elem->localreq && recv_elem->cur_offset != 0 && recv_elem->remote_info.msgsize != 0) {
         if (MPIDI_OFI_COMM(recv_elem->comm_ptr).enable_striping) {
             /* Subtract one stripe_chunk_size because we send the first chunk via a regular message
              * instead of the memory region */