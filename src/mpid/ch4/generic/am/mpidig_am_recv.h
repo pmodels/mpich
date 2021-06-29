4cda05e3d8cd8629a595829cdc0c70b2fccc1be8
Yanfei Guo <yguo@anl.gov>
2021-06-28 20:17:25 -0500

    generic: cleanup file with only one function

--- src/mpid/ch4/generic/am/mpidig_am_recv.h
+++ src/mpid/ch4/generic/am/mpidig_am_recv.h
@@ -8,7 +8,6 @@
 
 #include "ch4_impl.h"
 #include "ch4r_proc.h"
-#include "ch4r_recv.h"
 
 MPL_STATIC_INLINE_PREFIX void MPIDIG_prepare_recv_req(int rank, int tag,
                                                       MPIR_Context_id_t context_id, void *buf,
@@ -96,6 +95,37 @@ MPL_STATIC_INLINE_PREFIX int MPIDIG_copy_from_unexp_req(MPIR_Request * req, void
                                       MPIDIG_REQUEST(req, buffer));
 
     return mpi_errno;
+}
+
+MPL_STATIC_INLINE_PREFIX int MPIDIG_reply_ssend(MPIR_Request * rreq)
+{
+    int mpi_errno = MPI_SUCCESS;
+    MPIDIG_ssend_ack_msg_t ack_msg;
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDIG_REPLY_SSEND);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDIG_REPLY_SSEND);
+    ack_msg.sreq_ptr = MPIDIG_REQUEST(rreq, req->rreq.peer_req_ptr);
+
+#ifndef MPIDI_CH4_DIRECT_NETMOD
+    if (MPIDI_REQUEST(rreq, is_local))
+        mpi_errno =
+            MPIDI_SHM_am_send_hdr_reply(rreq->comm,
+                                        MPIDIG_REQUEST(rreq, rank), MPIDIG_SSEND_ACK, &ack_msg,
+                                        (MPI_Aint) sizeof(ack_msg));
+    else
+#endif
+    {
+        mpi_errno =
+            MPIDI_NM_am_send_hdr_reply(rreq->comm,
+                                       MPIDIG_REQUEST(rreq, rank), MPIDIG_SSEND_ACK, &ack_msg,
+                                       (MPI_Aint) sizeof(ack_msg));
+    }
+
+    MPIR_ERR_CHECK(mpi_errno);
+  fn_exit:
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDIG_REPLY_SSEND);
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
 }
 
 MPL_STATIC_INLINE_PREFIX int MPIDIG_handle_unexpected(void *buf, MPI_Aint count,
