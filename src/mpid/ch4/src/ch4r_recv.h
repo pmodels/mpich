8e85d8ee8b4690aaddb7f3d0a9dbb41db241680c
Yanfei Guo <yguo@anl.gov>
2021-06-28 19:41:52 -0500

    generic: adding util function for copy unexp data into user buffer
    This removes the duplicated code for checking data size and copying used
    in three places.

--- src/mpid/ch4/src/ch4r_recv.h
+++ src/mpid/ch4/src/ch4r_recv.h
@@ -43,11 +43,6 @@ MPL_STATIC_INLINE_PREFIX int MPIDIG_reply_ssend(MPIR_Request * rreq)
 MPL_STATIC_INLINE_PREFIX int MPIDIG_handle_unexp_mrecv(MPIR_Request * rreq)
 {
     int mpi_errno = MPI_SUCCESS;
-    size_t message_sz;
-    int dt_contig;
-    MPI_Aint dt_true_lb;
-    MPIR_Datatype *dt_ptr;
-    size_t data_sz ATTRIBUTE((unused)), dt_sz, nbytes;
     void *buf;
     MPI_Aint count;
     MPI_Datatype datatype;
@@ -62,42 +57,8 @@ MPL_STATIC_INLINE_PREFIX int MPIDIG_handle_unexp_mrecv(MPIR_Request * rreq)
     count = MPIDIG_REQUEST(rreq, req->rreq.mrcv_count);
     datatype = MPIDIG_REQUEST(rreq, req->rreq.mrcv_datatype);
 
-    message_sz = MPIDIG_REQUEST(rreq, count);
-    MPIR_Datatype_get_size_macro(datatype, dt_sz);
+    MPIDIG_copy_from_unexp_req(rreq, buf, datatype, count);
 
-    if (message_sz > count * dt_sz) {
-        rreq->status.MPI_ERROR = MPIR_Err_create_code(rreq->status.MPI_ERROR,
-                                                      MPIR_ERR_RECOVERABLE, __FUNCTION__, __LINE__,
-                                                      MPI_ERR_TRUNCATE, "**truncate",
-                                                      "**truncate %d %d %d %d",
-                                                      rreq->status.MPI_SOURCE, rreq->status.MPI_TAG,
-                                                      dt_sz * count, message_sz);
-        nbytes = dt_sz * count;
-    } else {
-        nbytes = message_sz;
-    }
-
-    MPIR_STATUS_SET_COUNT(rreq->status, nbytes);
-    MPIDI_Datatype_get_info(count, datatype, dt_contig, data_sz, dt_ptr, dt_true_lb);
-
-    if (!dt_contig) {
-        MPI_Aint actual_unpack_bytes;
-        mpi_errno = MPIR_Typerep_unpack(MPIDIG_REQUEST(rreq, buffer), nbytes, buf,
-                                        count, datatype, 0, &actual_unpack_bytes);
-        MPIR_ERR_CHECK(mpi_errno);
-
-        if (actual_unpack_bytes != (MPI_Aint) nbytes) {
-            mpi_errno = MPIR_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE,
-                                             __FUNCTION__, __LINE__,
-                                             MPI_ERR_TYPE, "**dtypemismatch", 0);
-            rreq->status.MPI_ERROR = mpi_errno;
-        }
-    } else {
-        MPIR_Typerep_copy((char *) buf + dt_true_lb, MPIDIG_REQUEST(rreq, buffer), nbytes);
-    }
-
-    MPIDU_genq_private_pool_free_cell(MPIDI_global.unexp_pack_buf_pool,
-                                      MPIDIG_REQUEST(rreq, buffer));
     rreq->kind = MPIR_REQUEST_KIND__RECV;
 
     if (MPIDIG_REQUEST(rreq, req->status) & MPIDIG_REQ_PEER_SSEND) {
