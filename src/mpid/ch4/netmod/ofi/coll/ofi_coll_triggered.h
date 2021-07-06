4aa3206faf7b2b213a8e29637cede323e30396cc
Islam, Nusrat <nusrat.islam@intel.com>
2021-07-06 11:48:48 -0500

    ch4/ofi: Add triggered ops based collective algorithms
    These are triggered ops based Bcast algorithms that use
    one-sided or two-sided communication.

---
+++ src/mpid/ch4/netmod/ofi/coll/ofi_coll_triggered.h
@@ -0,0 +1,8 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  Copyright (C) by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ *
+ */
+#include "ofi_bcast_tree_tagged.h"
+#include "ofi_bcast_tree_rma.h"
