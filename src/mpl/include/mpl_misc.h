7de9db9bfda036b4fe7ce5ef03c56f1dea059fd2
Sagar Thapaliya <sagar.thapaliya@intel.com>
2021-07-21 14:03:50 -0500

    mpl/misc: Add MPL_get_nprocs
    This patch adds MPL_get_nprocs, which returns the number of
    available processors.
    
    Co-authored-by: Mikhail Shiryaev <mikhail.shiryaev@intel.com>

---
+++ src/mpl/include/mpl_misc.h
@@ -0,0 +1,16 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/* vim: set ft=c.mpich : */
+/*
+ *  (C) 2018 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
+#ifndef MPL_MISC_H_INCLUDED
+#define MPL_MISC_H_INCLUDED
+
+#include "mplconfig.h"
+
+/* Returns the number of processors currently available in the system */
+int MPL_get_nprocs(void);
+
+#endif /* MPL_MISC_H_INCLUDED */
