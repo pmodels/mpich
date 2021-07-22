289559e8a4dd2e319c96feeb1aaadc07e067e760
Ken Raffenetti <raffenet@mcs.anl.gov>
2021-07-22 09:42:28 -0500

    Revert "ch4/part: Convert nm/shm init calls to hooks"
    This reverts commit f486f2611072286b2c5eecd6a53c305a7928ce29.

--- src/mpid/ch4/netmod/ofi/ofi_part.c
+++ src/mpid/ch4/netmod/ofi/ofi_part.c
@@ -7,20 +7,21 @@
 #include "ofi_impl.h"
 #include "ofi_noinline.h"
 
-int MPIDI_OFI_mpi_psend_init_hook(void *buf, int partitions, MPI_Aint count,
-                                  MPI_Datatype datatype, int dest, int tag,
-                                  MPIR_Comm * comm, MPIR_Info * info,
-                                  MPIDI_av_entry_t * av, MPIR_Request ** request)
+int MPIDI_OFI_mpi_psend_init(void *buf, int partitions, MPI_Aint count,
+                             MPI_Datatype datatype, int dest, int tag,
+                             MPIR_Comm * comm, MPIR_Info * info,
+                             MPIDI_av_entry_t * av, MPIR_Request ** request)
 {
-    return MPI_SUCCESS;
+    return MPIDIG_mpi_psend_init(buf, partitions, count, datatype, dest, tag, comm, info, request);
 }
 
-int MPIDI_OFI_mpi_precv_init_hook(void *buf, int partitions, MPI_Aint count,
-                                  MPI_Datatype datatype, int source, int tag,
-                                  MPIR_Comm * comm, MPIR_Info * info,
-                                  MPIDI_av_entry_t * av, MPIR_Request ** request)
+int MPIDI_OFI_mpi_precv_init(void *buf, int partitions, MPI_Aint count,
+                             MPI_Datatype datatype, int source, int tag,
+                             MPIR_Comm * comm, MPIR_Info * info,
+                             MPIDI_av_entry_t * av, MPIR_Request ** request)
 {
-    return MPI_SUCCESS;
+    return MPIDIG_mpi_precv_init(buf, partitions, count, datatype, source, tag, comm,
+                                 info, request);
 }
 
 int MPIDI_OFI_precv_matched_hook(MPIR_Request * part_req)
--- src/mpid/ch4/shm/src/shm_part.c
+++ src/mpid/ch4/shm/src/shm_part.c
@@ -7,22 +7,22 @@
 #include "shm_noinline.h"
 #include "../posix/posix_noinline.h"
 
-int MPIDI_SHM_mpi_psend_init_hook(void *buf, int partitions, MPI_Aint count,
-                                  MPI_Datatype datatype, int dest, int tag,
-                                  MPIR_Comm * comm, MPIR_Info * info,
-                                  MPIDI_av_entry_t * av, MPIR_Request ** request)
+int MPIDI_SHM_mpi_psend_init(void *buf, int partitions, MPI_Aint count,
+                             MPI_Datatype datatype, int dest, int tag,
+                             MPIR_Comm * comm, MPIR_Info * info,
+                             MPIDI_av_entry_t * av, MPIR_Request ** request)
 {
-    return MPIDI_POSIX_mpi_psend_init_hook(buf, partitions, count, datatype, dest, tag, comm,
-                                           info, av, request);
+    return MPIDI_POSIX_mpi_psend_init(buf, partitions, count, datatype, dest, tag, comm,
+                                      info, av, request);
 }
 
-int MPIDI_SHM_mpi_precv_init_hook(void *buf, int partitions, MPI_Aint count,
-                                  MPI_Datatype datatype, int source, int tag,
-                                  MPIR_Comm * comm, MPIR_Info * info,
-                                  MPIDI_av_entry_t * av, MPIR_Request ** request)
+int MPIDI_SHM_mpi_precv_init(void *buf, int partitions, MPI_Aint count,
+                             MPI_Datatype datatype, int source, int tag,
+                             MPIR_Comm * comm, MPIR_Info * info,
+                             MPIDI_av_entry_t * av, MPIR_Request ** request)
 {
-    return MPIDI_POSIX_mpi_precv_init_hook(buf, partitions, count, datatype, source, tag, comm,
-                                           info, av, request);
+    return MPIDI_POSIX_mpi_precv_init(buf, partitions, count, datatype, source, tag, comm,
+                                      info, av, request);
 }
 
 int MPIDI_SHM_precv_matched_hook(MPIR_Request * part_req)
