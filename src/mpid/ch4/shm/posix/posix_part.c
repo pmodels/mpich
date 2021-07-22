0f83eb166799adf2421fb3d8c3b665fc72920d74
Ken Raffenetti <raffenet@mcs.anl.gov>
2021-07-22 09:42:28 -0500

    Revert "ch4/part: Convert nm/shm init calls to hooks"
    This reverts commit 0272a3f9bb396e878254f5c410fb2184b8fb4663.

--- src/mpid/ch4/shm/posix/posix_part.c
+++ src/mpid/ch4/shm/posix/posix_part.c
@@ -6,20 +6,21 @@
 #include "mpidimpl.h"
 #include "posix_noinline.h"
 
-int MPIDI_POSIX_mpi_psend_init_hook(void *buf, int partitions, MPI_Aint count,
-                                    MPI_Datatype datatype, int dest, int tag,
-                                    MPIR_Comm * comm, MPIR_Info * info,
-                                    MPIDI_av_entry_t * av, MPIR_Request ** request)
+int MPIDI_POSIX_mpi_psend_init(void *buf, int partitions, MPI_Aint count,
+                               MPI_Datatype datatype, int dest, int tag,
+                               MPIR_Comm * comm, MPIR_Info * info,
+                               MPIDI_av_entry_t * av, MPIR_Request ** request)
 {
-    return MPI_SUCCESS;
+    return MPIDIG_mpi_psend_init(buf, partitions, count, datatype, dest, tag, comm, info, request);
 }
 
-int MPIDI_POSIX_mpi_precv_init_hook(void *buf, int partitions, MPI_Aint count,
-                                    MPI_Datatype datatype, int source, int tag,
-                                    MPIR_Comm * comm, MPIR_Info * info,
-                                    MPIDI_av_entry_t * av, MPIR_Request ** request)
+int MPIDI_POSIX_mpi_precv_init(void *buf, int partitions, MPI_Aint count,
+                               MPI_Datatype datatype, int source, int tag,
+                               MPIR_Comm * comm, MPIR_Info * info,
+                               MPIDI_av_entry_t * av, MPIR_Request ** request)
 {
-    return MPI_SUCCESS;
+    return MPIDIG_mpi_precv_init(buf, partitions, count, datatype, source, tag, comm,
+                                 info, request);
 }
 
 int MPIDI_POSIX_precv_matched_hook(MPIR_Request * part_req)
