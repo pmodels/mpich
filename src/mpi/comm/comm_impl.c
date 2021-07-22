19c3a903fac97bbe13947553274a9eba798ff729
Hui Zhou <hzhou321@anl.gov>
2021-07-21 20:43:16 -0500

    comm: add MPIR_peer_intercomm_create
    A peer intercomm is a 1-to-1 intercomm internally created by device
    layer to facilitate connecting dynamic processes.

--- src/mpi/comm/comm_impl.c
+++ src/mpi/comm/comm_impl.c
@@ -1195,6 +1195,52 @@ int MPIR_Intercomm_create_impl(MPIR_Comm * local_comm_ptr, int local_leader,
     goto fn_exit;
 }
 
+/* Peer intercomm is a 1-to-1 intercomm, internally created by device layer
+ * to facilitate connecting dynamic processes */
+
+int MPIR_peer_intercomm_create(MPIR_Context_id_t context_id, MPIR_Context_id_t recvcontext_id,
+                               uint64_t remote_lpid, int is_low_group, MPIR_Comm ** newcomm)
+{
+    int mpi_errno = MPI_SUCCESS;
+
+    mpi_errno = MPIR_Comm_create(newcomm);
+    MPIR_ERR_CHECK(mpi_errno);
+
+    (*newcomm)->context_id = context_id;
+    (*newcomm)->recvcontext_id = recvcontext_id;
+    (*newcomm)->remote_size = 1;
+    (*newcomm)->local_size = 1;
+    (*newcomm)->rank = 0;
+    (*newcomm)->comm_kind = MPIR_COMM_KIND__INTERCOMM;
+    (*newcomm)->local_comm = 0;
+    (*newcomm)->is_low_group = is_low_group;
+
+    mpi_errno = MPID_Create_intercomm_from_lpids(*newcomm, 1, &remote_lpid);
+    MPIR_ERR_CHECK(mpi_errno);
+
+    MPIR_Comm *comm_self = MPIR_Process.comm_self;
+    MPIR_Comm_map_dup(*newcomm, comm_self, MPIR_COMM_MAP_DIR__L2L);
+
+    /* Inherit the error handler  */
+    MPID_THREAD_CS_ENTER(POBJ, comm_self->mutex);
+    MPID_THREAD_CS_ENTER(VCI, comm_self->mutex);
+    (*newcomm)->errhandler = comm_self->errhandler;
+    if (comm_self->errhandler) {
+        MPIR_Errhandler_add_ref(comm_self->errhandler);
+    }
+    MPID_THREAD_CS_EXIT(POBJ, comm_self->mutex);
+    MPID_THREAD_CS_EXIT(VCI, comm_self->mutex);
+
+    (*newcomm)->tainted = 1;
+    mpi_errno = MPIR_Comm_commit(*newcomm);
+    MPIR_ERR_CHECK(mpi_errno);
+
+  fn_exit:
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
+}
+
 /* This function creates mapping for new communicator
  * basing on network addresses of existing communicator.
  */
