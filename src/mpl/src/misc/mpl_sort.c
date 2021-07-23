06dfaed2afeb75d6e7a1ec328846602262fdc024
Gengbin Zheng <gengbin.zheng@intel.com>
2021-07-23 00:10:05 -0500

    mpl: add utility function for integer sort

---
+++ src/mpl/src/misc/mpl_sort.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) by Argonne National Laboratory
+ *     See COPYRIGHT in top-level directory
+ */
+
+#include "mpl.h"
+
+/* This is a simple function to compare two integers. */
+#if defined(HAVE_QSORT)
+static int compare_int(const void *a, const void *b)
+{
+    return (*(int *) a - *(int *) b);
+}
+#endif
+
+void MPL_sort_int_array(int *arr, int n)
+{
+#if defined(HAVE_QSORT)
+    qsort(arr, n, sizeof(int), compare_int);
+#else
+    int i, j, value;
+    for (i = 0; i < n; i++) {
+        value = arr[i];
+        j = i;
+        while (j > 0 && arr[j - 1] > value) {
+            arr[j] = arr[j - 1];
+            j = j - 1;
+        }
+        arr[j] = value;
+    }
+#endif
+}
