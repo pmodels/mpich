e5060848afa48b1a869b36814477f0b6c3ec1947
Hui Zhou <hzhou321@anl.gov>
2021-07-24 11:17:13 -0500

    ch4/ucx: add MPIDI_UCX_dynamic_send/recv stub

--- src/mpid/ch4/netmod/ucx/ucx_spawn.c
+++ src/mpid/ch4/netmod/ucx/ucx_spawn.c
@@ -6,6 +6,24 @@
 #include "mpidimpl.h"
 #include "ucx_impl.h"
 
+int MPIDI_UCX_dynamic_send(uint64_t remote_gpid, int tag, const void *buf, int size, int timeout)
+{
+    int mpi_errno = MPI_SUCCESS;
+
+    MPIR_ERR_SET(mpi_errno, MPI_ERR_OTHER, "**ucx_nm_notsupported");
+
+    return mpi_errno;
+}
+
+int MPIDI_UCX_dynamic_recv(int tag, void *buf, int size, int timeout)
+{
+    int mpi_errno = MPI_SUCCESS;
+
+    MPIR_ERR_SET(mpi_errno, MPI_ERR_OTHER, "**ucx_nm_notsupported");
+
+    return mpi_errno;
+}
+
 int MPIDI_UCX_mpi_open_port(MPIR_Info * info_ptr, char *port_name)
 {
     int mpi_errno = MPI_SUCCESS;
