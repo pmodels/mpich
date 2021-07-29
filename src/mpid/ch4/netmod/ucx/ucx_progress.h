dd2670a659676326a135cbe9ddf2b726fa68459e
Yanfei Guo <yguo@anl.gov>
2021-07-29 12:41:40 -0500

    ch4/ucx: add missing function logging macros

--- src/mpid/ch4/netmod/ucx/ucx_progress.h
+++ src/mpid/ch4/netmod/ucx/ucx_progress.h
@@ -13,6 +13,10 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_NM_progress(int vci, int blocking)
     int mpi_errno = MPI_SUCCESS;
 
     int vni = MPIDI_UCX_vci_to_vni(vci);
+
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_NM_PROGRESS);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_NM_PROGRESS);
+
     if (vni < 0) {
         /* skip if the vci is not for us */
         return MPI_SUCCESS;
@@ -20,6 +24,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_NM_progress(int vci, int blocking)
 
     ucp_worker_progress(MPIDI_UCX_global.ctx[vni].worker);
 
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_NM_PROGRESS);
     return mpi_errno;
 }
 
