4652e43d0235ac7dc6889a6b5cc8fecfc94e1f77
Hui Zhou <hzhou321@anl.gov>
2021-08-07 09:25:09 -0500

    mpl: fix checking of MPL_HAVE_DECL__SC_NPROCESSORS_ONLN
    AC_CHECK_DECL will always define the macro both when the check succeeds
    or fail. With the latter, it defines the macro to 0. Thus merely
    checking #if defined is not sufficient.

--- src/mpl/src/misc/mpl_misc.c
+++ src/mpl/src/misc/mpl_misc.c
@@ -19,7 +19,7 @@ int MPL_get_nprocs(void)
 {
 #if defined (MPL_HAVE_GET_NPROCS)
     return get_nprocs();
-#elif defined (MPL_HAVE_DECL__SC_NPROCESSORS_ONLN)
+#elif defined (MPL_HAVE_DECL__SC_NPROCESSORS_ONLN) && MPL_HAVE_DECL__SC_NPROCESSORS_ONLN
     int count = sysconf(_SC_NPROCESSORS_ONLN);
     return (count > 0) ? count : 1;
 #else
