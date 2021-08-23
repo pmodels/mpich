bf32ea2f8495a203e7069a66df640ed9ed7a7408
Ken Raffenetti <raffenet@mcs.anl.gov>
2021-08-23 13:46:01 -0500

    ch4/ucx: Always progress after disconnect
    Even if all endpoint disconnects happen immediately, still run worker
    progress before destroy. In practice, this can complete outstanding
    receive requests that were freed by the user with MPI_REQUEST_FREE.
    
    Fixes pmodels/mpich#3469.

--- src/mpid/ch4/netmod/ucx/ucx_init.c
+++ src/mpid/ch4/netmod/ucx/ucx_init.c
@@ -391,8 +391,8 @@ int MPIDI_UCX_mpi_finalize_hook(void)
 
     /* now complete the outstaning requests! Important: call progress in between, otherwise we
      * deadlock! */
-    int completed = p;
-    while (completed != 0) {
+    int completed;
+    do {
         for (int i = 0; i < MPIDI_UCX_global.num_vnis; i++) {
             ucp_worker_progress(MPIDI_UCX_global.ctx[i].worker);
         }
@@ -401,7 +401,7 @@ int MPIDI_UCX_mpi_finalize_hook(void)
             if (ucp_request_is_completed(pending[i]) != 0)
                 completed -= 1;
         }
-    }
+    } while (completed != 0);
 
     for (int i = 0; i < p; i++) {
         ucp_request_release(pending[i]);
