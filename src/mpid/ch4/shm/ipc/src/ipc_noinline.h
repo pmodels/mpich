8f7e539ea39224d1cbcee42bb877eb520541a342
Alex Brooks <alex.brooks@intel.com>
2021-08-26 16:23:16 -0500

    ch4/ipc: implement send/recv for fd-based ipc handles
    In order to properly transfer file descriptors (fds), UNIX sockets must
    be used via sendmsg/recvmsg.

--- src/mpid/ch4/shm/ipc/src/ipc_noinline.h
+++ src/mpid/ch4/shm/ipc/src/ipc_noinline.h
@@ -17,4 +17,7 @@ int MPIDI_IPC_mpi_finalize_hook(void);
 int MPIDI_IPC_mpi_win_create_hook(MPIR_Win * win);
 int MPIDI_IPC_mpi_win_free_hook(MPIR_Win * win);
 
+int MPIDI_IPC_mpi_fd_send(int rank, int fd, void *payload, size_t payload_len);
+int MPIDI_IPC_mpi_fd_recv(int rank, int *fd, void *payload, size_t payload_len, int flags);
+
 #endif /* IPC_NOINLINE_H_INCLUDED */
