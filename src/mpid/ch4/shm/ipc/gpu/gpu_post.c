2ba75d2a91a932f944db43f6c1ca3d91a7a80de6
Gengbin Zheng <gengbin.zheng@intel.com>
2021-09-10 10:59:18 -0700

    ch4/ipc: check mem_id to invalidate cache entry when using Intel GPU
    Level zero maintains a unique mem_id for each device allocation.
    Using mem_id to check if a cached entry is valid before using it.

--- src/mpid/ch4/shm/ipc/gpu/gpu_post.c
+++ src/mpid/ch4/shm/ipc/gpu/gpu_post.c
@@ -190,6 +190,14 @@ int MPIDI_GPU_get_ipc_attr(const void *vaddr, int rank, MPIR_Comm * comm,
     mpi_errno = ipc_handle_cache_search(track_tree, pbase, len, (void **) &handle_obj);
     MPIR_ERR_CHECK(mpi_errno);
 
+#ifdef MPL_HAVE_ZE
+    if (handle_obj && handle_obj->mem_id != ipc_attr->gpu_attr.device_attr.prop.id) {
+        mpi_errno = ipc_handle_cache_delete(track_tree,
+                                            (void *) handle_obj->remote_base_addr, handle_obj->len);
+        MPIR_ERR_CHECK(mpi_errno);
+        handle_obj = NULL;
+    }
+#endif
     if (handle_obj == NULL) {
         mpl_err = MPL_gpu_ipc_handle_create(pbase, &ipc_attr->ipc_handle.gpu.ipc_handle);
         MPIR_ERR_CHKANDJUMP(mpl_err != MPL_SUCCESS, mpi_errno, MPI_ERR_OTHER,
@@ -208,6 +216,11 @@ int MPIDI_GPU_get_ipc_attr(const void *vaddr, int rank, MPIR_Comm * comm,
     ipc_attr->ipc_handle.gpu.len = len;
     ipc_attr->ipc_handle.gpu.node_rank = MPIR_Process.local_rank;
     ipc_attr->ipc_handle.gpu.offset = (uintptr_t) vaddr - (uintptr_t) pbase;
+#ifdef MPL_HAVE_ZE
+    ipc_attr->ipc_handle.gpu.mem_id = ipc_attr->gpu_attr.device_attr.prop.id;
+#else
+    ipc_attr->ipc_handle.gpu.mem_id = 0;
+#endif
 
     ipc_attr->ipc_handle.gpu.global_dev_id = global_dev_id;
     ipc_attr->threshold.send_lmt_sz = MPIR_CVAR_CH4_IPC_GPU_P2P_THRESHOLD;
