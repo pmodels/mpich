a3bff04878b9abb1aa1c0686bb4e59237ffc1654
Alex Brooks <alex.brooks@intel.com>
2021-09-10 16:58:09 -0500

    mpl/ze: prevent multiple initializations

--- src/mpl/src/gpu/mpl_gpu_ze.c
+++ src/mpl/src/gpu/mpl_gpu_ze.c
@@ -175,7 +175,11 @@ int MPL_gpu_dev_affinity_to_env(int dev_count, char **dev_list, char **env)
 
 int MPL_gpu_init(int debug_summary)
 {
-    int mpl_err;
+    int mpl_err = MPL_SUCCESS;
+    if (gpu_initialized) {
+        goto fn_exit;
+    }
+
     mpl_err = gpu_ze_init_driver();
     if (mpl_err != MPL_SUCCESS)
         goto fn_fail;
@@ -187,6 +191,11 @@ int MPL_gpu_init(int debug_summary)
 
     device_count = global_ze_device_count;
     max_dev_id = device_count - 1;
+
+    if (device_count <= 0) {
+        gpu_initialized = 1;
+        goto fn_exit;
+    }
 
     local_to_global_map = MPL_malloc(device_count * sizeof(int), MPL_MEM_OTHER);
     global_to_local_map = MPL_malloc(device_count * sizeof(int), MPL_MEM_OTHER);
