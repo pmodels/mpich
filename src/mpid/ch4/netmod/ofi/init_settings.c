b53a7140facf85b6b32fe9bae60fcd80c5ae83b9
Wesley Bland <wesley.bland@intel.com>
2021-09-29 15:35:43 -0500

    ch4/ofi: Add support for FI_MR_ENDPOINT
    FI_MR_ENDPOINT requires that memory regions be bound to an endpoint
    before they can be used. This is in addition to any other bindings that
    may need to happen.
    
    The MR also needs to be enabled after all of the bindings are done.

--- src/mpid/ch4/netmod/ofi/init_settings.c
+++ src/mpid/ch4/netmod/ofi/init_settings.c
@@ -61,6 +61,7 @@ void MPIDI_OFI_init_hints(struct fi_info *hints)
     /*           endpoint, so the netmod requires dynamic memory regions        */
     /* ------------------------------------------------------------------------ */
     hints->mode = FI_CONTEXT | FI_ASYNC_IOV | FI_RX_CQ_DATA;    /* We can handle contexts  */
+    hints->domain_attr->mr_mode |= FI_MR_ENDPOINT;
 
     if (MPIDI_OFI_get_required_version() >= FI_VERSION(1, 5)) {
 #ifdef FI_CONTEXT2
@@ -159,6 +160,8 @@ void MPIDI_OFI_init_hints(struct fi_info *hints)
             hints->domain_attr->mr_mode |= FI_MR_PROV_KEY;
         }
 #endif
+        /* This doesn't require an if check because it is supported all of the time. */
+        hints->domain_attr->mr_mode |= FI_MR_ENDPOINT;
     } else {
         /* In old versions FI_MR_BASIC is equivallent to set
          * FI_MR_VIRT_ADDR, FI_MR_PROV_KEY, and FI_MR_ALLOCATED on.
