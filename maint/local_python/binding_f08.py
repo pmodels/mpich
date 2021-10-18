627f1976823671af03dcc5be7e0cfa23bd633740
Hui Zhou <hzhou321@anl.gov>
2021-10-18 13:05:31 -0500

    f08: misc fixes when INTEGER is not c_int
    In alltoallw functions when MPI_IN_PLACE is used, we still need
    convert the send parameters to match the parameter types.
    
    We can't directly pass len(string) as c_int. Use an intermediate
    variable instead.

--- maint/local_python/binding_f08.py
+++ maint/local_python/binding_f08.py
@@ -199,18 +199,20 @@ def dump_f08_wrappers_f(func, is_large):
     # alltoallw inplace hack (since it is a corner case)
     def dump_alltoallw_inplace(arg_list_1, arg_list_2, convert_list_2):
         # cannot use like sendcounts(1:length)
-        send_args = "sendbuf, sendcounts, sdispls, sendtypes(1:1)%MPI_VAL"
-        args1 = send_args + ", " + ', '.join(arg_list_1[4:])
-        args2 = send_args + ", " + ', '.join(arg_list_2[4:])
-        dump_F_if_open("c_int == kind(0)")
-        dump_fortran_line("ierror_c = %s(%s)" % (c_func_name, args1))
-        dump_F_else()
-        G.out.append("recvcounts_c = recvcounts(1:length)")
-        G.out.append("rdispls_c = rdispls_c(1:length)")
-        G.out.append("recvtypes_c = recvtypes(1:length)%MPI_VAL")
-        dump_fortran_line("ierror_c = %s(%s)" % (c_func_name, args2))
-        G.out.extend(convert_list_2)
-        dump_F_if_close()
+        if G.opts['fint-size'] == G.opts['cint-size']:
+            send_args = "sendbuf, sendcounts, sdispls, sendtypes(1:1)%MPI_VAL"
+            args1 = send_args + ", " + ', '.join(arg_list_1[4:])
+            dump_fortran_line("ierror_c = %s(%s)" % (c_func_name, args1))
+        else:
+            args2 = ', '.join(arg_list_2)
+            G.out.append("sendcounts_c = sendcounts(1:1)")
+            G.out.append("sdispls_c = sdispls_c(1:1)")
+            G.out.append("sendtypes_c = sendtypes(1:1)%MPI_VAL")
+            G.out.append("recvcounts_c = recvcounts(1:length)")
+            G.out.append("rdispls_c = rdispls_c(1:length)")
+            G.out.append("recvtypes_c = recvtypes(1:length)%MPI_VAL")
+            dump_fortran_line("ierror_c = %s(%s)" % (c_func_name, args2))
+            G.out.extend(convert_list_2)
 
     # ----
     def process_integer(p):
@@ -573,6 +575,12 @@ def dump_f08_wrappers_f(func, is_large):
         arg = "%s_c" % p['name']
         return (arg, arg)
 
+    def post_string_len(v):
+        c_decl_list.append("INTEGER(c_int) :: %s_len" % v)
+        convert_list_pre.append("%s_len = len(%s)" % (v, v))
+        arg_list_1.append("%s_len" % v)
+        arg_list_2.append("%s_len" % v)
+
     # ----
     has_attribute_val = False
     for p in func['parameters']:
@@ -630,13 +638,10 @@ def dump_f08_wrappers_f(func, is_large):
         arg_list_1.append("MPIR_ATTR_AINT")
         arg_list_2.append("MPIR_ATTR_AINT")
     elif func['name'] == "MPI_Comm_spawn":
-        arg_list_1.append("len(argv)")
-        arg_list_2.append("len(argv)")
+        post_string_len("argv")
     elif func['name'] == "MPI_Comm_spawn_multiple":
-        arg_list_1.append("len(array_of_commands)")
-        arg_list_1.append("len(array_of_argv)")
-        arg_list_2.append("len(array_of_commands)")
-        arg_list_2.append("len(array_of_argv)")
+        post_string_len("array_of_commands")
+        post_string_len("array_of_argv")
 
     # -- return
     if 'return' not in func:
@@ -702,16 +707,13 @@ def dump_f08_wrappers_f(func, is_large):
         dump_F_if_open("c_associated(c_loc(sendbuf), c_loc(MPI_IN_PLACE))")
         dump_alltoallw_inplace(arg_list_1, arg_list_2, convert_list_2)
         dump_F_else()
-    if need_check_int_kind:
-        dump_F_if_open("c_int == kind(0)")
+    if need_check_int_kind and G.opts['fint-size'] == G.opts['cint-size']:
         dump_call("%s = %s(%s)" % (ret, c_func_name, ', '.join(arg_list_1)), False)
-        dump_F_else()
-    G.out.extend(convert_list_1)
-    dump_call("%s = %s(%s)" % (ret, c_func_name, ', '.join(arg_list_2)), True)
-    G.out.extend(convert_list_2)
+    else:
+        G.out.extend(convert_list_1)
+        dump_call("%s = %s(%s)" % (ret, c_func_name, ', '.join(arg_list_2)), True)
+        G.out.extend(convert_list_2)
 
-    if need_check_int_kind:
-        dump_F_if_close()
     if is_alltoallw:
         dump_F_if_close()
     G.out.append("")
