47390c1aa3b730a6770161e9a5b38962b3788a7e
Rob Latham <robl@mcs.anl.gov>
2021-10-18 14:15:45 -0500

    romio: write visibility feature
    Lots of file systems implement POSIX semantics: a write on one process
    immediately visible on others.

--- src/mpi/romio/adio/ad_nfs/ad_nfs_features.c
+++ src/mpi/romio/adio/ad_nfs/ad_nfs_features.c
@@ -13,10 +13,12 @@ int ADIOI_NFS_Feature(ADIO_File fd, int flag)
         case ADIO_LOCKS:
         case ADIO_SEQUENTIAL:
         case ADIO_DATA_SIEVING_WRITES:
+        case ADIO_ATOMIC_MODE:
             return 1;
         case ADIO_SCALABLE_OPEN:
         case ADIO_UNLINK_AFTER_CLOSE:
         case ADIO_SCALABLE_RESIZE:
+        case ADIO_IMMEDIATELY_VISIBLE:
         default:
             return 0;
     }
