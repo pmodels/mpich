43790fa3b5373a80467d765e27400bb8fee1a443
Islam, Nusrat <nusrat.islam@intel.com>
2021-11-18 16:24:50 -0600

    ch4/ofi: add list to store registration handles
    Even though some providers cache the handles for memory registration
    by default, they need to be cached in the MPICH layer also so that they
    can be freed during MPI_Finalize().

--- src/mpid/ch4/netmod/ofi/ofi_init.c
+++ src/mpid/ch4/netmod/ofi/ofi_init.c
@@ -804,6 +804,8 @@ int MPIDI_OFI_init_local(int *tag_bits)
     MPIDIU_map_create(&MPIDI_OFI_global.win_map, MPL_MEM_RMA);
     MPIDIU_map_create(&MPIDI_OFI_global.req_map, MPL_MEM_OTHER);
 
+    MPIDI_OFI_global.gdr_mrs = NULL;
+
     /* Initialize RMA keys allocator */
     MPIDI_OFI_mr_key_allocator_init();
 
@@ -1145,6 +1147,21 @@ int MPIDI_OFI_mpi_finalize_hook(void)
             MPIDI_OFI_PROGRESS(vni);
         }
         MPIR_Assert(MPIDI_OFI_global.per_vni[vni].am_inflight_inject_emus == 0);
+    }
+
+    if (MPIDI_OFI_ENABLE_HMEM && MPIDI_OFI_ENABLE_MR_HMEM) {
+        MPIDI_GPU_RDMA_queue_t *queue_mr;
+        DL_FOREACH(MPIDI_OFI_global.gdr_mrs, queue_mr) {
+            if (queue_mr->mr) {
+                struct fid_mr *mr = (struct fid_mr *) queue_mr->mr;
+                if (mr != NULL) {
+                    MPIDI_OFI_CALL(fi_close(&mr->fid), mr_unreg);
+                }
+
+                DL_DELETE(MPIDI_OFI_global.gdr_mrs, queue_mr);
+                MPL_free(queue_mr);
+            }
+        }
     }
 
     /* Tearing down endpoints in reverse order they were created */
