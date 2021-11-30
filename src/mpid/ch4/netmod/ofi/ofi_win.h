b4c060bb420fa5a3f4c4c0ef603e06f54ef6b1f1
Wesley Bland <wesley.bland@intel.com>
2021-11-30 10:48:15 -0800

    ch4/ofi: Create capability for counter wait objects
    The CXI provider does not currently support counter wait objects so we
    need a capability set to easily turn that off when it can't be used.
    Unfortunately, there is no way to programmatically discover what support
    is present so this will have to be hand-coded.

--- src/mpid/ch4/netmod/ofi/ofi_win.h
+++ src/mpid/ch4/netmod/ofi/ofi_win.h
@@ -48,7 +48,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_OFI_win_do_progress(MPIR_Win * win, int vni)
             donecount = fi_cntr_read(MPIDI_OFI_WIN(win).cmpl_cntr);
             itercount++;
 
-            if (itercount == 1000) {
+            if (itercount == 1000 && MPIDI_OFI_COUNTER_WAIT_OBJECTS) {
                 ret = fi_cntr_wait(MPIDI_OFI_WIN(win).cmpl_cntr, tcount, 0);
                 MPIDI_OFI_ERR(ret < 0 && ret != -FI_ETIMEDOUT,
                               mpi_errno,
