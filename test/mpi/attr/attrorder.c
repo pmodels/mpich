4a031bcb33695b0fec8904089a63a8bec988ef51
Hui Zhou <hzhou321@anl.gov>
2021-12-11 11:23:31 -0600

    test: convert rest of the attr tests to functions
    Only test in attr folder that is not converted is attrend2 since that
    test tests MPI_Finalize behaviors.

--- test/mpi/attr/attrorder.c
+++ test/mpi/attr/attrorder.c
@@ -3,26 +3,27 @@
  *     See COPYRIGHT in top-level directory
  */
 
-#include "mpi.h"
-#include <stdio.h>
 #include "mpitest.h"
+
+#ifdef MULTI_TESTS
+#define run attr_attrorder
+int run(const char *arg);
+#endif
 
 /*
 static char MTestDescrip[] = "Test creating and inserting attributes in \
 different orders to ensure that the list management code handles all cases.";
 */
 
-int checkAttrs(MPI_Comm comm, int n, int key[], int attrval[]);
-int checkNoAttrs(MPI_Comm comm, int n, int key[]);
+static int checkAttrs(MPI_Comm comm, int n, int key[], int attrval[]);
+static int checkNoAttrs(MPI_Comm comm, int n, int key[]);
 
-int main(int argc, char *argv[])
+int run(const char *arg)
 {
     int errs = 0;
     int key[3], attrval[3];
     int i;
     MPI_Comm comm;
-
-    MTest_Init(&argc, &argv);
 
     {
         comm = MPI_COMM_WORLD;
@@ -76,11 +77,10 @@ int main(int argc, char *argv[])
         }
     }
 
-    MTest_Finalize(errs);
-    return MTestReturnValue(errs);
+    return errs;
 }
 
-int checkAttrs(MPI_Comm comm, int n, int key[], int attrval[])
+static int checkAttrs(MPI_Comm comm, int n, int key[], int attrval[])
 {
     int errs = 0;
     int i, flag, *val_p;
@@ -99,7 +99,7 @@ int checkAttrs(MPI_Comm comm, int n, int key[], int attrval[])
     return errs;
 }
 
-int checkNoAttrs(MPI_Comm comm, int n, int key[])
+static int checkNoAttrs(MPI_Comm comm, int n, int key[])
 {
     int errs = 0;
     int i, flag, *val_p;
