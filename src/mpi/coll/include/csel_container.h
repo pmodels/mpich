c179cc9ce5e235f066b7fda2042b6e3c44ca398a
Taru Doodi <taru.doodi@intel.com>
2022-01-11 13:53:45 -0600

    coll: Add CVARs for blocking Alltoall high radix brucks

--- src/mpi/coll/include/csel_container.h
+++ src/mpi/coll/include/csel_container.h
@@ -28,6 +28,7 @@ typedef enum {
     MPII_CSEL_CONTAINER_TYPE__ALGORITHM__MPIR_Allreduce_inter_reduce_exchange_bcast,
     MPII_CSEL_CONTAINER_TYPE__ALGORITHM__MPIR_Allreduce_allcomm_nb,
     MPII_CSEL_CONTAINER_TYPE__ALGORITHM__MPIR_Alltoall_intra_brucks,
+    MPII_CSEL_CONTAINER_TYPE__ALGORITHM__MPIR_Alltoall_intra_k_brucks,
     MPII_CSEL_CONTAINER_TYPE__ALGORITHM__MPIR_Alltoall_intra_pairwise,
     MPII_CSEL_CONTAINER_TYPE__ALGORITHM__MPIR_Alltoall_intra_pairwise_sendrecv_replace,
     MPII_CSEL_CONTAINER_TYPE__ALGORITHM__MPIR_Alltoall_intra_scattered,
@@ -295,6 +296,11 @@ typedef struct {
                 int recv_pre_posted;
             } intra_pipelined_tree;
         } bcast;
+        struct {
+            struct {
+                int k;
+            } intra_k_brucks;
+        } alltoall;
         struct {
             struct {
                 int k;
