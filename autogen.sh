8d805f9fb8f6050d2b69c0db13595927a4b2a697
Hui Zhou <hzhou321@anl.gov>
2022-01-23 09:39:45 -0600

    autogen: in quick mode generate single c binding source
    This will accelerate the compilation.

--- autogen.sh
+++ autogen.sh
@@ -530,7 +530,11 @@ fn_gen_coll() {
 fn_gen_binding_c() {
     set_PYTHON
     echo_n "generating MPI C functions..."
-    $PYTHON maint/gen_binding_c.py
+    if test "$do_quick" = "no" ; then
+        $PYTHON maint/gen_binding_c.py
+    else
+        $PYTHON maint/gen_binding_c.py -single-source
+    fi
     echo "done"
 }
 
