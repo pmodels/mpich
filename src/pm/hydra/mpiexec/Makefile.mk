5d4c2b76fc12fda0e2f09cc35b0d74ce656a1e86
Hui Zhou <hzhou321@anl.gov>
2022-02-06 15:22:50 -0600

    hydra: move main hydra source into mpiexec folder
    Rather than searching for the main hydra code, move the main code into
    mpiexec folder and flatten its directory layout.
    
    There was a --with-hydr-ui configure option that were designed to build
    different versions of mpiexec. This design seems never panned out.
    Remove the option for now.

--- src/pm/hydra/mpiexec/Makefile.mk
+++ src/pm/hydra/mpiexec/Makefile.mk
@@ -3,13 +3,14 @@
 ##     See COPYRIGHT in top-level directory
 ##
 
-AM_CPPFLAGS += -I$(top_srcdir)/ui/utils -DHYDRA_CONF_FILE=\"@sysconfdir@/mpiexec.hydra.conf\"
-
-noinst_HEADERS += ui/mpich/mpiexec.h
-
 bin_PROGRAMS += mpiexec.hydra
 
-mpiexec_hydra_SOURCES = ui/mpich/mpiexec.c ui/mpich/utils.c
+mpiexec_hydra_SOURCES = \
+    mpiexec/mpiexec.c \
+    mpiexec/utils.c \
+    mpiexec/uiu.c
+
+mpiexec_hydra_CPPFLAGS = $(AM_CPPFLAGS) -I$(top_srcdir)/mpiexec -DHYDRA_CONF_FILE=\"@sysconfdir@/mpiexec.hydra.conf\"
 mpiexec_hydra_LDFLAGS = $(external_ldflags) -L$(top_builddir)
 mpiexec_hydra_LDADD = -lpm -lhydra $(external_libs)
 mpiexec_hydra_DEPENDENCIES = libpm.la libhydra.la
