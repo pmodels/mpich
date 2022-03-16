b045f267fa3afe718cacf1484fdca37c6c1febfd
Hui Zhou <hzhou321@anl.gov>
2022-03-15 22:24:30 -0500

    ch4/posix: workaround for inter-process mutex on FreeBSD
    On FreeBSD (at lease for ver 12.2), destroying the mutex and recreate
    the mutex, the new mutex will not work for inter-process. As workaround,
    delay destroying window mutex until finalize.
    
    This applies the same fix as the one we did for ch3.

--- src/mpid/ch4/shm/posix/posix_impl.h
+++ src/mpid/ch4/shm/posix/posix_impl.h
@@ -14,6 +14,8 @@
 #include "posix_eager_impl.h"
 #include "posix_progress.h"
 
+void MPIDI_POSIX_delay_shm_mutex_destroy(int rank, MPL_proc_mutex_t * shm_mutex_ptr);
+
 MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_get_vsi(int flag, MPIR_Comm * comm_ptr,
                                                  int src_rank, int dst_rank, int tag)
 {
