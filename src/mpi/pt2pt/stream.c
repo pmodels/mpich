6ae4a0aca6936a38968e57e9e006c622565d4477
Hui Zhou <hzhou321@anl.gov>
2022-03-21 22:31:23 -0500

    pt2pt: use MPIR_Typerep_pack_stream in stream.c
    In the stream callback function we can't use gpu APIs reliably, use
    MPIR_Typerep_pack_stream and MPIR_Typerep_unpack_stream so the callback
    function only access CPU communications.

--- src/mpi/pt2pt/stream.c
+++ src/mpi/pt2pt/stream.c
@@ -7,12 +7,12 @@
 
 /* ---- Send stream ---- */
 struct send_data {
-    const void *buf;
-    MPI_Aint count;
-    MPI_Datatype datatype;
     int dest;
     int tag;
     MPIR_Comm *comm_ptr;
+    void *host_buf;
+    MPI_Aint data_sz;
+    MPI_Aint actual_pack_bytes;
 };
 
 static void send_stream_cb(void *data)
@@ -21,7 +21,9 @@ static void send_stream_cb(void *data)
     MPIR_Request *request_ptr = NULL;
 
     struct send_data *p = data;
-    mpi_errno = MPID_Send(p->buf, p->count, p->datatype, p->dest, p->tag, p->comm_ptr,
+    assert(p->actual_pack_bytes == p->data_sz);
+
+    mpi_errno = MPID_Send(p->host_buf, p->data_sz, MPI_BYTE, p->dest, p->tag, p->comm_ptr,
                           MPIR_CONTEXT_INTRA_PT2PT, &request_ptr);
     assert(mpi_errno == MPI_SUCCESS);
     assert(request_ptr != NULL);
@@ -31,6 +33,7 @@ static void send_stream_cb(void *data)
 
     MPIR_Request_free(request_ptr);
 
+    MPIR_gpu_free_host(p->host_buf);
     MPL_free(data);
 }
 
@@ -43,9 +46,16 @@ int MPIR_Send_enqueue_impl(const void *buf, MPI_Aint count, MPI_Datatype datatyp
     p = MPL_malloc(sizeof(struct send_data), MPL_MEM_OTHER);
     MPIR_ERR_CHKANDJUMP(!p, mpi_errno, MPI_ERR_OTHER, "**nomem");
 
-    p->buf = buf;
-    p->count = count;
-    p->datatype = datatype;
+    MPI_Aint dt_size;
+    MPIR_Datatype_get_size_macro(datatype, dt_size);
+    p->data_sz = dt_size * count;
+
+    MPIR_gpu_malloc_host(&p->host_buf, p->data_sz);
+
+    mpi_errno = MPIR_Typerep_pack_stream(buf, count, datatype, 0, p->host_buf, p->data_sz,
+                                         &p->actual_pack_bytes, stream);
+    MPIR_ERR_CHECK(mpi_errno);
+
     p->dest = dest;
     p->tag = tag;
     p->comm_ptr = comm_ptr;
@@ -60,13 +70,13 @@ int MPIR_Send_enqueue_impl(const void *buf, MPI_Aint count, MPI_Datatype datatyp
 
 /* ---- Recv stream ---- */
 struct recv_data {
-    void *buf;
-    MPI_Aint count;
-    MPI_Datatype datatype;
     int source;
     int tag;
     MPIR_Comm *comm_ptr;
     MPI_Status *status;
+    void *host_buf;
+    MPI_Aint data_sz;
+    MPI_Aint actual_unpack_bytes;
 };
 
 static void recv_stream_cb(void *data)
@@ -75,7 +85,7 @@ static void recv_stream_cb(void *data)
     MPIR_Request *request_ptr = NULL;
 
     struct recv_data *p = data;
-    mpi_errno = MPID_Recv(p->buf, p->count, p->datatype, p->source, p->tag, p->comm_ptr,
+    mpi_errno = MPID_Recv(p->host_buf, p->data_sz, MPI_BYTE, p->source, p->tag, p->comm_ptr,
                           MPIR_CONTEXT_INTRA_PT2PT, p->status, &request_ptr);
     assert(mpi_errno == MPI_SUCCESS);
     assert(request_ptr != NULL);
@@ -85,7 +95,14 @@ static void recv_stream_cb(void *data)
 
     MPIR_Request_extract_status(request_ptr, p->status);
     MPIR_Request_free(request_ptr);
+}
 
+static void recv_stream_cleanup_cb(void *data)
+{
+    struct recv_data *p = data;
+    assert(p->actual_unpack_bytes == p->data_sz);
+
+    MPIR_gpu_free_host(p->host_buf);
     MPL_free(data);
 }
 
@@ -99,15 +116,24 @@ int MPIR_Recv_enqueue_impl(void *buf, MPI_Aint count, MPI_Datatype datatype,
     p = MPL_malloc(sizeof(struct recv_data), MPL_MEM_OTHER);
     MPIR_ERR_CHKANDJUMP(!p, mpi_errno, MPI_ERR_OTHER, "**nomem");
 
-    p->buf = buf;
-    p->count = count;
-    p->datatype = datatype;
+    MPI_Aint dt_size;
+    MPIR_Datatype_get_size_macro(datatype, dt_size);
+    p->data_sz = dt_size * count;
+
+    MPIR_gpu_malloc_host(&p->host_buf, p->data_sz);
+
     p->source = source;
     p->tag = tag;
     p->comm_ptr = comm_ptr;
     p->status = status;
 
     MPL_gpu_launch_hostfn(stream, recv_stream_cb, p);
+
+    mpi_errno = MPIR_Typerep_unpack_stream(p->host_buf, p->data_sz, buf, count, datatype, 0,
+                                           &p->actual_unpack_bytes, stream);
+    MPIR_ERR_CHECK(mpi_errno);
+
+    MPL_gpu_launch_hostfn(stream, recv_stream_cleanup_cb, p);
 
   fn_exit:
     return mpi_errno;
