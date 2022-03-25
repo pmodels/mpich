5bfa024f11b3934bcb2f7111a20792cfde2900b8
Hui Zhou <hzhou321@anl.gov>
2022-03-24 22:38:29 -0500

    mpl: add MPL_gpu_stream_t
    As a wrapper for cudaStream_t.

--- src/mpl/include/mpl_gpu_ze.h
+++ src/mpl/include/mpl_gpu_ze.h
@@ -16,6 +16,8 @@ typedef struct {
 typedef ze_ipc_mem_handle_t MPL_gpu_ipc_mem_handle_t;
 typedef ze_device_handle_t MPL_gpu_device_handle_t;
 typedef ze_alloc_attr_t MPL_gpu_device_attr;
+/* FIXME: implement ze stream */
+typedef int MPL_gpu_stream_t;
 #define MPL_GPU_DEVICE_INVALID NULL
 
 #endif /* ifndef MPL_GPU_ZE_H_INCLUDED */
