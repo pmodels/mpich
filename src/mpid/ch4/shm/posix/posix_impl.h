7b3a9b98cfd959855c51bb8caa48216f63f7a9a0
Hui Zhou <hzhou321@anl.gov>
2022-04-18 09:35:13 -0500

    ch4: modify THREAD_CS macros to skip explicit vci
    If the vci is explicit vci, it is always called from a serial context
    and the critical section can be skipped.

--- src/mpid/ch4/shm/posix/posix_impl.h
+++ src/mpid/ch4/shm/posix/posix_impl.h
@@ -14,6 +14,20 @@
 #include "posix_eager_impl.h"
 #include "posix_progress.h"
 
+#define MPIDI_POSIX_THREAD_CS_ENTER_VCI(vci) \
+    do { \
+        if (!MPIDI_VCI_IS_EXPLICIT(vci)) { \
+            MPID_THREAD_CS_ENTER(VCI, MPIDI_VCI(vci).lock); \
+        } \
+    } while (0)
+
+#define MPIDI_POSIX_THREAD_CS_EXIT_VCI(vci) \
+    do { \
+        if (!MPIDI_VCI_IS_EXPLICIT(vci)) { \
+            MPID_THREAD_CS_EXIT(VCI, MPIDI_VCI(vci).lock); \
+        } \
+    } while (0)
+
 void MPIDI_POSIX_delay_shm_mutex_destroy(int rank, MPL_proc_mutex_t * shm_mutex_ptr);
 
 #endif /* POSIX_IMPL_H_INCLUDED */
