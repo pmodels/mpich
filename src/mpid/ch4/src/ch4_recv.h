0b4b9d09e25f6e56126d9686e87c251966fdc62e
Hui Zhou <hzhou321@anl.gov>
2022-05-26 20:13:47 -0500

    ch4: enter CS at ch4 layer for anysource receive
    Ch4 anysource receive need issue two separate request for both shm and
    netmod. It is important to prevent shmmem progress for the entire
    process or inconsistency may arrise.
    
    With the previous code, there is a chance another thread. e.g. the async
    thread, may complete the shmmem receive before netmod receive is posted,
    and the posix progress may try to cancel a non existent netmod
    anysrc_partner.

--- src/mpid/ch4/src/ch4_recv.h
+++ src/mpid/ch4/src/ch4_recv.h
@@ -15,20 +15,24 @@ MPL_STATIC_INLINE_PREFIX int anysource_irecv(void *buf, MPI_Aint count, MPI_Data
                                              MPIR_Request ** request)
 {
     int mpi_errno = MPI_SUCCESS;
-    int need_unlock = 0;
+
+    /* Need critical section to prevent shmmem progress in-between
+     * 1. Enter VCI lock and pre-allocate request for shm receive.
+     * 2. MPIDI_SHM_mpi_irecv with pre-allocated request
+     * 3. MPIDI_NM_mpi_irecv need use recursive locking in case it share the shm vci lock
+     */
+#if MPICH_THREAD_GRANULARITY == MPICH_THREAD_GRANULARITY__VCI
+    int vsi;
+    MPIDI_POSIX_RECV_VSI(vsi);
+    MPID_THREAD_CS_ENTER(VCI, MPIDI_VCI(vsi).lock);
+
+    *request = MPIR_Request_create_from_pool(MPIR_REQUEST_KIND__RECV, vsi, 1);
+    MPIR_Assert(*request);
+#endif
 
     mpi_errno = MPIDI_SHM_mpi_irecv(buf, count, datatype, rank, tag, comm, attr, request);
     MPIR_ERR_CHECK(mpi_errno);
 
-    MPIR_Assert(*request);
-    /* 1. hold shm vci lock to prevent shm progress while we establish netmod request.
-     * 2. MPIDI_NM_mpi_irecv need use recursive locking in case it share the shm vci lock
-     */
-#if MPICH_THREAD_GRANULARITY == MPICH_THREAD_GRANULARITY__VCI
-    int shm_vci = MPIDI_Request_get_vci(*request);
-#endif
-    MPID_THREAD_CS_ENTER(VCI, MPIDI_VCI(shm_vci).lock);
-    need_unlock = 1;
     if (!MPIR_Request_is_complete(*request) && !MPIDIG_REQUEST_IN_PROGRESS(*request)) {
         MPIR_Request *nm_rreq = NULL;
         mpi_errno = MPIDI_NM_mpi_irecv(buf, count, datatype, rank, tag, comm, attr,
@@ -49,9 +53,9 @@ MPL_STATIC_INLINE_PREFIX int anysource_irecv(void *buf, MPI_Aint count, MPI_Data
         }
     }
   fn_exit:
-    if (need_unlock) {
-        MPID_THREAD_CS_EXIT(VCI, MPIDI_VCI(shm_vci).lock);
-    }
+#if MPICH_THREAD_GRANULARITY == MPICH_THREAD_GRANULARITY__VCI
+    MPID_THREAD_CS_EXIT(VCI, MPIDI_VCI(vsi).lock);
+#endif
     return mpi_errno;
   fn_fail:
     goto fn_exit;
