1be8dd16b2ea39a02f61ae43837a47f8efba6cda
Hui Zhou <hzhou321@anl.gov>
2022-08-06 10:39:24 -0500

    hydra/mpiexec: rename bcast_keyvals
    This is a common utility function, albeit it is only used in PMI-v1.

--- src/pm/hydra/mpiexec/pmiserv_pmi_v1.c
+++ src/pm/hydra/mpiexec/pmiserv_pmi_v1.c
@@ -10,67 +10,6 @@
 #include "pmiserv_pmi.h"
 #include "pmiserv_utils.h"
 
-static HYD_status bcast_keyvals(int fd, int pid)
-{
-    int keyval_count, arg_count, j;
-    struct HYD_pmcd_pmi_kvs_pair *run;
-    struct HYD_proxy *proxy, *tproxy;
-    struct HYD_pmcd_pmi_pg_scratch *pg_scratch;
-    HYD_status status = HYD_SUCCESS;
-
-    HYDU_FUNC_ENTER();
-
-    proxy = HYD_pmcd_pmi_find_proxy(fd);
-    HYDU_ASSERT(proxy, status);
-    pg_scratch = (struct HYD_pmcd_pmi_pg_scratch *) proxy->pg->pg_scratch;
-
-    /* find the number of keyvals */
-    keyval_count = 0;
-    for (run = pg_scratch->kvs->key_pair; run; run = run->next)
-        keyval_count++;
-
-    keyval_count -= pg_scratch->keyval_dist_count;
-
-    struct PMIU_cmd pmi;
-    if (keyval_count) {
-        PMIU_cmd_init_static(&pmi, 1, "keyval_cache");
-        arg_count = 1;
-        for (run = pg_scratch->kvs->key_pair, j = 0; run; run = run->next, j++) {
-            if (j < pg_scratch->keyval_dist_count)
-                continue;
-
-            PMIU_cmd_add_str(&pmi, run->key, run->val);
-
-            arg_count++;
-            if (arg_count >= MAX_PMI_ARGS) {
-                pg_scratch->keyval_dist_count += (arg_count - 1);
-                for (tproxy = proxy->pg->proxy_list; tproxy; tproxy = tproxy->next) {
-                    status = HYD_pmiserv_pmi_reply(tproxy->control_fd, pid, &pmi);
-                    HYDU_ERR_POP(status, "error writing PMI line\n");
-                }
-
-                PMIU_cmd_init_static(&pmi, 1, "keyval_cache");
-                arg_count = 1;
-            }
-        }
-
-        if (arg_count > 1) {
-            pg_scratch->keyval_dist_count += (arg_count - 1);
-            for (tproxy = proxy->pg->proxy_list; tproxy; tproxy = tproxy->next) {
-                status = HYD_pmiserv_pmi_reply(tproxy->control_fd, pid, &pmi);
-                HYDU_ERR_POP(status, "error writing PMI line\n");
-            }
-        }
-    }
-
-  fn_exit:
-    HYDU_FUNC_EXIT();
-    return status;
-
-  fn_fail:
-    goto fn_exit;
-}
-
 static HYD_status fn_barrier_in(int fd, int pid, int pgid, struct PMIU_cmd *pmi)
 {
     struct HYD_proxy *proxy, *tproxy;
@@ -85,7 +24,7 @@ static HYD_status fn_barrier_in(int fd, int pid, int pgid, struct PMIU_cmd *pmi)
     if (proxy->pg->barrier_count == proxy->pg->proxy_count) {
         proxy->pg->barrier_count = 0;
 
-        bcast_keyvals(fd, pid);
+        HYD_pmiserv_bcast_keyvals(fd, pid);
 
         struct PMIU_cmd pmi_response;
         PMIU_cmd_init_static(&pmi_response, 1, "barrier_out");
@@ -479,7 +418,7 @@ static HYD_status fn_spawn(int fd, int pid, int pgid, struct PMIU_cmd *pmi)
 
     /* Cache the pre-initialized keyvals on the new proxies */
     if (preput_num)
-        bcast_keyvals(fd, pid);
+        HYD_pmiserv_bcast_keyvals(fd, pid);
 
   fn_exit:
     HYD_STRING_STASH_FREE(proxy_stash);
