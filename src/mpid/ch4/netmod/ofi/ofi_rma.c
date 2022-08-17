d0b71ff6877f9c98221697b261418698a0bfb4e6
Hui Zhou <hzhou321@anl.gov>
2022-08-17 14:29:13 -0500

    ch4/ADI: use MPI_Aint for count parameters
    To support large count APIs, we need use MPI_Aint for count parameters
    throughout.
    
    Fixed ADI:
        MPID_Get
        MPID_Put
        MPID_Accumulate
        MPID_Get_accumulate
        MPID_Rget
        MPID_Rput
        MPID_Raccumulate
        MPID_Rget_accumulate

--- src/mpid/ch4/netmod/ofi/ofi_rma.c
+++ src/mpid/ch4/netmod/ofi/ofi_rma.c
@@ -51,9 +51,9 @@ void MPIDI_OFI_complete_chunks(MPIDI_OFI_win_request_t * winreq)
     winreq->chunks = NULL;
 }
 
-int MPIDI_OFI_nopack_putget(const void *origin_addr, int origin_count,
+int MPIDI_OFI_nopack_putget(const void *origin_addr, MPI_Aint origin_count,
                             MPI_Datatype origin_datatype, int target_rank,
-                            int target_count, MPI_Datatype target_datatype,
+                            MPI_Aint target_count, MPI_Datatype target_datatype,
                             MPIDI_OFI_target_mr_t target_mr, MPIR_Win * win,
                             MPIDI_av_entry_t * addr, int rma_type, MPIR_Request ** sigreq)
 {
@@ -351,9 +351,9 @@ static int issue_packed_get(MPIR_Win * win, MPIDI_OFI_win_request_t * req)
     goto fn_exit;
 }
 
-int MPIDI_OFI_pack_put(const void *origin_addr, int origin_count,
+int MPIDI_OFI_pack_put(const void *origin_addr, MPI_Aint origin_count,
                        MPI_Datatype origin_datatype, int target_rank,
-                       int target_count, MPI_Datatype target_datatype,
+                       MPI_Aint target_count, MPI_Datatype target_datatype,
                        MPIDI_OFI_target_mr_t target_mr, MPIR_Win * win,
                        MPIDI_av_entry_t * addr, MPIR_Request ** sigreq)
 {
@@ -412,9 +412,9 @@ int MPIDI_OFI_pack_put(const void *origin_addr, int origin_count,
     goto fn_exit;
 }
 
-int MPIDI_OFI_pack_get(void *origin_addr, int origin_count,
+int MPIDI_OFI_pack_get(void *origin_addr, MPI_Aint origin_count,
                        MPI_Datatype origin_datatype, int target_rank,
-                       int target_count, MPI_Datatype target_datatype,
+                       MPI_Aint target_count, MPI_Datatype target_datatype,
                        MPIDI_OFI_target_mr_t target_mr, MPIR_Win * win,
                        MPIDI_av_entry_t * addr, MPIR_Request ** sigreq)
 {
