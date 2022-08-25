82832d54a77396311558940e8e00a618fc67e291
Ken Raffenetti <raffenet@mcs.anl.gov>
2022-08-24 21:09:48 -0500

    mpir/pmi: Add publish/lookup functions
    These were previously defined in src/nameserv/pmi, which still
    exists. Just move the pmi usage to mpir_pmi.c to be consistent.

--- src/nameserv/pmi/pmi_nameserv.c
+++ src/nameserv/pmi/pmi_nameserv.c
@@ -32,75 +32,27 @@ int MPID_NS_Create(const MPIR_Info * info_ptr, MPID_NS_Handle * handle_ptr)
 int MPID_NS_Publish(MPID_NS_Handle handle, const MPIR_Info * info_ptr,
                     const char service_name[], const char port[])
 {
-    int mpi_errno = MPI_SUCCESS;
-    int rc;
     MPL_UNREFERENCED_ARG(info_ptr);
     MPL_UNREFERENCED_ARG(handle);
 
-#ifdef USE_PMI2_API
-    /* release the global CS for PMI calls */
-    MPID_THREAD_CS_EXIT(GLOBAL, MPIR_THREAD_GLOBAL_ALLFUNC_MUTEX);
-    rc = PMI2_Nameserv_publish(service_name, NULL, port);
-    MPID_THREAD_CS_ENTER(GLOBAL, MPIR_THREAD_GLOBAL_ALLFUNC_MUTEX);
-#elif defined(USE_PMIX_API)
-    MPIR_Assert(0);
-#else
-    rc = PMI_Publish_name(service_name, port);
-#endif
-    MPIR_ERR_CHKANDJUMP1(rc, mpi_errno, MPI_ERR_NAME, "**namepubnotpub", "**namepubnotpub %s",
-                         service_name);
-
-  fn_fail:
-    return mpi_errno;
+    return MPIR_pmi_publish(service_name, port);
 }
 
 int MPID_NS_Lookup(MPID_NS_Handle handle, const MPIR_Info * info_ptr,
                    const char service_name[], char port[])
 {
-    int mpi_errno = MPI_SUCCESS;
-    int rc;
     MPL_UNREFERENCED_ARG(info_ptr);
     MPL_UNREFERENCED_ARG(handle);
 
-#ifdef USE_PMI2_API
-    /* release the global CS for PMI calls */
-    MPID_THREAD_CS_EXIT(GLOBAL, MPIR_THREAD_GLOBAL_ALLFUNC_MUTEX);
-    rc = PMI2_Nameserv_lookup(service_name, NULL, port, MPI_MAX_PORT_NAME);
-    MPID_THREAD_CS_ENTER(GLOBAL, MPIR_THREAD_GLOBAL_ALLFUNC_MUTEX);
-#elif defined(USE_PMIX_API)
-    MPIR_Assert(0);
-#else
-    rc = PMI_Lookup_name(service_name, port);
-#endif
-    MPIR_ERR_CHKANDJUMP1(rc, mpi_errno, MPI_ERR_NAME, "**namepubnotfound", "**namepubnotfound %s",
-                         service_name);
-
-  fn_fail:
-    return mpi_errno;
+    return MPIR_pmi_lookup(service_name, port);
 }
 
 int MPID_NS_Unpublish(MPID_NS_Handle handle, const MPIR_Info * info_ptr, const char service_name[])
 {
-    int mpi_errno = MPI_SUCCESS;
-    int rc;
     MPL_UNREFERENCED_ARG(info_ptr);
     MPL_UNREFERENCED_ARG(handle);
 
-#ifdef USE_PMI2_API
-    /* release the global CS for PMI calls */
-    MPID_THREAD_CS_EXIT(GLOBAL, MPIR_THREAD_GLOBAL_ALLFUNC_MUTEX);
-    rc = PMI2_Nameserv_unpublish(service_name, NULL);
-    MPID_THREAD_CS_ENTER(GLOBAL, MPIR_THREAD_GLOBAL_ALLFUNC_MUTEX);
-#elif defined(USE_PMIX_API)
-    MPIR_Assert(0);
-#else
-    rc = PMI_Unpublish_name(service_name);
-#endif
-    MPIR_ERR_CHKANDJUMP1(rc, mpi_errno, MPI_ERR_SERVICE, "**namepubnotunpub",
-                         "**namepubnotunpub %s", service_name);
-
-  fn_fail:
-    return mpi_errno;
+    return MPIR_pmi_unpublish(service_name);
 }
 
 int MPID_NS_Free(MPID_NS_Handle * handle_ptr)
