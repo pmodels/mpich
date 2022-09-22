c09fa15caf0399a942cfea4663f2bf11b9c3a634
Ken Raffenetti <raffenet@mcs.anl.gov>
2022-09-22 11:40:18 -0500

    test: Add test for detecting unmatched messages
    During MPI_COMM_FREE, if a communicator has unmatched messages in the
    queue, the implementation can detect and notify the user of potential
    programming errors.

---
+++ test/mpi/errors/comm/unmatched.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) by Argonne National Laboratory
+ *     See COPYRIGHT in top-level directory
+ */
+
+#include "mpitest.h"
+
+int main(void)
+{
+    int mpi_errno = MPI_SUCCESS;
+    int errs = 0;
+
+    MTest_Init(NULL, NULL);
+
+    MPI_Comm comm;
+    MPI_Comm_dup(MPI_COMM_WORLD, &comm);
+    MPI_Comm_set_errhandler(comm, MPI_ERRORS_RETURN);
+    /* The implementation may raise an error on the default handler
+     * since it will happen during MPI_Comm_free. The default,
+     * as of MPI-4.0, is the error handler for MPI_COMM_SELF.
+     * Prior to MPI-4.0, it was MPI_COMM_WORLD. */
+    MPI_Comm_set_errhandler(MPI_COMM_SELF, MPI_ERRORS_RETURN);
+    MPI_Comm_set_errhandler(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
+
+    int rank;
+    MPI_Comm_rank(comm, &rank);
+
+    if (rank == 0) {
+        /* if an implementation does not use eager
+         * protocol, this send may hang */
+        MPI_Send(NULL, 0, MPI_INT, 1, 0, comm);
+    }
+
+    MPI_Barrier(comm);
+
+    mpi_errno = MPI_Comm_free(&comm);
+    /* unmatched message should be detected at rank 1 */
+    if (rank == 1 && mpi_errno == MPI_SUCCESS) {
+        errs++;
+    }
+
+    MTest_Finalize(errs);
+
+    return MTestReturnValue(errs);
+}
