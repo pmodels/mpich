708057241686d958187ee1c3ba59fdd17eac8eef
Ken Raffenetti <raffenet@mcs.anl.gov>
2023-01-03 09:21:25 -0600

    maint: Require automake >= 1.16.5 in release.pl
    This will ensure tarballs are generated with up-to-date config.guess
    files for better cross compilation support. Fixes pmodels/mpich#4987.

--- maint/release.pl
+++ maint/release.pl
@@ -160,9 +160,11 @@ if (!$has_doctext || !$has_latex) {
 print("\n");
 
 ## NOTE: Different autotools versions may result in accidental ABI chanages.
-## For flexibility, we no longer enforce the check for specific versions.
-## Always double check using the ABI compatibility tool before the final release.
-
+## For flexibility, we no longer enforce the check for specific versions of
+## autoconf and libtool. We do require a minimum version of automake so tarballs
+## contain up-to-date config.guess files. Always double check using the ABI
+## compatibility tool before the final release.
+check_autotools_version("automake", "1.16.5")
 
 my $tdir = tempdir(CLEANUP => 1);
 my $local_git_clone = "${tdir}/mpich-clone";
