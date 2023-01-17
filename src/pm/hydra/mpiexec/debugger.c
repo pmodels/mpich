pmodels/mpich

commit 2906ecf583d8321bd72ea37ef89e5c76e34af8c5

hydra/mpiexec: iterate proxy_list as array
Remove the list mechanism.

src/pm/hydra/mpiexec/debugger.c
@@ -25,7 +25,6 @@ int MPIR_Breakpoint(void)
 
 HYD_status HYDT_dbg_setup_procdesc(struct HYD_pg * pg)
 {
-    struct HYD_proxy *proxy;
     struct HYD_exec *exec;
     int i, j, k, np, round;
     HYD_status status = HYD_SUCCESS;
@@ -43,13 +42,12 @@ HYD_status HYDT_dbg_setup_procdesc(struct HYD_pg * pg)
      * proxy.  When we run out of proxies, we go back to the first
      * proxy and find the next set of contiguous ranks on that
      * proxy. */
-    for (proxy = pg->proxy_list, i = 0;; proxy = proxy->next) {
+
+    i = 0;
+    for (int i_proxy = 0;; i_proxy = (i_proxy + 1) % pg->proxy_count) {
+        struct HYD_proxy *proxy = &pg->proxy_list[i_proxy];
         j = 0;
         k = 0;
-        if (!proxy) {
-            proxy = pg->proxy_list;
-            round++;
-        }
         for (exec = proxy->exec_list; exec; exec = exec->next) {
             for (np = 0; np < exec->proc_count; np++) {
                 if (k + np >= ((round + 1) * proxy->node->core_count))

src/pm/hydra/lib/utils/alloc.c
@@ -128,7 +128,7 @@ static HYD_status alloc_proxy_list(struct HYD_node *node_list, int pgid,
         proxy->node = node;
         proxy->pgid = pgid;
 
-        proxy->proxy_id = -1;   /* Do we need this? */
+        proxy->proxy_id = i;
         proxy->exec_launch_info = NULL;
 
         proxy->proxy_process_count = 0;
@@ -360,9 +360,11 @@ HYD_status HYDU_create_proxy_list(int count, struct HYD_exec *exec_list, struct 
 
     /* If all proxies have as many filler processes as the number of
      * cores, we can reduce those filler processes */
-    if (dummy_fillers)
-        for (struct HYD_proxy * proxy = proxy_list; proxy; proxy = proxy->next)
-            proxy->filler_processes -= proxy->node->core_count;
+    if (dummy_fillers) {
+        for (int i = 0; i < proxy_count; i++) {
+            proxy_list[i].filler_processes -= proxy_list[i].node->core_count;
+        }
+    }
 
     /* Proxy list is created; add the executables to the proxy list */
     if (proxy_count == 1) {
@@ -375,13 +377,17 @@ HYD_status HYDU_create_proxy_list(int count, struct HYD_exec *exec_list, struct 
     } else {
         struct HYD_exec *exec = exec_list;
 
-        int filler_round = 1;
-        struct HYD_proxy *proxy;
-        for (proxy = proxy_list; proxy && proxy->filler_processes == 0; proxy = proxy->next);
-        if (proxy == NULL) {
-            filler_round = 0;
-            proxy = proxy_list;
+        int i_proxy = 0;
+        int filler_round = 0;
+        for (int i = 0; i < proxy_count; i++) {
+            if (proxy_list[i].filler_processes > 0) {
+                i_proxy = i;
+                filler_round = 1;
+                break;
+            }
         }
+        struct HYD_proxy *proxy;
+        proxy = &proxy_list[i_proxy];
 
         int exec_rem_procs = exec->proc_count;
         int proxy_rem_cores = filler_round ? proxy->filler_processes : proxy->node->core_count;
@@ -397,12 +403,12 @@ HYD_status HYDU_create_proxy_list(int count, struct HYD_exec *exec_list, struct 
             HYDU_ASSERT(exec_rem_procs, status);
 
             while (proxy_rem_cores == 0) {
-                proxy = proxy->next;
-
-                if (proxy == NULL) {
+                i_proxy++;
+                if (i_proxy == proxy_count) {
+                    i_proxy = 0;
                     filler_round = 0;
-                    proxy = proxy_list;
                 }
+                proxy = &proxy_list[i_proxy];
 
                 proxy_rem_cores = filler_round ? proxy->filler_processes : proxy->node->core_count;
             }
@@ -419,13 +425,7 @@ HYD_status HYDU_create_proxy_list(int count, struct HYD_exec *exec_list, struct 
         }
     }
 
-    int i = 0;
-    for (struct HYD_proxy * proxy = proxy_list; proxy; proxy = proxy->next) {
-        proxy->proxy_id = i;
-        i++;
-    }
-
-    *proxy_count_p = i;
+    *proxy_count_p = proxy_count;
     *proxy_list_p = proxy_list;
 
   fn_exit:

src/pm/hydra/mpiexec/mpiexec.c
@@ -23,7 +23,6 @@ static int get_exit_status(int pgid);
 
 int main(int argc, char **argv)
 {
-    struct HYD_proxy *proxy;
     struct HYD_exec *exec;
     struct HYD_node *node;
     int i, user_provided_host_list, global_core_count;
@@ -285,7 +284,8 @@ static int get_exit_status(int pgid)
     if (HYD_ui_mpich_info.print_all_exitcodes)
         HYDU_dump(stdout, "Exit codes: ");
     int exit_status = 0;
-    for (struct HYD_proxy * proxy = pg->proxy_list; proxy; proxy = proxy->next) {
+    for (int i = 0; i < pg->proxy_count; i++) {
+        struct HYD_proxy *proxy = &pg->proxy_list[i];
         if (proxy->exit_status == NULL) {
             /* We didn't receive the exit status for this proxy */
             continue;
@@ -294,14 +294,14 @@ static int get_exit_status(int pgid)
         if (HYD_ui_mpich_info.print_all_exitcodes)
             HYDU_dump_noprefix(stdout, "[%s] ", proxy->node->hostname);
 
-        for (int i = 0; i < proxy->proxy_process_count; i++) {
+        for (int j = 0; j < proxy->proxy_process_count; j++) {
             if (HYD_ui_mpich_info.print_all_exitcodes) {
-                HYDU_dump_noprefix(stdout, "%d", proxy->exit_status[i]);
-                if (i < proxy->proxy_process_count - 1)
+                HYDU_dump_noprefix(stdout, "%d", proxy->exit_status[j]);
+                if (j < proxy->proxy_process_count - 1)
                     HYDU_dump_noprefix(stdout, ",");
             }
 
-            exit_status |= proxy->exit_status[i];
+            exit_status |= proxy->exit_status[j];
         }
 
         if (HYD_ui_mpich_info.print_all_exitcodes)

src/pm/hydra/mpiexec/pmiserv_cb.c
@@ -86,7 +86,6 @@ static HYD_status handle_pmi_cmd(struct HYD_proxy *proxy, int pgid, int pid, cha
 
 static HYD_status cleanup_proxy(struct HYD_proxy *proxy)
 {
-    struct HYD_proxy *tproxy;
     struct HYD_pg *pg = PMISERV_pg_by_id(proxy->pgid);
     struct HYD_pmcd_pmi_pg_scratch *pg_scratch;
     HYD_status status = HYD_SUCCESS;
@@ -104,16 +103,20 @@ static HYD_status cleanup_proxy(struct HYD_proxy *proxy)
     }
 
     /* If there is an active proxy, don't clean up the PG */
-    for (tproxy = pg->proxy_list; tproxy; tproxy = tproxy->next)
-        if (tproxy->control_fd != HYD_FD_CLOSED)
+    for (int i = 0; i < pg->proxy_count; i++) {
+        if (pg->proxy_list[i].control_fd != HYD_FD_CLOSED) {
             goto fn_exit;
+        }
+    }
 
     pg->is_active = false;
 
     /* If there is no active proxy, ignore the proxies that haven't
      * connected back to us yet. */
-    for (tproxy = pg->proxy_list; tproxy; tproxy = tproxy->next)
-        tproxy->control_fd = HYD_FD_CLOSED;
+    /* HZ: seems redundant */
+    for (int i = 0; i < pg->proxy_count; i++) {
+        pg->proxy_list[i].control_fd = HYD_FD_CLOSED;
+    }
 
     /* All proxies in this process group have terminated */
     pg_scratch = (struct HYD_pmcd_pmi_pg_scratch *) pg->pg_scratch;
@@ -143,8 +146,9 @@ static HYD_status cleanup_proxy(struct HYD_proxy *proxy)
         HYDT_dbg_free_procdesc();
 
     /* Reset the node allocations for this PG */
-    for (tproxy = pg->proxy_list; tproxy; tproxy = tproxy->next)
-        tproxy->node->active_processes -= tproxy->proxy_process_count;
+    for (int i = 0; i < pg->proxy_count; i++) {
+        pg->proxy_list[i].node->active_processes -= pg->proxy_list[i].proxy_process_count;
+    }
 
   fn_exit:
     HYDU_FUNC_EXIT();
@@ -156,15 +160,14 @@ static HYD_status cleanup_proxy(struct HYD_proxy *proxy)
 
 HYD_status HYD_pmcd_pmiserv_cleanup_all_pgs(void)
 {
-    struct HYD_proxy *proxy;
     HYD_status status = HYD_SUCCESS;
 
     HYDU_FUNC_ENTER();
 
     for (int i = 0; i < PMISERV_pg_max_id(); i++) {
         struct HYD_pg *pg = PMISERV_pg_by_id(i);
-        for (proxy = pg->proxy_list; proxy; proxy = proxy->next) {
-            status = cleanup_proxy(proxy);
+        for (int j = 0; j < pg->proxy_count; j++) {
+            status = cleanup_proxy(&pg->proxy_list[j]);
             HYDU_ERR_POP(status, "unable to cleanup proxy\n");
         }
     }
@@ -201,8 +204,7 @@ HYD_status HYD_pmcd_pmiserv_send_signal(struct HYD_proxy *proxy, int signum)
 
 static HYD_status control_cb(int fd, HYD_event_t events, void *userp)
 {
-    int count, closed, i;
-    struct HYD_pg *pg;
+    int count, closed;
     struct HYD_pmcd_hdr hdr;
     struct HYD_proxy *proxy, *tproxy;
     struct HYD_pmcd_pmi_pg_scratch *pg_scratch;
@@ -240,9 +242,11 @@ static HYD_status control_cb(int fd, HYD_event_t events, void *userp)
         pg = PMISERV_pg_by_id(proxy->pgid);
 
         /* Check if all the PIDs have been received */
-        for (tproxy = pg->proxy_list; tproxy; tproxy = tproxy->next)
-            if (tproxy->pid == NULL)
+        for (int i = 0; i < pg->proxy_count; i++) {
+            if (pg->proxy_list[i].pid == NULL) {
                 goto fn_exit;
+            }
+        }
 
         /* Call the debugger initialization */
         status = HYDT_dbg_setup_procdesc(pg);
@@ -263,7 +267,7 @@ static HYD_status control_cb(int fd, HYD_event_t events, void *userp)
         /* If any of the processes was killed with a signal, cleanup
          * the remaining processes */
         if (HYD_server_info.user_global.auto_cleanup) {
-            for (i = 0; i < proxy->proxy_process_count; i++) {
+            for (int i = 0; i < proxy->proxy_process_count; i++) {
                 if (!WIFEXITED(proxy->exit_status[i])) {
                     int code = proxy->exit_status[i];
                     /* show the value passed to exit(), not (val<<8) */
@@ -406,7 +410,8 @@ static HYD_status control_cb(int fd, HYD_event_t events, void *userp)
 
             for (int i = 0; i < PMISERV_pg_max_id(); i++) {
                 struct HYD_pg *tmp_pg = PMISERV_pg_by_id(i);
-                for (tproxy = tmp_pg->proxy_list; tproxy; tproxy = tproxy->next) {
+                for (int j = 0; j < tmp_pg->proxy_count; j++) {
+                    tproxy = &tmp_pg->proxy_list[j];
                     if (tproxy->control_fd == HYD_FD_UNSET || tproxy->control_fd == HYD_FD_CLOSED)
                         continue;
 
@@ -487,13 +492,9 @@ HYD_status HYD_pmcd_pmiserv_proxy_init_cb(int fd, HYD_event_t events, void *user
     if (!pg)
         HYDU_ERR_SETANDJUMP(status, HYD_INTERNAL_ERROR, "could not find pg with ID %d\n", pgid);
 
-    /* Find the proxy */
-    for (proxy = pg->proxy_list; proxy; proxy = proxy->next) {
-        if (proxy->proxy_id == proxy_id)
-            break;
-    }
-    HYDU_ERR_CHKANDJUMP(status, proxy == NULL, HYD_INTERNAL_ERROR,
+    HYDU_ERR_CHKANDJUMP(status, !(proxy_id >= 0 && proxy_id < pg->proxy_count), HYD_INTERNAL_ERROR,
                         "cannot find proxy with ID %d\n", proxy_id);
+    proxy = &pg->proxy_list[proxy_id];
 
     /* This will be the control socket for this proxy */
     proxy->control_fd = fd;

src/pm/hydra/mpiexec/pmiserv_misc.c
@@ -12,7 +12,6 @@
 
 HYD_status HYD_pmiserv_barrier(struct HYD_proxy *proxy, int pid, int pgid, struct PMIU_cmd *pmi)
 {
-    struct HYD_proxy *tproxy;
     HYD_status status = HYD_SUCCESS;
 
     HYDU_FUNC_ENTER();
@@ -28,8 +27,8 @@ HYD_status HYD_pmiserv_barrier(struct HYD_proxy *proxy, int pid, int pgid, struc
 
         struct PMIU_cmd pmi_response;
         PMIU_cmd_init_static(&pmi_response, 1, "barrier_out");
-        for (tproxy = pg->proxy_list; tproxy; tproxy = tproxy->next) {
-            status = HYD_pmiserv_pmi_reply(tproxy, pid, &pmi_response);
+        for (int i = 0; i < pg->proxy_count; i++) {
+            status = HYD_pmiserv_pmi_reply(&pg->proxy_list[i], pid, &pmi_response);
             HYDU_ERR_POP(status, "error writing PMI line\n");
         }
     }

src/pm/hydra/mpiexec/pmiserv_pmci.c
@@ -16,7 +16,6 @@ static HYD_status ui_cmd_cb(int fd, HYD_event_t events, void *userp)
 {
     struct HYD_cmd cmd;
     int count, closed;
-    struct HYD_proxy *proxy;
     HYD_status status = HYD_SUCCESS;
 
     HYDU_FUNC_ENTER();
@@ -57,8 +56,8 @@ static HYD_status ui_cmd_cb(int fd, HYD_event_t events, void *userp)
     } else if (cmd.type == HYD_SIGNAL) {
         for (int i = 0; i < PMISERV_pg_max_id(); i++) {
             struct HYD_pg *pg = PMISERV_pg_by_id(i);
-            for (proxy = pg->proxy_list; proxy; proxy = proxy->next) {
-                status = HYD_pmcd_pmiserv_send_signal(proxy, cmd.signum);
+            for (int j = 0; j < pg->proxy_count; j++) {
+                status = HYD_pmcd_pmiserv_send_signal(&pg->proxy_list[j], cmd.signum);
                 HYDU_ERR_POP(status, "unable to send signal downstream\n");
             }
         }
@@ -75,7 +74,6 @@ static HYD_status ui_cmd_cb(int fd, HYD_event_t events, void *userp)
 
 HYD_status HYD_pmci_launch_procs(void)
 {
-    struct HYD_proxy *proxy;
     struct HYD_string_stash proxy_stash;
     char *control_port = NULL;
     int node_count, i, *control_fd;
@@ -112,9 +110,7 @@ HYD_status HYD_pmci_launch_procs(void)
     status = HYD_pmcd_pmi_fill_in_exec_launch_info(pg);
     HYDU_ERR_POP(status, "unable to fill in executable arguments\n");
 
-    node_count = 0;
-    for (proxy = pg->proxy_list; proxy; proxy = proxy->next)
-        node_count++;
+    node_count = pg->proxy_count;
 
     HYDU_MALLOC_OR_JUMP(control_fd, int *, node_count * sizeof(int), status);
     for (i = 0; i < node_count; i++)
@@ -123,14 +119,15 @@ HYD_status HYD_pmci_launch_procs(void)
     status = HYDT_bsci_launch_procs(proxy_stash.strlist, pg->proxy_list, HYD_TRUE, control_fd);
     HYDU_ERR_POP(status, "launcher cannot launch processes\n");
 
-    for (i = 0, proxy = pg->proxy_list; proxy; proxy = proxy->next, i++)
+    for (i = 0; i < pg->proxy_count; i++) {
         if (control_fd[i] != HYD_FD_UNSET) {
-            proxy->control_fd = control_fd[i];
+            pg->proxy_list[i].control_fd = control_fd[i];
 
             status = HYDT_dmx_register_fd(1, &control_fd[i], HYD_POLLIN, (void *) (size_t) 0,
                                           HYD_pmcd_pmiserv_proxy_init_cb);
             HYDU_ERR_POP(status, "unable to register fd\n");
         }
+    }
 
     MPL_free(control_fd);
 

src/pm/hydra/mpiexec/pmiserv_pmi.c
@@ -59,8 +59,8 @@ HYD_status HYD_pmiserv_bcast_keyvals(struct HYD_proxy * proxy, int pid)
             arg_count++;
             if (arg_count >= MAX_PMI_ARGS) {
                 pg_scratch->keyval_dist_count += (arg_count - 1);
-                for (struct HYD_proxy * tproxy = pg->proxy_list; tproxy; tproxy = tproxy->next) {
-                    status = HYD_pmiserv_pmi_reply(tproxy, pid, &pmi);
+                for (int i = 0; i < pg->proxy_count; i++) {
+                    status = HYD_pmiserv_pmi_reply(&pg->proxy_list[i], pid, &pmi);
                     HYDU_ERR_POP(status, "error writing PMI line\n");
                 }
 
@@ -71,8 +71,8 @@ HYD_status HYD_pmiserv_bcast_keyvals(struct HYD_proxy * proxy, int pid)
 
         if (arg_count > 1) {
             pg_scratch->keyval_dist_count += (arg_count - 1);
-            for (struct HYD_proxy * tproxy = pg->proxy_list; tproxy; tproxy = tproxy->next) {
-                status = HYD_pmiserv_pmi_reply(tproxy, pid, &pmi);
+            for (int i = 0; i < pg->proxy_count; i++) {
+                status = HYD_pmiserv_pmi_reply(&pg->proxy_list[i], pid, &pmi);
                 HYDU_ERR_POP(status, "error writing PMI line\n");
             }
         }

src/pm/hydra/mpiexec/pmiserv_utils.c
@@ -145,7 +145,6 @@ static HYD_status pmi_process_mapping(struct HYD_pg *pg, char **process_mapping_
         struct block *next;
     } *blocklist_head, *blocklist_tail = NULL, *block, *nblock;
     struct HYD_node *node;
-    struct HYD_proxy *proxy;
     HYD_status status = HYD_SUCCESS;
 
     HYDU_FUNC_ENTER();
@@ -167,12 +166,14 @@ static HYD_status pmi_process_mapping(struct HYD_pg *pg, char **process_mapping_
     blocklist_head = NULL;
 
     filler_round = 1;
-    for (proxy = pg->proxy_list;; proxy = proxy->next) {
-        if (filler_round && proxy == NULL) {
-            proxy = pg->proxy_list;
+    for (int i_proxy = 0;; i_proxy++) {
+        if (filler_round && i_proxy == pg->proxy_count) {
+            i_proxy = 0;
             filler_round = 0;
-        } else if (proxy == NULL)
+        } else if (i_proxy == pg->proxy_count) {
             break;
+        }
+        struct HYD_proxy *proxy = &pg->proxy_list[i_proxy];
 
         if (filler_round && proxy->filler_processes == 0)
             continue;
@@ -280,14 +281,12 @@ static HYD_status add_env_to_exec_stash(struct HYD_string_stash *exec_stash, con
     goto fn_exit;
 }
 
-
 HYD_status HYD_pmcd_pmi_fill_in_exec_launch_info(struct HYD_pg *pg)
 {
     int inherited_env_count, user_env_count, system_env_count, exec_count;
-    int proxy_count, total_filler_processes, total_core_count;
+    int total_filler_processes, total_core_count;
     int pmi_id, *filler_pmi_ids = NULL, *nonfiller_pmi_ids = NULL;
     struct HYD_env *env;
-    struct HYD_proxy *proxy;
     struct HYD_exec *exec;
     struct HYD_pmcd_pmi_pg_scratch *pg_scratch;
     char *mapping = NULL, *map;
@@ -306,32 +305,30 @@ HYD_status HYD_pmcd_pmi_fill_in_exec_launch_info(struct HYD_pg *pg)
 
     /* Create the arguments list for each proxy */
     total_filler_processes = 0;
-    proxy_count = 0;
-    for (proxy = pg->proxy_list; proxy; proxy = proxy->next) {
-        total_filler_processes += proxy->filler_processes;
-        proxy_count++;
+    for (int i = 0; i < pg->proxy_count; i++) {
+        total_filler_processes += pg->proxy_list[i].filler_processes;
     }
 
     total_core_count = 0;
-    for (proxy = pg->proxy_list; proxy; proxy = proxy->next)
-        total_core_count += proxy->node->core_count;
+    for (int i = 0; i < pg->proxy_count; i++) {
+        total_core_count += pg->proxy_list[i].node->core_count;
+    }
 
-    HYDU_MALLOC_OR_JUMP(filler_pmi_ids, int *, proxy_count * sizeof(int), status);
-    HYDU_MALLOC_OR_JUMP(nonfiller_pmi_ids, int *, proxy_count * sizeof(int), status);
+    HYDU_MALLOC_OR_JUMP(filler_pmi_ids, int *, pg->proxy_count * sizeof(int), status);
+    HYDU_MALLOC_OR_JUMP(nonfiller_pmi_ids, int *, pg->proxy_count * sizeof(int), status);
 
-    int i;
     pmi_id = 0;
-    for (proxy = pg->proxy_list, i = 0; proxy; proxy = proxy->next, i++) {
+    for (int i = 0; i < pg->proxy_count; i++) {
         filler_pmi_ids[i] = pmi_id;
-        pmi_id += proxy->filler_processes;
+        pmi_id += pg->proxy_list[i].filler_processes;
     }
-    for (proxy = pg->proxy_list, i = 0; proxy; proxy = proxy->next, i++) {
+    for (int i = 0; i < pg->proxy_count; i++) {
         nonfiller_pmi_ids[i] = pmi_id;
-        pmi_id += proxy->node->core_count;
+        pmi_id += pg->proxy_list[i].node->core_count;
     }
 
-    proxy_count = 0;
-    for (proxy = pg->proxy_list; proxy; proxy = proxy->next) {
+    for (int i = 0; i < pg->proxy_count; i++) {
+        struct HYD_proxy *proxy = &pg->proxy_list[i];
         for (inherited_env_count = 0, env = HYD_server_info.user_global.global_env.inherited;
              env; env = env->next, inherited_env_count++);
         for (user_env_count = 0, env = HYD_server_info.user_global.global_env.user; env;
@@ -374,9 +371,9 @@ HYD_status HYD_pmcd_pmi_fill_in_exec_launch_info(struct HYD_pg *pg)
         HYD_STRING_STASH(exec_stash, MPL_strdup("--pmi-id-map"), status);
 
         HYD_STRING_STASH_INIT(stash);
-        HYD_STRING_STASH(stash, HYDU_int_to_str(filler_pmi_ids[proxy_count]), status);
+        HYD_STRING_STASH(stash, HYDU_int_to_str(filler_pmi_ids[i]), status);
         HYD_STRING_STASH(stash, MPL_strdup(","), status);
-        HYD_STRING_STASH(stash, HYDU_int_to_str(nonfiller_pmi_ids[proxy_count]), status);
+        HYD_STRING_STASH(stash, HYDU_int_to_str(nonfiller_pmi_ids[i]), status);
         HYD_STRING_SPIT(stash, map, status);
 
         HYD_STRING_STASH(exec_stash, map, status);
@@ -480,7 +477,7 @@ HYD_status HYD_pmcd_pmi_fill_in_exec_launch_info(struct HYD_pg *pg)
         }
 
         if (HYD_server_info.user_global.debug) {
-            HYDU_dump_noprefix(stdout, "Arguments being passed to proxy %d:\n", proxy_count);
+            HYDU_dump_noprefix(stdout, "Arguments being passed to proxy %d:\n", i);
             HYDU_print_strlist(exec_stash.strlist);
             HYDU_dump_noprefix(stdout, "\n");
         }
@@ -489,8 +486,6 @@ HYD_status HYD_pmcd_pmi_fill_in_exec_launch_info(struct HYD_pg *pg)
         HYDU_ERR_POP(status, "unable to dup strlist\n");
 
         HYD_STRING_STASH_FREE(exec_stash);
-
-        proxy_count++;
     }
 
   fn_exit:

commit 60e5a8068066ea3ffb5677b50e665cbb364c418b

pm/hydra: Fix MPIR_proctable
Make sure to update the "round" variable as we construct
MPIR_proctable. This was accidentally broken in [2906ecf5].

Fixes pmodels/mpich#6363.

src/pm/hydra/mpiexec/debugger.c
@@ -44,8 +44,9 @@ HYD_status HYDT_dbg_setup_procdesc(struct HYD_pg * pg)
      * proxy. */
 
     i = 0;
-    for (int i_proxy = 0;; i_proxy = (i_proxy + 1) % pg->proxy_count) {
-        struct HYD_proxy *proxy = &pg->proxy_list[i_proxy];
+    for (int i_proxy = 0;; i_proxy++) {
+        struct HYD_proxy *proxy = &pg->proxy_list[i_proxy % pg->proxy_count];
+        round = i_proxy / pg->proxy_count;
         j = 0;
         k = 0;
         for (exec = proxy->exec_list; exec; exec = exec->next) {