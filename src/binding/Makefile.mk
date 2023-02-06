b353559175f2c1eea1a4f5f0c67a35d2c0e1c10b
Hui Zhou <hzhou321@anl.gov>
2023-02-05 23:41:33 -0600

    ABI: add build files
    Use --enable-mpi-abi configure option to enable build and installing
    libmpi_abi.so.

--- src/binding/Makefile.mk
+++ src/binding/Makefile.mk
@@ -4,6 +4,7 @@
 ##
 
 include $(top_srcdir)/src/binding/c/Makefile.mk
+include $(top_srcdir)/src/binding/abi/Makefile.mk
 include $(top_srcdir)/src/binding/cxx/Makefile.mk
 include $(top_srcdir)/src/binding/fortran/mpif_h/Makefile.mk
 include $(top_srcdir)/src/binding/fortran/use_mpi/Makefile.mk
---
+++ src/binding/abi/Makefile.mk
@@ -0,0 +1,14 @@
+##
+## Copyright (C) by Argonne National Laboratory
+##     See COPYRIGHT in top-level directory
+##
+
+if BUILD_ABI_LIB
+
+include_HEADERS += src/binding/abi/mpi_abi.h
+
+mpi_abi_sources += \
+    src/binding/abi/mpi_abi_util.c \
+    src/binding/abi/c_binding_abi.c
+
+endif BUILD_ABI_LIB
