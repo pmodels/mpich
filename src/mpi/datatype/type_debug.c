6ce0ea445ba2172f1544bfcbf4ece3d569b9714a
Hui Zhou <hzhou321@anl.gov>
2023-02-22 15:00:16 -0600

    datatype: macro-guard removed combiner constants
    MPI_COMBINER_HVECTOR_INTEGER, MPI_COMBINER_HINDEXED_INTEGER, and
    MPI_COMBINER_STRUCT_INTEGER are removed in the current MPI standards and
    thus can be missing from the abi header. Add macro-guard to prevent
    compile failures.

--- src/mpi/datatype/type_debug.c
+++ src/mpi/datatype/type_debug.c
@@ -244,16 +244,18 @@ char *MPIR_Datatype_combiner_to_string(int combiner)
         return c_struct;
     if (combiner == MPI_COMBINER_DUP)
         return c_dup;
+#ifndef BUILD_MPI_ABI
     if (combiner == MPI_COMBINER_HVECTOR_INTEGER)
         return c_hvector_integer;
     if (combiner == MPI_COMBINER_HINDEXED_INTEGER)
         return c_hindexed_integer;
+    if (combiner == MPI_COMBINER_STRUCT_INTEGER)
+        return c_struct_integer;
+#endif
     if (combiner == MPI_COMBINER_INDEXED_BLOCK)
         return c_indexed_block;
     if (combiner == MPI_COMBINER_HINDEXED_BLOCK)
         return c_hindexed_block;
-    if (combiner == MPI_COMBINER_STRUCT_INTEGER)
-        return c_struct_integer;
     if (combiner == MPI_COMBINER_SUBARRAY)
         return c_subarray;
     if (combiner == MPI_COMBINER_DARRAY)
