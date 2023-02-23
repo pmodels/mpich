b85633d813862c595a07a0cbe611e5836a7d83c2
Hui Zhou <hzhou321@anl.gov>
2023-02-23 00:11:43 -0600

    binding/c: do not check status with ERRTEST_ARGNULL
    MPI_STATUS_IGNORE may be NULL, e.g. in MPI-5 ABI.

--- maint/local_python/binding_c.py
+++ maint/local_python/binding_c.py
@@ -542,6 +542,8 @@ def process_func_parameters(func):
             # -- output parameter --
             if p['length']:
                 validation_list.append({'kind': "ARGNULL-length", 'name': name, 'length': p['length']})
+            elif p['kind'] == 'STATUS':
+                pass
             else:
                 validation_list.append({'kind': "ARGNULL", 'name': name})
             if RE.search(r'(get_errhandler|mpi_comm_get_parent)$', func_name, re.IGNORECASE):
