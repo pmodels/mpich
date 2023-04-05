87ee2a918e08321866566954e864ce540eee05ff
Sonja Happ <sonja.happ@par-tec.com>
2023-04-05 12:02:00 +0200

    bindings/session: Use MPIR_Err_return_session(_init) in bindings
    Use the new error return functions for MPI sessions in the
    respective bindings.

--- maint/local_python/binding_c.py
+++ maint/local_python/binding_c.py
@@ -531,6 +531,8 @@ def process_func_parameters(func):
             func['_has_comm'] = name
         elif name == "win":
             func['_has_win'] = name
+        elif name == "session":
+            func['_has_session'] = name
 
         if 'ANY' in func['_skip_validate'] or kind in func['_skip_validate'] or name in func['_skip_validate']:
             # -- user bypass --
@@ -1614,6 +1616,10 @@ def dump_mpi_fn_fail(func):
             G.out.append("mpi_errno = MPIR_Err_return_comm(%s_ptr, __func__, mpi_errno);" % func['_has_comm'])
         elif '_has_win' in func:
             G.out.append("mpi_errno = MPIR_Err_return_win(win_ptr, __func__, mpi_errno);")
+        elif RE.match(r'mpi_session_init', func['name'], re.IGNORECASE):
+            G.out.append("mpi_errno = MPIR_Err_return_session_init(errhandler_ptr, __func__, mpi_errno);")
+        elif '_has_session' in func:
+            G.out.append("mpi_errno = MPIR_Err_return_session(session_ptr, __func__, mpi_errno);")
         else:
             G.out.append("mpi_errno = MPIR_Err_return_comm(0, __func__, mpi_errno);")
 
