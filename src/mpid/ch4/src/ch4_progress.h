fbc83550503b1c4f536c1ec713804672dfd36156
Hui Zhou <hzhou321@anl.gov>
2023-04-05 10:28:11 -0500

    threadcomm: fix threadcomm progress
    Ch4 progress loop only exits when per-vci progress counter gets updated.
    The counter is updated via MPID_Request_free_hook. However threadcomm
    may use request pool beyond MPIDI_global.n_vcis. We need make sure to
    check the progress counter even as we skipping the ch4-layer progress.
    
    Also fix MPIR_Progress_hook_exec_all to check the made_progress
    parameter.

--- src/mpid/ch4/src/ch4_progress.h
+++ src/mpid/ch4/src/ch4_progress.h
@@ -101,7 +101,10 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_progress_test(MPID_Progress_state * state)
 #endif
 
 #ifdef ENABLE_THREADCOMM
-    MPIR_Threadcomm_progress();
+    MPIR_Threadcomm_progress(&made_progress);
+    if (made_progress) {
+        goto fn_exit;
+    }
 #endif
 
     if (state->flag & MPIDI_PROGRESS_HOOKS) {
@@ -126,6 +129,9 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_progress_test(MPID_Progress_state * state)
     } else {
         for (int i = 0; i < state->vci_count; i++) {
             int vci = state->vci[i];
+            if (vci >= MPIDI_global.n_vcis) {
+                continue;
+            }
             MPIDI_PROGRESS(vci);
         }
     }
