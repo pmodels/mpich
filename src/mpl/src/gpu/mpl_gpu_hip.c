fed629657fad5f4bf29732ae57db2259534ca355
Hui Zhou <hzhou321@anl.gov>
2023-04-18 09:17:34 -0500

    mpl/hip: add missing MPL_gpu_ipc_handle_destroy
    Commit 2083dc4 added MPL_gpu_ipc_handle_destroy but neglected adding it
    in mpl_gpu_hip.c.

--- src/mpl/src/gpu/mpl_gpu_hip.c
+++ src/mpl/src/gpu/mpl_gpu_hip.c
@@ -132,6 +132,11 @@ int MPL_gpu_ipc_handle_create(const void *ptr, MPL_gpu_ipc_mem_handle_t * ipc_ha
     goto fn_exit;
 }
 
+int MPL_gpu_ipc_handle_destroy(const void *ptr)
+{
+    return MPL_SUCCESS;
+}
+
 int MPL_gpu_ipc_handle_map(MPL_gpu_ipc_mem_handle_t ipc_handle, int dev_id, void **ptr)
 {
     int mpl_err = MPL_SUCCESS;
