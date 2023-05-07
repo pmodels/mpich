ef5620cbc71119cd109697106820dd4498f3f767
Hui Zhou <hzhou321@anl.gov>
2023-05-06 20:50:06 -0500

    test/threadcomm: enhance the test to cover pt2pt messages
    Add test_sendrecv and test_isendirecv.

--- test/mpi/impls/mpich/threads/threadcomm/threadcomm.c
+++ test/mpi/impls/mpich/threads/threadcomm/threadcomm.c
@@ -9,6 +9,17 @@
 static int do_restart = 0;
 static int do_commdup = 0;
 
+#define MAX_COUNT 10000
+enum op {
+    DO_SEND,
+    DO_ISEND,
+    DO_RECV,
+    DO_IRECV,
+};
+
+static void test_sendrecv(MPI_Comm comm, int src, int dst, int count,
+                          int send_op, int recv_op, bool check_status);
+
 static MTEST_THREAD_RETURN_TYPE test_thread(void *arg)
 {
     MPI_Comm comm = *(MPI_Comm *) arg;
@@ -26,7 +37,16 @@ static MTEST_THREAD_RETURN_TYPE test_thread(void *arg)
         MPI_Comm_rank(use_comm, &rank);
         MTestPrintfMsg(1, "Rank %d / %d\n", rank, size);
 
-        /* TODO: add message tests */
+        for (int src = 0; src < size; src++) {
+            for (int dst = 0; dst < size; dst++) {
+                for (int count = 1; count < MAX_COUNT; count *= 8) {
+                    test_sendrecv(use_comm, src, dst, count, DO_SEND, DO_RECV, true);
+                    test_sendrecv(use_comm, src, dst, count, DO_ISEND, DO_IRECV, true);
+                    test_sendrecv(use_comm, src, dst, count, DO_SEND, DO_RECV, false);
+                    test_sendrecv(use_comm, src, dst, count, DO_ISEND, DO_IRECV, false);
+                }
+            }
+        }
 
         if (do_commdup) {
             MPI_Comm_free(&use_comm);
@@ -61,4 +81,68 @@ int main(int argc, char *argv[])
 
     MTest_Finalize(errs);
     return errs;
+}
+
+static void test_sendrecv(MPI_Comm comm, int src, int dst, int count,
+                          int send_op, int recv_op, bool check_status)
+{
+    int rank;
+    MPI_Comm_rank(comm, &rank);
+
+    int tag = 9;
+    int buf[MAX_COUNT];
+
+    if (src == dst) {
+        return;
+    }
+
+    MTestPrintfMsg(1, "test_sendrecv: %d -> %d, count=%d, send_op=%d, recv_op=%d, status:%d\n",
+                   src, dst, count, send_op, recv_op, check_status);
+
+    if (rank == src) {
+        for (int i = 0; i < count; i++) {
+            buf[i] = i;
+        }
+        if (send_op == DO_SEND) {
+            MPI_Send(buf, count, MPI_INT, dst, tag, comm);
+        } else if (send_op == DO_ISEND) {
+            MPI_Request req;
+            MPI_Isend(buf, count, MPI_INT, dst, tag, comm, &req);
+            MPI_Wait(&req, MPI_STATUS_IGNORE);
+        }
+    }
+    if (rank == dst) {
+        MPI_Status static_status;
+        MPI_Status *status = &static_status;
+        if (!check_status) {
+            status = MPI_STATUS_IGNORE;
+        }
+
+        if (recv_op == DO_RECV) {
+            MPI_Recv(buf, count, MPI_INT, src, tag, comm, status);
+        } else if (recv_op == DO_IRECV) {
+            MPI_Request req;
+            MPI_Irecv(buf, count, MPI_INT, src, tag, comm, &req);
+            MPI_Wait(&req, status);
+        }
+
+        int errs = 0;
+        for (int i = 0; i < count; i++) {
+            if (buf[i] != i) {
+                errs++;
+            }
+        }
+        if (errs > 0) {
+            printf("sendrecv %d -> %d, data error %d / %d\n", src, dst, errs, count);
+        }
+
+        if (check_status) {
+            if (status->MPI_SOURCE != src) {
+                printf("expect status->MPI_SOURCE = %d, got %d\n", src, status->MPI_SOURCE);
+            }
+            if (status->MPI_TAG != tag) {
+                printf("expect status->MPI_TAG = %d, got %d\n", tag, status->MPI_TAG);
+            }
+        }
+    }
 }
