0b36544955370273142d17f3b576e36d7f88b9e7
Hui Zhou <hzhou321@anl.gov>
2023-05-09 10:09:58 -0500

    mpi_t: read /etc/mpich.conf if exists
    Add mechanism to load a global config file that sets environment
    variables. It has a ":force" syntax to allow system admin to set
    settings that will be very difficult for users to overwrite since it is
    set during MPI_Init time. Without the ":force" keyword, it merely sets
    default environment that user can overwrite from job scripts.
    
    It is designed for CVAR environment variables, but it will set any
    environment variables.
    
    Co-authored-by: RaymondMichael

--- src/mpi_t/mpit_initthread.c
+++ src/mpi_t/mpit_initthread.c
@@ -5,6 +5,73 @@
 
 #include "mpiimpl.h"
 
+static bool debug_config_files = false;
+
+/*
+ * Read and set all the variables from the passed file
+ */
+static void read_files(const char fname[])
+{
+    int lineno = 0;
+    char storage[1024], *line;
+    FILE *f;
+
+    if (!(f = fopen(fname, "r"))) {
+        if (debug_config_files) {
+            fprintf(stderr, "Couldn't open %s: %m\n", fname);
+        }
+        return;
+    }
+
+    while ((line = fgets(storage, sizeof(storage), f))) {
+        char key[128], val[512], *spot;
+        bool force = false, already;
+
+        lineno++;
+
+        if ('\n' == line[0] || '#' == line[0])
+            continue;
+        if ('\0' == line[0])
+            break;
+
+        if (2 > sscanf(line, "%127[^=]=%511s", key, val)) {
+            fprintf(stderr, "Error parsing config file %s line %d: %s\n", fname, lineno, line);
+            goto err;
+        }
+        // If KEY:force=VALUE, then write prior values
+        if ((spot = strstr(key, ":force"))) {
+            force = true;
+            *spot = '\0';
+        }
+
+        already = getenv(key);
+        if (setenv(key, val, force)) {
+            fprintf(stderr, "Error setting %s from config file %s to %s\n", key, fname, val);
+            goto err;
+        }
+
+        if (debug_config_files && (!already || force)) {
+            fprintf(stderr, "Set %s=%s from %s:%d\n", key, val, fname, lineno);
+        }
+    }
+  err:
+    fclose(f);
+}
+
+/*
+ * Read global config files that have MPICH_ environment variables for us to
+ * use
+ */
+static void read_config_files(void)
+{
+    char *fname, *env;
+
+    if (getenv("MPICH_DEBUG_CONFIG_FILES"))
+        debug_config_files = true;
+
+    read_files("/etc/mpich.conf");
+}
+
 static inline void MPIR_T_enum_env_init(void)
 {
     static const UT_icd enum_table_entry_icd = { sizeof(MPIR_T_enum_t), NULL, NULL, NULL };
@@ -35,6 +102,8 @@ int MPIR_T_env_initialized = FALSE;
 int MPIR_T_env_init(void)
 {
     int mpi_errno = MPI_SUCCESS;
+
+    read_config_files();
 
     if (!MPIR_T_env_initialized) {
         MPIR_T_env_initialized = TRUE;
