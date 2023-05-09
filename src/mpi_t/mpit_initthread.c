7673b1096fba2972fd7b3a737934f0a54ed0c657
Hui Zhou <hzhou321@anl.gov>
2023-05-09 11:08:00 -0500

    mpit: clean up and add debug summary for config file
    Minor code cleanup. Also add a line to indicate global config file
    if MPIR_CVAR_DEBUG_SUMMARY is set.

--- src/mpi_t/mpit_initthread.c
+++ src/mpi_t/mpit_initthread.c
@@ -5,27 +5,23 @@
 
 #include "mpiimpl.h"
 
-static bool debug_config_files = false;
-
 /*
- * Read and set all the variables from the passed file
+ * Read global config files that have MPICH_ environment variables for us to
+ * use
  */
-static void read_files(const char fname[])
+static int read_config_files(const char *fname)
 {
     int lineno = 0;
     char storage[1024], *line;
     FILE *f;
 
     if (!(f = fopen(fname, "r"))) {
-        if (debug_config_files) {
-            fprintf(stderr, "Couldn't open %s: %m\n", fname);
-        }
-        return;
+        return FALSE;
     }
 
     while ((line = fgets(storage, sizeof(storage), f))) {
         char key[128], val[512], *spot;
-        bool force = false, already;
+        bool force = false;
 
         lineno++;
 
@@ -38,38 +34,20 @@ static void read_files(const char fname[])
             fprintf(stderr, "Error parsing config file %s line %d: %s\n", fname, lineno, line);
             goto err;
         }
-        // If KEY:force=VALUE, then write prior values
+        /* If KEY:force=VALUE, then write prior values */
         if ((spot = strstr(key, ":force"))) {
             force = true;
             *spot = '\0';
         }
 
-        already = getenv(key);
         if (setenv(key, val, force)) {
             fprintf(stderr, "Error setting %s from config file %s to %s\n", key, fname, val);
             goto err;
         }
-
-        if (debug_config_files && (!already || force)) {
-            fprintf(stderr, "Set %s=%s from %s:%d\n", key, val, fname, lineno);
-        }
     }
   err:
     fclose(f);
-}
-
-/*
- * Read global config files that have MPICH_ environment variables for us to
- * use
- */
-static void read_config_files(void)
-{
-    char *fname, *env;
-
-    if (getenv("MPICH_DEBUG_CONFIG_FILES"))
-        debug_config_files = true;
-
-    read_files("/etc/mpich.conf");
+    return TRUE;
 }
 
 static inline void MPIR_T_enum_env_init(void)
@@ -103,7 +81,10 @@ int MPIR_T_env_init(void)
 {
     int mpi_errno = MPI_SUCCESS;
 
-    read_config_files();
+    const char *config_filename = "/etc/mpich.conf";
+    if (!read_config_files(config_filename)) {
+        config_filename = NULL;
+    }
 
     if (!MPIR_T_env_initialized) {
         MPIR_T_env_initialized = TRUE;
@@ -111,6 +92,9 @@ int MPIR_T_env_init(void)
         MPIR_T_cat_env_init();
         mpi_errno = MPIR_T_cvar_env_init();
         MPIR_T_pvar_env_init();
+        if (MPIR_CVAR_DEBUG_SUMMARY && config_filename) {
+            printf("Global config file: %s\n", config_filename);
+        }
     }
     return mpi_errno;
 }
