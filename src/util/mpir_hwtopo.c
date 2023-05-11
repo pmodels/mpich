7e8457cde11c12c830d8eb93787fa266c9344691
Taru Doodi <taru.doodi@intel.com>
2023-05-11 12:37:19 -0700

    hwloc: Add support for GROUP objects for SNC4 detection
    Co-authored-by: Nitin Bhat <nitin.bhat@intel.com>

--- src/util/mpir_hwtopo.c
+++ src/util/mpir_hwtopo.c
@@ -106,6 +106,9 @@ static hwloc_obj_type_t get_hwloc_obj_type(MPIR_hwtopo_type_e type)
         case MPIR_HWTOPO_TYPE__CPU:
             hwloc_obj_type = HWLOC_OBJ_PACKAGE;
             break;
+        case MPIR_HWTOPO_TYPE__GROUP:
+            hwloc_obj_type = HWLOC_OBJ_GROUP;
+            break;
         case MPIR_HWTOPO_TYPE__CORE:
             hwloc_obj_type = HWLOC_OBJ_CORE;
             break;
@@ -313,6 +316,7 @@ MPIR_hwtopo_type_e MPIR_hwtopo_get_type_id(const char *name)
         {"machine", MPIR_HWTOPO_TYPE__NODE},
         {"socket", MPIR_HWTOPO_TYPE__SOCKET},
         {"package", MPIR_HWTOPO_TYPE__PACKAGE},
+        {"group", MPIR_HWTOPO_TYPE__GROUP},
         {"cpu", MPIR_HWTOPO_TYPE__CPU},
         {"core", MPIR_HWTOPO_TYPE__CORE},
         {"hwthread", MPIR_HWTOPO_TYPE__HWTHREAD},
@@ -620,4 +624,58 @@ MPIR_hwtopo_gid_t MPIR_hwtopo_get_dev_parent_by_pci(int domain, int bus, int dev
     gid = HWTOPO_GET_GID(class, first_non_io->depth, first_non_io->logical_index);
 #endif
     return gid;
+}
+
+int MPIR_hwtopo_get_num_numa_nodes(void)
+{
+    int num_numa_nodes = 0;
+
+#ifdef HAVE_HWLOC
+    MPIR_hwtopo_gid_t gid = MPIR_hwtopo_get_obj_by_name("node");
+    hwloc_obj_t obj =
+        hwloc_get_obj_by_depth(hwloc_topology, HWTOPO_GET_DEPTH(gid), HWTOPO_GET_INDEX(gid));
+
+    hwloc_obj_t tmp = NULL;
+
+    while ((tmp = hwloc_get_next_obj_by_type(hwloc_topology, HWLOC_OBJ_NUMANODE, tmp)) != NULL) {
+        if (hwloc_bitmap_isset(obj->nodeset, tmp->os_index)) {
+            num_numa_nodes++;
+        }
+    }
+#endif
+    return num_numa_nodes;
+}
+
+MPIR_hwtopo_gid_t MPIR_hwtopo_get_first_pu_group(void)
+{
+    MPIR_hwtopo_gid_t gid = MPIR_HWTOPO_GID_ROOT;
+#ifdef HAVE_HWLOC
+    hwloc_cpuset_t cpuset = hwloc_bitmap_alloc();
+    hwloc_get_proc_cpubind(hwloc_topology, getpid(), cpuset, HWLOC_CPUBIND_PROCESS);
+
+    hwloc_obj_t obj = hwloc_get_pu_obj_by_os_index(hwloc_topology, hwloc_bitmap_first(cpuset));
+    gid = HWTOPO_GET_GID(get_type_class(obj->type), obj->depth, obj->logical_index);
+
+    /* Traverse up the PU object until a group object is reached */
+    while (obj && obj->type != HWLOC_OBJ_GROUP && obj->parent)
+        obj = obj->parent;
+    gid = HWTOPO_GET_GID(get_type_class(obj->type), obj->depth, obj->logical_index);
+#endif
+    return gid;
+}
+
+MPIR_hwtopo_gid_t MPIR_hwtopo_get_parent_socket(MPIR_hwtopo_gid_t gid)
+{
+    MPIR_hwtopo_gid_t parent_gid = MPIR_HWTOPO_GID_ROOT;
+#ifdef HAVE_HWLOC
+    hwloc_obj_t obj =
+        hwloc_get_obj_by_depth(hwloc_topology, HWTOPO_GET_DEPTH(gid), HWTOPO_GET_INDEX(gid));
+
+    while (obj && obj->parent && obj->type != HWLOC_OBJ_PACKAGE)
+        obj = obj->parent;
+
+    if (obj->type == HWLOC_OBJ_PACKAGE)
+        parent_gid = HWTOPO_GET_GID(get_type_class(obj->type), obj->depth, obj->logical_index);
+#endif
+    return parent_gid;
 }
