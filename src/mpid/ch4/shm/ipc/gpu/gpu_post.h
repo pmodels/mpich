b355d7ffbe3a0144fd61ed3d786fec76dc73e0c4
Hui Zhou <hzhou321@anl.gov>
2023-07-02 14:55:25 -0500

    ch4/ipc/gpu: refactor ipc_handle_track_trees utils
    Store MPL_gpu_ipc_mem_handle_t directly rather than
    MPIDI_IPCI_ipc_handle_t. Add separate utilities to avoid mixing
    different requirement of caching local ipc handles versus caching remote
    mapped addresses.

--- src/mpid/ch4/shm/ipc/gpu/gpu_post.h
+++ src/mpid/ch4/shm/ipc/gpu/gpu_post.h
@@ -16,7 +16,6 @@ int MPIDI_GPU_ipc_handle_unmap(void *vaddr, MPIDI_GPU_ipc_handle_t handle, int d
 int MPIDI_GPU_init_local(void);
 int MPIDI_GPU_init_world(void);
 int MPIDI_GPU_mpi_finalize_hook(void);
-int MPIDI_GPU_ipc_handle_cache_insert(int rank, MPIR_Comm * comm, MPIDI_GPU_ipc_handle_t handle);
 int MPIDI_GPU_ipc_fast_memcpy(MPIDI_IPCI_ipc_handle_t ipc_handle, void *dest_vaddr,
                               MPI_Aint src_data_sz, MPI_Datatype datatype);
 int MPIDI_GPUI_create_ipc_track_trees(void);
