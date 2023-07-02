38d2a26bc7c50042a786f799baee59123333241c
Hui Zhou <hzhou321@anl.gov>
2023-07-02 09:01:59 -0500

    ch4/ipc: refactor ipc query and handle preparation
    Make ipc attr query and ipc handle preparation orthogonal and use
    separate data structure to avoid complication. In attr query, we focus
    on whether we should enable IPC, and only cache necessary information
    locally that is sufficient for preparing the handle. All the actual
    handle preparation logic moved to the separate function later.
    
    Different IPC features have different logic. Move most of the special
    data structure and logic inside the feature code to avoid changes in one
    feature affecting the code in other features.

--- src/mpid/ch4/shm/ipc/gpu/gpu_pre.h
+++ src/mpid/ch4/shm/ipc/gpu/gpu_pre.h
@@ -13,6 +13,7 @@ enum {
     MPIDI_GPU_IPC_HANDLE_REMAP_REQUIRED,
 };
 
+/* memory handle definition */
 typedef struct MPIDI_GPU_ipc_handle {
     MPL_gpu_ipc_mem_handle_t ipc_handle;
     int global_dev_id;
@@ -23,5 +24,14 @@ typedef struct MPIDI_GPU_ipc_handle {
     uintptr_t offset;
     int handle_status;
 } MPIDI_GPU_ipc_handle_t;
+
+/* local struct used for query and preparing memory handle */
+typedef struct MPIDI_GPU_ipc_attr {
+    int remote_rank;            /* global rank or MPI_PROC_NULL */
+    MPL_pointer_attr_t gpu_attr;
+    const void *vaddr;
+    void *bounds_base;
+    MPI_Aint bounds_len;
+} MPIDI_GPU_ipc_attr_t;
 
 #endif /* GPU_PRE_H_INCLUDED */
