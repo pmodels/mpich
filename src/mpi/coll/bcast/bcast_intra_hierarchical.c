f1a53f886727fb019fee63bf259a902fbed59baa
Kevin Liu <kliu@c359471l.cels.anl.gov>
2023-07-06 17:22:05 -0500

    finished outline for hierarchical bcast

--- src/mpi/coll/bcast/bcast_intra_hierarchical.c
+++ src/mpi/coll/bcast/bcast_intra_hierarchical.c
@@ -15,7 +15,7 @@ int MPIR_Bcast_intra_hierarchical(void* buffer, MPI_Aint count, MPI_Datatype dat
     MPI_Aint type_size, nbytes = 0;
     MPI_Status *status_p;
 
-    int rank, group_idx;
+    int rank, group_idx, group_root, group_size;
     int* group;
     bool is is_group_root;
 
@@ -27,16 +27,20 @@ int MPIR_Bcast_intra_hierarchical(void* buffer, MPI_Aint count, MPI_Datatype dat
     status_p = MPI_STATUS_IGNORE;
 #endif
 
+// It is probabaly necessary to have the parent communicator here
+
 #ifdef HAVE_ERROR_CHECKING
     MPIR_Assert(MPIR_Comm_is_parent_comm(comm_ptr));
 #endif
 
     rank = comm_ptr->rank;
-    
+
     /* TODO: find_group_idx needs to be implemented */
 
     group_idx = find_group_idx(hierarchy, hierarchy_size, rank);
     group = hierarchy[group_idx];
+    group_root = group[0];
+    group_size = group_sizes[group_idx];
     is_group_root = group[0] == rank;
 
     MPIR_Datatype_get_size_macro(datatype, type_size);
@@ -45,38 +49,21 @@ int MPIR_Bcast_intra_hierarchical(void* buffer, MPI_Aint count, MPI_Datatype dat
     if (nbytes == 0)
         goto fn_exit;   /* nothing to do */
 
+    if ((nbytes < MPIR_CVAR_BCAST_SHORT_MSG_SIZE) ||
+    (group_sizes[group_idx] < MPIR_CVAR_BCAST_MIN_PROCS)) {
 
-    if (is_group_root) {
+        MPIR_Bcast_intra_binomial_group(buffer,
+                              count,
+                              datatype,
+                              group_root, comm_ptr, group, group_size, errflag);
         
-        if ((nbytes < MPIR_CVAR_BCAST_SHORT_MSG_SIZE) ||
-        (group_sizes[group_idx] < MPIR_CVAR_BCAST_MIN_PROCS)) {
-            
-            
-
-
-
-
-        } else {
-
-
-
-
-
-
-
-        }
-
-
-
-
-
-
-
+    } else {
+        // large and medium sized msgs
+        MPIR_Bcast_intra_scatter_ring_allgather_group(buffer, 
+                                            count, 
+                                            datatype, 
+                                            group_root, comm_ptr, group, group_size, errflag);
     }
-
-
-
-    
 
     fn_exit:
         return mpi_errno_ret;
@@ -89,113 +76,3 @@ int MPIR_Bcast_intra_hierarchical(void* buffer, MPI_Aint count, MPI_Datatype dat
 
 
 
-int MPIR_Bcast_intra_smp(void *buffer, MPI_Aint count, MPI_Datatype datatype, int root,
-                         MPIR_Comm * comm_ptr, MPIR_Errflag_t errflag)
-{
-    int mpi_errno = MPI_SUCCESS;
-    int mpi_errno_ret = MPI_SUCCESS;
-    MPI_Aint type_size, nbytes = 0;
-    MPI_Status *status_p;
-
-    MPI_Status status;
-    status_p = &status;
-    MPI_Aint recvd_size;
-
-    status_p = MPI_STATUS_IGNORE;
-
-
-
-    MPIR_Assert(MPIR_Comm_is_parent_comm(comm_ptr));
-
-
-    MPIR_Datatype_get_size_macro(datatype, type_size);
-
-    nbytes = type_size * count;
-    if (nbytes == 0)
-        goto fn_exit;   /* nothing to do */
-
-    if ((nbytes < MPIR_CVAR_BCAST_SHORT_MSG_SIZE) ||
-        (comm_ptr->local_size < MPIR_CVAR_BCAST_MIN_PROCS)) {
-        /* send to intranode-rank 0 on the root's node */
-        if (comm_ptr->node_comm != NULL && MPIR_Get_intranode_rank(comm_ptr, root) > 0) {       /* is not the node root (0) and is on our node (!-1) */
-            if (root == comm_ptr->rank) {
-                mpi_errno = MPIC_Send(buffer, count, datatype, 0,
-                                      MPIR_BCAST_TAG, comm_ptr->node_comm, errflag);
-                MPIR_ERR_COLL_CHECKANDCONT(mpi_errno, errflag, mpi_errno_ret);
-            } else if (0 == comm_ptr->node_comm->rank) {
-                mpi_errno =
-                    MPIC_Recv(buffer, count, datatype, MPIR_Get_intranode_rank(comm_ptr, root),
-                              MPIR_BCAST_TAG, comm_ptr->node_comm, status_p);
-                MPIR_ERR_COLL_CHECKANDCONT(mpi_errno, errflag, mpi_errno_ret);
-#ifdef HAVE_ERROR_CHECKING
-                /* check that we received as much as we expected */
-                MPIR_Get_count_impl(status_p, MPI_BYTE, &recvd_size);
-                MPIR_ERR_COLL_CHECK_SIZE(recvd_size, nbytes, errflag, mpi_errno_ret);
-#endif
-            }
-
-        }
-
-        /* perform the internode broadcast */
-        if (comm_ptr->node_roots_comm != NULL) {
-            mpi_errno = MPIR_Bcast(buffer, count, datatype,
-                                   MPIR_Get_internode_rank(comm_ptr, root),
-                                   comm_ptr->node_roots_comm, errflag);
-            MPIR_ERR_COLL_CHECKANDCONT(mpi_errno, errflag, mpi_errno_ret);
-        }
-
-        /* perform the intranode broadcast on all except for the root's node */
-        if (comm_ptr->node_comm != NULL) {
-            mpi_errno = MPIR_Bcast(buffer, count, datatype, 0, comm_ptr->node_comm, errflag);
-            MPIR_ERR_COLL_CHECKANDCONT(mpi_errno, errflag, mpi_errno_ret);
-        }
-    } else {    /* (nbytes > MPIR_CVAR_BCAST_SHORT_MSG_SIZE) && (comm_ptr->size >= MPIR_CVAR_BCAST_MIN_PROCS) */
-
-        /* supposedly...
-         * smp+doubling good for pof2
-         * reg+ring better for non-pof2 */
-        if (nbytes < MPIR_CVAR_BCAST_LONG_MSG_SIZE && MPL_is_pof2(comm_ptr->local_size)) {
-            /* medium-sized msg and pof2 np */
-
-            /* perform the intranode broadcast on the root's node */
-            if (comm_ptr->node_comm != NULL && MPIR_Get_intranode_rank(comm_ptr, root) > 0) {   /* is not the node root (0) and is on our node (!-1) */
-                /* FIXME binomial may not be the best algorithm for on-node
-                 * bcast.  We need a more comprehensive system for selecting the
-                 * right algorithms here. */
-                mpi_errno = MPIR_Bcast(buffer, count, datatype,
-                                       MPIR_Get_intranode_rank(comm_ptr, root),
-                                       comm_ptr->node_comm, errflag);
-                MPIR_ERR_COLL_CHECKANDCONT(mpi_errno, errflag, mpi_errno_ret);
-            }
-
-            /* perform the internode broadcast */
-            if (comm_ptr->node_roots_comm != NULL) {
-                mpi_errno = MPIR_Bcast(buffer, count, datatype,
-                                       MPIR_Get_internode_rank(comm_ptr, root),
-                                       comm_ptr->node_roots_comm, errflag);
-                MPIR_ERR_COLL_CHECKANDCONT(mpi_errno, errflag, mpi_errno_ret);
-            }
-
-            /* perform the intranode broadcast on all except for the root's node */
-            if (comm_ptr->node_comm != NULL && MPIR_Get_intranode_rank(comm_ptr, root) <= 0) {  /* 0 if root was local root too, -1 if different node than root */
-                /* FIXME binomial may not be the best algorithm for on-node
-                 * bcast.  We need a more comprehensive system for selecting the
-                 * right algorithms here. */
-                mpi_errno = MPIR_Bcast(buffer, count, datatype, 0, comm_ptr->node_comm, errflag);
-                MPIR_ERR_COLL_CHECKANDCONT(mpi_errno, errflag, mpi_errno_ret);
-            }
-        } else {        /* large msg or non-pof2 */
-
-            /* FIXME It would be good to have an SMP-aware version of this
-             * algorithm that (at least approximately) minimized internode
-             * communication. */
-            mpi_errno =
-                MPIR_Bcast_intra_scatter_ring_allgather(buffer, count, datatype, root, comm_ptr,
-                                                        errflag);
-            MPIR_ERR_COLL_CHECKANDCONT(mpi_errno, errflag, mpi_errno_ret);
-        }
-    }
-
-  fn_exit:
-    return mpi_errno_ret;
-}
