pmodels/mpich

commit 3ac9843d50013cc4f09f10101f9fcc239ad2b1a3

threadcomm: add pt2pt interthread framework
The framework for sending and receiving interthread messages.

The blocking version of send/recv are slightly optimized by only poll
interthread progress.

The factorization of MPIR_Threadcomm_{send,recv}_attr are preparing for
supporting collectives.

The threadcomm will use all available request pools (vcis). It may
enter ch4-layer progress with vcis beyond what's configured in ch4.
Simply skip them since it only depends on threadcomm_progress.

Use different request pool to avoid threads contending on allocating
requests.

To-be added by next commits:
* Posted and unexpected message queue
* Postponed send queue
* Eager transport

src/mpid/ch4/src/ch4_progress.h
@@ -100,6 +100,13 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_progress_test(MPID_Progress_state * state)
     }
 #endif
 
+#ifdef ENABLE_THREADCOMM
+    MPIR_Threadcomm_progress(&made_progress);
+    if (made_progress) {
+        goto fn_exit;
+    }
+#endif
+
     if (state->flag & MPIDI_PROGRESS_HOOKS) {
         mpi_errno = MPIR_Progress_hook_exec_all(&made_progress);
         MPIR_ERR_CHECK(mpi_errno);
@@ -122,6 +129,9 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_progress_test(MPID_Progress_state * state)
     } else {
         for (int i = 0; i < state->vci_count; i++) {
             int vci = state->vci[i];
+            if (vci >= MPIDI_global.n_vcis) {
+                continue;
+            }
             MPIDI_PROGRESS(vci);
         }
     }

src/include/mpir_request.h
@@ -171,6 +171,8 @@ enum MPIR_sched_type {
   (e.g., 'MPIR_Request_send_t') that extends the 'MPIR_Request'.
 
   S*/
+
+#define MPIR_REQUEST_UNION_SIZE  40
 struct MPIR_Request {
     MPIR_OBJECT_HEADER;         /* adds handle and ref_count fields */
 
@@ -222,6 +224,9 @@ struct MPIR_Request {
         struct {
             MPIR_Win *win;
         } rma;                  /* kind : MPIR_REQUEST_KIND__RMA */
+        /* Reserve space for local usages. For example, threadcomm, the actual struct
+         * is defined locally and is used via casting */
+        char dummy[MPIR_REQUEST_UNION_SIZE];
     } u;
 
 #if defined HAVE_DEBUGGER_SUPPORT

src/include/mpir_threadcomm.h
@@ -8,6 +8,13 @@
 
 #include "utarray.h"
 
+#define MPIR_THREADCOMM_USE_NONE  0
+#define MPIR_THREADCOMM_USE_FBOX  1
+#define MPIR_THREADCOMM_USE_QUEUE 2
+
+/* TODO: add transport implementation */
+#define MPIR_THREADCOMM_TRANSPORT MPIR_THREADCOMM_USE_NONE
+
 typedef struct MPIR_Threadcomm {
     MPIR_OBJECT_HEADER;
     MPIR_Comm *comm;
@@ -43,8 +50,7 @@ typedef struct MPIR_threadcomm_tls_t {
     int tid;
     MPIR_Attribute *attributes;
     /* postponed send request */
-    MPIR_Request *pending_sreqs;
-    /* posted message queue */
+    /* posted receive queue */
     /* unexpected message queue */
 } MPIR_threadcomm_tls_t;
 
@@ -107,6 +113,14 @@ MPL_STATIC_INLINE_PREFIX int MPIR_threadcomm_get_tid(MPIR_Threadcomm * threadcom
     MPIR_threadcomm_tls_t *p = MPIR_threadcomm_get_tls(threadcomm);
     return p->tid;
 }
+
+int MPIR_Threadcomm_isend_attr(const void *buf, MPI_Aint count, MPI_Datatype datatype,
+                               int rank, int tag, MPIR_Threadcomm * threadcomm, int attr,
+                               MPIR_Request ** req);
+int MPIR_Threadcomm_irecv_attr(void *buf, MPI_Aint count, MPI_Datatype datatype,
+                               int rank, int tag, MPIR_Threadcomm * threadcomm, int attr,
+                               MPIR_Request ** req, bool has_status);
+int MPIR_Threadcomm_progress(int *made_progress);
 
 #endif /* ENABLE_THREADCOMM */
 

src/mpi/threadcomm/Makefile.mk
@@ -4,4 +4,5 @@
 ##
 
 mpi_core_sources +=   \
-    src/mpi/threadcomm/threadcomm_impl.c
+    src/mpi/threadcomm/threadcomm_impl.c \
+    src/mpi/threadcomm/threadcomm_pt2pt_impl.c

src/mpi/threadcomm/threadcomm_impl.c
@@ -6,6 +6,7 @@
 #include "mpiimpl.h"
 
 #ifdef ENABLE_THREADCOMM
+#include "threadcomm_pt2pt.h"
 
 bool MPIR_threadcomm_was_thread_single;
 UT_icd MPIR_threadcomm_icd = { sizeof(MPIR_threadcomm_tls_t), NULL, NULL, NULL };
@@ -55,6 +56,8 @@ int MPIR_Threadcomm_init_impl(MPIR_Comm * comm, int num_threads, MPIR_Comm ** co
     MPL_atomic_relaxed_store_int(&threadcomm->leave_counter, num_threads);
     MPL_atomic_relaxed_store_int(&threadcomm->barrier_flag, 0);
 
+    /* initialize transport */
+
     MPL_free(threads_table);
 
   fn_exit:
@@ -92,6 +95,8 @@ int MPIR_Threadcomm_free_impl(MPIR_Comm * comm)
     MPIR_ERR_CHECK(mpi_errno);
 
     MPL_free(threadcomm->rank_offset_table);
+
+    /* free transport */
 
     MPL_free(threadcomm);
 

src/mpi/threadcomm/threadcomm_pt2pt.h
@@ -1,0 +1,328 @@
+/*
+ * Copyright (C) by Argonne National Laboratory
+ *     See COPYRIGHT in top-level directory
+ */
+
+#ifndef THREADCOMM_PT2PT_H_INCLUDED
+#define THREADCOMM_PT2PT_H_INCLUDED
+
+/* this is an internal header for threadcomm_pt2pt_impl.c */
+
+typedef enum {
+    MPIR_THREADCOMM_MSGTYPE_EAGER,
+    MPIR_THREADCOMM_MSGTYPE_IPC,
+    MPIR_THREADCOMM_MSGTYPE_SYNC,
+} MPIR_Threadcomm_msgtype_t;
+
+/* send packet, needed for both eager and ipc */
+struct send_hdr {
+    MPIR_Threadcomm_msgtype_t type;
+    int src_id;
+    int tag;
+    int attr;
+    union {
+        /* eager */
+        MPI_Aint data_sz;
+        /* ipc */
+        MPIR_Request *sreq;
+    } u;
+};
+
+/* request union fields, e.g. sreq->u, needed for ipc */
+struct send_req {
+    const void *buf;
+    MPI_Aint count;
+    MPI_Datatype datatype;
+    int src_id;
+    int dst_id;
+    int tag;
+    int attr;
+};
+
+/* request union fields, e.g. rreq->u, needed for enqueue to posted_list */
+struct posted_req {
+    void *buf;
+    MPI_Aint count;
+    MPI_Datatype datatype;
+    int src_id;
+    int tag;
+    int attr;
+};
+
+/* cell to enqueue to unexp_list, hdr copied to cell */
+struct unexp {
+    struct unexp *next, *prev;
+    struct send_hdr hdr;
+};
+
+static int threadcomm_eager_send(int src_id, int dst_id, struct send_hdr *hdr,
+                                 const void *data, MPI_Aint count, MPI_Datatype datatype,
+                                 MPIR_Threadcomm * threadcomm);
+
+static void threadcomm_recv_event(struct send_hdr *hdr, MPIR_threadcomm_tls_t * p);
+static void threadcomm_data_copy(struct send_hdr *hdr, void *buf, MPI_Aint count,
+                                 MPI_Datatype datatype, MPIR_threadcomm_tls_t * p);
+static void threadcomm_set_status(struct send_hdr *hdr, MPI_Status * status,
+                                  MPIR_threadcomm_tls_t * p);
+static void threadcomm_enqueue_posted(void *buf, MPI_Aint count, MPI_Datatype datatype,
+                                      int src_id, int tag, int attr,
+                                      MPIR_Request * rreq, MPIR_threadcomm_tls_t * p);
+static MPIR_Request *threadcomm_match_posted(int src_id, int tag, int attr,
+                                             MPIR_threadcomm_tls_t * p);
+static void threadcomm_complete_posted(struct send_hdr *hdr, MPIR_Request * rreq,
+                                       MPIR_threadcomm_tls_t * p);
+static void threadcomm_enqueue_unexp(struct send_hdr *hdr, MPIR_threadcomm_tls_t * p);
+static struct send_hdr *threadcomm_match_unexp(int src_id, int tag, int attr,
+                                               MPIR_threadcomm_tls_t * p);
+static void threadcomm_complete_unexp(struct send_hdr *hdr, MPIR_threadcomm_tls_t * p);
+
+/* NOTE: it's possible to skip vci lock if we ensure each thread access a unique pool.
+ *       This is currently difficult to manage due to we dub the inter-process messages.
+ */
+#define MPIR_THREADCOMM_REQUEST_CREATE(req, kind, threadcomm, tls) \
+    do { \
+        int pool = (tls)->tid % MPIR_REQUEST_NUM_POOLS; \
+        (req) = MPIR_Request_create_from_pool_safe(kind, pool, 2); \
+        MPIR_ERR_CHKANDJUMP(!(req), mpi_errno, MPI_ERR_OTHER, "**nomem"); \
+    } while (0)
+
+/* ---- The main functions: send, recv, progress ---- */
+
+MPL_STATIC_INLINE_PREFIX
+    int MPIR_Threadcomm_send(const void *buf, MPI_Aint count, MPI_Datatype datatype,
+                             int dst_id, int tag, MPIR_Threadcomm * threadcomm,
+                             int attr, MPIR_Request ** req)
+{
+    int mpi_errno = MPI_SUCCESS;
+
+    MPL_COMPILE_TIME_ASSERT(sizeof(struct send_req) <= MPIR_REQUEST_UNION_SIZE);
+
+    MPIR_threadcomm_tls_t *p = MPIR_threadcomm_get_tls(threadcomm);
+    MPIR_Assert(p);
+
+    MPI_Aint data_sz;
+    MPIR_Datatype_get_size_macro(datatype, data_sz);
+    data_sz *= count;
+
+    bool try_eager = (data_sz <= (MPIR_THREADCOMM_MAX_PAYLOAD - sizeof(struct send_hdr)));
+    if (try_eager) {
+        struct send_hdr hdr;
+        hdr.type = MPIR_THREADCOMM_MSGTYPE_EAGER;
+        hdr.src_id = p->tid;
+        hdr.tag = tag;
+        hdr.attr = attr;
+        hdr.u.data_sz = data_sz;
+
+        int ret = threadcomm_eager_send(p->tid, dst_id, &hdr, buf, count, datatype, threadcomm);
+        if (ret == MPI_SUCCESS) {
+            *req = MPIR_Request_create_complete(MPIR_REQUEST_KIND__SEND);
+            goto fn_exit;
+        }
+    }
+
+    /* ---- do ipc ---- */
+    /* prepare sreq */
+    MPIR_Request *sreq;
+    MPIR_THREADCOMM_REQUEST_CREATE(sreq, MPIR_REQUEST_KIND__SEND, threadcomm, p);
+    struct send_req *u = (struct send_req *) &sreq->u;
+    u->buf = buf;
+    u->count = count;
+    u->datatype = datatype;
+    u->src_id = p->tid;
+    u->dst_id = dst_id;
+    u->tag = tag;
+
+    /* prepare send_hdr */
+    struct send_hdr hdr;
+    hdr.type = MPIR_THREADCOMM_MSGTYPE_IPC;
+    hdr.src_id = p->tid;
+    hdr.tag = tag;
+    hdr.attr = attr;
+    /* attach sreq */
+    hdr.u.sreq = sreq;
+
+    int ret = threadcomm_eager_send(p->tid, dst_id, &hdr, NULL, 0, MPI_DATATYPE_NULL, threadcomm);
+    if (ret != MPI_SUCCESS) {
+        /* TODO: enqueue request */
+        MPIR_Assert(0);
+    }
+
+    *req = sreq;
+
+  fn_exit:
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
+}
+
+MPL_STATIC_INLINE_PREFIX
+    int MPIR_Threadcomm_recv(void *buf, MPI_Aint count, MPI_Datatype datatype,
+                             int src_id, int tag, MPIR_Threadcomm * threadcomm,
+                             int attr, MPIR_Request ** req, bool has_status)
+{
+    int mpi_errno = MPI_SUCCESS;
+
+    MPIR_threadcomm_tls_t *p = MPIR_threadcomm_get_tls(threadcomm);
+
+    struct send_hdr *hdr = threadcomm_match_unexp(src_id, tag, attr, p);
+    if (hdr) {
+        if (has_status) {
+            MPIR_THREADCOMM_REQUEST_CREATE(*req, MPIR_REQUEST_KIND__RECV, threadcomm, p);
+            threadcomm_set_status(hdr, &(*req)->status, p);
+            MPIR_Request_complete(*req);
+        } else {
+            *req = MPIR_Request_create_complete(MPIR_REQUEST_KIND__RECV);
+        }
+        threadcomm_data_copy(hdr, buf, count, datatype, p);
+        threadcomm_complete_unexp(hdr, p);
+    } else {
+        MPIR_THREADCOMM_REQUEST_CREATE(*req, MPIR_REQUEST_KIND__RECV, threadcomm, p);
+        threadcomm_enqueue_posted(buf, count, datatype, src_id, tag, attr, *req, p);
+    }
+
+  fn_exit:
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
+}
+
+MPL_STATIC_INLINE_PREFIX int threadcomm_progress_send(int *made_progress)
+{
+    MPIR_threadcomm_tls_t *p = ut_type_array(MPIR_threadcomm_array, MPIR_threadcomm_tls_t *);
+    for (int i = 0; i < utarray_len(MPIR_threadcomm_array); i++) {
+        /* TODO: try send postponed send requests */
+    }
+    return MPI_SUCCESS;
+}
+
+MPL_STATIC_INLINE_PREFIX int threadcomm_progress_recv(int *made_progress)
+{
+    MPIR_threadcomm_tls_t *p = ut_type_array(MPIR_threadcomm_array, MPIR_threadcomm_tls_t *);
+    for (int i = 0; i < utarray_len(MPIR_threadcomm_array); i++) {
+        struct send_hdr *hdr = NULL;
+        /* TODO: poll transport for incoming messages */
+        if (hdr) {
+            threadcomm_recv_event(hdr, &p[i]);
+        }
+    }
+    return MPI_SUCCESS;
+}
+
+/* ---- internal functions ---- */
+
+static int threadcomm_eager_send(int src_id, int dst_id, struct send_hdr *hdr,
+                                 const void *data, MPI_Aint count, MPI_Datatype datatype,
+                                 MPIR_Threadcomm * threadcomm)
+{
+    MPIR_Assert(0);     /* TODO */
+    return MPI_SUCCESS;
+}
+
+static void threadcomm_recv_event(struct send_hdr *hdr, MPIR_threadcomm_tls_t * p)
+{
+    if (hdr->type == MPIR_THREADCOMM_MSGTYPE_SYNC) {
+        /* This is sync message from receiver */
+        MPIR_Request_complete(hdr->u.sreq);
+        return;
+    }
+
+    MPIR_Request *rreq = threadcomm_match_posted(hdr->src_id, hdr->tag, hdr->attr, p);
+    if (rreq) {
+        threadcomm_complete_posted(hdr, rreq, p);
+        threadcomm_set_status(hdr, &rreq->status, p);
+    } else {
+        threadcomm_enqueue_unexp(hdr, p);
+    }
+}
+
+/* copy data from hdr to recv buffer, send ack unless it's eager */
+static void threadcomm_data_copy(struct send_hdr *hdr, void *buf, MPI_Aint count,
+                                 MPI_Datatype datatype, MPIR_threadcomm_tls_t * p)
+{
+    if (hdr->type == MPIR_THREADCOMM_MSGTYPE_EAGER) {
+        MPI_Aint actual_unpack_bytes;
+        const void *data = hdr + 1;
+        MPIR_Typerep_unpack(data, hdr->u.data_sz, buf, count, datatype,
+                            0, &actual_unpack_bytes, MPIR_TYPEREP_FLAG_NONE);
+        MPIR_Assert(actual_unpack_bytes == hdr->u.data_sz);
+    } else if (hdr->type == MPIR_THREADCOMM_MSGTYPE_IPC) {
+        MPIR_Request *sreq = hdr->u.sreq;
+        struct send_req *sreq_u = (struct send_req *) &(sreq->u);
+        MPIR_Localcopy(sreq_u->buf, sreq_u->count, sreq_u->datatype, buf, count, datatype);
+
+        /* send ack */
+        struct send_hdr tmp_hdr;
+        tmp_hdr.type = MPIR_THREADCOMM_MSGTYPE_SYNC;
+        tmp_hdr.src_id = p->tid;
+        tmp_hdr.u.sreq = sreq;
+        int ret = threadcomm_eager_send(p->tid, hdr->src_id, &tmp_hdr, NULL, 0, MPI_DATATYPE_NULL,
+                                        p->threadcomm);
+        if (ret != MPI_SUCCESS) {
+            /* TODO: enqueue sending the sync message */
+            MPIR_Assert(0);
+        }
+    }
+}
+
+static void threadcomm_set_status(struct send_hdr *hdr, MPI_Status * status,
+                                  MPIR_threadcomm_tls_t * p)
+{
+    status->MPI_SOURCE = MPIR_THREADCOMM_TID_TO_RANK(p->threadcomm, hdr->src_id);
+    status->MPI_TAG = hdr->tag;
+    status->MPI_ERROR = MPIR_PT2PT_ATTR_GET_ERRFLAG(hdr->attr);
+
+    /* set count */
+    MPI_Aint data_sz;
+    if (hdr->type == MPIR_THREADCOMM_MSGTYPE_EAGER) {
+        data_sz = hdr->u.data_sz;
+    } else if (hdr->type == MPIR_THREADCOMM_MSGTYPE_IPC) {
+        MPIR_Request *sreq = hdr->u.sreq;
+        struct send_req *sreq_u = (struct send_req *) &(sreq->u);
+        MPIR_Datatype_get_size_macro(sreq_u->datatype, data_sz);
+        data_sz *= sreq_u->count;
+    } else {
+        MPIR_Assert(0);
+        data_sz = 0;
+    }
+    MPIR_STATUS_SET_COUNT((*status), data_sz);
+}
+
+static void threadcomm_enqueue_posted(void *buf, MPI_Aint count, MPI_Datatype datatype,
+                                      int src_id, int tag, int attr,
+                                      MPIR_Request * rreq, MPIR_threadcomm_tls_t * p)
+{
+    MPIR_Assert(0);     /* TODO */
+    return rreq;
+}
+
+static MPIR_Request *threadcomm_match_posted(int src_id, int tag, int attr,
+                                             MPIR_threadcomm_tls_t * p)
+{
+    MPIR_Assert(0);     /* TODO */
+    return NULL;
+}
+
+static void threadcomm_complete_posted(struct send_hdr *hdr, MPIR_Request * rreq,
+                                       MPIR_threadcomm_tls_t * p)
+{
+    MPIR_Assert(0);     /* TODO */
+}
+
+static void threadcomm_enqueue_unexp(struct send_hdr *hdr, MPIR_threadcomm_tls_t * p)
+{
+    MPIR_Assert(0);     /* TODO */
+}
+
+static struct send_hdr *threadcomm_match_unexp(int src_id, int tag, int attr,
+                                               MPIR_threadcomm_tls_t * p)
+{
+    MPIR_Assert(0);     /* TODO */
+    return NULL;
+}
+
+static void threadcomm_complete_unexp(struct send_hdr *hdr, MPIR_threadcomm_tls_t * p)
+{
+    MPIR_Assert(0);     /* TODO */
+}
+
+#endif /* THREADCOMM_PT2PT_H_INCLUDED */

src/mpi/threadcomm/threadcomm_pt2pt_impl.c
@@ -1,0 +1,147 @@
+/*
+ * Copyright (C) by Argonne National Laboratory
+ *     See COPYRIGHT in top-level directory
+ */
+
+#include "mpiimpl.h"
+
+#ifdef ENABLE_THREADCOMM
+
+#include "threadcomm_pt2pt.h"
+
+int MPIR_Threadcomm_isend_attr(const void *buf, MPI_Aint count, MPI_Datatype datatype,
+                               int rank, int tag, MPIR_Threadcomm * threadcomm, int attr,
+                               MPIR_Request ** req)
+{
+    int mpi_errno = MPI_SUCCESS;
+
+    if (MPIR_THREADCOMM_RANK_IS_INTERTHREAD(threadcomm, rank)) {
+        int dst_id = MPIR_THREADCOMM_RANK_TO_TID(threadcomm, rank);
+        mpi_errno = MPIR_Threadcomm_send(buf, count, datatype, dst_id, tag, threadcomm, attr, req);
+        MPIR_ERR_CHECK(mpi_errno);
+    } else {
+        /* TODO */
+        MPIR_Assert(0);
+        /* use threadcomm->comm, but we need patch the request or tag for proper matching */
+    }
+
+  fn_exit:
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
+}
+
+int MPIR_Threadcomm_irecv_attr(void *buf, MPI_Aint count, MPI_Datatype datatype,
+                               int rank, int tag, MPIR_Threadcomm * threadcomm, int attr,
+                               MPIR_Request ** req, bool has_status)
+{
+    int mpi_errno = MPI_SUCCESS;
+
+    if (MPIR_THREADCOMM_RANK_IS_INTERTHREAD(threadcomm, rank)) {
+        int src_id = MPIR_THREADCOMM_RANK_TO_TID(threadcomm, rank);
+        mpi_errno = MPIR_Threadcomm_recv(buf, count, datatype, src_id, tag,
+                                         threadcomm, attr, req, has_status);
+        MPIR_ERR_CHECK(mpi_errno);
+    } else {
+        /* TODO */
+        MPIR_Assert(0);
+        /* use threadcomm->comm, but we need patch the request or tag for proper matching */
+    }
+
+  fn_exit:
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
+}
+
+int MPIR_Threadcomm_isend_impl(const void *buf, MPI_Aint count, MPI_Datatype datatype,
+                               int rank, int tag, MPIR_Comm * comm, MPIR_Request ** req)
+{
+    MPIR_Assert(comm->threadcomm);
+    return MPIR_Threadcomm_isend_attr(buf, count, datatype, rank, tag, comm->threadcomm, 0, req);
+}
+
+int MPIR_Threadcomm_irecv_impl(void *buf, MPI_Aint count, MPI_Datatype datatype,
+                               int rank, int tag, MPIR_Comm * comm, MPIR_Request ** req)
+{
+    MPIR_Assert(comm->threadcomm);
+    return MPIR_Threadcomm_irecv_attr(buf, count, datatype, rank, tag,
+                                      comm->threadcomm, 0, req, true);
+}
+
+int MPIR_Threadcomm_send_impl(const void *buf, MPI_Aint count, MPI_Datatype datatype,
+                              int rank, int tag, MPIR_Comm * comm)
+{
+    int mpi_errno = MPI_SUCCESS;
+
+    MPIR_Assert(comm->threadcomm);
+    MPIR_Threadcomm *threadcomm = comm->threadcomm;
+
+    MPIR_Request *sreq;
+    mpi_errno = MPIR_Threadcomm_isend_attr(buf, count, datatype, rank, tag, threadcomm, 0, &sreq);
+    MPIR_ERR_CHECK(mpi_errno);
+
+    if (MPIR_THREADCOMM_RANK_IS_INTERTHREAD(threadcomm, rank)) {
+        while (!MPIR_Request_is_complete(sreq)) {
+            int made_progress;
+            MPIR_Threadcomm_progress(&made_progress);
+        }
+    } else {
+        mpi_errno = MPID_Wait(sreq, MPI_STATUS_IGNORE);
+        MPIR_ERR_CHECK(mpi_errno);
+    }
+    MPIR_Request_free(sreq);
+
+  fn_exit:
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
+}
+
+int MPIR_Threadcomm_recv_impl(void *buf, MPI_Aint count, MPI_Datatype datatype,
+                              int rank, int tag, MPIR_Comm * comm, MPI_Status * status)
+{
+    int mpi_errno = MPI_SUCCESS;
+
+    MPIR_Assert(comm->threadcomm);
+    MPIR_Threadcomm *threadcomm = comm->threadcomm;
+
+    bool has_status = (status != MPI_STATUS_IGNORE);
+
+    MPIR_Request *rreq;
+    mpi_errno = MPIR_Threadcomm_irecv_attr(buf, count, datatype, rank, tag,
+                                           threadcomm, 0, &rreq, has_status);
+    MPIR_ERR_CHECK(mpi_errno);
+
+    if (rank >= 0 && MPIR_THREADCOMM_RANK_IS_INTERTHREAD(threadcomm, rank)) {
+        while (!MPIR_Request_is_complete(rreq)) {
+            int made_progress;
+            MPIR_Threadcomm_progress(&made_progress);
+        }
+    } else {
+        mpi_errno = MPID_Wait(rreq, MPI_STATUS_IGNORE);
+        MPIR_ERR_CHECK(mpi_errno);
+    }
+    mpi_errno = rreq->status.MPI_ERROR;
+    MPIR_Request_extract_status(rreq, status);
+    MPIR_Request_free(rreq);
+
+  fn_exit:
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
+}
+
+int MPIR_Threadcomm_progress(int *made_progress)
+{
+    int mpi_errno = MPI_SUCCESS;
+
+    if (MPIR_threadcomm_array && utarray_len(MPIR_threadcomm_array)) {
+        threadcomm_progress_send(made_progress);
+        threadcomm_progress_recv(made_progress);
+    }
+
+    return mpi_errno;
+}
+
+#endif

commit 65224b51e70d52c5c28be4c48d3fa50d4ad6ce70

ch4: fix vci range check in progress
This fixes a bug introduced in commit "3ac9843d50 -
threadcomm: add pt2pt interthread framework". It neglected to consider
reserved vci, and thus breaks the gpu stream tests..

src/mpid/ch4/src/ch4_progress.h
@@ -129,7 +129,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_progress_test(MPID_Progress_state * state)
     } else {
         for (int i = 0; i < state->vci_count; i++) {
             int vci = state->vci[i];
-            if (vci >= MPIDI_global.n_vcis) {
+            if (vci >= MPIDI_global.n_total_vcis) {
                 continue;
             }
             MPIDI_PROGRESS(vci);