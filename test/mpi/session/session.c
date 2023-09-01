fde63562331fef99d64b6475cede9074f73a74dc
Sonja Happ <sonja.happ@par-tec.com>
2023-09-01 14:52:56 +0200

    test/session: add session_mod_group test
    Add a session test with a group that is smaller than world but
    larger than self to cover more potential error cases.

--- test/mpi/session/session.c
+++ test/mpi/session/session.c
@@ -93,6 +93,12 @@ int library_foo_test(void)
         rank = -1;
         goto fn_exit;
     }
+#ifdef MOD_GROUP
+    if (lib_comm == MPI_COMM_NULL) {
+        /* Skip the reduce operation for rank that is not in smaller group/ comm */
+        goto fn_exit;
+    }
+#endif
 
     int sum;
     rc = MPI_Reduce(&rank, &sum, 1, MPI_INT, MPI_SUM, 0, lib_comm);
@@ -157,7 +163,68 @@ void library_foo_init(int *rank, int *size)
         errs++;
         goto fn_exit;
     }
+#ifdef MOD_GROUP
+    int grp_size, grp_rank, newgrp_size, newgrp_rank, comm_size = 0;
+    MPI_Group newgroup = MPI_GROUP_NULL;
+    MPI_Group_size(wgroup, &grp_size);
+    MPI_Group_rank(wgroup, &grp_rank);
+    *rank = grp_rank;
+    *size = grp_size;
+    int excl[1];
 
+    if (grp_size >= 2) {
+        /* Create a new group where the highest rank is excluded */
+        excl[0] = grp_size - 1;
+        rc = MPI_Group_excl(wgroup, 1, excl, &newgroup);
+        if (rc != MPI_SUCCESS) {
+            printf("Error on MPI_Group_excl\n");
+            errs++;
+            goto fn_exit;
+        }
+
+        MPI_Group_size(newgroup, &newgrp_size);
+        MPI_Group_rank(newgroup, &newgrp_rank);
+        if (newgrp_size != grp_size - 1) {
+            errs++;
+            goto fn_exit;
+        }
+        /* Use the new smaller group to create the lib_comm */
+        if (newgrp_rank == MPI_UNDEFINED) {
+            rc = MPI_Comm_create_from_group(MPI_GROUP_EMPTY,
+                                            "org.mpi-forum.mpi-v4_0.example-ex10_8", MPI_INFO_NULL,
+                                            MPI_ERRORS_RETURN, &lib_comm);
+        } else {
+            rc = MPI_Comm_create_from_group(newgroup, "org.mpi-forum.mpi-v4_0.example-ex10_8",
+                                            MPI_INFO_NULL, MPI_ERRORS_RETURN, &lib_comm);
+        }
+        if (rc != MPI_SUCCESS) {
+            errs++;
+            goto fn_exit;
+        }
+        if (lib_comm != MPI_COMM_NULL) {
+            MPI_Comm_size(lib_comm, &comm_size);
+            if (comm_size != newgrp_size) {
+                printf("Error: communicator for smaller group has size %d (expected %d)\n",
+                       comm_size, newgrp_size);
+                errs++;
+                goto fn_exit;
+            }
+            /* set size to size of smaller comm to enable
+             * result check of reduce operation */
+            *size = newgrp_size;
+        } else {
+            /* Only for the rank that has been excluded from the group it is ok to have MPI_COMM_NULL */
+            if (grp_rank != grp_size - 1) {
+                errs++;
+                printf("Error: communicator for smaller group is MPI_COMM_NULL\n");
+                goto fn_exit;
+            }
+        }
+    } else {
+        printf("This test has to be started with at least 2 processes!\n");
+        goto fn_exit;
+    }
+#else
     /* get a communicator */
     rc = MPI_Comm_create_from_group(wgroup, "org.mpi-forum.mpi-v4_0.example-ex10_8",
                                     MPI_INFO_NULL, MPI_ERRORS_RETURN, &lib_comm);
@@ -167,12 +234,18 @@ void library_foo_init(int *rank, int *size)
     }
     MPI_Comm_size(lib_comm, size);
     MPI_Comm_rank(lib_comm, rank);
+#endif
 
     /* free group, library doesn’t need it. */
   fn_exit:
     if (wgroup != MPI_GROUP_NULL) {
         MPI_Group_free(&wgroup);
     }
+#ifdef MOD_GROUP
+    if (newgroup != MPI_GROUP_NULL) {
+        MPI_Group_free(&newgroup);
+    }
+#endif
     if (sinfo != MPI_INFO_NULL) {
         MPI_Info_free(&sinfo);
     }
