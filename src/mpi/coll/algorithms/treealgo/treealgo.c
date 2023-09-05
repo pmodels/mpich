b3721aa76a9ebe49864c48d5fec64f7b83bb497c
Xi Luo <xi.luo@intel.com>
2023-09-05 14:35:25 -0700

    coll: dump the tree if cvar is enabled
    Dump the tree to json files when MPIR_CVAR_TREE_DUMP is set.
    
    Co-authored-by: Gengbin Zheng <gengbin.zheng@intel.com>
    Co-authored-by: Xi Luo <xi.luo@intel.com>

--- src/mpi/coll/algorithms/treealgo/treealgo.c
+++ src/mpi/coll/algorithms/treealgo/treealgo.c
@@ -7,6 +7,9 @@
 #include "treealgo.h"
 #include "treeutil.h"
 
+static void dump_node(MPIR_Treealgo_tree_t * node, FILE * output_stream);
+static void dump_tree(int tree_type, int rank, MPIR_Treealgo_tree_t * ct);
+
 int MPII_Treealgo_init(void)
 {
     int mpi_errno = MPI_SUCCESS;
@@ -62,6 +65,9 @@ int MPIR_Treealgo_tree_create(int rank, int nranks, int tree_type, int k, int ro
     MPIR_FUNC_EXIT;
 
   fn_exit:
+    if (MPIR_CVAR_COLL_TREE_DUMP) {
+        dump_tree(tree_type, rank, ct);
+    }
     return mpi_errno;
 
   fn_fail:
@@ -95,6 +101,9 @@ int MPIR_Treealgo_tree_create_topo_aware(MPIR_Comm * comm, int tree_type, int k,
     MPIR_FUNC_EXIT;
 
   fn_exit:
+    if (MPIR_CVAR_COLL_TREE_DUMP) {
+        dump_tree(tree_type, comm->rank, ct);
+    }
     return mpi_errno;
 
   fn_fail:
@@ -120,6 +129,9 @@ int MPIR_Treealgo_tree_create_topo_wave(MPIR_Comm * comm, int k, int root,
     MPIR_FUNC_EXIT;
 
   fn_exit:
+    if (MPIR_CVAR_COLL_TREE_DUMP) {
+        dump_tree(MPIR_TREE_TYPE_TOPOLOGY_WAVE, comm->rank, ct);
+    }
     return mpi_errno;
 
   fn_fail:
@@ -129,4 +141,28 @@ int MPIR_Treealgo_tree_create_topo_wave(MPIR_Comm * comm, int k, int root,
 void MPIR_Treealgo_tree_free(MPIR_Treealgo_tree_t * tree)
 {
     utarray_free(tree->children);
+}
+
+
+static void dump_node(MPIR_Treealgo_tree_t * node, FILE * output_stream)
+{
+    fprintf(output_stream, "{ \"rank\": %d, \"nranks\": %d, \"parent\": %d, \"children\": [",
+            node->rank, node->nranks, node->parent);
+    for (int child = 0; child < node->num_children; child++) {
+        if (child > 0) {
+            fprintf(output_stream, ",");
+        }
+        fprintf(output_stream, "%d", tree_ut_int_elt(node->children, child));
+    }
+    fprintf(output_stream, "] }\n");
+}
+
+static void dump_tree(int tree_type, int rank, MPIR_Treealgo_tree_t * ct)
+{
+    char outfile_name[PATH_MAX];
+    sprintf(outfile_name, "%s%d.json", "colltree", rank);
+    fprintf(stdout, "tree_type=%d: dumping %s\n", tree_type, outfile_name);
+    FILE *outfile = fopen(outfile_name, "w");
+    dump_node(ct, outfile);
+    fclose(outfile);
 }
