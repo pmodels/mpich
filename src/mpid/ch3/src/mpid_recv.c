pmodels/mpich

commit f7470a15c0f3a1cdef45f7508c160d3adcec42ab

ch3: fix segfault when sending MPI_DATATYPE_NULL
The datatype macros in ch3 neglected to account for MPI_DATATYPE_NULL.

src/mpid/ch3/src/mpid_recv.c
@@ -102,11 +102,8 @@ int MPID_Recv(void * buf, MPI_Aint count, MPI_Datatype datatype, int rank, int t
 		/* The data is still being transferred across the net.  
 		   We'll leave it to the progress engine to handle once the
 		   entire message has arrived. */
-		if (!HANDLE_IS_BUILTIN(datatype))
-		{
-		    MPIR_Datatype_get_ptr(datatype, rreq->dev.datatype_ptr);
-            MPIR_Datatype_ptr_add_ref(rreq->dev.datatype_ptr);
-		}
+
+                MPIR_Datatype_add_ref_if_not_builtin(datatype);
 	    }
 	}
 	else if (MPIDI_Request_get_msg_type(rreq) == MPIDI_REQUEST_RNDV_MSG)
@@ -155,11 +152,7 @@ int MPID_Recv(void * buf, MPI_Aint count, MPI_Datatype datatype, int rank, int t
 	   of the actions that are taken when a request is freed. 
 	   (specifically, the datatype and comm both have their refs
 	   decremented, and are freed if the refs are zero) */
-	if (!HANDLE_IS_BUILTIN(datatype))
-	{
-	    MPIR_Datatype_get_ptr(datatype, rreq->dev.datatype_ptr);
-        MPIR_Datatype_ptr_add_ref(rreq->dev.datatype_ptr);
-	}
+        MPIR_Datatype_add_ref_if_not_builtin(datatype);
 
 	rreq->dev.recv_pending_count = 1;
 	/* We must wait until here to exit the msgqueue critical section

src/mpid/ch3/src/mpid_isend.c
@@ -88,10 +88,7 @@ int MPID_Isend(const void * buf, MPI_Aint count, MPI_Datatype datatype, int rank
     MPIDI_Request_create_sreq(sreq, mpi_errno, goto fn_exit);
     MPIDI_Request_set_type(sreq, MPIDI_REQUEST_TYPE_SEND);
 
-    MPIDI_Datatype_get_info(count, datatype, dt_contig, data_sz, dt_ptr, 
-			    dt_true_lb);
-    
-    if (data_sz == 0)
+    if (count == 0)
     {
 	MPIDI_CH3_Pkt_t upkt;
 	MPIDI_CH3_Pkt_eager_send_t * const eager_pkt = &upkt.eager_send;
@@ -124,6 +121,8 @@ int MPID_Isend(const void * buf, MPI_Aint count, MPI_Datatype datatype, int rank
 
 	goto fn_exit;
     }
+
+    MPIDI_Datatype_get_info(count, datatype, dt_contig, data_sz, dt_ptr, dt_true_lb);
 
     MPIDI_CH3_GET_EAGER_THRESHOLD(&eager_threshold, comm, vc);
 

src/mpid/ch3/src/mpid_issend.c
@@ -57,14 +57,14 @@ int MPID_Issend(const void * buf, MPI_Aint count, MPI_Datatype datatype, int ran
     MPIDI_Request_create_sreq(sreq, mpi_errno, goto fn_exit);
     MPIDI_Request_set_type(sreq, MPIDI_REQUEST_TYPE_SSEND);
     
-    MPIDI_Datatype_get_info(count, datatype, dt_contig, data_sz, dt_ptr, dt_true_lb);
-    
-    if (data_sz == 0)
+    if (count == 0)
     {
 	mpi_errno = MPIDI_CH3_EagerSyncZero( &sreq, rank, tag, comm, 
 					     context_offset );
 	goto fn_exit;
     }
+
+    MPIDI_Datatype_get_info(count, datatype, dt_contig, data_sz, dt_ptr, dt_true_lb);
 
     MPIDI_CH3_GET_EAGER_THRESHOLD(&eager_threshold, comm, vc);
 

src/mpid/ch3/src/mpid_send.c
@@ -86,11 +86,7 @@ int MPID_Send(const void * buf, MPI_Aint count, MPI_Datatype datatype, int rank,
     }
 #endif
 
-    MPIDI_Datatype_get_info(count, datatype, dt_contig, data_sz, dt_ptr, 
-			    dt_true_lb);
-
-
-    if (data_sz == 0)
+    if (count == 0)
     {
 	MPIDI_CH3_Pkt_t upkt;
 	MPIDI_CH3_Pkt_eager_send_t * const eager_pkt = &upkt.eager_send;
@@ -123,6 +119,8 @@ int MPID_Send(const void * buf, MPI_Aint count, MPI_Datatype datatype, int rank,
 	
 	goto fn_exit;
     }
+
+    MPIDI_Datatype_get_info(count, datatype, dt_contig, data_sz, dt_ptr, dt_true_lb);
 
     MPIDI_CH3_GET_EAGER_THRESHOLD(&eager_threshold, comm, vc);
 

src/mpid/ch3/src/mpid_startall.c
@@ -199,11 +199,7 @@ int MPID_Ssend_init(const void * buf, MPI_Aint count, MPI_Datatype datatype, int
     int context_offset = MPIR_PT2PT_ATTR_CONTEXT_OFFSET(attr);
     MPIDI_Request_create_psreq(sreq, mpi_errno, goto fn_exit);
     MPIDI_Request_set_type(sreq, MPIDI_REQUEST_TYPE_SSEND);
-    if (!HANDLE_IS_BUILTIN(datatype))
-    {
-	MPIR_Datatype_get_ptr(datatype, sreq->dev.datatype_ptr);
-    MPIR_Datatype_ptr_add_ref(sreq->dev.datatype_ptr);
-    }
+    MPIR_Datatype_add_ref_if_not_builtin(datatype);
     *request = sreq;
 
   fn_exit:    

commit e0fd81a2bbd2bc00c4d4ff55c9035c2bf33a3c7a

ch3: revert changes in src/mpid/ch3/src/mpid_recv.c
Commit f7470a15 was incomplete in fixing ch3 mpid_recv.c. It neglected
to update rreq->dev.datatype_ptr, which missed the corresponding
MPIR_Datatype_release. Since the receive path does not have the original
issue with MPI_DATATYPE_NULL as in the send path, we revert the changes.

This fixes the datatype handle leak caused by f7470a15.

src/mpid/ch3/src/mpid_recv.c
@@ -102,8 +102,11 @@ int MPID_Recv(void * buf, MPI_Aint count, MPI_Datatype datatype, int rank, int t
 		/* The data is still being transferred across the net.  
 		   We'll leave it to the progress engine to handle once the
 		   entire message has arrived. */
-
-                MPIR_Datatype_add_ref_if_not_builtin(datatype);
+		if (!HANDLE_IS_BUILTIN(datatype))
+		{
+		    MPIR_Datatype_get_ptr(datatype, rreq->dev.datatype_ptr);
+            MPIR_Datatype_ptr_add_ref(rreq->dev.datatype_ptr);
+		}
 	    }
 	}
 	else if (MPIDI_Request_get_msg_type(rreq) == MPIDI_REQUEST_RNDV_MSG)
@@ -152,7 +155,11 @@ int MPID_Recv(void * buf, MPI_Aint count, MPI_Datatype datatype, int rank, int t
 	   of the actions that are taken when a request is freed. 
 	   (specifically, the datatype and comm both have their refs
 	   decremented, and are freed if the refs are zero) */
-        MPIR_Datatype_add_ref_if_not_builtin(datatype);
+	if (!HANDLE_IS_BUILTIN(datatype))
+	{
+	    MPIR_Datatype_get_ptr(datatype, rreq->dev.datatype_ptr);
+        MPIR_Datatype_ptr_add_ref(rreq->dev.datatype_ptr);
+	}
 
 	rreq->dev.recv_pending_count = 1;
 	/* We must wait until here to exit the msgqueue critical section