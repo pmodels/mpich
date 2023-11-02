90638811ab1920cc715791f606288c2da5a33305
Xi Luo <xi.luo@intel.com>
2023-11-02 16:16:52 -0700

    coll: enable topology-aware CVARs in json
    Allow setting up topology-aware CVARs in collective tuning json file for bcast, ireduce and allreduce.

--- src/mpi/coll/bcast/bcast_intra_tree.c
+++ src/mpi/coll/bcast/bcast_intra_tree.c
@@ -80,13 +80,36 @@ int MPIR_Bcast_intra_tree(void *buffer,
                 MPIR_Treealgo_tree_create_topo_aware(comm_ptr, tree_type, branching_factor, root,
                                                      MPIR_CVAR_BCAST_TOPO_REORDER_ENABLE, &my_tree);
         } else if (tree_type == MPIR_TREE_TYPE_TOPOLOGY_WAVE) {
+            MPIR_Csel_coll_sig_s coll_sig = {
+                .coll_type = MPIR_CSEL_COLL_TYPE__BCAST,
+                .comm_ptr = comm_ptr,
+                .u.bcast.buffer = buffer,
+                .u.bcast.count = count,
+                .u.bcast.datatype = datatype,
+                .u.bcast.root = root,
+            };
+
+            int overhead = MPIR_CVAR_BCAST_TOPO_OVERHEAD;
+            int lat_diff_groups = MPIR_CVAR_BCAST_TOPO_DIFF_GROUPS;
+            int lat_diff_switches = MPIR_CVAR_BCAST_TOPO_DIFF_SWITCHES;
+            int lat_same_switches = MPIR_CVAR_BCAST_TOPO_SAME_SWITCHES;
+
+            MPII_Csel_container_s *cnt = MPIR_Csel_search(comm_ptr->csel_comm, coll_sig);
+            MPIR_Assert(cnt);
+
+            if (cnt->id == MPII_CSEL_CONTAINER_TYPE__ALGORITHM__MPIR_Bcast_intra_tree) {
+                overhead = cnt->u.bcast.intra_tree.topo_overhead;
+                lat_diff_groups = cnt->u.bcast.intra_tree.topo_diff_groups;
+                lat_diff_switches = cnt->u.bcast.intra_tree.topo_diff_switches;
+                lat_same_switches = cnt->u.bcast.intra_tree.topo_same_switches;
+            }
+
             mpi_errno =
                 MPIR_Treealgo_tree_create_topo_wave(comm_ptr, branching_factor, root,
                                                     MPIR_CVAR_BCAST_TOPO_REORDER_ENABLE,
-                                                    MPIR_CVAR_BCAST_TOPO_OVERHEAD,
-                                                    MPIR_CVAR_BCAST_TOPO_DIFF_GROUPS,
-                                                    MPIR_CVAR_BCAST_TOPO_DIFF_SWITCHES,
-                                                    MPIR_CVAR_BCAST_TOPO_SAME_SWITCHES, &my_tree);
+                                                    overhead, lat_diff_groups, lat_diff_switches,
+                                                    lat_same_switches, &my_tree);
+
         } else {
             mpi_errno =
                 MPIR_Treealgo_tree_create(rank, comm_size, tree_type, branching_factor, root,
