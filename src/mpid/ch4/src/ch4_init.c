1b9c5ad81860d291cb6e58822f34fcb572810260
Hui Zhou <hzhou321@anl.gov>
2023-11-14 21:26:41 -0600

    ch4: checkout return from MPID_Comm_connect
    Check error return to show error stack instead of just an assertion
    error.

--- src/mpid/ch4/src/ch4_init.c
+++ src/mpid/ch4/src/ch4_init.c
@@ -610,7 +610,10 @@ int MPID_InitCompleted(void)
         mpi_errno =
             MPIR_pmi_kvs_parent_get(MPIDI_PARENT_PORT_KVSKEY, parent_port, MPI_MAX_PORT_NAME);
         MPIR_ERR_CHECK(mpi_errno);
-        MPID_Comm_connect(parent_port, NULL, 0, MPIR_Process.comm_world, &MPIR_Process.comm_parent);
+        mpi_errno = MPID_Comm_connect(parent_port, NULL, 0, MPIR_Process.comm_world,
+                                      &MPIR_Process.comm_parent);
+        MPIR_ERR_CHECK(mpi_errno);
+
         MPIR_Assert(MPIR_Process.comm_parent != NULL);
         MPL_strncpy(MPIR_Process.comm_parent->name, "MPI_COMM_PARENT", MPI_MAX_OBJECT_NAME);
     }
