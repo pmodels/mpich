pmodels/mpich

commit ab70b3a2985caa2113f5f2c588b2ebd6446f74a5

ABI: fix param_direction=out|inout
Pass NULL thru for error-checking.

maint/local_python/binding_c.py
@@ -1006,29 +1006,26 @@ def dump_abi_wrappers(func, is_large):
                 print("Function %s - array param %s" % (func['name'], p['name']))
         else:
             if p['param_direction'] == 'out':
-                pre_filters.append("MPI_%s %s_i;" % (T, name))
-                pre_filters.append("MPI_%s *%s = &%s_i;" % (T, name, name))
                 # pass NULL thru for error-checking
-                pre_filters.append("if (%s_abi == NULL) {" % name)
-                pre_filters.append("    %s = NULL;" % name)
-                pre_filters.append("}")
-                if RE.match(r'MPI_T_cvar_get_info', func['name']):
-                    post_filters.append("if (%s_abi) {" % name)
-                    post_filters.append("    *%s_abi = ABI_%s_from_mpi(%s_i);" % (name, T, name));
-                    post_filters.append("}")
-                else:
-                    # FIXME: which cases should we leave the output handle untouched?
-                    if T == 'Comm':
-                        post_filters.append("if (%s_abi) {" % name)
-                    else:
-                        post_filters.append("if (ret == MPI_SUCCESS) {")
-                    post_filters.append("    *%s_abi = ABI_%s_from_mpi(%s_i);" % (name, T, name));
-                    post_filters.append("}")
-            elif p['param_direction'] == 'inout' or func['name'] == "MPI_Cancel":
                 pre_filters.append("MPI_%s %s_i;" % (T, name))
-                pre_filters.append("%s_i = ABI_%s_to_mpi(*%s_abi);" % (name, T, name))
-                pre_filters.append("MPI_%s *%s = &%s_i;" % (T, name, name))
-                post_filters.append("*%s_abi = ABI_%s_from_mpi(%s_i);" % (name, T, name));
+                pre_filters.append("MPI_%s *%s = NULL;" % (T, name))
+                pre_filters.append("if (%s_abi != NULL) {" % name)
+                pre_filters.append("    %s = &%s_i;" % (name, name))
+                pre_filters.append("}")
+                post_filters.append("if (ret == MPI_SUCCESS && %s_abi != NULL) {" % name)
+                post_filters.append("    *%s_abi = ABI_%s_from_mpi(%s_i);" % (name, T, name));
+                post_filters.append("}")
+            elif p['param_direction'] == 'inout' or func['name'] == "MPI_Cancel":
+                # pass NULL thru for error-checking
+                pre_filters.append("MPI_%s %s_i;" % (T, name))
+                pre_filters.append("MPI_%s *%s = NULL;" % (T, name))
+                pre_filters.append("if (%s_abi != NULL) {" % name)
+                pre_filters.append("    %s_i = ABI_%s_to_mpi(*%s_abi);" % (name, T, name))
+                pre_filters.append("    %s = &%s_i;" % (name, name))
+                pre_filters.append("}")
+                post_filters.append("if (%s_abi != NULL) {" % name)
+                post_filters.append("    *%s_abi = ABI_%s_from_mpi(%s_i);" % (name, T, name));
+                post_filters.append("}")
             elif is_alltoallw and name == "comm":
                 # already done
                 pass

commit 7a3f687cb3feb941af7ad05864d6675647cbad7d

abi: set comm to MPI_COMM_NULL on creation failure
This behavior was broken by commit ab70b3a, result failure in
test/mpi/errors/topo/cartsmall.

maint/local_python/binding_c.py
@@ -1007,12 +1007,12 @@ def dump_abi_wrappers(func, is_large):
         else:
             if p['param_direction'] == 'out':
                 # pass NULL thru for error-checking
-                pre_filters.append("MPI_%s %s_i;" % (T, name))
+                pre_filters.append("MPI_%s %s_i = MPI_%s_NULL;" % (T, name, T.upper()))
                 pre_filters.append("MPI_%s *%s = NULL;" % (T, name))
                 pre_filters.append("if (%s_abi != NULL) {" % name)
                 pre_filters.append("    %s = &%s_i;" % (name, name))
                 pre_filters.append("}")
-                post_filters.append("if (ret == MPI_SUCCESS && %s_abi != NULL) {" % name)
+                post_filters.append("if (%s_abi != NULL) {" % name)
                 post_filters.append("    *%s_abi = ABI_%s_from_mpi(%s_i);" % (name, T, name));
                 post_filters.append("}")
             elif p['param_direction'] == 'inout' or func['name'] == "MPI_Cancel":