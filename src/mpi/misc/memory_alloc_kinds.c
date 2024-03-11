d1490eb01c7272f1ef4b56d7b90f29788cd3b701
Ken Raffenetti <raffenet@mcs.anl.gov>
2024-03-11 16:05:43 -0500

    mpi/misc: Add GPU memory allocation kinds
    Add support for querying GPU memory kinds as specified by the current
    draft of the MPI Memory Allocation Kinds side
    document (https://github.com/mpi-forum/mem-alloc).

--- src/mpi/misc/memory_alloc_kinds.c
+++ src/mpi/misc/memory_alloc_kinds.c
@@ -5,34 +5,57 @@
 
 #include "mpiimpl.h"
 
-static const char *memory_alloc_kinds[3][5] = {
-    /* mpi 4.1 kinds */
-    {"mpi", "alloc_mem", "win_allocate", "win_allocate_shared", NULL},
-    {"system", NULL},
-    {NULL}
-};
+/* mpi 4.1 kinds */
+static const char *mpi_kinds[5] =
+    { "mpi", "alloc_mem", "win_allocate", "win_allocate_shared", NULL };
+static const char *system_kinds[2] = { "system", NULL };
+
+/* memory allocation kinds side doc 1.0 */
+static const char *cuda_kinds[5] = { "cuda", "host", "device", "managed", NULL };
+static const char *rocm_kinds[5] = { "rocm", "host", "device", "managed", NULL };
+static const char *ze_kinds[5] = { "level_zero", "host", "device", "shared", NULL };
+
+/* we support a maximum of 3 kinds at a time: mpi,system,gpu */
+static const char **memory_alloc_kinds[4] = { mpi_kinds, system_kinds, NULL, NULL };
 
 static bool is_supported(const char *kind);
+static void init_gpu_kinds(void);
 
 int MPIR_get_supported_memory_kinds(char *requested_kinds, char **out_kinds)
 {
+    static bool gpu_init_done = 0;
     char *tmp_strs[1024];
-    int num = 2;
-    /* mpi and system kinds are always supported */
-    tmp_strs[0] = MPL_strdup("mpi");
-    tmp_strs[1] = MPL_strdup("system");
+    int num = 0;
 
-    /* match user kinds with supported kinds */
+    if (!gpu_init_done) {
+        init_gpu_kinds();
+    }
+
+    /* add kinds without restrictors */
+    for (int i = 0; memory_alloc_kinds[i]; i++) {
+        tmp_strs[i] = MPL_strdup(memory_alloc_kinds[i][0]);
+        num++;
+    }
+
+    /* match user requested kinds with supported kinds */
     if (requested_kinds != NULL) {
         char *tmp = MPL_strdup(requested_kinds);
         char *save_tmp = tmp;
         for (char *kind = MPL_strsep(&tmp, ","); kind; kind = MPL_strsep(&tmp, ",")) {
-            if (!MPL_stricmp(kind, "mpi") || !MPL_stricmp(kind, "system")) {
-                continue;
-            } else if (is_supported(kind)) {
-                tmp_strs[num++] = MPL_strdup(kind);
-                /* FIXME: use dynamic storage */
-                MPIR_Assert(num < 1024);
+            if (is_supported(kind)) {
+                /* check if kind is already in supported list */
+                bool found = false;
+                for (int i = 0; i < num; i++) {
+                    if (MPL_stricmp(kind, tmp_strs[i]) == 0) {
+                        found = true;
+                    }
+                }
+
+                if (!found) {
+                    tmp_strs[num++] = MPL_strdup(kind);
+                    /* FIXME: use dynamic storage */
+                    MPIR_Assert(num < 1024);
+                }
             }
         }
         MPL_free(save_tmp);
@@ -50,6 +73,20 @@ int MPIR_get_supported_memory_kinds(char *requested_kinds, char **out_kinds)
 
 /*** static functions ***/
 
+/* Requested kinds are of the form <kind>[:restrictor1][:restrictor2][...]
+ * MPICH does not differentiate any of the defined restrictors. Thus, if the
+ * user asks for a memory kind with one or more restrictors, we only check to
+ * see if those restrictors are valid. For example:
+ *
+ * supported:     cuda
+ * supported:     cuda:device
+ * supported:     cuda:device:managed
+ * supported:     mpi:alloc_mem
+ * supported:     rocm
+ * not supported: cuda:dev
+ * not supported: mpi:host
+ * not supported: system:foo
+ */
 static bool is_supported(const char *kind)
 {
     bool ret = false;
@@ -57,7 +94,8 @@ static bool is_supported(const char *kind)
     char *save_tmp = tmp;
 
     char *k = MPL_strsep(&tmp, ":");
-    for (int i = 0; memory_alloc_kinds[i][0]; i++) {
+
+    for (int i = 0; memory_alloc_kinds[i]; i++) {
         if (!MPL_stricmp(k, memory_alloc_kinds[i][0])) {
             ret = true;
 
@@ -79,4 +117,20 @@ static bool is_supported(const char *kind)
 
     MPL_free(save_tmp);
     return ret;
+}
+
+void init_gpu_kinds(void)
+{
+    if (MPIR_CVAR_ENABLE_GPU) {
+        MPL_gpu_type_t type;
+        MPL_gpu_query_support(&type);
+
+        if (type == MPL_GPU_TYPE_CUDA) {
+            memory_alloc_kinds[2] = cuda_kinds;
+        } else if (type == MPL_GPU_TYPE_HIP) {
+            memory_alloc_kinds[2] = rocm_kinds;
+        } else if (type == MPL_GPU_TYPE_ZE) {
+            memory_alloc_kinds[2] = ze_kinds;
+        }
+    }
 }
