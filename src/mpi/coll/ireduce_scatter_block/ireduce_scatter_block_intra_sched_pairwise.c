919dfdbedaf90934f8c056042bee1709533efea2
Hui Zhou <hzhou321@anl.gov>
2024-03-13 11:45:11 -0500

    coll: fix tructions in internal large count types
    This fixes following algorithms:
        ireduce_scatter_intra_sched_pairwise
        ireduce_scatter_block_intra_sched_pairwise
        reduce_scatter_intra_pairwise

--- src/mpi/coll/ireduce_scatter_block/ireduce_scatter_block_intra_sched_pairwise.c
+++ src/mpi/coll/ireduce_scatter_block/ireduce_scatter_block_intra_sched_pairwise.c
@@ -14,10 +14,10 @@ int MPIR_Ireduce_scatter_block_intra_sched_pairwise(const void *sendbuf, void *r
     int mpi_errno = MPI_SUCCESS;
     int rank, comm_size, i;
     MPI_Aint extent, true_extent, true_lb;
-    int *disps;
+    MPI_Aint *disps;
     void *tmp_recvbuf;
     int src, dst;
-    int total_count;
+    MPI_Aint total_count;
 
     comm_size = comm_ptr->local_size;
     rank = comm_ptr->rank;
@@ -31,7 +31,7 @@ int MPIR_Ireduce_scatter_block_intra_sched_pairwise(const void *sendbuf, void *r
     }
 #endif /* HAVE_ERROR_CHECKING */
 
-    disps = MPIR_Sched_alloc_state(s, comm_size * sizeof(int));
+    disps = MPIR_Sched_alloc_state(s, comm_size * sizeof(MPI_Aint));
     MPIR_ERR_CHKANDJUMP(!disps, mpi_errno, MPI_ERR_OTHER, "**nomem");
 
     total_count = 0;
