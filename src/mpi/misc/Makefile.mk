b6deea2f1c4e1d1b54ac4acc35d9b4a3d6d1fa2b
Ken Raffenetti <raffenet@mcs.anl.gov>
2024-03-15 11:10:47 -0500

    mpi/misc: Move GPU query support implementation to MPIR layer
    There is a use-case for this function to be called during MPI init in
    support the memory allocation kinds feature. Add an MPIR function which
    bypasses the MPI initialized check.

--- src/mpi/misc/Makefile.mk
+++ src/mpi/misc/Makefile.mk
@@ -6,4 +6,5 @@
 mpi_core_sources += \
     src/mpi/misc/utils.c \
     src/mpi/misc/f2c_impl.c \
-    src/mpi/misc/memory_alloc_kinds.c
+    src/mpi/misc/memory_alloc_kinds.c \
+    src/mpi/misc/gpu_query.c
