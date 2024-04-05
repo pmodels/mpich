84aa833a0df150cfdf13c38837a6f1eb50ce431f
Hui Zhou <hzhou321@anl.gov>
2024-04-05 12:34:03 -0500

    f77: use proxies for errhandler callbacks
    Use MPIX_{Comm,Win,File,Session}_create_errhandler_x, which allows
    passing an extra_state as context and a destructor for cleanup.

--- src/binding/fortran/mpif_h/user_proxy.c
+++ src/binding/fortran/mpif_h/user_proxy.c
@@ -5,6 +5,7 @@
 
 #include "mpi_fortimpl.h"
 #include "mpl.h"
+#include <assert.h>
 
 /* ---- user op ----------------- */
 struct F77_op_state {
@@ -35,6 +36,88 @@ int MPII_op_create(F77_OpFunction * opfn, MPI_Fint commute, MPI_Fint * op)
     int ret = MPIX_Op_create_x(F77_op_proxy, F77_op_free, commute, p, &op_i);
     if (ret == MPI_SUCCESS) {
         *op = MPI_Op_c2f(op_i);
+    } else {
+        MPL_free(p);
+    }
+
+    return ret;
+}
+
+/* ---- user errhandler  ----------------- */
+struct F77_errhan_state {
+    F77_ErrFunction *err_fn;
+};
+
+static void F77_comm_errhan_proxy(MPI_Comm comm, int error_code, void *extra_state)
+{
+    struct F77_errhan_state *p = extra_state;
+    MPI_Fint comm_i = MPI_Comm_c2f(comm);
+    MPI_Fint error_code_i = error_code;
+
+    p->err_fn(&comm_i, &error_code_i);
+}
+
+static void F77_win_errhan_proxy(MPI_Win win, int error_code, void *extra_state)
+{
+    struct F77_errhan_state *p = extra_state;
+    MPI_Fint win_i = MPI_Win_c2f(win);
+    MPI_Fint error_code_i = error_code;
+
+    p->err_fn(&win_i, &error_code_i);
+}
+
+static void F77_file_errhan_proxy(MPI_File file, int error_code, void *extra_state)
+{
+    struct F77_errhan_state *p = extra_state;
+    MPI_Fint file_i = MPI_File_c2f(file);
+    MPI_Fint error_code_i = error_code;
+
+    p->err_fn(&file_i, &error_code_i);
+}
+
+static void F77_session_errhan_proxy(MPI_Session session, int error_code, void *extra_state)
+{
+    struct F77_errhan_state *p = extra_state;
+    MPI_Fint session_i = MPI_Session_c2f(session);
+    MPI_Fint error_code_i = error_code;
+
+    p->err_fn(&session_i, &error_code_i);
+}
+
+static void F77_errhan_free(void *extra_state)
+{
+    MPL_free(extra_state);
+}
+
+int MPII_errhan_create(F77_ErrFunction * err_fn, MPI_Fint * errhandler, enum F77_handle_type type)
+{
+    struct F77_errhan_state *p = MPL_malloc(sizeof(struct F77_errhan_state), MPL_MEM_OTHER);
+    p->err_fn = err_fn;
+
+    MPI_Errhandler errhandler_i;
+    int ret = MPI_SUCCESS;
+    switch (type) {
+        case F77_COMM:
+            ret = MPIX_Comm_create_errhandler_x(F77_comm_errhan_proxy, F77_errhan_free,
+                                                p, &errhandler_i);
+            break;
+        case F77_WIN:
+            ret = MPIX_Win_create_errhandler_x(F77_win_errhan_proxy, F77_errhan_free,
+                                               p, &errhandler_i);
+            break;
+        case F77_FILE:
+            ret = MPIX_File_create_errhandler_x(F77_file_errhan_proxy, F77_errhan_free,
+                                                p, &errhandler_i);
+            break;
+        case F77_SESSION:
+            ret = MPIX_Session_create_errhandler_x(F77_session_errhan_proxy, F77_errhan_free,
+                                                   p, &errhandler_i);
+            break;
+        default:
+            assert(0);
+    }
+    if (ret == MPI_SUCCESS) {
+        *errhandler = MPI_Errhandler_c2f(errhandler_i);
     } else {
         MPL_free(p);
     }
