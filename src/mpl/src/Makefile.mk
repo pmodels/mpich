dbe6bbd5677a1bdffbd92c43698d69d882a02242
Hui Zhou <hzhou321@anl.gov>
2024-08-21 12:21:06 -0500

    mpl: collapse some Makefile.mk
    Collapse those trivial sub-Makefile.mk into src/Makefile.mk. This will
    make it simpler later when we need to split libmpl (e.g. split off the
    gpu dependency).

--- src/mpl/src/Makefile.mk
+++ src/mpl/src/Makefile.mk
@@ -3,20 +3,42 @@
 ##     See COPYRIGHT in top-level directory
 ##
 
-lib@MPLLIBNAME@_la_SOURCES += \
-    src/mpl_rankmap.c
+noinst_HEADERS += src/timer/mpl_timer_common.h
 
-include src/atomic/Makefile.mk
-include src/bt/Makefile.mk
-include src/dbg/Makefile.mk
-include src/env/Makefile.mk
-include src/mem/Makefile.mk
-include src/msg/Makefile.mk
-include src/sock/Makefile.mk
-include src/str/Makefile.mk
-include src/misc/Makefile.mk
-include src/thread/Makefile.mk
-include src/timer/Makefile.mk
-include src/shm/Makefile.mk
+lib@MPLLIBNAME@_la_SOURCES += \
+    src/mpl_rankmap.c \
+    src/atomic/mpl_atomic.c \
+    src/bt/mpl_bt.c \
+    src/dbg/mpl_dbg.c \
+    src/env/mpl_env.c \
+    src/mem/mpl_trmem.c \
+    src/msg/mpl_msg.c \
+    src/sock/mpl_sock.c \
+    src/sock/mpl_sockaddr.c \
+    src/sock/mpl_host.c \
+    src/str/mpl_str.c \
+    src/str/mpl_argstr.c \
+    src/str/mpl_arg_serial.c \
+    src/misc/mpl_misc.c \
+    src/misc/mpl_mkstemp.c \
+    src/thread/mpl_thread.c \
+    src/thread/mpl_thread_win.c \
+    src/thread/mpl_thread_solaris.c \
+    src/thread/mpl_thread_argobots.c \
+    src/thread/mpl_thread_qthreads.c \
+    src/thread/mpl_thread_posix.c \
+    src/thread/mpl_thread_uti.c \
+    src/timer/mpl_timer_clock_gettime.c \
+    src/timer/mpl_timer_gcc_ia64_cycle.c \
+    src/timer/mpl_timer_gethrtime.c \
+    src/timer/mpl_timer_gettimeofday.c \
+    src/timer/mpl_timer_linux86_cycle.c \
+    src/timer/mpl_timer_ppc64_cycle.c \
+    src/timer/mpl_timer_mach_absolute_time.c \
+    src/shm/mpl_shm.c \
+    src/shm/mpl_shm_sysv.c \
+    src/shm/mpl_shm_mmap.c \
+    src/shm/mpl_shm_win.c \
+    src/gavl/mpl_gavl.c
+
 include src/gpu/Makefile.mk
-include src/gavl/Makefile.mk
