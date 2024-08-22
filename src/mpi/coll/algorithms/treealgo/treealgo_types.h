7ea94c7d7cced9aabcbd3cce1dd5ab0385841eca
Hui Zhou <hzhou321@anl.gov>
2024-08-22 17:33:36 -0500

    coll: refactor caching tree in the comm struct
    Use a single "cached_tree" rather than 3 different fields for each tree
    type.

--- src/mpi/coll/algorithms/treealgo/treealgo_types.h
+++ src/mpi/coll/algorithms/treealgo/treealgo_types.h
@@ -8,6 +8,16 @@
 
 #include <utarray.h>
 
+/* enumerator for different tree types */
+typedef enum MPIR_Tree_type_t {
+    MPIR_TREE_TYPE_KARY = 0,
+    MPIR_TREE_TYPE_KNOMIAL_1,
+    MPIR_TREE_TYPE_KNOMIAL_2,
+    MPIR_TREE_TYPE_TOPOLOGY_AWARE,
+    MPIR_TREE_TYPE_TOPOLOGY_AWARE_K,
+    MPIR_TREE_TYPE_TOPOLOGY_WAVE,
+} MPIR_Tree_type_t;
+
 typedef struct {
     int rank;
     int nranks;
@@ -15,5 +25,21 @@ typedef struct {
     int num_children;
     UT_array *children;
 } MPIR_Treealgo_tree_t;
+
+typedef struct {
+    MPIR_Tree_type_t type;
+    int root;
+    union {
+        struct {
+            int k;
+        } topo_aware;
+        struct {
+            int overhead;
+            int lat_diff_groups;
+            int lat_diff_switches;
+            int lat_same_switches;
+        } topo_wave;
+    } u;
+} MPIR_Treealgo_param_t;
 
 #endif /* TREEALGO_TYPES_H_INCLUDED */
--- src/mpi/coll/include/coll_types.h
+++ src/mpi/coll/include/coll_types.h
@@ -13,16 +13,6 @@
 #define MPIR_COLL_FLAG_REDUCE_L 1
 #define MPIR_COLL_FLAG_REDUCE_R 0
 
-/* enumerator for different tree types */
-typedef enum MPIR_Tree_type_t {
-    MPIR_TREE_TYPE_KARY = 0,
-    MPIR_TREE_TYPE_KNOMIAL_1,
-    MPIR_TREE_TYPE_KNOMIAL_2,
-    MPIR_TREE_TYPE_TOPOLOGY_AWARE,
-    MPIR_TREE_TYPE_TOPOLOGY_AWARE_K,
-    MPIR_TREE_TYPE_TOPOLOGY_WAVE,
-} MPIR_Tree_type_t;
-
 /* enumerator for different recexch types */
 enum {
     MPIR_IALLREDUCE_RECEXCH_TYPE_SINGLE_BUFFER = 0,
