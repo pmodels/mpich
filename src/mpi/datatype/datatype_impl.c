43ddf0e86d4d63e7f3b6e0e02045d2792e1b7997
Ken Raffenetti <raffenet@mcs.anl.gov>
2024-09-16 16:19:20 -0500

    datatype: Fix UB warnings in MPIR_Get_address_impl
    Newer compilers warn about undefined behavior when doing arithmetic with
    NULL pointers. We can just return the pointer value as the address all
    the time.

--- src/mpi/datatype/datatype_impl.c
+++ src/mpi/datatype/datatype_impl.c
@@ -8,24 +8,7 @@
 
 int MPIR_Get_address_impl(const void *location, MPI_Aint * address)
 {
-    /* SX_4 needs to set CHAR_PTR_IS_ADDRESS
-     * The reason is that it computes the different in two pointers in
-     * an "int", and addresses typically have the high (bit 31) bit set;
-     * thus the difference, when cast as MPI_Aint (long), is sign-extended,
-     * making the absolute address negative.  Without a copy of the C
-     * standard, I can't tell if this is a compiler bug or a language bug.
-     */
-#ifdef CHAR_PTR_IS_ADDRESS
     *address = (MPI_Aint) location;
-#else
-    /* Note that this is the "portable" way to generate an address.
-     * The difference of two pointers is the number of elements
-     * between them, so this gives the number of chars between location
-     * and ptr.  As long as sizeof(char) represents one byte,
-     * of bytes from 0 to location */
-    *address = (MPI_Aint) ((char *) location - (char *) MPI_BOTTOM);
-#endif
-    /* The same code is used in MPI_Address */
     return MPI_SUCCESS;
 }
 
