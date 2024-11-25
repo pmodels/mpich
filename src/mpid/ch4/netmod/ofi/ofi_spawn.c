19fe5bc6115f6794496503e4846ee0f20e9aff65
Ken Raffenetti <raffenet@mcs.anl.gov>
2024-11-25 10:54:23 -0600

    ch4/ofi: Add NIC information to error output
    On systems with multiple NICs, it could be helpful to know which NIC an
    error was detected on in case there are hardware issues that need
    investigating. Add NIC information to error checking macros. For now, we
    report the default NIC used by each process. TODO: extend to support
    multi-nic usage and take the device number as input for more
    fine-grained reporting.

--- src/mpid/ch4/netmod/ofi/ofi_spawn.c
+++ src/mpid/ch4/netmod/ofi/ofi_spawn.c
@@ -55,8 +55,9 @@ int MPIDI_OFI_dynamic_send(uint64_t remote_gpid, int tag, const void *buf, int s
         int rc;
         rc = fi_cancel((fid_t) MPIDI_OFI_global.ctx[ctx_idx].tx, (void *) &req.context);
         if (rc && rc != -FI_ENOENT) {
-            MPIR_ERR_CHKANDJUMP1(rc < 0, mpi_errno, MPI_ERR_OTHER, "**ofid_cancel",
-                                 "**ofid_cancel %s", fi_strerror(-rc));
+            MPIR_ERR_CHKANDJUMP2(rc < 0, mpi_errno, MPI_ERR_OTHER, "**ofid_cancel",
+                                 "**ofid_cancel %s %s", MPIDI_OFI_DEFAULT_NIC_NAME,
+                                 fi_strerror(-rc));
 
         }
         while (!req.done) {
@@ -111,8 +112,9 @@ int MPIDI_OFI_dynamic_recv(int tag, void *buf, int size, int timeout)
         int rc;
         rc = fi_cancel((fid_t) MPIDI_OFI_global.ctx[ctx_idx].rx, (void *) &req.context);
         if (rc && rc != -FI_ENOENT) {
-            MPIR_ERR_CHKANDJUMP1(rc < 0, mpi_errno, MPI_ERR_OTHER, "**ofid_cancel",
-                                 "**ofid_cancel %s", fi_strerror(-rc));
+            MPIR_ERR_CHKANDJUMP2(rc < 0, mpi_errno, MPI_ERR_OTHER, "**ofid_cancel",
+                                 "**ofid_cancel %s %s", MPIDI_OFI_DEFAULT_NIC_NAME,
+                                 fi_strerror(-rc));
 
         }
         while (!req.done) {
