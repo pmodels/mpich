152f0102dfeddcd3dfa4d73ab2cc1334e3c7b8ee
Sonja Happ <sonja.happ@par-tec.com>
2024-11-26 16:51:36 +0100

    session: fix typo in include pragma

--- src/include/mpir_session.h
+++ src/include/mpir_session.h
@@ -59,4 +59,4 @@ int MPIR_Session_get_pset_info_impl(MPIR_Session * session_ptr, const char *pset
 int MPIR_Session_init_impl(MPIR_Info * info_ptr, MPIR_Errhandler * errhandler_ptr,
                            MPIR_Session ** session_ptr);
 
-#endif /* MPIR_SESSSION_H_INCLUDED */
+#endif /* MPIR_SESSION_H_INCLUDED */
