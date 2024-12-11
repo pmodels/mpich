6355b580d74876d9ae4ed82482614784074dbfd8
Hui Zhou <hzhou321@anl.gov>
2024-12-11 16:22:31 -0600

    group: add builtin MPIR_GROUP_{WORLD,SELF}
    Add builtin MPIR_GROUP_WORLD and MPIR_GROUP_SELF, so we can create
    builtin communicators from builtin groups.

--- src/include/mpir_group.h
+++ src/include/mpir_group.h
@@ -43,6 +43,13 @@
 
  S*/
 
+/* In addition to MPI_GROUP_EMPTY, internally we have a few more builtins */
+#define MPIR_GROUP_WORLD  ((MPI_Group)0x48000001)
+#define MPIR_GROUP_SELF   ((MPI_Group)0x48000002)
+
+#define MPIR_GROUP_WORLD_PTR (MPIR_Group_builtin + 1)
+#define MPIR_GROUP_SELF_PTR  (MPIR_Group_builtin + 2)
+
 /* Worlds -
  * We need a device-independent way of identifying processes. Assuming the concept of
  * "worlds", we can describe a process with (world_idx, world_rank).
