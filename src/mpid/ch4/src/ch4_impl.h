421ee8300bb64cf927f011553c2037511a2ac345
Hui Zhou <hzhou321@anl.gov>
2024-12-11 18:57:37 -0600

    mpid: replace usage of uint64_t lpid with MPIR_Lpid
    There is no real difference between lpid and gpid. Thus rename gpid in
    the device layer to lpid for clarification.
    
    Replace the usage of uint64_t as the type of lpid to MPIR_Lpid. This
    improves consistency.

--- src/mpid/ch4/src/ch4_impl.h
+++ src/mpid/ch4/src/ch4_impl.h
@@ -16,7 +16,7 @@
 int MPIDIU_Intercomm_map_bcast_intra(MPIR_Comm * local_comm, int local_leader, int *remote_size,
                                      int *is_low_group, int pure_intracomm,
                                      int *remote_upid_size, char *remote_upids,
-                                     uint64_t ** remote_gpids);
+                                     MPIR_Lpid ** remote_lpids);
 int MPIDIG_get_context_index(uint64_t context_id);
 uint64_t MPIDIG_generate_win_id(MPIR_Comm * comm_ptr);
 
