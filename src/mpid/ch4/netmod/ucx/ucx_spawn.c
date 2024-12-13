e0db5a8d4a8a1f130ade50e8a6d1504305fe8fb9
Hui Zhou <hzhou321@anl.gov>
2024-12-12 21:04:05 -0600

    ch4: new netmod api to support dynamic processes
    Add -
    
    * MPIDI_NM_get_dynamic_av - obtain a temporary av from a upid (address
    name).
    
    * MPIDI_NM_dynamic_sendrecv - used by local group leaders to exchange
    data over dynamic_av.

--- src/mpid/ch4/netmod/ucx/ucx_spawn.c
+++ src/mpid/ch4/netmod/ucx/ucx_spawn.c
@@ -121,6 +121,79 @@ int MPIDI_UCX_dynamic_recv(int tag, void *buf, int size, int timeout)
     return mpi_errno;
 }
 
+int MPIDI_UCX_dynamic_sendrecv(MPIR_Lpid remote_lpid, int tag,
+                               const void *send_buf, int send_size, void *recv_buf, int recv_size)
+{
+    int mpi_errno = MPI_SUCCESS;
+
+    /* NOTE: dynamic_sendrecv is always called inside CS of vci 0 */
+    int vci = 0;
+#ifdef MPICH_DEBUG_MUTEX
+    MPID_THREAD_ASSERT_IN_CS(VCI, (*(MPID_Thread_mutex_t *) MPIR_Request_mem[vci].lock));
+#endif
+
+    uint64_t ucx_tag = MPIDI_UCX_DYNPROC_MASK + tag;
+    uint64_t tag_mask = 0xffffffffffffffff;     /* for recv */
+    MPIDI_av_entry_t *av = MPIDIU_lpid_to_av_slow(remote_lpid);
+    ucp_ep_h ep = MPIDI_UCX_AV_TO_EP(av, vci, vci);
+
+    ucs_status_ptr_t status;
+
+    /* send */
+    bool send_done = false;
+    if (send_size > 0) {
+        ucp_request_param_t send_param = {
+            .op_attr_mask = UCP_OP_ATTR_FIELD_CALLBACK | UCP_OP_ATTR_FIELD_USER_DATA,
+            .cb.send = dynamic_send_cb,
+            .user_data = &send_done,
+        };
+
+        status = ucp_tag_send_nbx(ep, send_buf, send_size, ucx_tag, &send_param);
+        if (status == UCS_OK) {
+            send_done = true;
+        } else if (UCS_PTR_IS_ERR(status)) {
+            /* FIXME: better error */
+            mpi_errno = MPI_ERR_PORT;
+            goto fn_exit;
+        }
+    } else {
+        send_done = true;
+    }
+
+    /* recv */
+    bool recv_done = false;
+    if (recv_size > 0) {
+        ucp_request_param_t recv_param = {
+            .op_attr_mask = UCP_OP_ATTR_FIELD_CALLBACK | UCP_OP_ATTR_FIELD_USER_DATA,
+            .cb.recv = dynamic_recv_cb,
+            .user_data = &recv_done,
+        };
+
+        status = ucp_tag_recv_nbx(MPIDI_UCX_global.ctx[vci].worker, recv_buf, recv_size,
+                                  ucx_tag, tag_mask, &recv_param);
+        if (status == UCS_OK) {
+            recv_done = true;
+        } else if (UCS_PTR_IS_ERR(status)) {
+            /* FIXME: better error */
+            mpi_errno = MPI_ERR_PORT;
+            goto fn_exit;
+        }
+    } else {
+        recv_done = true;
+    }
+
+    /* wait */
+    while (!send_done || !recv_done) {
+        ucp_worker_progress(MPIDI_UCX_global.ctx[vci].worker);
+    }
+
+  fn_exit:
+    if (status != UCS_OK) {
+        ucp_request_release(status);
+    }
+    return mpi_errno;
+}
+
 int MPIDI_UCX_get_local_upids(MPIR_Comm * comm, int **local_upid_size, char **local_upids)
 {
     int mpi_errno = MPI_SUCCESS;
@@ -145,6 +218,41 @@ int MPIDI_UCX_get_local_upids(MPIR_Comm * comm, int **local_upid_size, char **lo
   fn_fail:
     MPIR_CHKPMEM_REAP();
     goto fn_exit;
+}
+
+int MPIDI_UCX_insert_upid(MPIR_Lpid lpid, const char *upid, int upid_len)
+{
+    MPIDI_av_entry_t *av = MPIDIU_lpid_to_av_slow(lpid);
+
+    bool is_dynamic = (lpid & MPIR_LPID_DYNAMIC_MASK);
+    bool do_insert;
+    if (is_dynamic) {
+        do_insert = true;
+    } else if (!av->dest[0][0]) {
+        MPIDI_av_entry_t *dynamic_av = MPIDIU_find_dynamic_av(upid, upid_len);
+        if (dynamic_av) {
+            /* just copy it over */
+            av->dest[0][0] = dynamic_av->dest[0][0];
+            do_insert = false;
+        } else {
+            do_insert = true;
+        }
+    }
+
+    if (do_insert) {
+        /* new entry */
+        ucp_ep_params_t ep_params;
+        ucs_status_t ucx_status;
+        ep_params.field_mask = UCP_EP_PARAM_FIELD_REMOTE_ADDRESS;
+        ep_params.address = (ucp_address_t *) upid;
+        ucx_status = ucp_ep_create(MPIDI_UCX_global.ctx[0].worker, &ep_params, &av->dest[0][0]);
+        MPIDI_UCX_CHK_STATUS(ucx_status);
+    }
+
+    if (!is_dynamic) {
+        MPIDIU_upidhash_add(upid, upid_len, lpid);
+    }
+    return MPI_SUCCESS;
 }
 
 int MPIDI_UCX_upids_to_lpids(int size, int *remote_upid_size, char *remote_upids,
