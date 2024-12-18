1f26014f3b2ca885aba3a5de0873c907895bcfd7
Hui Zhou <hzhou321@anl.gov>
2024-12-17 18:06:50 -0600

    ch4: refactor ch4_spawn

--- src/mpid/ch4/src/ch4_proc.c
+++ src/mpid/ch4/src/ch4_proc.c
@@ -246,7 +246,8 @@ int MPIDIU_insert_dynamic_upid(MPIR_Lpid * lpid_out, const char *upid, int upid_
     av->upid = upid;
     av->upid_len = upid_len;
 
-    /* TODO: call MPIDI_NM_insert_upid to enable communication */
+    mpi_errno = MPIDI_NM_insert_upid(*lpid_out, upid, upid_len);
+    MPIR_ERR_CHECK(mpi_errno);
 
     MPIDIU_DYN_AV_TABLE.size++;
 
