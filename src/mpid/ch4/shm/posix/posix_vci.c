5435ef123557d0a3aa545ea5ff81350dfb4d66f3
Hui Zhou <hzhou321@anl.gov>
2024-12-26 00:08:57 -0600

    ch4: add netmod/shm API comm_set_vcis
    Add netmod and shm API MPIDI_{NM,SHM}_comm_set_vcis so
    MPIDI_Comm_set_vcis can call to set up multiple vcis and multiple nics.

---
+++ src/mpid/ch4/shm/posix/posix_vci.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) by Argonne National Laboratory
+ *     See COPYRIGHT in top-level directory
+ */
+
+#include "mpidimpl.h"
+#include "posix_types.h"
+
+int MPIDI_POSIX_comm_set_vcis(MPIR_Comm * comm, int num_vcis)
+{
+    int mpi_errno = MPI_SUCCESS;
+    return mpi_errno;
+}
