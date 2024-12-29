5ec8ab4f625e8409eed419a63a1c01b1a13a052c
Hui Zhou <hzhou321@anl.gov>
2024-12-29 10:54:49 -0600

    ch4: do multiple vci creations in MPIDI_Comm_set_vcis
    * Call MPIDI_{NM,SHM}_comm_set_vcis in MPIDI_Comm_set_vcis.
    * Combine creation of additional vcis and all address excahange in
    MPIDI_NM_set_vcis.
    * Move additional vci init from MPIDI_POSIX_post_init to
    MPIDI_POSIX_comm_set_vcis.

--- src/mpid/ch4/netmod/ucx/ucx_vci.c
+++ src/mpid/ch4/netmod/ucx/ucx_vci.c
@@ -9,5 +9,28 @@
 int MPIDI_UCX_comm_set_vcis(MPIR_Comm * comm, int num_vcis, int *all_num_vcis)
 {
     int mpi_errno = MPI_SUCCESS;
+
+    MPIDI_UCX_global.num_vcis = num_vcis;
+
+    /* set up local vcis */
+    for (int i = 1; i < MPIDI_UCX_global.num_vcis; i++) {
+        mpi_errno = MPIDI_UCX_init_worker(i);
+        MPIR_ERR_CHECK(mpi_errno);
+    }
+
+    /* UCX netmod only support the same number of vcis on all procs */
+    for (int i = 0; i < comm->local_size; i++) {
+        all_num_vcis[i] = num_vcis;
+    }
+
+    /* address exchange */
+    if (num_vcis > 1) {
+        mpi_errno = MPIDI_UCX_all_vcis_address_exchange();
+        MPIR_ERR_CHECK(mpi_errno);
+    }
+
+  fn_exit:
     return mpi_errno;
+  fn_fail:
+    goto fn_exit;
 }
