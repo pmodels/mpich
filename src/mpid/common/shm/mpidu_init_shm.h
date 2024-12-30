9d5025ceb2d5b2b214fd47f2853f60e63da41938
Hui Zhou <hzhou321@anl.gov>
2024-12-30 10:10:41 -0600

    init_shm: add MPIDU_Init_shm_comm_alloc
    Add routine to support allocating a shared memory by a comm, which allows -
    
    * create shared memory by a smaller comm than a comm_world
    * attach the shared memory by later processes
    * potentially allowing shm communication with dynamic processes - we
    need a way to discover and attach to Init_shm (via intercomm) and the
    initial shared memory need pre-allocate to account for new processes.
    
    For now, we need this to support MPIDI_POSIX_comm_set_vcis.

--- src/mpid/common/shm/mpidu_init_shm.h
+++ src/mpid/common/shm/mpidu_init_shm.h
@@ -17,10 +17,6 @@
        shared memory will not work properly. Consider disable it with --enable-nolocal.
 #endif
 
-typedef struct MPIDU_Init_shm_block {
-    char block[MPIDU_INIT_SHM_BLOCK_SIZE];
-} MPIDU_Init_shm_block_t;
-
 int MPIDU_Init_shm_init(void);
 int MPIDU_Init_shm_finalize(void);
 int MPIDU_Init_shm_barrier(void);
@@ -31,5 +27,14 @@ int MPIDU_Init_shm_query(int local_rank, void **target_addr);
 int MPIDU_Init_shm_alloc(size_t len, void **ptr);
 int MPIDU_Init_shm_free(void *ptr);
 int MPIDU_Init_shm_is_symm(void *ptr);
+
+/* support routines for MPIDU_Init_shm_root_alloc */
+int MPIDU_Init_shm_atomic_count(void);
+int MPIDU_Init_shm_atomic_put(void *orig, size_t len);
+int MPIDU_Init_shm_atomic_get(void *target, size_t len);
+
+/* comm root allocate a shared memory and put the serialized handle if previously not allocated,
+ * Otherwise, retrieve the handle and attach. Broadcast the handle to rest of the comm */
+int MPIDU_Init_shm_comm_alloc(MPIR_Comm * comm, size_t len, void **ptr);
 
 #endif /* MPIDU_INIT_SHM_H_INCLUDED */
