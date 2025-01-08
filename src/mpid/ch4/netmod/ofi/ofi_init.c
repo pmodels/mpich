4933770d4c05068ec4873efd6a63c56551cb70b1
Hui Zhou <hzhou321@anl.gov>
2025-01-07 22:39:44 -0600

    ch4/ofi: use both local and remote vci/nic in av_to_phys
    We may relax the av insertion order which may require the full
    (vci_local, nic_local, vci_remote, nic_remote) to look up an actual
    destination address.
    
    Add MPIDI_OFI_av_to_phys_root for convenience and quick survey on where
    we restrict in only root vci (such as the init and spawn paths).
    
    Remove MPIDI_OFI_comm_to_phys and prefer an explicit
    MPIDIU_comm_rank_to_av and then MPIDI_OFI_av_to_phys.
    
    Refactor MPIDI_OFI_SET_AM_HDR_COMMON in ofi_am_impl.h to directly use
    dst_addr (as remote_id) rather than to recalculate it.

--- src/mpid/ch4/netmod/ofi/ofi_init.c
+++ src/mpid/ch4/netmod/ofi/ofi_init.c
@@ -953,7 +953,7 @@ static int flush_send(int dst, int nic, int vci, MPIDI_OFI_dynamic_process_reque
 {
     int mpi_errno = MPI_SUCCESS;
 
-    fi_addr_t addr = MPIDI_OFI_av_to_phys(&MPIDIU_get_av(0, dst), nic, vci);
+    fi_addr_t addr = MPIDI_OFI_av_to_phys(&MPIDIU_get_av(0, dst), vci, nic, vci, nic);
     static int data = 0;
     uint64_t match_bits =
         MPIDI_OFI_init_sendtag(MPIDI_OFI_FLUSH_CONTEXT_ID, 0, MPIDI_OFI_FLUSH_TAG);
@@ -984,7 +984,7 @@ static int flush_recv(int src, int nic, int vci, MPIDI_OFI_dynamic_process_reque
 {
     int mpi_errno = MPI_SUCCESS;
 
-    fi_addr_t addr = MPIDI_OFI_av_to_phys(&MPIDIU_get_av(0, src), nic, vci);
+    fi_addr_t addr = MPIDI_OFI_av_to_phys(&MPIDIU_get_av(0, src), vci, nic, vci, nic);
     uint64_t mask_bits = 0;
     uint64_t match_bits =
         MPIDI_OFI_init_sendtag(MPIDI_OFI_FLUSH_CONTEXT_ID, 0, MPIDI_OFI_FLUSH_TAG);
