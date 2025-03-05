6a6274f44c05c79bcaab0b84f35de97433829bc5
Ken Raffenetti <raffenet@mcs.anl.gov>
2025-03-05 09:54:59 -0600

    ch4/ipc: Replace mapped GPU IPC handle cache
    Remove mapped buffer cache using AVL trees and replace with a hash
    table. The key for the hash is the combination of the sender base
    address and their local rank on the node. Each entry can store a mapped
    address for each device visible to the process doing the mapping.

--- src/mpid/ch4/shm/ipc/gpu/gpu_types.h
+++ src/mpid/ch4/shm/ipc/gpu/gpu_types.h
@@ -8,13 +8,22 @@
 
 #include "uthash.h"
 
+struct MPIDI_GPUI_map_cache_entry {
+    UT_hash_handle hh;
+    struct map_key {
+        int remote_rank;
+        const void *remote_addr;
+    } key;
+    const void *mapped_addrs[]; /* array of base addresses indexed by device id */
+};
+
 typedef struct {
     int *local_ranks;
     int *local_procs;
     int local_device_count;
     int global_max_dev_id;
     int initialized;
-    MPL_gavl_tree_t ***ipc_handle_mapped_trees;
+    struct MPIDI_GPUI_map_cache_entry *ipc_map_cache;
     MPL_gavl_tree_t **ipc_handle_track_trees;
 } MPIDI_GPUI_global_t;
 
