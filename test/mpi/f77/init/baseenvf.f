6859af592e49e3342452e5b93398dce3e13874d4
Hui Zhou <hzhou321@anl.gov>
2025-03-21 13:59:33 -0500

    test: update baseenv tests for MPI_VERSION
    MPI_VERSION has been upgraded to 5.

--- test/mpi/f77/init/baseenvf.f
+++ test/mpi/f77/init/baseenvf.f
@@ -45,7 +45,7 @@ C
      &              MPI_SUBVERSION
           print *, 'Version in get_version is ', iv, '.', isubv
        endif
-       if (iv .lt. 1 .or. iv .gt. 4) then
+       if (iv .lt. 1 .or. iv .gt. 5) then
           errs = errs + 1
           print *, 'Version of MPI is invalid (=', iv, ')'
        endif
