8429916c987b3207a64cac37abd8b3b5a60410e9
Hui Zhou <hzhou321@anl.gov>
2025-03-25 16:09:32 -0500

    ch4/ipc: add count to MPIDI_IPCI_is_repeat_addr
    Track how often the address is being repeated.

--- src/mpid/ch4/shm/ipc/src/globals.c
+++ src/mpid/ch4/shm/ipc/src/globals.c
@@ -17,8 +17,15 @@ cvars:
       verbosity   : MPI_T_VERBOSITY_USER_BASIC
       scope       : MPI_T_SCOPE_ALL_EQ
       description : >-
-        If an address is used more than once in the last ten send operations,
-        map it for IPC use even if it is below the IPC threshold.
+        Enable to track how often a buffer is being sent repeatedly. This will
+        be used in determine whether to use IPC algorithm to deliver the message.
+        The choice will depend on the IPC driver. In the case of high-latency
+        buffers such as GPU device buffer, we will enable IPC if EITHER the
+        message size is above a threshold or the message buffer is being repeated.
+        On the other hand, if the address mapping overhead is relatively high,
+        such as the case for XPMEM, we will enable IPC when BOTH conditions --
+        message size and repeat count -- are met.
+
 === END_MPI_T_CVAR_INFO_BLOCK ===
 */
 
@@ -28,22 +35,25 @@ MPIDI_IPCI_global_t MPIDI_IPCI_global;
 MPL_dbg_class MPIDI_IPCI_DBG_GENERAL;
 #endif
 
-bool MPIDI_IPCI_is_repeat_addr(void *addr)
+int MPIDI_IPCI_is_repeat_addr(const void *addr)
 {
     if (!MPIR_CVAR_CH4_IPC_MAP_REPEAT_ADDR) {
-        return false;
+        return 0;
     }
-
-    static void *repeat_addr[10] = { 0 };
+#define MAX_REPEAT_ADDRS 16     /* a bit arbitrary - TODO: make it tunable */
+    static const void *repeat_addr[MAX_REPEAT_ADDRS] = { 0 };
+    static int repeat_addr_count[MAX_REPEAT_ADDRS];
     static int addr_idx = 0;
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < MAX_REPEAT_ADDRS; i++) {
         if (addr == repeat_addr[i]) {
-            return true;
+            repeat_addr_count[i]++;
+            return repeat_addr_count[i];
         }
     }
 
     repeat_addr[addr_idx] = addr;
-    addr_idx = (addr_idx + 1) % 10;
-    return false;
+    repeat_addr_count[addr_idx] = 0;
+    addr_idx = (addr_idx + 1) % MAX_REPEAT_ADDRS;
+    return 0;
 }
