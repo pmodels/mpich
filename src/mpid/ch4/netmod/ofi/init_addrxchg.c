01beaeac8971e1dc2a5701c7de109775bbe32802
Hui Zhou <hzhou321@anl.gov>
2025-04-01 09:29:25 -0500

    ch4/ofi: add MPIDI_OFI_comm_addr_exchange
    Similar to MPIDI_OFI_addr_exchange_root_ctx but work within a
    communicator. We will replace MPIDI_OFI_addr_exchange_root_ctx() with
    MPIDI_OFI_comm_addr_exchange(comm_world).

--- src/mpid/ch4/netmod/ofi/init_addrxchg.c
+++ src/mpid/ch4/netmod/ofi/init_addrxchg.c
@@ -8,35 +8,121 @@
 #include "ofi_init.h"
 #include "mpidu_bc.h"
 
-/* NOTE on av insertion order:
- *
- * Each nic-vci is an endpoint with a unique address, and inside libfabric maintains
- * one av table. Thus to fully store the address mapping, we'll need a multi-dim table as
- *     av_table[src_nic][src_vci][dest_rank][dest_nic][dest_vci]
- * Note, this table is for illustration, and different from MPIDI_OFI_addr_t.
- *
- * However, if we insert the address carefully, we can manage to make the av table inside
- * each endpoint *identical*. Then, we can omit the dimension of [src_nic][src_vci].
- *
- * To achieve that, we use the following 3-step process (described with above illustrative av_table).
- *
- * Step 1. insert and store       av_table[ 0 ][ 0 ][rank][ 0 ][ 0 ]
- *
- * Step 2. insert and store       av_table[ 0 ][ 0 ][rank][nic][vci]
- *
- * Step 3. insert (but not store) av_table[nic][vci][rank][nic][vci]
- *
- * The step 1 is done in addr_exchange_root_vci. Step 2 and 3 are done in addr_exchange_all_vcis.
- * Step 3 populates av tables inside libfabric for all non-zero endpoints, but they should be
- * identical to the table in root endpoint, thus no need to store them in mpich. Thus the table is
- * reduced to
- *      av_table[rank] -> dest[nic][vci]
- *
- * With single-nic and single-vci, only step 1 is needed.
- *
- * We do step 1 during world-init, and step 2 & 3 during post-init. The separation
- * isolates multi-nic/vci complications from bootstrapping phase.
+/* Perform address exchange and enable netmod communication over the comm. */
+
+/* NOTE: we assume the intranode shm communication is already enabled.
+ * NOTE: the root av no longer require fixed insertion order. See comments in ofi_vci.c.
  */
+int MPIDI_OFI_comm_addr_exchange(MPIR_Comm * comm)
+{
+    int mpi_errno = MPI_SUCCESS;
+    MPIR_CHKLMEM_DECL();
+
+    /* only comm_world for now
+     * TODO: Use an attribute to mark whether this is a comm from
+     *       MPI_Comm_create_from_group and to be checked in NM_mpi_comm_pre_hook.
+     */
+    MPIR_Assert(comm == MPIR_Process.comm_world);
+
+    MPIR_Assert(comm->attr & MPIR_COMM_ATTR__HIERARCHY);
+
+    /* First, each get its own name */
+    MPIDI_OFI_global.addrnamelen = FI_NAME_MAX;
+    MPIDI_OFI_CALL(fi_getname
+                   ((fid_t) MPIDI_OFI_global.ctx[0].ep, MPIDI_OFI_global.addrname[0],
+                    &MPIDI_OFI_global.addrnamelen), getname);
+
+    char *addrname = MPIDI_OFI_global.addrname[0];
+    int addrnamelen = MPIDI_OFI_global.addrnamelen;
+
+    int local_rank = comm->local_rank;
+    int external_size = comm->num_external;
+
+    if (external_size == 1) {
+        /* skip root addrexch if we are the only node */
+        goto all_addrexch;
+    }
+
+    /* PMI allgather over node roots and av_insert to activate node_roots_comm */
+    if (local_rank == 0) {
+        char *roots_names;
+        MPIR_CHKLMEM_MALLOC(roots_names, external_size * addrnamelen);
+
+        MPIR_PMI_DOMAIN domain = MPIR_PMI_DOMAIN_NODE_ROOTS;
+        mpi_errno = MPIR_pmi_allgather(addrname, addrnamelen, roots_names, addrnamelen, domain);
+        MPIR_ERR_CHECK(mpi_errno);
+
+        /* insert av and activate node_roots_comm */
+        MPIR_Comm *node_roots_comm = MPIR_Comm_get_node_roots_comm(comm);
+        for (int i = 0; i < external_size; i++) {
+            MPIDI_av_entry_t *av = MPIDIU_comm_rank_to_av(node_roots_comm, i);
+            MPIR_Lpid lpid = MPIR_comm_rank_to_lpid(node_roots_comm, i);
+            if (MPIDI_OFI_AV_IS_UNSET(av, lpid)) {
+                MPIDI_OFI_CALL(fi_av_insert
+                               (MPIDI_OFI_global.ctx[0].av, roots_names + i * addrnamelen, 1,
+                                &MPIDI_OFI_AV_ADDR_ROOT(av), 0ULL, NULL), avmap);
+                if (MPIDI_OFI_AV_ADDR_ROOT(av) == 0) {
+                    MPIDI_OFI_global.lpid0 = lpid;
+                }
+            }
+        }
+    } else {
+        /* just for the PMI_Barrier */
+        MPIR_PMI_DOMAIN domain = MPIR_PMI_DOMAIN_NODE_ROOTS;
+        mpi_errno = MPIR_pmi_allgather(addrname, addrnamelen, NULL, addrnamelen, domain);
+        MPIR_ERR_CHECK(mpi_errno);
+    }
+
+  all_addrexch:
+    if (external_size == comm->local_size) {
+        /* if no local, we are done. */
+        goto fn_exit;
+    }
+
+    /* -- rest of the addr exchange over node_code and node_roots_comm -- */
+    /* since the orders will be rearranged by nodes, let's echange rank along with name */
+    struct rankname {
+        int rank;
+        char name[];
+    };
+    int rankname_len = sizeof(int) + addrnamelen;
+
+    struct rankname *my_rankname, *all_ranknames;
+    MPIR_CHKLMEM_MALLOC(my_rankname, rankname_len);
+    MPIR_CHKLMEM_MALLOC(all_ranknames, comm->local_size * rankname_len);
+
+    my_rankname->rank = comm->rank;
+    memcpy(my_rankname->name, addrname, addrnamelen);
+
+    /* Use an smp algorithm explicitly that only require a working node_comm and node_roots_comm.
+     * The node_comm is activated by MPIDI_SHM_mpi_comm_commit_pre_hook. The node_roots_comm
+     * is activated by the earlier address exchange using PMI.
+     */
+    mpi_errno = MPIR_Allgather_intra_smp_no_order(my_rankname, rankname_len, MPIR_BYTE_INTERNAL,
+                                                  all_ranknames, rankname_len, MPIR_BYTE_INTERNAL,
+                                                  comm, MPIR_ERR_NONE);
+    MPIR_ERR_CHECK(mpi_errno);
+
+    /* av insert, skipping over existing entries */
+    for (int i = 0; i < comm->local_size; i++) {
+        struct rankname *p = (struct rankname *) ((char *) all_ranknames + i * rankname_len);
+        MPIDI_av_entry_t *av = MPIDIU_comm_rank_to_av(comm, p->rank);
+        MPIR_Lpid lpid = MPIR_comm_rank_to_lpid(comm, p->rank);
+        if (MPIDI_OFI_AV_IS_UNSET(av, lpid)) {
+            MPIDI_OFI_CALL(fi_av_insert(MPIDI_OFI_global.ctx[0].av, p->name, 1,
+                                        &MPIDI_OFI_AV_ADDR_ROOT(av), 0ULL, NULL), avmap);
+            if (MPIDI_OFI_AV_ADDR_ROOT(av) == 0) {
+                MPIDI_OFI_global.lpid0 = lpid;
+            }
+        }
+    }
+
+  fn_exit:
+    MPIR_CHKLMEM_FREEALL();
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
+}
 
 /* Step 1: exchange root contexts */
 int MPIDI_OFI_addr_exchange_root_ctx(void)
