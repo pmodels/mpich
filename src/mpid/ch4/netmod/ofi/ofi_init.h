e3edf9e88dadb9c4817739dae0cb2a08fbb1b651
Hui Zhou <hzhou321@anl.gov>
2025-04-02 13:20:07 -0500

    ch4/ofi: remove MPIDI_OFI_addr_exchange_root_ctx
    MPIDI_OFI_addr_exchange_root_ctx is replaced by
    MPIDI_OFI_comm_addr_exchange.

--- src/mpid/ch4/netmod/ofi/ofi_init.h
+++ src/mpid/ch4/netmod/ofi/ofi_init.h
@@ -34,7 +34,6 @@ void MPIDI_OFI_update_global_settings(struct fi_info *prov);
 bool MPIDI_OFI_nic_already_used(const struct fi_info *prov, struct fi_info **others, int nic_count);
 
 int MPIDI_OFI_create_vci_context(int vci, int nic);
-int MPIDI_OFI_addr_exchange_root_ctx(void);
 int MPIDI_OFI_am_init(int vci);
 int MPIDI_OFI_am_post_recv(int vci, int nic);
 
