0860c206b6ae429fc49d562b91e229d9420def6d
Hui Zhou <hzhou321@anl.gov>
2025-04-14 12:18:16 -0500

    ch4/xpmem: MPIDI_XPMEM_comm_bootstrap not assuming comm_world
    XPMEM exchanges MPIDI_XPMEMI_global.segid in MPIDI_XPMEM_comm_bootstrap.
    Assuming the node_comm is already working after
    MPIDI_POSIX_comm_bootstrap, we can direct use MPIR_Allgather_impl over
    node_comm rather than Init_shm.

--- src/mpid/ch4/shm/ipc/xpmem/xpmem_init.c
+++ src/mpid/ch4/shm/ipc/xpmem/xpmem_init.c
@@ -8,19 +8,13 @@
 #include "mpidu_init_shm.h"
 #include "xpmem_seg.h"
 
-static int xpmem_initialized = 0;
-
 int MPIDI_XPMEM_init_local(void)
 {
-    return MPI_SUCCESS;
-}
-
-int MPIDI_XPMEM_comm_bootstrap(MPIR_Comm * comm)
-{
     int mpi_errno = MPI_SUCCESS;
-    int i;
+    MPIR_FUNC_ENTER;
+    MPIR_CHKPMEM_DECL();
 
-    MPIR_Assert(comm == MPIR_Process.comm_world);
+    MPIDI_XPMEMI_global.segid = -1;
 
     /* If the user has disabled XPMEM, don't even try to initialize it. */
     if (!MPIR_CVAR_CH4_XPMEM_ENABLE) {
@@ -30,10 +24,6 @@ int MPIDI_XPMEM_comm_bootstrap(MPIR_Comm * comm)
     if (MPIR_Process.local_size == 1) {
         goto fn_exit;
     }
-
-    MPIR_FUNC_ENTER;
-    MPIR_CHKPMEM_DECL();
-
 #ifdef MPL_USE_DBG_LOGGING
     MPIDI_XPMEMI_DBG_GENERAL = MPL_dbg_class_alloc("SHM_IPC_XPMEM", "shm_ipc_xpmem");
 #endif /* MPL_USE_DBG_LOGGING */
@@ -53,20 +43,14 @@ int MPIDI_XPMEM_comm_bootstrap(MPIR_Comm * comm)
     /* 64-bit segment ID */
     XPMEM_TRACE("init: make segid: 0x%lx\n", (uint64_t) MPIDI_XPMEMI_global.segid);
 
-    MPIDU_Init_shm_put(&MPIDI_XPMEMI_global.segid, sizeof(xpmem_segid_t));
-    MPIDU_Init_shm_barrier();
-
     /* Initialize segmap for every local processes */
     MPIDI_XPMEMI_global.segmaps = NULL;
     MPIR_CHKPMEM_MALLOC(MPIDI_XPMEMI_global.segmaps,
                         sizeof(MPIDI_XPMEMI_segmap_t) * MPIR_Process.local_size, MPL_MEM_SHM);
-    for (i = 0; i < MPIR_Process.local_size; i++) {
-        MPIDU_Init_shm_get(i, sizeof(xpmem_segid_t), &MPIDI_XPMEMI_global.segmaps[i].remote_segid);
-        MPIR_ERR_CHKANDJUMP(MPIDI_XPMEMI_global.segmaps[i].remote_segid == -1, mpi_errno,
-                            MPI_ERR_OTHER, "**xpmem_segid");
+    for (int i = 0; i < MPIR_Process.local_size; i++) {
+        MPIDI_XPMEMI_global.segmaps[i].remote_segid = -1;
         MPIDI_XPMEMI_global.segmaps[i].apid = -1;       /* get apid at the first communication  */
     }
-    MPIDU_Init_shm_barrier();
 
     /* Initialize other global parameters */
     MPIDI_XPMEMI_global.sys_page_sz = (size_t) sysconf(_SC_PAGESIZE);
@@ -75,13 +59,10 @@ int MPIDI_XPMEM_comm_bootstrap(MPIR_Comm * comm)
         /* Init AVL tree based segment cache */
         MPIDI_XPMEMI_segtree_init(&MPIDI_XPMEMI_global.segmaps[i].segcache_ubuf);       /* Initialize user buffer tree */
     }
-    MPIDU_Init_shm_barrier();
-
-    xpmem_initialized = 1;
 
   fn_exit:
     MPIR_FUNC_EXIT;
-    return mpi_errno;
+    return MPI_SUCCESS;
   fn_fail:
     if (MPIDI_XPMEMI_global.segid != -1) {
         xpmem_remove(MPIDI_XPMEMI_global.segid);
@@ -93,7 +74,45 @@ int MPIDI_XPMEM_comm_bootstrap(MPIR_Comm * comm)
     XPMEM_TRACE("init: xpmem_make failed. Disabling XPMEM support");
     MPIR_CVAR_CH4_XPMEM_ENABLE = 0;
 
-    MPIR_CHKPMEM_REAP();
+    goto fn_exit;
+}
+
+int MPIDI_XPMEM_comm_bootstrap(MPIR_Comm * comm)
+{
+    int mpi_errno = MPI_SUCCESS;
+    MPIR_FUNC_ENTER;
+    MPIR_CHKLMEM_DECL();
+
+    int local_size = comm->num_local;
+    if (local_size > 1) {
+        /* NOTE: always run Allgather in case some process disable XPMEM inconsistently */
+        MPIR_Comm *node_comm = MPIR_Comm_get_node_comm(comm);
+
+        xpmem_segid_t *all_segids;
+        MPIR_CHKLMEM_MALLOC(all_segids, local_size * sizeof(xpmem_segid_t));
+
+        mpi_errno = MPIR_Allgather_impl(&MPIDI_XPMEMI_global.segid, sizeof(xpmem_segid_t),
+                                        MPIR_BYTE_INTERNAL, all_segids, sizeof(xpmem_segid_t),
+                                        MPIR_BYTE_INTERNAL, comm, MPIR_ERR_NONE);
+        MPIR_ERR_CHECK(mpi_errno);
+
+        int num_disabled = 0;
+        for (int i = 0; i < local_size; i++) {
+            int grank = MPIDIU_get_grank(rank, comm);
+            int local_id = MPIDI_POSIX_global.local_ranks[grank];;
+            MPIDI_XPMEMI_global.segmaps[local_id].remote_segid = all_segids[i];
+            if (all_segids[i] == -1) {
+                num_disabled++;
+            }
+        }
+        MPIR_Assertp(num_disabled == 0 || num_disabled == local_size);
+    }
+
+  fn_exit:
+    MPIR_CHKLMEM_FREEALL();
+    MPIR_FUNC_EXIT;
+    return mpi_errno;
+  fn_fail:
     goto fn_exit;
 }
 
@@ -103,7 +122,7 @@ int MPIDI_XPMEM_mpi_finalize_hook(void)
     int i, ret = 0;
     MPIR_FUNC_ENTER;
 
-    if (MPIDI_XPMEMI_global.segid == -1 || !xpmem_initialized) {
+    if (MPIDI_XPMEMI_global.segid == -1) {
         /* if XPMEM was disabled at runtime, return */
         goto fn_exit;
     }
@@ -127,8 +146,6 @@ int MPIDI_XPMEM_mpi_finalize_hook(void)
     ret = xpmem_remove(MPIDI_XPMEMI_global.segid);
     /* success(0) or failure(-1) */
     MPIR_ERR_CHKANDJUMP(ret == -1, mpi_errno, MPI_ERR_OTHER, "**xpmem_remove");
-
-    xpmem_initialized = 0;
 
   fn_exit:
     MPIR_FUNC_EXIT;
