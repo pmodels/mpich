198f2f291f2b608824e96bb4e703c00759681ca2
Hui Zhou <hzhou321@anl.gov>
2025-04-20 23:03:19 -0500

    ch4/posix: move allgather of topo to comm_commit_hook
    Move allgather of topo info to MPIDI_POSIX_mpi_comm_commit_post_hook
    since we won't have a world init stage. For now, we only do it if we are
    initializing a comm_world. It is possible to do this in comm_bootstrap,
    but for now this will do. It just means the topo aware optimization
    won't be available in a session model.

--- src/mpid/ch4/shm/posix/posix_comm.c
+++ src/mpid/ch4/shm/posix/posix_comm.c
@@ -33,8 +33,55 @@ int MPIDI_POSIX_mpi_comm_commit_pre_hook(MPIR_Comm * comm)
 int MPIDI_POSIX_mpi_comm_commit_post_hook(MPIR_Comm * comm)
 {
     int mpi_errno = MPI_SUCCESS;
-
     MPIR_FUNC_ENTER;
+    MPIR_CHKLMEM_DECL();
+
+    if (comm == MPIR_Process.comm_world) {
+        /* gather topo info from local procs and calculate distance */
+        if (MPIR_CVAR_CH4_SHM_POSIX_TOPO_ENABLE && MPIR_Process.local_size > 1) {
+            int topo_info_size = sizeof(MPIDI_POSIX_topo_info_t);
+            MPIDI_POSIX_topo_info_t *local_rank_topo;
+            MPIR_CHKLMEM_MALLOC(local_rank_topo, MPIR_Process.local_size * topo_info_size);
+
+            MPIR_Comm *node_comm = MPIR_Comm_get_node_comm(comm);
+            mpi_errno = MPIR_Allgather_fallback(&MPIDI_POSIX_global.topo, topo_info_size,
+                                                MPIR_BYTE_INTERNAL,
+                                                local_rank_topo, topo_info_size, MPIR_BYTE_INTERNAL,
+                                                node_comm, MPIR_ERR_NONE);
+            MPIR_ERR_CHECK(mpi_errno);
+
+            for (int i = 0; i < MPIR_Process.local_size; i++) {
+                if (local_rank_topo[i].l3_cache_id == -1 || local_rank_topo[i].numa_id == -1) {
+                    /* if topo info is incomplete, treat the node as local as fallback */
+                    MPIDI_POSIX_global.local_rank_dist[i] = MPIDI_POSIX_DIST__LOCAL;
+                    continue;
+                }
+                if (local_rank_topo[i].l3_cache_id != MPIDI_POSIX_global.topo.l3_cache_id) {
+                    MPIDI_POSIX_global.local_rank_dist[i] = MPIDI_POSIX_DIST__NO_SHARED_CACHE;
+                    continue;
+                }
+                if (local_rank_topo[i].numa_id != MPIDI_POSIX_global.topo.numa_id) {
+                    MPIDI_POSIX_global.local_rank_dist[i] = MPIDI_POSIX_DIST__INTER_NUMA;
+                    continue;
+                }
+            }
+
+            if (MPIR_CVAR_DEBUG_SUMMARY >= 2) {
+                if (MPIR_Process.rank == 0) {
+                    fprintf(stdout, "====== POSIX Topo Dist ======\n");
+                }
+                fprintf(stdout, "Rank: %d, Local_rank: %d [ %d", MPIR_Process.rank,
+                        MPIR_Process.local_rank, MPIDI_POSIX_global.local_rank_dist[0]);
+                for (int i = 1; i < MPIR_Process.local_size; i++) {
+                    fprintf(stdout, ", %d", MPIDI_POSIX_global.local_rank_dist[i]);
+                }
+                fprintf(stdout, " ]\n");
+                if (MPIR_Process.rank == 0) {
+                    fprintf(stdout, "=============================\n");
+                }
+            }
+        }
+    }
 
     /* prune selection tree */
     if (MPIDI_global.shm.posix.csel_root) {
@@ -55,6 +102,7 @@ int MPIDI_POSIX_mpi_comm_commit_post_hook(MPIR_Comm * comm)
     }
 
   fn_exit:
+    MPIR_CHKLMEM_FREEALL();
     MPIR_FUNC_EXIT;
     return mpi_errno;
   fn_fail:
