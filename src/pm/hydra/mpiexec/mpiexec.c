pmodels/mpich

commit 7e8756255546ca5af2e01220e08790e143fe6bf0

hydra/mpiexec: fix segfaults when no executable provided
We need more robust check when no executables are provided. This fixes
the segfault when user do things like `mpiexec -v` without executables.

src/pm/hydra/mpiexec/mpiexec.c
@@ -44,11 +44,6 @@ int main(int argc, char **argv)
 
     PMISERV_pg_init();
 
-    if (argc == 1) {
-        HYDU_ERR_SETANDJUMP(status, HYD_INVALID_PARAM,
-                            "No executable provided. Try -h for usages.\n");
-    }
-
     /* Get user preferences */
     status = HYD_uii_mpx_get_parameters(argv);
     HYDU_ERR_POP(status, "error parsing parameters\n");
@@ -59,6 +54,11 @@ int main(int argc, char **argv)
             HYDU_ERR_SETANDJUMP(status, HYD_INVALID_PARAM,
                                 "Missing executable. Try -h for usages.\n");
         }
+    }
+
+    if (!HYD_uii_mpx_exec_list) {
+        HYDU_ERR_SETANDJUMP(status, HYD_INVALID_PARAM,
+                            "No executable provided. Try -h for usages.\n");
     }
 
     /* The demux engine should be initialized before any sockets are

commit ef6de6ad85f24e112a6460cdefcfa0ea831c2b17

hydra: fix singleton init
The previous fix 7e87562555 broke the singleton init. Singleton init is
allowed to proceed without executables in the command line.

src/pm/hydra/mpiexec/mpiexec.c
@@ -56,7 +56,7 @@ int main(int argc, char **argv)
         }
     }
 
-    if (!HYD_uii_mpx_exec_list) {
+    if (!HYD_uii_mpx_exec_list && !HYD_server_info.is_singleton) {
         HYDU_ERR_SETANDJUMP(status, HYD_INVALID_PARAM,
                             "No executable provided. Try -h for usages.\n");
     }