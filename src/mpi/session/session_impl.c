61a1959d30ea69e695565b53b70712afee928d19
Hui Zhou <hzhou321@anl.gov>
2025-05-13 19:45:22 -0500

    session: whitespace fix

--- src/mpi/session/session_impl.c
+++ src/mpi/session/session_impl.c
@@ -30,8 +30,7 @@ int MPIR_Session_init_impl(MPIR_Info * info_ptr, MPIR_Errhandler * errhandler_pt
     MPIR_ERR_CHECK(mpi_errno);
 
     /* Get the strict finalize parameter via info object (if any) */
-    mpi_errno =
-        MPIR_Session_get_strict_finalize_from_info(info_ptr, &strict_finalize);
+    mpi_errno = MPIR_Session_get_strict_finalize_from_info(info_ptr, &strict_finalize);
     MPIR_ERR_CHECK(mpi_errno);
 
     /* Remark on MPI_THREAD_SINGLE: Multiple sessions may run in threads
@@ -52,7 +51,6 @@ int MPIR_Session_init_impl(MPIR_Info * info_ptr, MPIR_Errhandler * errhandler_pt
     MPIR_ERR_CHECK(mpi_errno);
 
     session_ptr->thread_level = provided;
-
     session_ptr->requested_thread_level = thread_level;
     session_ptr->strict_finalize = strict_finalize;
 
