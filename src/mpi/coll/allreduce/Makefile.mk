35c6b526f79e1f6af44a1cc2fedff275652b17c6
Sean Rhee <seanrhee2024@u.northwestern.edu>
2025-06-06 15:14:07 -0500

    draft

--- src/mpi/coll/allreduce/Makefile.mk
+++ src/mpi/coll/allreduce/Makefile.mk
@@ -11,6 +11,7 @@ mpi_core_sources +=											\
     src/mpi/coll/allreduce/allreduce_allcomm_nb.c	\
     src/mpi/coll/allreduce/allreduce_intra_recursive_doubling.c	\
     src/mpi/coll/allreduce/allreduce_intra_recursive_multiplying.c	\
+    src/mpi/coll/allreduce/allreduce_intra_circ_vring.c \
     src/mpi/coll/allreduce/allreduce_intra_reduce_scatter_allgather.c	\
     src/mpi/coll/allreduce/allreduce_intra_smp.c	\
     src/mpi/coll/allreduce/allreduce_intra_tree.c   \
