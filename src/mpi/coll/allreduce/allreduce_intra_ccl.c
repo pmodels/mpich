96760f4a20c3bb29615097acd7922b1f8842386f
Grace Li <grace.li@anl.gov>
2025-06-09 14:45:05 +0000

    Integrate an RCCL-backed Allreduce implementation into MPICH

--- src/mpi/coll/allreduce/allreduce_intra_ccl.c
+++ src/mpi/coll/allreduce/allreduce_intra_ccl.c
@@ -16,17 +16,28 @@ int MPIR_Allreduce_intra_ccl(const void *sendbuf, void *recvbuf, MPI_Aint count,
 {
     switch (ccl) {
 #ifdef ENABLE_NCCL
-        case MPIR_CVAR_ALLREDUCE_CCL_auto:     // Not sure yet how to handle "auto"
-        case MPIR_CVAR_ALLREDUCE_CCL_nccl:
-            if (MPIR_NCCL_check_requirements_red_op(sendbuf, recvbuf, datatype, op)) {
-                return MPIR_NCCL_Allreduce(sendbuf, recvbuf, count, datatype, op, comm_ptr,
-                                           errflag);
-            }
+    case MPIR_CVAR_ALLREDUCE_CCL_auto:
+    case MPIR_CVAR_ALLREDUCE_CCL_nccl:
+        if (MPIR_NCCL_check_requirements_red_op(sendbuf, recvbuf, datatype, op)) {
+            return MPIR_NCCL_Allreduce(sendbuf, recvbuf, count, datatype, op, comm_ptr,
+                                       errflag);
+        }
+        break;
 #endif
-        default:
-            goto fallback;
-    }
 
+#ifdef ENABLE_RCCL
+    case MPIR_CVAR_ALLREDUCE_CCL_auto:
+    case MPIR_CVAR_ALLREDUCE_CCL_rccl:
+        if (MPIR_RCCL_check_requirements_red_op(sendbuf, recvbuf, datatype, op)) {
+            return MPIR_RCCL_Allreduce(sendbuf, recvbuf, count, datatype, op, comm_ptr,
+                                       errflag);
+        }
+        break;
+#endif
+
+    default:
+        goto fallback;
+    }
   fallback:
     return MPIR_Allreduce_allcomm_auto(sendbuf, recvbuf, count, datatype, op, comm_ptr, errflag);
 }
