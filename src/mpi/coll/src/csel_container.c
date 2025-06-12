e41fa0d137eb77612d983d4d83a2685d0e3885fc
Sean Rhee <seanrhee2024@u.northwestern.edu>
2025-06-12 10:58:22 -0500

    bcast

--- src/mpi/coll/src/csel_container.c
+++ src/mpi/coll/src/csel_container.c
@@ -95,6 +95,16 @@ static void parse_container_params(struct json_object *obj, MPII_Csel_container_
             }
             break;
 
+        case MPII_CSEL_CONTAINER_TYPE__ALGORITHM__MPIR_Bcast_intra_circ_vring:
+            {
+                json_object_object_foreach(obj, key, val) {
+                    ckey = MPL_strdup_no_spaces(key);
+                    if (!strncmp(ckey, "n_chunk=", strlen("n_chunk=")))
+                        cnt->u.bcast.intra_circ_vring.n_chunk = atoi(ckey + strlen("n_chunk="));
+                    MPL_free(ckey);
+                }
+            }
+
         case MPII_CSEL_CONTAINER_TYPE__ALGORITHM__MPIR_Ireduce_intra_tsp_tree:
             {
                 json_object_object_foreach(obj, key, val) {
@@ -484,6 +494,8 @@ void *MPII_Create_container(struct json_object *obj)
                 MPII_CSEL_CONTAINER_TYPE__ALGORITHM__MPIR_Bcast_intra_scatter_recursive_doubling_allgather;
         else if (!strcmp(ckey, "algorithm=MPIR_Bcast_intra_scatter_ring_allgather"))
             cnt->id = MPII_CSEL_CONTAINER_TYPE__ALGORITHM__MPIR_Bcast_intra_scatter_ring_allgather;
+        else if (!strcmp(ckey, "algorithm=MPIR_Bcast_intra_circ_vring"))
+            cnt->id = MPII_CSEL_CONTAINER_TYPE__ALGORITHM__MPIR_Bcast_intra_circ_vring;
         else if (!strcmp(ckey, "algorithm=MPIR_Bcast_intra_smp"))
             cnt->id = MPII_CSEL_CONTAINER_TYPE__ALGORITHM__MPIR_Bcast_intra_smp;
         else if (!strcmp(ckey, "algorithm=MPIR_Bcast_intra_tree"))
