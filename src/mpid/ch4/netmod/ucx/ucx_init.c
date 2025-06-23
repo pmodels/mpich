4b8a027527b054736b79af266c06bbec1685a550
Ken Raffenetti <raffenet@mcs.anl.gov>
2025-06-23 14:00:42 -0500

    ch4/{ofi,ucx}: Skip collective finalize in sessions model
    If there is no MPI_COMM_WORLD, we should skip calling a global PMI
    barrier during finalization. It is not guaranteed that world processes
    have initialized MPI (thus PMI), so a barrier could hang.

--- src/mpid/ch4/netmod/ucx/ucx_init.c
+++ src/mpid/ch4/netmod/ucx/ucx_init.c
@@ -243,7 +243,7 @@ int MPIDI_UCX_mpi_finalize_hook(void)
     int n = MPIDI_UCX_global.num_vcis;
     pending = MPL_malloc(sizeof(ucs_status_ptr_t) * MPIR_Process.size * n * n, MPL_MEM_OTHER);
 
-    if (!MPIR_CVAR_NO_COLLECTIVE_FINALIZE) {
+    if (MPIR_Process.comm_world && !MPIR_CVAR_NO_COLLECTIVE_FINALIZE) {
         /* if some process are not present, the disconnect may timeout and give errors */
         mpi_errno = MPIR_pmi_barrier();
         MPIR_ERR_CHECK(mpi_errno);
