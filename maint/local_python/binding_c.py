ad8082ba34997be68b8172ac18d94cbad6b01b91
Hui Zhou <hzhou321@anl.gov>
2025-07-01 10:01:23 -0500

    fixup! binding/abi: fix declaration for HAVE_WEAK_ATTRIBUTE

--- maint/local_python/binding_c.py
+++ maint/local_python/binding_c.py
@@ -2997,7 +2997,7 @@ def get_C_return(func, mapping, is_abi=False):
         ret = mapping[func['return']]
         if func['return'] == 'EXTRA_STATE':
             ret = 'void *'
-        if is_abi:
+        if is_abi and func['dir'] != 'io':
             re_Handle = get_abi_re_Handle()
             ret = re.sub(re_Handle, r'ABI_\1', ret)
     return ret
@@ -3007,7 +3007,7 @@ def get_C_params(func, mapping, is_abi=False):
     re_Handle = get_abi_re_Handle()
     for p in func['c_parameters']:
         param = get_C_param(p, func, mapping)
-        if is_abi:
+        if is_abi and func['dir'] != 'io':
             param = re.sub(re_Handle, r'ABI_\1', param)
         param_list.append(param)
     if not len(param_list):
