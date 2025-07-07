87a8bf4e39ab2047de6aa54b737ac3aa7d5f4023
Hui Zhou <hzhou321@anl.gov>
2025-07-07 12:07:58 -0500

    ch4/ipc: add req to MPIDI_GPU_fill_ipc_handle
    Depend on options, GPU ipc may require storing the gpu attr in the
    request object so it can free the resource at completion time.
    Add the request parameter to MPIDI_GPU_fill_ipc_handle so its logic is
    contained in src/mpid/ch4/shm/ipc/gpu/gpu_post.c.

--- src/mpid/ch4/shm/posix/posix_coll_gpu_ipc.h
+++ src/mpid/ch4/shm/posix/posix_coll_gpu_ipc.h
@@ -92,7 +92,7 @@ static int allgather_ipc_handles(const void *buf, MPI_Aint count, MPI_Datatype d
     MPIDI_IPCI_ipc_handle_t my_ipc_handle;
     memset(&my_ipc_handle, 0, sizeof(my_ipc_handle));
     if (ipc_attr.ipc_type == MPIDI_IPCI_TYPE__GPU) {
-        mpi_errno = MPIDI_GPU_fill_ipc_handle(&ipc_attr, &my_ipc_handle);
+        mpi_errno = MPIDI_GPU_fill_ipc_handle(&ipc_attr, &my_ipc_handle, NULL);
         MPIR_ERR_CHECK(mpi_errno);
     } else {
         my_ipc_handle.gpu.global_dev_id = -1;
