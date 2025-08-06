c8b5a8a9a1baafa73749e3a27de4632db2bf5322
Hui Zhou <hzhou321@anl.gov>
2025-08-06 09:43:12 -0500

    ch4/ofi: add rndv read protocol
    A reimplementation of the huge protocol. It supports the multinic
    striping feature..
    
    To support rdma read into non-contig buffer or when directly read into
    gpu buffer is not supported, we should allocate pipeline chunk buffer
    and perform Ilocalcopy_gpu after the read.

--- src/mpid/ch4/netmod/ofi/ofi_events.h
+++ src/mpid/ch4/netmod/ofi/ofi_events.h
@@ -38,6 +38,9 @@ int MPIDI_OFI_peek_rndv_event(int vci, struct fi_cq_tagged_entry *wc, MPIR_Reque
 int MPIDI_OFI_rndv_cts_event(int vci, struct fi_cq_tagged_entry *wc, MPIR_Request * req);
 int MPIDI_OFI_pipeline_send_chunk_event(struct fi_cq_tagged_entry *wc, MPIR_Request * r);
 int MPIDI_OFI_pipeline_recv_chunk_event(struct fi_cq_tagged_entry *wc, MPIR_Request * r);
+int MPIDI_OFI_rndvread_recv_mrs_event(struct fi_cq_tagged_entry *wc, MPIR_Request * r);
+int MPIDI_OFI_rndvread_read_chunk_event(struct fi_cq_tagged_entry *wc, MPIR_Request * r);
+int MPIDI_OFI_rndvread_ack_event(struct fi_cq_tagged_entry *wc, MPIR_Request * r);
 
 MPL_STATIC_INLINE_PREFIX MPL_gpu_engine_type_t MPIDI_OFI_gpu_get_recv_engine_type(void)
 {
