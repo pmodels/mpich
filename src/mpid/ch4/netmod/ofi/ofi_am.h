5e8794dc10e36b2edd941377ac008b0bebb91a94
Hui Zhou <hzhou321@anl.gov>
2025-08-20 11:36:12 -0500

    ch4: pass rreq to MPIDI_NM_am_can_do_tag
    Pass the request object to MPIDI_NM_am_can_do_tag so the netmod can
    decide based on message size etc.
    
    Do the same for MPIDI_SHM_am_can_do_tag for consistency.

--- src/mpid/ch4/netmod/ofi/ofi_am.h
+++ src/mpid/ch4/netmod/ofi/ofi_am.h
@@ -215,9 +215,17 @@ MPL_STATIC_INLINE_PREFIX bool MPIDI_NM_am_check_eager(MPI_Aint am_hdr_sz, MPI_Ai
     }
 }
 
-MPL_STATIC_INLINE_PREFIX bool MPIDI_NM_am_can_do_tag(void)
+MPL_STATIC_INLINE_PREFIX bool MPIDI_NM_am_can_do_tag(MPIR_Request * rreq)
 {
-    return MPIDI_OFI_ENABLE_TAGGED;
+    if (MPIDI_OFI_ENABLE_TAGGED) {
+        MPI_Aint data_sz;
+        MPIR_Datatype_get_size_macro(MPIDIG_REQUEST(rreq, datatype), data_sz);
+        data_sz *= MPIDIG_REQUEST(rreq, count);
+        if (data_sz <= MPIDI_OFI_global.max_msg_size) {
+            return true;
+        }
+    }
+    return false;
 }
 
 MPL_STATIC_INLINE_PREFIX MPIDIG_recv_data_copy_cb MPIDI_NM_am_get_data_copy_cb(uint32_t attr)
