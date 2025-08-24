f4bf49dfeafe41e504ac95b2a9631e40d59dbba7
Hui Zhou <hzhou321@anl.gov>
2025-08-23 21:27:57 -0500

    coll: reorganize csel headers
    Move MPIR_Csel_coll_sig_s and MPII_Csel_container_s definitions to
    mpir_coll.h since they are now common interface to all collective
    algorithms.
    
    Move the rest of the csel header to coll_csel.h and only include it
    where needed.

---
+++ src/mpi/coll/include/coll_csel.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) by Argonne National Laboratory
+ *     See COPYRIGHT in top-level directory
+ */
+
+#ifndef COLL_CSEL_H_INCLUDED
+#define COLL_CSEL_H_INCLUDED
+
+#include "json.h"
+
+int MPIR_Csel_create_from_file(const char *json_file,
+                               void *(*create_container) (struct json_object *), void **csel);
+int MPIR_Csel_create_from_buf(const char *json,
+                              void *(*create_container) (struct json_object *), void **csel);
+int MPIR_Csel_free(void *csel);
+int MPIR_Csel_prune(void *root_csel, MPIR_Comm * comm_ptr, void **comm_csel);
+void *MPIR_Csel_search(void *csel, MPIR_Csel_coll_sig_s * coll_sig);
+
+void *MPII_Create_container(struct json_object *obj);
+
+#endif /* COLL_CSEL_H_INCLUDED */
