df62792b6d04898e940908faef1755f7c0dd5f4f
Hui Zhou <hzhou321@anl.gov>
2025-08-28 12:11:00 -0500

    ADI/csel: add hooks for initializing coll_sig
    Add MPIR_init_coll_sig and MPID_init_coll_sig so we can add arbitrary
    attr bits or additional fields without hacking maint/gen_coll.py.

--- maint/gen_coll.py
+++ maint/gen_coll.py
@@ -716,6 +716,9 @@ def dump_coll_impl(name, blocking_type):
             phash[p['name']] = 1
             G.out.append("coll_sig.u.%s.%s = %s;" % (name, p['name'], p['name']))
 
+    G.out.append("MPIR_Init_coll_sig(&coll_sig);")
+    G.out.append("MPID_Init_coll_sig(&coll_sig);")
+
     # Call csel
     G.out.append("")
     G.out.append("mpi_errno = MPIR_Coll_composition_auto(&coll_sig);")
