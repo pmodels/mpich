ffe4cbc0fd25b155a0e684ff000cf0e8b5b1093d
Ken Raffenetti <raffenet@mcs.anl.gov>
2025-09-19 14:40:11 -0500

    ch4/ucx: Move hcoll comm creation to post commit hook
    For builtin comms, the necessary infrastructure for creating an hcoll
    comm may not yet be ready. Move to post commit hook to guarantee
    communication is ready.

--- src/mpid/ch4/netmod/ucx/ucx_comm.c
+++ src/mpid/ch4/netmod/ucx/ucx_comm.c
@@ -15,10 +15,6 @@ int MPIDI_UCX_mpi_comm_commit_pre_hook(MPIR_Comm * comm)
     int mpi_errno = MPI_SUCCESS;
     MPIR_FUNC_ENTER;
 
-#if defined HAVE_HCOLL
-    hcoll_comm_create(comm, NULL);
-#endif
-
     MPIR_FUNC_EXIT;
     return mpi_errno;
 }
@@ -28,6 +24,10 @@ int MPIDI_UCX_mpi_comm_commit_post_hook(MPIR_Comm * comm)
     int mpi_errno = MPI_SUCCESS;
 
     MPIR_FUNC_ENTER;
+
+#if defined HAVE_HCOLL
+    hcoll_comm_create(comm, NULL);
+#endif
 
     MPIR_FUNC_EXIT;
     return mpi_errno;
