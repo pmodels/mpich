353239dcc175d0537d9683e329c0a5bef97216f6
Ethan Wong <ewong@anl.gov>
2025-09-30 09:58:10 -0500

    datatype: re-add C types to `MPIR_Type_match_size_impl`
    The C datatypes were removed in c7fc51b as only the hardcoded constants
    in `mpi.h` were necessary to check for size matches. However, with
    internal datatype checking, the C types must be re-appended so they can
    be matched even if Fortran types are not present (i.e. with
    `--disable-fortran`).

--- src/mpi/datatype/datatype_impl.c
+++ src/mpi/datatype/datatype_impl.c
@@ -311,13 +311,16 @@ int MPIR_Type_match_size_impl(int typeclass, int size, MPI_Datatype * datatype)
      * not available. We test for that case separately.  We also
      * prefer the Fortran types to the C type, if they are available */
     static MPI_Datatype real_types[] = {
-        MPI_REAL2, MPI_REAL4, MPI_REAL8, MPI_REAL16, MPI_LONG_DOUBLE,
+        MPI_REAL2, MPI_REAL4, MPI_REAL8, MPI_REAL16,
+        MPI_FLOAT, MPI_DOUBLE, MPI_LONG_DOUBLE,
     };
     static MPI_Datatype int_types[] = {
         MPI_INTEGER1, MPI_INTEGER2, MPI_INTEGER4, MPI_INTEGER8, MPI_INTEGER16,
+        MPI_CHAR, MPI_SHORT, MPI_INT, MPI_LONG, MPI_LONG_LONG,
     };
     static MPI_Datatype complex_types[] = {
         MPI_COMPLEX4, MPI_COMPLEX8, MPI_COMPLEX16, MPI_COMPLEX32,
+        MPI_C_FLOAT_COMPLEX, MPI_C_DOUBLE_COMPLEX, MPI_C_LONG_DOUBLE_COMPLEX,
     };
 
     /* The following implementation follows the suggestion in the
