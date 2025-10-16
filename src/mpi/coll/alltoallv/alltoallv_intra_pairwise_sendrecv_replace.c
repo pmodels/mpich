1ed9640c9ad0281d293177e300274bc75375c68b
Hui Zhou <hzhou321@anl.gov>
2025-10-16 11:53:18 -0500

    Revert "temp: update alltoallv_intra_pairwise_sendrecv_replace"
    This reverts commit e627a5687000f29c9f2a0f695b9f8ee07af4feef.

--- src/mpi/coll/alltoallv/alltoallv_intra_pairwise_sendrecv_replace.c
+++ src/mpi/coll/alltoallv/alltoallv_intra_pairwise_sendrecv_replace.c
@@ -4,22 +4,6 @@
  */
 
 #include "mpiimpl.h"
-/*
-=== BEGIN_MPI_T_CVAR_INFO_BLOCK ===
-
-cvars:
-    - name        : MPIR_CVAR_ALLTOALLV_PAIRWISE_NEW
-      category    : COLLECTIVE
-      type        : boolean
-      default     : 0
-      class       : none
-      verbosity   : MPI_T_VERBOSITY_USER_BASIC
-      scope       : MPI_T_SCOPE_LOCAL
-      description : >-
-        If on, use a new algorithm that orders bit-wise pairs on all processes.
-
-=== END_MPI_T_CVAR_INFO_BLOCK ===
-*/
 
 /* Algorithm: Pairwise sendrecv
  *
@@ -40,13 +24,15 @@ int MPIR_Alltoallv_intra_pairwise_sendrecv_replace(const void *sendbuf, const MP
                                                    const MPI_Aint * rdispls, MPI_Datatype recvtype,
                                                    MPIR_Comm * comm_ptr, int coll_attr)
 {
+    int comm_size, i, j;
+    MPI_Aint recv_extent;
     int mpi_errno = MPI_SUCCESS;
+    MPI_Status status;
+    int rank;
 
-    int rank, comm_size;
     MPIR_COMM_RANK_SIZE(comm_ptr, rank, comm_size);
 
     /* Get extent of recv type, but send type is only valid if (sendbuf!=MPI_IN_PLACE) */
-    MPI_Aint recv_extent;
     MPIR_Datatype_get_extent_macro(recvtype, recv_extent);
 
 #ifdef HAVE_ERROR_CHECKING
@@ -62,45 +48,27 @@ int MPIR_Alltoallv_intra_pairwise_sendrecv_replace(const void *sendbuf, const MP
      * time and there will be multiple repeated malloc/free's rather than
      * maintaining a single buffer across the whole loop.  Something like
      * MADRE is probably the best solution for the MPI_IN_PLACE scenario. */
+    for (i = 0; i < comm_size; ++i) {
+        /* start inner loop at i to avoid re-exchanging data */
+        for (j = i; j < comm_size; ++j) {
+            if (rank == i) {
+                /* also covers the (rank == i && rank == j) case */
+                mpi_errno = MPIC_Sendrecv_replace(((char *) recvbuf + rdispls[j] * recv_extent),
+                                                  recvcounts[j], recvtype,
+                                                  j, MPIR_ALLTOALLV_TAG,
+                                                  j, MPIR_ALLTOALLV_TAG,
+                                                  comm_ptr, &status, coll_attr);
+                MPIR_ERR_CHECK(mpi_errno);
 
-    if (!MPIR_CVAR_ALLTOALLV_PAIRWISE_NEW) {
-        MPIR_Assert(comm_ptr->intranode_table);
-        /* -- 1st exchange within intranode -- */
-        for (int i = 0; i < comm_size; ++i) {
-            if (comm_ptr->intranode_table[i] == -1)
-                continue;
-            mpi_errno = MPIC_Sendrecv_replace(((char *) recvbuf + rdispls[i] * recv_extent),
-                                              recvcounts[i], recvtype, i, MPIR_ALLTOALLV_TAG,
-                                              i, MPIR_ALLTOALLV_TAG,
-                                              comm_ptr, MPI_STATUS_IGNORE, coll_attr);
-            MPIR_ERR_CHECK(mpi_errno);
-        }
-        /* -- 2nd exchange over internode -- */
-        for (int i = 0; i < comm_size; ++i) {
-            if (comm_ptr->intranode_table[i] > -1)
-                continue;
-            mpi_errno = MPIC_Sendrecv_replace(((char *) recvbuf + rdispls[i] * recv_extent),
-                                              recvcounts[i], recvtype, i, MPIR_ALLTOALLV_TAG,
-                                              i, MPIR_ALLTOALLV_TAG,
-                                              comm_ptr, MPI_STATUS_IGNORE, coll_attr);
-            MPIR_ERR_CHECK(mpi_errno);
-        }
-    } else {
-        /* Ordering the pairs by flipping bits */
-        int max_mask = 1;
-        while (max_mask < comm_size) {
-            max_mask *= 2;
-        }
-        for (int mask = 0; mask < max_mask; mask++) {
-            int r = comm_ptr->rank ^ mask;
-            if (r >= comm_size) {
-                continue;
+            } else if (rank == j) {
+                /* same as above with i/j args reversed */
+                mpi_errno = MPIC_Sendrecv_replace(((char *) recvbuf + rdispls[i] * recv_extent),
+                                                  recvcounts[i], recvtype,
+                                                  i, MPIR_ALLTOALLV_TAG,
+                                                  i, MPIR_ALLTOALLV_TAG,
+                                                  comm_ptr, &status, coll_attr);
+                MPIR_ERR_CHECK(mpi_errno);
             }
-            mpi_errno = MPIC_Sendrecv_replace(((char *) recvbuf + rdispls[r] * recv_extent),
-                                              recvcounts[r], recvtype, r, MPIR_ALLTOALLV_TAG,
-                                              r, MPIR_ALLTOALLV_TAG,
-                                              comm_ptr, MPI_STATUS_IGNORE, coll_attr);
-            MPIR_ERR_CHECK(mpi_errno);
         }
     }
 
