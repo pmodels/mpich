b9a7f22f2f47487153a91f0b914a69ced9988da5
Hui Zhou <hzhou321@anl.gov>
2025-10-28 09:22:55 -0500

    ch4/ofi: use the vcis stored in rreq for imrecv
    We store both vci_local and vci_remote in rreq from improbe. Imrecv need
    use the same vcis.

--- src/mpid/ch4/netmod/ofi/ofi_recv.h
+++ src/mpid/ch4/netmod/ofi/ofi_recv.h
@@ -316,10 +316,14 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_NM_mpi_imrecv(void *buf,
         mpi_errno = MPIDIG_mpi_imrecv(buf, count, datatype, message);
     } else {
         rreq = message;
+        int vci_remote = MPIDI_OFI_REQUEST(rreq, vci_remote);
+        int vci_local = MPIDI_OFI_REQUEST(rreq, vci_local);
+        MPIR_Assert(vci_local == vci);
         av = MPIDIU_comm_rank_to_av(rreq->comm, message->status.MPI_SOURCE);
         /* FIXME: need get vci_src in the request */
         mpi_errno = MPIDI_OFI_do_irecv(buf, count, datatype, message->status.MPI_SOURCE,
-                                       message->status.MPI_TAG, rreq->comm, 0, av, 0, vci,
+                                       message->status.MPI_TAG, rreq->comm,
+                                       0, av, vci_remote, vci_local,
                                        &rreq, MPIDI_OFI_USE_EXISTING, FI_CLAIM | FI_COMPLETION);
         MPIDI_OFI_REQUEST(rreq, am_req) = NULL;
     }
