dea0c58aa3261a4fb4937c646630a3570b06c71d
Hui Zhou <hzhou321@anl.gov>
2025-10-31 14:42:40 -0500

    ch4/cma: check ptrace_scope at init
    Users may not have ptrace_scope permission set. Check it at init and
    turn MPIR_CVAR_CH4_CMA_ENABLE off if we don't have the permission.

--- src/mpid/ch4/shm/ipc/cma/cma_init.c
+++ src/mpid/ch4/shm/ipc/cma/cma_init.c
@@ -12,6 +12,28 @@ int MPIDI_CMA_init_local(void)
     int mpi_errno = MPI_SUCCESS;
     MPIR_FUNC_ENTER;
 
+    bool cma_happy = false;
+
+    /* check ptrace permission */
+    int fd = open("/proc/sys/kernel/yama/ptrace_scope", O_RDONLY);
+    char buffer = '0';
+    if (fd >= 0) {
+        int ret = read(fd, &buffer, 1);
+        if (ret >= 0) {
+            if (buffer != '0') {
+                cma_happy = true;
+            }
+            close(fd);
+        }
+    }
+
+    if (!cma_happy) {
+        /* strictly we need a collective, which is troublesome during init.
+         * For now, let's assume users will have ptrace_scope setting consistently.
+         */
+        MPIR_CVAR_CH4_CMA_ENABLE = 0;
+    }
+
     MPIR_FUNC_EXIT;
     return mpi_errno;
 }
