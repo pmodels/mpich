f7e90c364716643956e6d5bf7ae8269c717f3a15
Hui Zhou <hzhou321@anl.gov>
2025-12-10 13:20:09 -0600

    autogen: avoid duplicating confdb and submodules
    Define and propagate $MPICH_CONFDB to avoid duplicating confdb into
    subdirs.
    
    Avoid duplicating submodules such as mpl, pmi, hwloc.
    
    Add funcations such as fn_hydra, fn_pmi, fn_test for preparing release
    tarballs.

--- src/pm/hydra/autogen.sh
+++ src/pm/hydra/autogen.sh
@@ -8,6 +8,28 @@ if test -z "$AUTORECONF" ; then
     AUTORECONF="autoreconf"
 fi
 
+if test -n "$MPICH_CONFDB" ; then
+    CONFARGS="-I $MPICH_CONFDB -vif"
+    mkdir -p confdb
+else
+    if ! test -d confdb ; then
+        echo "Missing confdb!"
+        exit 1
+    fi
+
+    CONFARGS="-vif"
+    # set it for mpl
+    export MPICH_CONFDB=`realpath confdb`
+fi
+
+echo "=== running autoreconf in '.' ==="
+$AUTORECONF $CONFARGS || exit 1
+
+if test -d "modules/hwloc" ; then
+    echo "=== running autogen.sh in 'modules/hwloc' ==="
+    (cd modules/hwloc && ./autogen.sh) || exit 1
+fi
+
 if test -d "modules/mpl" ; then
     echo "=== running autogen.sh in 'modules/mpl' ==="
     (cd modules/mpl && ./autogen.sh) || exit 1
@@ -17,14 +39,6 @@ if test -d "modules/pmi" ; then
     echo "=== running autogen.sh in 'modules/pmi' ==="
     (cd modules/pmi && ./autogen.sh) || exit 1
 fi
-
-if test -d "modules/hwloc" ; then
-    echo "=== running autogen.sh in 'modules/hwloc' ==="
-    (cd modules/hwloc && ./autogen.sh) || exit 1
-fi
-
-echo "=== running autoreconf in '.' ==="
-$AUTORECONF -vif || exit 1
 
 # Remove the autom4te.cache folders for a release-like structure.
 find . -name autom4te.cache | xargs rm -rf
--- src/pmi/autogen.sh
+++ src/pmi/autogen.sh
@@ -8,7 +8,21 @@ if test -z "$AUTORECONF" ; then
     AUTORECONF="autoreconf"
 fi
 
-$AUTORECONF -vif || exit 1
+if test -n "$MPICH_CONFDB" ; then
+    CONFARGS="-I $MPICH_CONFDB -vif"
+    mkdir -p confdb
+else
+    if ! test -d confdb ; then
+        echo "Missing confdb!"
+        exit 1
+    fi
+
+    CONFARGS="-vif"
+    # set it for mpl
+    export MPICH_CONFDB=`realpath confdb`
+fi
+
+$AUTORECONF $CONFARGS || exit 1
 
 if test -d mpl ; then
     echo "=== running autogen.sh in 'mpl' ==="
