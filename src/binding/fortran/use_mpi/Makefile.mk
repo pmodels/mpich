5afe57bcaeecd40211f1d37ae74de976df1ad025
Hui Zhou <hzhou321@anl.gov>
2025-12-21 15:26:33 -0600

    fortran: fortran binding as a separate package
    Configure and build the fortran binding separately from MPICH.

--- src/binding/fortran/use_mpi/Makefile.mk
+++ src/binding/fortran/use_mpi/Makefile.mk
@@ -21,25 +21,25 @@ if BUILD_FC_BINDING
 # We need to tell some compilers (e.g., Solaris f90) to look for modules in the
 # current directory when the source file is not in the working directory (i.e.,
 # in a VPATH build)
-AM_FCFLAGS += @FCINCFLAG@src/binding/fortran/use_mpi
+AM_FCFLAGS += @FCINCFLAG@use_mpi
 
-AM_CPPFLAGS += -Isrc/binding/fortran/use_mpi
+AM_CPPFLAGS += -Iuse_mpi
 noinst_HEADERS +=                     \
-    src/binding/fortran/use_mpi/mpifnoext.h
+    use_mpi/mpifnoext.h
 
 # cause any .$(MOD) files to be output in the f90 bindings directory instead of
 # the current directory
-FC_COMPILE_MODS += $(FCMODOUTFLAG)src/binding/fortran/use_mpi
+FC_COMPILE_MODS += $(FCMODOUTFLAG)use_mpi
 
 mpifort_convenience_libs += lib/libf90_mpi.la
 noinst_LTLIBRARIES += lib/libf90_mpi.la
 
 lib_libf90_mpi_la_SOURCES = \
-    src/binding/fortran/use_mpi/mpi.f90 \
-    src/binding/fortran/use_mpi/mpi_constants.f90 \
-    src/binding/fortran/use_mpi/mpi_sizeofs.f90 \
-    src/binding/fortran/use_mpi/mpi_base.f90 \
-    src/binding/fortran/use_mpi/pmpi_base.f90
+    use_mpi/mpi.f90 \
+    use_mpi/mpi_constants.f90 \
+    use_mpi/mpi_sizeofs.f90 \
+    use_mpi/mpi_base.f90 \
+    use_mpi/pmpi_base.f90
 
 # FIXME: We may want to edit the mpif.h to convert Fortran77-specific
 # items (such as an integer*8 used for file offsets) into the
@@ -49,202 +49,202 @@ lib_libf90_mpi_la_SOURCES = \
 # We need the MPI constants in a separate module for some of the
 # interface definitions (the ones that need MPI_ADDRESS_KIND or
 # MPI_OFFSET_KIND)
-src/binding/fortran/use_mpi/mpi.$(MOD)-stamp: src/binding/fortran/use_mpi/$(MPICONSTMOD).$(MOD) src/binding/fortran/use_mpi/$(MPISIZEOFMOD).$(MOD) src/binding/fortran/use_mpi/$(MPIBASEMOD).$(MOD) src/binding/fortran/use_mpi/$(PMPIBASEMOD).$(MOD) $(srcdir)/src/binding/fortran/use_mpi/mpi.f90 src/binding/fortran/use_mpi/mpifnoext.h
-	@rm -f src/binding/fortran/use_mpi/mpi-tmp
-	@touch src/binding/fortran/use_mpi/mpi-tmp
-	@( cd src/binding/fortran/use_mpi && \
+use_mpi/mpi.$(MOD)-stamp: use_mpi/$(MPICONSTMOD).$(MOD) use_mpi/$(MPISIZEOFMOD).$(MOD) use_mpi/$(MPIBASEMOD).$(MOD) use_mpi/$(PMPIBASEMOD).$(MOD) $(srcdir)/use_mpi/mpi.f90 use_mpi/mpifnoext.h
+	@rm -f use_mpi/mpi-tmp
+	@touch use_mpi/mpi-tmp
+	@( cd use_mpi && \
 	   if [ "$(FCEXT)" != "f90" ] || [ ! -f mpi.$(FCEXT) ] ; then \
 	       rm -f mpi.$(FCEXT) ; \
-	       $(LN_S) $(abs_top_srcdir)/src/binding/fortran/use_mpi/mpi.f90 mpi.$(FCEXT) ; \
+	       $(LN_S) $(abs_srcdir)/use_mpi/mpi.f90 mpi.$(FCEXT) ; \
 	   fi )
-	$(mod_verbose)$(FC_COMPILE_MODS) -c src/binding/fortran/use_mpi/mpi.$(FCEXT) -o src/binding/fortran/use_mpi/mpi.lo
-	@( cd src/binding/fortran/use_mpi && \
+	$(mod_verbose)$(FC_COMPILE_MODS) -c use_mpi/mpi.$(FCEXT) -o use_mpi/mpi.lo
+	@( cd use_mpi && \
 	   if [ "$(FCEXT)" != "f90" ] || [ ! -f mpi.$(FCEXT) ] ; then \
 	       rm -f mpi.$(FCEXT) ; \
 	   fi )
-	@mv src/binding/fortran/use_mpi/mpi-tmp src/binding/fortran/use_mpi/mpi.$(MOD)-stamp
+	@mv use_mpi/mpi-tmp use_mpi/mpi.$(MOD)-stamp
 
-src/binding/fortran/use_mpi/mpi.lo src/binding/fortran/use_mpi/$(MPIMOD).$(MOD): src/binding/fortran/use_mpi/mpi.$(MOD)-stamp
+use_mpi/mpi.lo use_mpi/$(MPIMOD).$(MOD): use_mpi/mpi.$(MOD)-stamp
 ## Recover from the removal of $@
 	@if test -f $@; then :; else \
-	  trap 'rm -rf src/binding/fortran/use_mpi/mpi-lock src/binding/fortran/use_mpi/mpi.$(MOD)-stamp' 1 2 13 15; \
-	  if mkdir src/binding/fortran/use_mpi/mpi-lock 2>/dev/null; then \
+	  trap 'rm -rf use_mpi/mpi-lock use_mpi/mpi.$(MOD)-stamp' 1 2 13 15; \
+	  if mkdir use_mpi/mpi-lock 2>/dev/null; then \
 ## This code is being executed by the first process.
-	    rm -f src/binding/fortran/use_mpi/mpi.$(MOD)-stamp; \
-	    $(MAKE) $(AM_MAKEFLAGS) src/binding/fortran/use_mpi/mpi.$(MOD)-stamp; \
-	    rmdir src/binding/fortran/use_mpi/mpi-lock; \
+	    rm -f use_mpi/mpi.$(MOD)-stamp; \
+	    $(MAKE) $(AM_MAKEFLAGS) use_mpi/mpi.$(MOD)-stamp; \
+	    rmdir use_mpi/mpi-lock; \
 	  else \
 ## This code is being executed by the follower processes.
 ## Wait until the first process is done.
-	    while test -d src/binding/fortran/use_mpi/mpi-lock; do sleep 1; done; \
+	    while test -d use_mpi/mpi-lock; do sleep 1; done; \
 ## Succeed if and only if the first process succeeded.
-	    test -f src/binding/fortran/use_mpi/mpi.$(MOD)-stamp; exit $$?; \
+	    test -f use_mpi/mpi.$(MOD)-stamp; exit $$?; \
 	  fi; \
 	fi
 
-CLEANFILES += src/binding/fortran/use_mpi/mpi.$(MOD)-stamp src/binding/fortran/use_mpi/$(MPIMOD).$(MOD) src/binding/fortran/use_mpi/mpi.lo src/binding/fortran/use_mpi/mpi-tmp
+CLEANFILES += use_mpi/mpi.$(MOD)-stamp use_mpi/$(MPIMOD).$(MOD) use_mpi/mpi.lo use_mpi/mpi-tmp
 
 
-src/binding/fortran/use_mpi/mpi_constants.$(MOD)-stamp: src/binding/fortran/use_mpi/mpi_constants.f90 src/binding/fortran/use_mpi/mpifnoext.h
-	@rm -f src/binding/fortran/use_mpi/mpi_constants-tmp
-	@touch src/binding/fortran/use_mpi/mpi_constants-tmp
-	@( cd src/binding/fortran/use_mpi && \
+use_mpi/mpi_constants.$(MOD)-stamp: use_mpi/mpi_constants.f90 use_mpi/mpifnoext.h
+	@rm -f use_mpi/mpi_constants-tmp
+	@touch use_mpi/mpi_constants-tmp
+	@( cd use_mpi && \
 	   if [ "$(FCEXT)" != "f90" ] || [ ! -f mpi_constants.$(FCEXT) ] ; then \
 	       rm -f mpi_constants.$(FCEXT) ; \
-	       $(LN_S) $(abs_top_srcdir)/src/binding/fortran/use_mpi/mpi_constants.f90 mpi_constants.$(FCEXT) ; \
+	       $(LN_S) $(abs_srcdir)/use_mpi/mpi_constants.f90 mpi_constants.$(FCEXT) ; \
 	   fi )
-	$(mod_verbose)$(FC_COMPILE_MODS) -c src/binding/fortran/use_mpi/mpi_constants.$(FCEXT) -o src/binding/fortran/use_mpi/mpi_constants.lo
-	@( cd src/binding/fortran/use_mpi && \
+	$(mod_verbose)$(FC_COMPILE_MODS) -c use_mpi/mpi_constants.$(FCEXT) -o use_mpi/mpi_constants.lo
+	@( cd use_mpi && \
 	   if [ "$(FCEXT)" != "f90" ] || [ ! -f mpi_constants.$(FCEXT) ] ; then \
 	       rm -f mpi_constants.$(FCEXT) ; \
 	   fi )
-	@mv src/binding/fortran/use_mpi/mpi_constants-tmp src/binding/fortran/use_mpi/mpi_constants.$(MOD)-stamp
+	@mv use_mpi/mpi_constants-tmp use_mpi/mpi_constants.$(MOD)-stamp
 
-src/binding/fortran/use_mpi/mpi_constants.lo src/binding/fortran/use_mpi/$(MPICONSTMOD).$(MOD): src/binding/fortran/use_mpi/mpi_constants.$(MOD)-stamp
+use_mpi/mpi_constants.lo use_mpi/$(MPICONSTMOD).$(MOD): use_mpi/mpi_constants.$(MOD)-stamp
 ## Recover from the removal of $@
 	@if test -f $@; then :; else \
-	  trap 'rm -rf src/binding/fortran/use_mpi/mpi_constants-lock src/binding/fortran/use_mpi/mpi_constants.$(MOD)-stamp' 1 2 13 15; \
-	  if mkdir src/binding/fortran/use_mpi/mpi_constants-lock 2>/dev/null; then \
+	  trap 'rm -rf use_mpi/mpi_constants-lock use_mpi/mpi_constants.$(MOD)-stamp' 1 2 13 15; \
+	  if mkdir use_mpi/mpi_constants-lock 2>/dev/null; then \
 ## This code is being executed by the first process.
-	    rm -f src/binding/fortran/use_mpi/mpi_constants.$(MOD)-stamp; \
-	    $(MAKE) $(AM_MAKEFLAGS) src/binding/fortran/use_mpi/mpi_constants.$(MOD)-stamp; \
-	    rmdir src/binding/fortran/use_mpi/mpi_constants-lock; \
+	    rm -f use_mpi/mpi_constants.$(MOD)-stamp; \
+	    $(MAKE) $(AM_MAKEFLAGS) use_mpi/mpi_constants.$(MOD)-stamp; \
+	    rmdir use_mpi/mpi_constants-lock; \
 	  else \
 ## This code is being executed by the follower processes.
 ## Wait until the first process is done.
-	    while test -d src/binding/fortran/use_mpi/mpi_constants-lock; do sleep 1; done; \
+	    while test -d use_mpi/mpi_constants-lock; do sleep 1; done; \
 ## Succeed if and only if the first process succeeded.
-	    test -f src/binding/fortran/use_mpi/mpi_constants.$(MOD)-stamp; exit $$?; \
+	    test -f use_mpi/mpi_constants.$(MOD)-stamp; exit $$?; \
 	  fi; \
 	fi
 
-CLEANFILES += src/binding/fortran/use_mpi/mpi_constants.$(MOD)-stamp src/binding/fortran/use_mpi/$(MPICONSTMOD).$(MOD) src/binding/fortran/use_mpi/mpi_constants.lo src/binding/fortran/use_mpi/mpi_constants-tmp
+CLEANFILES += use_mpi/mpi_constants.$(MOD)-stamp use_mpi/$(MPICONSTMOD).$(MOD) use_mpi/mpi_constants.lo use_mpi/mpi_constants-tmp
 
 
-src/binding/fortran/use_mpi/mpi_sizeofs.$(MOD)-stamp: src/binding/fortran/use_mpi/mpi_sizeofs.f90 src/binding/fortran/use_mpi/mpifnoext.h
-	@rm -f src/binding/fortran/use_mpi/mpi_sizeofs-tmp
-	@touch src/binding/fortran/use_mpi/mpi_sizeofs-tmp
-	@( cd src/binding/fortran/use_mpi && \
+use_mpi/mpi_sizeofs.$(MOD)-stamp: use_mpi/mpi_sizeofs.f90 use_mpi/mpifnoext.h
+	@rm -f use_mpi/mpi_sizeofs-tmp
+	@touch use_mpi/mpi_sizeofs-tmp
+	@( cd use_mpi && \
 	   if [ "$(FCEXT)" != "f90" ] ; then \
 	       rm -f mpi_sizeofs.$(FCEXT) ; \
 	       $(LN_S) mpi_sizeofs.f90 mpi_sizeofs.$(FCEXT) ; \
 	   fi )
-	$(mod_verbose)$(FC_COMPILE_MODS) -c src/binding/fortran/use_mpi/mpi_sizeofs.$(FCEXT) -o src/binding/fortran/use_mpi/mpi_sizeofs.lo
-	@( cd src/binding/fortran/use_mpi && \
+	$(mod_verbose)$(FC_COMPILE_MODS) -c use_mpi/mpi_sizeofs.$(FCEXT) -o use_mpi/mpi_sizeofs.lo
+	@( cd use_mpi && \
 	   if [ "$(FCEXT)" != "f90" ] ; then \
 	       rm -f mpi_sizeofs.$(FCEXT) ; \
 	   fi )
-	@mv src/binding/fortran/use_mpi/mpi_sizeofs-tmp src/binding/fortran/use_mpi/mpi_sizeofs.$(MOD)-stamp
+	@mv use_mpi/mpi_sizeofs-tmp use_mpi/mpi_sizeofs.$(MOD)-stamp
 
-src/binding/fortran/use_mpi/mpi_sizeofs.lo src/binding/fortran/use_mpi/$(MPISIZEOFMOD).$(MOD): src/binding/fortran/use_mpi/mpi_sizeofs.$(MOD)-stamp
+use_mpi/mpi_sizeofs.lo use_mpi/$(MPISIZEOFMOD).$(MOD): use_mpi/mpi_sizeofs.$(MOD)-stamp
 ## Recover from the removal of $@
 	@if test -f $@; then :; else \
-	  trap 'rm -rf src/binding/fortran/use_mpi/mpi_sizeofs-lock src/binding/fortran/use_mpi/mpi_sizeofs.$(MOD)-stamp' 1 2 13 15; \
-	  if mkdir src/binding/fortran/use_mpi/mpi_sizeofs-lock 2>/dev/null; then \
+	  trap 'rm -rf use_mpi/mpi_sizeofs-lock use_mpi/mpi_sizeofs.$(MOD)-stamp' 1 2 13 15; \
+	  if mkdir use_mpi/mpi_sizeofs-lock 2>/dev/null; then \
 ## This code is being executed by the first process.
-	    rm -f src/binding/fortran/use_mpi/mpi_sizeofs.$(MOD)-stamp; \
-	    $(MAKE) $(AM_MAKEFLAGS) src/binding/fortran/use_mpi/mpi_sizeofs.$(MOD)-stamp; \
-	    rmdir src/binding/fortran/use_mpi/mpi_sizeofs-lock; \
+	    rm -f use_mpi/mpi_sizeofs.$(MOD)-stamp; \
+	    $(MAKE) $(AM_MAKEFLAGS) use_mpi/mpi_sizeofs.$(MOD)-stamp; \
+	    rmdir use_mpi/mpi_sizeofs-lock; \
 	  else \
 ## This code is being executed by the follower processes.
 ## Wait until the first process is done.
-	    while test -d src/binding/fortran/use_mpi/mpi_sizeofs-lock; do sleep 1; done; \
+	    while test -d use_mpi/mpi_sizeofs-lock; do sleep 1; done; \
 ## Succeed if and only if the first process succeeded.
-	    test -f src/binding/fortran/use_mpi/mpi_sizeofs.$(MOD)-stamp; exit $$?; \
+	    test -f use_mpi/mpi_sizeofs.$(MOD)-stamp; exit $$?; \
 	  fi; \
 	fi
 
-CLEANFILES += src/binding/fortran/use_mpi/mpi_sizeofs.$(MOD)-stamp src/binding/fortran/use_mpi/$(MPISIZEOFMOD).$(MOD) src/binding/fortran/use_mpi/mpi_sizeofs.lo src/binding/fortran/use_mpi/mpi_sizeofs-tmp
+CLEANFILES += use_mpi/mpi_sizeofs.$(MOD)-stamp use_mpi/$(MPISIZEOFMOD).$(MOD) use_mpi/mpi_sizeofs.lo use_mpi/mpi_sizeofs-tmp
 
 
-src/binding/fortran/use_mpi/mpi_base.$(MOD)-stamp: src/binding/fortran/use_mpi/mpi_base.f90 src/binding/fortran/use_mpi/$(MPICONSTMOD).$(MOD)
-	@rm -f src/binding/fortran/use_mpi/mpi_base-tmp
-	@touch src/binding/fortran/use_mpi/mpi_base-tmp
-	@( cd src/binding/fortran/use_mpi && \
+use_mpi/mpi_base.$(MOD)-stamp: use_mpi/mpi_base.f90 use_mpi/$(MPICONSTMOD).$(MOD)
+	@rm -f use_mpi/mpi_base-tmp
+	@touch use_mpi/mpi_base-tmp
+	@( cd use_mpi && \
 	   if [ "$(FCEXT)" != "f90" ] ; then \
 	       rm -f mpi_base.$(FCEXT) ; \
 	       $(LN_S) mpi_base.f90 mpi_base.$(FCEXT) ; \
 	   fi )
-	$(mod_verbose)$(FC_COMPILE_MODS) -c src/binding/fortran/use_mpi/mpi_base.$(FCEXT) -o src/binding/fortran/use_mpi/mpi_base.lo
-	@( cd src/binding/fortran/use_mpi && \
+	$(mod_verbose)$(FC_COMPILE_MODS) -c use_mpi/mpi_base.$(FCEXT) -o use_mpi/mpi_base.lo
+	@( cd use_mpi && \
 	   if [ "$(FCEXT)" != "f90" ] ; then \
 	       rm -f mpi_base.$(FCEXT) ; \
 	   fi )
-	@mv src/binding/fortran/use_mpi/mpi_base-tmp src/binding/fortran/use_mpi/mpi_base.$(MOD)-stamp
+	@mv use_mpi/mpi_base-tmp use_mpi/mpi_base.$(MOD)-stamp
 
-src/binding/fortran/use_mpi/mpi_base.lo src/binding/fortran/use_mpi/$(MPIBASEMOD).$(MOD): src/binding/fortran/use_mpi/mpi_base.$(MOD)-stamp
+use_mpi/mpi_base.lo use_mpi/$(MPIBASEMOD).$(MOD): use_mpi/mpi_base.$(MOD)-stamp
 ## Recover from the removal of $@
 	@if test -f $@; then :; else \
-	  trap 'rm -rf src/binding/fortran/use_mpi/mpi_base-lock src/binding/fortran/use_mpi/mpi_base.$(MOD)-stamp' 1 2 13 15; \
-	  if mkdir src/binding/fortran/use_mpi/mpi_base-lock 2>/dev/null; then \
+	  trap 'rm -rf use_mpi/mpi_base-lock use_mpi/mpi_base.$(MOD)-stamp' 1 2 13 15; \
+	  if mkdir use_mpi/mpi_base-lock 2>/dev/null; then \
 ## This code is being executed by the first process.
-	    rm -f src/binding/fortran/use_mpi/mpi_base.$(MOD)-stamp; \
-	    $(MAKE) $(AM_MAKEFLAGS) src/binding/fortran/use_mpi/mpi_base.$(MOD)-stamp; \
-	    rmdir src/binding/fortran/use_mpi/mpi_base-lock; \
+	    rm -f use_mpi/mpi_base.$(MOD)-stamp; \
+	    $(MAKE) $(AM_MAKEFLAGS) use_mpi/mpi_base.$(MOD)-stamp; \
+	    rmdir use_mpi/mpi_base-lock; \
 	  else \
 ## This code is being executed by the follower processes.
 ## Wait until the first process is done.
-	    while test -d src/binding/fortran/use_mpi/mpi_base-lock; do sleep 1; done; \
+	    while test -d use_mpi/mpi_base-lock; do sleep 1; done; \
 ## Succeed if and only if the first process succeeded.
-	    test -f src/binding/fortran/use_mpi/mpi_base.$(MOD)-stamp; exit $$?; \
+	    test -f use_mpi/mpi_base.$(MOD)-stamp; exit $$?; \
 	  fi; \
 	fi
 
-CLEANFILES += src/binding/fortran/use_mpi/mpi_base.$(MOD)-stamp src/binding/fortran/use_mpi/$(MPIBASEMOD).$(MOD) src/binding/fortran/use_mpi/mpi_base.lo src/binding/fortran/use_mpi/mpi_base-tmp
+CLEANFILES += use_mpi/mpi_base.$(MOD)-stamp use_mpi/$(MPIBASEMOD).$(MOD) use_mpi/mpi_base.lo use_mpi/mpi_base-tmp
 
-src/binding/fortran/use_mpi/pmpi_base.$(MOD)-stamp: src/binding/fortran/use_mpi/pmpi_base.f90 src/binding/fortran/use_mpi/$(MPICONSTMOD).$(MOD)
-	@rm -f src/binding/fortran/use_mpi/pmpi_base-tmp
-	@touch src/binding/fortran/use_mpi/pmpi_base-tmp
-	@( cd src/binding/fortran/use_mpi && \
+use_mpi/pmpi_base.$(MOD)-stamp: use_mpi/pmpi_base.f90 use_mpi/$(MPICONSTMOD).$(MOD)
+	@rm -f use_mpi/pmpi_base-tmp
+	@touch use_mpi/pmpi_base-tmp
+	@( cd use_mpi && \
 	   if [ "$(FCEXT)" != "f90" ] ; then \
 	       rm -f pmpi_base.$(FCEXT) ; \
 	       $(LN_S) pmpi_base.f90 pmpi_base.$(FCEXT) ; \
 	   fi )
-	$(mod_verbose)$(FC_COMPILE_MODS) -c src/binding/fortran/use_mpi/pmpi_base.$(FCEXT) -o src/binding/fortran/use_mpi/pmpi_base.lo
-	@( cd src/binding/fortran/use_mpi && \
+	$(mod_verbose)$(FC_COMPILE_MODS) -c use_mpi/pmpi_base.$(FCEXT) -o use_mpi/pmpi_base.lo
+	@( cd use_mpi && \
 	   if [ "$(FCEXT)" != "f90" ] ; then \
 	       rm -f pmpi_base.$(FCEXT) ; \
 	   fi )
-	@mv src/binding/fortran/use_mpi/pmpi_base-tmp src/binding/fortran/use_mpi/pmpi_base.$(MOD)-stamp
+	@mv use_mpi/pmpi_base-tmp use_mpi/pmpi_base.$(MOD)-stamp
 
-src/binding/fortran/use_mpi/pmpi_base.lo src/binding/fortran/use_mpi/$(PMPIBASEMOD).$(MOD): src/binding/fortran/use_mpi/pmpi_base.$(MOD)-stamp
+use_mpi/pmpi_base.lo use_mpi/$(PMPIBASEMOD).$(MOD): use_mpi/pmpi_base.$(MOD)-stamp
 ## Recover from the removal of $@
 	@if test -f $@; then :; else \
-	  trap 'rm -rf src/binding/fortran/use_mpi/pmpi_base-lock src/binding/fortran/use_mpi/pmpi_base.$(MOD)-stamp' 1 2 13 15; \
-	  if mkdir src/binding/fortran/use_mpi/pmpi_base-lock 2>/dev/null; then \
+	  trap 'rm -rf use_mpi/pmpi_base-lock use_mpi/pmpi_base.$(MOD)-stamp' 1 2 13 15; \
+	  if mkdir use_mpi/pmpi_base-lock 2>/dev/null; then \
 ## This code is being executed by the first process.
-	    rm -f src/binding/fortran/use_mpi/pmpi_base.$(MOD)-stamp; \
-	    $(MAKE) $(AM_MAKEFLAGS) src/binding/fortran/use_mpi/pmpi_base.$(MOD)-stamp; \
-	    rmdir src/binding/fortran/use_mpi/pmpi_base-lock; \
+	    rm -f use_mpi/pmpi_base.$(MOD)-stamp; \
+	    $(MAKE) $(AM_MAKEFLAGS) use_mpi/pmpi_base.$(MOD)-stamp; \
+	    rmdir use_mpi/pmpi_base-lock; \
 	  else \
 ## This code is being executed by the follower processes.
 ## Wait until the first process is done.
-	    while test -d src/binding/fortran/use_mpi/pmpi_base-lock; do sleep 1; done; \
+	    while test -d use_mpi/pmpi_base-lock; do sleep 1; done; \
 ## Succeed if and only if the first process succeeded.
-	    test -f src/binding/fortran/use_mpi/pmpi_base.$(MOD)-stamp; exit $$?; \
+	    test -f use_mpi/pmpi_base.$(MOD)-stamp; exit $$?; \
 	  fi; \
 	fi
 
-CLEANFILES += src/binding/fortran/use_mpi/pmpi_base.$(MOD)-stamp src/binding/fortran/use_mpi/$(PMPIBASEMOD).$(MOD) src/binding/fortran/use_mpi/pmpi_base.lo src/binding/fortran/use_mpi/pmpi_base-tmp
+CLEANFILES += use_mpi/pmpi_base.$(MOD)-stamp use_mpi/$(PMPIBASEMOD).$(MOD) use_mpi/pmpi_base.lo use_mpi/pmpi_base-tmp
 
 
 mpi_fc_modules += \
-    src/binding/fortran/use_mpi/$(MPIMOD).$(MOD) \
-    src/binding/fortran/use_mpi/$(MPISIZEOFMOD).$(MOD) \
-    src/binding/fortran/use_mpi/$(MPICONSTMOD).$(MOD) \
-    src/binding/fortran/use_mpi/$(MPIBASEMOD).$(MOD) \
-    src/binding/fortran/use_mpi/$(PMPIBASEMOD).$(MOD)
+    use_mpi/$(MPIMOD).$(MOD) \
+    use_mpi/$(MPISIZEOFMOD).$(MOD) \
+    use_mpi/$(MPICONSTMOD).$(MOD) \
+    use_mpi/$(MPIBASEMOD).$(MOD) \
+    use_mpi/$(PMPIBASEMOD).$(MOD)
 
 # We need a free-format version of mpif.h with no external commands,
 # including declarations such as `REAL*8 MPI_WTIME, PMPI_WTIME`.
 # Filtering on `PMPI_` covers those lines.
 # NOTE: do not use extended regex to allow posix sed.
-src/binding/fortran/use_mpi/mpifnoext.h: src/binding/fortran/mpif_h/mpif.h
+use_mpi/mpifnoext.h: mpif_h/mpif.h
 	rm -f $@
 	sed -e 's/^C/\!/g' -e '/EXTERNAL/d' \
-	-e '/PMPI_/d' src/binding/fortran/mpif_h/mpif.h > $@
+	-e '/PMPI_/d' mpif_h/mpif.h > $@
 
-CLEANFILES += src/binding/fortran/use_mpi/mpifnoext.h
+CLEANFILES += use_mpi/mpifnoext.h
 
 MAINTAINERCLEANFILES += $(mpi_sources) fproto.h
 CLEANFILES += $(mpi_fc_modules)
