b5cd0ed6e7d3f9809b547ea575115c38c89285a4
Hui Zhou <hzhou321@anl.gov>
2026-02-06 16:34:03 -0600

    coll/circ_graph: Add MPIR_cga_init/finalize
    Add placeholder code for MPIR_cga_init and MPIR_cga_finalize in
    preparation of pre-allocating genq pools for pipelining chunks.

--- src/mpi/coll/algorithms/circ_graph/Makefile.mk
+++ src/mpi/coll/algorithms/circ_graph/Makefile.mk
@@ -7,4 +7,5 @@ AM_CPPFLAGS += -I$(top_srcdir)/src/mpi/coll/algorithms/circ_graph
 
 mpi_core_sources += \
     src/mpi/coll/algorithms/circ_graph/circ_graph.c \
+    src/mpi/coll/algorithms/circ_graph/cga_init.c \
     src/mpi/coll/algorithms/circ_graph/cga_request_queue.c
