9973310837059ba21c3192b1a06fd87bbe674c55
Carsten Clauss <clauss@par-tec.com>
2026-04-28 17:02:26 +0200

    mpid/ucc: introduce MPIDI_common_ucc_req_t struct
    Instead of passing `ucc_coll_req_h` and `MPIR_Request` explicitly,
    both are now encapsulated within the new `MPIDI_common_ucc_req_t`
    struct to streamline the interfaces.

--- src/mpid/common/ucc/mpid_ucc_alltoallv.c
+++ src/mpid/common/ucc/mpid_ucc_alltoallv.c
@@ -11,8 +11,8 @@ static inline ucc_status_t mpidi_ucc_alltoallv_init(const void *sbuf, const MPI_
                                                     const MPI_Aint sdispls[], MPI_Datatype sdtype,
                                                     void *rbuf, const MPI_Aint rcounts[],
                                                     const MPI_Aint rdispls[], MPI_Datatype rdtype,
-                                                    MPIR_Comm * comm_ptr, ucc_coll_req_h * req,
-                                                    MPIR_Request * coll_req)
+                                                    MPIR_Comm * comm_ptr,
+                                                    MPIDI_common_ucc_req_t * req)
 {
     bool is_inplace = (sbuf == MPI_IN_PLACE);
     int comm_size = MPIR_Comm_size(comm_ptr);
@@ -78,7 +78,8 @@ static inline ucc_status_t mpidi_ucc_alltoallv_init(const void *sbuf, const MPI_
                                                  mpidi_ucc_dtype_to_str(ucc_rdt));
     }
 
-    MPIDI_COMMON_UCC_REQ_INIT(coll_req, req, coll, comm_ptr);
+    MPIDI_COMMON_UCC_REQ_INIT(req, coll, comm_ptr);
+
     return UCC_OK;
   fallback:
     return UCC_ERR_NOT_SUPPORTED;
@@ -89,7 +90,7 @@ int MPIDI_common_ucc_alltoallv(const void *sbuf, const MPI_Aint scounts[], const
                                const MPI_Aint rdispls[], MPI_Datatype rdtype, MPIR_Comm * comm_ptr)
 {
     int mpidi_ucc_err = MPIDI_COMMON_UCC_RETVAL_SUCCESS;
-    ucc_coll_req_h req;
+    MPIDI_common_ucc_req_t req = { 0 };
 
     MPIDI_COMMON_UCC_CHECK_ENABLED(comm_ptr, alltoallv);
 
@@ -97,9 +98,9 @@ int MPIDI_common_ucc_alltoallv(const void *sbuf, const MPI_Aint scounts[], const
 
     MPIDI_COMMON_UCC_CALL_AND_CHECK(mpidi_ucc_alltoallv_init
                                     (sbuf, scounts, sdispls, sdtype, rbuf, rcounts, rdispls, rdtype,
-                                     comm_ptr, &req, NULL));
-    MPIDI_COMMON_UCC_POST_AND_CHECK(req);
-    MPIDI_COMMON_UCC_WAIT_AND_CHECK(req);
+                                     comm_ptr, &req));
+    MPIDI_COMMON_UCC_POST_AND_CHECK(req.ucc_req);
+    MPIDI_COMMON_UCC_WAIT_AND_CHECK(req.ucc_req);
 
     MPIDI_COMMON_UCC_VERBOSE_COLLOP_DONE_SUCCESS(alltoallv);
 
