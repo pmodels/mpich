551fbb04718fd02a814daae2d51a958448741dde
Hui Zhou <hzhou321@anl.gov>
2026-05-06 08:29:27 -0500

    mpl: use MPL_MEM_HASH and bool in mpl_hash
    Since MPL_hash is effectively a memory storage device, add a memory
    class to track it.
    
    Use bool instead of int or char value of 0 and 1.
    
    Fix MPL_hash_has, it need check whether the hash is empty.

--- src/mpl/include/mpl_hash.h
+++ src/mpl/include/mpl_hash.h
@@ -13,14 +13,14 @@ struct strpool {
 struct MPL_hash {
     int n_size;
     int n_count;
-    char *p_exist;
+    bool *p_exist;
     int *p_key;
     struct strpool pool;
     int *p_val;
 };
 
 struct MPL_hash *MPL_hash_new(void);
-int MPL_hash_has(struct MPL_hash *hv, const char *s_key);
+bool MPL_hash_has(struct MPL_hash *hv, const char *s_key);
 void MPL_hash_set(struct MPL_hash *hv, const char *s_key, const char *s_value);
 char *MPL_hash_get(struct MPL_hash *hv, const char *s_key);
 void MPL_hash_free(struct MPL_hash *hv);
