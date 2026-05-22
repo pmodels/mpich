93f25ed21fb71bd9272237af252a9e42e13c2009
Hui Zhou <hzhou321@anl.gov>
2026-05-22 11:03:57 -0500

    datatype: create_f90 do not depend on fortran
    MPI_Type_creat_f90_{integer,real,complex} need return a predefined
    matching type that should also support builtin reduction operations. We
    only can do that by mapping to support internal types. Thus it does not
    depend on whether a fortran type is supported by the fortran compiler or
    not. If a fortran type don't have a matching internal type, we can't
    return that type to the user anyway.
    
    The MPI standard does say that "It is erroneous to supply values of p
    and r not supported by the (Fortran) compiler". This is a constraint on
    user. If we want to be pedantic, the fortran binding should check and
    return error before it calls the C implementation.

--- src/mpi/datatype/create_f90.c
+++ src/mpi/datatype/create_f90.c
@@ -4,36 +4,57 @@
  */
 
 #include "mpiimpl.h"
-#include "mpif90model.h"
 
-#ifndef HAVE_FORTRAN_BINDING
-int MPIR_Type_create_f90_integer_impl(int range, MPI_Datatype * newtype)
-{
-    *newtype = MPI_DATATYPE_NULL;
-    return MPI_SUCCESS;
-}
-
-int MPIR_Type_create_f90_real_impl(int precision, int range, MPI_Datatype * newtype)
-{
-    *newtype = MPI_DATATYPE_NULL;
-    return MPI_SUCCESS;
-}
-
-int MPIR_Type_create_f90_complex_impl(int precision, int range, MPI_Datatype * newtype)
-{
-    *newtype = MPI_DATATYPE_NULL;
-    return MPI_SUCCESS;
-}
-
-#else /* HAVE_FORTRAN_BINDING */
 typedef struct intModel {
-    int range, kind, bytes;
+    int range;
+    MPI_Datatype basetype;
 } intModel;
 
 typedef struct realModel {
     int digits, exponent;
-    MPI_Datatype dtype;
+    MPI_Datatype basetype;
 } realModel;
+
+static intModel f90_integer_map[] = {
+    {1, MPIR_INT8},
+    {2, MPIR_INT16},
+    {4, MPIR_INT32},
+    {8, MPIR_INT64},
+#if defined(MPIR_INT128_CTYPE)
+    {16, MPIR_INT128},
+#endif
+};
+
+static realModel f90_real_model[] = {
+#ifdef MPIR_FLOAT16_CTYPE
+    {3, 4, MPIR_FLOAT16},
+#endif
+    {6, 37, MPIR_FLOAT32},
+    {15, 307, MPIR_FLOAT64},
+#ifdef MPIR_ALT_FLOAT96_CTYPE
+    {18, 4931, MPIR_ALT_FLOAT96},       /* long double on Linux 32-bit */
+#endif
+#ifdef MPIR_ALT_FLOAT128_CTYPE
+    {18, 4931, MPIR_ALT_FLOAT128},      /* long double on Linux 64-bit */
+#endif
+#ifdef MPIR_FLOAT16_CTYPE
+    {33, 4931, MPIR_FLOAT128},  /* long double on Arm64, __float128 */
+#endif
+};
+
+static realModel f90_complex_model[] = {
+    {6, 37, MPIR_COMPLEX32},
+    {15, 307, MPIR_COMPLEX64},
+#ifdef MPIR_ALT_FLOAT96_CTYPE
+    {18, 4931, MPIR_ALT_COMPLEX96},     /* long double on Linux 32-bit */
+#endif
+#ifdef MPIR_ALT_FLOAT128_CTYPE
+    {18, 4931, MPIR_ALT_COMPLEX128},    /* long double on Linux 64-bit */
+#endif
+#ifdef MPIR_FLOAT16_CTYPE
+    {33, 4931, MPIR_COMPLEX128},        /* long double on Arm64, __float128 */
+#endif
+};
 
 static int MPIR_Create_unnamed_predefined(MPI_Datatype old, int combiner,
                                           int r, int p, MPI_Datatype * new_ptr);
@@ -42,32 +63,11 @@ int MPIR_Type_create_f90_integer_impl(int range, MPI_Datatype * newtype)
 {
     int mpi_errno = MPI_SUCCESS;
 
-    static intModel f90_integer_map[] = { MPIR_F90_INTEGER_MODEL_MAP {0, 0, 0} };
-
     MPI_Datatype basetype = MPI_DATATYPE_NULL;
-    for (int i = 0; f90_integer_map[i].range > 0; i++) {
+    int num = sizeof(f90_integer_map) / sizeof(f90_integer_map[0]);
+    for (int i = 0; i < num; i++) {
         if (f90_integer_map[i].range >= range) {
-            /* Find the corresponding INTEGER type */
-            int bytes = f90_integer_map[i].bytes;
-            switch (bytes) {
-                case 1:
-                    basetype = MPIR_INT8;
-                    break;
-                case 2:
-                    basetype = MPIR_INT16;
-                    break;
-                case 4:
-                    basetype = MPIR_INT32;
-                    break;
-                case 8:
-                    basetype = MPIR_INT64;
-                    break;
-                case 16:
-                    basetype = MPIR_INT128;
-                    break;
-                default:
-                    break;
-            }
+            basetype = f90_integer_map[i].basetype;
             break;
         }
     }
@@ -92,34 +92,12 @@ int MPIR_Type_create_f90_integer_impl(int range, MPI_Datatype * newtype)
 int MPIR_Type_create_f90_real_impl(int precision, int range, MPI_Datatype * newtype)
 {
     int mpi_errno = MPI_SUCCESS;
-    MPI_Datatype basetype;
 
-
-    static int setupPredefTypes = 1;
-    static realModel f90_real_model[2] = {
-        {MPIR_F90_REAL_MODEL, MPIR_REAL_INTERNAL},
-        {MPIR_F90_DOUBLE_MODEL, MPIR_DOUBLE_PRECISION_INTERNAL}
-    };
-
-    /* MPI 2.1, Section 16.2, page 473 lines 12-27 make it clear that
-     * these types must returned the combiner version. */
-    if (setupPredefTypes) {
-        setupPredefTypes = 0;
-        for (int i = 0; i < 2; i++) {
-            MPI_Datatype oldType = f90_real_model[i].dtype;
-            mpi_errno = MPIR_Create_unnamed_predefined(oldType,
-                                                       MPI_COMBINER_F90_REAL,
-                                                       f90_real_model[i].digits,
-                                                       f90_real_model[i].exponent,
-                                                       &f90_real_model[i].dtype);
-            MPIR_ERR_CHECK(mpi_errno);
-        }
-    }
-
-    basetype = MPI_DATATYPE_NULL;
-    for (int i = 0; i < 2; i++) {
+    MPI_Datatype basetype = MPI_DATATYPE_NULL;
+    int num = sizeof(f90_real_model) / sizeof(f90_real_model[0]);
+    for (int i = 0; i < num; i++) {
         if (f90_real_model[i].digits >= precision && f90_real_model[i].exponent >= range) {
-            basetype = f90_real_model[i].dtype;
+            basetype = f90_real_model[i].basetype;
             break;
         }
     }
@@ -146,33 +124,12 @@ int MPIR_Type_create_f90_real_impl(int precision, int range, MPI_Datatype * newt
 int MPIR_Type_create_f90_complex_impl(int precision, int range, MPI_Datatype * newtype)
 {
     int mpi_errno = MPI_SUCCESS;
-    MPI_Datatype basetype;
 
-    static int setupPredefTypes = 1;
-    static realModel f90_real_model[2] = {
-        {MPIR_F90_REAL_MODEL, MPIR_COMPLEX_INTERNAL},
-        {MPIR_F90_DOUBLE_MODEL, MPIR_DOUBLE_COMPLEX_INTERNAL}
-    };
-
-    /* MPI 2.1, Section 16.2, page 473 lines 12-27 make it clear that
-     * these types must return the combiner version. */
-    if (setupPredefTypes) {
-        setupPredefTypes = 0;
-        for (int i = 0; i < 2; i++) {
-            MPI_Datatype oldType = f90_real_model[i].dtype;
-            mpi_errno = MPIR_Create_unnamed_predefined(oldType,
-                                                       MPI_COMBINER_F90_COMPLEX,
-                                                       f90_real_model[i].digits,
-                                                       f90_real_model[i].exponent,
-                                                       &f90_real_model[i].dtype);
-            MPIR_ERR_CHECK(mpi_errno);
-        }
-    }
-
-    basetype = MPI_DATATYPE_NULL;
-    for (int i = 0; i < 2; i++) {
+    MPI_Datatype basetype = MPI_DATATYPE_NULL;
+    int num = sizeof(f90_complex_model) / sizeof(f90_complex_model[0]);
+    for (int i = 0; i < num; i++) {
         if (f90_real_model[i].digits >= precision && f90_real_model[i].exponent >= range) {
-            basetype = f90_real_model[i].dtype;
+            basetype = f90_real_model[i].basetype;
             break;
         }
     }
@@ -196,7 +153,6 @@ int MPIR_Type_create_f90_complex_impl(int precision, int range, MPI_Datatype * n
     goto fn_exit;
 }
 
-#ifdef HAVE_FORTRAN_BINDING
 /* The MPI standard requires that the datatypes that are returned from
    the MPI_Create_f90_xxx be both predefined and return the r (and p
    if real/complex) with which it was created.  The following contains routines
@@ -315,89 +271,3 @@ static int MPIR_Create_unnamed_predefined(MPI_Datatype old, int combiner,
   fn_fail:
     return mpi_errno;
 }
-#endif
-
-/*
-   The simple approach used here is to store (unordered) the precision and
-   range of each type that is created as a result of a user-call to
-   one of the MPI_Type_create_f90_xxx routines.  The standard requires
-   that the *same* handle be returned to allow simple == comparisons.
-   A finalize callback is added to free up any remaining storage
-*/
-
-#if 0
-/* Given the requested range and the length of the type in bytes,
-   return the matching datatype */
-
-int MPIR_Match_f90_int(int range, int length, MPI_Datatype * newtype)
-{
-    /* Has this type been requested before? */
-    for (int i = 0; i < nAllocInteger; i++) {
-        if (f90IntTypes[i].r == range) {
-            *newtype = f90IntTypes[i].d;
-            return 0;
-        }
-    }
-
-    /* No.  Try to add it */
-    if (nAllocInteger >= MAX_F90_TYPES) {
-        int line = -1;          /* Hack to suppress warning message from
-                                 * extracterrmsgs, since this code should
-                                 * reflect the routine from which it was called,
-                                 * since the decomposition of the internal routine
-                                 * is of no relevance to either the user or
-                                 * developer */
-        return MPIR_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE,
-                                    "MPI_Type_create_f90_integer", __LINE__,
-                                    MPI_ERR_INTERN, "**f90typetoomany", 0);
-    }
-
-    /* Temporary */
-    *newtype = MPI_DATATYPE_NULL;
-
-    mpi_errno = MPIR_Create_unnamed_predefined(,, r, -1, newtype);
-    f90IntTypes[nAllocInteger].r = range;
-    f90IntTypes[nAllocInteger].p = -1;
-    f90IntTypes[nAllocInteger++].d = *newtype;
-
-    return 0;
-}
-
-/* Build a new type */
-static int MPIR_Create_unnamed_predefined(MPI_Datatype old, int combiner,
-                                          int r, int p, MPI_Datatype * new_ptr)
-{
-    int mpi_errno;
-
-    /* Create a contiguous type from one instance of the named type */
-    mpi_errno = MPIR_Type_contiguous(1, old, new_ptr);
-
-    /* Initialize the contents data */
-    if (mpi_errno == MPI_SUCCESS) {
-        MPIR_Datatype *new_dtp;
-        int vals[2];
-        int nvals = 0;
-
-        switch (combiner) {
-            case MPI_COMBINER_F90_INTEGER:
-                nvals = 1;
-                vals[0] = r;
-                break;
-
-            case MPI_COMBINER_F90_REAL:
-            case MPI_COMBINER_F90_COMPLEX:
-                nvals = 2;
-                vals[0] = p;
-                vals[1] = r;
-                break;
-        }
-
-        MPIR_Datatype_get_ptr(*new_ptr, new_dtp);
-        mpi_errno = MPIR_Datatype_set_contents(new_dtp, combiner, nvals, 0, 0, 0,
-                                               vals, NULL, NULL, NULL);
-    }
-
-    return mpi_errno;
-}
-#endif
-#endif /* HAVE_FORTRAN_BINDING */
