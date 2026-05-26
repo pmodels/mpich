7706dbb976094957db8c1aab2b6401b7d147d8e6
Hui Zhou <hzhou321@anl.gov>
2026-05-26 10:44:28 -0500

    binding/f2c: move interop interface to mpifort
    Move all the fortran inter-op C API to libmpifort.so. There is no
    Fortran dependence in libmpi.so, including MPI_Fint. Install Fortran
    binding to provide MPI_Fint and f2c/c2f functions.
    
    We can't use MPIR_Err_create_code now the fortran bindings are separate.
    Return MPI_ERR_ARG instead.

--- src/binding/fortran/mpif_h/f2c_impl.c
+++ src/binding/fortran/mpif_h/f2c_impl.c
@@ -3,15 +3,12 @@
  *     See COPYRIGHT in top-level directory
  */
 
-#include "mpiimpl.h"
-
-#ifndef MPICH_BUILD_MPI_ABI
+#include "mpi_fortimpl.h"
 
 int MPIR_Status_f2c_impl(const MPI_Fint * f_status, MPI_Status * c_status)
 {
     if (f_status == MPI_F_STATUS_IGNORE || f_status == MPI_F_STATUSES_IGNORE) {
-        return MPIR_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE,
-                                    __func__, __LINE__, MPI_ERR_OTHER, "**notfstatignore", 0);
+        return MPI_ERR_ARG;
     }
 #ifdef HAVE_FINT_IS_INT
     *c_status = *(MPI_Status *) f_status;
@@ -28,8 +25,7 @@ int MPIR_Status_f2c_impl(const MPI_Fint * f_status, MPI_Status * c_status)
 int MPIR_Status_c2f_impl(const MPI_Status * c_status, MPI_Fint * f_status)
 {
     if (c_status == MPI_STATUS_IGNORE || c_status == MPI_STATUSES_IGNORE) {
-        return MPIR_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE,
-                                    __func__, __LINE__, MPI_ERR_OTHER, "**notcstatignore", 0);
+        return MPI_ERR_ARG;
     }
 #ifdef HAVE_FINT_IS_INT
     *(MPI_Status *) f_status = *c_status;
@@ -46,8 +42,7 @@ int MPIR_Status_c2f_impl(const MPI_Status * c_status, MPI_Fint * f_status)
 int MPIR_Status_f2f08_impl(const MPI_Fint * f_status, MPI_F08_status * f08_status)
 {
     if (f_status == MPI_F_STATUS_IGNORE || f_status == MPI_F_STATUSES_IGNORE) {
-        return MPIR_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE,
-                                    __func__, __LINE__, MPI_ERR_OTHER, "**notfstatignore", 0);
+        return MPI_ERR_ARG;
     }
     /* f_status and f08_status are always byte-equivalent */
     *f08_status = *(MPI_F08_status *) f_status;
@@ -57,8 +52,7 @@ int MPIR_Status_f2f08_impl(const MPI_Fint * f_status, MPI_F08_status * f08_statu
 int MPIR_Status_f082f_impl(const MPI_F08_status * f08_status, MPI_Fint * f_status)
 {
     if (f08_status == MPI_F08_STATUS_IGNORE || f08_status == MPI_F08_STATUSES_IGNORE) {
-        return MPIR_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE,
-                                    __func__, __LINE__, MPI_ERR_OTHER, "**notcstatignore", 0);
+        return MPI_ERR_ARG;
     }
     /* f_status and f08_status are always byte-equivalent */
     *(MPI_F08_status *) f_status = *f08_status;
@@ -68,8 +62,7 @@ int MPIR_Status_f082f_impl(const MPI_F08_status * f08_status, MPI_Fint * f_statu
 int MPIR_Status_f082c_impl(const MPI_F08_status * f08_status, MPI_Status * c_status)
 {
     if (f08_status == MPI_F08_STATUS_IGNORE || f08_status == MPI_F08_STATUSES_IGNORE) {
-        return MPIR_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE,
-                                    __func__, __LINE__, MPI_ERR_OTHER, "**notfstatignore", 0);
+        return MPI_ERR_ARG;
     }
 #ifdef HAVE_FINT_IS_INT
     *c_status = *(MPI_Status *) f08_status;
@@ -86,8 +79,7 @@ int MPIR_Status_f082c_impl(const MPI_F08_status * f08_status, MPI_Status * c_sta
 int MPIR_Status_c2f08_impl(const MPI_Status * c_status, MPI_F08_status * f08_status)
 {
     if (c_status == MPI_STATUS_IGNORE || c_status == MPI_STATUSES_IGNORE) {
-        return MPIR_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE,
-                                    __func__, __LINE__, MPI_ERR_OTHER, "**notcstatignore", 0);
+        return MPI_ERR_ARG;
     }
 #ifdef HAVE_FINT_IS_INT
     *(MPI_Status *) f08_status = *c_status;
@@ -100,5 +92,3 @@ int MPIR_Status_c2f08_impl(const MPI_Status * c_status, MPI_F08_status * f08_sta
 #endif
     return MPI_SUCCESS;
 }
-
-#endif /* MPICH_BUILD_MPI_ABI */
