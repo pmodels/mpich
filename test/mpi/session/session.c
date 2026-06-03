ed48a9bf0fdf99005469abeec03599e9cf63e976
Sonja Happ <sonja.happ@par-tec.com>
2026-06-03 17:02:55 +0200

    test/session: Add session_mod_group and session_mod_group_re_init tests
    In these tests, the first group/comm created in a session are smaller
    than the world group/comm. Only the second group/comm are the world
    group/comm.
    
    These tests stress the group-wise bootstrapping mechanisms as well as
    testing if MPI_COMM_CREATE_FROM_GROUP returns MPI_COMM_NULL for an empty
    input group.

--- test/mpi/session/session.c
+++ test/mpi/session/session.c
@@ -20,6 +20,8 @@
  * WITH_WORLD:  Have MPI world model initialized prior to the first MPI_Session_init
  * RE_INIT:     Initialize another session after closing the previous one (only if
  *              not using MULT_INIT)
+ * MOD_GROUP:   Create the first communicator from an MPI Group that is smaller
+ *              than the world group
  */
 
 int errs = 0;
@@ -75,6 +77,9 @@ int main(int argc, char *argv[])
 
 static MPI_Session lib_shandle = MPI_SESSION_NULL;
 static MPI_Comm lib_comm = MPI_COMM_NULL;
+#ifdef MOD_GROUP
+static MPI_Comm lib_comm_group = MPI_COMM_NULL;
+#endif
 
 int check_thread_level(char *value)
 {
@@ -106,6 +111,30 @@ int library_foo_test(void)
     MPI_Comm_rank(lib_comm, &rank);
 
     int sum;
+#ifdef MOD_GROUP
+    /* Skip the reduce operation for rank that is not in smaller group/ comm */
+    if (lib_comm_group != MPI_COMM_NULL) {
+        int group_comm_size;
+        int group_comm_rank;
+
+        MPI_Comm_size(lib_comm_group, &group_comm_size);
+        MPI_Comm_rank(lib_comm_group, &group_comm_rank);
+
+        rc = MPI_Reduce(&group_comm_rank, &sum, 1, MPI_INT, MPI_SUM, 0, lib_comm_group);
+        if (rc != MPI_SUCCESS) {
+            printf("Error on reduce\n");
+            errs++;
+        }
+        if (group_comm_rank == 0) {
+            if (sum != (group_comm_size - 1) * group_comm_size / 2) {
+                printf("MPI_Reduce: expect %d, got %d\n",
+                       (group_comm_size - 1) * group_comm_size / 2, sum);
+                errs++;
+            }
+        }
+    }
+#endif
+    sum = 0;
     rc = MPI_Reduce(&rank, &sum, 1, MPI_INT, MPI_SUM, 0, lib_comm);
     if (rc != MPI_SUCCESS) {
         printf("Error on reduce\n");
@@ -171,7 +200,84 @@ void library_foo_init(void)
         errs++;
         goto fn_exit;
     }
+#ifdef MOD_GROUP
+    int grp_size, grp_rank, newgrp_size, newgrp_rank, comm_size = 0;
+    MPI_Group newgroup = MPI_GROUP_NULL;
+    MPI_Group_size(wgroup, &grp_size);
+    MPI_Group_rank(wgroup, &grp_rank);
+    int excl[1];
 
+    if (grp_size < 2) {
+        printf("This test has to be started with at least 2 processes!\n");
+        errs++;
+        goto fn_exit;
+    }
+
+    /* Create a new group where a rank is excluded */
+    excl[0] = grp_size - 2;
+    rc = MPI_Group_excl(wgroup, 1, excl, &newgroup);
+    if (rc != MPI_SUCCESS) {
+        printf("Error on MPI_Group_excl\n");
+        errs++;
+        goto fn_exit;
+    }
+
+    MPI_Group_size(newgroup, &newgrp_size);
+    MPI_Group_rank(newgroup, &newgrp_rank);
+    if (newgrp_size != grp_size - 1) {
+        printf("Error: newgrp_size = %d, expected %d\n", newgrp_size, grp_size - 1);
+        errs++;
+        goto fn_exit;
+    }
+
+    /* Use the new smaller group to create the lib_comm_group */
+    if (newgrp_rank == MPI_UNDEFINED) {
+        rc = MPI_Comm_create_from_group(MPI_GROUP_EMPTY, "group", MPI_INFO_NULL,
+                                        MPI_ERRORS_RETURN, &lib_comm_group);
+        if (lib_comm_group != MPI_COMM_NULL) {
+            printf
+                ("Error: Expected comm from group MPI_GROUP_EMPTY to be MPI_COMM_NULL, but it is not.\n");
+            errs++;
+            goto fn_exit;
+        }
+    } else {
+        rc = MPI_Comm_create_from_group(newgroup, "group", MPI_INFO_NULL,
+                                        MPI_ERRORS_RETURN, &lib_comm_group);
+        if (lib_comm_group == MPI_COMM_NULL) {
+            printf
+                ("Error: Expected comm from non-NULL group not to be MPI_COMM_NULL, but it is.\n");
+            errs++;
+            goto fn_exit;
+        }
+    }
+    if (rc != MPI_SUCCESS) {
+        errs++;
+        goto fn_exit;
+    }
+
+    if (lib_comm_group != MPI_COMM_NULL) {
+        MPI_Comm_size(lib_comm_group, &comm_size);
+        if (comm_size != newgrp_size) {
+            printf("Error: communicator for smaller group has size %d (expected %d)\n",
+                   comm_size, newgrp_size);
+            errs++;
+            goto fn_exit;
+        }
+    } else {
+        /* Only for the rank that has been excluded from the group it is ok to have MPI_COMM_NULL */
+        if (grp_rank != grp_size - 2) {
+            errs++;
+            printf("Error: communicator for smaller group is MPI_COMM_NULL\n");
+            goto fn_exit;
+        }
+    }
+
+    if (newgroup != MPI_GROUP_NULL) {
+        MPI_Group_free(&newgroup);
+    }
+
+  mod_group_end:
+#endif
     /* get a communicator */
     rc = MPI_Comm_create_from_group(wgroup, "org.mpi-forum.mpi-v4_0.example-ex10_8",
                                     MPI_INFO_NULL, MPI_ERRORS_RETURN, &lib_comm);
@@ -204,6 +310,16 @@ void library_foo_finalize(void)
             return;
         }
     }
+#ifdef MOD_GROUP
+    if (lib_comm_group != MPI_COMM_NULL) {
+        rc = MPI_Comm_free(&lib_comm_group);
+        if (rc != MPI_SUCCESS) {
+            printf("MPI_Comm_free returned %d\n", rc);
+            errs++;
+            return;
+        }
+    }
+#endif
 
     if (lib_shandle != MPI_SESSION_NULL) {
         rc = MPI_Session_finalize(&lib_shandle);
