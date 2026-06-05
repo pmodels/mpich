2b312ed41e2f75f6334aa5acc399f64bcc6a018d
Lisandro Dalcin <dalcinl@gmail.com>
2026-06-05 14:39:45 +0300

    datatype: use complex_map for creating Fortran 90 datatypes
    In MPIR_Type_create_f90_complex_impl, use the f90 complex model
    rather than the f90 real model.
    
    This is likely a copy&paste mistake from commit 66cd573.

--- src/mpi/datatype/create_f90.c
+++ src/mpi/datatype/create_f90.c
@@ -128,8 +128,8 @@ int MPIR_Type_create_f90_complex_impl(int precision, int range, MPI_Datatype * n
     MPI_Datatype basetype = MPI_DATATYPE_NULL;
     int num = sizeof(f90_complex_model) / sizeof(f90_complex_model[0]);
     for (int i = 0; i < num; i++) {
-        if (f90_real_model[i].digits >= precision && f90_real_model[i].exponent >= range) {
-            basetype = f90_real_model[i].basetype;
+        if (f90_complex_model[i].digits >= precision && f90_complex_model[i].exponent >= range) {
+            basetype = f90_complex_model[i].basetype;
             break;
         }
     }
