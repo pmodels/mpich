8a63c78f31fce94cdf72bd63f2ea8d324a514753
Hui Zhou <hzhou321@anl.gov>
2026-06-13 09:17:23 -0500

    abi: double inclusion of mpi_abi_util.c under noweak
    When weak symbols are unavailable, we build both libmpi.so and
    libpmpi.so. Internal symbols should only included in libpmpi.
    Including mpi_abi_util.c in mpi_abi_sources cause it to be included in
    both libmpi and libpmpi. This causes the internal global variable such
    as abi_datatype_builtins, that used to convert between ABI handles had
    two copies and result in inconsistencies.

--- src/binding/abi/Makefile.mk
+++ src/binding/abi/Makefile.mk
@@ -8,8 +8,10 @@ if BUILD_ABI_LIB
 include_HEADERS += src/binding/abi/mpi_abi.h
 
 mpi_abi_sources += \
-    src/binding/abi/mpi_abi_util.c \
     src/binding/abi/c_binding_abi.c \
     src/binding/abi/io_abi.c
+
+mpi_abi_core_sources += \
+    src/binding/abi/mpi_abi_util.c
 
 endif BUILD_ABI_LIB
