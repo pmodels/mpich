a24151753914283db332086f60ae763a801f3bd7
Growl <uwsy1059@qq.com>
2026-07-28 16:35:57 +0800

    Move packaging folder out of src

--- src/packaging/Makefile.mk
+++
@@ -1,7 +0,0 @@
-##
-## Copyright (C) by Argonne National Laboratory
-##     See COPYRIGHT in top-level directory
-##
-
-include $(top_srcdir)/src/packaging/pkgconfig/Makefile.mk
-include $(top_srcdir)/src/packaging/envmods/Makefile.mk
